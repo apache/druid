9f405c0b1e77567814b3b8105c6941e1aa5ba68e
Nelson Ray <ncray86@gmail.com>
2013-03-07 15:06:12 -0800

    fix constant segment balancing and separate out the differences between segment assignment and balancing

--- server/src/main/java/com/metamx/druid/master/rules/LoadRule.java
+++ server/src/main/java/com/metamx/druid/master/rules/LoadRule.java
@@ -91,7 +91,7 @@ public abstract class LoadRule implements Rule
     final MasterStats stats = new MasterStats();
 
     while (totalReplicants < expectedReplicants) {
-      final ServerHolder holder = analyzer.findNewSegmentHome(segment, serverHolderList);
+      final ServerHolder holder = analyzer.findNewSegmentHomeAssign(segment, serverHolderList);
 
       if (holder == null) {
         log.warn(
