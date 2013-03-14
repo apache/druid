055a98a89c0a0093db78f07ce54cfbc2b8d78fc9
Gian Merlino <gianmerlino@gmail.com>
2013-03-14 10:37:40 -0700

    Merger: Fix VersionConverterTask version selection by making SegmentInsertAction more flexible

--- merger/src/main/java/com/metamx/druid/merger/common/actions/SegmentInsertAction.java
+++ merger/src/main/java/com/metamx/druid/merger/common/actions/SegmentInsertAction.java
@@ -1,5 +1,6 @@
 package com.metamx.druid.merger.common.actions;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.common.base.Predicate;
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableSet;
@@ -18,20 +19,42 @@ import java.util.Set;
 
 public class SegmentInsertAction implements TaskAction<Void>
 {
+  @JsonIgnore
   private final Set<DataSegment> segments;
+
+  @JsonIgnore
+  private final boolean allowOlderVersions;
+
+  public SegmentInsertAction(Set<DataSegment> segments)
+  {
+    this(segments, false);
+  }
 
   @JsonCreator
   public SegmentInsertAction(
-      @JsonProperty("segments") Set<DataSegment> segments
+      @JsonProperty("segments") Set<DataSegment> segments,
+      @JsonProperty("allowOlderVersions") boolean allowOlderVersions
   )
   {
     this.segments = ImmutableSet.copyOf(segments);
+    this.allowOlderVersions = allowOlderVersions;
   }
 
   @JsonProperty
   public Set<DataSegment> getSegments()
   {
     return segments;
+  }
+
+  @JsonProperty
+  public boolean isAllowOlderVersions()
+  {
+    return allowOlderVersions;
+  }
+
+  public SegmentInsertAction withAllowOlderVersions(boolean _allowOlderVersions)
+  {
+    return new SegmentInsertAction(segments, _allowOlderVersions);
   }
 
   public TypeReference<Void> getReturnTypeReference()
@@ -42,7 +65,7 @@ public class SegmentInsertAction implements TaskAction<Void>
   @Override
   public Void perform(Task task, TaskActionToolbox toolbox)
   {
-    if(!toolbox.taskLockCoversSegments(task, segments, false)) {
+    if(!toolbox.taskLockCoversSegments(task, segments, allowOlderVersions)) {
       throw new ISE("Segments not covered by locks for task: %s", task.getId());
     }
 
