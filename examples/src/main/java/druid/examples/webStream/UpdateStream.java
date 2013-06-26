8bc4d7c436b79718706cd42168ca0a9f8302fdf9
Dhruv Parthasarathy <dhroovie@gmail.com>
2013-06-26 15:16:21 -0700

    added druid header

--- examples/src/main/java/druid/examples/webStream/UpdateStream.java
+++ examples/src/main/java/druid/examples/webStream/UpdateStream.java
@@ -1,3 +1,22 @@
+/*
+ * Druid - a distributed column store.
+ * Copyright (C) 2012  Metamarkets Group Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package druid.examples.webStream;
 
 import com.google.common.base.Throwables;
@@ -8,10 +27,6 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.lang.reflect.Type;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
@@ -23,7 +38,7 @@ public class UpdateStream implements Runnable
   private InputSupplier supplier;
   private BlockingQueue<Map<String, Object>> queue;
   private ObjectMapper mapper;
-  private final TypeReference<HashMap<String,Object>> typeRef;
+  private final TypeReference<HashMap<String, Object>> typeRef;
   private static final long queueWaitTime = 15L;
 
   public UpdateStream(InputSupplier supplier, BlockingQueue<Map<String, Object>> queue)
@@ -31,12 +46,13 @@ public class UpdateStream implements Runnable
     this.supplier = supplier;
     this.queue = queue;
     this.mapper = new ObjectMapper();
-    this.typeRef= new TypeReference<HashMap<String, Object>>()
+    this.typeRef = new TypeReference<HashMap<String, Object>>()
     {
     };
   }
 
-  private boolean isValid(String s){
+  private boolean isValid(String s)
+  {
     return !(s.isEmpty());
   }
 
--- examples/src/main/java/druid/examples/webStream/WebJsonSupplier.java
+++ examples/src/main/java/druid/examples/webStream/WebJsonSupplier.java
@@ -1,3 +1,22 @@
+/*
+ * Druid - a distributed column store.
+ * Copyright (C) 2012  Metamarkets Group Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package druid.examples.webStream;
 
 import com.google.common.io.InputSupplier;
@@ -6,7 +25,6 @@ import com.metamx.emitter.EmittingLogger;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
@@ -23,10 +41,10 @@ public class WebJsonSupplier implements InputSupplier<BufferedReader>
   {
     this.dimensions = dimensions;
     this.urlString = urlString;
-    try{
+    try {
       this.url = new URL(urlString);
     }
-    catch (Exception e){
+    catch (Exception e) {
       e.printStackTrace();
       log.info("Malformed url");
     }
@@ -36,7 +54,7 @@ public class WebJsonSupplier implements InputSupplier<BufferedReader>
   public BufferedReader getInput() throws IOException
   {
     URL url = new URL(urlString);
-    URLConnection connection =  url.openConnection();
+    URLConnection connection = url.openConnection();
     connection.setDoInput(true);
     //connection.setDoOutput(true);
 
