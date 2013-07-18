d4afeb5ebe3fff3d6c22350b4b46435e1256af98
cheddar <cheddar@metamarkets.com>
2013-07-18 11:18:28 -0700

    1) upload.sh pushes up to artifacts/releases 2) Delete some commented stuff in build.sh

--- upload.sh
+++ upload.sh
@@ -3,4 +3,4 @@
 #
 # Script to upload tarball of assembly build to static.druid.io for serving
 #
-s3cmd put services/target/druid-services-*-bin.tar.gz s3://static.druid.io/artifacts/
+s3cmd put services/target/druid-services-*-bin.tar.gz s3://static.druid.io/artifacts/releases
