e283de68318f4e0568cc703c357e695c72d8bce1
fjy <fangjin@metamarkets.com>
2013-08-20 19:34:30 -0700

    fix another bug with RTR to remove things correctly from running tasks

--- indexing-service/src/main/java/com/metamx/druid/indexing/coordinator/RemoteTaskRunner.java
+++ indexing-service/src/main/java/com/metamx/druid/indexing/coordinator/RemoteTaskRunner.java
@@ -661,7 +661,7 @@ public class RemoteTaskRunner implements TaskRunner, TaskLogProvider
         );
         tasksToFail.addAll(zkWorker.getRunningTaskIds());
         for (String assignedTask : tasksToFail) {
-          RemoteTaskRunnerWorkItem taskRunnerWorkItem = runningTasks.get(assignedTask);
+          RemoteTaskRunnerWorkItem taskRunnerWorkItem = runningTasks.remove(assignedTask);
           if (taskRunnerWorkItem != null) {
             String taskPath = JOINER.join(config.getIndexerTaskPath(), worker.getHost(), assignedTask);
             if (cf.checkExists().forPath(taskPath) != null) {
