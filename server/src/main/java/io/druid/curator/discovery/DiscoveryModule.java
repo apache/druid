be5bb7f2eb13f709464acfd8f04eca4fe2628965
fjy <fangjin@metamarkets.com>
2013-09-24 14:20:56 -0700

    fix lifecycle startup/stop ordering problem with discovery module and make druid able to load local extensions

--- server/src/main/java/io/druid/curator/discovery/DiscoveryModule.java
+++ server/src/main/java/io/druid/curator/discovery/DiscoveryModule.java
@@ -59,7 +59,7 @@ import java.util.concurrent.ThreadFactory;
 /**
  * The DiscoveryModule allows for the registration of Keys of DruidNode objects, which it intends to be
  * automatically announced at the end of the lifecycle start.
- *
+ * <p/>
  * In order for this to work a ServiceAnnouncer instance *must* be injected and instantiated first.
  * This can often be achieved by registering ServiceAnnouncer.class with the LifecycleModule.
  */
@@ -69,19 +69,25 @@ public class DiscoveryModule implements Module
 
   /**
    * Requests that the un-annotated DruidNode instance be injected and published as part of the lifecycle.
-   *
+   * <p/>
    * That is, this module will announce the DruidNode instance returned by
    * injector.getInstance(Key.get(DruidNode.class)) automatically.
    * Announcement will happen in the LAST stage of the Lifecycle
    */
   public static void registerDefault(Binder binder)
   {
-    registerKey(binder, Key.get(new TypeLiteral<DruidNode>(){}));
+    registerKey(
+        binder, Key.get(
+        new TypeLiteral<DruidNode>()
+        {
+        }
+    )
+    );
   }
 
   /**
    * Requests that the annotated DruidNode instance be injected and published as part of the lifecycle.
-   *
+   * <p/>
    * That is, this module will announce the DruidNode instance returned by
    * injector.getInstance(Key.get(DruidNode.class, annotation)) automatically.
    * Announcement will happen in the LAST stage of the Lifecycle
@@ -90,12 +96,18 @@ public class DiscoveryModule implements Module
    */
   public static void register(Binder binder, Annotation annotation)
   {
-    registerKey(binder, Key.get(new TypeLiteral<DruidNode>(){}, annotation));
+    registerKey(
+        binder, Key.get(
+        new TypeLiteral<DruidNode>()
+        {
+        }, annotation
+    )
+    );
   }
 
   /**
    * Requests that the annotated DruidNode instance be injected and published as part of the lifecycle.
-   *
+   * <p/>
    * That is, this module will announce the DruidNode instance returned by
    * injector.getInstance(Key.get(DruidNode.class, annotation)) automatically.
    * Announcement will happen in the LAST stage of the Lifecycle
@@ -104,12 +116,18 @@ public class DiscoveryModule implements Module
    */
   public static void register(Binder binder, Class<? extends Annotation> annotation)
   {
-    registerKey(binder, Key.get(new TypeLiteral<DruidNode>(){}, annotation));
+    registerKey(
+        binder, Key.get(
+        new TypeLiteral<DruidNode>()
+        {
+        }, annotation
+    )
+    );
   }
 
   /**
    * Requests that the keyed DruidNode instance be injected and published as part of the lifecycle.
-   *
+   * <p/>
    * That is, this module will announce the DruidNode instance returned by
    * injector.getInstance(Key.get(DruidNode.class, annotation)) automatically.
    * Announcement will happen in the LAST stage of the Lifecycle
@@ -137,7 +155,9 @@ public class DiscoveryModule implements Module
           .asEagerSingleton();
   }
 
-  @Provides @LazySingleton @Named(NAME)
+  @Provides
+  @LazySingleton
+  @Named(NAME)
   public CuratorServiceAnnouncer getServiceAnnouncer(
       final CuratorServiceAnnouncer announcer,
       final Injector injector,
@@ -181,7 +201,8 @@ public class DiscoveryModule implements Module
     return announcer;
   }
 
-  @Provides @LazySingleton
+  @Provides
+  @LazySingleton
   public ServiceDiscovery<Void> getServiceDiscovery(
       CuratorFramework curator,
       CuratorDiscoveryConfig config,
@@ -217,14 +238,14 @@ public class DiscoveryModule implements Module
               throw Throwables.propagate(e);
             }
           }
-        },
-        Lifecycle.Stage.LAST
+        }
     );
 
     return serviceDiscovery;
   }
 
-  @Provides @LazySingleton
+  @Provides
+  @LazySingleton
   public ServerDiscoveryFactory getServerDiscoveryFactory(
       ServiceDiscovery<Void> serviceDiscovery
   )
