61367c6b13701c28cd127789440f7210604cbc0d
fjy <fangjin@metamarkets.com>
2013-11-14 13:16:38 -0800

    prepare for next release

--- services/src/main/java/io/druid/cli/CliHadoopIndexer.java
+++ services/src/main/java/io/druid/cli/CliHadoopIndexer.java
@@ -40,7 +40,7 @@ import java.util.List;
  */
 @Command(
     name = "hadoop",
-    description = "Runs the batch Hadoop Druid Indexer, see http://druid.io/docs/0.6.15/Batch-ingestion.html for a description."
+    description = "Runs the batch Hadoop Druid Indexer, see http://druid.io/docs/0.6.16/Batch-ingestion.html for a description."
 )
 public class CliHadoopIndexer implements Runnable
 {
