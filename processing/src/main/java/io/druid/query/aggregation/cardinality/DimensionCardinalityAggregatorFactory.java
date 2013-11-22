c29abeb8c72458b75da4fa6c0354b5b7cca31a80
Eric Tschetter <cheddar@tidepool.org>
2013-11-22 10:31:44 -0600

    Add dimension cardinality aggregator

---
+++ processing/src/main/java/io/druid/query/aggregation/cardinality/DimensionCardinalityAggregatorFactory.java
@@ -0,0 +1,195 @@
+package io.druid.query.aggregation.cardinality;
+
+
+import com.clearspring.analytics.stream.cardinality.CardinalityMergeException;
+import com.clearspring.analytics.stream.cardinality.HyperLogLogPlus;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Charsets;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
+import com.google.common.collect.Lists;
+import com.google.common.primitives.Longs;
+import com.metamx.common.ISE;
+import io.druid.query.aggregation.Aggregator;
+import io.druid.query.aggregation.AggregatorFactory;
+import io.druid.query.aggregation.BufferAggregator;
+import io.druid.query.aggregation.NoopAggregator;
+import io.druid.query.aggregation.NoopBufferAggregator;
+import io.druid.segment.ColumnSelectorFactory;
+import io.druid.segment.ObjectColumnSelector;
+import org.apache.commons.codec.binary.Base64;
+import org.codehaus.jackson.annotate.JsonCreator;
+
+import java.nio.ByteBuffer;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ *
+ */
+public class DimensionCardinalityAggregatorFactory implements AggregatorFactory
+{
+  private static final byte[] CACHE_KEY = new byte[]{0x9};
+
+  private final String name;
+  private final String fieldName;
+
+  @JsonCreator
+  public DimensionCardinalityAggregatorFactory(
+      @JsonProperty("name") String name,
+      @JsonProperty("fieldName") String fieldName
+  )
+  {
+    Preconditions.checkNotNull(name, "must specify a name on DimensionCardinality aggregators");
+    Preconditions.checkNotNull(fieldName, "must specify a fieldName on DimensionCardinality aggregators");
+
+    this.name = name;
+    this.fieldName = fieldName;
+  }
+
+  @Override
+  public Aggregator factorize(ColumnSelectorFactory metricFactory)
+  {
+    ObjectColumnSelector selector = metricFactory.makeObjectColumnSelector(fieldName);
+
+    if (selector == null) {
+      return new NoopAggregator(name)
+      {
+        @Override
+        public Object get()
+        {
+          return DimensionCardinalityAggregator.makeHllPlus();
+        }
+      };
+    }
+
+    return new DimensionCardinalityAggregator(name, selector);
+  }
+
+  @Override
+  public BufferAggregator factorizeBuffered(ColumnSelectorFactory metricFactory)
+  {
+    ObjectColumnSelector selector = metricFactory.makeObjectColumnSelector(fieldName);
+
+    if (selector == null) {
+      return new NoopBufferAggregator(){
+        @Override
+        public Object get(ByteBuffer buf, int position)
+        {
+          return DimensionCardinalityAggregator.makeHllPlus();
+        }
+      };
+    }
+
+    return new DimensionCardinalityBufferAggregator(selector);
+  }
+
+  @Override
+  public Comparator getComparator()
+  {
+    return new Comparator()
+    {
+      @Override
+      public int compare(Object o1, Object o2)
+      {
+        return Longs.compare(((HyperLogLogPlus) o1).cardinality(), ((HyperLogLogPlus) o2).cardinality());
+      }
+    };
+  }
+
+  @Override
+  public Object combine(Object lhs, Object rhs)
+  {
+    try {
+      return ((HyperLogLogPlus) lhs).merge((HyperLogLogPlus) rhs);
+    } catch (CardinalityMergeException e) {
+      throw Throwables.propagate(e);
+    }
+  }
+
+  @Override
+  public AggregatorFactory getCombiningFactory()
+  {
+    return new DimensionCardinalityAggregatorFactory(name, name);
+  }
+
+  @Override
+  public Object deserialize(Object object)
+  {
+    final byte[] bytes;
+    if (object instanceof String) {
+      bytes = Base64.decodeBase64((String) object);
+    }
+    else if (object instanceof byte[]) {
+      bytes = (byte[]) object;
+    }
+    else {
+      throw new ISE("Cannot deserialize class[%s]", object.getClass());
+    }
+
+    return DimensionCardinalityAggregator.fromBytes(bytes);
+  }
+
+  @Override
+  public Object finalizeComputation(Object object)
+  {
+    return ((HyperLogLogPlus) object).cardinality();
+  }
+
+  @Override
+  @JsonProperty
+  public String getName()
+  {
+    return name;
+  }
+
+  @JsonProperty
+  public String getFieldName()
+  {
+    return fieldName;
+  }
+
+  @Override
+  public List<String> requiredFields()
+  {
+    return Lists.newArrayList(fieldName);
+  }
+
+  @Override
+  public byte[] getCacheKey()
+  {
+    byte[] fieldNameBytes = fieldName.getBytes(Charsets.UTF_8);
+
+    ByteBuffer buf = ByteBuffer.allocate(1 + fieldNameBytes.length);
+    buf.put(CACHE_KEY);
+    buf.put(fieldNameBytes);
+    return buf.array();
+  }
+
+  @Override
+  public String getTypeName()
+  {
+    return "hll+";
+  }
+
+  @Override
+  public int getMaxIntermediateSize()
+  {
+    return DimensionCardinalityAggregator.MAX_SIZE_BYTES;
+  }
+
+  @Override
+  public Object getAggregatorStartValue()
+  {
+    return DimensionCardinalityAggregator.makeHllPlus();
+  }
+
+  @Override
+  public String toString()
+  {
+    return "DimensionCardinalityAggregatorFactory{" +
+        "name='" + name + '\'' +
+        ", fieldName='" + fieldName + '\'' +
+        '}';
+  }
+}
---
+++ processing/src/main/java/io/druid/query/aggregation/cardinality/DimensionCardinalityBufferAggregator.java
@@ -0,0 +1,100 @@
+package io.druid.query.aggregation.cardinality;
+
+import com.clearspring.analytics.stream.cardinality.CardinalityMergeException;
+import com.clearspring.analytics.stream.cardinality.HyperLogLogPlus;
+import com.google.common.base.Throwables;
+import io.druid.query.aggregation.BufferAggregator;
+import io.druid.segment.ObjectColumnSelector;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.List;
+
+/**
+ *
+ */
+public class DimensionCardinalityBufferAggregator implements BufferAggregator
+{
+  private static final byte[] emptyBytes;
+
+  static {
+    try {
+      emptyBytes = DimensionCardinalityAggregator.makeHllPlus().getBytes();
+    } catch (IOException e) {
+      throw Throwables.propagate(e);
+    }
+  }
+
+  private final ObjectColumnSelector selector;
+
+  public DimensionCardinalityBufferAggregator(
+      ObjectColumnSelector selector
+  )
+  {
+    this.selector = selector;
+  }
+
+  @Override
+  public void init(ByteBuffer buf, int position)
+  {
+    ByteBuffer duplicate = buf.duplicate();
+    duplicate.position(position);
+    duplicate.putInt(emptyBytes.length);
+    duplicate.put(emptyBytes);
+  }
+
+  @Override
+  public void aggregate(ByteBuffer buf, int position)
+  {
+    HyperLogLogPlus hll = (HyperLogLogPlus) get(buf, position);
+
+    Object obj = selector.get();
+    if (obj instanceof HyperLogLogPlus) {
+      try {
+        hll.addAll((HyperLogLogPlus) obj);
+      } catch (CardinalityMergeException e) {
+        throw Throwables.propagate(e);
+      }
+    } else {
+      throw new UnsupportedOperationException("Can only operate on HLL+ objects.");
+    }
+
+    writeHll(buf, position, hll);
+  }
+
+  @Override
+  public Object get(ByteBuffer buf, int position)
+  {
+    ByteBuffer duplicate = buf.duplicate();
+    duplicate.position(position);
+    int size = duplicate.getInt();
+
+    byte[] bytes = new byte[size];
+    duplicate.get(bytes);
+    return DimensionCardinalityAggregator.fromBytes(bytes);
+  }
+
+  @Override
+  public float getFloat(ByteBuffer buf, int position)
+  {
+    return ((HyperLogLogPlus) get(buf, position)).cardinality();
+  }
+
+  @Override
+  public void close()
+  {
+  }
+
+  private void writeHll(ByteBuffer buf, int position, HyperLogLogPlus hll)
+  {
+    try {
+      byte[] outBytes = hll.getBytes();
+      ByteBuffer outBuf = buf.duplicate();
+      outBuf.position(position);
+      outBuf.putInt(outBytes.length);
+      outBuf.put(outBytes);
+    } catch (IOException e) {
+      throw Throwables.propagate(e);
+    }
+  }
+}
