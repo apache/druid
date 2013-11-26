ddb6e83f170ced7f2746d5ad81e0086d7087b9f1
fjy <fangjin@metamarkets.com>
2013-11-25 17:53:17 -0800

    fix realtime default and prepare for next deploy

--- services/src/main/java/io/druid/guice/RealtimeModule.java
+++ services/src/main/java/io/druid/guice/RealtimeModule.java
@@ -48,13 +48,13 @@ public class RealtimeModule implements Module
         binder,
         "druid.publish.type",
         Key.get(SegmentPublisher.class),
-        Key.get(NoopSegmentPublisher.class)
+        Key.get(DbSegmentPublisher.class)
     );
     final MapBinder<String, SegmentPublisher> publisherBinder = PolyBind.optionBinder(
         binder,
         Key.get(SegmentPublisher.class)
     );
-    publisherBinder.addBinding("db").to(DbSegmentPublisher.class);
+    publisherBinder.addBinding("noop").to(NoopSegmentPublisher.class);
     binder.bind(DbSegmentPublisher.class).in(LazySingleton.class);
 
     JsonConfigProvider.bind(binder, "druid.realtime", RealtimeManagerConfig.class);
