b78919c729be298474c00bd8a3504160408dcfc2
fjy <fangjin@metamarkets.com>
2013-12-05 16:49:28 -0800

    fix NPEs

--- processing/src/test/java/io/druid/query/QueryRunnerTestHelper.java
+++ processing/src/test/java/io/druid/query/QueryRunnerTestHelper.java
@@ -110,7 +110,7 @@ public class QueryRunnerTestHelper
     return Arrays.asList(
         new Object[][]{
             {
-                makeQueryRunner(factory, new IncrementalIndexSegment(rtIndex))
+                makeQueryRunner(factory, new IncrementalIndexSegment(rtIndex, null))
             },
             {
                 makeQueryRunner(factory, new QueryableIndexSegment(null, mMappedTestIndex))
