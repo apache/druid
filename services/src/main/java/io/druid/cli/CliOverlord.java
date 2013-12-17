f2241c8885b75e4482c234bf45ae57f58b378254
fjy <fangjin@metamarkets.com>
2013-12-16 16:16:00 -0800

    prepare for next release

--- services/src/main/java/io/druid/cli/CliOverlord.java
+++ services/src/main/java/io/druid/cli/CliOverlord.java
@@ -95,7 +95,7 @@ import java.util.List;
  */
 @Command(
     name = "overlord",
-    description = "Runs an Overlord node, see http://druid.io/docs/0.6.36/Indexing-Service.html for a description"
+    description = "Runs an Overlord node, see http://druid.io/docs/0.6.37/Indexing-Service.html for a description"
 )
 public class CliOverlord extends ServerRunnable
 {
