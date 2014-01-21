ebc66df27d612ffa2040e620437c6793397750ff
fjy <fangjin@metamarkets.com>
2014-01-20 16:58:53 -0800

    use terminateWithIds in terminate

--- indexing-service/src/main/java/io/druid/indexing/overlord/scaling/EC2AutoScalingStrategy.java
+++ indexing-service/src/main/java/io/druid/indexing/overlord/scaling/EC2AutoScalingStrategy.java
@@ -155,32 +155,15 @@ public class EC2AutoScalingStrategy implements AutoScalingStrategy
     }
 
     try {
-      log.info("Terminating instances[%s]", instances);
-      amazonEC2Client.terminateInstances(
-          new TerminateInstancesRequest(
-              Lists.transform(
-                  instances,
-                  new Function<Instance, String>()
-                  {
-                    @Override
-                    public String apply(Instance input)
-                    {
-                      return input.getInstanceId();
-                    }
-                  }
-              )
-          )
-      );
-
-      return new AutoScalingData(
+      return terminateWithIds(
           Lists.transform(
-              ips,
-              new Function<String, String>()
+              instances,
+              new Function<Instance, String>()
               {
                 @Override
-                public String apply(@Nullable String input)
+                public String apply(Instance input)
                 {
-                  return String.format("%s:%s", input, config.getWorkerPort());
+                  return input.getInstanceId();
                 }
               }
           )
