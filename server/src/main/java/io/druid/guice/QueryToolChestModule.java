1f690a37ca9f2a7835c5b49a9579151fdbffa2dc
Eric Tschetter <cheddar@tidepool.org>
2014-02-07 10:32:27 -0600

    Pull out a byte[] for Jackson

--- server/src/main/java/io/druid/guice/QueryToolChestModule.java
+++ server/src/main/java/io/druid/guice/QueryToolChestModule.java
@@ -55,6 +55,7 @@ import io.druid.query.topn.TopNQueryConfig;
 import io.druid.query.topn.TopNQueryQueryToolChest;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.Map;
 
@@ -89,7 +90,10 @@ public class QueryToolChestModule implements DruidModule
                   SerializerProvider serializerProvider)
                   throws IOException
               {
-                jsonGenerator.writeObject(hyperLogLogPlus.getBuffer());
+                ByteBuffer buf = hyperLogLogPlus.getBuffer();
+                byte[] bytes = new byte[buf.remaining()];
+                buf.put(bytes);
+                jsonGenerator.writeObject(bytes);
               }
             }
         )
