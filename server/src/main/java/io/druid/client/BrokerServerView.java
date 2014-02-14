4b902aed232e9ad30cc3ecad518491f97cac2ab4
Xavier Léauté <xavier@metamarkets.com>
2014-02-13 23:24:59 -0800

    fix typo

--- server/src/main/java/io/druid/client/BrokerServerView.java
+++ server/src/main/java/io/druid/client/BrokerServerView.java
@@ -54,7 +54,7 @@ public class BrokerServerView implements TimelineServerView
   private final Map<String, ServerSelector> selectors;
   private final Map<String, VersionedIntervalTimeline<String, ServerSelector>> timelines;
 
-  private final QueryToolChestWarehouse warehose;
+  private final QueryToolChestWarehouse warehouse;
   private final ObjectMapper smileMapper;
   private final HttpClient httpClient;
   private final ServerView baseView;
@@ -62,14 +62,14 @@ public class BrokerServerView implements TimelineServerView
 
   @Inject
   public BrokerServerView(
-      QueryToolChestWarehouse warehose,
+      QueryToolChestWarehouse warehouse,
       ObjectMapper smileMapper,
       @Client HttpClient httpClient,
       ServerView baseView,
       ServerSelectorStrategy serverSelectorStrategy
   )
   {
-    this.warehose = warehose;
+    this.warehouse = warehouse;
     this.smileMapper = smileMapper;
     this.httpClient = httpClient;
     this.baseView = baseView;
@@ -149,7 +149,7 @@ public class BrokerServerView implements TimelineServerView
 
   private DirectDruidClient makeDirectClient(DruidServer server)
   {
-    return new DirectDruidClient(warehose, smileMapper, httpClient, server.getHost());
+    return new DirectDruidClient(warehouse, smileMapper, httpClient, server.getHost());
   }
 
   private QueryableDruidServer removeServer(DruidServer server)
