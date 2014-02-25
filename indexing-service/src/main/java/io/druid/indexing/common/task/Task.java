2f610355859a6c5ed1d93c50e1a852fae94a100d
Xavier Léauté <xavier@metamarkets.com>
2014-02-25 13:26:16 -0800

    add restore task

--- indexing-service/src/main/java/io/druid/indexing/common/task/Task.java
+++ indexing-service/src/main/java/io/druid/indexing/common/task/Task.java
@@ -47,6 +47,7 @@ import io.druid.query.QueryRunner;
     @JsonSubTypes.Type(name = "kill", value = KillTask.class),
     @JsonSubTypes.Type(name = "move", value = MoveTask.class),
     @JsonSubTypes.Type(name = "archive", value = ArchiveTask.class),
+    @JsonSubTypes.Type(name = "restore", value = RestoreTask.class),
     @JsonSubTypes.Type(name = "index", value = IndexTask.class),
     @JsonSubTypes.Type(name = "index_hadoop", value = HadoopIndexTask.class),
     @JsonSubTypes.Type(name = "index_realtime", value = RealtimeIndexTask.class),
