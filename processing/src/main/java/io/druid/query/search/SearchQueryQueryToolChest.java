eedd6a1b9ec759cbbf6a7ca3c737e1842e7a0465
nishantmonu51 <nishant.monu51@gmail.com>
2014-02-28 12:38:39 +0530

    fix #362
    fix #362

--- processing/src/main/java/io/druid/query/search/SearchQueryQueryToolChest.java
+++ processing/src/main/java/io/druid/query/search/SearchQueryQueryToolChest.java
@@ -99,7 +99,7 @@ public class SearchQueryQueryToolChest extends QueryToolChest<Result<SearchResul
       )
       {
         SearchQuery query = (SearchQuery) input;
-        return new SearchBinaryFn(query.getSort(), query.getGranularity());
+        return new SearchBinaryFn(query.getSort(), query.getGranularity(), query.getLimit());
       }
     };
   }
