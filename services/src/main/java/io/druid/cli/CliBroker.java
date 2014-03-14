b3fb2fea9ca6017c2f2475630509e9f8e7c9398c
fjy <fangjin@metamarkets.com>
2014-03-14 10:57:43 -0700

    prepare for next release

--- services/src/main/java/io/druid/cli/CliBroker.java
+++ services/src/main/java/io/druid/cli/CliBroker.java
@@ -54,7 +54,7 @@ import java.util.List;
  */
 @Command(
     name = "broker",
-    description = "Runs a broker node, see http://druid.io/docs/0.6.65/Broker.html for a description"
+    description = "Runs a broker node, see http://druid.io/docs/0.6.66/Broker.html for a description"
 )
 public class CliBroker extends ServerRunnable
 {
