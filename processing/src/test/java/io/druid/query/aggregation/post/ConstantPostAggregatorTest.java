32ad26210dd229f1853a16696642e7b0d1b44daa
nishantmonu51 <nishant.monu51@gmail.com>
2014-03-18 20:34:49 +0530

    Fix backwards compatibilty constant post aggregator

--- processing/src/test/java/io/druid/query/aggregation/post/ConstantPostAggregatorTest.java
+++ processing/src/test/java/io/druid/query/aggregation/post/ConstantPostAggregatorTest.java
@@ -19,6 +19,7 @@
 
 package io.druid.query.aggregation.post;
 
+import io.druid.jackson.DefaultObjectMapper;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -33,11 +34,11 @@ public class ConstantPostAggregatorTest
   {
     ConstantPostAggregator constantPostAggregator;
 
-    constantPostAggregator = new ConstantPostAggregator("shichi", 7);
+    constantPostAggregator = new ConstantPostAggregator("shichi", 7, null);
     Assert.assertEquals(7, constantPostAggregator.compute(null));
-    constantPostAggregator = new ConstantPostAggregator("rei", 0.0);
+    constantPostAggregator = new ConstantPostAggregator("rei", 0.0, null);
     Assert.assertEquals(0.0, constantPostAggregator.compute(null));
-    constantPostAggregator = new ConstantPostAggregator("ichi", 1.0);
+    constantPostAggregator = new ConstantPostAggregator("ichi", 1.0, null);
     Assert.assertNotSame(1, constantPostAggregator.compute(null));
   }
 
@@ -45,10 +46,35 @@ public class ConstantPostAggregatorTest
   public void testComparator()
   {
     ConstantPostAggregator constantPostAggregator =
-        new ConstantPostAggregator("thistestbasicallydoesnothing unhappyface", 1);
+        new ConstantPostAggregator("thistestbasicallydoesnothing unhappyface", 1, null);
     Comparator comp = constantPostAggregator.getComparator();
     Assert.assertEquals(0, comp.compare(0, constantPostAggregator.compute(null)));
     Assert.assertEquals(0, comp.compare(0, 1));
     Assert.assertEquals(0, comp.compare(1, 0));
+  }
+
+  @Test
+  public void testSerdeBackwardsCompatible() throws Exception
+  {
+
+    DefaultObjectMapper mapper = new DefaultObjectMapper();
+    ConstantPostAggregator aggregator = mapper.readValue(
+        "{\"type\":\"constant\",\"name\":\"thistestbasicallydoesnothing unhappyface\",\"constantValue\":1}\n",
+        ConstantPostAggregator.class
+    );
+    Assert.assertEquals(new Integer(1), aggregator.getConstantValue());
+  }
+
+  @Test
+  public void testSerde() throws Exception
+  {
+
+    DefaultObjectMapper mapper = new DefaultObjectMapper();
+    ConstantPostAggregator aggregator = new ConstantPostAggregator("aggregator", 2, null);
+    ConstantPostAggregator aggregator1 = mapper.readValue(
+        mapper.writeValueAsString(aggregator),
+        ConstantPostAggregator.class
+    );
+    Assert.assertEquals(aggregator, aggregator1);
   }
 }
