f42b850d6009993a79970c4465a4cc8aea599436
Xavier Léauté <xavier@metamarkets.com>
2014-03-26 09:31:45 -0700

    clean up + header

--- processing/src/test/java/io/druid/query/aggregation/hyperloglog/HyperLogLogCollectorBenchmark.java
+++ processing/src/test/java/io/druid/query/aggregation/hyperloglog/HyperLogLogCollectorBenchmark.java
@@ -1,3 +1,22 @@
+/*
+ * Druid - a distributed column store.
+ * Copyright (C) 2012, 2013, 2014  Metamarkets Group Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package io.druid.query.aggregation.hyperloglog;
 
 import com.google.caliper.Param;
@@ -12,7 +31,6 @@ import sun.misc.Unsafe;
 import java.lang.reflect.Field;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.util.List;
 
 public class HyperLogLogCollectorBenchmark extends SimpleBenchmark
@@ -125,24 +143,7 @@ public class HyperLogLogCollectorBenchmark extends SimpleBenchmark
     return rolling.estimateCardinality();
   }
 
-//  public double timeFoldIterable(int reps) {
-//    HyperLogLogCollector rolling = allocateCollector(targetIsDirect, align);
-//
-//    final Iterator<HyperLogLogCollector> it = Iterables.cycle(collectors).iterator();
-//    for(int k = 0; k < reps; ++k) {
-//      for(int i = 0; i < count; ++i) {
-//        rolling.fold(it.next());
-//      }
-//    }
-//    return rolling.estimateCardinality();
-//  }
-
   public static void main(String[] args) throws Exception {
-//    HyperLogLogCollectorBenchmark benchmark = new HyperLogLogCollectorBenchmark();
-//    benchmark.foldCount = 100;
-//    benchmark.setUp();
-//    double d = benchmark.timeFold(10_000);
-//    System.out.println(d);
     Runner.main(HyperLogLogCollectorBenchmark.class, args);
   }
 }
