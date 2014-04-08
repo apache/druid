7db756f819f2d5f4a65c64ad701f0eb9e6e17a98
fjy <fangjin@metamarkets.com>
2014-04-08 16:25:59 -0700

    prepare for next release

--- services/src/main/java/io/druid/cli/CliCoordinator.java
+++ services/src/main/java/io/druid/cli/CliCoordinator.java
@@ -66,7 +66,7 @@ import java.util.List;
  */
 @Command(
     name = "coordinator",
-    description = "Runs the Coordinator, see http://druid.io/docs/0.6.85/Coordinator.html for a description."
+    description = "Runs the Coordinator, see http://druid.io/docs/0.6.86/Coordinator.html for a description."
 )
 public class CliCoordinator extends ServerRunnable
 {
