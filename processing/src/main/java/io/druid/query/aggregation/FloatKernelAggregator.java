1573a097c6deef287af265705006986ee77e0c62
Alex Demidko <dralexthk@gmail.com>
2014-04-10 18:07:26 -0700

    Float kernel aggregator

---
+++ processing/src/main/java/io/druid/query/aggregation/FloatKernelAggregator.java
@@ -0,0 +1,115 @@
+/*
+ * Druid - a distributed column store.
+ * Copyright (C) 2012, 2013, 2014  Metamarkets Group Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package io.druid.query.aggregation;
+
+import com.nativelibs4java.opencl.CLBuffer;
+import com.nativelibs4java.opencl.CLContext;
+import com.nativelibs4java.opencl.CLDevice;
+import com.nativelibs4java.opencl.CLEvent;
+import com.nativelibs4java.opencl.CLMem;
+import com.nativelibs4java.opencl.CLPlatform;
+import com.nativelibs4java.opencl.CLQueue;
+import com.nativelibs4java.opencl.JavaCL;
+import com.nativelibs4java.opencl.util.OpenCLType;
+import com.nativelibs4java.opencl.util.ReductionUtils;
+import io.druid.query.aggregation.gpu.CLUtils;
+import io.druid.segment.FloatBufferSelector;
+import org.bridj.Pointer;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class FloatKernelAggregator implements KernelAggregator
+{
+  private final FloatBufferSelector selector;
+
+  private final CLContext context;
+  private final CLQueue queue;
+
+  private final CLBuffer<Float> totalBuffer;
+  private int totalBufferOffset = 0;
+
+  private final ReductionUtils.Reductor<Float> reductor;
+
+  private List<CLEvent> copyEvents;
+
+  public FloatKernelAggregator(
+      FloatBufferSelector selector,
+      CLContext context,
+      CLQueue queue
+  )
+  {
+    this.selector = selector;
+    this.context = context;
+    this.queue = queue;
+
+    this.totalBuffer = this.context.createFloatBuffer(CLMem.Usage.Input, selector.size());
+    this.reductor = ReductionUtils.createReductor(context, ReductionUtils.Operation.Add, OpenCLType.Float, 1);
+
+    copyEvents = new ArrayList<CLEvent>();
+  }
+
+
+  @Override
+  public void copyBuffer()
+  {
+    FloatBuffer currentBuffer = selector.getBuffer();
+    CLBuffer<Float> buf = context.createFloatBuffer(CLMem.Usage.Input, Pointer.pointerToFloats(currentBuffer));
+
+    int bufRemaining = currentBuffer.remaining();
+    CLEvent copyEvent = buf.copyTo(queue, 0, bufRemaining, totalBuffer, totalBufferOffset);
+    totalBufferOffset += bufRemaining;
+
+    copyEvents.add(copyEvent);
+  }
+
+  @Override
+  public void run(IntBuffer buckets, ByteBuffer out, int position)
+  {
+    Pointer<Float> ptr = reductor.reduce(queue, this.totalBuffer, copyEvents.toArray(new CLEvent[copyEvents.size()]));
+    Float value = ptr.get();
+
+    // TODO: count in buckets
+    out.asFloatBuffer().put(position, value);
+  }
+
+  @Override
+  public Object get(ByteBuffer buf, int position)
+  {
+    return null;
+  }
+
+  @Override
+  public float getFloat(ByteBuffer buf, int position)
+  {
+    return 0;
+  }
+
+  @Override
+  public void close()
+  {
+    totalBuffer.release();
+  }
+}
--- processing/src/main/java/io/druid/query/aggregation/KernelAggregatorFactory.java
+++ processing/src/main/java/io/druid/query/aggregation/KernelAggregatorFactory.java
@@ -22,20 +22,33 @@ package io.druid.query.aggregation;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Preconditions;
+import com.google.common.primitives.Floats;
+import com.nativelibs4java.opencl.CLContext;
+import com.nativelibs4java.opencl.CLDevice;
+import com.nativelibs4java.opencl.CLPlatform;
+import com.nativelibs4java.opencl.CLQueue;
+import com.nativelibs4java.opencl.JavaCL;
 import io.druid.query.aggregation.Aggregator;
 import io.druid.query.aggregation.AggregatorFactory;
 import io.druid.query.aggregation.BufferAggregator;
+import io.druid.query.aggregation.gpu.CLUtils;
 import io.druid.segment.BufferSelectorFactory;
 import io.druid.segment.ColumnSelectorFactory;
 import io.druid.segment.FloatBufferSelector;
 
+import java.nio.ByteOrder;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 
 public class KernelAggregatorFactory implements AggregatorFactory
 {
   private final String fieldName;
   private final String name;
+
+  private final CLContext context;
+  private final CLQueue queue;
+  private final ByteOrder byteOrder;
 
   @JsonCreator
   public KernelAggregatorFactory(
@@ -48,13 +61,21 @@ public class KernelAggregatorFactory implements AggregatorFactory
 
     this.name = name;
     this.fieldName = fieldName;
+
+    CLDevice device = CLUtils.findDevice("i7");
+    if (device == null) {
+      throw new IllegalStateException("No OpenCL-capable device!");
+    }
+
+    this.context = JavaCL.createContext(new HashMap<CLPlatform.ContextProperties, Object>(), device);
+    this.queue = this.context.createDefaultQueue();
+    this.byteOrder = this.context.getByteOrder();
   }
 
   public KernelAggregator factorizeKernel(BufferSelectorFactory columnFactory)
   {
-    FloatBufferSelector floatBufferSelector = columnFactory.makeFloatBufferSelector(fieldName);
-    throw new UnsupportedOperationException();
-    // return new KernelAggregator();
+    FloatBufferSelector floatBufferSelector = columnFactory.makeFloatBufferSelector(fieldName, byteOrder);
+    return new FloatKernelAggregator(floatBufferSelector, context, queue);
   }
 
   @Override
@@ -126,12 +147,17 @@ public class KernelAggregatorFactory implements AggregatorFactory
   @Override
   public int getMaxIntermediateSize()
   {
-    return 0;
+    return Floats.BYTES;
   }
 
   @Override
   public Object getAggregatorStartValue()
   {
     return null;
+  }
+
+  public ByteOrder getByteOrder()
+  {
+    return byteOrder;
   }
 }
