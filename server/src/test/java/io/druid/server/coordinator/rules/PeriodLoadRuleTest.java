3fd651c2359bc2e22de741942493590d9f1be6fc
fjy <fangjin@metamarkets.com>
2014-04-30 10:43:30 -0700

    fix the rule again, update docs, and prepare for next release

--- server/src/test/java/io/druid/server/coordinator/rules/PeriodLoadRuleTest.java
+++ server/src/test/java/io/druid/server/coordinator/rules/PeriodLoadRuleTest.java
@@ -49,7 +49,7 @@ public class PeriodLoadRuleTest
 
     Assert.assertTrue(rule.appliesTo(builder.interval(new Interval("2012-01-01/2012-12-31")).build(), now));
     Assert.assertTrue(rule.appliesTo(builder.interval(new Interval("1000-01-01/2012-12-31")).build(), now));
-    Assert.assertFalse(rule.appliesTo(builder.interval(new Interval("0500-01-01/2100-12-31")).build(), now));
+    Assert.assertTrue(rule.appliesTo(builder.interval(new Interval("0500-01-01/2100-12-31")).build(), now));
   }
 
   @Test
@@ -64,7 +64,7 @@ public class PeriodLoadRuleTest
     );
 
     Assert.assertTrue(rule.appliesTo(builder.interval(new Interval(now.minusWeeks(1), now)).build(), now));
-    Assert.assertFalse(
+    Assert.assertTrue(
         rule.appliesTo(
             builder.interval(new Interval(now.minusDays(1), now.plusDays(1)))
                    .build(),
