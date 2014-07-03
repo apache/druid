4ab9af36b0e8aef820d8387af54c98adc6be5818
Jae Hyeon Bae <metacret@gmail.com>
2014-07-03 16:52:47 -0700

    License header and utf-8

--- s3-extensions/src/main/java/io/druid/storage/s3/FileSessionCredentialsProvider.java
+++ s3-extensions/src/main/java/io/druid/storage/s3/FileSessionCredentialsProvider.java
@@ -1,11 +1,30 @@
+/*
+ * Druid - a distributed column store.
+ * Copyright (C) 2012, 2013  Metamarkets Group Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package io.druid.storage.s3;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSSessionCredentials;
+import com.google.common.base.Charsets;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -45,7 +64,7 @@ public class FileSessionCredentialsProvider implements AWSCredentialsProvider {
   @Override
   public void refresh() {
     try {
-      List<String> lines = Files.readAllLines(Paths.get(sessionCredentials), Charset.defaultCharset());
+      List<String> lines = Files.readAllLines(Paths.get(sessionCredentials), Charsets.UTF_8);
       Properties props = new Properties();
       for (String line : lines) {
         String[] tokens = line.split("=");
