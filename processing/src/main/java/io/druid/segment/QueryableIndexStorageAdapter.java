df7a39a682e60326a8db7542da54d8f2308e5df1
Xavier Léauté <xavier@metamarkets.com>
2014-07-28 09:59:21 -0700

    fix index out of bounds

--- processing/src/main/java/io/druid/segment/QueryableIndexStorageAdapter.java
+++ processing/src/main/java/io/druid/segment/QueryableIndexStorageAdapter.java
@@ -508,7 +508,7 @@ public class QueryableIndexStorageAdapter implements StorageAdapter
                               if (multiValueRow.size() == 0) {
                                 return null;
                               } else if (multiValueRow.size() == 1) {
-                                return columnVals.lookupName(multiValueRow.get(1));
+                                return columnVals.lookupName(multiValueRow.get(0));
                               } else {
                                 final String[] strings = new String[multiValueRow.size()];
                                 for (int i = 0 ; i < multiValueRow.size() ; i++) {
