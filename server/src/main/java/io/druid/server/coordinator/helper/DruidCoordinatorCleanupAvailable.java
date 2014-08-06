70660ad295d745d41a58de9ee0148813d4e1209c
Xavier Léauté <xavier@metamarkets.com>
2014-08-06 14:06:52 -0700

    separate cleanup of overshadowed vs. unavailable segments

--- server/src/main/java/io/druid/server/coordinator/helper/DruidCoordinatorCleanupAvailable.java
+++ server/src/main/java/io/druid/server/coordinator/helper/DruidCoordinatorCleanupAvailable.java
@@ -23,8 +23,6 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.MinMaxPriorityQueue;
 import com.metamx.common.guava.Comparators;
 import com.metamx.common.logger.Logger;
-import io.druid.client.DruidDataSource;
-import io.druid.client.DruidServer;
 import io.druid.client.ImmutableDruidDataSource;
 import io.druid.client.ImmutableDruidServer;
 import io.druid.server.coordinator.CoordinatorStats;
@@ -43,13 +41,11 @@ import java.util.Set;
 
 /**
  */
-public class DruidCoordinatorCleanup implements DruidCoordinatorHelper
+public class DruidCoordinatorCleanupAvailable implements DruidCoordinatorHelper
 {
-  private static final Logger log = new Logger(DruidCoordinatorCleanup.class);
-
   private final DruidCoordinator coordinator;
 
-  public DruidCoordinatorCleanup(
+  public DruidCoordinatorCleanupAvailable(
       DruidCoordinator coordinator
   )
   {
@@ -95,43 +91,10 @@ public class DruidCoordinatorCleanup implements DruidCoordinatorHelper
       }
     }
 
-    // Delete segments that are old
-    // Unservice old partitions if we've had enough time to make sure we aren't flapping with old data
-    if (params.hasDeletionWaitTimeElapsed()) {
-      Map<String, VersionedIntervalTimeline<String, DataSegment>> timelines = Maps.newHashMap();
-
-      for (MinMaxPriorityQueue<ServerHolder> serverHolders : cluster.getSortedServersByTier()) {
-        for (ServerHolder serverHolder : serverHolders) {
-          ImmutableDruidServer server = serverHolder.getServer();
-
-          for (ImmutableDruidDataSource dataSource : server.getDataSources()) {
-            VersionedIntervalTimeline<String, DataSegment> timeline = timelines.get(dataSource.getName());
-            if (timeline == null) {
-              timeline = new VersionedIntervalTimeline<String, DataSegment>(Comparators.comparable());
-              timelines.put(dataSource.getName(), timeline);
-            }
-
-            for (DataSegment segment : dataSource.getSegments()) {
-              timeline.add(
-                  segment.getInterval(), segment.getVersion(), segment.getShardSpec().createChunk(segment)
-              );
-            }
-          }
-        }
-      }
-
-      for (VersionedIntervalTimeline<String, DataSegment> timeline : timelines.values()) {
-        for (TimelineObjectHolder<String, DataSegment> holder : timeline.findOvershadowed()) {
-          for (DataSegment dataSegment : holder.getObject().payloads()) {
-            coordinator.removeSegment(dataSegment);
-            stats.addToGlobalStat("overShadowedCount", 1);
-          }
-        }
-      }
-    }
-
     return params.buildFromExisting()
                  .withCoordinatorStats(stats)
                  .build();
   }
+
+
 }
