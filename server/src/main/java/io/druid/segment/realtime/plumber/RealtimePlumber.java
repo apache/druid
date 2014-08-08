179170224d6f8b547a7b6ba2c2fe3d1b17bde65b
Gian Merlino <gianmerlino@gmail.com>
2014-08-08 11:20:36 -0700

    Fix conditional.

--- server/src/main/java/io/druid/segment/realtime/plumber/RealtimePlumber.java
+++ server/src/main/java/io/druid/segment/realtime/plumber/RealtimePlumber.java
@@ -356,7 +356,7 @@ public class RealtimePlumber implements Plumber
               log.makeAlert(e, "Failed to persist merged index[%s]", schema.getDataSource())
                  .addData("interval", interval)
                  .emit();
-              if (!shuttingDown) {
+              if (shuttingDown) {
                 // We're trying to shut down, and this segment failed to push. Let's just get rid of it.
                 // This call will also delete possibly-partially-written files, so we don't need to do it explicitly.
                 abandonSegment(truncatedTime, sink);
