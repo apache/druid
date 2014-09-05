5dd23b19c38d25bbe39f6f7ed14f7a38792ed604
Xavier Léauté <xavier@metamarkets.com>
2014-09-05 10:55:17 -0700

    fix interval start before end in TimewarpOperator

--- processing/src/main/java/io/druid/query/TimewarpOperator.java
+++ processing/src/main/java/io/druid/query/TimewarpOperator.java
@@ -85,7 +85,7 @@ public class TimewarpOperator<T> implements PostProcessingOperator<T>
 
         final Interval interval = query.getIntervals().get(0);
         final Interval modifiedInterval = new Interval(
-            interval.getStartMillis() + offset,
+            Math.min(interval.getStartMillis() + offset, now + offset),
             Math.min(interval.getEndMillis() + offset, now + offset)
         );
         return Sequences.map(
