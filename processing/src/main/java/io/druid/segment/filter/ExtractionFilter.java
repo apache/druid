e3260aa1779b9bf08309a7f0959cd46d4c3279e9
nishantmonu51 <nishant.monu51@gmail.com>
2014-11-19 17:42:37 +0530

    Filtered Aggregator fixes + enhancements
    - fix NPE on IncrementIndex
    - refactor code to support AND, OR filter
    - tests for AND & OR filter
    - handling for missing column / null values

--- processing/src/main/java/io/druid/segment/filter/ExtractionFilter.java
+++ processing/src/main/java/io/druid/segment/filter/ExtractionFilter.java
@@ -26,6 +26,7 @@ import io.druid.query.filter.BitmapIndexSelector;
 import io.druid.query.filter.Filter;
 import io.druid.query.filter.ValueMatcher;
 import io.druid.query.filter.ValueMatcherFactory;
+import io.druid.segment.ColumnSelectorFactory;
 import io.druid.segment.data.Indexed;
 
 import java.util.List;
@@ -75,4 +76,11 @@ public class ExtractionFilter implements Filter
   {
     throw new UnsupportedOperationException();
   }
+
+  @Override
+  public ValueMatcher makeMatcher(ColumnSelectorFactory factory)
+  {
+    throw new UnsupportedOperationException();
+  }
+
 }
