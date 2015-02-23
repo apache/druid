a010b22e570c26e40249d6d57ecce07ff0f7477b
Himanshu Gupta <himanshg@yahoo-inc.com>
2015-02-23 13:42:20 -0600

    UTs update for indexing-hadoop

--- indexing-hadoop/src/main/java/io/druid/indexer/SortableBytes.java
+++ indexing-hadoop/src/main/java/io/druid/indexer/SortableBytes.java
@@ -40,10 +40,6 @@ public class SortableBytes
   {
     this.groupKey = groupKey;
     this.sortKey = sortKey;
-
-    if ("".equals(sortKey)) {
-      throw new IllegalArgumentException();
-    }
   }
 
   public byte[] getGroupKey()
