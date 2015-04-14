45186ef9c6c5ef8ba0457f609b9e40535d01049f
jitesh4112 <jitesh4112@gmail.com>
2015-04-14 15:38:53 +0530

    retentionRule

---
+++ server/src/main/java/io/druid/server/coordinator/rules/RetentionPeriodDropRule.java
@@ -0,0 +1,62 @@
+/*
+ * Druid - a distributed column store.
+ * Copyright (C) 2012  Metamarkets Group Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package io.druid.server.coordinator.rules;
+
+import io.druid.timeline.DataSegment;
+
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import org.joda.time.Period;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ */
+public class RetentionPeriodDropRule extends DropRule {
+	private final Period period;
+
+	@JsonCreator
+	public RetentionPeriodDropRule(@JsonProperty("period") Period period) {
+		this.period = period;
+	}
+
+	@Override
+	@JsonProperty
+	public String getType() {
+		return "dropByRetentionPeriod";
+	}
+
+	@JsonProperty
+	public Period getPeriod() {
+		return period;
+	}
+
+	@Override
+	public boolean appliesTo(DataSegment segment, DateTime referenceTimestamp) {		
+		return appliesTo(segment.getInterval(), referenceTimestamp);
+	}
+
+	@Override
+	public boolean appliesTo(Interval interval, DateTime referenceTimestamp) {
+		final Interval currInterval = new Interval(period, referenceTimestamp);
+	    return !currInterval.contains(interval);
+	}
+}
