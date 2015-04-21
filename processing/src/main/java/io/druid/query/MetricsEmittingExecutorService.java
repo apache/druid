1331193caa9077c6cb2ac0891c138e99f0473b5c
Charles Allen <charles@allen-net.com>
2015-04-20 21:06:22 -0700

    Add metric for cpu user time usage of threads in the processing pool

--- processing/src/main/java/io/druid/query/MetricsEmittingExecutorService.java
+++ processing/src/main/java/io/druid/query/MetricsEmittingExecutorService.java
@@ -20,16 +20,49 @@ package io.druid.query;
 import com.google.common.util.concurrent.ForwardingListeningExecutorService;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.Runnables;
 import com.metamx.emitter.service.ServiceEmitter;
 import com.metamx.emitter.service.ServiceMetricEvent;
 
+import javax.validation.constraints.NotNull;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.util.concurrent.Callable;
 
 public class MetricsEmittingExecutorService extends ForwardingListeningExecutorService
 {
+  private static final ThreadMXBean THREAD_MX_BEAN = ManagementFactory.getThreadMXBean();
+  private static final ThreadLocal<Long> THREAD_USER_START = new ThreadLocal<Long>()
+  {
+    @Override
+    public Long initialValue()
+    {
+      return 0L;
+    }
+  };
+  private static final Runnable PRE = THREAD_MX_BEAN.isCurrentThreadCpuTimeSupported() ? new Runnable()
+  {
+    @Override
+    public void run()
+    {
+      THREAD_USER_START.set(THREAD_MX_BEAN.getCurrentThreadUserTime());
+    }
+  } : Runnables.doNothing();
+
   private final ListeningExecutorService delegate;
   private final ServiceEmitter emitter;
   private final ServiceMetricEvent.Builder metricBuilder;
+
+  private final Runnable post = THREAD_MX_BEAN.isCurrentThreadCpuTimeSupported() ? new Runnable()
+  {
+    @Override
+    public void run()
+    {
+      emitter.emit(
+          metricBuilder.build("exec/cpu", THREAD_MX_BEAN.getCurrentThreadUserTime() - THREAD_USER_START.get())
+      );
+    }
+  } : Runnables.doNothing();
 
   public MetricsEmittingExecutorService(
       ListeningExecutorService delegate,
@@ -38,7 +71,6 @@ public class MetricsEmittingExecutorService extends ForwardingListeningExecutorS
   )
   {
     super();
-
     this.delegate = delegate;
     this.emitter = emitter;
     this.metricBuilder = metricBuilder;
@@ -51,17 +83,37 @@ public class MetricsEmittingExecutorService extends ForwardingListeningExecutorS
   }
 
   @Override
-  public <T> ListenableFuture<T> submit(Callable<T> tCallable)
+  public <T> ListenableFuture<T> submit(final Callable<T> tCallable)
   {
+    final Callable<T> wrappedPrioritizedCallable =
+        (tCallable instanceof PrioritizedCallable)
+        ? PrioritizedExecutorService.wrapCallable((PrioritizedCallable<T>) tCallable, PRE, post)
+        : wrapCallable(tCallable, PRE, post);
     emitMetrics();
-    return delegate.submit(tCallable);
+    return super.submit(wrappedPrioritizedCallable);
+  }
+
+  @Override
+  public ListenableFuture<?> submit(final Runnable runnable)
+  {
+    return submit(runnable, (Object) null);
+  }
+
+  @Override
+  public <T> ListenableFuture<T> submit(final Runnable runnable, T result)
+  {
+    final Runnable wrappedPrioritizedRunnable =
+        (runnable instanceof PrioritizedRunnable)
+        ? PrioritizedExecutorService.wrapRunnable((PrioritizedRunnable) runnable, PRE, post)
+        : wrapRunnable(runnable, PRE, post);
+    emitMetrics();
+    return super.submit(wrappedPrioritizedRunnable, result);
   }
 
   @Override
   public void execute(Runnable runnable)
   {
-    emitMetrics();
-    delegate.execute(runnable);
+    submit(runnable);
   }
 
   private void emitMetrics()
@@ -69,5 +121,42 @@ public class MetricsEmittingExecutorService extends ForwardingListeningExecutorS
     if (delegate instanceof PrioritizedExecutorService) {
       emitter.emit(metricBuilder.build("exec/backlog", ((PrioritizedExecutorService) delegate).getQueueSize()));
     }
+  }
+
+  public static Runnable wrapRunnable(
+      @NotNull final Runnable runnable,
+      @NotNull final Runnable pre,
+      @NotNull final Runnable post
+  )
+  {
+    return new Runnable()
+    {
+      @Override
+      public void run()
+      {
+        pre.run();
+        runnable.run();
+        post.run();
+      }
+    };
+  }
+
+  public static <V> Callable<V> wrapCallable(
+      @NotNull final Callable<V> callable,
+      @NotNull final Runnable pre,
+      @NotNull final Runnable post
+  )
+  {
+    return new Callable<V>()
+    {
+      @Override
+      public V call() throws Exception
+      {
+        pre.run();
+        final V retVal = callable.call();
+        post.run();
+        return retVal;
+      }
+    };
   }
 }
