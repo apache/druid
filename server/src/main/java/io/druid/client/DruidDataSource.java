01448d264c5244f189b69f59b4d781702b2b97d2
Parag Jain <pjain11@yahoo-inc.com>
2015-04-29 11:34:00 -0500

    Fix warn msg and added some unit tests

--- server/src/main/java/io/druid/client/DruidDataSource.java
+++ server/src/main/java/io/druid/client/DruidDataSource.java
@@ -131,4 +131,42 @@ public class DruidDataSource
       );
     }
   }
+
+  @Override
+  public boolean equals(Object o)
+  {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    DruidDataSource that = (DruidDataSource) o;
+
+    if (name != null ? !name.equals(that.name) : that.name != null) {
+      return false;
+    }
+    if (partitionNames != null ? !partitionNames.equals(that.partitionNames) : that.partitionNames != null) {
+      return false;
+    }
+    if (properties != null ? !properties.equals(that.properties) : that.properties != null) {
+      return false;
+    }
+    if (segmentsHolder != null ? !segmentsHolder.equals(that.segmentsHolder) : that.segmentsHolder != null) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  public int hashCode()
+  {
+    int result = name != null ? name.hashCode() : 0;
+    result = 31 * result + (properties != null ? properties.hashCode() : 0);
+    result = 31 * result + (partitionNames != null ? partitionNames.hashCode() : 0);
+    result = 31 * result + (segmentsHolder != null ? segmentsHolder.hashCode() : 0);
+    return result;
+  }
 }
