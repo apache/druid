a7f9bb1a1ea5540a64c894472a6ffa223b3db3b5
fjy <fangjinyang@gmail.com>
2015-04-30 08:26:52 -0700

    Additional coordinator logs to figure out when racey things might be happening

--- server/src/main/java/io/druid/server/coordinator/DruidCoordinator.java
+++ server/src/main/java/io/druid/server/coordinator/DruidCoordinator.java
@@ -507,6 +507,7 @@ public class DruidCoordinator
       }
 
       log.info("I am the leader of the coordinators, all must bow!");
+      log.info("Starting coordination in [%s]", config.getCoordinatorStartDelay());
       try {
         leaderCounter++;
         leader = true;
