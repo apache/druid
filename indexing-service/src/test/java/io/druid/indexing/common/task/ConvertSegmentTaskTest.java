0c459543ecb98c147af4e6f25ad89d40cc674521
Himanshu Gupta <g.himanshu@gmail.com>
2015-05-11 22:06:37 -0500

    demo the deserialization failure of ConvertSegmentTask with convert_segment

--- indexing-service/src/test/java/io/druid/indexing/common/task/ConvertSegmentTaskTest.java
+++ indexing-service/src/test/java/io/druid/indexing/common/task/ConvertSegmentTaskTest.java
@@ -61,4 +61,43 @@ public class ConvertSegmentTaskTest
     task2 = jsonMapper.readValue(jsonMapper.writerWithDefaultPrettyPrinter().writeValueAsString(task), Task.class);
     Assert.assertEquals(task, task2);
   }
+
+  @Test
+  public void testDeserialization() throws Exception
+  {
+    DefaultObjectMapper jsonMapper = new DefaultObjectMapper();
+
+    //this one deserializes successfully because "version_converter" is listed first on the Task interface
+    ConvertSegmentTask task1 = jsonMapper.readValue(taskString("version_converter"), ConvertSegmentTask.class);
+
+    //this one fails to deserialize with following error
+    //com.fasterxml.jackson.databind.JsonMappingException: Could not resolve type id 'convert_segment' into a subtype of
+    //[simple type, class io.druid.indexing.common.task.ConvertSegmentTask]
+    ConvertSegmentTask task2 = jsonMapper.readValue(taskString("convert_segment"), ConvertSegmentTask.class);
+  }
+
+  private String taskString(String type)
+  {
+    return "{"
+           +  "\"type\" : \"" + type + "\","
+           +  "\"id\" : \"convert_segment_billy_2015-05-12T02:51:35.863Z_2015-05-12T02:51:36.943Z_2015-05-12T02:51:38.262Z\","
+           +  "\"groupId\" : \"convert_segment_billy_2015-05-12T02:51:35.863Z_2015-05-12T02:51:36.943Z_2015-05-12T02:51:38.262Z\","
+           +  "\"dataSource\" : \"billy\","
+           +  "\"interval\" : \"2015-05-12T02:51:35.863Z/2015-05-12T02:51:36.943Z\","
+           +  "\"segment\" : null,"
+           +  "\"indexSpec\" : {"
+           +  "  \"bitmap\" : {"
+           +  "    \"type\" : \"concise\""
+           +  "  },"
+           +  "  \"dimensionCompression\" : null,"
+           +  "  \"metricCompression\" : null"
+           +  "},"
+           +  "\"force\" : false,"
+           +  "\"validate\" : true,"
+           +  "\"resource\" : {"
+           +  "  \"availabilityGroup\" : \"convert_segment_billy_2015-05-12T02:51:35.863Z_2015-05-12T02:51:36.943Z_2015-05-12T02:51:38.262Z\","
+           +  "  \"requiredCapacity\" : 1"
+           +"}"
+           +"}";
+  }
 }
