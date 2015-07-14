9400c473a81170f6ef3742bea13ba34cbfda2af6
Charles Allen <charles@allen-net.com>
2015-07-14 16:21:36 -0700

    Remove comments

--- indexing-service/src/main/java/io/druid/indexing/overlord/TaskQueue.java
+++ indexing-service/src/main/java/io/druid/indexing/overlord/TaskQueue.java
@@ -79,12 +79,12 @@ public class TaskQueue
   private final Condition managementMayBeNecessary = giant.newCondition();
   private final ExecutorService managerExec = Executors.newSingleThreadExecutor(
       new ThreadFactoryBuilder()
-          .setDaemon(false) // Don't use Execs because of this
+          .setDaemon(false)
           .setNameFormat("TaskQueue-Manager").build()
   );
   private final ScheduledExecutorService storageSyncExec = Executors.newSingleThreadScheduledExecutor(
       new ThreadFactoryBuilder()
-          .setDaemon(false) // Don't use Execs because of this
+          .setDaemon(false)
           .setNameFormat("TaskQueue-StorageSync").build()
   );
 
