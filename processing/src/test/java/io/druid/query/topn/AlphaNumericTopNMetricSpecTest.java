2c464ad9367de51ab42607072beaad14be3df592
Xavier Léauté <xavier@metamarkets.com>
2015-07-16 17:47:16 -0700

    correct reference in docs + more tests / examples

--- processing/src/test/java/io/druid/query/topn/AlphaNumericTopNMetricSpecTest.java
+++ processing/src/test/java/io/druid/query/topn/AlphaNumericTopNMetricSpecTest.java
@@ -18,12 +18,16 @@
 package io.druid.query.topn;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import io.druid.jackson.DefaultObjectMapper;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class AlphaNumericTopNMetricSpecTest
 {
@@ -61,6 +65,28 @@ public class AlphaNumericTopNMetricSpecTest
     // ... don't work too well
     Assert.assertTrue(comparator.compare("1.3", "1.15") < 0);
 
+    // but you can sort ranges
+    List<String> sorted = Lists.newArrayList("1-5", "11-15", "16-20", "21-25", "26-30", "6-10", "Other");
+    Collections.sort(sorted, comparator);
+
+    Assert.assertEquals(
+        ImmutableList.of("1-5", "6-10", "11-15", "16-20", "21-25", "26-30", "Other"),
+        sorted
+    );
+
+    List<String> sortedFixedDecimal = Lists.newArrayList(
+        "Other", "[0.00-0.05)", "[0.05-0.10)", "[0.10-0.50)", "[0.50-1.00)",
+        "[1.00-5.00)", "[5.00-10.00)", "[10.00-20.00)"
+    );
+    Collections.sort(sortedFixedDecimal, comparator);
+
+    Assert.assertEquals(
+        ImmutableList.of(
+            "[0.00-0.05)", "[0.05-0.10)", "[0.10-0.50)", "[0.50-1.00)",
+            "[1.00-5.00)", "[5.00-10.00)", "[10.00-20.00)", "Other"
+        ),
+        sortedFixedDecimal
+    );
   }
 
   @Test
