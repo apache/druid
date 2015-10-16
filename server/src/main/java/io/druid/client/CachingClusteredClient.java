bf214f23b29373d7ac8dc190d00162b0a446731d
Nishant <nishant.monu51@gmail.com>
2015-10-16 19:43:21 +0530

    Improve message for alert - Add Datasource info.

--- server/src/main/java/io/druid/client/CachingClusteredClient.java
+++ server/src/main/java/io/druid/client/CachingClusteredClient.java
@@ -237,7 +237,7 @@ public class CachingClusteredClient<T> implements QueryRunner<T>
       final QueryableDruidServer queryableDruidServer = segment.lhs.pick();
 
       if (queryableDruidServer == null) {
-        log.makeAlert("No servers found for %s?! How can this be?!", segment.rhs).emit();
+        log.makeAlert("No servers found for SegmentDescriptor[%s] for DataSource[%s]?! How can this be?!", segment.rhs, query.getDataSource()).emit();
       } else {
         final DruidServer server = queryableDruidServer.getServer();
         List<SegmentDescriptor> descriptors = serverSegments.get(server);
