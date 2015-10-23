a71c7270b9d3468d1184c25311e6669290ea4505
Himanshu Gupta <g.himanshu@gmail.com>
2015-10-23 16:20:48 -0500

    making [GreaterThan,LessThan,Equals] HavingSpecs more robust by carefully using long vs float for comparison

--- processing/src/main/java/io/druid/query/groupby/having/GreaterThanHavingSpec.java
+++ processing/src/main/java/io/druid/query/groupby/having/GreaterThanHavingSpec.java
@@ -62,9 +62,7 @@ public class GreaterThanHavingSpec implements HavingSpec
   @Override
   public boolean eval(Row row)
   {
-    float metricValue = row.getFloatMetric(aggregationName);
-
-    return Float.compare(metricValue, value.floatValue()) > 0;
+    return HavingSpecMetricComparator.compare(row, aggregationName, value) > 0;
   }
 
   @Override
