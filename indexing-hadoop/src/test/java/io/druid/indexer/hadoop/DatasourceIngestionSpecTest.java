9218b14a75fe59a51f11f02181ea5ab5fd991c91
Himanshu Gupta <g.himanshu@gmail.com>
2016-01-26 17:00:12 -0600

    add ignoreWhenNoSegments flag to optionally ignore the dataSource inputSpec when no segments were found

--- indexing-hadoop/src/test/java/io/druid/indexer/hadoop/DatasourceIngestionSpecTest.java
+++ indexing-hadoop/src/test/java/io/druid/indexer/hadoop/DatasourceIngestionSpecTest.java
@@ -49,7 +49,8 @@ public class DatasourceIngestionSpecTest
         new SelectorDimFilter("dim", "value"),
         QueryGranularity.DAY,
         Lists.newArrayList("d1", "d2"),
-        Lists.newArrayList("m1", "m2", "m3")
+        Lists.newArrayList("m1", "m2", "m3"),
+        false
     );
 
     DatasourceIngestionSpec actual = MAPPER.readValue(MAPPER.writeValueAsString(expected), DatasourceIngestionSpec.class);
@@ -60,22 +61,63 @@ public class DatasourceIngestionSpecTest
   @Test
   public void testMultiIntervalSerde() throws Exception
   {
+    //defaults
+    String jsonStr = "{\n"
+                     + "  \"dataSource\": \"test\",\n"
+                     + "  \"intervals\": [\"2014/2015\", \"2016/2017\"]\n"
+                     + "}\n";
+
+    DatasourceIngestionSpec actual = MAPPER.readValue(
+        MAPPER.writeValueAsString(
+            MAPPER.readValue(jsonStr, DatasourceIngestionSpec.class)
+        ),
+        DatasourceIngestionSpec.class
+    );
+
     List<Interval> intervals = ImmutableList.of(Interval.parse("2014/2015"), Interval.parse("2016/2017"));
+
     DatasourceIngestionSpec expected = new DatasourceIngestionSpec(
+        "test",
+        null,
+        intervals,
+        null,
+        null,
+        null,
+        null,
+        false
+    );
+
+    Assert.assertEquals(expected, actual);
+
+    //non-defaults
+    jsonStr = "{\n"
+              + "  \"dataSource\": \"test\",\n"
+              + "  \"intervals\": [\"2014/2015\", \"2016/2017\"],\n"
+              + "  \"filter\": { \"type\": \"selector\", \"dimension\": \"dim\", \"value\": \"value\"},\n"
+              + "  \"granularity\": \"day\",\n"
+              + "  \"dimensions\": [\"d1\", \"d2\"],\n"
+              + "  \"metrics\": [\"m1\", \"m2\", \"m3\"],\n"
+              + "  \"ignoreWhenNoSegments\": true\n"
+              + "}\n";
+
+    expected = new DatasourceIngestionSpec(
         "test",
         null,
         intervals,
         new SelectorDimFilter("dim", "value"),
         QueryGranularity.DAY,
         Lists.newArrayList("d1", "d2"),
-        Lists.newArrayList("m1", "m2", "m3")
+        Lists.newArrayList("m1", "m2", "m3"),
+        true
     );
 
-    DatasourceIngestionSpec actual = MAPPER.readValue(
-        MAPPER.writeValueAsString(expected),
+    actual = MAPPER.readValue(
+        MAPPER.writeValueAsString(
+            MAPPER.readValue(jsonStr, DatasourceIngestionSpec.class)
+        ),
         DatasourceIngestionSpec.class
     );
-    Assert.assertEquals(intervals, actual.getIntervals());
+
     Assert.assertEquals(expected, actual);
   }
 
@@ -86,7 +128,7 @@ public class DatasourceIngestionSpecTest
     DatasourceIngestionSpec actual = MAPPER.readValue(jsonStr, DatasourceIngestionSpec.class);
 
     Assert.assertEquals(
-        new DatasourceIngestionSpec("test", Interval.parse("2014/2015"), null, null, null, null, null),
+        new DatasourceIngestionSpec("test", Interval.parse("2014/2015"), null, null, null, null, null, false),
         actual
     );
   }
