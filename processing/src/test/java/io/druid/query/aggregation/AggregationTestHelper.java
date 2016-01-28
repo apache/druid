5ddcf11269b776b899945094986916860c6eb58a
saydakov <saydakov@yahoo-inc.com>
2016-01-28 13:20:50 -0800

    fixed createIndex

--- processing/src/test/java/io/druid/query/aggregation/AggregationTestHelper.java
+++ processing/src/test/java/io/druid/query/aggregation/AggregationTestHelper.java
@@ -314,21 +314,19 @@ public class AggregationTestHelper
       index = new OnheapIncrementalIndex(minTimestamp, gran, metrics, deserializeComplexMetrics, maxRowCount);
       while (rows.hasNext()) {
         Object row = rows.next();
-        try {
-          if (row instanceof String && parser instanceof StringInputRowParser) {
-            //Note: this is required because StringInputRowParser is InputRowParser<ByteBuffer> as opposed to
-            //InputRowsParser<String>
-            index.add(((StringInputRowParser) parser).parse((String) row));
-          } else {
-            index.add(parser.parse(row));
-          }
-        }
-        catch (IndexSizeExceededException ex) {
+        if (!index.canAppendRow()) {
           File tmp = tempFolder.newFolder();
           toMerge.add(tmp);
           indexMerger.persist(index, tmp, new IndexSpec());
           index.close();
           index = new OnheapIncrementalIndex(minTimestamp, gran, metrics, deserializeComplexMetrics, maxRowCount);
+        }
+        if (row instanceof String && parser instanceof StringInputRowParser) {
+          //Note: this is required because StringInputRowParser is InputRowParser<ByteBuffer> as opposed to
+          //InputRowsParser<String>
+          index.add(((StringInputRowParser) parser).parse((String) row));
+        } else {
+          index.add(parser.parse(row));
         }
       }
 
