0c3099c47877051213babccaef8ce1631090e258
Rostislav Pashuto <r_pashuto@indatalabs.com>
2016-01-29 17:57:45 +0300

    Parse protobuf nested messages and their fileds, allow to define any message name to parse (not only 1st one)

--- processing/src/main/java/io/druid/data/input/ProtoBufInputRowParser.java
+++ processing/src/main/java/io/druid/data/input/ProtoBufInputRowParser.java
@@ -23,6 +23,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.google.common.base.Throwables;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.Descriptors;
@@ -34,6 +35,8 @@ import io.druid.data.input.impl.ParseSpec;
 
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import static com.google.protobuf.DescriptorProtos.FileDescriptorSet;
@@ -44,20 +47,25 @@ import static com.google.protobuf.Descriptors.FileDescriptor;
 public class ProtoBufInputRowParser implements ByteBufferInputRowParser
 {
   private static final Logger log = new Logger(ProtoBufInputRowParser.class);
+  private static final int MAX_MESSAGE_DEPTH = 10;
+  private static final String CHILD_NAME_SEPARATOR = ".";
 
   private final ParseSpec parseSpec;
   private final MapInputRowParser mapParser;
   private final String descriptorFileInClasspath;
+  private final String protoMessageFullName;
 
   @JsonCreator
   public ProtoBufInputRowParser(
-      @JsonProperty("parseSpec") ParseSpec parseSpec,
-      @JsonProperty("descriptor") String descriptorFileInClasspath
+          @JsonProperty("parseSpec") ParseSpec parseSpec,
+          @JsonProperty("descriptor") String descriptorFileInClasspath,
+          @JsonProperty("message") String protoMessageFullName
   )
   {
     this.parseSpec = parseSpec;
     this.descriptorFileInClasspath = descriptorFileInClasspath;
-      this.mapParser = new MapInputRowParser(this.parseSpec);
+    this.protoMessageFullName = protoMessageFullName;
+    this.mapParser = new MapInputRowParser(this.parseSpec);
   }
 
   @Override
@@ -69,7 +77,7 @@ public class ProtoBufInputRowParser implements ByteBufferInputRowParser
   @Override
   public ProtoBufInputRowParser withParseSpec(ParseSpec parseSpec)
   {
-    return new ProtoBufInputRowParser(parseSpec, descriptorFileInClasspath);
+    return new ProtoBufInputRowParser(parseSpec, descriptorFileInClasspath, protoMessageFullName);
   }
 
   @Override
@@ -89,29 +97,108 @@ public class ProtoBufInputRowParser implements ByteBufferInputRowParser
 
     try {
       DynamicMessage message = DynamicMessage.parseFrom(descriptor, ByteString.copyFrom(input));
-      Map<Descriptors.FieldDescriptor, Object> allFields = message.getAllFields();
-
-      for (Map.Entry<Descriptors.FieldDescriptor, Object> entry : allFields.entrySet()) {
-        String name = entry.getKey().getName();
-        if (theMap.containsKey(name)) {
-          continue;
-          // Perhaps throw an exception here?
-          // throw new RuntimeException("dupicate key " + name + " in " + message);
-        }
-        Object value = entry.getValue();
-        if (value instanceof Descriptors.EnumValueDescriptor) {
-          Descriptors.EnumValueDescriptor desc = (Descriptors.EnumValueDescriptor) value;
-          value = desc.getName();
-        }
-
-        theMap.put(name, value);
-      }
-
+      parseMessage(theMap, null, message, MAX_MESSAGE_DEPTH);
     }
     catch (InvalidProtocolBufferException e) {
       log.warn(e, "Problem with protobuf something");
     }
     return theMap;
+  }
+
+
+  private void parseMessage(Map<String, Object> theMap, String parentName, DynamicMessage message, int depthLimit) {
+    Map<Descriptors.FieldDescriptor, Object> allFields = message.getAllFields();
+    for (Map.Entry<Descriptors.FieldDescriptor, Object> entry : allFields.entrySet()) {
+      String name = glueName(parentName, entry.getKey().getName());
+      if (theMap.containsKey(name)) {
+        continue;
+        // Perhaps throw an exception here?
+        // throw new RuntimeException("dupicate key " + name + " in " + message);
+      }
+      Object value = entry.getValue();
+      if (value instanceof DynamicMessage) {
+        if (depthLimit > 0) {
+          parseMessage(theMap, name, (DynamicMessage) value, depthLimit - 1);
+        }
+      } else if (value instanceof Collection) {
+        for (Object element : (Collection) value) {
+          if (element instanceof DynamicMessage) {
+            if (depthLimit > 0) {
+              Map<String, Object> colMap = Maps.newHashMap();
+              // parse recursively
+              parseMessage(colMap, name, (DynamicMessage) element, depthLimit - 1);
+              // inspect parsed values and convert it to the collection
+              addNewValuesToTheMapCollection(theMap, colMap);
+            }
+          } else if (element instanceof Descriptors.EnumValueDescriptor) {
+            Descriptors.EnumValueDescriptor desc = (Descriptors.EnumValueDescriptor) element;
+            addValueToTheMapCollectionIgnoreDuplicatesHandling(theMap, name, desc.getName());
+          } else {
+            // not a dynamic message or enum, for example it an element from the list of ints
+            addValueToTheMapCollectionIgnoreDuplicatesHandling(theMap, name, element);
+          }
+        }
+        continue;
+      } else if (value instanceof Descriptors.EnumValueDescriptor) {
+        Descriptors.EnumValueDescriptor desc = (Descriptors.EnumValueDescriptor) value;
+        value = desc.getName();
+      }
+      // other types
+      theMap.put(name, value);
+    }
+  }
+
+  private void addNewValuesToTheMapCollection(Map<String, Object> theMap, Map<String, Object> colMap) {
+    for (Map.Entry<String, Object> entrySet : colMap.entrySet()) {
+      try {
+        addValueToTheMapCollection (theMap, entrySet.getKey(), entrySet.getValue());
+      } catch (IllegalKeyException e) {
+        log.warn(e.getMessage(), e);
+      }
+    }
+  }
+
+  private void addValueToTheMapCollection (Map<String, Object> theMap, String key, Object value) throws IllegalKeyException {
+    // get existing value for this key
+    Object maybeCollection = theMap.get(key);
+    Collection collection;
+    if (maybeCollection == null) {
+      collection = Lists.newArrayList(); // MapBasedRow uses list check instead of collection, thus we cannot use Set here
+    } else if (maybeCollection instanceof Collection) {
+      collection = (Collection) maybeCollection;
+    } else { // duplicate names/naming collision
+      throw new IllegalKeyException("not a collection for the key " + key);
+    }
+    // avoid list of lists of lists of ..., merge all such end-values to the top list
+    if (value instanceof Collection) {
+      for (Object singleValue : (Collection) value) {
+        addUniqueValuesToCollection(collection, singleValue);
+      }
+    } else {
+      addUniqueValuesToCollection(collection, value);
+    }
+    theMap.put(key, collection);
+  }
+
+  private void addValueToTheMapCollectionIgnoreDuplicatesHandling(Map<String, Object> theMap, String key, Object value) {
+    try {
+      addValueToTheMapCollection (theMap, key, value);
+    } catch (IllegalKeyException e) {
+      log.warn(e.getMessage(), e);
+    }
+  }
+
+  private String glueName(String parentName, String name) {
+    if (parentName != null) {
+      return parentName + CHILD_NAME_SEPARATOR + name;
+    }
+    return name;
+  }
+
+  private void addUniqueValuesToCollection(Collection collection, Object value) {
+    if (!collection.contains(value)) {
+      collection.add(value);
+    }
   }
 
   private Descriptor getDescriptor(String descriptorFileInClassPath)
@@ -120,13 +207,29 @@ public class ProtoBufInputRowParser implements ByteBufferInputRowParser
       InputStream fin = this.getClass().getClassLoader().getResourceAsStream(descriptorFileInClassPath);
       FileDescriptorSet set = FileDescriptorSet.parseFrom(fin);
       FileDescriptor file = FileDescriptor.buildFrom(
-          set.getFile(0), new FileDescriptor[]
-          {}
+              set.getFile(0), new FileDescriptor[]
+                      {}
       );
-      return file.getMessageTypes().get(0);
+      List<Descriptor> messageTypes = file.getMessageTypes();
+      final Descriptor defaultDescriptor = messageTypes.get(0);
+      if (protoMessageFullName != null) {
+        for (Descriptor descriptor : messageTypes) {
+          String name = descriptor.getFullName();
+          if (name.equals(protoMessageFullName)) {
+            return descriptor;
+          }
+        }
+      }
+      return defaultDescriptor;
     }
     catch (Exception e) {
       throw Throwables.propagate(e);
+    }
+  }
+
+  private class IllegalKeyException extends Exception {
+    public IllegalKeyException(String message) {
+      super(message);
     }
   }
 }
--- processing/src/test/java/io/druid/data/input/ProtoBufInputRowParserTest.java
+++ processing/src/test/java/io/druid/data/input/ProtoBufInputRowParserTest.java
@@ -1,24 +1,23 @@
 /*
- * Licensed to Metamarkets Group Inc. (Metamarkets) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. Metamarkets licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Druid - a distributed column store.
+ * Copyright 2012 - 2015 Metamarkets Group Inc.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package io.druid.data.input;
 
+import com.google.common.collect.Lists;
 import io.druid.data.input.impl.DimensionsSpec;
 import io.druid.data.input.impl.JSONParseSpec;
 import io.druid.data.input.impl.TimestampSpec;
@@ -30,12 +29,25 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Tests protoBuf parsing.
+ * To rebuild the .desc file run this command from ./processing folder as a basedir
+ * protoc --descriptor_set_out=src/test/resources/prototest.desc --include_imports src/test/resources/ProtoTest.proto
+ * To rebuild the .proto file and ProtoTestEventWrapper class run this command from ./processing folder as a basedir
+ * protoc --java_out=src/test/java src/test/resources/ProtoTest.proto
+ */
 public class ProtoBufInputRowParserTest
 {
 
   public static final String[] DIMENSIONS = new String[]{"eventType", "id", "someOtherId", "isValid"};
+  public static final String[] DIMENSIONS_WITH_SUB = new String[]{"id", "description",
+          "location.country", "location.state", "location.city", "location.zipcode",
+          "income.income", "income.currency", "income.occupationType",
+          "interests.interestName", "interests.subInterestNames", "interests.interestCode.code"
+  };
 
   /*
   eventType = 1;
@@ -57,34 +69,32 @@ public class ProtoBufInputRowParserTest
 
     //configure parser with desc file
     ProtoBufInputRowParser parser = new ProtoBufInputRowParser(
-        new JSONParseSpec(
-            new TimestampSpec("timestamp", "iso", null),
-            new DimensionsSpec(Arrays.asList(DIMENSIONS), Arrays.<String>asList(), null)
-        ),
-        "prototest.desc"
+            new JSONParseSpec(
+                    new TimestampSpec("timestamp", "iso", null),
+                    new DimensionsSpec(Arrays.asList(DIMENSIONS), Arrays.<String>asList(), null)
+            ),
+            "prototest.desc", null
     );
 
-
     //create binary of proto test event
-    DateTime dateTime = new DateTime(2012, 07, 12, 9, 30);
+    DateTime dateTime = new DateTime(2012, 7, 12, 9, 30);
     ProtoTestEventWrapper.ProtoTestEvent event = ProtoTestEventWrapper.ProtoTestEvent.newBuilder()
-                                                                      .setDescription("description")
-                                                                      .setEventType(ProtoTestEventWrapper.ProtoTestEvent.EventCategory.CATEGORY_ONE)
-                                                                      .setId(4711L)
-                                                                      .setIsValid(true)
-                                                                      .setSomeOtherId(4712)
-                                                                      .setTimestamp(dateTime.toString())
-                                                                      .setSomeFloatColumn(47.11F)
-                                                                      .setSomeIntColumn(815)
-                                                                      .setSomeLongColumn(816L)
-                                                                      .build();
+            .setDescription("description")
+            .setEventType(ProtoTestEventWrapper.ProtoTestEvent.EventCategory.CATEGORY_ONE)
+            .setId(4711L)
+            .setIsValid(true)
+            .setSomeOtherId(4712)
+            .setTimestamp(dateTime.toString())
+            .setSomeFloatColumn(47.11F)
+            .setSomeIntColumn(815)
+            .setSomeLongColumn(816L)
+            .build();
 
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     event.writeTo(out);
 
     InputRow row = parser.parse(ByteBuffer.wrap(out.toByteArray()));
     System.out.println(row);
-
     assertEquals(Arrays.asList(DIMENSIONS), row.getDimensions());
     assertEquals(dateTime.getMillis(), row.getTimestampFromEpoch());
 
@@ -101,10 +111,102 @@ public class ProtoBufInputRowParserTest
 
   }
 
+  @Test
+  public void testParseSecondMessageByName() throws Exception
+  {
+
+    //configure parser with desc file
+    ProtoBufInputRowParser parser = new ProtoBufInputRowParser(
+            new JSONParseSpec(
+                    new TimestampSpec("timestamp", "iso", null),
+                    new DimensionsSpec(Arrays.asList(DIMENSIONS_WITH_SUB), Arrays.<String>asList(), null)
+            ),
+            "prototest.desc", "prototest.ProtoTestEventWithMessages" // package and message names from .desc file
+    );
+
+    //create binary of proto test event
+    final DateTime dateTime = new DateTime(2012, 7, 12, 9, 30);
+    final String id = "test id";
+    final String description = "test description";
+    final String country = "BY";
+    final String state = "MSK";
+    final String city = "Minsk";
+    final String zipcode = "220000";
+    final float income = 500f;
+    final ProtoTestEventWrapper.ProtoTestEventWithMessages.Currency currency = ProtoTestEventWrapper.ProtoTestEventWithMessages.Currency.USD;
+    final String interestName1 = "Druid";
+    final float interestRate1 = 0.8f;
+    final String interestName2 = "Java";
+    final float interestRate2 = 0.2f;
+    final List<String> interestNames = Lists.newArrayList(interestName1, interestName2);
+    final List<String> subInterests = Lists.newArrayList("sub1", "sub2");
+    final List<ProtoTestEventWrapper.ProtoTestEventWithMessages.OccupationType> occupationTypes = Lists.newArrayList(ProtoTestEventWrapper.ProtoTestEventWithMessages.OccupationType.SINGLE, ProtoTestEventWrapper.ProtoTestEventWithMessages.OccupationType.MULTI);
+    final List<String> occupationTypeStrings = Lists.newArrayList(occupationTypes.get(0).name(), occupationTypes.get(1).name());
+    final String interestCode = "A671B353C";
+    ProtoTestEventWrapper.ProtoTestEventWithMessages event = ProtoTestEventWrapper.ProtoTestEventWithMessages.newBuilder()
+            .setId(id)
+            .setTimestamp(dateTime.toString())
+            .setDescription(description)
+            .setLocation(ProtoTestEventWrapper.ProtoTestEventWithMessages.Location.newBuilder()
+                    .setCountry(country)
+                    .setState(state)
+                    .setCity(city)
+                    .setZipcode(zipcode))
+            .setIncome(ProtoTestEventWrapper.ProtoTestEventWithMessages.Income.newBuilder()
+                    .setCurrency(ProtoTestEventWrapper.ProtoTestEventWithMessages.Currency.USD)
+                    .setIncome(income)
+                    .addAllOccupationType(occupationTypes))
+            .addInterests(ProtoTestEventWrapper.ProtoTestEventWithMessages.Interest.newBuilder()
+                    .setInterestName(interestName1)
+                    .setInterestRate(interestRate1)
+                    .addAllSubInterestNames(subInterests)
+                    .setInterestCode(ProtoTestEventWrapper.ProtoTestEventWithMessages.Interest.InterestCode.newBuilder().setCode(interestCode)))
+            .addInterests(ProtoTestEventWrapper.ProtoTestEventWithMessages.Interest.newBuilder()
+                    .setInterestName(interestName2)
+                    .setInterestRate(interestRate2)
+                    .addAllSubInterestNames(subInterests))
+            .build();
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    event.writeTo(out);
+
+    InputRow row = parser.parse(ByteBuffer.wrap(out.toByteArray()));
+    System.out.println(row);
+    assertEquals(Arrays.asList(DIMENSIONS_WITH_SUB), row.getDimensions());
+    assertEquals(dateTime.getMillis(), row.getTimestampFromEpoch());
+
+    assertDimensionEquals(row, "id", id);
+    assertDimensionEquals(row, "description", description);
+    // test access to sub-fields by dot
+    assertDimensionEquals(row, "location.country", country);
+    assertDimensionEquals(row, "location.state", state);
+    assertDimensionEquals(row, "location.city", city);
+    assertDimensionEquals(row, "location.zipcode", zipcode);
+    assertEquals(income, row.getFloatMetric("income.income"), 0.1f);
+    assertDimensionEquals(row, "income.currency", currency.name());
+    // test access to sub-fields of collection by dot since druid support multi-values columns
+    // string
+    assertDimensionEqualsMultipleValues(row, "interests.interestName", interestNames.toArray(new String[interestNames.size()]), interestNames.size());
+    // unique strings from deeper level
+    assertDimensionEqualsMultipleValues(row, "interests.subInterestNames", subInterests.toArray(new String[subInterests.size()]), subInterests.size());
+    // enum
+    assertDimensionEqualsMultipleValues(row, "income.occupationType", occupationTypeStrings.toArray(new String[occupationTypeStrings.size()]), occupationTypeStrings.size());
+    // 3rd level variable
+    assertDimensionEquals(row, "interests.interestCode.code", interestCode);
+  }
+
   private void assertDimensionEquals(InputRow row, String dimension, Object expected)
   {
     List<String> values = row.getDimension(dimension);
-    assertEquals(1, values.size());
-    assertEquals(expected, values.get(0));
+    assertEquals("invalid values array size", 1, values.size());
+    assertEquals("invalid dimension value", expected, values.get(0));
   }
+
+  private void assertDimensionEqualsMultipleValues(InputRow row, String dimension, String[] expected, int size)
+  {
+    List<String> values = row.getDimension(dimension);
+    assertEquals("invalid values array size:" + values, size, values.size());
+    assertArrayEquals("invalid list of values", expected, values.toArray());
+  }
+
 }
