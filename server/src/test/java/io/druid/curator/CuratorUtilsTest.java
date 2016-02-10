8e90d6474ee21aa6563de0cfe6940d09630663a8
Gian Merlino <gianmerlino@gmail.com>
2016-02-10 11:51:04 -0800

    Harmonize znode writing code in RTR and Worker.
    - Throw most exceptions rather than suppressing them, which should help
      detect problems. Continue suppressing exceptions that make sense to
      suppress.
    - Handle payload length checks consistently, and improve error message.
    - Remove unused WorkerCuratorCoordinator.announceTaskAnnouncement method.
    - Max znode length should be int, not long.
    - Add tests.

---
+++ server/src/test/java/io/druid/curator/CuratorUtilsTest.java
@@ -0,0 +1,117 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.druid.curator;
+
+import org.apache.zookeeper.CreateMode;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CuratorUtilsTest extends CuratorTestBase
+{
+  @Before
+  public void setUp() throws Exception
+  {
+    setupServerAndCurator();
+  }
+
+  @After
+  public void tearDown()
+  {
+    tearDownServerAndCurator();
+  }
+
+  @Test(timeout = 10_000L)
+  public void testCreateIfNotExists() throws Exception
+  {
+    curator.start();
+    curator.blockUntilConnected();
+
+    CuratorUtils.createIfNotExists(
+        curator,
+        "/foo/bar",
+        CreateMode.PERSISTENT,
+        "baz".getBytes(),
+        CuratorUtils.DEFAULT_MAX_ZNODE_BYTES
+    );
+    Assert.assertEquals("baz", new String(curator.getData().forPath("/foo/bar")));
+
+    CuratorUtils.createIfNotExists(
+        curator,
+        "/foo/bar",
+        CreateMode.PERSISTENT,
+        "qux".getBytes(),
+        CuratorUtils.DEFAULT_MAX_ZNODE_BYTES
+    );
+    Assert.assertEquals("baz", new String(curator.getData().forPath("/foo/bar")));
+  }
+
+  @Test(timeout = 10_000L)
+  public void testCreateIfNotExistsPayloadTooLarge() throws Exception
+  {
+    curator.start();
+    curator.blockUntilConnected();
+
+    Exception thrown = null;
+    try {
+      CuratorUtils.createIfNotExists(curator, "/foo/bar", CreateMode.PERSISTENT, "baz".getBytes(), 2);
+    }
+    catch (Exception e) {
+      thrown = e;
+    }
+
+    Assert.assertTrue(thrown instanceof IllegalArgumentException);
+    Assert.assertNull(curator.checkExists().forPath("/foo/bar"));
+  }
+
+  @Test(timeout = 10_000L)
+  public void testCreateOrSet() throws Exception
+  {
+    curator.start();
+    curator.blockUntilConnected();
+
+    Assert.assertNull(curator.checkExists().forPath("/foo/bar"));
+
+    CuratorUtils.createOrSet(curator, "/foo/bar", CreateMode.PERSISTENT, "baz".getBytes(), 3);
+    Assert.assertEquals("baz", new String(curator.getData().forPath("/foo/bar")));
+
+    CuratorUtils.createOrSet(curator, "/foo/bar", CreateMode.PERSISTENT, "qux".getBytes(), 3);
+    Assert.assertEquals("qux", new String(curator.getData().forPath("/foo/bar")));
+  }
+
+  @Test(timeout = 10_000L)
+  public void testCreateOrSetPayloadTooLarge() throws Exception
+  {
+    curator.start();
+    curator.blockUntilConnected();
+
+    Exception thrown = null;
+    try {
+      CuratorUtils.createOrSet(curator, "/foo/bar", CreateMode.PERSISTENT, "baz".getBytes(), 2);
+    }
+    catch (Exception e) {
+      thrown = e;
+    }
+
+    Assert.assertTrue(thrown instanceof IllegalArgumentException);
+    Assert.assertNull(curator.checkExists().forPath("/foo/bar"));
+  }
+}
