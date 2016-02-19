70b173ddc9fae519fb685e4b59a0e1ba6d273708
Charles Allen <charles@allen-net.com>
2016-02-19 09:32:03 -0800

    Add hadoop classloader isolation tests for HadoopTask

--- indexing-service/src/main/java/io/druid/indexing/common/task/HadoopTask.java
+++ indexing-service/src/main/java/io/druid/indexing/common/task/HadoopTask.java
@@ -36,6 +36,7 @@ import javax.annotation.Nullable;
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -96,7 +97,36 @@ public abstract class HadoopTask extends AbstractTask
     }
   };
 
-  protected ClassLoader buildClassLoader(final TaskToolbox toolbox) throws Exception
+  /**
+   * This makes an isolated classloader that has classes loaded in the "proper" priority.
+   *
+   * This isolation is *only* for the part of the HadoopTask that calls runTask in an isolated manner.
+   *
+   * Jars for the job are the same jars as for the classloader EXCEPT the hadoopDependencyCoordinates, which are not used in the job jars.
+   *
+   * The URLs in the resultant classloader are loaded in this priority:
+   *
+   * 1. Non-Druid jars (see IS_DRUID_URL) found in the ClassLoader for HadoopIndexTask.class. This will probably be the ApplicationClassLoader
+   * 2. Hadoop jars found in the hadoop dependency coordinates directory, loaded in the order they are specified in
+   * 3. Druid jars (see IS_DRUID_URL) found in the ClassLoader for HadoopIndexTask.class
+   * 4. Extension URLs maintaining the order specified in the extensions list in the extensions config
+   *
+   * At one point I tried making each one of these steps a URLClassLoader, but it is not easy to make a properly
+   * predictive IS_DRUID_URL that captures all things which reference druid classes. This lead to a case where the
+   * class loader isolation worked great for stock druid, but failed in many common use cases including extension
+   * jars on the classpath which were not listed in the extensions list.
+   *
+   * As such, the current approach is to make a list of URLs for a URLClassLoader based on the priority above, and use
+   * THAT ClassLoader with a null parent as the isolated loader for running hadoop or hadoop-like driver tasks.
+   * Such an approach combined with reasonable exclusions in io.druid.cli.PullDependencies#exclusions tries to maintain
+   * sanity in a ClassLoader where all jars (which are isolated by extension ClassLoaders in the Druid framework) are
+   * jumbled together into one ClassLoader for Hadoop and Hadoop-like tasks (Spark for example).
+   *
+   * @param toolbox The toolbox to pull the default coordinates from if not present in the task
+   * @return An isolated URLClassLoader not tied by parent chain to the ApplicationClassLoader
+   * @throws MalformedURLException from Initialization.getClassLoaderForExtension
+   */
+  protected ClassLoader buildClassLoader(final TaskToolbox toolbox) throws MalformedURLException
   {
     final List<String> finalHadoopDependencyCoordinates = hadoopDependencyCoordinates != null
                                                           ? hadoopDependencyCoordinates
---
+++ indexing-service/src/test/java/io/druid/indexing/common/task/HadoopTaskTest.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.druid.indexing.common.task;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import io.druid.indexer.updater.HadoopDruidConverterConfig;
+import io.druid.indexing.common.TaskStatus;
+import io.druid.indexing.common.TaskToolbox;
+import io.druid.indexing.common.actions.TaskActionClient;
+import io.druid.indexing.common.config.TaskConfig;
+import org.apache.hadoop.yarn.util.ApplicationClassLoader;
+import org.easymock.EasyMock;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.net.URLClassLoader;
+
+public class HadoopTaskTest
+{
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+  @Test
+  public void testBuildClassLoader() throws Exception
+  {
+    final HadoopTask task = new HadoopTask(
+        "taskId",
+        "dataSource",
+        ImmutableList.<String>of(),
+        ImmutableMap.<String, Object>of()
+    )
+    {
+      @Override
+      public String getType()
+      {
+        return null;
+      }
+
+      @Override
+      public boolean isReady(TaskActionClient taskActionClient) throws Exception
+      {
+        return false;
+      }
+
+      @Override
+      public TaskStatus run(TaskToolbox toolbox) throws Exception
+      {
+        return null;
+      }
+    };
+    final TaskToolbox toolbox = EasyMock.createStrictMock(TaskToolbox.class);
+    EasyMock.expect(toolbox.getConfig()).andReturn(new TaskConfig(
+        temporaryFolder.newFolder().toString(),
+        null,
+        null,
+        null,
+        ImmutableList.of("something:hadoop:1"),
+        false,
+        null,
+        null
+    )).once();
+    EasyMock.replay(toolbox);
+
+    final ClassLoader classLoader = task.buildClassLoader(toolbox);
+    assertClassLoaderIsSingular(classLoader);
+
+    final Class<?> hadoopClazz = Class.forName("org.apache.hadoop.fs.FSDataInputStream", false, classLoader);
+    assertClassLoaderIsSingular(hadoopClazz.getClassLoader());
+
+    final Class<?> druidHadoopConfigClazz = Class.forName("io.druid.indexer.HadoopDruidIndexerConfig", false, classLoader);
+    assertClassLoaderIsSingular(druidHadoopConfigClazz.getClassLoader());
+  }
+  public static void assertClassLoaderIsSingular(ClassLoader classLoader)
+  {
+    // This is a check against the current HadoopTask which creates a single URLClassLoader with null parent
+    Assert.assertNull(classLoader.getParent());
+    Assert.assertFalse(classLoader instanceof ApplicationClassLoader);
+    Assert.assertTrue(classLoader instanceof URLClassLoader);
+
+    final ClassLoader appLoader = HadoopDruidConverterConfig.class.getClassLoader();
+    Assert.assertNotEquals(String.format("ClassLoader [%s] is not isolated!", classLoader), appLoader, classLoader);
+  }
+}
