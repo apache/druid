63c123d3f53fd150a883cb8f9aacae9b577464bd
Charles Allen <charles@allen-net.com>
2016-02-25 08:04:25 -0800

    UnRegister port in ForkingTaskRunner

--- indexing-service/src/main/java/io/druid/indexing/overlord/ForkingTaskRunner.java
+++ indexing-service/src/main/java/io/druid/indexing/overlord/ForkingTaskRunner.java
@@ -450,6 +450,7 @@ public class ForkingTaskRunner implements TaskRunner, TaskLogStreamer
                               }
                             }
 
+                            portFinder.markPortUnused(childPort);
                             if (childChatHandlerPort > 0) {
                               portFinder.markPortUnused(childChatHandlerPort);
                             }
