b531ab15e1bc32702b158f14fe6aa03b8fa79386
Jaehong Choi <hong83@gmail.com>
2016-04-26 01:45:46 +0900

    initial commits for finalizeFieldAccess #2433

--- processing/src/main/java/io/druid/query/Queries.java
+++ processing/src/main/java/io/druid/query/Queries.java
@@ -20,11 +20,16 @@
 package io.druid.query;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import io.druid.query.aggregation.AggregatorFactory;
+import io.druid.query.aggregation.HasDependentAggFactories;
 import io.druid.query.aggregation.PostAggregator;
 
+import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -38,15 +43,16 @@ public class Queries
   {
     Preconditions.checkNotNull(aggFactories, "aggregations cannot be null");
 
-    final Set<String> aggNames = Sets.newHashSet();
+    final Map<String, AggregatorFactory> aggsFactoryMap = Maps.newHashMap();
     for (AggregatorFactory aggFactory : aggFactories) {
-      Preconditions.checkArgument(aggNames.add(aggFactory.getName()), "[%s] already defined", aggFactory.getName());
+      Preconditions.checkArgument(aggsFactoryMap.containsKey(aggFactory.getName()), "[%s] already defined", aggFactory.getName());
+      aggsFactoryMap.put(aggFactory.getName(), aggFactory);
     }
 
     if (postAggs != null && !postAggs.isEmpty()) {
-      final Set<String> combinedAggNames = Sets.newHashSet(aggNames);
+      final Set<String> combinedAggNames = aggsFactoryMap.keySet();
 
-      for (PostAggregator postAgg : postAggs) {
+      for (final PostAggregator postAgg : postAggs) {
         final Set<String> dependencies = postAgg.getDependentFields();
         final Set<String> missing = Sets.difference(dependencies, combinedAggNames);
 
@@ -55,6 +61,18 @@ public class Queries
             "Missing fields [%s] for postAggregator [%s]", missing, postAgg.getName()
         );
         Preconditions.checkArgument(combinedAggNames.add(postAgg.getName()), "[%s] already defined", postAgg.getName());
+
+        if (postAgg instanceof HasDependentAggFactories) {
+          HasDependentAggFactories richPostAgg = (HasDependentAggFactories)postAgg;
+          richPostAgg.setDependentAggFactories(Maps.filterKeys(aggsFactoryMap, new Predicate<String>()
+          {
+            @Override
+            public boolean apply(@Nullable String input)
+            {
+              return postAgg.getDependentFields().contains(input);
+            }
+          }));
+        }
       }
     }
   }
---
+++ processing/src/test/java/io/druid/query/aggregation/post/FinalFieldAccessPostAggregatorTest.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.druid.query.aggregation.post;
+
+import io.druid.query.aggregation.CountAggregator;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FinalFieldAccessPostAggregatorTest
+{
+  @Test
+  public void testComputeWithoutFinalizing()
+  {
+    FinalFieldAccessPostAggregator finalFieldAccessPostAggregator
+        = new FinalFieldAccessPostAggregator("finalName1", "rows");
+    CountAggregator agg = new CountAggregator("rows");
+    Map<String, Object> metricValues = new HashMap<String, Object>();
+    metricValues.put(agg.getName(), agg.get());
+    Assert.assertEquals(new Long(0L), finalFieldAccessPostAggregator.compute(metricValues));
+
+    agg.aggregate();
+    agg.aggregate();
+    agg.aggregate();
+    metricValues.put(agg.getName(), agg.get());
+    Assert.assertEquals(new Long(3L), finalFieldAccessPostAggregator.compute(metricValues));
+  }
+
+  @Test
+  public void testComputedWithFinalizing()
+  {
+    FinalFieldAccessPostAggregator finalFieldAccessPostAggregator
+        = new FinalFieldAccessPostAggregator("finalName1", "rows");
+  }
+}
