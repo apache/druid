84681990ed79fb1079028f7b471c5891d3274a82
Jaehong Choi <hong83@gmail.com>
2016-04-26 19:20:26 +0900

    add Uts

--- processing/src/test/java/io/druid/query/aggregation/post/FinalFieldAccessPostAggregatorTest.java
+++ processing/src/test/java/io/druid/query/aggregation/post/FinalFieldAccessPostAggregatorTest.java
@@ -19,37 +19,124 @@
 
 package io.druid.query.aggregation.post;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.metamx.common.guava.Sequence;
+import com.metamx.common.guava.Sequences;
+import io.druid.data.input.MapBasedRow;
+import io.druid.granularity.QueryGranularity;
+import io.druid.jackson.AggregatorsModule;
+import io.druid.query.aggregation.AggregationTestHelper;
+import io.druid.query.aggregation.Aggregator;
+import io.druid.query.aggregation.AggregatorFactory;
 import io.druid.query.aggregation.CountAggregator;
+import io.druid.query.aggregation.PostAggregator;
+import org.easymock.EasyMock;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.util.HashMap;
+import java.io.File;
 import java.util.Map;
 
 public class FinalFieldAccessPostAggregatorTest
 {
+  @Rule
+  public final TemporaryFolder tempFoler = new TemporaryFolder();
+
   @Test
   public void testComputeWithoutFinalizing()
   {
-    FinalFieldAccessPostAggregator finalFieldAccessPostAggregator
-        = new FinalFieldAccessPostAggregator("finalName1", "rows");
-    CountAggregator agg = new CountAggregator("rows");
-    Map<String, Object> metricValues = new HashMap<String, Object>();
-    metricValues.put(agg.getName(), agg.get());
-    Assert.assertEquals(new Long(0L), finalFieldAccessPostAggregator.compute(metricValues));
+    Aggregator agg = new CountAggregator("rows");
+    agg.aggregate();
+    agg.aggregate();
+    agg.aggregate();
 
-    agg.aggregate();
-    agg.aggregate();
-    agg.aggregate();
+    Map<String, Object> metricValues = Maps.newHashMap();
     metricValues.put(agg.getName(), agg.get());
-    Assert.assertEquals(new Long(3L), finalFieldAccessPostAggregator.compute(metricValues));
+
+    PostAggregator postAgg = new FinalFieldAccessPostAggregator("final_rows", "rows");
+    Assert.assertEquals(new Long(3L), postAgg.compute(metricValues));
   }
 
   @Test
   public void testComputedWithFinalizing()
   {
-    FinalFieldAccessPostAggregator finalFieldAccessPostAggregator
-        = new FinalFieldAccessPostAggregator("finalName1", "rows");
+    AggregatorFactory aggFactory = EasyMock.createMock(AggregatorFactory.class);
+    EasyMock.expect(aggFactory.finalizeComputation("test"))
+            .andReturn(new Long(3L))
+            .times(1);
+    EasyMock.replay(aggFactory);
 
+    FinalFieldAccessPostAggregator postAgg = new FinalFieldAccessPostAggregator("final_billy", "billy");
+    postAgg.setDependentAggFactories(ImmutableMap.of("billy", aggFactory));
+
+    Map<String, Object> metricValues = Maps.newHashMap();
+    metricValues.put("billy", "test");
+
+    Assert.assertEquals(new Long(3L), postAgg.compute(metricValues));
+    EasyMock.verify(aggFactory);
+  }
+
+  @Test
+  public void tesstIngestAndQuery() throws Exception
+  {
+    AggregationTestHelper helper = AggregationTestHelper.createGroupByQueryAggregationTestHelper(
+        Lists.newArrayList(new AggregatorsModule()),
+        tempFoler
+    );
+
+    String metricSpec = "[{"
+                        + "\"type\": \"hyperUnique\","
+                        + "\"name\": \"index_hll\","
+                        + "\"fieldName\": \"market\""
+                        + "}]";
+
+    String parseSpec = "{"
+                       + "\"type\" : \"string\","
+                       + "\"parseSpec\" : {"
+                       + "    \"format\" : \"tsv\","
+                       + "    \"timestampSpec\" : {"
+                       + "        \"column\" : \"timestamp\","
+                       + "        \"format\" : \"auto\""
+                       + "},"
+                       + "    \"dimensionsSpec\" : {"
+                       + "        \"dimensions\": [],"
+                       + "        \"dimensionExclusions\" : [],"
+                       + "        \"spatialDimensions\" : []"
+                       + "    },"
+                       + "    \"columns\": [\"timestamp\", \"market\", \"quality\", \"placement\", \"placementish\", \"index\"]"
+                       + "  }"
+                       + "}";
+
+    String query = "{"
+                   + "\"queryType\": \"groupBy\","
+                   + "\"dataSource\": \"test_datasource\","
+                   + "\"granularity\": \"ALL\","
+                   + "\"dimensions\": [],"
+                   + "\"aggregations\": ["
+                   + "  { \"type\": \"hyperUnique\", \"name\": \"index_hll\", \"fieldName\": \"index_hll\" }"
+                   + "],"
+                   + "\"postAggregations\": ["
+                   + "  { \"type\": \"finalFieldAccess\", \"name\": \"index_unique_count\", \"fieldName\": \"index_hll\" }"
+                   + "],"
+                   + "\"intervals\": [ \"1970/2050\" ]"
+                   + "}";
+
+    Sequence seq = helper.createIndexAndRunQueryOnSegment(
+        new File(this.getClass().getClassLoader().getResource("druid.sample.tsv").getFile()),
+        parseSpec,
+        metricSpec,
+        0,
+        QueryGranularity.NONE,
+        50000,
+        query
+    );
+
+    MapBasedRow row = (MapBasedRow) Sequences.toList(seq, Lists.newArrayList()).get(0);
+    Assert.assertEquals(3.0, row.getFloatMetric("index_hll"), 0.1);
+    Assert.assertEquals(3.0, row.getFloatMetric("index_unique_count"), 0.1);
   }
 }
