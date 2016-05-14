apache/druid

commit 488d12d592fa522e751e86dd42c2b417f22bcff3

CombiningSequence: Delay making next yielder on creation until it is actually asked for. (#2892)
This fixes the behavior of limited combining sequences (otherwise limit = 1 would
actually yield 2 elements).

common/src/main/java/io/druid/common/guava/CombiningSequence.java
@@ -81,7 +81,7 @@ public class CombiningSequence<T> implements Sequence<T>
   }
 
   public <OutType> Yielder<OutType> makeYielder(
-      Yielder<T> yielder,
+      final Yielder<T> yielder,
       final CombiningYieldingAccumulator<OutType, T> combiningAccumulator,
       boolean finalValue
   )
@@ -92,7 +92,7 @@ public class CombiningSequence<T> implements Sequence<T>
 
     if(!yielder.isDone()) {
       retVal = combiningAccumulator.getRetVal();
-      finalYielder = yielder.next(yielder.get());
+      finalYielder = null;
       finalFinalValue = false;
     } else {
       if(!finalValue && combiningAccumulator.accumulatedSomething()) {
@@ -124,7 +124,11 @@ public class CombiningSequence<T> implements Sequence<T>
       public Yielder<OutType> next(OutType initValue)
       {
         combiningAccumulator.reset();
-        return makeYielder(finalYielder, combiningAccumulator, finalFinalValue);
+        return makeYielder(
+            finalYielder == null ? yielder.next(yielder.get()) : finalYielder,
+            combiningAccumulator,
+            finalFinalValue
+        );
       }
 
       @Override
@@ -136,7 +140,9 @@ public class CombiningSequence<T> implements Sequence<T>
       @Override
       public void close() throws IOException
       {
-        finalYielder.close();
+        if (finalYielder != null) {
+          finalYielder.close();
+        }
       }
     };
   }

common/src/test/java/io/druid/common/guava/CombiningSequenceTest.java
@@ -20,6 +20,7 @@
 package io.druid.common.guava;
 
 import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Iterators;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Ordering;
@@ -47,7 +48,7 @@ public class CombiningSequenceTest
   @Parameterized.Parameters
   public static Collection<Object[]> valuesToTry()
   {
-    return Arrays.asList(new Object[][] {
+    return Arrays.asList(new Object[][]{
         {1}, {2}, {3}, {4}, {5}, {1000}
     });
   }
@@ -194,27 +195,48 @@ public class CombiningSequenceTest
   private void testCombining(List<Pair<Integer, Integer>> pairs, List<Pair<Integer, Integer>> expected)
       throws IOException
   {
-    Sequence<Pair<Integer, Integer>> seq = new CombiningSequence<Pair<Integer, Integer>>(
-        Sequences.simple(pairs),
-        Ordering.natural().onResultOf(Pair.<Integer, Integer>lhsFn()),
-        new BinaryFn<Pair<Integer, Integer>, Pair<Integer, Integer>, Pair<Integer, Integer>>()
-        {
-          @Override
-          public Pair<Integer, Integer> apply(
-              Pair<Integer, Integer> lhs, Pair<Integer, Integer> rhs
-          )
-          {
-            if (lhs == null) {
-              return rhs;
-            }
+    for (int limit = 0; limit < expected.size() + 1; limit++) {
+      // limit = 0 doesn't work properly; it returns 1 element
+      final int expectedLimit = limit == 0 ? 1 : limit;
 
-            if (rhs == null) {
-              return lhs;
-            }
+      testCombining(
+          pairs,
+          Lists.newArrayList(Iterables.limit(expected, expectedLimit)),
+          limit
+      );
+    }
+  }
 
-            return Pair.of(lhs.lhs, lhs.rhs + rhs.rhs);
-          }
-        }
+  private void testCombining(
+      List<Pair<Integer, Integer>> pairs,
+      List<Pair<Integer, Integer>> expected,
+      int limit
+  ) throws IOException
+  {
+    Sequence<Pair<Integer, Integer>> seq = Sequences.limit(
+        new CombiningSequence<>(
+            Sequences.simple(pairs),
+            Ordering.natural().onResultOf(Pair.<Integer, Integer>lhsFn()),
+            new BinaryFn<Pair<Integer, Integer>, Pair<Integer, Integer>, Pair<Integer, Integer>>()
+            {
+              @Override
+              public Pair<Integer, Integer> apply(
+                  Pair<Integer, Integer> lhs, Pair<Integer, Integer> rhs
+              )
+              {
+                if (lhs == null) {
+                  return rhs;
+                }
+
+                if (rhs == null) {
+                  return lhs;
+                }
+
+                return Pair.of(lhs.lhs, lhs.rhs + rhs.rhs);
+              }
+            }
+        ),
+        limit
     );
 
     List<Pair<Integer, Integer>> merged = Sequences.toList(seq, Lists.<Pair<Integer, Integer>>newArrayList());
@@ -233,7 +255,9 @@ public class CombiningSequenceTest
           )
           {
             count++;
-            if(count % yieldEvery == 0) yield();
+            if (count % yieldEvery == 0) {
+              yield();
+            }
             return rhs;
           }
         }

commit 05aef8606fb13d7f01f51b557046966abb93a43e

Fix CombiningSequence.close on single element sequences.
Regression introduced by #2892.

common/src/main/java/io/druid/common/guava/CombiningSequence.java
@@ -140,9 +140,7 @@ public class CombiningSequence<T> implements Sequence<T>
       @Override
       public void close() throws IOException
       {
-        if (finalYielder != null) {
-          finalYielder.close();
-        }
+        yielder.close();
       }
     };
   }

common/src/test/java/io/druid/common/guava/CombiningSequenceTest.java
@@ -25,6 +25,7 @@ import com.google.common.collect.Iterators;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Ordering;
 import com.metamx.common.Pair;
+import com.metamx.common.guava.ResourceClosingSequence;
 import com.metamx.common.guava.Sequence;
 import com.metamx.common.guava.Sequences;
 import com.metamx.common.guava.Yielder;
@@ -36,11 +37,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import javax.annotation.Nullable;
+import java.io.Closeable;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(Parameterized.class)
 public class CombiningSequenceTest
@@ -61,7 +65,7 @@ public class CombiningSequenceTest
   }
 
   @Test
-  public void testMerge() throws IOException
+  public void testMerge() throws Exception
   {
     List<Pair<Integer, Integer>> pairs = Arrays.asList(
         Pair.of(0, 1),
@@ -87,7 +91,7 @@ public class CombiningSequenceTest
   }
 
   @Test
-  public void testNoMergeOne() throws IOException
+  public void testNoMergeOne() throws Exception
   {
     List<Pair<Integer, Integer>> pairs = Arrays.asList(
         Pair.of(0, 1)
@@ -101,7 +105,7 @@ public class CombiningSequenceTest
   }
 
   @Test
-  public void testMergeMany() throws IOException
+  public void testMergeMany() throws Exception
   {
     List<Pair<Integer, Integer>> pairs = Arrays.asList(
         Pair.of(0, 6),
@@ -125,7 +129,7 @@ public class CombiningSequenceTest
   }
 
   @Test
-  public void testNoMergeTwo() throws IOException
+  public void testNoMergeTwo() throws Exception
   {
     List<Pair<Integer, Integer>> pairs = Arrays.asList(
         Pair.of(0, 1),
@@ -141,7 +145,7 @@ public class CombiningSequenceTest
   }
 
   @Test
-  public void testMergeTwo() throws IOException
+  public void testMergeTwo() throws Exception
   {
     List<Pair<Integer, Integer>> pairs = Arrays.asList(
         Pair.of(0, 1),
@@ -156,7 +160,7 @@ public class CombiningSequenceTest
   }
 
   @Test
-  public void testMergeSomeThingsMergedAtEnd() throws IOException
+  public void testMergeSomeThingsMergedAtEnd() throws Exception
   {
     List<Pair<Integer, Integer>> pairs = Arrays.asList(
         Pair.of(0, 1),
@@ -193,7 +197,7 @@ public class CombiningSequenceTest
   }
 
   private void testCombining(List<Pair<Integer, Integer>> pairs, List<Pair<Integer, Integer>> expected)
-      throws IOException
+      throws Exception
   {
     for (int limit = 0; limit < expected.size() + 1; limit++) {
       // limit = 0 doesn't work properly; it returns 1 element
@@ -211,11 +215,22 @@ public class CombiningSequenceTest
       List<Pair<Integer, Integer>> pairs,
       List<Pair<Integer, Integer>> expected,
       int limit
-  ) throws IOException
+  ) throws Exception
   {
+    // Test that closing works too
+    final CountDownLatch closed = new CountDownLatch(1);
+    final Closeable closeable = new Closeable()
+    {
+      @Override
+      public void close() throws IOException
+      {
+        closed.countDown();
+      }
+    };
+
     Sequence<Pair<Integer, Integer>> seq = Sequences.limit(
         new CombiningSequence<>(
-            Sequences.simple(pairs),
+            new ResourceClosingSequence<>(Sequences.simple(pairs), closeable),
             Ordering.natural().onResultOf(Pair.<Integer, Integer>lhsFn()),
             new BinaryFn<Pair<Integer, Integer>, Pair<Integer, Integer>, Pair<Integer, Integer>>()
             {
@@ -294,5 +309,7 @@ public class CombiningSequenceTest
     Assert.assertTrue(yielder.isDone());
     Assert.assertFalse(expectedVals.hasNext());
     yielder.close();
+
+    Assert.assertTrue("resource closed", closed.await(10000, TimeUnit.MILLISECONDS));
   }
 }