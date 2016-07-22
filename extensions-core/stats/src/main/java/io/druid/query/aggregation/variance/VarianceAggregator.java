metatron-app/metatron-cube

Support variance and standard deviation

processing/src/main/java/io/druid/query/aggregation/VarianceAggregator.java
@@ -1,0 +1,142 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.druid.query.aggregation;
+
+import io.druid.segment.FloatColumnSelector;
+import io.druid.segment.LongColumnSelector;
+import io.druid.segment.ObjectColumnSelector;
+
+/**
+ */
+public abstract class VarianceAggregator implements Aggregator
+{
+  protected final String name;
+
+  protected final VarianceHolder holder = new VarianceHolder();
+
+  public VarianceAggregator(String name)
+  {
+    this.name = name;
+  }
+
+  @Override
+  public void reset()
+  {
+    holder.reset();
+  }
+
+  @Override
+  public Object get()
+  {
+    return holder;
+  }
+
+  @Override
+  public String getName()
+  {
+    return name;
+  }
+
+  @Override
+  public void close()
+  {
+  }
+
+  @Override
+  public float getFloat()
+  {
+    throw new UnsupportedOperationException("VarianceAggregator does not support getFloat()");
+  }
+
+  @Override
+  public long getLong()
+  {
+    throw new UnsupportedOperationException("VarianceAggregator does not support getLong()");
+  }
+
+  public static final class FloatInput extends VarianceAggregator
+  {
+    private final FloatColumnSelector selector;
+
+    public FloatInput(String name, FloatColumnSelector selector)
+    {
+      super(name);
+      this.selector = selector;
+    }
+
+    @Override
+    public void aggregate()
+    {
+      holder.add(selector.get());
+    }
+
+    @Override
+    public Aggregator clone()
+    {
+      return new FloatInput(name, selector);
+    }
+  }
+
+  public static final class LongInput extends VarianceAggregator
+  {
+    private final LongColumnSelector selector;
+
+    public LongInput(String name, LongColumnSelector selector)
+    {
+      super(name);
+      this.selector = selector;
+    }
+
+    @Override
+    public void aggregate()
+    {
+      holder.add(selector.get());
+    }
+
+    @Override
+    public Aggregator clone()
+    {
+      return new LongInput(name, selector);
+    }
+  }
+
+  public static final class ObjectInput extends VarianceAggregator
+  {
+    private final ObjectColumnSelector selector;
+
+    public ObjectInput(String name, ObjectColumnSelector selector)
+    {
+      super(name);
+      this.selector = selector;
+    }
+
+    @Override
+    public void aggregate()
+    {
+      VarianceHolder.combineValues(holder, selector.get());
+    }
+
+    @Override
+    public Aggregator clone()
+    {
+      return new ObjectInput(name, selector);
+    }
+  }
+}

processing/src/test/java/io/druid/query/aggregation/VarianceAggregatorTest.java
@@ -1,0 +1,163 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.druid.query.aggregation;
+
+import io.druid.jackson.DefaultObjectMapper;
+import io.druid.segment.ColumnSelectorFactory;
+import io.druid.segment.column.ValueType;
+import org.easymock.EasyMock;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+
+/**
+ */
+public class VarianceAggregatorTest
+{
+  private VarianceAggregatorFactory aggFactory;
+  private ColumnSelectorFactory colSelectorFactory;
+  private TestFloatColumnSelector selector;
+
+  private final float[] values = {1.1f, 2.7f, 3.5f, 1.3f};
+  private final double[] variances = new double[values.length]; // calculated
+
+  public VarianceAggregatorTest() throws Exception
+  {
+    String aggSpecJson = "{\"type\": \"variance\", \"name\": \"billy\", \"fieldName\": \"nilly\"}";
+    aggFactory = new DefaultObjectMapper().readValue(aggSpecJson, VarianceAggregatorFactory.class);
+    double sum = 0;
+    for (int i = 0; i < values.length; i++) {
+      sum += values[i];
+      if (i > 0) {
+        double mean = sum / (i + 1);
+        double temp = 0;
+        for (int j = 0; j <= i; j++) {
+          temp += Math.pow(values[j] - mean, 2);
+        }
+        variances[i] = temp / (i + 1);
+      }
+    }
+  }
+
+  @Before
+  public void setup()
+  {
+    selector = new TestFloatColumnSelector(values);
+    colSelectorFactory = EasyMock.createMock(ColumnSelectorFactory.class);
+    EasyMock.expect(colSelectorFactory.columnType("nilly")).andReturn(ValueType.FLOAT);
+    EasyMock.expect(colSelectorFactory.makeFloatColumnSelector("nilly")).andReturn(selector);
+    EasyMock.replay(colSelectorFactory);
+  }
+
+  @Test
+  public void testDoubleVarianceAggregator()
+  {
+    VarianceAggregator agg = (VarianceAggregator) aggFactory.factorize(colSelectorFactory);
+
+    Assert.assertEquals("billy", agg.getName());
+
+    assertValues((VarianceHolder) agg.get(), 0, 0d, 0d);
+    aggregate(selector, agg);
+    assertValues((VarianceHolder) agg.get(), 1, 1.1d, 0d);
+    aggregate(selector, agg);
+    assertValues((VarianceHolder) agg.get(), 2, 3.8d, 1.28d);
+    aggregate(selector, agg);
+    assertValues((VarianceHolder) agg.get(), 3, 7.3d, 2.9866d);
+    aggregate(selector, agg);
+    assertValues((VarianceHolder) agg.get(), 4, 8.6d, 3.95d);
+
+    agg.reset();
+    assertValues((VarianceHolder) agg.get(), 0, 0d, 0d);
+  }
+
+  private void assertValues(VarianceHolder holder, long count, double sum, double nvariance)
+  {
+    Assert.assertEquals(count, holder.count);
+    Assert.assertEquals(sum, holder.sum, 0.0001);
+    Assert.assertEquals(nvariance, holder.nvariance, 0.0001);
+    if (count == 0) {
+      Assert.assertNull(holder.getVariance());
+    } else {
+      Assert.assertEquals(holder.getVariance(), variances[(int) count - 1], 0.0001);
+    }
+  }
+
+  @Test
+  public void testDoubleVarianceBufferAggregator()
+  {
+    VarianceBufferAggregator agg = (VarianceBufferAggregator) aggFactory.factorizeBuffered(
+        colSelectorFactory
+    );
+
+    ByteBuffer buffer = ByteBuffer.wrap(new byte[aggFactory.getMaxIntermediateSize()]);
+    agg.init(buffer, 0);
+
+    assertValues((VarianceHolder) agg.get(buffer, 0), 0, 0d, 0d);
+    aggregate(selector, agg, buffer, 0);
+    assertValues((VarianceHolder) agg.get(buffer, 0), 1, 1.1d, 0d);
+    aggregate(selector, agg, buffer, 0);
+    assertValues((VarianceHolder) agg.get(buffer, 0), 2, 3.8d, 1.28d);
+    aggregate(selector, agg, buffer, 0);
+    assertValues((VarianceHolder) agg.get(buffer, 0), 3, 7.3d, 2.9866d);
+    aggregate(selector, agg, buffer, 0);
+    assertValues((VarianceHolder) agg.get(buffer, 0), 4, 8.6d, 3.95d);
+  }
+
+  @Test
+  public void testCombine()
+  {
+    VarianceHolder holder1 = new VarianceHolder().add(1.1f).add(2.7f);
+    VarianceHolder holder2 = new VarianceHolder().add(3.5f).add(1.3f);
+    VarianceHolder expected = new VarianceHolder(4, 8.6d, 3.95d);
+    Assert.assertTrue(expected.equalsWithEpsilon((VarianceHolder) aggFactory.combine(holder1, holder2), 0.00001));
+  }
+
+  @Test
+  public void testEqualsAndHashCode() throws Exception
+  {
+    VarianceAggregatorFactory one = new VarianceAggregatorFactory("name1", "fieldName1");
+    VarianceAggregatorFactory oneMore = new VarianceAggregatorFactory("name1", "fieldName1");
+    VarianceAggregatorFactory two = new VarianceAggregatorFactory("name2", "fieldName2");
+
+    Assert.assertEquals(one.hashCode(), oneMore.hashCode());
+
+    Assert.assertTrue(one.equals(oneMore));
+    Assert.assertFalse(one.equals(two));
+  }
+
+  private void aggregate(TestFloatColumnSelector selector, VarianceAggregator agg)
+  {
+    agg.aggregate();
+    selector.increment();
+  }
+
+  private void aggregate(
+      TestFloatColumnSelector selector,
+      VarianceBufferAggregator agg,
+      ByteBuffer buff,
+      int position
+  )
+  {
+    agg.aggregate(buff, position);
+    selector.increment();
+  }
+}

apache/druid

extensions-core/stats/src/main/java/io/druid/query/aggregation/variance/VarianceAggregator.java
@@ -1,0 +1,125 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.druid.query.aggregation.variance;
+
+import io.druid.query.aggregation.Aggregator;
+import io.druid.segment.FloatColumnSelector;
+import io.druid.segment.LongColumnSelector;
+import io.druid.segment.ObjectColumnSelector;
+
+/**
+ */
+public abstract class VarianceAggregator implements Aggregator
+{
+  protected final String name;
+
+  protected final VarianceAggregatorCollector holder = new VarianceAggregatorCollector();
+
+  public VarianceAggregator(String name)
+  {
+    this.name = name;
+  }
+
+  @Override
+  public void reset()
+  {
+    holder.reset();
+  }
+
+  @Override
+  public Object get()
+  {
+    return holder;
+  }
+
+  @Override
+  public String getName()
+  {
+    return name;
+  }
+
+  @Override
+  public void close()
+  {
+  }
+
+  @Override
+  public float getFloat()
+  {
+    throw new UnsupportedOperationException("VarianceAggregator does not support getFloat()");
+  }
+
+  @Override
+  public long getLong()
+  {
+    throw new UnsupportedOperationException("VarianceAggregator does not support getLong()");
+  }
+
+  public static final class FloatVarianceAggregator extends VarianceAggregator
+  {
+    private final FloatColumnSelector selector;
+
+    public FloatVarianceAggregator(String name, FloatColumnSelector selector)
+    {
+      super(name);
+      this.selector = selector;
+    }
+
+    @Override
+    public void aggregate()
+    {
+      holder.add(selector.get());
+    }
+  }
+
+  public static final class LongVarianceAggregator extends VarianceAggregator
+  {
+    private final LongColumnSelector selector;
+
+    public LongVarianceAggregator(String name, LongColumnSelector selector)
+    {
+      super(name);
+      this.selector = selector;
+    }
+
+    @Override
+    public void aggregate()
+    {
+      holder.add(selector.get());
+    }
+  }
+
+  public static final class ObjectVarianceAggregator extends VarianceAggregator
+  {
+    private final ObjectColumnSelector selector;
+
+    public ObjectVarianceAggregator(String name, ObjectColumnSelector selector)
+    {
+      super(name);
+      this.selector = selector;
+    }
+
+    @Override
+    public void aggregate()
+    {
+      VarianceAggregatorCollector.combineValues(holder, selector.get());
+    }
+  }
+}

extensions-core/stats/src/test/java/io/druid/query/aggregation/variance/VarianceAggregatorTest.java
@@ -1,0 +1,173 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.druid.query.aggregation.variance;
+
+import io.druid.jackson.DefaultObjectMapper;
+import io.druid.query.aggregation.TestFloatColumnSelector;
+import io.druid.query.aggregation.TestObjectColumnSelector;
+import io.druid.segment.ColumnSelectorFactory;
+import org.easymock.EasyMock;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+
+/**
+ */
+public class VarianceAggregatorTest
+{
+  private VarianceAggregatorFactory aggFactory;
+  private ColumnSelectorFactory colSelectorFactory;
+  private TestFloatColumnSelector selector;
+
+  private final float[] values = {1.1f, 2.7f, 3.5f, 1.3f};
+  private final double[] variances_pop = new double[values.length]; // calculated
+  private final double[] variances_samp = new double[values.length]; // calculated
+
+  public VarianceAggregatorTest() throws Exception
+  {
+    String aggSpecJson = "{\"type\": \"variance\", \"name\": \"billy\", \"fieldName\": \"nilly\"}";
+    aggFactory = new DefaultObjectMapper().readValue(aggSpecJson, VarianceAggregatorFactory.class);
+    double sum = 0;
+    for (int i = 0; i < values.length; i++) {
+      sum += values[i];
+      if (i > 0) {
+        double mean = sum / (i + 1);
+        double temp = 0;
+        for (int j = 0; j <= i; j++) {
+          temp += Math.pow(values[j] - mean, 2);
+        }
+        variances_pop[i] = temp / (i + 1);
+        variances_samp[i] = temp / i;
+      }
+    }
+  }
+
+  @Before
+  public void setup()
+  {
+    selector = new TestFloatColumnSelector(values);
+    colSelectorFactory = EasyMock.createMock(ColumnSelectorFactory.class);
+    EasyMock.expect(colSelectorFactory.makeObjectColumnSelector("nilly")).andReturn(new TestObjectColumnSelector(0.0f));
+    EasyMock.expect(colSelectorFactory.makeFloatColumnSelector("nilly")).andReturn(selector);
+    EasyMock.replay(colSelectorFactory);
+  }
+
+  @Test
+  public void testDoubleVarianceAggregator()
+  {
+    VarianceAggregator agg = (VarianceAggregator) aggFactory.factorize(colSelectorFactory);
+
+    Assert.assertEquals("billy", agg.getName());
+
+    assertValues((VarianceAggregatorCollector) agg.get(), 0, 0d, 0d);
+    aggregate(selector, agg);
+    assertValues((VarianceAggregatorCollector) agg.get(), 1, 1.1d, 0d);
+    aggregate(selector, agg);
+    assertValues((VarianceAggregatorCollector) agg.get(), 2, 3.8d, 1.28d);
+    aggregate(selector, agg);
+    assertValues((VarianceAggregatorCollector) agg.get(), 3, 7.3d, 2.9866d);
+    aggregate(selector, agg);
+    assertValues((VarianceAggregatorCollector) agg.get(), 4, 8.6d, 3.95d);
+
+    agg.reset();
+    assertValues((VarianceAggregatorCollector) agg.get(), 0, 0d, 0d);
+  }
+
+  private void assertValues(VarianceAggregatorCollector holder, long count, double sum, double nvariance)
+  {
+    Assert.assertEquals(count, holder.count);
+    Assert.assertEquals(sum, holder.sum, 0.0001);
+    Assert.assertEquals(nvariance, holder.nvariance, 0.0001);
+    if (count == 0) {
+      try {
+        holder.getVariance(false);
+        Assert.fail("Should throw ISE");
+      }
+      catch (IllegalStateException e) {
+        Assert.assertTrue(e.getMessage().contains("should not be empty holder"));
+      }
+    } else {
+      Assert.assertEquals(holder.getVariance(true), variances_pop[(int) count - 1], 0.0001);
+      Assert.assertEquals(holder.getVariance(false), variances_samp[(int) count - 1], 0.0001);
+    }
+  }
+
+  @Test
+  public void testDoubleVarianceBufferAggregator()
+  {
+    VarianceBufferAggregator agg = (VarianceBufferAggregator) aggFactory.factorizeBuffered(
+        colSelectorFactory
+    );
+
+    ByteBuffer buffer = ByteBuffer.wrap(new byte[aggFactory.getMaxIntermediateSize()]);
+    agg.init(buffer, 0);
+
+    assertValues((VarianceAggregatorCollector) agg.get(buffer, 0), 0, 0d, 0d);
+    aggregate(selector, agg, buffer, 0);
+    assertValues((VarianceAggregatorCollector) agg.get(buffer, 0), 1, 1.1d, 0d);
+    aggregate(selector, agg, buffer, 0);
+    assertValues((VarianceAggregatorCollector) agg.get(buffer, 0), 2, 3.8d, 1.28d);
+    aggregate(selector, agg, buffer, 0);
+    assertValues((VarianceAggregatorCollector) agg.get(buffer, 0), 3, 7.3d, 2.9866d);
+    aggregate(selector, agg, buffer, 0);
+    assertValues((VarianceAggregatorCollector) agg.get(buffer, 0), 4, 8.6d, 3.95d);
+  }
+
+  @Test
+  public void testCombine()
+  {
+    VarianceAggregatorCollector holder1 = new VarianceAggregatorCollector().add(1.1f).add(2.7f);
+    VarianceAggregatorCollector holder2 = new VarianceAggregatorCollector().add(3.5f).add(1.3f);
+    VarianceAggregatorCollector expected = new VarianceAggregatorCollector(4, 8.6d, 3.95d);
+    Assert.assertTrue(expected.equalsWithEpsilon((VarianceAggregatorCollector) aggFactory.combine(holder1, holder2), 0.00001));
+  }
+
+  @Test
+  public void testEqualsAndHashCode() throws Exception
+  {
+    VarianceAggregatorFactory one = new VarianceAggregatorFactory("name1", "fieldName1");
+    VarianceAggregatorFactory oneMore = new VarianceAggregatorFactory("name1", "fieldName1");
+    VarianceAggregatorFactory two = new VarianceAggregatorFactory("name2", "fieldName2");
+
+    Assert.assertEquals(one.hashCode(), oneMore.hashCode());
+
+    Assert.assertTrue(one.equals(oneMore));
+    Assert.assertFalse(one.equals(two));
+  }
+
+  private void aggregate(TestFloatColumnSelector selector, VarianceAggregator agg)
+  {
+    agg.aggregate();
+    selector.increment();
+  }
+
+  private void aggregate(
+      TestFloatColumnSelector selector,
+      VarianceBufferAggregator agg,
+      ByteBuffer buff,
+      int position
+  )
+  {
+    agg.aggregate(buff, position);
+    selector.increment();
+  }
+}