ad4b02f9b9a29fd048b315ade4e4c495a20b277c
Charles Allen <charles@allen-net.com>
2016-07-29 15:15:41 -0700

    Add `slf4j` requst logger (#3146)
    * Add `slf4j` requst logger
    
    * Address comments
    
    * Fix conflicts with master
    
    * Fix removed map value

---
+++ server/src/main/java/io/druid/server/log/LoggingRequestLogger.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.druid.server.log;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import com.metamx.common.logger.Logger;
+import io.druid.query.Query;
+import io.druid.server.RequestLogLine;
+import org.slf4j.MDC;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class LoggingRequestLogger implements RequestLogger
+{
+  private static final Logger LOG = new Logger(LoggingRequestLogger.class);
+
+  private final ObjectMapper mapper;
+  private final boolean setMDC;
+  private final boolean setContextMDC;
+
+  public LoggingRequestLogger(
+      ObjectMapper mapper,
+      boolean setMDC,
+      boolean setContextMDC
+  )
+  {
+    this.mapper = mapper;
+    this.setMDC = setMDC;
+    this.setContextMDC = setContextMDC;
+  }
+
+  @Override
+  public void log(RequestLogLine requestLogLine) throws IOException
+  {
+    final Map mdc = MDC.getCopyOfContextMap();
+    // MDC must be set during the `LOG.info` call at the end of the try block.
+    try {
+      if (setMDC) {
+        try {
+          final Query query = requestLogLine.getQuery();
+          MDC.put("queryId", query.getId());
+          MDC.put("dataSource", query.getDataSource().toString());
+          MDC.put("queryType", query.getType());
+          MDC.put("hasFilters", Boolean.toString(query.hasFilters()));
+          MDC.put("remoteAddr", requestLogLine.getRemoteAddr());
+          MDC.put("duration", query.getDuration().toString());
+          MDC.put("descending", Boolean.toString(query.isDescending()));
+          if (setContextMDC) {
+            final Iterable<Map.Entry<String, Object>> entries = query.getContext() == null
+                                                                ? ImmutableList.<Map.Entry<String, Object>>of()
+                                                                : query.getContext().entrySet();
+            for (Map.Entry<String, Object> entry : entries) {
+              MDC.put(entry.getKey(), entry.getValue() == null ? "NULL" : entry.getValue().toString());
+            }
+          }
+        }
+        catch (RuntimeException re) {
+          LOG.error(re, "Error preparing MDC");
+        }
+      }
+      final String line = requestLogLine.getLine(mapper);
+
+      // MDC must be set here
+      LOG.info("%s", line);
+    }
+    finally {
+      if (setMDC) {
+        if (mdc != null) {
+          MDC.setContextMap(mdc);
+        } else {
+          MDC.clear();
+        }
+      }
+    }
+  }
+
+  public boolean isSetMDC()
+  {
+    return setMDC;
+  }
+
+  public boolean isSetContextMDC()
+  {
+    return setContextMDC;
+  }
+}
---
+++ server/src/test/java/io/druid/server/log/LoggingRequestLoggerProviderTest.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.druid.server.log;
+
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Binder;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.Module;
+import com.google.inject.name.Names;
+import io.druid.guice.GuiceInjectors;
+import io.druid.guice.JsonConfigProvider;
+import io.druid.guice.JsonConfigurator;
+import io.druid.initialization.Initialization;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Properties;
+import java.util.UUID;
+
+public class LoggingRequestLoggerProviderTest
+{
+  private final String propertyPrefix = UUID.randomUUID().toString().replace('-', '_');
+  private final JsonConfigProvider<RequestLoggerProvider> provider = JsonConfigProvider.of(
+      propertyPrefix,
+      RequestLoggerProvider.class
+  );
+  private final Injector injector = makeInjector();
+
+  @Test
+  public void testDefaultConfigParsing()
+  {
+    final Properties properties = new Properties();
+    properties.put(propertyPrefix + ".type", "slf4j");
+    provider.inject(properties, injector.getInstance(JsonConfigurator.class));
+    final LoggingRequestLogger requestLogger = (LoggingRequestLogger) provider.get().get().get();
+    Assert.assertFalse(requestLogger.isSetContextMDC());
+    Assert.assertFalse(requestLogger.isSetMDC());
+  }
+
+  @Test
+  public void testConfigParsingFull()
+  {
+    final Properties properties = new Properties();
+    properties.put(propertyPrefix + ".type", "slf4j");
+    properties.put(propertyPrefix + ".setMDC", "true");
+    properties.put(propertyPrefix + ".setContextMDC", "true");
+    provider.inject(properties, injector.getInstance(JsonConfigurator.class));
+    final LoggingRequestLogger requestLogger = (LoggingRequestLogger) provider.get().get().get();
+    Assert.assertTrue(requestLogger.isSetContextMDC());
+    Assert.assertTrue(requestLogger.isSetMDC());
+  }
+
+  private Injector makeInjector()
+  {
+    return Initialization.makeInjectorWithModules(
+        GuiceInjectors.makeStartupInjector(),
+        ImmutableList.<Module>of(
+            new Module()
+            {
+              @Override
+              public void configure(Binder binder)
+              {
+                binder.bind(Key.get(String.class, Names.named("serviceName"))).toInstance("some service");
+                binder.bind(Key.get(Integer.class, Names.named("servicePort"))).toInstance(0);
+                JsonConfigProvider.bind(binder, propertyPrefix, RequestLoggerProvider.class);
+              }
+            }
+        )
+    );
+  }
+}
