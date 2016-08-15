f7740aa73f4c96b31159243922a5a5ec3414ea1f
rajk-tetration <rajkesav@tetrationanalytics.com>
2016-08-15 10:25:24 -0700

    Adding filters for TimeBoundary on backend (#3168)
    * Adding filters for TimeBoundary on backend
    
    Signed-off-by: Balachandar Kesavan <raj.ksvn@gmail.com>
    
    * updating TimeBoundaryQuery constructor in QueryHostFinderTest
    
    * add filter helpers
    
    * update filterSegments + test
    
    * Conditional filterSegment depending on whether a filter exists
    
    * Style changes
    
    * Trigger rebuild
    
    * Adding documentation for timeboundaryquery filtering
    
    * added filter serialization to timeboundaryquery cache
    
    * code style changes

--- processing/src/main/java/io/druid/query/timeboundary/TimeBoundaryQuery.java
+++ processing/src/main/java/io/druid/query/timeboundary/TimeBoundaryQuery.java
@@ -32,6 +32,7 @@ import io.druid.query.Result;
 import io.druid.query.filter.DimFilter;
 import io.druid.query.spec.MultipleIntervalSegmentSpec;
 import io.druid.query.spec.QuerySegmentSpec;
+import io.druid.query.filter.DimFilter;
 import org.joda.time.DateTime;
 import org.joda.time.Interval;
 
@@ -53,6 +54,7 @@ public class TimeBoundaryQuery extends BaseQuery<Result<TimeBoundaryResultValue>
 
   private static final byte CACHE_TYPE_ID = 0x0;
 
+  private final DimFilter dimFilter;
   private final String bound;
 
   @JsonCreator
@@ -60,6 +62,7 @@ public class TimeBoundaryQuery extends BaseQuery<Result<TimeBoundaryResultValue>
       @JsonProperty("dataSource") DataSource dataSource,
       @JsonProperty("intervals") QuerySegmentSpec querySegmentSpec,
       @JsonProperty("bound") String bound,
+      @JsonProperty("filter") DimFilter dimFilter,
       @JsonProperty("context") Map<String, Object> context
   )
   {
@@ -71,13 +74,13 @@ public class TimeBoundaryQuery extends BaseQuery<Result<TimeBoundaryResultValue>
         context
     );
 
+    this.dimFilter = dimFilter;
     this.bound = bound == null ? "" : bound;
   }
 
   @Override
-  public boolean hasFilters()
-  {
-    return false;
+  public boolean hasFilters() {
+    return dimFilter != null;
   }
 
   @Override
@@ -90,6 +93,12 @@ public class TimeBoundaryQuery extends BaseQuery<Result<TimeBoundaryResultValue>
   public String getType()
   {
     return Query.TIME_BOUNDARY;
+  }
+
+  @JsonProperty("filter")
+  public DimFilter getDimensionsFilter()
+  {
+    return dimFilter;
   }
 
   @JsonProperty
@@ -105,6 +114,7 @@ public class TimeBoundaryQuery extends BaseQuery<Result<TimeBoundaryResultValue>
         getDataSource(),
         getQuerySegmentSpec(),
         bound,
+        dimFilter,
         computeOverridenContext(contextOverrides)
     );
   }
@@ -116,6 +126,7 @@ public class TimeBoundaryQuery extends BaseQuery<Result<TimeBoundaryResultValue>
         getDataSource(),
         spec,
         bound,
+        dimFilter,
         getContext()
     );
   }
@@ -127,16 +138,21 @@ public class TimeBoundaryQuery extends BaseQuery<Result<TimeBoundaryResultValue>
         dataSource,
         getQuerySegmentSpec(),
         bound,
+        dimFilter,
         getContext()
     );
   }
 
   public byte[] getCacheKey()
   {
+    final byte[] filterBytes = dimFilter == null ? new byte[]{} : dimFilter.getCacheKey();
     final byte[] boundBytes = StringUtils.toUtf8(bound);
-    return ByteBuffer.allocate(1 + boundBytes.length)
+    final byte delimiter = (byte) 0xff;
+    return ByteBuffer.allocate(2 + boundBytes.length + filterBytes.length)
                      .put(CACHE_TYPE_ID)
                      .put(boundBytes)
+                     .put(delimiter)
+                     .put(filterBytes)
                      .array();
   }
 
@@ -218,6 +234,7 @@ public class TimeBoundaryQuery extends BaseQuery<Result<TimeBoundaryResultValue>
            ", querySegmentSpec=" + getQuerySegmentSpec() +
            ", duration=" + getDuration() +
            ", bound=" + bound +
+           ", dimFilter=" + dimFilter +
            '}';
   }
 
@@ -240,6 +257,10 @@ public class TimeBoundaryQuery extends BaseQuery<Result<TimeBoundaryResultValue>
       return false;
     }
 
+    if (dimFilter != null ? !dimFilter.equals(that.dimFilter) : that.dimFilter != null) {
+      return false;
+    }
+
     return true;
   }
 
@@ -248,6 +269,7 @@ public class TimeBoundaryQuery extends BaseQuery<Result<TimeBoundaryResultValue>
   {
     int result = super.hashCode();
     result = 31 * result + bound.hashCode();
+    result = 31 * result + (dimFilter != null ? dimFilter.hashCode() : 0);
     return result;
   }
 }
