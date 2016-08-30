433c09b8b431763e6d4abc3a70280d539d6c1f90
Slim Bouguerra <slim.bouguerra@gmail.com>
2016-08-30 15:21:33 -0700

    adding kerberos authentication

--- indexing-hadoop/src/main/java/io/druid/indexer/JobHelper.java
+++ indexing-hadoop/src/main/java/io/druid/indexer/JobHelper.java
@@ -21,9 +21,9 @@ package io.druid.indexer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.base.Predicate;
+import com.google.common.base.Strings;
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Sets;
 import com.google.common.io.ByteStreams;
 import com.google.common.io.Files;
 import com.google.common.io.OutputSupplier;
@@ -47,6 +47,7 @@ import org.apache.hadoop.io.retry.RetryProxy;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.TaskAttemptID;
+import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.util.Progressable;
 import org.joda.time.DateTime;
 
@@ -61,7 +62,6 @@ import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
@@ -92,6 +92,33 @@ public class JobHelper
     return new Path(base, "classpath");
   }
 
+  /**
+   * Dose authenticate against a secured hadoop cluster
+   * In case of any bug fix make sure to fix the code at HdfsStorageAuthentication#authenticate as well.
+   * @param config containing the principal name and keytab path.
+   */
+  public static void authenticate(HadoopDruidIndexerConfig config)
+  {
+    String principal = config.HADOOP_KERBEROS_CONFIG.getPrincipal();
+    String keytab = config.HADOOP_KERBEROS_CONFIG.getKeytab();
+    if (Strings.isNullOrEmpty(principal) || Strings.isNullOrEmpty(keytab)) {
+      return;
+    }
+    Configuration conf = new Configuration();
+    UserGroupInformation.setConfiguration(conf);
+    if (UserGroupInformation.isSecurityEnabled()) {
+      try {
+        if (UserGroupInformation.getCurrentUser().hasKerberosCredentials() == false
+            || !UserGroupInformation.getCurrentUser().getUserName().equals(principal)) {
+          log.info("trying to authenticate user [%s] with keytab [%s]", principal, keytab);
+          UserGroupInformation.loginUserFromKeytab(principal, keytab);
+        }
+      }
+      catch (IOException e) {
+        throw new ISE(e, "Failed to authenticate user principal [%s] with keytab [%s]", principal, keytab);
+      }
+    }
+  }
   /**
    * Uploads jar files to hdfs and configures the classpath.
    * Snapshot jar files are uploaded to intermediateClasspath and not shared across multiple jobs.
@@ -294,6 +321,7 @@ public class JobHelper
 
   public static void ensurePaths(HadoopDruidIndexerConfig config)
   {
+    authenticate(config);
     // config.addInputPaths() can have side-effects ( boo! :( ), so this stuff needs to be done before anything else
     try {
       Job job = Job.getInstance(
