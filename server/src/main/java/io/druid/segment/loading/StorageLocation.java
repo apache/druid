a5739f277ffdba99f7bf61d59821152c269bc52e
Keuntae Park <sirpkt@apache.org>
2016-09-21 17:10:44 +0900

    remove currSize caclucation from StorageLocation because it would be done by ZkCoordinator

--- server/src/main/java/io/druid/segment/loading/StorageLocation.java
+++ server/src/main/java/io/druid/segment/loading/StorageLocation.java
@@ -21,18 +21,10 @@ package io.druid.segment.loading;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.Sets;
-import com.metamx.common.ISE;
 import io.druid.timeline.DataSegment;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
 */
@@ -47,14 +39,12 @@ public class StorageLocation
   @JsonProperty
   private long currSize;
   private final Set<DataSegment> segments;
-  private static final long BLOCK_SIZE_SHIFT = 12;                       // 1 << 12 - 4KB
-  private static final long BLOCK_SIZE_MASK = (1 << BLOCK_SIZE_SHIFT) - 1; // 0x7FFF
 
 
   StorageLocation(File path, long maxSize)
   {
     this.path = path;
-    this.currSize = getInitSize();
+    this.currSize = 0;
     this.presetMaxSize = maxSize;
     updateMaxSize();
 
@@ -94,60 +84,14 @@ public class StorageLocation
 
   synchronized long available()
   {
+    updateMaxSize();
+    
     return maxSize - currSize;
   }
 
   StorageLocation mostEmpty(StorageLocation other)
   {
     return available() > other.available() ? this : other;
-  }
-
-  private long getOccupySize(long size)
-  {
-    if ((size & BLOCK_SIZE_MASK) == 0) {
-      return size;
-    }
-    return (((size >> BLOCK_SIZE_SHIFT) + 1) << BLOCK_SIZE_SHIFT);
-  }
-
-  private long getInitSize()
-  {
-    final AtomicLong size = new AtomicLong(0);
-
-    try
-    {
-      Files.walkFileTree (
-          path.toPath(),
-          new SimpleFileVisitor<Path>()
-          {
-            @Override public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-
-              size.addAndGet (getOccupySize(attrs.size()));
-              return FileVisitResult.CONTINUE;
-            }
-
-            @Override public FileVisitResult
-            visitFileFailed(Path file, IOException exc) {
-
-              // Skip files that can't be visited
-              return FileVisitResult.CONTINUE;
-            }
-
-            @Override public FileVisitResult
-            postVisitDirectory (Path dir, IOException exc) {
-
-              // Ignore errors traversing a folder
-              return FileVisitResult.CONTINUE;
-            }
-          }
-          );
-    }
-    catch (IOException e)
-    {
-      throw new ISE(e.getMessage());
-    }
-
-    return size.get();
   }
 
   private void updateMaxSize()
