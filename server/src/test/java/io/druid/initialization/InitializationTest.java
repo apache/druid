6a6aa24b4e027a131e4a28c6bc58b41e38fdfd0e
Himanshu <g.himanshu@gmail.com>
2016-12-06 19:40:23 -0600

    allow and load extensions with absolute paths in druid.extensions.loadList (#3747)

--- server/src/test/java/io/druid/initialization/InitializationTest.java
+++ server/src/test/java/io/druid/initialization/InitializationTest.java
@@ -277,12 +277,15 @@ public class InitializationTest
   public void testGetExtensionFilesToLoad_with_load_list() throws IOException
   {
     final File extensionsDir = temporaryFolder.newFolder();
+
+    final File absolutePathExtension = temporaryFolder.newFolder();
+
     final ExtensionsConfig config = new ExtensionsConfig()
     {
       @Override
       public List<String> getLoadList()
       {
-        return Arrays.asList("mysql-metadata-storage", "druid-kafka-eight");
+        return Arrays.asList("mysql-metadata-storage", "druid-kafka-eight", absolutePathExtension.getAbsolutePath());
       }
 
       @Override
@@ -294,13 +297,13 @@ public class InitializationTest
     final File mysql_metadata_storage = new File(extensionsDir, "mysql-metadata-storage");
     final File druid_kafka_eight = new File(extensionsDir, "druid-kafka-eight");
     final File random_extension = new File(extensionsDir, "random-extensions");
+
     mysql_metadata_storage.mkdir();
     druid_kafka_eight.mkdir();
     random_extension.mkdir();
 
-    final File[] expectedFileList = new File[]{druid_kafka_eight, mysql_metadata_storage};
+    final File[] expectedFileList = new File[]{mysql_metadata_storage, druid_kafka_eight, absolutePathExtension};
     final File[] actualFileList = Initialization.getExtensionFilesToLoad(config);
-    Arrays.sort(actualFileList);
     Assert.assertArrayEquals(expectedFileList, actualFileList);
   }
 
