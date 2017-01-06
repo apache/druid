05fbbab0517e9a2c7c3262a324daa3a5b77f2b74
Niketh Sabbineni <niketh.sabbineni@gmail.com>
2017-01-05 16:44:17 -0800

    Use one pass instead of two

--- server/src/main/java/io/druid/server/coordinator/CostBalancerStrategy.java
+++ server/src/main/java/io/druid/server/coordinator/CostBalancerStrategy.java
@@ -350,20 +350,19 @@ public class CostBalancerStrategy implements BalancerStrategy
 
     final ListenableFuture<List<Pair<Double, ServerHolder>>> resultsFuture = Futures.allAsList(futures);
     final List<Pair<Double, ServerHolder>> bestServers = new ArrayList<>();
+    bestServers.add(bestServer);
     try {
       for (Pair<Double, ServerHolder> server : resultsFuture.get()) {
-        if (server.lhs < bestServer.lhs) {
-          bestServer = server;
-        }
-      }
-
-      // Find All best servers and choose one randomly
-      for (Pair<Double, ServerHolder> server : resultsFuture.get()) {
-        if (server.lhs == bestServer.lhs) {
+        if (server.lhs <= bestServers.get(0).lhs) {
+          if(server.lhs < bestServers.get(0).lhs){
+            bestServers.clear();
+          }
           bestServers.add(server);
         }
       }
-      bestServer = (bestServers.size() > 0) ? bestServers.get((int)Math.random()*bestServers.size()): bestServer;
+
+      // Randomly choose a server from the best servers
+      bestServer = bestServers.get((int)Math.random()*bestServers.size());
     }
     catch (Exception e) {
       log.makeAlert(e, "Cost Balancer Multithread strategy wasn't able to complete cost computation.").emit();
