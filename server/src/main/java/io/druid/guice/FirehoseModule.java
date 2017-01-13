2005b6bb3ff9e129d8ba3b69b0619b33873c550a
Himanshu Gupta <g.himanshu@gmail.com>
2017-01-13 11:00:43 -0600

    make ITKafkaTest less non-deterministic

--- server/src/main/java/io/druid/guice/FirehoseModule.java
+++ server/src/main/java/io/druid/guice/FirehoseModule.java
@@ -27,6 +27,7 @@ import io.druid.initialization.DruidModule;
 import io.druid.segment.realtime.firehose.ClippedFirehoseFactory;
 import io.druid.segment.realtime.firehose.CombiningFirehoseFactory;
 import io.druid.segment.realtime.firehose.EventReceiverFirehoseFactory;
+import io.druid.segment.realtime.firehose.FixedCountFirehoseFactory;
 import io.druid.segment.realtime.firehose.IrcFirehoseFactory;
 import io.druid.segment.realtime.firehose.LocalFirehoseFactory;
 import io.druid.segment.realtime.firehose.TimedShutoffFirehoseFactory;
@@ -54,7 +55,8 @@ public class FirehoseModule implements DruidModule
                 new NamedType(IrcFirehoseFactory.class, "irc"),
                 new NamedType(LocalFirehoseFactory.class, "local"),
                 new NamedType(EventReceiverFirehoseFactory.class, "receiver"),
-                new NamedType(CombiningFirehoseFactory.class, "combining")
+                new NamedType(CombiningFirehoseFactory.class, "combining"),
+                new NamedType(FixedCountFirehoseFactory.class, "fixedCount")
             )
     );
   }
