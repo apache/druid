7b3d68d083f89cf869c91efd8c0fa527ddcbbe79
Slim <b-slim@users.noreply.github.com>
2017-01-17 11:59:12 -0800

    renaming classes to be run by mvn and comment non operational tests (#3847)

--- processing/src/test/java/io/druid/segment/AppendTest.java
+++ processing/src/test/java/io/druid/segment/AppendTest.java
@@ -113,7 +113,7 @@ public class AppendTest
     // (1, 2) cover overlapping segments of the form
     // |------|
     //     |--------|
-    QueryableIndex appendedIndex = SchemalessIndex.getAppendedIncrementalIndex(
+    QueryableIndex appendedIndex = SchemalessIndexTest.getAppendedIncrementalIndex(
         Arrays.asList(
             new Pair<String, AggregatorFactory[]>("append.json.1", METRIC_AGGS_NO_UNIQ),
             new Pair<String, AggregatorFactory[]>("append.json.2", METRIC_AGGS)
@@ -128,7 +128,7 @@ public class AppendTest
     // (3, 4) cover overlapping segments of the form
     // |------------|
     //     |-----|
-    QueryableIndex append2 = SchemalessIndex.getAppendedIncrementalIndex(
+    QueryableIndex append2 = SchemalessIndexTest.getAppendedIncrementalIndex(
         Arrays.asList(
             new Pair<String, AggregatorFactory[]>("append.json.3", METRIC_AGGS_NO_UNIQ),
             new Pair<String, AggregatorFactory[]>("append.json.4", METRIC_AGGS)
@@ -144,7 +144,7 @@ public class AppendTest
     // |-------------|
     //   |---|
     //          |---|
-    QueryableIndex append3 = SchemalessIndex.getAppendedIncrementalIndex(
+    QueryableIndex append3 = SchemalessIndexTest.getAppendedIncrementalIndex(
         Arrays.asList(
             new Pair<String, AggregatorFactory[]>("append.json.5", METRIC_AGGS),
             new Pair<String, AggregatorFactory[]>("append.json.6", METRIC_AGGS),
