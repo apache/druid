1b9a6588ef2bfbc0835b79da8b481e772a50e8f9
leventov <leventov@ya.ru>
2017-02-06 18:25:40 -0600

    Make CalledFromHotLoop annotation Inherited

--- processing/src/main/java/io/druid/query/monomorphicprocessing/CalledFromHotLoop.java
+++ processing/src/main/java/io/druid/query/monomorphicprocessing/CalledFromHotLoop.java
@@ -21,6 +21,7 @@ package io.druid.query.monomorphicprocessing;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -34,6 +35,7 @@ import java.lang.annotation.Target;
  * @see HotLoopCallee
  */
 @Documented
+@Inherited
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.METHOD)
 public @interface CalledFromHotLoop
