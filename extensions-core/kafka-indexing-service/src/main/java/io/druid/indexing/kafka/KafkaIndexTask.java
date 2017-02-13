352fdb398dc215d1a3a7abba1e981728532887b4
Parag Jain <paragjain16@gmail.com>
2017-02-13 14:55:42 -0600

    add more data to alerts

--- extensions-core/kafka-indexing-service/src/main/java/io/druid/indexing/kafka/KafkaIndexTask.java
+++ extensions-core/kafka-indexing-service/src/main/java/io/druid/indexing/kafka/KafkaIndexTask.java
@@ -1057,10 +1057,9 @@ public class KafkaIndexTask extends AbstractTask implements ChatHandler
                                 ));
 
     if (result) {
-      log.makeAlert(
-          "Successfully sent the reset request for partitions [%s], waiting to be killed",
-          partitionOffsetMap.keySet()
-      ).emit();
+      log.makeAlert("Resetting Kafka offsets for datasource [%s]", getDataSource())
+         .addData("partitions", partitionOffsetMap.keySet())
+         .emit();
       // wait for being killed by supervisor
       try {
         Thread.sleep(Long.MAX_VALUE);
