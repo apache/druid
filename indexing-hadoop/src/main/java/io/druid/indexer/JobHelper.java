1a057fd89186a13f627d067949f4958c71b1cce0
Slim Bouguerra <slim.bouguerra@gmail.com>
2017-02-15 10:37:43 -0800

    Adding s3a schema and s3a implem to hdfs storage module.

--- indexing-hadoop/src/main/java/io/druid/indexer/JobHelper.java
+++ indexing-hadoop/src/main/java/io/druid/indexer/JobHelper.java
@@ -418,6 +418,7 @@ public class JobHelper
       case "hdfs":
       case "viewfs":
       case "maprfs":
+      case "s3a":
         loadSpec = ImmutableMap.<String, Object>of(
             "type", "hdfs",
             "path", indexOutURI.toString()
