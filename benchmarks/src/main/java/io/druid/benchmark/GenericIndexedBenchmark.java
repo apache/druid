ec8a1a6d5329348982f8f16897b75122e569df37
leventov <leventov@ya.ru>
2017-02-27 19:17:55 -0600

    Update GenericIndexedBenchmark

--- benchmarks/src/main/java/io/druid/benchmark/GenericIndexedBenchmark.java
+++ benchmarks/src/main/java/io/druid/benchmark/GenericIndexedBenchmark.java
@@ -19,41 +19,132 @@
 
 package io.druid.benchmark;
 
+import com.google.common.io.Files;
+import com.google.common.primitives.Ints;
+import io.druid.java.util.common.io.smoosh.FileSmoosher;
+import io.druid.java.util.common.io.smoosh.SmooshedFileMapper;
 import io.druid.segment.data.GenericIndexed;
+import io.druid.segment.data.GenericIndexedWriter;
+import io.druid.segment.data.ObjectStrategy;
+import io.druid.segment.data.TmpFileIOPeon;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OperationsPerInvocation;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-@OperationsPerInvocation(GenericIndexedBenchmark.N)
+@OperationsPerInvocation(GenericIndexedBenchmark.ITERATIONS)
 @Warmup(iterations = 5)
 @Measurement(iterations = 20)
 @Fork(1)
+@State(Scope.Benchmark)
 public class GenericIndexedBenchmark
 {
-  static final int N = 10000;
-  private static final GenericIndexed<String> genericIndexed;
-  static {
-    String[] strings = new String[N];
-    for (int i = 0; i < N; i++) {
-      strings[i] = String.valueOf(i);
+  public static final int ITERATIONS = 10000;
+
+  static final ObjectStrategy<byte[]> byteArrayStrategy = new ObjectStrategy<byte[]>()
+  {
+    @Override
+    public Class<? extends byte[]> getClazz()
+    {
+      return byte[].class;
     }
-    genericIndexed = GenericIndexed.fromArray(strings, GenericIndexed.STRING_STRATEGY);
+
+    @Override
+    public byte[] fromByteBuffer(ByteBuffer buffer, int numBytes)
+    {
+      byte[] result = new byte[numBytes];
+      buffer.get(result);
+      return result;
+    }
+
+    @Override
+    public byte[] toBytes(byte[] val)
+    {
+      return val;
+    }
+
+    @Override
+    public int compare(byte[] o1, byte[] o2)
+    {
+      return Ints.compare(Ints.fromByteArray(o1), Ints.fromByteArray(o2));
+    }
+  };
+
+
+  @Param({"10000"})
+  public int n;
+  @Param({"8"})
+  public int elementSize;
+
+  private File file;
+  private File smooshDir;
+  private GenericIndexed<byte[]> genericIndexed;
+  private int[] iterationIndexes;
+
+  @Setup(Level.Trial)
+  public void createGenericIndexed() throws IOException
+  {
+    GenericIndexedWriter<byte[]> genericIndexedWriter = new GenericIndexedWriter<>(
+        new TmpFileIOPeon(),
+        "genericIndexedBenchmark",
+        byteArrayStrategy
+    );
+    genericIndexedWriter.open();
+
+    final ByteBuffer element = ByteBuffer.allocate(elementSize);
+    for (int i = 0; i < n; i++) {
+      element.putInt(0, i);
+      genericIndexedWriter.write(element.array());
+    }
+    genericIndexedWriter.close();
+    smooshDir = Files.createTempDir();
+    file = File.createTempFile("genericIndexedBenchmark", "meta");
+
+    try (FileChannel fileChannel =
+             FileChannel.open(file.toPath(), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+         FileSmoosher fileSmoosher = new FileSmoosher(smooshDir)) {
+      genericIndexedWriter.writeToChannel(fileChannel, fileSmoosher);
+    }
+
+    FileChannel fileChannel = FileChannel.open(file.toPath());
+    MappedByteBuffer byteBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, file.length());
+    genericIndexed = GenericIndexed.read(byteBuffer, byteArrayStrategy, SmooshedFileMapper.load(smooshDir));
+  }
+
+  @Setup(Level.Trial)
+  public void createIterationIndexes()
+  {
+    iterationIndexes = new int[ITERATIONS];
+    for (int i = 0; i < ITERATIONS; i++) {
+      iterationIndexes[i] = ThreadLocalRandom.current().nextInt(n);
+    }
   }
 
   @Benchmark
   public void get(Blackhole bh)
   {
-    for (int i = 0; i < N; i++) {
+    for (int i : iterationIndexes) {
       bh.consume(genericIndexed.get(i));
     }
   }
