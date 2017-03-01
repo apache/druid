ce61bc9aaed024904f8cb3390f6301400f9a5558
Himanshu <g.himanshu@gmail.com>
2017-03-01 15:07:12 -0600

    fix TimeDimExtractionFn.apply() under concurrency (#3984)

--- processing/src/main/java/io/druid/query/extraction/ExtractionFn.java
+++ processing/src/main/java/io/druid/query/extraction/ExtractionFn.java
@@ -46,7 +46,8 @@ import io.druid.query.lookup.RegisteredLookupExtractionFn;
     @JsonSubTypes.Type(name = "strlen", value = StrlenExtractionFn.class)
 })
 /**
- * An ExtractionFn is a function that can be used to transform the values of a column (typically a dimension)
+ * An ExtractionFn is a function that can be used to transform the values of a column (typically a dimension).
+ * Note that ExtractionFn implementations are expected to be Threadsafe.
  *
  * A simple example of the type of operation this enables is the RegexDimExtractionFn which applies a
  * regular expression with a capture group.  When the regular expression matches the value of a dimension,
