6636261d6f101d9beb603b6861252328697c29a3
Gian Merlino <gian@imply.io>
2017-03-04 11:16:54 -0800

    Use defaults in cache key.

--- processing/src/main/java/io/druid/query/extraction/TimeFormatExtractionFn.java
+++ processing/src/main/java/io/druid/query/extraction/TimeFormatExtractionFn.java
@@ -106,8 +106,8 @@ public class TimeFormatExtractionFn implements ExtractionFn
   @Override
   public byte[] getCacheKey()
   {
-    final String tzId = tz == null ? "" : tz.getID();
-    final String localeTag = locale == null ? "" : locale.toLanguageTag();
+    final String tzId = (tz == null ? DateTimeZone.UTC : tz).getID();
+    final String localeTag = (locale == null ? Locale.getDefault() : locale).toLanguageTag();
     final byte[] exprBytes = StringUtils.toUtf8(format + "\u0001" + tzId + "\u0001" + localeTag);
     final byte[] granularityCacheKey = granularity.getCacheKey();
     return ByteBuffer.allocate(4 + exprBytes.length + granularityCacheKey.length)
