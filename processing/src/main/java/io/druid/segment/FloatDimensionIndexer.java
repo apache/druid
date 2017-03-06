dbe1606b03c407974b554dd90086f491ebdcfff6
Gian Merlino <gianmerlino@gmail.com>
2017-03-06 10:01:30 -0800

    Fix float, long dimension indexer object selectors. (#4012)
    Their "convertUnsortedEncodedKeyComponentToActualArrayOrList" methods didn't respect the contract,
    which says they should return single values (not array/list) if there is only a single value
    to return. This affects the behavior of ObjectColumnSelectors on realtime segments.

--- processing/src/main/java/io/druid/segment/FloatDimensionIndexer.java
+++ processing/src/main/java/io/druid/segment/FloatDimensionIndexer.java
@@ -19,7 +19,6 @@
 
 package io.druid.segment;
 
-import com.google.common.collect.ImmutableList;
 import io.druid.collections.bitmap.BitmapFactory;
 import io.druid.collections.bitmap.MutableBitmap;
 import io.druid.query.dimension.DimensionSpec;
@@ -199,7 +198,7 @@ public class FloatDimensionIndexer implements DimensionIndexer<Float, Float, Flo
   @Override
   public Object convertUnsortedEncodedKeyComponentToActualArrayOrList(Float key, boolean asList)
   {
-    return ImmutableList.of(key);
+    return key;
   }
 
   @Override
