72f4a907a5dd89cbdefacb5c6f7752456c03f60e
Kenji Noguchi <kenji.noguchi@verizondigitalmedia.com>
2017-03-11 20:13:19 -0800

    Ported PR #3509

--- extensions-contrib/protobuf-extensions/src/main/java/io/druid/data/input/protobuf/ProtoBufInputRowParser.java
+++ extensions-contrib/protobuf-extensions/src/main/java/io/druid/data/input/protobuf/ProtoBufInputRowParser.java
@@ -23,108 +23,86 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.google.common.base.Throwables;
-import com.google.common.collect.Maps;
 import com.google.protobuf.ByteString;
-import com.google.protobuf.DescriptorProtos;
-import com.google.protobuf.Descriptors;
+import com.google.protobuf.DescriptorProtos.FileDescriptorSet;
+import com.google.protobuf.Descriptors.Descriptor;
+import com.google.protobuf.Descriptors.FileDescriptor;
 import com.google.protobuf.DynamicMessage;
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
 import io.druid.data.input.ByteBufferInputRowParser;
 import io.druid.data.input.InputRow;
-import io.druid.data.input.impl.MapInputRowParser;
+import io.druid.data.input.MapBasedInputRow;
 import io.druid.data.input.impl.ParseSpec;
 import io.druid.java.util.common.logger.Logger;
+import io.druid.java.util.common.parsers.ParseException;
+import io.druid.java.util.common.parsers.Parser;
 
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.util.Map;
 
 @JsonTypeName("protobuf")
-public class ProtoBufInputRowParser implements ByteBufferInputRowParser
-{
+public class ProtoBufInputRowParser implements ByteBufferInputRowParser {
   private static final Logger log = new Logger(ProtoBufInputRowParser.class);
 
   private final ParseSpec parseSpec;
-  private final MapInputRowParser mapParser;
+  private Parser<String, Object> parser;
   private final String descriptorFileInClasspath;
 
   @JsonCreator
   public ProtoBufInputRowParser(
       @JsonProperty("parseSpec") ParseSpec parseSpec,
       @JsonProperty("descriptor") String descriptorFileInClasspath
-  )
-  {
+  ) {
     this.parseSpec = parseSpec;
     this.descriptorFileInClasspath = descriptorFileInClasspath;
-      this.mapParser = new MapInputRowParser(this.parseSpec);
+    this.parser = parseSpec.makeParser();
   }
 
   @Override
-  public ParseSpec getParseSpec()
-  {
+  public ParseSpec getParseSpec() {
     return parseSpec;
   }
 
   @Override
-  public ProtoBufInputRowParser withParseSpec(ParseSpec parseSpec)
-  {
+  public ProtoBufInputRowParser withParseSpec(ParseSpec parseSpec) {
     return new ProtoBufInputRowParser(parseSpec, descriptorFileInClasspath);
   }
 
   @Override
-  public InputRow parse(ByteBuffer input)
-  {
-    // We should really create a ProtoBufBasedInputRow that does not need an intermediate map but accesses
-    // the DynamicMessage directly...
-    Map<String, Object> theMap = buildStringKeyMap(input);
-
-    return mapParser.parse(theMap);
-  }
-
-  private Map<String, Object> buildStringKeyMap(ByteBuffer input)
-  {
-    final Descriptors.Descriptor descriptor = getDescriptor(descriptorFileInClasspath);
-    final Map<String, Object> theMap = Maps.newHashMap();
-
+  public InputRow parse(ByteBuffer input) {
+    final Descriptor descriptor = getDescriptor(descriptorFileInClasspath);
+    DynamicMessage message;
     try {
-      DynamicMessage message = DynamicMessage.parseFrom(descriptor, ByteString.copyFrom(input));
-      Map<Descriptors.FieldDescriptor, Object> allFields = message.getAllFields();
-
-      for (Map.Entry<Descriptors.FieldDescriptor, Object> entry : allFields.entrySet()) {
-        String name = entry.getKey().getName();
-        if (theMap.containsKey(name)) {
-          continue;
-          // Perhaps throw an exception here?
-          // throw new RuntimeException("dupicate key " + name + " in " + message);
-        }
-        Object value = entry.getValue();
-        if (value instanceof Descriptors.EnumValueDescriptor) {
-          Descriptors.EnumValueDescriptor desc = (Descriptors.EnumValueDescriptor) value;
-          value = desc.getName();
-        }
-
-        theMap.put(name, value);
-      }
-
+      message = DynamicMessage.parseFrom(descriptor, ByteString.copyFrom(input));
+    } catch (InvalidProtocolBufferException e) {
+      throw new ParseException(e, "Invalid protobuf exception");
     }
-    catch (InvalidProtocolBufferException e) {
-      log.warn(e, "Problem with protobuf something");
+    String json = null;
+    try {
+      json = JsonFormat.printer().print(message);
+    } catch (InvalidProtocolBufferException e) {
+      e.printStackTrace();
     }
-    return theMap;
+    Map<String, Object> record = parser.parse(json);
+    return new MapBasedInputRow(
+        parseSpec.getTimestampSpec().extractTimestamp(record),
+        parseSpec.getDimensionsSpec().getDimensionNames(),
+        record
+    );
   }
 
-  private Descriptors.Descriptor getDescriptor(String descriptorFileInClassPath)
-  {
+  private Descriptor getDescriptor(String descriptorFileInClassPath) {
     try {
       InputStream fin = this.getClass().getClassLoader().getResourceAsStream(descriptorFileInClassPath);
-      DescriptorProtos.FileDescriptorSet set = DescriptorProtos.FileDescriptorSet.parseFrom(fin);
-      Descriptors.FileDescriptor file = Descriptors.FileDescriptor.buildFrom(
-          set.getFile(0), new Descriptors.FileDescriptor[]
-          {}
+      FileDescriptorSet set = FileDescriptorSet.parseFrom(fin);
+      FileDescriptor file = FileDescriptor.buildFrom(
+          set.getFile(0), new FileDescriptor[]
+              {}
       );
       return file.getMessageTypes().get(0);
-    }
-    catch (Exception e) {
+    } catch (Exception e) {
       throw Throwables.propagate(e);
     }
   }
