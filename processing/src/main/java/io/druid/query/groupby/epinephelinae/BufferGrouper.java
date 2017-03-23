ff7f90b02d2a2bd8447370243bea4efdde248da5
Akash Dwivedi <akash.exe@gmail.com>
2017-03-23 13:07:59 -0700

    relocate method in BufferAggregator. (#4071)
    *  relocate method in BufferAggregator.
    
    * Unused import.
    
    * Detailed javadoc.
    
    * using Int2ObjectMap.
    
    * batch relocate.
    
    * Revert batch relocate.
    
    * Unused import.
    
    * code comments.
    
    * code comment.

--- processing/src/main/java/io/druid/query/groupby/epinephelinae/BufferGrouper.java
+++ processing/src/main/java/io/druid/query/groupby/epinephelinae/BufferGrouper.java
@@ -430,8 +430,9 @@ public class BufferGrouper<KeyType> implements Grouper<KeyType>
 
     for (int oldBucket = 0; oldBucket < buckets; oldBucket++) {
       if (isUsed(oldBucket)) {
+        int oldPosition = oldBucket * bucketSize;
         entryBuffer.limit((oldBucket + 1) * bucketSize);
-        entryBuffer.position(oldBucket * bucketSize);
+        entryBuffer.position(oldPosition);
         keyBuffer.limit(entryBuffer.position() + HASH_SIZE + keySize);
         keyBuffer.position(entryBuffer.position() + HASH_SIZE);
 
@@ -442,8 +443,18 @@ public class BufferGrouper<KeyType> implements Grouper<KeyType>
           throw new ISE("WTF?! Couldn't find a bucket while resizing?!");
         }
 
-        newTableBuffer.position(newBucket * bucketSize);
+        int newPosition = newBucket * bucketSize;
+        newTableBuffer.position(newPosition);
         newTableBuffer.put(entryBuffer);
+
+        for (int i = 0; i < aggregators.length; i++) {
+          aggregators[i].relocate(
+              oldPosition + aggregatorOffsets[i],
+              newPosition + aggregatorOffsets[i],
+              tableBuffer,
+              newTableBuffer
+          );
+        }
 
         buffer.putInt(tableArenaSize + newSize * Ints.BYTES, newBucket * bucketSize);
         newSize++;
