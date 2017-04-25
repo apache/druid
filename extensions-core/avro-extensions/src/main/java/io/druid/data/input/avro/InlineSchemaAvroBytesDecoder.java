de815da9422ae87bc7cff58a154d098908e6625f
Benedict Jin <1571805553@qq.com>
2017-04-25 11:46:32 +0800

    Some code refactor for better performance of `Avro-Extension` (#4092)
    * 1. Collections.singletonList instand of Arrays.asList; 2. close FSDataInputStream/ByteBufferInputStream for releasing resource; 3. convert com.google.common.base.Function into java.util.function.Function; 4. others code refactor
    
    * Put each param on its own line for  code style
    
    * Revert GenericRecordAsMap back about `Function`

--- extensions-core/avro-extensions/src/main/java/io/druid/data/input/avro/InlineSchemaAvroBytesDecoder.java
+++ extensions-core/avro-extensions/src/main/java/io/druid/data/input/avro/InlineSchemaAvroBytesDecoder.java
@@ -48,6 +48,7 @@ public class InlineSchemaAvroBytesDecoder implements AvroBytesDecoder
 
   private final Schema schemaObj;
   private final Map<String, Object> schema;
+  private final DatumReader<GenericRecord> reader;
 
   @JsonCreator
   public InlineSchemaAvroBytesDecoder(
@@ -61,7 +62,8 @@ public class InlineSchemaAvroBytesDecoder implements AvroBytesDecoder
     String schemaStr = mapper.writeValueAsString(schema);
 
     LOGGER.info("Schema string [%s]", schemaStr);
-    schemaObj = new Schema.Parser().parse(schemaStr);
+    this.schemaObj = new Schema.Parser().parse(schemaStr);
+    this.reader = new GenericDatumReader<>(this.schemaObj);
   }
 
   //For UT only
@@ -69,6 +71,7 @@ public class InlineSchemaAvroBytesDecoder implements AvroBytesDecoder
   InlineSchemaAvroBytesDecoder(Schema schemaObj)
   {
     this.schemaObj = schemaObj;
+    this.reader = new GenericDatumReader<>(schemaObj);
     this.schema = null;
   }
 
@@ -81,9 +84,7 @@ public class InlineSchemaAvroBytesDecoder implements AvroBytesDecoder
   @Override
   public GenericRecord parse(ByteBuffer bytes)
   {
-    DatumReader<GenericRecord> reader = new GenericDatumReader<GenericRecord>(schemaObj);
-    ByteBufferInputStream inputStream = new ByteBufferInputStream(Collections.singletonList(bytes));
-    try {
+    try (ByteBufferInputStream inputStream = new ByteBufferInputStream(Collections.singletonList(bytes))) {
       return reader.read(null, DecoderFactory.get().binaryDecoder(inputStream, null));
     }
     catch (EOFException eof) {
