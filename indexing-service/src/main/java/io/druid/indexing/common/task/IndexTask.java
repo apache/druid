7e7d6f04740e040ea5616d6fa22b0be93f536df1
fjy <fangjinyang@gmail.com>
2017-04-25 18:12:44 -0700

    more cr

--- indexing-service/src/main/java/io/druid/indexing/common/task/IndexTask.java
+++ indexing-service/src/main/java/io/druid/indexing/common/task/IndexTask.java
@@ -272,6 +272,7 @@ public class IndexTask extends AbstractTask
       while (firehose.hasMore()) {
         final InputRow inputRow = firehose.nextRow();
 
+        // If a header is present in the data (and with proper configurations), a null InputRow will get created
         if (inputRow == null) {
           continue;
         }
