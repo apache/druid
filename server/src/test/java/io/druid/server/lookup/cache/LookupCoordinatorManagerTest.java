417714d228972c6c397265d9d822af0e9b895a0c
Himanshu <g.himanshu@gmail.com>
2017-05-04 13:15:30 -0500

    additional lookup status discovery http endpoints at coordinator (#4228)
    * additional lookup status discovery http endpoints at coordinator
    
    * more changes
    
    * jsonize the error msgs as well
    
    * fix tests

--- server/src/test/java/io/druid/server/lookup/cache/LookupCoordinatorManagerTest.java
+++ server/src/test/java/io/druid/server/lookup/cache/LookupCoordinatorManagerTest.java
@@ -1135,7 +1135,7 @@ public class LookupCoordinatorManagerTest
         lookupsCommunicator
     );
 
-    Assert.assertNull(manager.knownOldState.get());
+    Assert.assertTrue(manager.knownOldState.get().isEmpty());
 
     manager.start();
 
@@ -1335,6 +1335,7 @@ public class LookupCoordinatorManagerTest
         configManager,
         lookupCoordinatorManagerConfig
     );
+    manager.start();
     Assert.assertEquals(fakeChildren, manager.discoverTiers());
     EasyMock.verify(discoverer);
   }
@@ -1371,12 +1372,10 @@ public class LookupCoordinatorManagerTest
         configManager,
         lookupCoordinatorManagerConfig
     );
-    try {
-      manager.discoverTiers();
-    }
-    finally {
-      EasyMock.verify(discoverer);
-    }
+
+    manager.start();
+    manager.discoverTiers();
+    EasyMock.verify(discoverer);
   }
 
   //tests that lookups stored in db from 0.10.0 are converted and restored.
