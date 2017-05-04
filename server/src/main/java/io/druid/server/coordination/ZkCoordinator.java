f75b3f3de899f63b8c7be889c0823c24ab3e97f1
Gian Merlino <gian@imply.io>
2017-05-03 17:27:29 -0700

    Only alert once.

--- server/src/main/java/io/druid/server/coordination/ZkCoordinator.java
+++ server/src/main/java/io/druid/server/coordination/ZkCoordinator.java
@@ -252,6 +252,7 @@ public class ZkCoordinator implements DataSegmentChangeHandler
 
     List<DataSegment> cachedSegments = Lists.newArrayList();
     File[] segmentsToLoad = baseDir.listFiles();
+    int ignored = 0;
     for (int i = 0; i < segmentsToLoad.length; i++) {
       File file = segmentsToLoad[i];
       log.info("Loading segment cache file [%d/%d][%s].", i, segmentsToLoad.length, file);
@@ -259,10 +260,8 @@ public class ZkCoordinator implements DataSegmentChangeHandler
         final DataSegment segment = jsonMapper.readValue(file, DataSegment.class);
 
         if (!segment.getIdentifier().equals(file.getName())) {
-          log.makeAlert("Ignoring cache file for dataSource[%s].", segment.getDataSource())
-             .addData("cacheFile", file.getPath())
-             .addData("segmentIdentifier", segment.getIdentifier())
-             .emit();
+          log.warn("Ignoring cache file[%s] for segment[%s].", file.getPath(), segment.getIdentifier());
+          ignored++;
         } else if (serverManager.isSegmentCached(segment)) {
           cachedSegments.add(segment);
         } else {
@@ -279,6 +278,12 @@ public class ZkCoordinator implements DataSegmentChangeHandler
            .addData("file", file)
            .emit();
       }
+    }
+
+    if (ignored > 0) {
+      log.makeAlert("Ignored misnamed segment cache files on startup.")
+         .addData("numIgnored", ignored)
+         .emit();
     }
 
     addSegments(
