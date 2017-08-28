2e374433ce89fb517d62929467d666a1c8b50593
leventov <leventov@ya.ru>
2017-08-28 11:47:55 -0500

    Comments

--- processing/src/main/java/io/druid/segment/incremental/TimeAndDimsHolder.java
+++ processing/src/main/java/io/druid/segment/incremental/TimeAndDimsHolder.java
@@ -37,11 +37,18 @@ public class TimeAndDimsHolder
     this.currEntry = currEntry;
   }
 
+  /**
+   * This method doesn't have well-defined semantics ("key" of what?), should be removed in favor of {@link #get()}.
+   */
   public IncrementalIndex.TimeAndDims getKey()
   {
     return currEntry;
   }
 
+  /**
+   * This method doesn't have well-defined semantics ("value" of what?), should be removed in favor of chaining
+   * get().getRowIndex().
+   */
   public int getValue()
   {
     return currEntry.getRowIndex();
