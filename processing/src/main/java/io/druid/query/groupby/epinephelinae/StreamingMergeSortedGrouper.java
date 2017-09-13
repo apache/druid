4628c5e5c2dbdd97a52a7ac2f48c024417268c3d
Jihoon Son <jihoonson@apache.org>
2017-09-13 12:14:16 -0700

    Remove loop

--- processing/src/main/java/io/druid/query/groupby/epinephelinae/StreamingMergeSortedGrouper.java
+++ processing/src/main/java/io/druid/query/groupby/epinephelinae/StreamingMergeSortedGrouper.java
@@ -205,10 +205,11 @@ public class StreamingMergeSortedGrouper<KeyType> implements Grouper<KeyType>
       }
     }
 
-    for (; i + Integer.BYTES <= keySize; i += Integer.BYTES) {
+    if (i + Integer.BYTES <= keySize) {
       if (curKeyBuffer.getInt(i) != buffer.getInt(bufferOffset + i)) {
         return false;
       }
+      i += Integer.BYTES;
     }
 
     for (; i < keySize; i++) {
