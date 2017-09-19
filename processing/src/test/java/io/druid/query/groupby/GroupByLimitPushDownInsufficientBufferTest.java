e4fac0785e8756e3a1c96ceefc8fc3d0a2c305dd
leventov <leventov@ya.ru>
2017-09-18 23:29:31 -0500

    Fix test

--- processing/src/test/java/io/druid/query/groupby/GroupByLimitPushDownInsufficientBufferTest.java
+++ processing/src/test/java/io/druid/query/groupby/GroupByLimitPushDownInsufficientBufferTest.java
@@ -48,6 +48,7 @@ import io.druid.java.util.common.guava.Sequence;
 import io.druid.java.util.common.guava.Sequences;
 import io.druid.java.util.common.logger.Logger;
 import io.druid.math.expr.ExprMacroTable;
+import io.druid.output.OffHeapMemoryOutputMediumFactory;
 import io.druid.query.BySegmentQueryRunner;
 import io.druid.query.DruidProcessingConfig;
 import io.druid.query.FinalizeResultsQueryRunner;
@@ -117,6 +118,7 @@ public class GroupByLimitPushDownInsufficientBufferTest
     );
     INDEX_IO = new IndexIO(
         JSON_MAPPER,
+        OffHeapMemoryOutputMediumFactory.instance(),
         new ColumnConfig()
         {
           @Override
@@ -126,7 +128,7 @@ public class GroupByLimitPushDownInsufficientBufferTest
           }
         }
     );
-    INDEX_MERGER_V9 = new IndexMergerV9(JSON_MAPPER, INDEX_IO);
+    INDEX_MERGER_V9 = new IndexMergerV9(JSON_MAPPER, INDEX_IO, OffHeapMemoryOutputMediumFactory.instance());
   }
 
 
@@ -210,7 +212,8 @@ public class GroupByLimitPushDownInsufficientBufferTest
     final File fileA = INDEX_MERGER_V9.persist(
         indexA,
         new File(tmpDir, "A"),
-        new IndexSpec()
+        new IndexSpec(),
+        OffHeapMemoryOutputMediumFactory.instance()
     );
     QueryableIndex qindexA = INDEX_IO.loadIndex(fileA);
 
@@ -251,7 +254,8 @@ public class GroupByLimitPushDownInsufficientBufferTest
     final File fileB = INDEX_MERGER_V9.persist(
         indexB,
         new File(tmpDir, "B"),
-        new IndexSpec()
+        new IndexSpec(),
+        OffHeapMemoryOutputMediumFactory.instance()
     );
     QueryableIndex qindexB = INDEX_IO.loadIndex(fileB);
 
