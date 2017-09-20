3c363ea1057f7fcd82a51711733a1bedb6d01354
saydakov <saydakov@yahoo-inc.com>
2017-09-20 12:23:44 -0700

    style fix

--- extensions-core/datasketches/src/main/java/io/druid/query/aggregation/datasketches/theta/SynchronizedUnion.java
+++ extensions-core/datasketches/src/main/java/io/druid/query/aggregation/datasketches/theta/SynchronizedUnion.java
@@ -115,7 +115,8 @@ public class SynchronizedUnion implements Union
   }
 
   @Override
-  public boolean isSameResource(Memory mem) {
+  public boolean isSameResource(Memory mem)
+  {
     return delegate.isSameResource(mem);
   }
 }
