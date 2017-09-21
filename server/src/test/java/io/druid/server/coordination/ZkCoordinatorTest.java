3a6318eceea805b58916adf5f788d7cff096837e
Roman Leventov <leventov.ru@gmail.com>
2017-09-21 02:55:09 -0500

    Remove SegmentLoaderConfig.numLoadingThreads config (#4831)

--- server/src/test/java/io/druid/server/coordination/ZkCoordinatorTest.java
+++ server/src/test/java/io/druid/server/coordination/ZkCoordinatorTest.java
@@ -203,12 +203,6 @@ public class ZkCoordinatorTest extends CuratorTestBase
           }
 
           @Override
-          public int getNumLoadingThreads()
-          {
-            return 5;
-          }
-
-          @Override
           public int getAnnounceIntervalMillis()
           {
             return 50;
@@ -491,12 +485,6 @@ public class ZkCoordinatorTest extends CuratorTestBase
                   public File getInfoDir()
                   {
                     return infoDir;
-                  }
-
-                  @Override
-                  public int getNumLoadingThreads()
-                  {
-                    return 5;
                   }
 
                   @Override
