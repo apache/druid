840c6445c06718f00c1e0c79ab0547fe27770b7c
Atul Mohan <atulmohan@yahoo-inc.com>
2017-10-04 16:31:01 -0500

    Remove check to see future is done

--- server/src/main/java/io/druid/query/lookup/LookupReferencesManager.java
+++ server/src/main/java/io/druid/query/lookup/LookupReferencesManager.java
@@ -490,9 +490,7 @@ public class LookupReferencesManager
     catch (Exception e) {
       LOG.error(e, "Failed to finish lookup load process.");
       for (Future future : futures) {
-        if (!future.isDone()) {
           future.cancel(true);
-        }
       }
     }
     finally {
