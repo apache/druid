c2bd45bf3040452b6c1a2cb69359a326f93df184
Himanshu <g.himanshu@gmail.com>
2017-11-10 12:39:35 -0600

    Fix coordinator/overlord redirects when TLS is enabled (#5037) (#5068)
    * Fix coordinator/overlord redirects when TLS is enabled
    
    * address review comment
    
    * fix UTs
    
    * workaround to not ignore URL instance to fix the teamcity build
    
    * update tls doc

--- server/src/main/java/io/druid/server/http/RedirectInfo.java
+++ server/src/main/java/io/druid/server/http/RedirectInfo.java
@@ -25,7 +25,6 @@ import java.net.URL;
  */
 public interface RedirectInfo
 {
-  public boolean doLocal(String requestURI);
-
-  public URL getRedirectURL(String scheme, String queryString, String requestURI);
+  boolean doLocal(String requestURI);
+  URL getRedirectURL(String queryString, String requestURI);
 }
