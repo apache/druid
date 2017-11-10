aa79205e4d64e73af9fae80f592299852cab73ea
egor-ryashin <egor.ryashin@metamarkets.com>
2017-11-10 16:34:37 +0300

    removed an experimental travis test exception

--- integration-tests/src/test/java/io/druid/tests/indexer/ITRealtimeIndexTaskTest.java
+++ integration-tests/src/test/java/io/druid/tests/indexer/ITRealtimeIndexTaskTest.java
@@ -174,7 +174,6 @@ public class ITRealtimeIndexTaskTest extends AbstractIndexerTest
     finally {
       unloadAndKillData(INDEX_DATASOURCE);
     }
-    throw new RuntimeException();
   }
 
   private String setShutOffTime(String taskAsString, DateTime time)
