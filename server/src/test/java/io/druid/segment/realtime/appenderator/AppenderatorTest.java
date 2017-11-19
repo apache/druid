5297070fa6a68f090c273a075dd513049def3f22
Parag Jain <paragjain16@gmail.com>
2017-11-19 12:50:19 -0600

    add unit test

--- server/src/test/java/io/druid/segment/realtime/appenderator/AppenderatorTest.java
+++ server/src/test/java/io/druid/segment/realtime/appenderator/AppenderatorTest.java
@@ -198,6 +198,52 @@ public class AppenderatorTest
   }
 
   @Test
+  public void testMaxRowsInMemoryDisallowIncrementalPersists() throws Exception
+  {
+    try (final AppenderatorTester tester = new AppenderatorTester(3, false)) {
+      final Appenderator appenderator = tester.getAppenderator();
+      final AtomicInteger eventCount = new AtomicInteger(0);
+      final Supplier<Committer> committerSupplier = () -> {
+        final Object metadata = ImmutableMap.of("eventCount", eventCount.get());
+
+        return new Committer()
+        {
+          @Override
+          public Object getMetadata()
+          {
+            return metadata;
+          }
+
+          @Override
+          public void run()
+          {
+            // Do nothing
+          }
+        };
+      };
+
+      Assert.assertEquals(0, ((AppenderatorImpl) appenderator).getRowsInMemory());
+      appenderator.startJob();
+      Assert.assertEquals(0, ((AppenderatorImpl) appenderator).getRowsInMemory());
+      appenderator.add(IDENTIFIERS.get(0), IR("2000", "foo", 1), committerSupplier, false);
+      Assert.assertEquals(1, ((AppenderatorImpl) appenderator).getRowsInMemory());
+      appenderator.add(IDENTIFIERS.get(1), IR("2000", "bar", 1), committerSupplier, false);
+      Assert.assertEquals(2, ((AppenderatorImpl) appenderator).getRowsInMemory());
+      appenderator.add(IDENTIFIERS.get(1), IR("2000", "bar", 1), committerSupplier, false);
+      Assert.assertEquals(2, ((AppenderatorImpl) appenderator).getRowsInMemory());
+      appenderator.add(IDENTIFIERS.get(0), IR("2000", "baz", 1), committerSupplier, false);
+      Assert.assertEquals(3, ((AppenderatorImpl) appenderator).getRowsInMemory());
+      appenderator.add(IDENTIFIERS.get(1), IR("2000", "qux", 1), committerSupplier, false);
+      Assert.assertEquals(4, ((AppenderatorImpl) appenderator).getRowsInMemory());
+      appenderator.add(IDENTIFIERS.get(0), IR("2000", "bob", 1), committerSupplier, false);
+      Assert.assertEquals(5, ((AppenderatorImpl) appenderator).getRowsInMemory());
+      appenderator.persist(ImmutableList.of(IDENTIFIERS.get(1)), committerSupplier.get());
+      Assert.assertEquals(3, ((AppenderatorImpl) appenderator).getRowsInMemory());
+      appenderator.close();
+      Assert.assertEquals(0, ((AppenderatorImpl) appenderator).getRowsInMemory());
+    }
+  }
+  @Test
   public void testRestoreFromDisk() throws Exception
   {
     final RealtimeTuningConfig tuningConfig;
