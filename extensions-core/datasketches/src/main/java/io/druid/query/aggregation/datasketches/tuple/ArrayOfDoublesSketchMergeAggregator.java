a374f0810dd1120d83daab35082df0e2c1b9de38
saydakov <saydakov@yahoo-inc.com>
2018-01-22 17:52:19 -0800

    removed @Override, strict compiler doesn't like it

--- extensions-core/datasketches/src/main/java/io/druid/query/aggregation/datasketches/tuple/ArrayOfDoublesSketchMergeAggregator.java
+++ extensions-core/datasketches/src/main/java/io/druid/query/aggregation/datasketches/tuple/ArrayOfDoublesSketchMergeAggregator.java
@@ -67,7 +67,6 @@ public class ArrayOfDoublesSketchMergeAggregator implements Aggregator
     throw new UnsupportedOperationException("Not implemented");
   }
 
-  @Override
   public void reset()
   {
     union.reset();
