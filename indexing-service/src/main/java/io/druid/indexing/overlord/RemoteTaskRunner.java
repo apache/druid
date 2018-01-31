3a1099ef73c5daed79cad339ed73f03bc8b81eb4
Clint Wylie <clint.wylie@metamarkets.com>
2018-01-31 15:09:43 -0800

    add comment

--- indexing-service/src/main/java/io/druid/indexing/overlord/RemoteTaskRunner.java
+++ indexing-service/src/main/java/io/druid/indexing/overlord/RemoteTaskRunner.java
@@ -1235,6 +1235,8 @@ public class RemoteTaskRunner implements WorkerTaskRunner, TaskLogStreamer
   @Override
   public Collection<Worker> markWorkersLazy(Predicate<ImmutableWorkerInfo> isLazyWorker, int maxWorkers)
   {
+    // skip the lock and bail early if we should not mark any workers lazy (e.g. number
+    // of current workers is at or below the minNumWorkers of autoscaler config)
     if (maxWorkers < 1) {
       return Collections.emptyList();
     }
