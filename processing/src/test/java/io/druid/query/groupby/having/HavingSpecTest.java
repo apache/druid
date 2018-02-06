apache/druid

commit a7e802c9d42a93830ee387bca38bf1d5f5c29b95

greater-than/less-than/equal-to havingSpec to call AggregatorFactory.finalizeComputation(..) (#4883)
* greater-than/less-than/equal-to havingSpec to call AggregatorFactory.finalizeComputation(..)

* fix the unit test and expect having to work on hyperUnique agg

* test fix

* fix style errors

processing/src/main/java/io/druid/query/groupby/having/HavingSpecMetricComparator.java
@@ -19,9 +19,15 @@
 
 package io.druid.query.groupby.having;
 
+import com.google.common.primitives.Doubles;
+import com.google.common.primitives.Floats;
+import com.google.common.primitives.Ints;
+import com.google.common.primitives.Longs;
 import io.druid.data.input.Row;
+import io.druid.query.aggregation.AggregatorFactory;
 import io.druid.java.util.common.ISE;
 
+import java.util.Map;
 import java.util.regex.Pattern;
 
 /**
@@ -30,16 +36,28 @@ class HavingSpecMetricComparator
 {
   static final Pattern LONG_PAT = Pattern.compile("[-|+]?\\d+");
 
-  static int compare(Row row, String aggregationName, Number value)
+  static int compare(Row row, String aggregationName, Number value, Map<String, AggregatorFactory> aggregators)
   {
+
     Object metricValueObj = row.getRaw(aggregationName);
+
     if (metricValueObj != null) {
+      if (aggregators != null && aggregators.containsKey(aggregationName)) {
+        metricValueObj = aggregators.get(aggregationName).finalizeComputation(metricValueObj);
+      }
+
       if (metricValueObj instanceof Long) {
-        return Long.compare((Long) metricValueObj, value.longValue());
+        long l = ((Long) metricValueObj).longValue();
+        return Longs.compare(l, value.longValue());
       } else if (metricValueObj instanceof Float) {
-        return Float.compare((Float) metricValueObj, value.floatValue());
+        float l = ((Float) metricValueObj).floatValue();
+        return Floats.compare(l, value.floatValue());
       } else if (metricValueObj instanceof Double) {
-        return Double.compare((Double) metricValueObj, value.doubleValue());
+        double l = ((Double) metricValueObj).longValue();
+        return Doubles.compare(l, value.doubleValue());
+      } else if (metricValueObj instanceof Integer) {
+        int l = ((Integer) metricValueObj).intValue();
+        return Ints.compare(l, value.intValue());
       } else if (metricValueObj instanceof String) {
         String metricValueStr = (String) metricValueObj;
         if (LONG_PAT.matcher(metricValueStr).matches()) {

processing/src/main/java/io/druid/query/groupby/GroupByQuery.java
@@ -68,6 +68,7 @@ import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -144,6 +145,7 @@ public class GroupByQuery extends BaseQuery<Row>
           postProcessingFn,
           (Sequence<Row> input) -> {
             havingSpec.setRowSignature(GroupByQueryHelper.rowSignatureFor(GroupByQuery.this));
+            havingSpec.setAggregators(getAggregatorsMap(aggregatorSpecs));
             return Sequences.filter(input, havingSpec::eval);
           }
       );
@@ -694,6 +696,13 @@ public class GroupByQuery extends BaseQuery<Row>
           Column.TIME_COLUMN_NAME
       );
     }
+  }
+
+  private static Map<String, AggregatorFactory> getAggregatorsMap(List<AggregatorFactory> aggregatorSpecs)
+  {
+    Map<String, AggregatorFactory> map = new HashMap<>(aggregatorSpecs.size());
+    aggregatorSpecs.stream().forEach(v -> map.put(v.getName(), v));
+    return map;
   }
 
   public static class Builder

processing/src/main/java/io/druid/query/groupby/having/AndHavingSpec.java
@@ -23,6 +23,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableList;
 import io.druid.data.input.Row;
+import io.druid.query.aggregation.AggregatorFactory;
 import io.druid.segment.column.ValueType;
 
 import java.util.List;
@@ -52,6 +53,14 @@ public class AndHavingSpec extends BaseHavingSpec
   {
     for (HavingSpec havingSpec : havingSpecs) {
       havingSpec.setRowSignature(rowSignature);
+    }
+  }
+
+  @Override
+  public void setAggregators(Map<String, AggregatorFactory> aggregators)
+  {
+    for (HavingSpec havingSpec : havingSpecs) {
+      havingSpec.setAggregators(aggregators);
     }
   }
 

processing/src/main/java/io/druid/query/groupby/having/BaseHavingSpec.java
@@ -19,6 +19,7 @@
 
 package io.druid.query.groupby.having;
 
+import io.druid.query.aggregation.AggregatorFactory;
 import io.druid.segment.column.ValueType;
 
 import java.util.Map;
@@ -29,5 +30,11 @@ public abstract class BaseHavingSpec implements HavingSpec
   public void setRowSignature(Map<String, ValueType> rowSignature)
   {
     // Do nothing.
+  }
+
+  @Override
+  public void setAggregators(Map<String, AggregatorFactory> aggregators)
+  {
+
   }
 }

processing/src/main/java/io/druid/query/groupby/having/EqualToHavingSpec.java
@@ -22,6 +22,9 @@ package io.druid.query.groupby.having;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.druid.data.input.Row;
+import io.druid.query.aggregation.AggregatorFactory;
+
+import java.util.Map;
 
 /**
  * The "=" operator in a "having" clause. This is similar to SQL's "having aggregation = value",
@@ -31,6 +34,8 @@ public class EqualToHavingSpec extends BaseHavingSpec
 {
   private final String aggregationName;
   private final Number value;
+
+  private volatile Map<String, AggregatorFactory> aggregators;
 
   @JsonCreator
   public EqualToHavingSpec(
@@ -55,9 +60,15 @@ public class EqualToHavingSpec extends BaseHavingSpec
   }
 
   @Override
+  public void setAggregators(Map<String, AggregatorFactory> aggregators)
+  {
+    this.aggregators = aggregators;
+  }
+
+  @Override
   public boolean eval(Row row)
   {
-    return HavingSpecMetricComparator.compare(row, aggregationName, value) == 0;
+    return HavingSpecMetricComparator.compare(row, aggregationName, value, aggregators) == 0;
   }
 
   /**

processing/src/main/java/io/druid/query/groupby/having/GreaterThanHavingSpec.java
@@ -22,6 +22,9 @@ package io.druid.query.groupby.having;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.druid.data.input.Row;
+import io.druid.query.aggregation.AggregatorFactory;
+
+import java.util.Map;
 
 /**
  * The "&gt;" operator in a "having" clause. This is similar to SQL's "having aggregation &gt; value",
@@ -31,6 +34,8 @@ public class GreaterThanHavingSpec extends BaseHavingSpec
 {
   private final String aggregationName;
   private final Number value;
+
+  private volatile Map<String, AggregatorFactory> aggregators;
 
   @JsonCreator
   public GreaterThanHavingSpec(
@@ -55,9 +60,15 @@ public class GreaterThanHavingSpec extends BaseHavingSpec
   }
 
   @Override
+  public void setAggregators(Map<String, AggregatorFactory> aggregators)
+  {
+    this.aggregators = aggregators;
+  }
+
+  @Override
   public boolean eval(Row row)
   {
-    return HavingSpecMetricComparator.compare(row, aggregationName, value) > 0;
+    return HavingSpecMetricComparator.compare(row, aggregationName, value, aggregators) > 0;
   }
 
   /**

processing/src/main/java/io/druid/query/groupby/having/HavingSpec.java
@@ -22,6 +22,7 @@ package io.druid.query.groupby.having;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.druid.data.input.Row;
+import io.druid.query.aggregation.AggregatorFactory;
 import io.druid.segment.column.ValueType;
 
 import java.util.Map;
@@ -57,6 +58,8 @@ public interface HavingSpec
    * @param rowSignature signature of the rows
    */
   void setRowSignature(Map<String, ValueType> rowSignature);
+
+  void setAggregators(Map<String, AggregatorFactory> aggregators);
 
   /**
    * Evaluates if a given row satisfies the having spec.

processing/src/main/java/io/druid/query/groupby/having/LessThanHavingSpec.java
@@ -21,6 +21,9 @@ package io.druid.query.groupby.having;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.druid.data.input.Row;
+import io.druid.query.aggregation.AggregatorFactory;
+
+import java.util.Map;
 
 /**
  * The "&lt;" operator in a "having" clause. This is similar to SQL's "having aggregation &lt; value",
@@ -30,6 +33,8 @@ public class LessThanHavingSpec extends BaseHavingSpec
 {
   private final String aggregationName;
   private final Number value;
+
+  private volatile Map<String, AggregatorFactory> aggregators;
 
   public LessThanHavingSpec(
       @JsonProperty("aggregation") String aggName,
@@ -53,9 +58,15 @@ public class LessThanHavingSpec extends BaseHavingSpec
   }
 
   @Override
+  public void setAggregators(Map<String, AggregatorFactory> aggregators)
+  {
+    this.aggregators = aggregators;
+  }
+
+  @Override
   public boolean eval(Row row)
   {
-    return HavingSpecMetricComparator.compare(row, aggregationName, value) < 0;
+    return HavingSpecMetricComparator.compare(row, aggregationName, value, aggregators) < 0;
   }
 
   /**

commit d67633d1fd5e3354b6bba16abe0edd1eee0cbfcd

Fix two improper casts in HavingSpecMetricComparator.
Fixes two things:

1. An improper double-to-long cast when comparing double metrics to any
   kind of value, which was a regression from #4883.
2. An improper double-to-long cast when comparing a long/int metric to a
   double/float value: the value was cast to long/int, drawing strange
   conclusions like int 100 matching a havingSpec of equalTo(100.5).

processing/src/main/java/io/druid/query/groupby/having/HavingSpecMetricComparator.java
@@ -24,9 +24,10 @@ import com.google.common.primitives.Floats;
 import com.google.common.primitives.Ints;
 import com.google.common.primitives.Longs;
 import io.druid.data.input.Row;
-import io.druid.query.aggregation.AggregatorFactory;
 import io.druid.java.util.common.ISE;
+import io.druid.query.aggregation.AggregatorFactory;
 
+import java.math.BigDecimal;
 import java.util.Map;
 import java.util.regex.Pattern;
 
@@ -46,18 +47,22 @@ class HavingSpecMetricComparator
         metricValueObj = aggregators.get(aggregationName).finalizeComputation(metricValueObj);
       }
 
-      if (metricValueObj instanceof Long) {
-        long l = ((Long) metricValueObj).longValue();
-        return Longs.compare(l, value.longValue());
+      if (metricValueObj instanceof Long || metricValueObj instanceof Integer) {
+        long n = ((Number) metricValueObj).longValue();
+
+        if (value instanceof Long || value instanceof Integer) {
+          return Longs.compare(n, value.longValue());
+        } else if (value instanceof Double || value instanceof Float) {
+          return BigDecimal.valueOf(n).compareTo(BigDecimal.valueOf(value.doubleValue()));
+        } else {
+          throw new ISE("Number was[%s]?!?", value.getClass().getName());
+        }
       } else if (metricValueObj instanceof Float) {
-        float l = ((Float) metricValueObj).floatValue();
-        return Floats.compare(l, value.floatValue());
+        float n = (Float) metricValueObj;
+        return Floats.compare(n, value.floatValue());
       } else if (metricValueObj instanceof Double) {
-        double l = ((Double) metricValueObj).longValue();
-        return Doubles.compare(l, value.doubleValue());
-      } else if (metricValueObj instanceof Integer) {
-        int l = ((Integer) metricValueObj).intValue();
-        return Ints.compare(l, value.intValue());
+        double n = (Double) metricValueObj;
+        return Doubles.compare(n, value.doubleValue());
       } else if (metricValueObj instanceof String) {
         String metricValueStr = (String) metricValueObj;
         if (LONG_PAT.matcher(metricValueStr).matches()) {

processing/src/test/java/io/druid/query/groupby/having/HavingSpecTest.java
@@ -101,9 +101,8 @@ public class HavingSpecTest
         "value", 1.3
     );
     ObjectMapper mapper = new DefaultObjectMapper();
-    @SuppressWarnings("unused") // expected exception
+    // noinspection unused
     HavingSpec spec = mapper.convertValue(greaterMap, HavingSpec.class);
-
   }
 
   @Test
@@ -156,7 +155,62 @@ public class HavingSpecTest
     assertFalse(spec.eval(getTestRow(100.05f)));
 
     spec = new EqualToHavingSpec("metric", 100.56f);
+    assertFalse(spec.eval(getTestRow(100L)));
+    assertFalse(spec.eval(getTestRow(100.0)));
+    assertFalse(spec.eval(getTestRow(100d)));
+    assertFalse(spec.eval(getTestRow(100.56d))); // False since 100.56d != (double) 100.56f
+    assertFalse(spec.eval(getTestRow(90.53d)));
     assertTrue(spec.eval(getTestRow(100.56f)));
+    assertFalse(spec.eval(getTestRow(90.53f)));
+    assertFalse(spec.eval(getTestRow(Long.MAX_VALUE)));
+
+    spec = new EqualToHavingSpec("metric", 100.56d);
+    assertFalse(spec.eval(getTestRow(100L)));
+    assertFalse(spec.eval(getTestRow(100.0)));
+    assertFalse(spec.eval(getTestRow(100d)));
+    assertTrue(spec.eval(getTestRow(100.56d))); // True since 100.56f == (float) 100.56d
+    assertFalse(spec.eval(getTestRow(90.53d)));
+    assertTrue(spec.eval(getTestRow(100.56f)));
+    assertFalse(spec.eval(getTestRow(90.53f)));
+    assertFalse(spec.eval(getTestRow(Long.MAX_VALUE)));
+
+    spec = new EqualToHavingSpec("metric", 100.0f);
+    assertTrue(spec.eval(getTestRow(100L)));
+    assertTrue(spec.eval(getTestRow(100.0)));
+    assertTrue(spec.eval(getTestRow(100d)));
+    assertFalse(spec.eval(getTestRow(100.56d)));
+    assertFalse(spec.eval(getTestRow(90.53d)));
+    assertFalse(spec.eval(getTestRow(100.56f)));
+    assertFalse(spec.eval(getTestRow(90.53f)));
+    assertFalse(spec.eval(getTestRow(Long.MAX_VALUE)));
+
+    spec = new EqualToHavingSpec("metric", 100.0d);
+    assertTrue(spec.eval(getTestRow(100L)));
+    assertTrue(spec.eval(getTestRow(100.0)));
+    assertTrue(spec.eval(getTestRow(100d)));
+    assertFalse(spec.eval(getTestRow(100.56d)));
+    assertFalse(spec.eval(getTestRow(90.53d)));
+    assertFalse(spec.eval(getTestRow(100.56f)));
+    assertFalse(spec.eval(getTestRow(90.53f)));
+    assertFalse(spec.eval(getTestRow(Long.MAX_VALUE)));
+
+    spec = new EqualToHavingSpec("metric", 100);
+    assertTrue(spec.eval(getTestRow(100L)));
+    assertTrue(spec.eval(getTestRow(100.0)));
+    assertTrue(spec.eval(getTestRow(100d)));
+    assertFalse(spec.eval(getTestRow(100.56d)));
+    assertFalse(spec.eval(getTestRow(90.53d)));
+    assertFalse(spec.eval(getTestRow(100.56f)));
+    assertFalse(spec.eval(getTestRow(90.53f)));
+    assertFalse(spec.eval(getTestRow(Long.MAX_VALUE)));
+
+    spec = new EqualToHavingSpec("metric", 100L);
+    assertTrue(spec.eval(getTestRow(100L)));
+    assertTrue(spec.eval(getTestRow(100.0)));
+    assertTrue(spec.eval(getTestRow(100d)));
+    assertFalse(spec.eval(getTestRow(100.56d)));
+    assertFalse(spec.eval(getTestRow(90.53d)));
+    assertFalse(spec.eval(getTestRow(100.56f)));
     assertFalse(spec.eval(getTestRow(90.53f)));
     assertFalse(spec.eval(getTestRow(Long.MAX_VALUE)));
   }