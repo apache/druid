37537b6c4e192ecea22b579d3bb57842f92bf25a
leventov <leventov@ya.ru>
2018-04-07 18:47:32 +0300

    Replace EmittedBatchCounter and UpdateCounter with (both not safe for concurrent increments/updates) with ConcurrentAwaitableCounter (safe for concurrent increments)

---
+++ java-util/src/test/java/io/druid/concurrent/ConcurrentAwaitableCounterTest.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.druid.concurrent;
+
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+
+public class ConcurrentAwaitableCounterTest
+{
+
+  @Test(timeout = 1000)
+  public void smokeTest() throws InterruptedException
+  {
+    ConcurrentAwaitableCounter counter = new ConcurrentAwaitableCounter();
+    CountDownLatch start = new CountDownLatch(1);
+    CountDownLatch finish = new CountDownLatch(7);
+    for (int i = 0; i < 2; i++) {
+      new Thread(() -> {
+        try {
+          start.await();
+          for (int j = 0; j < 10_000; j++) {
+            counter.increment();
+          }
+          finish.countDown();
+        }
+        catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
+
+      }).start();
+    }
+    for (int awaitCount : new int[] {0, 1, 100, 10_000, 20_000}) {
+      new Thread(() -> {
+        try {
+          start.await();
+          counter.awaitCount(awaitCount);
+          finish.countDown();
+        }
+        catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
+      }).start();
+    }
+    start.countDown();
+    finish.await();
+  }
+}
