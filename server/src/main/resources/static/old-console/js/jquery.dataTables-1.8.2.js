19e8b8e552aa64973fdbf9808f592bde9351d9a2
Slim <slim.bouguerra@gmail.com>
2018-04-24 11:06:30 -0700

    Adding licenses and enable apache-rat-plugi.
    Change-Id: I4685a2d9f1e147855dba69329b286f2d5bee3c18

--- server/src/main/resources/static/old-console/js/jquery.dataTables-1.8.2.js
+++ server/src/main/resources/static/old-console/js/jquery.dataTables-1.8.2.js
@@ -1,24 +1,19 @@
 /*
- * File:        jquery.dataTables.js
- * Version:     1.8.2
- * Description: Paginate, search and sort HTML tables
- * Author:      Allan Jardine (www.sprymedia.co.uk)
- * Created:     28/3/2008
- * Language:    Javascript
- * License:     GPL v2 or BSD 3 point style
- * Project:     Mtaala
- * Contact:     allan.jardine@sprymedia.co.uk
- * 
- * Copyright 2008-2011 Allan Jardine, all rights reserved.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * This source file is free software, under either the GPL v2 license or a
- * BSD style license, as supplied with this software.
- * 
- * This source file is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the license files for details.
- * 
- * For details please refer to: http://www.datatables.net
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 /*
