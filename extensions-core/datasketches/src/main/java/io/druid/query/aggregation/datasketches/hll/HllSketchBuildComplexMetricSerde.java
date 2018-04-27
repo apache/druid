03b4baefaa81a51f15618a6645f85b197c428c7f
saydakov <saydakov@yahoo-inc.com>
2018-04-27 14:20:20 -0700

    HllSketch module

---
+++ extensions-core/datasketches/src/main/java/io/druid/query/aggregation/datasketches/hll/HllSketchBuildComplexMetricSerde.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.druid.query.aggregation.datasketches.hll;
+
+import java.nio.ByteBuffer;
+
+import io.druid.data.input.InputRow;
+import io.druid.segment.column.ColumnBuilder;
+import io.druid.segment.data.GenericIndexed;
+import io.druid.segment.data.ObjectStrategy;
+import io.druid.segment.serde.ComplexColumnPartSupplier;
+import io.druid.segment.serde.ComplexMetricExtractor;
+import io.druid.segment.serde.ComplexMetricSerde;
+
+public class HllSketchBuildComplexMetricSerde extends ComplexMetricSerde
+{
+
+  @Override
+  public String getTypeName()
+  {
+    return HllSketchModule.TYPE_NAME; // must be common type name
+  }
+
+  @Override
+  public ObjectStrategy getObjectStrategy()
+  {
+    return HllSketchObjectStrategy.STRATEGY;
+  }
+
+  @Override
+  public ComplexMetricExtractor getExtractor()
+  {
+    return new ComplexMetricExtractor()
+    {
+      @Override
+      public Class<?> extractedClass()
+      {
+        return Object.class;
+      }
+
+      @Override
+      public Object extractValue(final InputRow inputRow, final String metricName)
+      {
+        return inputRow.getRaw(metricName);
+      }
+    };
+  }
+
+  @Override
+  public void deserializeColumn(final ByteBuffer buf, final ColumnBuilder columnBuilder)
+  {
+    columnBuilder.setComplexColumn(new ComplexColumnPartSupplier(getTypeName(),
+        GenericIndexed.read(buf, HllSketchObjectStrategy.STRATEGY)));
+  }
+
+}
