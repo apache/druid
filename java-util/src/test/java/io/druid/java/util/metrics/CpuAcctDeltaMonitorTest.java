6fa59acbac68a7b6985052b6c07af3084ade2040
Charles Allen <callen@snapchat.com>
2018-06-09 12:34:29 -0700

    Add cgroup memory monitor
    * Port of https://github.com/metamx/java-util/pull/67

--- java-util/src/test/java/io/druid/java/util/metrics/CpuAcctDeltaMonitorTest.java
+++ java-util/src/test/java/io/druid/java/util/metrics/CpuAcctDeltaMonitorTest.java
@@ -1,20 +1,17 @@
 /*
- * Licensed to Metamarkets Group Inc. (Metamarkets) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. Metamarkets licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Copyright 2017 Metamarkets Group Inc.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package io.druid.java.util.metrics;
@@ -51,7 +48,7 @@ public class CpuAcctDeltaMonitorTest
     TestUtils.setUpCgroups(procDir, cgroupDir);
     cpuacctDir = new File(
         cgroupDir,
-        "cpu,cpuacct/system.slice/mesos-agent-druid.service/f12ba7e0-fa16-462e-bb9d-652ccc27f0ee"
+        "cpu,cpuacct/system.slice/some.service/f12ba7e0-fa16-462e-bb9d-652ccc27f0ee"
     );
     Assert.assertTrue((cpuacctDir.isDirectory() && cpuacctDir.exists()) || cpuacctDir.mkdirs());
     TestUtils.copyResource("/cpuacct.usage_all", new File(cpuacctDir, "cpuacct.usage_all"));
@@ -81,7 +78,7 @@ public class CpuAcctDeltaMonitorTest
     try (final FileOutputStream fos = new FileOutputStream(cpuacct)) {
       fos.write(StringUtils.toUtf8("cpu user system\n"));
       for (int i = 0; i < 128; ++i) {
-        fos.write(StringUtils.toUtf8(StringUtils.format("%d 0 0\n", i)));
+        fos.write(StringUtils.toUtf8(String.format("%d 0 0\n", i)));
       }
     }
     final CpuAcctDeltaMonitor monitor = new CpuAcctDeltaMonitor(
