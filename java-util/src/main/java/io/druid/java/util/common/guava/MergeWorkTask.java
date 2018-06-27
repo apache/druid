e86227b55e4c433259a142579558670b4b465813
Charles Allen <callen@snapchat.com>
2018-06-26 17:53:42 -0700

    Make the returned merge sequence fetch intermediate results asap

--- java-util/src/main/java/io/druid/java/util/common/guava/MergeWorkTask.java
+++ java-util/src/main/java/io/druid/java/util/common/guava/MergeWorkTask.java
@@ -24,9 +24,13 @@ import com.google.common.collect.Ordering;
 
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Spliterator;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinTask;
 import java.util.function.Consumer;
@@ -37,10 +41,11 @@ public class MergeWorkTask<T> extends ForkJoinTask<Sequence<T>>
 
   /**
    * Take a stream of sequences, split them as possible, and do intermediate merges. If the input stream is not
-   * a parallel stream, ignore it. The stream attempts to use groups of {@code batchSize} to do its work, but this
+   * a parallel stream, do a traditional merge. The stream attempts to use groups of {@code batchSize} to do its work, but this
    * goal is on a best effort basis. Input streams that cannot be split or are not sized or not subsized might not be
    * elligable for this parallelization. The intermediate merges are done in the passed in ForkJoinPool, but the final
-   * merge is still done when the returned sequence accumulated.
+   * merge is still done when the returned sequence accumulated. The intermediate merges are yielded in the order
+   * in which they are ready.
    *
    * @param ordering      The ordering to pass into MergeSequence
    * @param baseSequences The sequences that need merged
@@ -64,6 +69,7 @@ public class MergeWorkTask<T> extends ForkJoinTask<Sequence<T>>
     @SuppressWarnings("unchecked") // Wildcard erasure is fine here
     final Spliterator<? extends Sequence<T>> baseSpliterator = (Spliterator<? extends Sequence<T>>) baseSequences.spliterator();
 
+    // Accumulate a list of forked off tasks
     final List<ForkJoinTask<Sequence<T>>> tasks = new ArrayList<>();
     final Deque<Spliterator<? extends Sequence<T>>> spliteratorStack = new LinkedList<>();
 
@@ -87,7 +93,57 @@ public class MergeWorkTask<T> extends ForkJoinTask<Sequence<T>>
       spliteratorStack.push(pop);
       spliteratorStack.push(other);
     }
-    return new MergeSequence<>(ordering, Sequences.simple(tasks.stream().map(ForkJoinTask::join)));
+
+    final BlockingQueue<Sequence<T>> readyForFinalMerge = new ArrayBlockingQueue<>(tasks.size());
+    tasks.forEach(t -> fjp.submit(() -> {
+      readyForFinalMerge.add(t.join());
+    }));
+
+    final long totalAdditions = tasks.size();
+    return new MergeSequence<>(
+        ordering,
+        new BaseSequence<>(
+            new BaseSequence.IteratorMaker<Sequence<T>, Iterator<Sequence<T>>>()
+            {
+              @Override
+              public Iterator<Sequence<T>> make()
+              {
+                return new Iterator<Sequence<T>>()
+                {
+                  long taken = 0L;
+
+                  @Override
+                  public boolean hasNext()
+                  {
+                    return taken < totalAdditions;
+                  }
+
+                  @Override
+                  public Sequence<T> next() throws NoSuchElementException
+                  {
+                    if (taken >= totalAdditions) {
+                      throw new NoSuchElementException();
+                    }
+                    try {
+                      taken++;
+                      return readyForFinalMerge.take();
+                    }
+                    catch (InterruptedException e) {
+                      Thread.currentThread().interrupt();
+                      throw new RuntimeException("Interrupted waiting for intermediate merge", e);
+                    }
+                  }
+                };
+              }
+
+              @Override
+              public void cleanup(Iterator<Sequence<T>> iterFromMake)
+              {
+                tasks.forEach(t -> t.cancel(true));
+              }
+            }
+        )
+    );
   }
 
   private final Ordering<? super T> ordering;
