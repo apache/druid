355eb7200d9c16543d877d1b63ec0d1d50cb60f8
Surekha Saharan <surekha.saharan@imply.io>
2018-07-06 13:43:00 -0700

    Remove unused import statement

--- processing/src/main/java/io/druid/segment/VirtualColumns.java
+++ processing/src/main/java/io/druid/segment/VirtualColumns.java
@@ -25,7 +25,6 @@ import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import io.druid.java.util.common.Cacheable;
