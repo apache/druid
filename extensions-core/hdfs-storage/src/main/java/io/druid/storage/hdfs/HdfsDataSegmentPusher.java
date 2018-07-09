0b7bf6859eea9f0eb78fbcb6e42a019ace1586c5
Jihoon Son <jihoonson@apache.org>
2018-07-09 13:24:53 -0700

    HdfsDataSegmentPusher: Close tmpIndexFile before copying it. (#5873) (#5968)
    It seems that copy-before-close works OK on HDFS, but it doesn't work
    on all filesystems. In particular, we observed this not working properly
    with Google Cloud Storage. And anyway, it's better hygiene to close files
    before attempting to copy them somewhere else.

--- extensions-core/hdfs-storage/src/main/java/io/druid/storage/hdfs/HdfsDataSegmentPusher.java
+++ extensions-core/hdfs-storage/src/main/java/io/druid/storage/hdfs/HdfsDataSegmentPusher.java
@@ -114,8 +114,11 @@ public class HdfsDataSegmentPusher implements DataSegmentPusher
 
     final long size;
     final DataSegment dataSegment;
-    try (FSDataOutputStream out = fs.create(tmpIndexFile)) {
-      size = CompressionUtils.zip(inDir, out);
+    try {
+      try (FSDataOutputStream out = fs.create(tmpIndexFile)) {
+        size = CompressionUtils.zip(inDir, out);
+      }
+
       final String uniquePrefix = useUniquePath ? DataSegmentPusher.generateUniquePath() + "_" : "";
       final Path outIndexFile = new Path(StringUtils.format(
           "%s/%s/%d_%sindex.zip",
