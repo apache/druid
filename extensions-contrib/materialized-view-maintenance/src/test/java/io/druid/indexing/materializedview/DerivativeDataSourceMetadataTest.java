3627805274356e2cc3c96053b935581da649ebea
Chengcheng Pei <cpei@ebay.com>
2018-07-30 16:48:53 -0400

    added another test for validating null baseDataSource

--- extensions-contrib/materialized-view-maintenance/src/test/java/io/druid/indexing/materializedview/DerivativeDataSourceMetadataTest.java
+++ extensions-contrib/materialized-view-maintenance/src/test/java/io/druid/indexing/materializedview/DerivativeDataSourceMetadataTest.java
@@ -38,9 +38,22 @@ public class DerivativeDataSourceMetadataTest
   {
     expectedException.expect(CoreMatchers.instanceOf(IllegalArgumentException.class));
     expectedException.expectMessage(
-            "baseDataSource cannot be null or empty. Please provide a baseDataSource."
+        "baseDataSource cannot be null or empty. Please provide a baseDataSource."
     );
     String baseDataSource = "";
+    Set<String> dims = Sets.newHashSet("dim1", "dim2", "dim3");
+    Set<String> metrics = Sets.newHashSet("cost");
+    DerivativeDataSourceMetadata metadata = new DerivativeDataSourceMetadata(baseDataSource, dims, metrics);
+  }
+
+  @Test
+  public void testNullBaseDataSource() throws Exception
+  {
+    expectedException.expect(CoreMatchers.instanceOf(IllegalArgumentException.class));
+    expectedException.expectMessage(
+        "baseDataSource cannot be null or empty. Please provide a baseDataSource."
+    );
+    String baseDataSource = null;
     Set<String> dims = Sets.newHashSet("dim1", "dim2", "dim3");
     Set<String> metrics = Sets.newHashSet("cost");
     DerivativeDataSourceMetadata metadata = new DerivativeDataSourceMetadata(baseDataSource, dims, metrics);
