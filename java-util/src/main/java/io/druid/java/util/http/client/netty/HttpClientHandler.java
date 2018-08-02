acad9d084e3280bdc58507862679383bf8c6042b
asdf2014 <benedictjin2016@gmail.com>
2018-08-02 16:28:09 +0800

    Add more checks for channel

--- java-util/src/main/java/io/druid/java/util/http/client/netty/HttpClientHandler.java
+++ java-util/src/main/java/io/druid/java/util/http/client/netty/HttpClientHandler.java
@@ -20,6 +20,7 @@
 package io.druid.java.util.http.client.netty;
 
 import io.druid.java.util.common.logger.Logger;
+import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelHandler;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.ExceptionEvent;
@@ -35,14 +36,18 @@ public class HttpClientHandler extends SimpleChannelUpstreamHandler
   @Override
   public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e)
   {
-    try {
-      final Throwable cause = e.getCause();
-      if (cause != null) {
-        LOGGER.error(cause, "Caught exception from [%s] channel handler", ctx.getName());
-      }
+    final Channel channel = ctx.getChannel();
+    if (channel == null) {
+      // For the case where this pipeline is not attached yet.
+      LOGGER.error("This channel is null. Channel handler is [%s]", ctx.getName());
+      return;
     }
-    finally {
-      ctx.getChannel().close();
+    final Throwable cause = e.getCause();
+    if (!channel.isOpen()) {
+      LOGGER.warn(cause, "Exception occurred with closed channel. Channel handler is [%s]", ctx.getName());
+    } else {
+      LOGGER.error(cause, "Caught exception from [%s] channel handler. Now, closing it...", ctx.getName());
+      channel.close();
     }
   }
 }
