34be2fc54489ad324adaad011ab7c1814b75f613
saydakov <saydakov@yahoo-inc.com>
2018-09-14 15:03:42 -0700

    updated license and imports

--- extensions-core/datasketches/src/main/java/io/druid/query/aggregation/datasketches/hll/HllSketchBuildComplexMetricSerde.java
+++ extensions-core/datasketches/src/main/java/io/druid/query/aggregation/datasketches/hll/HllSketchBuildComplexMetricSerde.java
@@ -1,18 +1,18 @@
 /*
- * Licensed to Metamarkets Group Inc. (Metamarkets) under one
- * or more contributor license agreements. See the NOTICE file
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership. Metamarkets licenses this file
+ * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
+ * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
@@ -21,13 +21,13 @@ package io.druid.query.aggregation.datasketches.hll;
 
 import java.nio.ByteBuffer;
 
-import io.druid.data.input.InputRow;
-import io.druid.segment.column.ColumnBuilder;
-import io.druid.segment.data.GenericIndexed;
-import io.druid.segment.data.ObjectStrategy;
-import io.druid.segment.serde.ComplexColumnPartSupplier;
-import io.druid.segment.serde.ComplexMetricExtractor;
-import io.druid.segment.serde.ComplexMetricSerde;
+import org.apache.druid.data.input.InputRow;
+import org.apache.druid.segment.column.ColumnBuilder;
+import org.apache.druid.segment.data.GenericIndexed;
+import org.apache.druid.segment.data.ObjectStrategy;
+import org.apache.druid.segment.serde.ComplexColumnPartSupplier;
+import org.apache.druid.segment.serde.ComplexMetricExtractor;
+import org.apache.druid.segment.serde.ComplexMetricSerde;
 
 public class HllSketchBuildComplexMetricSerde extends ComplexMetricSerde
 {
--- extensions-core/datasketches/src/test/java/io/druid/query/aggregation/datasketches/hll/HllSketchAggregatorTest.java
+++ extensions-core/datasketches/src/test/java/io/druid/query/aggregation/datasketches/hll/HllSketchAggregatorTest.java
@@ -1,18 +1,18 @@
 /*
- * Licensed to Metamarkets Group Inc. (Metamarkets) under one
- * or more contributor license agreements. See the NOTICE file
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership. Metamarkets licenses this file
+ * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
+ * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
@@ -32,13 +32,13 @@ import org.junit.runners.Parameterized;
 
 import com.google.common.collect.Lists;
 
-import io.druid.data.input.Row;
-import io.druid.initialization.DruidModule;
-import io.druid.java.util.common.granularity.Granularities;
-import io.druid.java.util.common.guava.Sequence;
-import io.druid.query.aggregation.AggregationTestHelper;
-import io.druid.query.groupby.GroupByQueryConfig;
-import io.druid.query.groupby.GroupByQueryRunnerTest;
+import org.apache.druid.data.input.Row;
+import org.apache.druid.initialization.DruidModule;
+import org.apache.druid.java.util.common.granularity.Granularities;
+import org.apache.druid.java.util.common.guava.Sequence;
+import org.apache.druid.query.aggregation.AggregationTestHelper;
+import org.apache.druid.query.groupby.GroupByQueryConfig;
+import org.apache.druid.query.groupby.GroupByQueryRunnerTest;
 
 @RunWith(Parameterized.class)
 public class HllSketchAggregatorTest
