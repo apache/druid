b584bf577058ea5336b46fdfc70bc8773b3164ee
Surekha Saharan <surekha.saharan@imply.io>
2018-10-20 15:16:31 -0700

    Remove unused import from test

--- extensions-core/kafka-indexing-service/src/test/java/org/apache/druid/indexing/kafka/KafkaIndexTaskTest.java
+++ extensions-core/kafka-indexing-service/src/test/java/org/apache/druid/indexing/kafka/KafkaIndexTaskTest.java
@@ -184,7 +184,6 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.apache.druid.query.QueryPlus.wrap;
 
