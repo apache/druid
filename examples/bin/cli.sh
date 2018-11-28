de671f1cec62068612683f44868bb15ad287fc01
Ruslan Dautkhanov <Tagar@users.noreply.github.com>
2018-11-28 00:33:13 -0700

    Delete cli.sh

--- examples/bin/cli.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-java -Xmx256m -Duser.timezone=UTC -Dfile.encoding=UTF-8 \
-        -cp lib/*:config/_common:config/broker \
-        io.druid.cli.Main $* \
-                | grep -v " INFO \[main\] io.druid.initialization.Initialization "
-
