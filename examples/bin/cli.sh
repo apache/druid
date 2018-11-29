2964b810707eb1a6a78eb7fda629842837ea7c23
Ruslan Dautkhanov <Tagar@users.noreply.github.com>
2018-11-28 17:51:25 -0700

    remove broker from cli.sh's cp

--- examples/bin/cli.sh
+++ examples/bin/cli.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 java -Xmx256m -Duser.timezone=UTC -Dfile.encoding=UTF-8 \
-        -cp lib/*:config/_common:config/broker \
+        -cp lib/*:config/_common \
         io.druid.cli.Main $* \
                 | grep -v " INFO \[main\] io.druid.initialization.Initialization "
 
