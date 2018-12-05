97c17b053b780d154f60d8b6747f920d21034120
Jihoon Son <jihoonson@apache.org>
2018-12-05 10:14:47 -0800

    Fix checkstyle

--- server/src/test/java/org/apache/druid/client/TestQueryRunner.java
+++ server/src/test/java/org/apache/druid/client/TestQueryRunner.java
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.apache.druid.client;
 
 import org.apache.druid.java.util.common.guava.Sequence;
