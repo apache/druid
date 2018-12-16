c713116a75fb13acdeaddf9e02b3e62f12ebda7d
Jonathan Wei <jon-wei@users.noreply.github.com>
2018-12-16 15:18:09 -0800

    Use @Coordinator leader client in CoordinatorRuleManager (#6729)

--- services/src/main/java/org/apache/druid/cli/CliRouter.java
+++ services/src/main/java/org/apache/druid/cli/CliRouter.java
@@ -23,15 +23,10 @@ import com.google.common.collect.ImmutableList;
 import com.google.inject.Binder;
 import com.google.inject.Key;
 import com.google.inject.Module;
-import com.google.inject.Provides;
 import com.google.inject.TypeLiteral;
 import com.google.inject.name.Names;
 import io.airlift.airline.Command;
 import org.apache.druid.curator.discovery.DiscoveryModule;
-import org.apache.druid.curator.discovery.ServerDiscoveryFactory;
-import org.apache.druid.curator.discovery.ServerDiscoverySelector;
-import org.apache.druid.discovery.DruidLeaderClient;
-import org.apache.druid.discovery.DruidNodeDiscoveryProvider;
 import org.apache.druid.discovery.NodeType;
 import org.apache.druid.guice.Jerseys;
 import org.apache.druid.guice.JsonConfigProvider;
@@ -41,11 +36,9 @@ import org.apache.druid.guice.ManageLifecycle;
 import org.apache.druid.guice.QueryRunnerFactoryModule;
 import org.apache.druid.guice.QueryableModule;
 import org.apache.druid.guice.RouterProcessingModule;
-import org.apache.druid.guice.annotations.EscalatedGlobal;
 import org.apache.druid.guice.annotations.Self;
 import org.apache.druid.guice.http.JettyHttpClientModule;
 import org.apache.druid.java.util.common.logger.Logger;
-import org.apache.druid.java.util.http.client.HttpClient;
 import org.apache.druid.query.lookup.LookupModule;
 import org.apache.druid.server.AsyncQueryForwardingServlet;
 import org.apache.druid.server.http.RouterResource;
@@ -126,33 +119,6 @@ public class CliRouter extends ServerRunnable
                 .toProvider(new DiscoverySideEffectsProvider(NodeType.ROUTER, ImmutableList.of()))
                 .in(LazySingleton.class);
             LifecycleModule.registerKey(binder, Key.get(DiscoverySideEffectsProvider.Child.class));
-          }
-
-          @Provides
-          @ManageLifecycle
-          public ServerDiscoverySelector getCoordinatorServerDiscoverySelector(
-              TieredBrokerConfig config,
-              ServerDiscoveryFactory factory
-          )
-          {
-            return factory.createSelector(config.getCoordinatorServiceName());
-          }
-
-          @Provides
-          @ManageLifecycle
-          public DruidLeaderClient getLeaderHttpClient(
-              @EscalatedGlobal HttpClient httpClient,
-              DruidNodeDiscoveryProvider druidNodeDiscoveryProvider,
-              ServerDiscoverySelector serverDiscoverySelector
-          )
-          {
-            return new DruidLeaderClient(
-                httpClient,
-                druidNodeDiscoveryProvider,
-                NodeType.COORDINATOR,
-                "/druid/coordinator/v1/leader",
-                serverDiscoverySelector
-            );
           }
         },
         new LookupModule()
