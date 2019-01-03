3a3280f8b1be459f77fb9bd7fd55a520d9706de3
Jihoon Son <jihoonson@apache.org>
2019-01-02 18:46:07 -0800

    add comment

--- core/src/main/java/org/apache/druid/java/util/common/parsers/DelimitedParser.java
+++ core/src/main/java/org/apache/druid/java/util/common/parsers/DelimitedParser.java
@@ -82,7 +82,9 @@ public class DelimitedParser extends AbstractFlatTextFormatParser
 
   /**
    * Copied from Guava's {@link Splitter#splitToList(CharSequence)}.
-   * This is to avoid the missing method signature error when using an old Guava library.
+   * This is to avoid the error of the missing method signature when using an old Guava library.
+   * For example, it may happen when running Druid Hadoop indexing jobs, since we may inherit the version provided by
+   * the Hadoop cluster. See https://github.com/apache/incubator-druid/issues/6801.
    */
   private List<String> splitToList(String input)
   {
