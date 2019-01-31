0fa68610eb76b427f92d3b52c5ed0f7ca91a1178
Surekha Saharan <surekha.saharan@imply.io>
2019-01-31 12:05:58 -0800

    unused import

--- sql/src/test/java/org/apache/druid/sql/calcite/schema/DruidSchemaTest.java
+++ sql/src/test/java/org/apache/druid/sql/calcite/schema/DruidSchemaTest.java
@@ -63,7 +63,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 public class DruidSchemaTest extends CalciteTestBase
