60b7684db725387b4d843385d9c61d50f2ed6744
Justin Borromeo <jborrome@edu.uwaterloo.ca>
2019-02-06 13:02:13 -0800

    Committing a param change to kick teamcity

--- benchmarks/src/main/java/org/apache/druid/benchmark/query/ScanBenchmark.java
+++ benchmarks/src/main/java/org/apache/druid/benchmark/query/ScanBenchmark.java
@@ -104,7 +104,7 @@ public class ScanBenchmark
   @Param({"2"})
   private int numProcessingThreads;
 
-  @Param({"250000"})
+  @Param({"200000"})
   private int rowsPerSegment;
 
   @Param({"basic.A"})
