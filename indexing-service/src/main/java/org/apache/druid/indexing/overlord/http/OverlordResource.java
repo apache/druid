11aa5aa3be86c84002ec4b26f000e0eeca404bf9
Clint Wylie <cjwylie@gmail.com>
2019-02-12 19:06:46 -0800

    this will fix it

--- indexing-service/src/main/java/org/apache/druid/indexing/overlord/http/OverlordResource.java
+++ indexing-service/src/main/java/org/apache/druid/indexing/overlord/http/OverlordResource.java
@@ -101,7 +101,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 /**
