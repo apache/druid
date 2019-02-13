2f4f6144ffc624a612289b89ac110217f6348c8f
Clint Wylie <cjwylie@gmail.com>
2019-02-12 18:42:52 -0800

    fix it

--- core/src/main/java/org/apache/druid/indexer/TaskStatusPlus.java
+++ core/src/main/java/org/apache/druid/indexer/TaskStatusPlus.java
@@ -23,7 +23,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Preconditions;
 import org.apache.druid.java.util.common.RE;
-import org.apache.druid.java.util.common.logger.Logger;
 import org.joda.time.DateTime;
 
 import javax.annotation.Nullable;
