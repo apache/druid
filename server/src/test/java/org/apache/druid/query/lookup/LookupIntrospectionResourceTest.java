49aa19a81f44d091ad06fe66bef236cded62354a
Clint Wylie <cjwylie@gmail.com>
2019-03-10 04:17:24 -0700

    fix and combine LookupIntrospectionResourceTest

--- server/src/test/java/org/apache/druid/query/lookup/LookupIntrospectionResourceTest.java
+++ server/src/test/java/org/apache/druid/query/lookup/LookupIntrospectionResourceTest.java
@@ -20,48 +20,95 @@
 package org.apache.druid.query.lookup;
 
 import com.google.common.collect.ImmutableMap;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
 import org.apache.druid.query.extraction.MapLookupExtractor;
+import org.apache.druid.server.WebserverTestUtils;
 import org.easymock.EasyMock;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.URI;
 
-/**
- * Test of actual introspection functionality available in {@link LookupIntrospectionResourceImplTest}
- */
 public class LookupIntrospectionResourceTest
 {
-  private LookupExtractorFactoryContainerProvider lookupExtractorFactoryContainerProvider =
-      EasyMock.createMock(LookupExtractorFactoryContainerProvider.class);
-  private LookupExtractorFactory lookupExtractorFactory = EasyMock.createMock(LookupExtractorFactory.class);
-  private LookupIntrospectHandler lookupIntrospectHandler = EasyMock.createMock(LookupIntrospectHandler.class);
+  private static LookupExtractorFactory mockLookupExtractorFactory = EasyMock.createMock(LookupExtractorFactory.class);
 
-  private LookupIntrospectionResource lookupIntrospectionResource = new LookupIntrospectionResource(
-      lookupExtractorFactoryContainerProvider);
+  private static LookupExtractorFactoryContainerProvider mockLookupExtractorFactoryContainerProvider =
+      EasyMock.createMock(LookupExtractorFactoryContainerProvider.class);
+
+  private static LookupIntrospectHandler mockLookupIntrospectHandler =
+      EasyMock.createMock(LookupIntrospectHandler.class);
+
+  private LookupIntrospectionResource lookupIntrospectionResource =
+      new LookupIntrospectionResource(mockLookupExtractorFactoryContainerProvider);
+
+  private URI baseUri;
+  private HttpServer server;
 
   @Before
-  public void setUp()
+  public void setup() throws Exception
   {
-    EasyMock.expect(lookupExtractorFactoryContainerProvider.get("lookupId")).andReturn(
+    LookupExtractorFactory actualLookupExtractorFactory = new MapLookupExtractorFactory(
+        ImmutableMap.of("key", "value", "key2", "value2"),
+        false
+    );
+
+    EasyMock.reset(mockLookupExtractorFactoryContainerProvider);
+    EasyMock.reset(mockLookupExtractorFactory);
+    EasyMock.reset(mockLookupIntrospectHandler);
+    EasyMock.expect(mockLookupExtractorFactoryContainerProvider.get("lookupId")).andReturn(
         new LookupExtractorFactoryContainer(
             "v0",
-            lookupExtractorFactory
+            mockLookupExtractorFactory
         )
     ).anyTimes();
-    EasyMock.expect(lookupExtractorFactoryContainerProvider.get(EasyMock.anyString())).andReturn(null).anyTimes();
-    EasyMock.replay(lookupExtractorFactoryContainerProvider);
+    EasyMock.expect(mockLookupExtractorFactoryContainerProvider.get("lookupId1")).andReturn(
+        new LookupExtractorFactoryContainer(
+            "v0",
+            actualLookupExtractorFactory
+        )
+    ).anyTimes();
+
+    EasyMock.expect(mockLookupExtractorFactoryContainerProvider.get(EasyMock.anyString())).andReturn(null).anyTimes();
+    EasyMock.replay(mockLookupExtractorFactoryContainerProvider);
+
+    baseUri = WebserverTestUtils.createBaseUri();
+    server = WebserverTestUtils.createServer(
+        "lookup-test",
+        baseUri,
+        LookupIntrospectionResource.class.getName(),
+        binder -> {
+          binder.bind(LookupExtractorFactoryContainerProvider.class).toInstance(
+              mockLookupExtractorFactoryContainerProvider);
+        }
+    );
+    server.start();
+  }
+
+  @After
+  public void teardown()
+  {
+    if (server != null) {
+      server.stop();
+    }
   }
 
   @Test
   public void testNotImplementedIntrospectLookup()
   {
-    EasyMock.expect(lookupExtractorFactory.getIntrospectHandler()).andReturn(null);
-    EasyMock.expect(lookupExtractorFactory.get())
+    EasyMock.expect(mockLookupExtractorFactory.getIntrospectHandler()).andReturn(null);
+    EasyMock.expect(mockLookupExtractorFactory.get())
             .andReturn(new MapLookupExtractor(ImmutableMap.of(), false))
             .anyTimes();
-    EasyMock.replay(lookupExtractorFactory);
+    EasyMock.replay(mockLookupExtractorFactory);
     Assert.assertEquals(
         Response.status(Response.Status.NOT_FOUND).build().getStatus(),
         ((Response) lookupIntrospectionResource.introspectLookup("lookupId")).getStatus()
@@ -79,11 +126,53 @@ public class LookupIntrospectionResourceTest
 
   @Test public void testExistingLookup()
   {
-    EasyMock.expect(lookupExtractorFactory.getIntrospectHandler()).andReturn(lookupIntrospectHandler);
-    EasyMock.expect(lookupExtractorFactory.get())
+    EasyMock.expect(mockLookupExtractorFactory.getIntrospectHandler()).andReturn(mockLookupIntrospectHandler);
+    EasyMock.expect(mockLookupExtractorFactory.get())
             .andReturn(new MapLookupExtractor(ImmutableMap.of(), false))
             .anyTimes();
-    EasyMock.replay(lookupExtractorFactory);
-    Assert.assertEquals(lookupIntrospectHandler, lookupIntrospectionResource.introspectLookup("lookupId"));
+    EasyMock.replay(mockLookupExtractorFactory);
+    Assert.assertEquals(mockLookupIntrospectHandler, lookupIntrospectionResource.introspectLookup("lookupId"));
+  }
+
+  @Test
+  public void testGetKey()
+  {
+    Client client = Client.create(new DefaultClientConfig());
+    WebResource service = client.resource(baseUri);
+
+    ClientResponse resp = service.path("/druid/v1/lookups/introspect/lookupId1/keys")
+                                 .accept(MediaType.APPLICATION_JSON)
+                                 .get(ClientResponse.class);
+    String s = resp.getEntity(String.class);
+    Assert.assertEquals("[key, key2]", s);
+    Assert.assertEquals(200, resp.getStatus());
+  }
+
+  @Test
+  public void testGetValue()
+  {
+    Client client = Client.create(new DefaultClientConfig());
+    WebResource service = client.resource(baseUri);
+
+    ClientResponse resp = service.path("/druid/v1/lookups/introspect/lookupId1/values")
+                                 .accept(MediaType.APPLICATION_JSON)
+                                 .get(ClientResponse.class);
+    String s = resp.getEntity(String.class);
+    Assert.assertEquals("[value, value2]", s);
+    Assert.assertEquals(200, resp.getStatus());
+  }
+
+  @Test
+  public void testGetMap()
+  {
+    Client client = Client.create(new DefaultClientConfig());
+    WebResource service = client.resource(baseUri);
+
+    ClientResponse resp = service.path("/druid/v1/lookups/introspect/lookupId1/")
+                                 .accept(MediaType.APPLICATION_JSON)
+                                 .get(ClientResponse.class);
+    String s = resp.getEntity(String.class);
+    Assert.assertEquals("{\"key\":\"value\",\"key2\":\"value2\"}", s);
+    Assert.assertEquals(200, resp.getStatus());
   }
 }
