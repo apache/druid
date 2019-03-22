b903dee54e4cb72179aff9870d422ed4b2e610dd
Clint Wylie <cjwylie@gmail.com>
2019-03-21 17:12:57 -0700

    fix Supervisor terminate call (#7315) (#7317)
    * fix Supervisor terminate call
    
    * fix refresh after submitting supervisor or task

--- web-console/src/views/tasks-view.tsx
+++ web-console/src/views/tasks-view.tsx
@@ -152,7 +152,7 @@ ORDER BY "rank" DESC, "created_time" DESC`);
     this.taskQueryManager.terminate();
   }
 
-  private async submitSupervisor(spec: JSON) {
+  private submitSupervisor = async (spec: JSON) => {
     try {
       await axios.post('/druid/indexer/v1/supervisor', spec);
     } catch (e) {
@@ -170,7 +170,7 @@ ORDER BY "rank" DESC, "created_time" DESC`);
     this.supervisorQueryManager.rerunLastQuery();
   }
 
-  private async submitTask(spec: JSON) {
+  private submitTask = async (spec: JSON) => {
     try {
       await axios.post('/druid/indexer/v1/task', spec);
     } catch (e) {
@@ -269,7 +269,7 @@ ORDER BY "rank" DESC, "created_time" DESC`);
     return <AsyncActionDialog
       action={
         terminateSupervisorId ? async () => {
-          const resp = await axios.post(`/druid/indexer/v1/supervisor/${terminateSupervisorId}/reset`, {});
+          const resp = await axios.post(`/druid/indexer/v1/supervisor/${terminateSupervisorId}/terminate`, {});
           return resp.data;
         } : null
       }
