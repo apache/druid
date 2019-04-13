85f10ed0d0df0e040a5448ce8b99a2c972537381
Justin Borromeo <jborrome@edu.uwaterloo.ca>
2019-04-12 19:08:34 -0700

    Support querying realtime segments using time-ordered scan queries and fix broken scan queries without time column (#7454)
    * Update scan query runner factory to accept SpecificSegmentSpec
    
    *  nit
    
    * Sorry travis
    
    * Improve logging and fix doc
    
    * Bug fix
    
    * Friendlier error msgs and tests to cover bug
    
    * Address Gian's comments
    
    * Fix doc
    
    * Added tests for empty and null column list
    
    * Style
    
    * Fix checking wrong order (looking at query param when it should be
    looking at the null-handled order)
    
    * Add test case for null order
    
    * Fix ScanQueryRunnerTest
    
    * Forbidden APIs fixed

--- processing/src/test/java/org/apache/druid/query/scan/ScanQueryRunnerFactoryTest.java
+++ processing/src/test/java/org/apache/druid/query/scan/ScanQueryRunnerFactoryTest.java
@@ -31,9 +31,15 @@ import org.apache.druid.query.QueryPlus;
 import org.apache.druid.query.QueryRunner;
 import org.apache.druid.query.QueryRunnerTestHelper;
 import org.apache.druid.query.SegmentDescriptor;
+import org.apache.druid.query.spec.LegacySegmentSpec;
+import org.apache.druid.query.spec.MultipleIntervalSegmentSpec;
+import org.apache.druid.query.spec.MultipleSpecificSegmentSpec;
+import org.apache.druid.query.spec.QuerySegmentSpec;
+import org.apache.druid.query.spec.SpecificSegmentSpec;
 import org.joda.time.Interval;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
@@ -43,12 +49,9 @@ import java.util.Collections;
 import java.util.List;
 
 
-@RunWith(Parameterized.class)
+@RunWith(Enclosed.class)
 public class ScanQueryRunnerFactoryTest
 {
-  private int numElements;
-  private ScanQuery query;
-  private ScanQuery.ResultFormat resultFormat;
 
   private static final ScanQueryRunnerFactory factory = new ScanQueryRunnerFactory(
       new ScanQueryQueryToolChest(
@@ -59,200 +62,261 @@ public class ScanQueryRunnerFactoryTest
       new ScanQueryConfig()
   );
 
-  public ScanQueryRunnerFactoryTest(
-      final int numElements,
-      final int batchSize,
-      final long limit,
-      final ScanQuery.ResultFormat resultFormat,
-      final ScanQuery.Order order
-  )
+  @RunWith(Parameterized.class)
+  public static class ScanQueryRunnerFactoryParameterizedTest
   {
-    this.numElements = numElements;
-    this.query = Druids.newScanQueryBuilder()
-                       .batchSize(batchSize)
-                       .limit(limit)
-                       .order(order)
-                       .intervals(QueryRunnerTestHelper.fullOnIntervalSpec)
-                       .dataSource("some datasource")
-                       .resultFormat(resultFormat)
-                       .build();
-    this.resultFormat = resultFormat;
-  }
+    private int numElements;
+    private ScanQuery query;
+    private ScanQuery.ResultFormat resultFormat;
 
-  @Parameterized.Parameters(name = "{0} {1} {2} {3} {4}")
-  public static Iterable<Object[]> constructorFeeder()
-  {
-    List<Integer> numsElements = ImmutableList.of(0, 10, 100);
-    List<Integer> batchSizes = ImmutableList.of(1, 100);
-    List<Long> limits = ImmutableList.of(3L, 1000L, Long.MAX_VALUE);
-    List<ScanQuery.ResultFormat> resultFormats = ImmutableList.of(
-        ScanQuery.ResultFormat.RESULT_FORMAT_LIST,
-        ScanQuery.ResultFormat.RESULT_FORMAT_COMPACTED_LIST
-    );
-    List<ScanQuery.Order> order = ImmutableList.of(
-        ScanQuery.Order.ASCENDING,
-        ScanQuery.Order.DESCENDING
-    );
-
-    return QueryRunnerTestHelper.cartesian(
-        numsElements,
-        batchSizes,
-        limits,
-        resultFormats,
-        order
-    );
-  }
-
-  @Test
-  public void testSortAndLimitScanResultValues()
-  {
-    List<ScanResultValue> srvs = new ArrayList<>(numElements);
-    List<Long> expectedEventTimestamps = new ArrayList<>();
-    for (int i = 0; i < numElements; i++) {
-      long timestamp = DateTimes.of("2015-01-01").plusHours(i).getMillis();
-      expectedEventTimestamps.add(timestamp);
-      srvs.add(ScanQueryTestHelper.generateScanResultValue(timestamp, resultFormat, 1));
+    public ScanQueryRunnerFactoryParameterizedTest(
+        final int numElements,
+        final int batchSize,
+        final long limit,
+        final ScanQuery.ResultFormat resultFormat,
+        final ScanQuery.Order order
+    )
+    {
+      this.numElements = numElements;
+      this.query = Druids.newScanQueryBuilder()
+                         .batchSize(batchSize)
+                         .limit(limit)
+                         .order(order)
+                         .intervals(QueryRunnerTestHelper.fullOnIntervalSpec)
+                         .dataSource("some datasource")
+                         .resultFormat(resultFormat)
+                         .build();
+      this.resultFormat = resultFormat;
     }
-    expectedEventTimestamps.sort((o1, o2) -> {
-      int retVal = 0;
-      if (o1 > o2) {
-        retVal = 1;
-      } else if (o1 < o2) {
-        retVal = -1;
+
+    @Parameterized.Parameters(name = "{0} {1} {2} {3} {4}")
+    public static Iterable<Object[]> constructorFeeder()
+    {
+      List<Integer> numsElements = ImmutableList.of(0, 10, 100);
+      List<Integer> batchSizes = ImmutableList.of(1, 100);
+      List<Long> limits = ImmutableList.of(3L, 1000L, Long.MAX_VALUE);
+      List<ScanQuery.ResultFormat> resultFormats = ImmutableList.of(
+          ScanQuery.ResultFormat.RESULT_FORMAT_LIST,
+          ScanQuery.ResultFormat.RESULT_FORMAT_COMPACTED_LIST
+      );
+      List<ScanQuery.Order> order = ImmutableList.of(
+          ScanQuery.Order.ASCENDING,
+          ScanQuery.Order.DESCENDING
+      );
+
+      return QueryRunnerTestHelper.cartesian(
+          numsElements,
+          batchSizes,
+          limits,
+          resultFormats,
+          order
+      );
+    }
+
+    @Test
+    public void testSortAndLimitScanResultValues()
+    {
+      List<ScanResultValue> srvs = new ArrayList<>(numElements);
+      List<Long> expectedEventTimestamps = new ArrayList<>();
+      for (int i = 0; i < numElements; i++) {
+        long timestamp = DateTimes.of("2015-01-01").plusHours(i).getMillis();
+        expectedEventTimestamps.add(timestamp);
+        srvs.add(ScanQueryTestHelper.generateScanResultValue(timestamp, resultFormat, 1));
       }
-      if (query.getOrder().equals(ScanQuery.Order.DESCENDING)) {
-        return retVal * -1;
+      expectedEventTimestamps.sort((o1, o2) -> {
+        int retVal = 0;
+        if (o1 > o2) {
+          retVal = 1;
+        } else if (o1 < o2) {
+          retVal = -1;
+        }
+        if (query.getOrder().equals(ScanQuery.Order.DESCENDING)) {
+          return retVal * -1;
+        }
+        return retVal;
+      });
+      Sequence<ScanResultValue> inputSequence = Sequences.simple(srvs);
+      try {
+        List<ScanResultValue> output = factory.priorityQueueSortAndLimit(
+            inputSequence,
+            query,
+            ImmutableList.of(new SegmentDescriptor(new Interval(
+                DateTimes.of("2010-01-01"),
+                DateTimes.of("2019-01-01").plusHours(1)
+            ), "1", 0))
+        ).toList();
+        if (query.getLimit() > Integer.MAX_VALUE) {
+          Assert.fail("Unsupported exception should have been thrown due to high limit");
+        }
+        validateSortedOutput(output, expectedEventTimestamps);
       }
-      return retVal;
-    });
-    Sequence<ScanResultValue> inputSequence = Sequences.simple(srvs);
-    try {
-      List<ScanResultValue> output = factory.priorityQueueSortAndLimit(
-          inputSequence,
-          query,
-          ImmutableList.of(new SegmentDescriptor(new Interval(
-              DateTimes.of("2010-01-01"),
-              DateTimes.of("2019-01-01").plusHours(1)
-          ), "1", 0))
-      ).toList();
-      if (query.getLimit() > Integer.MAX_VALUE) {
-        Assert.fail("Unsupported exception should have been thrown due to high limit");
+      catch (UOE e) {
+        if (query.getLimit() <= Integer.MAX_VALUE) {
+          Assert.fail("Unsupported operation exception should not have been thrown here");
+        }
       }
+    }
+
+    @Test
+    public void testNWayMerge()
+    {
+      List<Long> expectedEventTimestamps = new ArrayList<>(numElements * 3);
+
+      List<ScanResultValue> scanResultValues1 = new ArrayList<>(numElements);
+      for (int i = 0; i < numElements; i++) {
+        long timestamp = DateTimes.of("2015-01-01").plusMinutes(i * 2).getMillis();
+        expectedEventTimestamps.add(timestamp);
+        scanResultValues1.add(ScanQueryTestHelper.generateScanResultValue(timestamp, resultFormat, 1));
+      }
+
+      List<ScanResultValue> scanResultValues2 = new ArrayList<>(numElements);
+      for (int i = 0; i < numElements; i++) {
+        long timestamp = DateTimes.of("2015-01-01").plusMinutes(i * 2 + 1).getMillis();
+        expectedEventTimestamps.add(timestamp);
+        scanResultValues2.add(ScanQueryTestHelper.generateScanResultValue(timestamp, resultFormat, 1));
+      }
+
+      List<ScanResultValue> scanResultValues3 = new ArrayList<>(numElements);
+      for (int i = 0; i < numElements; i++) {
+        long timestamp = DateTimes.of("2015-01-02").plusMinutes(i).getMillis();
+        expectedEventTimestamps.add(timestamp);
+        scanResultValues3.add(ScanQueryTestHelper.generateScanResultValue(timestamp, resultFormat, 1));
+      }
+
+      if (query.getOrder() == ScanQuery.Order.DESCENDING) {
+        Collections.reverse(scanResultValues1);
+        Collections.reverse(scanResultValues2);
+        Collections.reverse(scanResultValues3);
+      }
+
+      QueryRunner<ScanResultValue> runnerSegment1Partition1 =
+          (queryPlus, responseContext) -> Sequences.simple(scanResultValues1);
+
+      QueryRunner<ScanResultValue> runnerSegment1Partition2 =
+          (queryPlus, responseContext) -> Sequences.simple(scanResultValues2);
+
+
+      QueryRunner<ScanResultValue> runnerSegment2Partition1 =
+          (queryPlus, responseContext) -> Sequences.simple(scanResultValues3);
+
+      QueryRunner<ScanResultValue> runnerSegment2Partition2 =
+          (queryPlus, responseContext) -> Sequences.empty();
+
+      List<List<QueryRunner<ScanResultValue>>> groupedRunners = new ArrayList<>(2);
+
+      if (query.getOrder() == ScanQuery.Order.DESCENDING) {
+        groupedRunners.add(Arrays.asList(runnerSegment2Partition1, runnerSegment2Partition2));
+        groupedRunners.add(Arrays.asList(runnerSegment1Partition1, runnerSegment1Partition2));
+      } else {
+        groupedRunners.add(Arrays.asList(runnerSegment1Partition1, runnerSegment1Partition2));
+        groupedRunners.add(Arrays.asList(runnerSegment2Partition1, runnerSegment2Partition2));
+      }
+
+      expectedEventTimestamps.sort((o1, o2) -> {
+        int retVal = 0;
+        if (o1 > o2) {
+          retVal = 1;
+        } else if (o1 < o2) {
+          retVal = -1;
+        }
+        if (query.getOrder().equals(ScanQuery.Order.DESCENDING)) {
+          return retVal * -1;
+        }
+        return retVal;
+      });
+
+      List<ScanResultValue> output =
+          factory.nWayMergeAndLimit(
+              groupedRunners,
+              QueryPlus.wrap(query),
+              ImmutableMap.of()
+          ).toList();
+
       validateSortedOutput(output, expectedEventTimestamps);
     }
-    catch (UOE e) {
-      if (query.getLimit() <= Integer.MAX_VALUE) {
-        Assert.fail("Unsupported operation exception should not have been thrown here");
+
+    private void validateSortedOutput(List<ScanResultValue> output, List<Long> expectedEventTimestamps)
+    {
+      // check each scan result value has one event
+      for (ScanResultValue srv : output) {
+        if (resultFormat.equals(ScanQuery.ResultFormat.RESULT_FORMAT_COMPACTED_LIST)) {
+          Assert.assertTrue(ScanQueryTestHelper.getEventsCompactedListResultFormat(srv).size() == 1);
+        } else if (resultFormat.equals(ScanQuery.ResultFormat.RESULT_FORMAT_LIST)) {
+          Assert.assertTrue(ScanQueryTestHelper.getEventsListResultFormat(srv).size() == 1);
+        }
+      }
+
+      // check total # of rows <= limit
+      Assert.assertTrue(output.size() <= query.getLimit());
+
+      // check ordering is correct
+      for (int i = 1; i < output.size(); i++) {
+        if (query.getOrder().equals(ScanQuery.Order.DESCENDING)) {
+          Assert.assertTrue(output.get(i).getFirstEventTimestamp(resultFormat) <
+                            output.get(i - 1).getFirstEventTimestamp(resultFormat));
+        } else {
+          Assert.assertTrue(output.get(i).getFirstEventTimestamp(resultFormat) >
+                            output.get(i - 1).getFirstEventTimestamp(resultFormat));
+        }
+      }
+
+      // check the values are correct
+      for (int i = 0; i < query.getLimit() && i < output.size(); i++) {
+        Assert.assertEquals((long) expectedEventTimestamps.get(i), output.get(i).getFirstEventTimestamp(resultFormat));
       }
     }
   }
 
-  @Test
-  public void testNWayMerge()
+  public static class ScanQueryRunnerFactoryNonParameterizedTest
   {
-    List<Long> expectedEventTimestamps = new ArrayList<>(numElements * 3);
+    private SegmentDescriptor descriptor = new SegmentDescriptor(new Interval(
+        DateTimes.of("2010-01-01"),
+        DateTimes.of("2019-01-01").plusHours(1)
+    ), "1", 0);
 
-    List<ScanResultValue> scanResultValues1 = new ArrayList<>(numElements);
-    for (int i = 0; i < numElements; i++) {
-      long timestamp = DateTimes.of("2015-01-01").plusMinutes(i * 2).getMillis();
-      expectedEventTimestamps.add(timestamp);
-      scanResultValues1.add(ScanQueryTestHelper.generateScanResultValue(timestamp, resultFormat, 1));
+    @Test
+    public void testGetValidSegmentDescriptorsFromSpec()
+    {
+      QuerySegmentSpec multiSpecificSpec = new MultipleSpecificSegmentSpec(
+          Collections.singletonList(
+              descriptor
+          )
+      );
+      QuerySegmentSpec singleSpecificSpec = new SpecificSegmentSpec(descriptor);
+
+      List<SegmentDescriptor> descriptors = factory.getSegmentDescriptorsFromSpecificQuerySpec(multiSpecificSpec);
+      Assert.assertEquals(1, descriptors.size());
+      Assert.assertEquals(descriptor, descriptors.get(0));
+
+      descriptors = factory.getSegmentDescriptorsFromSpecificQuerySpec(singleSpecificSpec);
+      Assert.assertEquals(1, descriptors.size());
+      Assert.assertEquals(descriptor, descriptors.get(0));
     }
 
-    List<ScanResultValue> scanResultValues2 = new ArrayList<>(numElements);
-    for (int i = 0; i < numElements; i++) {
-      long timestamp = DateTimes.of("2015-01-01").plusMinutes(i * 2 + 1).getMillis();
-      expectedEventTimestamps.add(timestamp);
-      scanResultValues2.add(ScanQueryTestHelper.generateScanResultValue(timestamp, resultFormat, 1));
+    @Test(expected = UOE.class)
+    public void testGetSegmentDescriptorsFromInvalidIntervalSpec()
+    {
+      QuerySegmentSpec multiIntervalSpec = new MultipleIntervalSegmentSpec(
+          Collections.singletonList(
+              new Interval(
+                  DateTimes.of("2010-01-01"),
+                  DateTimes.of("2019-01-01").plusHours(1)
+              )
+          )
+      );
+      factory.getSegmentDescriptorsFromSpecificQuerySpec(multiIntervalSpec);
     }
 
-    List<ScanResultValue> scanResultValues3 = new ArrayList<>(numElements);
-    for (int i = 0; i < numElements; i++) {
-      long timestamp = DateTimes.of("2015-01-02").plusMinutes(i).getMillis();
-      expectedEventTimestamps.add(timestamp);
-      scanResultValues3.add(ScanQueryTestHelper.generateScanResultValue(timestamp, resultFormat, 1));
-    }
-
-    if (query.getOrder() == ScanQuery.Order.DESCENDING) {
-      Collections.reverse(scanResultValues1);
-      Collections.reverse(scanResultValues2);
-      Collections.reverse(scanResultValues3);
-    }
-
-    QueryRunner<ScanResultValue> runnerSegment1Partition1 =
-        (queryPlus, responseContext) -> Sequences.simple(scanResultValues1);
-
-    QueryRunner<ScanResultValue> runnerSegment1Partition2 =
-        (queryPlus, responseContext) -> Sequences.simple(scanResultValues2);
-
-
-    QueryRunner<ScanResultValue> runnerSegment2Partition1 =
-        (queryPlus, responseContext) -> Sequences.simple(scanResultValues3);
-
-    QueryRunner<ScanResultValue> runnerSegment2Partition2 =
-        (queryPlus, responseContext) -> Sequences.empty();
-
-    List<List<QueryRunner<ScanResultValue>>> groupedRunners = new ArrayList<>(2);
-
-    if (query.getOrder() == ScanQuery.Order.DESCENDING) {
-      groupedRunners.add(Arrays.asList(runnerSegment2Partition1, runnerSegment2Partition2));
-      groupedRunners.add(Arrays.asList(runnerSegment1Partition1, runnerSegment1Partition2));
-    } else {
-      groupedRunners.add(Arrays.asList(runnerSegment1Partition1, runnerSegment1Partition2));
-      groupedRunners.add(Arrays.asList(runnerSegment2Partition1, runnerSegment2Partition2));
-    }
-
-    expectedEventTimestamps.sort((o1, o2) -> {
-      int retVal = 0;
-      if (o1 > o2) {
-        retVal = 1;
-      } else if (o1 < o2) {
-        retVal = -1;
-      }
-      if (query.getOrder().equals(ScanQuery.Order.DESCENDING)) {
-        return retVal * -1;
-      }
-      return retVal;
-    });
-
-    List<ScanResultValue> output =
-        factory.nWayMergeAndLimit(
-            groupedRunners,
-            QueryPlus.wrap(query),
-            ImmutableMap.of()
-        ).toList();
-
-    validateSortedOutput(output, expectedEventTimestamps);
-  }
-
-  private void validateSortedOutput(List<ScanResultValue> output, List<Long> expectedEventTimestamps)
-  {
-    // check each scan result value has one event
-    for (ScanResultValue srv : output) {
-      if (resultFormat.equals(ScanQuery.ResultFormat.RESULT_FORMAT_COMPACTED_LIST)) {
-        Assert.assertTrue(ScanQueryTestHelper.getEventsCompactedListResultFormat(srv).size() == 1);
-      } else if (resultFormat.equals(ScanQuery.ResultFormat.RESULT_FORMAT_LIST)) {
-        Assert.assertTrue(ScanQueryTestHelper.getEventsListResultFormat(srv).size() == 1);
-      }
-    }
-
-    // check total # of rows <= limit
-    Assert.assertTrue(output.size() <= query.getLimit());
-
-    // check ordering is correct
-    for (int i = 1; i < output.size(); i++) {
-      if (query.getOrder().equals(ScanQuery.Order.DESCENDING)) {
-        Assert.assertTrue(output.get(i).getFirstEventTimestamp(resultFormat) <
-                          output.get(i - 1).getFirstEventTimestamp(resultFormat));
-      } else {
-        Assert.assertTrue(output.get(i).getFirstEventTimestamp(resultFormat) >
-                          output.get(i - 1).getFirstEventTimestamp(resultFormat));
-      }
-    }
-
-    // check the values are correct
-    for (int i = 0; i < query.getLimit() && i < output.size(); i++) {
-      Assert.assertEquals((long) expectedEventTimestamps.get(i), output.get(i).getFirstEventTimestamp(resultFormat));
+    @Test(expected = UOE.class)
+    public void testGetSegmentDescriptorsFromInvalidLegacySpec()
+    {
+      QuerySegmentSpec legacySpec = new LegacySegmentSpec(
+          new Interval(
+              DateTimes.of("2010-01-01"),
+              DateTimes.of("2019-01-01").plusHours(1)
+          )
+      );
+      factory.getSegmentDescriptorsFromSpecificQuerySpec(legacySpec);
     }
   }
 }
