8e47e7dcebad2848c7a1cc522452e3ff493e782e
Lucas Capistrant <Lucas.Capistrant@target.com>
2019-04-15 14:28:46 -0500

    Add comment to clarify fully qualified references of Objects in SQLMetadataStorageActionHandler

--- server/src/main/java/org/apache/druid/metadata/SQLMetadataStorageActionHandler.java
+++ server/src/main/java/org/apache/druid/metadata/SQLMetadataStorageActionHandler.java
@@ -82,6 +82,7 @@ public abstract class SQLMetadataStorageActionHandler<EntryType, StatusType, Log
   )
   {
     this.connector = connector;
+    //fully qualified references required below due to identical package names across project modules.
     //noinspection UnnecessaryFullyQualifiedName
     this.jsonMapper = jsonMapper.copy().addMixIn(org.apache.druid.metadata.PasswordProvider.class,
             org.apache.druid.metadata.PasswordProviderRedactionMixIn.class);
