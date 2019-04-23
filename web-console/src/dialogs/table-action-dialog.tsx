7246c0b935e2600a8dd7a83fb850bd1ec482f169
shuqi7 <shuqi7sq@gmail.com>
2019-04-22 18:02:20 -0700

    Grouped actions in task/supervisor table to a dialog

---
+++ web-console/src/dialogs/table-action-dialog.tsx
@@ -0,0 +1,223 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Button, ButtonGroup, Dialog, Intent, TextArea} from '@blueprintjs/core';
+import axios from 'axios';
+import * as FileSaver from 'file-saver';
+import * as React from 'react';
+import * as CopyToClipboard from 'react-copy-to-clipboard';
+
+import {AppToaster} from '../singletons/toaster';
+import { getDruidErrorMessage } from '../utils';
+
+import './table-action-dialog.scss';
+
+interface TableActionDialogProps extends React.Props<any> {
+  onClose: () => void;
+  metaData: TableActionDialogMetaData;
+  terminateSupervisor: (id: string) => void;
+  resetSupervisor: (id: string) => void;
+  resumeSupervisor: (id: string) => void;
+  suspendSupervisor: (id: string) => void;
+  killTask: (id: string) => void;
+}
+
+interface TableActionDialogState {
+  jsonValue: string;
+  activeEndpoint: '' | 'status' | 'stats' | 'history' | 'reports' | 'log?offset=-8192' | 'log';
+}
+
+export interface TableActionDialogMetaData {
+  id: string | null;
+  mode: 'supervisor' | 'task' | null;
+  status: 'RUNNING' | 'WAITING' | 'PENDING' | 'SUCCESS' | 'FAILED' | null;
+  supervisorSuspended: boolean | null;
+}
+
+export class TableActionDialog extends React.Component<TableActionDialogProps, TableActionDialogState> {
+  constructor(props: TableActionDialogProps) {
+    super(props);
+    this.state = {
+      jsonValue: '',
+      activeEndpoint: ''
+    };
+  }
+
+  private getJsonInfo = async (endpoint: string) => {
+    const { mode, id } = this.props.metaData;
+    try {
+      const resp = await axios.get(`/druid/indexer/v1/${mode}/${id}/${endpoint}`);
+      const data = resp.data;
+      this.setState({
+        jsonValue: JSON.stringify(data, undefined, 2)
+      });
+    } catch (e) {
+      AppToaster.show({
+        message: getDruidErrorMessage(e),
+        intent: Intent.WARNING
+      });
+    }
+  }
+
+  private downloadJson = () => {
+    const { id, mode } = this.props.metaData;
+    const { jsonValue, activeEndpoint } = this.state;
+
+    const blob = new Blob([jsonValue], {
+      type: 'text/json'
+    });
+
+    FileSaver.saveAs(blob, `${mode}-${id}-${activeEndpoint === '' ? 'payload' : activeEndpoint}.json`);
+  }
+
+  render() {
+    const { onClose, terminateSupervisor, resetSupervisor, resumeSupervisor, suspendSupervisor } = this.props;
+    const { jsonValue, activeEndpoint } = this.state;
+    const { id, mode, status, supervisorSuspended } = this.props.metaData;
+
+    const supervisorTableInfoButtons = [
+      {icon: 'align-left', text: 'Payload', endpoint: ``},
+      {icon: 'dashboard', text: 'Status', endpoint: `status`},
+      {icon: 'chart', text: 'Statistics', endpoint: `stats`},
+      {icon: 'history', text: 'History', endpoint: `history`}
+    ];
+
+    const taskTableInfoButtons = [
+      {icon: 'align-left', text: 'Payload', endpoint: ``},
+      {icon: 'dashboard', text: 'Status', endpoint: `status`},
+      {icon: 'comparison', text: 'Reports', endpoint: `reports`},
+      {icon: 'align-justify', text: 'Logs', endpoint: `log?offset=-8192`}
+    ];
+
+    const tableInfoButtons = mode === 'supervisor' ? supervisorTableInfoButtons : taskTableInfoButtons;
+
+    const tableActionButtions = mode === 'supervisor'
+      ? [
+          <Button
+            key="terminate"
+            text={'Terminate'}
+            intent={Intent.DANGER}
+            onClick={() => id ? terminateSupervisor(id) : null}
+          />,
+          <Button
+            key="resumesuspend"
+            text={supervisorSuspended ? 'Resume' : 'Suspend'}
+            onClick={() => id ? (supervisorSuspended ? resumeSupervisor(id) : suspendSupervisor(id)) : null}
+          />,
+          <Button
+            key="reset"
+            text={'Reset'}
+            onClick={() => id ? resetSupervisor(id) : null}
+          />
+        ]
+      : <Button
+        text={'Kill'}
+        intent={Intent.DANGER}
+        onClick={() => id && (status === 'RUNNING' || status === 'WAITING' || status === 'PENDING') ? resetSupervisor(id) : null}
+      />;
+
+    return <Dialog
+      className={'table-action-dialog'}
+      isOpen
+      onClose={onClose}
+      onOpening={() => this.getJsonInfo(activeEndpoint)}
+    >
+      <div className={'side-bar'}>
+        {
+          tableInfoButtons.map((d: any) => {
+            return <Button
+              className={`info-button`}
+              minimal={activeEndpoint !== d.endpoint}
+              icon={d.icon}
+              key={d.text}
+              text={d.text}
+              intent={activeEndpoint === d.endpoint ? Intent.PRIMARY : Intent.NONE}
+              onClick={async () => {
+                await this.getJsonInfo(d.endpoint);
+                this.setState({activeEndpoint: d.endpoint});
+              }}
+            />;
+          })
+        }
+      </div>
+
+      <div className={'main-section'}>
+
+        <div className={'top-actions'}>
+          {
+            mode === 'task' && (activeEndpoint === 'log?offset=-8192' || activeEndpoint === 'log' ) &&
+            <ButtonGroup>
+              <Button
+                className={'view-all-button'}
+                text={activeEndpoint === 'log' ? 'View last 8kb' : 'View all'}
+                minimal
+                onClick={() => {
+                  const alternate =  activeEndpoint === 'log' ? 'log?offset=-8192' : 'log';
+                  this.setState({
+                    activeEndpoint: alternate
+                  });
+                  this.getJsonInfo(alternate);
+                }}
+              />
+            </ButtonGroup>
+          }
+          <ButtonGroup className={'right-buttons'}>
+            <Button
+              text={'Save'}
+              onClick={this.downloadJson}
+              minimal
+            />
+            <CopyToClipboard
+              text={jsonValue}
+            >
+              <Button
+                text={'Copy'}
+                onClick={() => {AppToaster.show({message: 'Copied to clipboard', intent: Intent.SUCCESS}); }}
+                minimal
+              />
+            </CopyToClipboard>
+            <Button
+              text={'View raw data'}
+              minimal
+              onClick={() => window.open(`/druid/indexer/v1/${mode}/${id}/${activeEndpoint}`, '_blank')}
+            />
+          </ButtonGroup>
+        </div>
+
+        <TextArea
+          readOnly
+          value={jsonValue}
+        />
+
+        <div className={'bottom-buttons'}>
+          <ButtonGroup>
+            {tableActionButtions}
+          </ButtonGroup>
+          <ButtonGroup className={'button-done'}>
+            <Button
+              text={'Done'}
+              intent={Intent.PRIMARY}
+              onClick={onClose}
+            />
+          </ButtonGroup>
+        </div>
+
+      </div>
+    </Dialog>;
+  }
+}
--- web-console/src/views/tasks-view.tsx
+++ web-console/src/views/tasks-view.tsx
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-import { Alert, Button, ButtonGroup, Intent, Label, Menu, MenuDivider, MenuItem, Popover, Position } from '@blueprintjs/core';
+import { Alert, Button, ButtonGroup, Icon, Intent, Label, Menu, MenuDivider, MenuItem, Popover, Position } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 import axios from 'axios';
 import * as React from 'react';
@@ -27,6 +27,7 @@ import { TableColumnSelection } from '../components/table-column-selection';
 import { ViewControlBar } from '../components/view-control-bar';
 import { AsyncActionDialog } from '../dialogs/async-action-dialog';
 import { SpecDialog } from '../dialogs/spec-dialog';
+import { TableActionDialog, TableActionDialogMetaData } from '../dialogs/table-action-dialog';
 import { AppToaster } from '../singletons/toaster';
 import { UrlBaser } from '../singletons/url-baser';
 import {
@@ -77,6 +78,8 @@ export interface TasksViewState {
   taskSpecDialogOpen: boolean;
   initSpec: any;
   alertErrorMsg: string | null;
+
+  tableActionDialogMetaData: TableActionDialogMetaData | null;
 }
 
 interface TaskQueryResultRow {
@@ -137,8 +140,9 @@ export class TasksView extends React.Component<TasksViewProps, TasksViewState> {
       supervisorSpecDialogOpen: false,
       taskSpecDialogOpen: false,
       initSpec: null,
-      alertErrorMsg: null
+      alertErrorMsg: null,
 
+      tableActionDialogMetaData: null
     };
 
     this.supervisorTableColumnSelectionHandler = new TableColumnSelectionHandler(
@@ -440,23 +444,24 @@ ORDER BY "rank" DESC, "created_time" DESC`);
             Header: 'Actions',
             id: 'actions',
             accessor: 'id',
-            width: 420,
             filterable: false,
             Cell: row => {
               const id = row.value;
-              const suspendResume = row.original.spec.suspended ?
-                <a onClick={() => this.setState({ resumeSupervisorId: id })}>Resume</a> :
-                <a onClick={() => this.setState({ suspendSupervisorId: id })}>Suspend</a>;
+              const metaData: TableActionDialogMetaData = {
+                id,
+                mode: 'supervisor',
+                supervisorSuspended: row.original.spec.suspended,
+                status: null
+              };
 
-              return <div>
-                <a href={UrlBaser.base(`/druid/indexer/v1/supervisor/${id}`)} target="_blank">Payload</a>&nbsp;&nbsp;&nbsp;
-                <a href={UrlBaser.base(`/druid/indexer/v1/supervisor/${id}/status`)} target="_blank">Status</a>&nbsp;&nbsp;&nbsp;
-                <a href={UrlBaser.base(`/druid/indexer/v1/supervisor/${id}/stats`)} target="_blank">Stats</a>&nbsp;&nbsp;&nbsp;
-                <a href={UrlBaser.base(`/druid/indexer/v1/supervisor/${id}/history`)} target="_blank">History</a>&nbsp;&nbsp;&nbsp;
-                {suspendResume}&nbsp;&nbsp;&nbsp;
-                <a onClick={() => this.setState({ resetSupervisorId: id })}>Reset</a>&nbsp;&nbsp;&nbsp;
-                <a onClick={() => this.setState({ terminateSupervisorId: id })}>Terminate</a>
-              </div>;
+              return <span
+                className={'action-icon'}
+                onClick={() => this.setState({
+                  tableActionDialogMetaData: metaData
+                })}
+              >
+                <Icon icon={`zoom-in`}/>
+              </span>;
             },
             show: supervisorTableColumnSelectionHandler.showColumn('Actions')
           }
@@ -601,20 +606,25 @@ ORDER BY "rank" DESC, "created_time" DESC`);
             Header: 'Actions',
             id: 'actions',
             accessor: 'task_id',
-            width: 360,
             filterable: false,
             Cell: row => {
               if (row.aggregated) return '';
               const id = row.value;
               const { status } = row.original;
-              return <div>
-                <a href={UrlBaser.base(`/druid/indexer/v1/task/${id}`)} target="_blank">Payload</a>&nbsp;&nbsp;&nbsp;
-                <a href={UrlBaser.base(`/druid/indexer/v1/task/${id}/status`)} target="_blank">Status</a>&nbsp;&nbsp;&nbsp;
-                <a href={UrlBaser.base(`/druid/indexer/v1/task/${id}/reports`)} target="_blank">Reports</a>&nbsp;&nbsp;&nbsp;
-                <a href={UrlBaser.base(`/druid/indexer/v1/task/${id}/log`)} target="_blank">Log (all)</a>&nbsp;&nbsp;&nbsp;
-                <a href={UrlBaser.base(`/druid/indexer/v1/task/${id}/log?offset=-8192`)} target="_blank">Log (last 8kb)</a>&nbsp;&nbsp;&nbsp;
-                {(status === 'RUNNING' || status === 'WAITING' || status === 'PENDING') && <a onClick={() => this.setState({ killTaskId: id })}>Kill</a>}
-              </div>;
+              const metaData: TableActionDialogMetaData = {
+                id,
+                mode: 'task',
+                supervisorSuspended: null,
+                status
+              };
+              return <span
+                className={'action-icon'}
+                onClick={() => this.setState({
+                    tableActionDialogMetaData: metaData
+                  })}
+              >
+                <Icon icon={`zoom-in`}/>
+              </span>;
             },
             Aggregated: row => '',
             show: taskTableColumnSelectionHandler.showColumn('Actions')
@@ -629,7 +639,7 @@ ORDER BY "rank" DESC, "created_time" DESC`);
 
   render() {
     const { goToSql, goToLoadDataView, noSqlMode } = this.props;
-    const { groupTasksBy, supervisorSpecDialogOpen, taskSpecDialogOpen, initSpec, alertErrorMsg } = this.state;
+    const { groupTasksBy, supervisorSpecDialogOpen, taskSpecDialogOpen, alertErrorMsg, tableActionDialogMetaData } = this.state;
     const { supervisorTableColumnSelectionHandler, taskTableColumnSelectionHandler } = this;
 
     const submitTaskMenu = <Menu>
@@ -723,6 +733,16 @@ ORDER BY "rank" DESC, "created_time" DESC`);
       >
         <p>{alertErrorMsg}</p>
       </Alert>
+      {tableActionDialogMetaData &&
+        <TableActionDialog
+          onClose={() => this.setState({tableActionDialogMetaData: null})}
+          metaData={tableActionDialogMetaData}
+          terminateSupervisor={(id: string) => this.setState({ terminateSupervisorId: id })}
+          resetSupervisor={(id: string) => this.setState({ resetSupervisorId: id })}
+          resumeSupervisor={(id: string) => this.setState({ resumeSupervisorId: id })}
+          suspendSupervisor={(id: string) => this.setState({ suspendSupervisorId: id })}
+          killTask={(id: string) => this.setState({ killTaskId: id })}
+        />}
     </div>;
   }
 }
