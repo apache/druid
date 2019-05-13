a80a9b8f9aacec515f7f7b0409e682a5e388a5e7
Tangyun <tangyun@bigo.sg>
2019-05-13 17:42:44 +0800

    fix compile.sh

--- deploy/deploy_extension/compile.sh
+++ deploy/deploy_extension/compile.sh
@@ -2,4 +2,4 @@
 echo "compiling project druid 14"
 cd ../../
 mvn clean install -Papache-release,dist,rat,bundle-contrib-exts \
--DskipTests -Drat.skip=true -Dgpg.skip  -Dcheckstyle.skip=true
+-DskipTests -Drat.skip=true -Dgpg.skip=true  -Dcheckstyle.skip=true
