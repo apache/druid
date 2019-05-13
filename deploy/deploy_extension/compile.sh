0a457491a6cbad1e82c817da9ebec7782b29db98
Tangyun <tangyun@bigo.sg>
2019-05-13 18:39:30 +0800

    extension install

--- deploy/deploy_extension/compile.sh
+++ deploy/deploy_extension/compile.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 echo "compiling project druid 14"
+
+# packaging extensions-contrib/bigo-extensions
+cd ../../extensions-contrib/bigo-extensions
+mvn package -DskipTests -Dcheckstyle.skip=true
 cd ../../
-mvn clean install -Papache-release,dist,rat,bundle-contrib-exts \
--DskipTests -Drat.skip=true -Dgpg.skip=true  -Dcheckstyle.skip=true
 mkdir -p distribution/target/bigo-extensions
 mkdir -p distribution/target/bigo-extensions/druid-bigo-extensions
-
 cp -f extensions-contrib/bigo-extensions/target/druid-bigo-extensions-0.14.1-incubating.jar \
     distribution/target/bigo-extensions/druid-bigo-extensions
-
-cp -f distribution/target/hadoop-dependency/hadoop-common-2.7.3.jar \
+cp -f distribution/target/hadoop-dependencies/hadoop-client/2.7.3/hadoop-common-2.7.3.jar \
     distribution/target/bigo-extensions/druid-bigo-extensions
 
+#
 cd distribution/target/
 tar -czvf bigo-extensions.tar.gz bigo-extensions
