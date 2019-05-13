59b10333e0cdd80b2873a569e3d46250736b300c
Tangyun <tangyun@bigo.sg>
2019-05-13 20:26:51 +0800

    fix deploy

--- deploy/deploy_extension/compile.sh
+++ deploy/deploy_extension/compile.sh
@@ -10,9 +10,9 @@ mvn package \
 cd ../../
 mkdir -p distribution/target/bigo-extensions
 mkdir -p distribution/target/bigo-extensions/druid-bigo-extensions
-cp -f extensions-contrib/bigo-extensions/target/druid-bigo-extensions-0.14.1-incubating.jar \
+cp -f extensions-contrib/bigo-extensions/target/druid-bigo-extensions-0.14.0-incubating.jar \
     distribution/target/bigo-extensions/druid-bigo-extensions
-cp -f distribution/target/hadoop-dependencies/hadoop-client/2.7.3/hadoop-common-2.7.3.jar \
+cp -f distribution/target/hadoop-dependencies/hadoop-client/2.8.3/hadoop-common-2.8.3.jar \
     distribution/target/bigo-extensions/druid-bigo-extensions
 
 #
