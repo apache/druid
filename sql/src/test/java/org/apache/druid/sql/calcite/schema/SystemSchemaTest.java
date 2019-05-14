2733f8ee95d05010fcf41cf95cc789c86920e87d
Roman Leventov <leventov.ru@gmail.com>
2019-05-14 18:44:59 +0200

    Unused imports

--- sql/src/test/java/org/apache/druid/sql/calcite/schema/SystemSchemaTest.java
+++ sql/src/test/java/org/apache/druid/sql/calcite/schema/SystemSchemaTest.java
@@ -101,8 +101,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class SystemSchemaTest extends CalciteTestBase
 {
