c09eb232310b79f89c1340cba3078e2b4dd539c8
Mohammad J Khan <mohammad.j.khan@target.com>
2019-07-02 16:44:01 -0500

    fixed checkstyle execution error, removed unused import

--- extensions-contrib/rabbitmq/src/test/java/org/apache/druid/examples/rabbitmq/RabbitMQProducerMain.java
+++ extensions-contrib/rabbitmq/src/test/java/org/apache/druid/examples/rabbitmq/RabbitMQProducerMain.java
@@ -26,7 +26,6 @@ import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
-import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.druid.java.util.common.StringUtils;
