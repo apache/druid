ad0921b395a223b0de6e694735c40fd7841c74ee
Jihoon Son <jihoonson@apache.org>
2019-07-05 09:54:47 -0700

    debug

--- docs/_bin/generate-license-dependency-reports.py
+++ docs/_bin/generate-license-dependency-reports.py
@@ -35,9 +35,9 @@ def generate_report(module_path, report_orig_path, report_out_path):
     try:
         # This command prints lots of false errors. Here, we redirect stdout and stderr to avoid them.
         command = "mvn -Ddependency.locations.enabled=false project-info-reports:dependencies"
-        subprocess.check_output(command, cwd=module_path, shell=True)
+        subprocess.call(command, cwd=module_path, shell=True)
         command = "cp -r {} {}".format(report_orig_path, report_out_path)
-        subprocess.check_output(command, cwd=module_path, shell=True)
+        subprocess.call(command, cwd=module_path, shell=True)
     except Exception as e:
         print("Encountered error [{}] when generating report for {}".format(e, module_path))
 
