79ae3d4761397d2b825d0cc72f24f700cdc2855e
Parag Jain <pjain1@users.noreply.github.com>
2019-07-09 13:21:36 +0530

    set DRUID_AUTHORIZATION_CHECKED attribute for router endpoints (#8026)
    * add state resource filter to router endpoints
    
    * add RouterResource to ResourceFilter test framework

--- services/src/main/java/org/apache/druid/cli/CliOverlord.java
+++ services/src/main/java/org/apache/druid/cli/CliOverlord.java
@@ -375,9 +375,9 @@ public class CliOverlord extends ServerRunnable
 
       AuthenticationUtils.addSecuritySanityCheckFilter(root, jsonMapper);
 
-      // perform no-op authorization for these resources
-      AuthenticationUtils.addNoopAuthorizationFilters(root, UNSECURED_PATHS);
-      AuthenticationUtils.addNoopAuthorizationFilters(root, authConfig.getUnsecuredPaths());
+      // perform no-op authorization/authentication for these resources
+      AuthenticationUtils.addNoopAuthenticationAndAuthorizationFilters(root, UNSECURED_PATHS);
+      AuthenticationUtils.addNoopAuthenticationAndAuthorizationFilters(root, authConfig.getUnsecuredPaths());
 
       final List<Authenticator> authenticators = authenticatorMapper.getAuthenticatorChain();
       AuthenticationUtils.addAuthenticationFilterChain(root, authenticators);
