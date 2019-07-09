4171d003b3936190d6a6246bdfe6b185e7c596db
Jihoon Son <jihoonson@apache.org>
2019-07-09 16:28:05 -0700

    unused import

--- indexing-service/src/test/java/org/apache/druid/indexing/common/TaskLockTest.java
+++ indexing-service/src/test/java/org/apache/druid/indexing/common/TaskLockTest.java
@@ -21,7 +21,6 @@ package org.apache.druid.indexing.common;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Preconditions;
 import org.apache.druid.jackson.DefaultObjectMapper;
