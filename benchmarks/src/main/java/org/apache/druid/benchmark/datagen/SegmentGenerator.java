abf9843e2a217656c4fa4bc79ffd771b13680281
Clint Wylie <cwylie@apache.org>
2019-07-11 23:03:16 -0700

    fail complex type 'serde' registration when registered type does not match expected type (#7985)
    * make ComplexMetrics.registerSerde type check on register, resolves #7959
    
    * add test
    
    * simplify
    
    * unused imports :/
    
    * simplify
    
    * burned by imports yet again
    
    * remove unused constructor
    
    * switch to getName
    
    * heh oops

--- benchmarks/src/main/java/org/apache/druid/benchmark/datagen/SegmentGenerator.java
+++ benchmarks/src/main/java/org/apache/druid/benchmark/datagen/SegmentGenerator.java
@@ -30,7 +30,6 @@ import org.apache.druid.data.input.impl.DoubleDimensionSchema;
 import org.apache.druid.data.input.impl.FloatDimensionSchema;
 import org.apache.druid.data.input.impl.LongDimensionSchema;
 import org.apache.druid.data.input.impl.StringDimensionSchema;
-import org.apache.druid.hll.HyperLogLogHash;
 import org.apache.druid.java.util.common.ISE;
 import org.apache.druid.java.util.common.granularity.Granularity;
 import org.apache.druid.java.util.common.logger.Logger;
@@ -79,7 +78,7 @@ public class SegmentGenerator implements Closeable
   )
   {
     // In case we need to generate hyperUniques.
-    ComplexMetrics.registerSerde("hyperUnique", () -> new HyperUniquesSerde(HyperLogLogHash.getDefault()));
+    ComplexMetrics.registerSerde("hyperUnique", new HyperUniquesSerde());
 
     final BenchmarkDataGenerator dataGenerator = new BenchmarkDataGenerator(
         schemaInfo.getColumnSchemas(),
