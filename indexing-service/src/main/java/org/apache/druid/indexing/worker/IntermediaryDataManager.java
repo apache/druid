d75b8de48e6c6bc989b36ef5ec381059fe828b9a
Jihoon Son <jihoonson@apache.org>
2019-07-19 15:19:42 -0700

    add comment

--- indexing-service/src/main/java/org/apache/druid/indexing/worker/IntermediaryDataManager.java
+++ indexing-service/src/main/java/org/apache/druid/indexing/worker/IntermediaryDataManager.java
@@ -301,6 +301,7 @@ public class IntermediaryDataManager
           }
         }
         catch (Exception e) {
+          // Print only log here to try other locations as well.
           log.warn(e, "Failed to write segmentFile at [%s]", destFile);
           location.removeFile(segmentFile);
         }
