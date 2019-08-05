1aa5484b1bc05f8939e16fbe7a5a33c25cb61d2d
Clint Wylie <cwylie@apache.org>
2019-08-05 16:42:59 -0700

    adjust variable naming

--- processing/src/test/java/org/apache/druid/query/filter/InDimFilterSerDesrTest.java
+++ processing/src/test/java/org/apache/druid/query/filter/InDimFilterSerDesrTest.java
@@ -37,7 +37,7 @@ public class InDimFilterSerDesrTest
 {
   private static ObjectMapper mapper;
 
-  private final String expectedFilter =
+  private final String serializedFilter =
       "{\"type\":\"in\",\"dimension\":\"dimTest\",\"values\":[\"bad\",\"good\"],\"extractionFn\":null}";
 
   @Before
@@ -50,7 +50,7 @@ public class InDimFilterSerDesrTest
   @Test
   public void testDeserialization() throws IOException
   {
-    final InDimFilter actualInDimFilter = mapper.readerFor(DimFilter.class).readValue(expectedFilter);
+    final InDimFilter actualInDimFilter = mapper.readerFor(DimFilter.class).readValue(serializedFilter);
     final InDimFilter expectedInDimFilter = new InDimFilter("dimTest", Arrays.asList("good", "bad"), null);
     Assert.assertEquals(expectedInDimFilter, actualInDimFilter);
   }
@@ -59,8 +59,8 @@ public class InDimFilterSerDesrTest
   public void testSerialization() throws IOException
   {
     final InDimFilter dimInFilter = new InDimFilter("dimTest", Arrays.asList("good", "bad"), null);
-    final String actualFilter = mapper.writeValueAsString(dimInFilter);
-    Assert.assertEquals(expectedFilter, actualFilter);
+    final String actualSerializedFilter = mapper.writeValueAsString(dimInFilter);
+    Assert.assertEquals(serializedFilter, actualSerializedFilter);
   }
 
   @Test
