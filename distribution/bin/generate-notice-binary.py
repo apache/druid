f6206477442e3a4ccee59ece305ed1602d654cd0
Clint Wylie <cwylie@apache.org>
2019-08-16 16:37:24 -0700

    move release scripts to distribution/bin, fixup notice script, trim dependencies for avro and kerberos in licenses.yaml

--- distribution/bin/generate-notice-binary.py
+++ distribution/bin/generate-notice-binary.py
@@ -16,59 +16,56 @@
 # limitations under the License.
 
 import argparse
-import sys
 import yaml
 
+from collections import defaultdict
 
-outfile = sys.stdout
+outfile = None
 
-
-moduleHeaderLine = "############"
-dependencyHeaderLine = "================="
+moduleHeaderLine = "#" * 12
+dependencyHeaderLine = "=" * 17
 
 def print_outfile(string):
     print(string, file=outfile)
 
-def print_error(string):
+def print_log_to_stderr(string):
     print(string, file=sys.stderr)
 
-def print_jar(name, version, notice):
-    print_outfile("{} {}-{}.jar {}".format(dependencyHeaderLine, name, version, dependencyHeaderLine))
-    print_outfile("{}\n\n\n\n".format(notice))
-
 def print_notice(dependency):
+    # note that a dependency may either supply a global notice in the 'notice' field, or, a per jar notice in the
+    # 'notices' field
     if 'notice' in dependency:
-        print_outfile("{} {} {} {}".format(dependencyHeaderLine, dependency['name'], dependency['version'], dependencyHeaderLine))
+        # single notice for dependency name, list out all 'libraries' if any, then print notice
+        print_outfile(f"{dependencyHeaderLine} {dependency['name']} {dependency['version']} {dependencyHeaderLine}")
         if 'libraries' in dependency:
             for library in dependency['libraries']:
                 for group_id, artifact_id in library.items():
                     print_outfile("{}.jar".format(artifact_id))
-            print_outfile("{}\n".format(dependencyHeaderLine))
-        print_outfile("{}\n\n\n\n".format(dependency['notice']))
+            print_outfile(f"{dependencyHeaderLine}\n")
+        print_outfile(f"{dependency['notice']}\n\n\n\n")
     elif 'notices' in dependency:
+        # if 'notices' is set instead of 'notice', then it has jar specific notices to print
         for notice_entry in dependency['notices']:
             for jar, notice in notice_entry.items():
-                print_jar(jar, dependency['version'], notice)
+                print_outfile(f"{dependencyHeaderLine} {name}-{version}.jar {dependencyHeaderLine}")
+                print_outfile(f"{notice}\n\n\n\n")
 
 
 def generate_notice(source_notice, dependences_yaml):
-    # Generate NOTICE.BINARY file
-    print_error("=== Generating the contents of NOTICE.BINARY file ===\n")
+    print_log_to_stderr("=== Generating the contents of NOTICE.BINARY file ===\n")
 
     # Print Apache license first.
     print_outfile(source_notice)
     with open(dependences_yaml) as registry_file:
-        dependencies_list = list(yaml.load_all(registry_file))
+        dependencies_set = set(yaml.load_all(registry_file))
 
     # Group dependencies by module
-    modules_map = {}
-    for dependency in dependencies_list:
+    modules_map = defaultdict(list)
+    for dependency in dependencies_set:
         if 'notice' in dependency or 'notices' in dependency:
-            if dependency['module'] not in modules_map:
-                modules_map[dependency['module']] = []
-            module_dependencies = modules_map[dependency['module']]
-            module_dependencies.append(dependency)
+            modules_map[dependency['module']].append(dependency)
 
+    # print notice(s) of dependencies by module
     for module_name, dependencies_of_module in modules_map.items():
         print_outfile("{} BINARY/{} {}\n".format(moduleHeaderLine, module_name.upper(), moduleHeaderLine))
         for dependency in dependencies_of_module:
