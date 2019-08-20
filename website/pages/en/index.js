48731412b2e2f915754b48f150d4a4f34218ce68
Vadim Ogievetsky <vadimon@gmail.com>
2019-08-20 13:46:50 -0700

    clean up placeholder page

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -18,15 +18,7 @@
 
 const React = require('react');
 
-const CompLibrary = require('../../core/CompLibrary.js');
-
-const Container = CompLibrary.Container;
-const GridBlock = CompLibrary.GridBlock;
-
-function Help(props) {
-  const {config: siteConfig, language = ''} = props;
-  const {baseUrl, docsUrl} = siteConfig;
-
+function PlaceholderPage(props) {
   return (
     <div className="docMainWrapper wrapper">
       This page will not be seen in the final website build. It is only here as a placeholder for docusaurus.
@@ -34,4 +26,4 @@ function Help(props) {
   );
 }
 
-module.exports = Help;
+module.exports = PlaceholderPage;
