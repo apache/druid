bf616cd1323e90b4917fb8f5f2f4af326f6b0947
Clint Wylie <cwylie@apache.org>
2019-08-19 18:39:43 -0700

    is ints

--- processing/src/test/java/org/apache/druid/query/context/ResponseContextTest.java
+++ processing/src/test/java/org/apache/druid/query/context/ResponseContextTest.java
@@ -291,6 +291,7 @@ public class ResponseContextTest
     );
     Assert.assertEquals("string-value", ctx.get(ResponseContext.Key.ETAG));
     Assert.assertEquals(100, ctx.get(ResponseContext.Key.NUM_SCANNED_ROWS));
+    Assert.assertEquals(100000, ctx.get(ResponseContext.Key.CPU_CONSUMED_NANOS));
     ctx.add(ResponseContext.Key.NUM_SCANNED_ROWS, 10L);
     Assert.assertEquals(110L, ctx.get(ResponseContext.Key.NUM_SCANNED_ROWS));
     ctx.add(ResponseContext.Key.CPU_CONSUMED_NANOS, 100);
