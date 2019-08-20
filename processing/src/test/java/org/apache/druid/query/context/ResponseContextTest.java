932d72ed28437948b162d8f4160fdaf81be3d89a
Clint Wylie <cwylie@apache.org>
2019-08-19 18:37:49 -0700

    now with more test

--- processing/src/test/java/org/apache/druid/query/context/ResponseContextTest.java
+++ processing/src/test/java/org/apache/druid/query/context/ResponseContextTest.java
@@ -280,13 +280,21 @@ public class ResponseContextTest
   {
     final DefaultObjectMapper mapper = new DefaultObjectMapper();
     final ResponseContext ctx = ResponseContext.deserialize(
-        mapper.writeValueAsString(ImmutableMap.of("ETag", "string-value", "count", 100)),
+        mapper.writeValueAsString(
+            ImmutableMap.of(
+                "ETag", "string-value",
+                "count", 100L,
+                "cpuConsumed", 100000L
+            )
+        ),
         mapper
     );
     Assert.assertEquals("string-value", ctx.get(ResponseContext.Key.ETAG));
     Assert.assertEquals(100, ctx.get(ResponseContext.Key.NUM_SCANNED_ROWS));
     ctx.add(ResponseContext.Key.NUM_SCANNED_ROWS, 10L);
     Assert.assertEquals(110L, ctx.get(ResponseContext.Key.NUM_SCANNED_ROWS));
+    ctx.add(ResponseContext.Key.CPU_CONSUMED_NANOS, 100);
+    Assert.assertEquals(100100L, ctx.get(ResponseContext.Key.CPU_CONSUMED_NANOS));
   }
 
   @Test(expected = IllegalStateException.class)
