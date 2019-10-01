82bfe86d0c4ec042b68876eee3b9e536fc100411
Fokko Driesprong <fokko@apache.org>
2019-10-01 03:53:18 +0300

    Make more package EverythingIsNonnullByDefault by default (#8198)
    * Make more package EverythingIsNonnullByDefault by default
    
    * Fixed additional voilations after pulling in master
    
    * Change iterator to list.addAll
    
    * Fix annotations

--- indexing-hadoop/src/main/java/org/apache/druid/indexer/DeterminePartitionsJob.java
+++ indexing-hadoop/src/main/java/org/apache/druid/indexer/DeterminePartitionsJob.java
@@ -325,6 +325,7 @@ public class DeterminePartitionsJob implements Jobby
 
   public static class DeterminePartitionsGroupByMapper extends HadoopDruidIndexerMapper<BytesWritable, NullWritable>
   {
+    @Nullable
     private Granularity rollupGranularity = null;
 
     @Override
@@ -374,6 +375,7 @@ public class DeterminePartitionsJob implements Jobby
   public static class DeterminePartitionsDimSelectionPostGroupByMapper
       extends Mapper<BytesWritable, NullWritable, BytesWritable, Text>
   {
+    @Nullable
     private DeterminePartitionsDimSelectionMapperHelper helper;
 
     @Override
@@ -537,6 +539,7 @@ public class DeterminePartitionsJob implements Jobby
   private abstract static class DeterminePartitionsDimSelectionBaseReducer
       extends Reducer<BytesWritable, Text, BytesWritable, Text>
   {
+    @Nullable
     protected volatile HadoopDruidIndexerConfig config = null;
 
     @Override
@@ -905,6 +908,7 @@ public class DeterminePartitionsJob implements Jobby
 
   private static class DimPartition
   {
+    @Nullable
     public ShardSpec shardSpec = null;
     int cardinality = 0;
     public long rows = 0;
