636cc0c94676b2474ae1c78e2038cbc6b2273719
renevan10 <renevan10@gmail.com>
2019-10-08 14:14:22 -0700

    Show data for table in dialog

--- web-console/src/dialogs/rollup-estimate-dialog/rollup-estimate-dialog.tsx
+++ web-console/src/dialogs/rollup-estimate-dialog/rollup-estimate-dialog.tsx
@@ -17,19 +17,170 @@
  */
 
 import { Dialog } from '@blueprintjs/core';
+import axios from 'axios';
+import {
+  HeaderRows,
+  isFirstRowHeader,
+  normalizeQueryResult,
+  shouldIncludeTimestamp,
+  sqlParserFactory,
+  SqlQuery,
+} from 'druid-query-toolkit';
+import Hjson from 'hjson';
+import memoizeOne from 'memoize-one';
 import * as React from 'react';
+import ReactTable from 'react-table';
+
+import { SQL_FUNCTIONS } from '../../../lib/sql-docs';
+import { Loader } from '../../components/index';
+import { getDruidErrorMessage, QueryManager } from '../../utils/index';
+import { isEmptyContext, QueryContext } from '../../utils/query-context';
+import { QueryView } from '../../views/index';
+import { QueryExtraInfoData } from '../../views/query-view/query-extra-info/query-extra-info';
+
+const parserRaw = sqlParserFactory(SQL_FUNCTIONS.map(sqlFunction => sqlFunction.name));
+
+const parser = memoizeOne((sql: string) => {
+  try {
+    return parserRaw(sql);
+  } catch {
+    return;
+  }
+});
+
+interface QueryWithContext {
+  queryString: string;
+  queryContext: QueryContext;
+  wrapQueryLimit: number | undefined;
+}
+
+interface QueryResult {
+  queryResult: HeaderRows;
+  queryExtraInfo: QueryExtraInfoData;
+  parsedQuery?: SqlQuery;
+}
 
 export interface RollupEstimateDialogProps {
   datasource: string;
   onClose: () => void;
 }
 
-export class RollupEstimateDialog extends React.PureComponent<RollupEstimateDialogProps> {
-  constructor(props: RollupEstimateDialogProps) {
-    super(props);
+export interface RollupEstimateDialogState {
+  queryString: string;
+  queryContext: QueryContext;
+  wrapQueryLimit: number | undefined;
+  loading: boolean;
+  result?: QueryResult;
+  error?: string;
+}
+
+export class RollupEstimateDialog extends React.PureComponent<
+  RollupEstimateDialogProps,
+  RollupEstimateDialogState
+> {
+  private sqlQueryManager: QueryManager<QueryWithContext, QueryResult>;
+  constructor(props: RollupEstimateDialogProps, context: any) {
+    super(props, context);
+    this.state = {
+      queryString: `SELECT * FROM "${props.datasource}"`,
+      queryContext: {},
+      wrapQueryLimit: 20,
+      loading: false,
+    };
+
+    this.sqlQueryManager = new QueryManager({
+      processQuery: async (queryWithContext: QueryWithContext): Promise<QueryResult> => {
+        const { queryString, queryContext, wrapQueryLimit } = queryWithContext;
+        let parsedQuery: SqlQuery | undefined;
+        let jsonQuery: any;
+
+        try {
+          parsedQuery = parser(queryString);
+        } catch {}
+
+        if (!(parsedQuery instanceof SqlQuery)) {
+          parsedQuery = undefined;
+        }
+        if (QueryView.isJsonLike(queryString)) {
+          jsonQuery = Hjson.parse(queryString);
+        } else {
+          const actualQuery = QueryView.wrapInLimitIfNeeded(queryString, wrapQueryLimit);
+
+          jsonQuery = {
+            query: actualQuery,
+            resultFormat: 'array',
+            header: true,
+          };
+        }
+
+        if (!isEmptyContext(queryContext)) {
+          jsonQuery.context = Object.assign(jsonQuery.context || {}, queryContext);
+        }
+
+        let rawQueryResult: unknown;
+        let queryId: string | undefined;
+        let sqlQueryId: string | undefined;
+        const startTime = new Date();
+        let endTime: Date;
+        if (!jsonQuery.queryType && typeof jsonQuery.query === 'string') {
+          try {
+            const sqlResultResp = await axios.post('/druid/v2/sql', jsonQuery);
+            endTime = new Date();
+            rawQueryResult = sqlResultResp.data;
+            sqlQueryId = sqlResultResp.headers['x-druid-sql-query-id'];
+          } catch (e) {
+            throw new Error(getDruidErrorMessage(e));
+          }
+        } else {
+          try {
+            const runeResultResp = await axios.post('/druid/v2', jsonQuery);
+            endTime = new Date();
+            rawQueryResult = runeResultResp.data;
+            queryId = runeResultResp.headers['x-druid-query-id'];
+          } catch (e) {
+            throw new Error(getDruidErrorMessage(e));
+          }
+        }
+
+        const queryResult = normalizeQueryResult(
+          rawQueryResult,
+          shouldIncludeTimestamp(jsonQuery),
+          isFirstRowHeader(jsonQuery),
+        );
+        console.log(queryResult);
+        return {
+          queryResult,
+          queryExtraInfo: {
+            queryId,
+            sqlQueryId,
+            startTime,
+            endTime,
+            numResults: queryResult.rows.length,
+            wrapQueryLimit,
+          },
+          parsedQuery,
+        };
+      },
+      onStateChange: ({ result, loading, error }) => {
+        this.setState({
+          result,
+          loading,
+          error,
+        });
+      },
+    });
+  }
+
+  componentDidMount() {
+    const { queryString, queryContext, wrapQueryLimit } = this.state;
+    this.sqlQueryManager.runQuery({ queryString, queryContext, wrapQueryLimit });
   }
   render(): JSX.Element {
     const { datasource, onClose } = this.props;
+    const { result, loading } = this.state;
+    console.log(result);
+    console.log(loading);
+    if (loading) return <Loader />;
     return (
       <Dialog
         isOpen={datasource !== undefined}
@@ -37,7 +188,31 @@ export class RollupEstimateDialog extends React.PureComponent<RollupEstimateDial
         canOutsideClickClose={false}
         title={'Estimate your rollup ratio'}
       >
-        {datasource}
+        <ReactTable
+          data={result ? result.queryResult.rows : []}
+          loading={loading}
+          columns={(result ? result.queryResult.header : []).map((h: any, i) => {
+            return {
+              Header: () => {
+                return <div>{h}</div>;
+              },
+              headerClassName: h,
+              accessor: String(i),
+              Cell: row => {
+                const value = row.value;
+                const popover = (
+                  <div>
+                    <div>{value}</div>
+                  </div>
+                );
+                if (value) {
+                  return popover;
+                }
+                return value;
+              },
+            };
+          })}
+        />
       </Dialog>
     );
   }
