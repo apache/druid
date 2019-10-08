845990a08fdb98d9b5f5f93fa642f5430a314c36
Mitch Lloyd <mitch.lloyd@gmail.com>
2019-10-08 16:11:46 -0700

    Add credentials for ECS

--- cloud/aws-common/src/main/java/org/apache/druid/common/aws/AWSCredentialsUtils.java
+++ cloud/aws-common/src/main/java/org/apache/druid/common/aws/AWSCredentialsUtils.java
@@ -23,6 +23,7 @@ import com.amazonaws.auth.AWSCredentialsProviderChain;
 import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.auth.SystemPropertiesCredentialsProvider;
+import com.amazonaws.auth.EC2ContainerCredentialsProviderWrapper;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 
 public class AWSCredentialsUtils
@@ -35,6 +36,7 @@ public class AWSCredentialsUtils
         new EnvironmentVariableCredentialsProvider(),
         new SystemPropertiesCredentialsProvider(),
         new ProfileCredentialsProvider(),
-        new InstanceProfileCredentialsProvider());
+        new EC2ContainerCredentialsProviderWrapper(),
+        InstanceProfileCredentialsProvider.getInstance());
   }
 }
