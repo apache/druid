f44002c6d0a63d858961c52bb75a20c0588483c1
Chi Cao Minh <chi.caominh@imply.io>
2019-10-11 22:02:26 -0700

    Bump jackson and avatica versions

--- indexing-hadoop/src/main/java/org/apache/druid/indexer/IndexGeneratorJob.java
+++ indexing-hadoop/src/main/java/org/apache/druid/indexer/IndexGeneratorJob.java
@@ -77,6 +77,7 @@ import org.joda.time.Interval;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -117,7 +118,7 @@ public class IndexGeneratorJob implements Jobby
       FileSystem fs = descriptorInfoDir.getFileSystem(conf);
 
       for (FileStatus status : fs.listStatus(descriptorInfoDir)) {
-        final DataSegment segment = jsonMapper.readValue(fs.open(status.getPath()), DataSegment.class);
+        final DataSegment segment = jsonMapper.readValue((InputStream) fs.open(status.getPath()), DataSegment.class);
         publishedSegmentsBuilder.add(segment);
         log.info("Adding segment %s to the list of published segments", segment.getId());
       }
