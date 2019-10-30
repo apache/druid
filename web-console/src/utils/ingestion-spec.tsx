07d03759ffe63ffdef5d799621a5402d8f7f9dcc
Vadim Ogievetsky <vadimon@gmail.com>
2019-10-29 20:53:38 -0700

    Support all possible metric types in the data loader

--- web-console/src/utils/ingestion-spec.tsx
+++ web-console/src/utils/ingestion-spec.tsx
@@ -668,6 +668,7 @@ const METRIC_SPEC_FORM_FIELDS: Field<MetricSpec>[] = [
   {
     name: 'name',
     type: 'string',
+    info: <>The metric name as it will appear in Druid.</>,
   },
   {
     name: 'type',
@@ -694,37 +695,21 @@ const METRIC_SPEC_FORM_FIELDS: Field<MetricSpec>[] = [
         group: 'last',
         suggestions: ['longLast', 'doubleLast', 'floatLast'],
       },
-      'cardinality',
+      'thetaSketch',
+      {
+        group: 'HLLSketch',
+        suggestions: ['HLLSketchBuild', 'HLLSketchMerge'],
+      },
       'hyperUnique',
       'filtered',
     ],
+    info: <>The aggregation function to apply.</>,
   },
   {
     name: 'fieldName',
     type: 'string',
-    defined: m => {
-      return [
-        'longSum',
-        'doubleSum',
-        'floatSum',
-        'longMin',
-        'doubleMin',
-        'floatMin',
-        'longMax',
-        'doubleMax',
-        'floatMax',
-        'longFirst',
-        'doubleFirst',
-        'floatFirst',
-        'stringFirst',
-        'longLast',
-        'doubleLast',
-        'floatLast',
-        'stringLast',
-        'cardinality',
-        'hyperUnique',
-      ].includes(m.type);
-    },
+    defined: m => m.type !== 'filtered',
+    info: <>The column name for the aggregator to operate on.</>,
   },
   {
     name: 'maxStringBytes',
@@ -742,21 +727,84 @@ const METRIC_SPEC_FORM_FIELDS: Field<MetricSpec>[] = [
       return ['stringFirst', 'stringLast'].includes(m.type);
     },
   },
+  // filtered
   {
     name: 'filter',
     type: 'json',
-    defined: m => {
-      return m.type === 'filtered';
-    },
+    defined: m => m.type === 'filtered',
   },
   {
     name: 'aggregator',
     type: 'json',
-    defined: m => {
-      return m.type === 'filtered';
-    },
+    defined: m => m.type === 'filtered',
   },
-  // ToDo: fill in approximates
+  // thetaSketch
+  {
+    name: 'size',
+    type: 'number',
+    defined: m => m.type === 'thetaSketch',
+    defaultValue: 16384,
+    info: (
+      <>
+        <p>
+          Must be a power of 2. Internally, size refers to the maximum number of entries sketch
+          object will retain. Higher size means higher accuracy but more space to store sketches.
+          Note that after you index with a particular size, druid will persist sketch in segments
+          and you will use size greater or equal to that at query time.
+        </p>
+        <p>
+          See the{' '}
+          <ExternalLink href="https://datasketches.github.io/docs/Theta/ThetaSize.html">
+            DataSketches site
+          </ExternalLink>{' '}
+          for details.
+        </p>
+        <p>In general, We recommend just sticking to default size.</p>
+      </>
+    ),
+  },
+  {
+    name: 'isInputThetaSketch',
+    type: 'boolean',
+    defined: m => m.type === 'thetaSketch',
+    defaultValue: false,
+    info: (
+      <>
+        This should only be used at indexing time if your input data contains theta sketch objects.
+        This would be the case if you use datasketches library outside of Druid, say with Pig/Hive,
+        to produce the data that you are ingesting into Druid
+      </>
+    ),
+  },
+  // HLLSketchBuild & HLLSketchMerge
+  {
+    name: 'lgK',
+    type: 'number',
+    defined: m => m.type === 'HLLSketchBuild' || m.type === 'HLLSketchMerge',
+    defaultValue: 12,
+    info: (
+      <>
+        <p>
+          log2 of K that is the number of buckets in the sketch, parameter that controls the size
+          and the accuracy.
+        </p>
+        <p>Must be between 4 to 21 inclusively.</p>
+      </>
+    ),
+  },
+  {
+    name: 'tgtHllType',
+    type: 'string',
+    defined: m => m.type === 'HLLSketchBuild' || m.type === 'HLLSketchMerge',
+    defaultValue: 'HLL_4',
+    suggestions: ['HLL_4', 'HLL_6', 'HLL_8'],
+    info: (
+      <>
+        The type of the target HLL sketch. Must be <Code>HLL_4</Code>, <Code>HLL_6</Code>, or{' '}
+        <Code>HLL_8</Code>.
+      </>
+    ),
+  },
 ];
 
 export function getMetricSpecFormFields() {
