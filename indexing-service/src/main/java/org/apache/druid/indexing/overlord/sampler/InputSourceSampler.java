7125e3e94bd468bc82c95fad68538890a23c69ee
Clint Wylie <cwylie@apache.org>
2019-11-20 17:21:32 -0800

    oops

--- indexing-service/src/main/java/org/apache/druid/indexing/overlord/sampler/InputSourceSampler.java
+++ indexing-service/src/main/java/org/apache/druid/indexing/overlord/sampler/InputSourceSampler.java
@@ -50,6 +50,7 @@ import org.apache.druid.segment.indexing.DataSchema;
 
 import javax.annotation.Nullable;
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -87,7 +88,7 @@ public class InputSourceSampler
       @Nullable final InputFormat inputFormat,
       @Nullable final DataSchema dataSchema,
       @Nullable final SamplerConfig samplerConfig
-  )
+  ) throws IOException
   {
     Preconditions.checkNotNull(inputSource, "inputSource required");
     if (inputSource.needsFormat()) {
@@ -190,7 +191,7 @@ public class InputSourceSampler
       InputSource inputSource,
       InputFormat inputFormat,
       File tempDir
-  )
+  ) throws IOException
   {
     final List<String> metricsNames = Arrays.stream(dataSchema.getAggregators())
                                             .map(AggregatorFactory::getName)
