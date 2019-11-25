eede9e397bbea9ffa0ecfbce361f32b164a8380c
liguifa <liguifa18840848462@outlook.com>
2019-11-25 08:39:30 +0800

    automatically update view data when new segments are generated

--- extensions-contrib/materialized-view-maintenance/src/main/java/org/apache/druid/indexing/materializedview/MaterializedViewSupervisor.java
+++ extensions-contrib/materialized-view-maintenance/src/main/java/org/apache/druid/indexing/materializedview/MaterializedViewSupervisor.java
@@ -23,8 +23,8 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.MapDifference;
-import com.google.common.collect.Maps;
 import com.google.common.collect.MapDifference.ValueDifference;
+import com.google.common.collect.Maps;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningScheduledExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
@@ -51,11 +51,9 @@ import org.apache.druid.metadata.EntryExistsException;
 import org.apache.druid.metadata.MetadataSupervisorManager;
 import org.apache.druid.metadata.SQLMetadataSegmentManager;
 import org.apache.druid.timeline.DataSegment;
-import org.apache.druid.java.util.common.logger.Logger;
 import org.joda.time.Duration;
 import org.joda.time.Interval;
 
-import javax.annotation.Nullable;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -64,10 +62,10 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
-import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
 import java.util.function.IntSupplier;
 
@@ -417,7 +415,7 @@ public class MaterializedViewSupervisor implements Supervisor
       while (diffiter.hasNext()) {
         Entry<Interval, ValueDifference<String>> entry = diffiter.next();
         String maxCreateDate = entry.getValue().leftValue();
-        if(!runningVersion.containsKey(entry.getKey())) {
+        if (!runningVersion.containsKey(entry.getKey())) {
           toBuildInterval.put(entry.getKey(), maxCreateDate);
         }
       }
