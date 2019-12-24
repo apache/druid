320c50d24a1932cc7b3dfb12417f244b1defd73f
Vadim Ogievetsky <vadim@ogievetsky.com>
2019-12-23 18:23:14 -0800

    Web console: fix spec reset (#9081)
    * extract spec type
    
    * better text
    
    * better copy
    
    * de incubate the console
    
    * fix status dialog scss

--- web-console/src/views/load-data-view/load-data-view.tsx
+++ web-console/src/views/load-data-view/load-data-view.tsx
@@ -740,8 +740,8 @@ export class LoadDataView extends React.PureComponent<LoadDataViewProps, LoadDat
           <>
             <p>Load data accessible through HTTP(s).</p>
             <p>
-              Data must be in a text format and the HTTP(s) endpoint must be reachable by every
-              Druid process in the cluster.
+              Data must be in text, orc, or parquet format and the HTTP(s) endpoint must be
+              reachable by every Druid process in the cluster.
             </p>
           </>
         );
@@ -754,8 +754,8 @@ export class LoadDataView extends React.PureComponent<LoadDataViewProps, LoadDat
             </p>
             <p>Load data directly from a local file.</p>
             <p>
-              Files must be in a text format and must be accessible to all the Druid processes in
-              the cluster.
+              Files must be in text, orc, or parquet format and must be accessible to all the Druid
+              processes in the cluster.
             </p>
           </>
         );
@@ -779,13 +779,13 @@ export class LoadDataView extends React.PureComponent<LoadDataViewProps, LoadDat
         );
 
       case 'index_parallel:s3':
-        return <p>Load text based data from Amazon S3.</p>;
+        return <p>Load text based, orc, or parquet data from Amazon S3.</p>;
 
       case 'index_parallel:google':
-        return <p>Load text based data from the Google Blobstore.</p>;
+        return <p>Load text based, orc, or parquet data from the Google Blobstore.</p>;
 
       case 'index_parallel:hdfs':
-        return <p>Load text based data from HDFS.</p>;
+        return <p>Load text based, orc, or parquet data from HDFS.</p>;
 
       case 'kafka':
         return <p>Load streaming data in real-time from Apache Kafka.</p>;
