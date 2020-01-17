63a4c1d8a205799432b61ae28ec32595ca56d5cf
Clint Wylie <cwylie@apache.org>
2020-01-17 11:36:20 -0800

    fix null handling for arithmetic post aggregator comparator (#9159) (#9202)
    * fix null handling for arithmetic postagg comparator, add test for comparator for min/max/quantile postaggs in histogram ext
    
    * fix

--- extensions-core/histogram/src/main/java/org/apache/druid/query/aggregation/histogram/QuantilePostAggregator.java
+++ extensions-core/histogram/src/main/java/org/apache/druid/query/aggregation/histogram/QuantilePostAggregator.java
@@ -36,14 +36,8 @@ import java.util.Set;
 @JsonTypeName("quantile")
 public class QuantilePostAggregator extends ApproximateHistogramPostAggregator
 {
-  static final Comparator COMPARATOR = new Comparator()
-  {
-    @Override
-    public int compare(Object o, Object o1)
-    {
-      return Double.compare(((Number) o).doubleValue(), ((Number) o1).doubleValue());
-    }
-  };
+  // this doesn't need to handle nulls because the values come from ApproximateHistogram
+  static final Comparator COMPARATOR = Comparator.comparingDouble(o -> ((Number) o).doubleValue());
 
   private final float probability;
   private final String fieldName;
