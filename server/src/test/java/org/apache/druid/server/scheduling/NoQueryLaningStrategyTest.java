373fd1158f13c9f7a77ac6ba1748aa2b2cbb8b99
Clint Wylie <cwylie@apache.org>
2020-03-06 21:08:54 -0800

    style

--- server/src/test/java/org/apache/druid/server/scheduling/NoQueryLaningStrategyTest.java
+++ server/src/test/java/org/apache/druid/server/scheduling/NoQueryLaningStrategyTest.java
@@ -33,8 +33,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class NoQueryLaningStrategyTest
 {
   private Druids.TimeseriesQueryBuilder queryBuilder;
