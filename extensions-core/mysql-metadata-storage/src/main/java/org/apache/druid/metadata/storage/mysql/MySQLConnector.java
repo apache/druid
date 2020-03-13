70ac72d75cf2e8167cefc3cd67e582f63220f7ad
Chi Cao Minh <chi.caominh@imply.io>
2020-03-13 11:48:50 -0700

    Update to mysql connector 5.1.48

--- extensions-core/mysql-metadata-storage/src/main/java/org/apache/druid/metadata/storage/mysql/MySQLConnector.java
+++ extensions-core/mysql-metadata-storage/src/main/java/org/apache/druid/metadata/storage/mysql/MySQLConnector.java
@@ -65,7 +65,7 @@ public class MySQLConnector extends SQLMetadataConnector
     catch (ClassNotFoundException e) {
       throw new ISE(e, "Could not find %s on the classpath. The MySQL Connector library is not included in the Druid "
                    + "distribution but is required to use MySQL. Please download a compatible library (for example "
-                   + "'mysql-connector-java-5.1.38.jar') and place it under 'extensions/mysql-metadata-storage/'. See "
+                   + "'mysql-connector-java-5.1.48.jar') and place it under 'extensions/mysql-metadata-storage/'. See "
                    + "https://druid.apache.org/downloads for more details.",
                 MYSQL_JDBC_DRIVER_CLASS_NAME
       );
