226f2b697c820412c9b5357427f93b015154dac8
Xavier Léauté <xavier@confluent.io>
2020-03-24 11:06:18 -0700

    confluent-extensions with custom transform specs (#9)

---
+++ extensions-contrib/confluent-extensions/src/main/java/io/confluent/druid/transform/ExtractTenantTopicTransform.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020 Confluent Inc.
+ */
+
+package io.confluent.druid.transform;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.druid.segment.transform.RowFunction;
+import org.apache.druid.segment.transform.Transform;
+
+public class ExtractTenantTopicTransform implements Transform
+{
+  private final String fieldName;
+  private final String name;
+
+  public ExtractTenantTopicTransform(
+      @JsonProperty("name") final String name,
+      @JsonProperty("fieldName") final String fieldName
+  )
+  {
+    this.fieldName = fieldName;
+    this.name = name;
+  }
+
+  @Override
+  public String getName()
+  {
+    return name;
+  }
+
+  @Override
+  public RowFunction getRowFunction()
+  {
+    return row -> {
+      Object existing = row.getRaw(name);
+      // do not overwrite existing values if present
+      if (existing != null) {
+        return existing;
+      }
+
+      Object value = row.getRaw(fieldName);
+      return value == null ? null : TenantUtils.extractTenantTopic(value.toString());
+    };
+  }
+}
