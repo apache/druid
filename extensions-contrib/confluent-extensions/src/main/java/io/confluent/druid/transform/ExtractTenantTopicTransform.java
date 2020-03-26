8db469ef56c4a57b706a36284862ecb98acc13b8
Xavier Léauté <xavier@confluent.io>
2020-03-26 09:19:37 -0700

    fix extraction transform serde (#10)

--- extensions-contrib/confluent-extensions/src/main/java/io/confluent/druid/transform/ExtractTenantTopicTransform.java
+++ extensions-contrib/confluent-extensions/src/main/java/io/confluent/druid/transform/ExtractTenantTopicTransform.java
@@ -5,8 +5,11 @@
 package io.confluent.druid.transform;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Preconditions;
 import org.apache.druid.segment.transform.RowFunction;
 import org.apache.druid.segment.transform.Transform;
+
+import java.util.Objects;
 
 public class ExtractTenantTopicTransform implements Transform
 {
@@ -18,14 +21,21 @@ public class ExtractTenantTopicTransform implements Transform
       @JsonProperty("fieldName") final String fieldName
   )
   {
-    this.fieldName = fieldName;
-    this.name = name;
+    this.name = Preconditions.checkNotNull(name, "name");
+    this.fieldName = Preconditions.checkNotNull(fieldName, "fieldName");
   }
 
+  @JsonProperty
   @Override
   public String getName()
   {
     return name;
+  }
+
+  @JsonProperty
+  public String getFieldName()
+  {
+    return fieldName;
   }
 
   @Override
@@ -41,5 +51,34 @@ public class ExtractTenantTopicTransform implements Transform
       Object value = row.getRaw(fieldName);
       return value == null ? null : TenantUtils.extractTenantTopic(value.toString());
     };
+  }
+
+  @Override
+  public boolean equals(Object o)
+  {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof ExtractTenantTopicTransform)) {
+      return false;
+    }
+    ExtractTenantTopicTransform that = (ExtractTenantTopicTransform) o;
+    return fieldName.equals(that.fieldName) &&
+           name.equals(that.name);
+  }
+
+  @Override
+  public int hashCode()
+  {
+    return Objects.hash(fieldName, name);
+  }
+
+  @Override
+  public String toString()
+  {
+    return "ExtractTenantTopicTransform{" +
+           "fieldName='" + fieldName + '\'' +
+           ", name='" + name + '\'' +
+           '}';
   }
 }
