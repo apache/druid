ae917f560ea881f10a1f4eac661290347ed413a5
tsmethurst <tobi.smethurst@klarrio.com>
2020-04-02 13:45:28 +0200

    Use SERVERS resource name

--- sql/src/main/java/org/apache/druid/sql/calcite/schema/SystemSchema.java
+++ sql/src/main/java/org/apache/druid/sql/calcite/schema/SystemSchema.java
@@ -72,6 +72,7 @@ import org.apache.druid.server.security.AuthorizerMapper;
 import org.apache.druid.server.security.ForbiddenException;
 import org.apache.druid.server.security.Resource;
 import org.apache.druid.server.security.ResourceAction;
+import org.apache.druid.server.security.ResourceType;
 import org.apache.druid.sql.calcite.planner.PlannerContext;
 import org.apache.druid.sql.calcite.table.RowSignatures;
 import org.apache.druid.timeline.DataSegment;
@@ -1006,7 +1007,7 @@ public class SystemSchema extends AbstractSchema
   {
     final Access stateAccess = AuthorizationUtils.authorizeAllResourceActions(
         authenticationResult,
-        Collections.singletonList(new ResourceAction(Resource.STATE_RESOURCE, Action.READ)),
+        Collections.singletonList(new ResourceAction(new Resource("SERVERS", ResourceType.STATE), Action.READ)),
         authorizerMapper
     );
     if (!stateAccess.isAllowed()) {
