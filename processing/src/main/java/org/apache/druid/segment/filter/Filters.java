e7ef80eade26fe2120015f5857fc6d3e7a4422c3
Jihoon Son <jihoonson@apache.org>
2020-04-09 18:00:12 -0700

    More optimize CNF conversion of filters (#9634) (#9656)
    * More optimize CNF conversion of filters
    
    * update license
    
    * fix build
    
    * checkstyle
    
    * remove unnecessary code
    
    * split helper
    
    * license
    
    * checkstyle
    
    * add comments on cnf conversion

--- processing/src/main/java/org/apache/druid/segment/filter/Filters.java
+++ processing/src/main/java/org/apache/druid/segment/filter/Filters.java
@@ -19,7 +19,6 @@
 
 package org.apache.druid.segment.filter;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
@@ -30,7 +29,6 @@ import org.apache.druid.collections.bitmap.ImmutableBitmap;
 import org.apache.druid.query.BitmapResultFactory;
 import org.apache.druid.query.Query;
 import org.apache.druid.query.filter.BitmapIndexSelector;
-import org.apache.druid.query.filter.BooleanFilter;
 import org.apache.druid.query.filter.DimFilter;
 import org.apache.druid.query.filter.DruidPredicateFactory;
 import org.apache.druid.query.filter.Filter;
@@ -44,12 +42,12 @@ import org.apache.druid.segment.column.BitmapIndex;
 import org.apache.druid.segment.column.ColumnHolder;
 import org.apache.druid.segment.data.CloseableIndexed;
 import org.apache.druid.segment.data.Indexed;
+import org.apache.druid.segment.filter.cnf.CalciteCnfHelper;
+import org.apache.druid.segment.filter.cnf.HiveCnfHelper;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -426,173 +424,27 @@ public class Filters
       return null;
     }
     boolean useCNF = query.getContextBoolean(CTX_KEY_USE_FILTER_CNF, false);
-    return useCNF ? toCNF(filter) : filter;
+    return useCNF ? Filters.toCnf(filter) : filter;
   }
 
-  public static Filter toCNF(Filter current)
+  public static Filter toCnf(Filter current)
   {
-    current = pushDownNot(current);
-    current = flatten(current);
-    current = convertToCNFInternal(current);
-    current = flatten(current);
+    // Push down NOT filters to leaves if possible to remove NOT on NOT filters and reduce hierarchy.
+    // ex) ~(a OR ~b) => ~a AND b
+    current = HiveCnfHelper.pushDownNot(current);
+    // Flatten nested AND and OR filters if possible.
+    // ex) a AND (b AND c) => a AND b AND c
+    current = HiveCnfHelper.flatten(current);
+    // Pull up AND filters first to convert the filter into a conjunctive form.
+    // It is important to pull before CNF conversion to not create a huge CNF.
+    // ex) (a AND b) OR (a AND c AND d) => a AND (b OR (c AND d))
+    current = CalciteCnfHelper.pull(current);
+    // Convert filter to CNF.
+    // a AND (b OR (c AND d)) => a AND (b OR c) AND (b OR d)
+    current = HiveCnfHelper.convertToCnf(current);
+    // Flatten again to remove any flattenable nested AND or OR filters created during CNF conversion.
+    current = HiveCnfHelper.flatten(current);
     return current;
-  }
-
-  // CNF conversion functions were adapted from Apache Hive, see:
-  // https://github.com/apache/hive/blob/branch-2.0/storage-api/src/java/org/apache/hadoop/hive/ql/io/sarg/SearchArgumentImpl.java
-  @VisibleForTesting
-  static Filter pushDownNot(Filter current)
-  {
-    if (current instanceof NotFilter) {
-      Filter child = ((NotFilter) current).getBaseFilter();
-      if (child instanceof NotFilter) {
-        return pushDownNot(((NotFilter) child).getBaseFilter());
-      }
-      if (child instanceof AndFilter) {
-        Set<Filter> children = new HashSet<>();
-        for (Filter grandChild : ((AndFilter) child).getFilters()) {
-          children.add(pushDownNot(new NotFilter(grandChild)));
-        }
-        return new OrFilter(children);
-      }
-      if (child instanceof OrFilter) {
-        Set<Filter> children = new HashSet<>();
-        for (Filter grandChild : ((OrFilter) child).getFilters()) {
-          children.add(pushDownNot(new NotFilter(grandChild)));
-        }
-        return new AndFilter(children);
-      }
-    }
-
-
-    if (current instanceof AndFilter) {
-      Set<Filter> children = new HashSet<>();
-      for (Filter child : ((AndFilter) current).getFilters()) {
-        children.add(pushDownNot(child));
-      }
-      return new AndFilter(children);
-    }
-
-
-    if (current instanceof OrFilter) {
-      Set<Filter> children = new HashSet<>();
-      for (Filter child : ((OrFilter) current).getFilters()) {
-        children.add(pushDownNot(child));
-      }
-      return new OrFilter(children);
-    }
-    return current;
-  }
-
-  // CNF conversion functions were adapted from Apache Hive, see:
-  // https://github.com/apache/hive/blob/branch-2.0/storage-api/src/java/org/apache/hadoop/hive/ql/io/sarg/SearchArgumentImpl.java
-  private static Filter convertToCNFInternal(Filter current)
-  {
-    if (current instanceof NotFilter) {
-      return new NotFilter(convertToCNFInternal(((NotFilter) current).getBaseFilter()));
-    }
-    if (current instanceof AndFilter) {
-      Set<Filter> children = new HashSet<>();
-      for (Filter child : ((AndFilter) current).getFilters()) {
-        children.add(convertToCNFInternal(child));
-      }
-      return new AndFilter(children);
-    }
-    if (current instanceof OrFilter) {
-      // a list of leaves that weren't under AND expressions
-      List<Filter> nonAndList = new ArrayList<Filter>();
-      // a list of AND expressions that we need to distribute
-      List<Filter> andList = new ArrayList<Filter>();
-      for (Filter child : ((OrFilter) current).getFilters()) {
-        if (child instanceof AndFilter) {
-          andList.add(child);
-        } else if (child instanceof OrFilter) {
-          // pull apart the kids of the OR expression
-          nonAndList.addAll(((OrFilter) child).getFilters());
-        } else {
-          nonAndList.add(child);
-        }
-      }
-      if (!andList.isEmpty()) {
-        Set<Filter> result = new HashSet<>();
-        generateAllCombinations(result, andList, nonAndList);
-        return new AndFilter(result);
-      }
-    }
-    return current;
-  }
-
-  // CNF conversion functions were adapted from Apache Hive, see:
-  // https://github.com/apache/hive/blob/branch-2.0/storage-api/src/java/org/apache/hadoop/hive/ql/io/sarg/SearchArgumentImpl.java
-  @VisibleForTesting
-  static Filter flatten(Filter root)
-  {
-    if (root instanceof BooleanFilter) {
-      List<Filter> children = new ArrayList<>(((BooleanFilter) root).getFilters());
-      // iterate through the index, so that if we add more children,
-      // they don't get re-visited
-      for (int i = 0; i < children.size(); ++i) {
-        Filter child = flatten(children.get(i));
-        // do we need to flatten?
-        if (child.getClass() == root.getClass() && !(child instanceof NotFilter)) {
-          boolean first = true;
-          Set<Filter> grandKids = ((BooleanFilter) child).getFilters();
-          for (Filter grandkid : grandKids) {
-            // for the first grandkid replace the original parent
-            if (first) {
-              first = false;
-              children.set(i, grandkid);
-            } else {
-              children.add(++i, grandkid);
-            }
-          }
-        } else {
-          children.set(i, child);
-        }
-      }
-      // if we have a singleton AND or OR, just return the child
-      if (children.size() == 1 && (root instanceof AndFilter || root instanceof OrFilter)) {
-        return children.get(0);
-      }
-
-      if (root instanceof AndFilter) {
-        return new AndFilter(children);
-      } else if (root instanceof OrFilter) {
-        return new OrFilter(children);
-      }
-    }
-    return root;
-  }
-
-  // CNF conversion functions were adapted from Apache Hive, see:
-  // https://github.com/apache/hive/blob/branch-2.0/storage-api/src/java/org/apache/hadoop/hive/ql/io/sarg/SearchArgumentImpl.java
-  private static void generateAllCombinations(
-      Set<Filter> result,
-      List<Filter> andList,
-      List<Filter> nonAndList
-  )
-  {
-    Set<Filter> children = ((AndFilter) andList.get(0)).getFilters();
-    if (result.isEmpty()) {
-      for (Filter child : children) {
-        Set<Filter> a = new HashSet<>(nonAndList);
-        a.add(child);
-        result.add(new OrFilter(a));
-      }
-    } else {
-      List<Filter> work = new ArrayList<>(result);
-      result.clear();
-      for (Filter child : children) {
-        for (Filter or : work) {
-          Set<Filter> a = new HashSet<>((((OrFilter) or).getFilters()));
-          a.add(child);
-          result.add(new OrFilter(a));
-        }
-      }
-    }
-    if (andList.size() > 1) {
-      generateAllCombinations(result, andList.subList(1, andList.size()), nonAndList);
-    }
   }
 
   /**
