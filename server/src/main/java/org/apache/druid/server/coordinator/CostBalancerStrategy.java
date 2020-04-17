dcdf09bc50b87aa5d7ffa1450f4e8229f84ba98a
samarthjain <samarth@apache.org>
2020-04-17 11:02:23 -0700

    Revert "Add logging to understand where time is spent  on coordinator during segment load"
    This reverts commit 438b87b409be5a665f2abba92248b13b30a89a4c.

--- server/src/main/java/org/apache/druid/server/coordinator/CostBalancerStrategy.java
+++ server/src/main/java/org/apache/druid/server/coordinator/CostBalancerStrategy.java
@@ -363,7 +363,6 @@ public class CostBalancerStrategy implements BalancerStrategy
       final boolean includeCurrentServer
   )
   {
-    long time = System.currentTimeMillis();
     Pair<Double, ServerHolder> bestServer = Pair.of(Double.POSITIVE_INFINITY, null);
 
     List<ListenableFuture<Pair<Double, ServerHolder>>> futures = new ArrayList<>();
@@ -395,7 +394,6 @@ public class CostBalancerStrategy implements BalancerStrategy
     catch (Exception e) {
       log.makeAlert(e, "Cost Balancer Multithread strategy wasn't able to complete cost computation.").emit();
     }
-    log.info("Time it took to figure out best server: " + (System.currentTimeMillis() - time));
     return bestServer;
   }
 }
