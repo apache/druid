5c53da14cf2fdb3627fa4e17cc3fcbba0efcf9bf
Xavier Léauté <xvrl@apache.org>
2020-05-20 17:30:45 -0700

    update kafka client version to 2.5.0
    - remove dependency on deprecated internal Kafka classes
    - upgrade zookeeper dependency to 3.5.x (required for kafka tests)
    - keep LZ4 version in line with the version shipped with Kafka

--- integration-tests/docker-base/setup.sh
+++ integration-tests/docker-base/setup.sh
@@ -31,18 +31,20 @@ apt-get install -y mysql-server
 apt-get install -y supervisor
 
 # Zookeeper
-wget -q -O /tmp/zookeeper-3.4.14.tar.gz "https://apache.org/dist/zookeeper/zookeeper-3.4.14/zookeeper-3.4.14.tar.gz"
-tar -xzf /tmp/zookeeper-3.4.14.tar.gz -C /usr/local
-cp /usr/local/zookeeper-3.4.14/conf/zoo_sample.cfg /usr/local/zookeeper-3.4.14/conf/zoo.cfg
-ln -s /usr/local/zookeeper-3.4.14 /usr/local/zookeeper
-rm /tmp/zookeeper-3.4.14.tar.gz
+ZK_VERSION=3.5.8
+wget -q -O /tmp/zookeeper-$ZK_VERSION.tar.gz "https://apache.org/dist/zookeeper/zookeeper-$ZK_VERSION/zookeeper-$ZK_VERSION.tar.gz"
+tar -xzf /tmp/zookeeper-$ZK_VERSION.tar.gz -C /usr/local
+cp /usr/local/zookeeper-$ZK_VERSION/conf/zoo_sample.cfg /usr/local/zookeeper-$ZK_VERSION/conf/zoo.cfg
+ln -s /usr/local/zookeeper-$ZK_VERSION /usr/local/zookeeper
+rm /tmp/zookeeper-$ZK_VERSION.tar.gz
 
 # Kafka
 # Match the version to the Kafka client used by KafkaSupervisor
-wget -q -O /tmp/kafka_2.12-2.1.1.tgz "https://apache.org/dist/kafka/2.1.1/kafka_2.12-2.1.1.tgz"
-tar -xzf /tmp/kafka_2.12-2.1.1.tgz -C /usr/local
-ln -s /usr/local/kafka_2.12-2.1.1 /usr/local/kafka
-rm /tmp/kafka_2.12-2.1.1.tgz
+KAFKA_VERSION=2.5.0
+wget -q -O /tmp/kafka_2.12-$KAFKA_VERSION.tgz "https://apache.org/dist/kafka/$KAFKA_VERSION/kafka_2.12-$KAFKA_VERSION.tgz"
+tar -xzf /tmp/kafka_2.12-$KAFKA_VERSION.tgz -C /usr/local
+ln -s /usr/local/kafka_2.12-$KAFKA_VERSION /usr/local/kafka
+rm /tmp/kafka_2.12-$KAFKA_VERSION.tgz
 
 # Druid system user
 adduser --system --group --no-create-home druid \
