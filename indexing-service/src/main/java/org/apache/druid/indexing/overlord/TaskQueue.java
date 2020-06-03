9f3a5a3922693ae9d0303d576adf46e3586d7d45
Jihoon Son <jihoonson@apache.org>
2020-06-03 10:48:57 -0700

    unused imports

--- indexing-service/src/main/java/org/apache/druid/indexing/overlord/TaskQueue.java
+++ indexing-service/src/main/java/org/apache/druid/indexing/overlord/TaskQueue.java
@@ -19,12 +19,9 @@
 
 package org.apache.druid.indexing.overlord;
 
-import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
