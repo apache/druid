9550f53ef3616ad7cb996a60782b462a706e3e07
Atul Mohan <atulmohan@yahoo-inc.com>
2020-06-05 09:07:46 -0500

    Remove annotation

--- server/src/main/java/org/apache/druid/metadata/SQLMetadataStorageActionHandler.java
+++ server/src/main/java/org/apache/druid/metadata/SQLMetadataStorageActionHandler.java
@@ -22,7 +22,6 @@ package org.apache.druid.metadata;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Optional;
 import com.google.common.base.Throwables;
 import com.google.common.collect.Maps;
@@ -173,7 +172,6 @@ public abstract class SQLMetadataStorageActionHandler<EntryType, StatusType, Log
     }
   }
 
-  @VisibleForTesting
   public static boolean isStatementException(Throwable e)
   {
     return e instanceof StatementException ||
