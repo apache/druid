f80c02da02351cbf5c430f293b6c20418321ef47
Maytas Monsereenusorn <maytasm@apache.org>
2020-06-23 15:54:37 -1000

    Fix HyperUniquesAggregatorFactory.estimateCardinality null handling to respect output type (#10063)
    * fix return type from HyperUniquesAggregator/HyperUniquesVectorAggregator
    
    * address comments
    
    * address comments

--- processing/src/test/java/org/apache/druid/query/topn/TopNQueryRunnerTest.java
+++ processing/src/test/java/org/apache/druid/query/topn/TopNQueryRunnerTest.java
@@ -647,15 +647,15 @@ public class TopNQueryRunnerTest extends InitializedNullHandlingTest
                 Arrays.<Map<String, Object>>asList(
                     ImmutableMap.<String, Object>builder()
                         .put("market", "spot")
-                        .put("uniques", 0)
+                        .put("uniques", 0d)
                         .build(),
                     ImmutableMap.<String, Object>builder()
                         .put("market", "total_market")
-                        .put("uniques", 0)
+                        .put("uniques", 0d)
                         .build(),
                     ImmutableMap.<String, Object>builder()
                         .put("market", "upfront")
-                        .put("uniques", 0)
+                        .put("uniques", 0d)
                         .build()
                 )
             )
