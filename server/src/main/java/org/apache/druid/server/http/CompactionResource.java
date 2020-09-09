ab5454b570d95266cde27530c41cacc1d358b72f
Maytas Monsereenusorn <maytasm@apache.org>
2020-09-08 22:51:55 -0700

    Auto-compaction snapshot API

--- server/src/main/java/org/apache/druid/server/http/CompactionResource.java
+++ server/src/main/java/org/apache/druid/server/http/CompactionResource.java
@@ -36,9 +36,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 @Path("/druid/coordinator/v1/compaction")
 public class CompactionResource
