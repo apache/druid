ea0a561b0f8fd5723b88cfc9ea2e397870d75a10
frank chen <frank.chen021@outlook.com>
2020-09-11 12:06:56 +0800

    support multi-line text

--- core/src/main/java/org/apache/druid/data/input/impl/JsonReader.java
+++ core/src/main/java/org/apache/druid/data/input/impl/JsonReader.java
@@ -19,12 +19,16 @@
 
 package org.apache.druid.data.input.impl;
 
+import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.druid.data.input.InputEntity;
+import org.apache.druid.data.input.InputEntityReader;
 import org.apache.druid.data.input.InputRow;
+import org.apache.druid.data.input.InputRowListPlusRawValues;
 import org.apache.druid.data.input.InputRowSchema;
-import org.apache.druid.data.input.TextReader;
+import org.apache.druid.java.util.common.parsers.CloseableIterator;
 import org.apache.druid.java.util.common.parsers.JSONFlattenerMaker;
 import org.apache.druid.java.util.common.parsers.JSONPathSpec;
 import org.apache.druid.java.util.common.parsers.ObjectFlattener;
@@ -33,13 +37,15 @@ import org.apache.druid.java.util.common.parsers.ParseException;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
-public class JsonReader extends TextReader
+public class JsonReader implements InputEntityReader
 {
   private final ObjectFlattener<JsonNode> flattener;
   private final ObjectMapper mapper;
+  private final InputEntity source;
+  private final InputRowSchema inputRowSchema;
 
   JsonReader(
       InputRowSchema inputRowSchema,
@@ -49,41 +55,99 @@ public class JsonReader extends TextReader
       boolean keepNullColumns
   )
   {
-    super(inputRowSchema, source);
+    this.inputRowSchema = inputRowSchema;
+    this.source = source;
     this.flattener = ObjectFlatteners.create(flattenSpec, new JSONFlattenerMaker(keepNullColumns));
     this.mapper = mapper;
   }
 
+
   @Override
-  public List<InputRow> parseInputRows(String line) throws IOException, ParseException
+  public CloseableIterator<InputRow> read() throws IOException
   {
-    final JsonNode document = mapper.readValue(line, JsonNode.class);
-    final Map<String, Object> flattened = flattener.flatten(document);
-    return Collections.singletonList(MapInputRowParser.parse(getInputRowSchema(), flattened));
+    final MappingIterator<JsonNode> delegate = mapper.readValues(
+        new JsonFactory().createParser(this.source.open()),
+        JsonNode.class
+    );
+
+    return new CloseableIterator<InputRow>()
+    {
+      @Override
+      public boolean hasNext()
+      {
+        return delegate.hasNext();
+      }
+
+      @Override
+      public InputRow next()
+      {
+        if (!hasNext()) {
+          throw new NoSuchElementException();
+        }
+
+        return toInputRow(delegate.next());
+      }
+
+      @Override
+      public void close()
+      {
+      }
+    };
   }
 
   @Override
-  public Map<String, Object> toMap(String intermediateRow) throws IOException
+  public CloseableIterator<InputRowListPlusRawValues> sample() throws IOException
   {
-    //noinspection unchecked
-    return mapper.readValue(intermediateRow, Map.class);
+    final MappingIterator<Map> delegate = mapper.readValues(
+        new JsonFactory().createParser(this.source.open()),
+        Map.class
+    );
+
+    return new CloseableIterator<InputRowListPlusRawValues>()
+    {
+      @Override
+      public boolean hasNext()
+      {
+        return delegate.hasNext();
+      }
+
+      @Override
+      public InputRowListPlusRawValues next()
+      {
+        if (!hasNext()) {
+          throw new NoSuchElementException();
+        }
+
+        final Map<String, Object> rawColumns;
+        try {
+          rawColumns = delegate.next();
+        }
+        catch (Exception e) {
+          return InputRowListPlusRawValues.of(null, new ParseException(e, "Unable to parse row into JSON"));
+        }
+        try {
+          return InputRowListPlusRawValues.of(Collections.singletonList(toInputRow(rawColumns)), rawColumns);
+        }
+        catch (ParseException e) {
+          return InputRowListPlusRawValues.of(rawColumns, e);
+        }
+      }
+
+      @Override
+      public void close()
+      {
+      }
+    };
   }
 
-  @Override
-  public int getNumHeaderLinesToSkip()
+  private InputRow toInputRow(Map map)
   {
-    return 0;
+    JsonNode node = mapper.valueToTree(map);
+    return toInputRow(node);
   }
 
-  @Override
-  public boolean needsToProcessHeaderLine()
+  private InputRow toInputRow(JsonNode node)
   {
-    return false;
-  }
-
-  @Override
-  public void processHeaderLine(String line)
-  {
-    // do nothing
+    return MapInputRowParser.parse(inputRowSchema, flattener.flatten(node));
   }
 }
