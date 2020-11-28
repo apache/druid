2b7d1063593757c636eb843d7015c58d240cbf46
Clint Wylie <cwylie@apache.org>
2020-11-27 16:25:08 -0800

    cache expression selector results by associating vector expression bindings to underlying vector offset

--- processing/src/main/java/org/apache/druid/segment/vector/VectorColumnSelectorFactory.java
+++ processing/src/main/java/org/apache/druid/segment/vector/VectorColumnSelectorFactory.java
@@ -33,9 +33,9 @@ import javax.annotation.Nullable;
 public interface VectorColumnSelectorFactory extends ColumnInspector
 {
   /**
-   * Returns a {@link VectorSizeInspector} for the {@link VectorCursor} that generated this object.
+   * Returns a {@link ReadableVectorOffset} for the {@link VectorCursor} that generated this object.
    */
-  VectorSizeInspector getVectorSizeInspector();
+  ReadableVectorOffset getReadableVectorOffset();
 
   /**
    * Returns the maximum vector size for the {@link VectorCursor} that generated this object.
@@ -44,7 +44,7 @@ public interface VectorColumnSelectorFactory extends ColumnInspector
    */
   default int getMaxVectorSize()
   {
-    return getVectorSizeInspector().getMaxVectorSize();
+    return getReadableVectorOffset().getMaxVectorSize();
   }
 
   /**
