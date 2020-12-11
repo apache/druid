1324ab9dbfbfb759ad9a6928ed7c1f9b2ddeabf5
Lucas.Capistrant <Lucas.Capistrant@target.com>
2020-12-11 17:28:02 -0600

    fix issues preventing ITIndexHadoopTest from running in local mode

--- integration-tests/src/main/java/org/apache/druid/testing/utils/ITRetryUtil.java
+++ integration-tests/src/main/java/org/apache/druid/testing/utils/ITRetryUtil.java
@@ -30,7 +30,7 @@ public class ITRetryUtil
 
   private static final Logger LOG = new Logger(ITRetryUtil.class);
 
-  public static final int DEFAULT_RETRY_COUNT = 150; // 5 minutes
+  public static final int DEFAULT_RETRY_COUNT = 300; // 10 minutes
 
   public static final long DEFAULT_RETRY_SLEEP = TimeUnit.SECONDS.toMillis(2);
 
