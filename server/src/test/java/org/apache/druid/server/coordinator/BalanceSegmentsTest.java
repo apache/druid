9197bb0b1738be30f79016dab9837002a8056152
Lucas.Capistrant <Lucas.Capistrant@target.com>
2020-12-18 18:42:24 -0600

    Add a test that helps ensure not regressions for percentOfSegmentsToConsiderPerMove

--- server/src/test/java/org/apache/druid/server/coordinator/BalanceSegmentsTest.java
+++ server/src/test/java/org/apache/druid/server/coordinator/BalanceSegmentsTest.java
@@ -536,6 +536,76 @@ public class BalanceSegmentsTest
     Assert.assertTrue(params.getCoordinatorStats().getTieredStat("movedCount", "normal") > 0);
   }
 
+  /**
+   * Testing that the dynamic coordinator config value, percentOfSegmentsToConsiderPerMove, is honored when calling
+   * out to pickSegmentToMove. This config limits the number of segments that are considered when looking for a segment
+   * to move.
+   */
+  @Test
+  public void testThatDynamicConfigIsHonoredWhenPickingSegmentToMove()
+  {
+    mockDruidServer(druidServer1, "1", "normal", 50L, 100L, Arrays.asList(segment1, segment2));
+    mockDruidServer(druidServer2, "2", "normal", 30L, 100L, Arrays.asList(segment3, segment4));
+    mockDruidServer(druidServer3, "3", "normal", 0L, 100L, Collections.emptyList());
+
+    EasyMock.replay(druidServer4);
+
+    mockCoordinator(coordinator);
+
+    BalancerStrategy strategy = EasyMock.createMock(BalancerStrategy.class);
+
+    // The first call for decommissioning servers
+    EasyMock.expect(
+        strategy.pickSegmentToMove(
+            ImmutableList.of(),
+            broadcastDatasources,
+            40
+        )
+    )
+            .andReturn(null);
+
+    // The second call for the single non decommissioning server move
+    EasyMock.expect(
+        strategy.pickSegmentToMove(
+            ImmutableList.of(
+                new ServerHolder(druidServer3, peon3, false),
+                new ServerHolder(druidServer2, peon2, false),
+                new ServerHolder(druidServer1, peon1, false)
+            ),
+            broadcastDatasources,
+            40
+        )
+    )
+            .andReturn(new BalancerSegmentHolder(druidServer2, segment3));
+
+    EasyMock.expect(strategy.findNewSegmentHomeBalancer(EasyMock.anyObject(), EasyMock.anyObject()))
+            .andReturn(new ServerHolder(druidServer3, peon3))
+            .anyTimes();
+    EasyMock.replay(strategy);
+
+    DruidCoordinatorRuntimeParams params = defaultRuntimeParamsBuilder(
+        ImmutableList.of(druidServer1, druidServer2, druidServer3),
+        ImmutableList.of(peon1, peon2, peon3),
+        ImmutableList.of(false, false, false)
+    )
+        .withDynamicConfigs(
+            CoordinatorDynamicConfig.builder()
+                                    .withMaxSegmentsToMove(1)
+                                    .withPercentOfSegmentsToConsiderPerMove(40)
+                                    .build()
+        )
+        .withBalancerStrategy(strategy)
+        .withBroadcastDatasources(broadcastDatasources)
+        .build();
+
+    params = new BalanceSegmentsTester(coordinator).run(params);
+    Assert.assertEquals(1L, params.getCoordinatorStats().getTieredStat("movedCount", "normal"));
+    Assert.assertThat(
+        peon3.getSegmentsToLoad(),
+        Matchers.is(Matchers.equalTo(ImmutableSet.of(segment3)))
+    );
+  }
+
   private DruidCoordinatorRuntimeParams.Builder defaultRuntimeParamsBuilder(
       List<ImmutableDruidServer> druidServers,
       List<LoadQueuePeon> peons
