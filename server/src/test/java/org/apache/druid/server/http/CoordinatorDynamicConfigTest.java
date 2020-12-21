d76c92487e4e70c75633ab0ead3670e6152af3cb
Lucas.Capistrant <Lucas.Capistrant@target.com>
2020-12-21 10:24:53 -0600

    remove unused import

--- server/src/test/java/org/apache/druid/server/http/CoordinatorDynamicConfigTest.java
+++ server/src/test/java/org/apache/druid/server/http/CoordinatorDynamicConfigTest.java
@@ -22,7 +22,6 @@ package org.apache.druid.server.http;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableSet;
-import org.apache.druid.guice.annotations.Json;
 import org.apache.druid.java.util.common.IAE;
 import org.apache.druid.segment.TestHelper;
 import org.apache.druid.server.coordinator.CoordinatorDynamicConfig;
