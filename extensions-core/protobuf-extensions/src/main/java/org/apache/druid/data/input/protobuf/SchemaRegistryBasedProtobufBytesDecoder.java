4db4f8cffd95041b248f8b83662b3673a75dd8cf
yuanyi <yuanyi@freewheel.tv>
2021-01-18 14:12:20 +0800

    add schema registry support for protobuf

---
+++ extensions-core/protobuf-extensions/src/main/java/org/apache/druid/data/input/protobuf/SchemaRegistryBasedProtobufBytesDecoder.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.data.input.protobuf;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.DynamicMessage;
+import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchema;
+import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchemaProvider;
+import org.apache.druid.java.util.common.logger.Logger;
+import org.apache.druid.java.util.common.parsers.ParseException;
+
+import java.nio.ByteBuffer;
+import java.util.Collections;
+import java.util.Objects;
+
+public class SchemaRegistryBasedProtobufBytesDecoder implements ProtobufBytesDecoder
+{
+
+  private static final Logger LOGGER = new Logger(SchemaRegistryBasedProtobufBytesDecoder.class);
+
+  private final SchemaRegistryClient registry;
+
+  @JsonCreator
+  public SchemaRegistryBasedProtobufBytesDecoder(
+      @JsonProperty("url") String url,
+      @JsonProperty("capacity") Integer capacity
+  )
+  {
+    int identityMapCapacity = capacity == null ? Integer.MAX_VALUE : capacity;
+    registry = new CachedSchemaRegistryClient(Collections.singletonList(url), identityMapCapacity, Collections.singletonList(new ProtobufSchemaProvider()), null);
+  }
+
+  //For UT only
+  @VisibleForTesting
+  SchemaRegistryBasedProtobufBytesDecoder(SchemaRegistryClient registry)
+  {
+    this.registry = registry;
+  }
+
+  @Override
+  public DynamicMessage parse(ByteBuffer bytes)
+  {
+    try {
+      bytes.get(); // ignore first \0 byte
+      int id = bytes.getInt(); // extract schema registry id
+      bytes.get(); // ignore \0 byte before PB message
+      int length = bytes.limit() - 2 - 4;
+      ProtobufSchema schema = (ProtobufSchema) registry.getSchemaById(id);
+      Descriptors.Descriptor descriptor = schema.toDescriptor();
+      byte[] rawMessage = new byte[length];
+      bytes.get(rawMessage, 0, length);
+      DynamicMessage message = DynamicMessage.parseFrom(descriptor, rawMessage);
+      return message;
+    }
+    catch (Exception e) {
+      LOGGER.error(e.getMessage());
+      throw new ParseException(e, "Fail to decode protobuf message!");
+    }
+  }
+
+  @Override
+  public boolean equals(Object o)
+  {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    SchemaRegistryBasedProtobufBytesDecoder that = (SchemaRegistryBasedProtobufBytesDecoder) o;
+
+    return Objects.equals(registry, that.registry);
+  }
+
+  @Override
+  public int hashCode()
+  {
+    return registry != null ? registry.hashCode() : 0;
+  }
+}
