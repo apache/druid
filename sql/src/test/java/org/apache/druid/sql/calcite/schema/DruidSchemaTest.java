8c6153d511e4bd9ea365190a59f4e29bdb402f97
zhangyue19921010 <69956021+zhangyue19921010@users.noreply.github.com>
2021-01-23 00:48:21 +0800

    [Bug Fix] Broker will not wait for its SQL metadata view to fully initialize before starting up, even though set awaitInitializationOnStart true (#10779)
    * enhance the logic of Start up DruidSchema immediately if there are no segments.
    
    * add UT to test DruidSchema init
    
    Co-authored-by: yuezhang <yuezhang@freewheel.tv>

--- sql/src/test/java/org/apache/druid/sql/calcite/schema/DruidSchemaTest.java
+++ sql/src/test/java/org/apache/druid/sql/calcite/schema/DruidSchemaTest.java
@@ -137,6 +137,7 @@ public class DruidSchemaTest extends CalciteTestBase
 
   private SpecificSegmentsQuerySegmentWalker walker = null;
   private DruidSchema schema = null;
+  private DruidSchema schema2 = null;
   private SegmentManager segmentManager;
   private Set<String> segmentDataSourceNames;
   private Set<String> joinableDataSourceNames;
@@ -269,6 +270,38 @@ public class DruidSchemaTest extends CalciteTestBase
       }
     };
 
+    schema2 = new DruidSchema(
+            CalciteTests.createMockQueryLifecycleFactory(walker, conglomerate),
+            serverView,
+            segmentManager,
+            new MapJoinableFactory(ImmutableSet.of(globalTableJoinable), ImmutableMap.of(globalTableJoinable.getClass(), GlobalTableDataSource.class)),
+            PLANNER_CONFIG_DEFAULT,
+            new NoopViewManager(),
+            new NoopEscalator()
+    )
+    {
+
+      boolean throwException = true;
+      @Override
+      protected DruidTable buildDruidTable(String dataSource)
+      {
+        DruidTable table = super.buildDruidTable(dataSource);
+        buildTableLatch.countDown();
+        return table;
+      }
+
+      @Override
+      Set<SegmentId> refreshSegments(final Set<SegmentId> segments) throws IOException
+      {
+        if (throwException) {
+          throwException = false;
+          throw new RuntimeException("Query[xxxx] url[http://xxxx:8083/druid/v2/] timed out.");
+        } else {
+          return super.refreshSegments(segments);
+        }
+      }
+    };
+
     schema.start();
     schema.awaitInitialization();
   }
@@ -288,6 +321,19 @@ public class DruidSchemaTest extends CalciteTestBase
     final Map<String, Table> tableMap = schema.getTableMap();
     Assert.assertEquals(ImmutableSet.of("foo", "foo2"), tableMap.keySet());
   }
+
+  @Test
+  public void testSchemaInit() throws InterruptedException
+  {
+    schema2.start();
+    schema2.awaitInitialization();
+    Map<String, Table> tableMap = schema2.getTableMap();
+    Assert.assertEquals(2, tableMap.size());
+    Assert.assertTrue(tableMap.containsKey("foo"));
+    Assert.assertTrue(tableMap.containsKey("foo2"));
+    schema2.stop();
+  }
+
 
   @Test
   public void testGetTableMapFoo()
