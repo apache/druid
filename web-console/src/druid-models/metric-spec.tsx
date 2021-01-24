90993d961928759f12efa0b6a02e9f562ae1ec07
Vadim Ogievetsky <vadim@ogievetsky.com>
2021-01-24 12:32:49 -0800

    Remove first / last suggestions

--- web-console/src/druid-models/metric-spec.tsx
+++ web-console/src/druid-models/metric-spec.tsx
@@ -67,14 +67,17 @@ export const METRIC_SPEC_FIELDS: Field<MetricSpec>[] = [
         group: 'max',
         suggestions: ['longMax', 'doubleMax', 'floatMax'],
       },
-      {
-        group: 'first',
-        suggestions: ['longFirst', 'doubleFirst', 'floatFirst'],
-      },
-      {
-        group: 'last',
-        suggestions: ['longLast', 'doubleLast', 'floatLast'],
-      },
+      // Do not show first and last aggregators as they can not be used in ingestion specs and this definition is only used in the data loader.
+      // Ref: https://druid.apache.org/docs/latest/querying/aggregations.html#first--last-aggregator
+      // Should the first / last aggregators become usable at ingestion time, comment in the lines below:
+      // {
+      //   group: 'first',
+      //   suggestions: ['longFirst', 'doubleFirst', 'floatFirst'],
+      // },
+      // {
+      //   group: 'last',
+      //   suggestions: ['longLast', 'doubleLast', 'floatLast'],
+      // },
       'thetaSketch',
       {
         group: 'HLLSketch',
