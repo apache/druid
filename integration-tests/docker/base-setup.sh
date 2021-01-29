942f8afc20f9489224f69eb35739916d06bdb856
Xavier Léauté <xvrl@apache.org>
2021-01-28 23:50:16 -0800

    add 3.4 ZK integration test

--- integration-tests/docker/base-setup.sh
+++ integration-tests/docker/base-setup.sh
@@ -42,13 +42,12 @@ install_zk() {
 ZK_VERSION=3.4.14
 ZK_TAR=zookeeper-$ZK_VERSION
 install_zk
+ln -s /usr/local/$ZK_TAR /usr/local/zookeeper-3.4
 
 ZK_VERSION=3.5.9
 ZK_TAR=apache-zookeeper-$ZK_VERSION-bin
 install_zk
-
-# set default ZK version
-ln -s /usr/local/$ZK_TAR /usr/local/zookeeper
+ln -s /usr/local/$ZK_TAR /usr/local/zookeeper-3.5
 
 # Kafka
 # Match the version to the Kafka client used by KafkaSupervisor
