7612764f16f4964db2dacf1f6bb7cbb7aa71f15a
Maytas Monsereenusorn <maytasm@apache.org>
2021-03-11 00:35:56 -0800

    fix IT

--- integration-tests/src/test/java/org/apache/druid/tests/indexer/AbstractStreamIndexingTest.java
+++ integration-tests/src/test/java/org/apache/druid/tests/indexer/AbstractStreamIndexingTest.java
@@ -188,7 +188,7 @@ public abstract class AbstractStreamIndexingTest extends AbstractIndexerTest
         EVENTS_PER_SECOND,
         CYCLE_PADDING_MS
     );
-    final GeneratedTestConfig generatedTestConfig = new GeneratedTestConfig(parserType, getResourceAsString(specPath));
+    final GeneratedTestConfig generatedTestConfig = new GeneratedTestConfig(parserType, getResourceAsString(specPath), true);
     try (
         final Closeable closer = createResourceCloser(generatedTestConfig);
         final StreamEventWriter streamEventWriter = createStreamEventWriter(config, transactionEnabled)
@@ -241,7 +241,8 @@ public abstract class AbstractStreamIndexingTest extends AbstractIndexerTest
   {
     final GeneratedTestConfig generatedTestConfig = new GeneratedTestConfig(
         INPUT_FORMAT,
-        getResourceAsString(JSON_INPUT_FORMAT_PATH)
+        getResourceAsString(JSON_INPUT_FORMAT_PATH),
+        true
     );
     try (
         final Closeable closer = createResourceCloser(generatedTestConfig);
@@ -304,7 +305,8 @@ public abstract class AbstractStreamIndexingTest extends AbstractIndexerTest
   {
     final GeneratedTestConfig generatedTestConfig = new GeneratedTestConfig(
             INPUT_FORMAT,
-            getResourceAsString(JSON_INPUT_FORMAT_PATH)
+            getResourceAsString(JSON_INPUT_FORMAT_PATH),
+            true
     );
     try (
             final Closeable closer = createResourceCloser(generatedTestConfig);
@@ -384,7 +386,8 @@ public abstract class AbstractStreamIndexingTest extends AbstractIndexerTest
   {
     final GeneratedTestConfig generatedTestConfig = new GeneratedTestConfig(
         INPUT_FORMAT,
-        getResourceAsString(JSON_INPUT_FORMAT_PATH)
+        getResourceAsString(JSON_INPUT_FORMAT_PATH),
+        true
     );
     try (
         final Closeable closer = createResourceCloser(generatedTestConfig);
@@ -461,7 +464,9 @@ public abstract class AbstractStreamIndexingTest extends AbstractIndexerTest
   {
     final GeneratedTestConfig generatedTestConfig = new GeneratedTestConfig(
         INPUT_FORMAT,
-        getResourceAsString(JSON_INPUT_FORMAT_PATH)
+        getResourceAsString(JSON_INPUT_FORMAT_PATH),
+        // getTaskStats does not work when datasource name has special characters
+        false
     );
     try (
         final Closeable closer = createResourceCloser(generatedTestConfig);
@@ -646,7 +651,7 @@ public abstract class AbstractStreamIndexingTest extends AbstractIndexerTest
     private Function<String, String> streamIngestionPropsTransform;
     private Function<String, String> streamQueryPropsTransform;
 
-    GeneratedTestConfig(String parserType, String parserOrInputFormat) throws Exception
+    GeneratedTestConfig(String parserType, String parserOrInputFormat, boolean useExtraDatasourceNameSuffix) throws Exception
     {
       streamName = getTestNamePrefix() + "_index_test_" + UUID.randomUUID();
       String datasource = getTestNamePrefix() + "_indexing_service_test_" + UUID.randomUUID();
@@ -662,7 +667,11 @@ public abstract class AbstractStreamIndexingTest extends AbstractIndexerTest
           30,
           "Wait for stream active"
       );
-      fullDatasourceName = datasource + config.getExtraDatasourceNameSuffix();
+      if (useExtraDatasourceNameSuffix) {
+        fullDatasourceName = datasource + config.getExtraDatasourceNameSuffix();
+      } else {
+        fullDatasourceName = datasource;
+      }
       streamIngestionPropsTransform = generateStreamIngestionPropsTransform(
           streamName,
           fullDatasourceName,
