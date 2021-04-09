c7fa3e83300ad446b9153ca752b88a18cfcf9217
zachary sherman <zachjsh@gmail.com>
2021-04-09 02:41:27 -0400

    * Cleanup test

--- indexing-hadoop/src/test/java/org/apache/druid/indexer/JobHelperPowerMockTest.java
+++ indexing-hadoop/src/test/java/org/apache/druid/indexer/JobHelperPowerMockTest.java
@@ -19,25 +19,10 @@
 
 package org.apache.druid.indexer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import org.apache.druid.data.input.impl.DimensionsSpec;
-import org.apache.druid.data.input.impl.JSONParseSpec;
-import org.apache.druid.data.input.impl.TimestampSpec;
-import org.apache.druid.indexer.path.StaticPathSpec;
 import org.apache.druid.java.util.common.IOE;
-import org.apache.druid.java.util.common.Intervals;
-import org.apache.druid.java.util.common.granularity.Granularities;
-import org.apache.druid.java.util.common.jackson.JacksonUtils;
-import org.apache.druid.java.util.common.parsers.JSONPathSpec;
-import org.apache.druid.query.aggregation.AggregatorFactory;
-import org.apache.druid.query.aggregation.CountAggregatorFactory;
-import org.apache.druid.segment.TestHelper;
-import org.apache.druid.segment.indexing.DataSchema;
-import org.apache.druid.segment.indexing.granularity.UniformGranularitySpec;
 import org.apache.druid.timeline.DataSegment;
-import org.apache.druid.timeline.partition.NoneShardSpec;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -55,97 +40,46 @@ import java.util.List;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({
-    FileSystemHelper.class
+    FileSystemHelper.class,
+    HadoopDruidIndexerConfig.class
 })
 @PowerMockIgnore({"javax.net.ssl.*"})
 public class JobHelperPowerMockTest
 {
-  private static final DataSegment DATA_SEGMENT = new DataSegment(
-      "test1",
-      Intervals.of("2000/3000"),
-      "ver",
-      ImmutableMap.of(
-          "type", "google",
-          "bucket", "test-test",
-          "path", "tmp/foo:bar/index1.zip"
-      ),
-      ImmutableList.of(),
-      ImmutableList.of(),
-      NoneShardSpec.instance(),
-      9,
-      1024
-  );
-  private static final ObjectMapper JSON_MAPPER = TestHelper.makeJsonMapper();
-  private static final DataSchema DATA_SCHEMA = new DataSchema(
-      "test_ds",
-      JSON_MAPPER.convertValue(
-          new HadoopyStringInputRowParser(
-              new JSONParseSpec(
-                  new TimestampSpec("t", "auto", null),
-                  new DimensionsSpec(
-                      DimensionsSpec.getDefaultSchemas(ImmutableList.of("dim1", "dim1t", "dim2")),
-                      null,
-                      null
-                  ),
-                  new JSONPathSpec(true, ImmutableList.of()),
-                  ImmutableMap.of(),
-                  null
-              )
-          ),
-          JacksonUtils.TYPE_REFERENCE_MAP_STRING_OBJECT
-      ),
-      new AggregatorFactory[]{new CountAggregatorFactory("rows")},
-      new UniformGranularitySpec(Granularities.DAY, Granularities.NONE, null),
-      null,
-      JSON_MAPPER
-  );
-
-  private static final HadoopIOConfig IO_CONFIG = new HadoopIOConfig(
-      JSON_MAPPER.convertValue(
-          new StaticPathSpec("dummyPath", null),
-          JacksonUtils.TYPE_REFERENCE_MAP_STRING_OBJECT
-      ),
-      null,
-      "dummyOutputPath"
-  );
-
-  private static final HadoopTuningConfig TUNING_CONFIG = HadoopTuningConfig
-      .makeDefaultTuningConfig()
-      .withWorkingPath("dummyWorkingPath");
-  private static final HadoopIngestionSpec DUMMY_SPEC = new HadoopIngestionSpec(DATA_SCHEMA, IO_CONFIG, TUNING_CONFIG);
+  private static final String TMP_PATH = "/tmp/index.zip.0";
+  private static final String FINAL_PATH = "/final/index.zip.0";
 
   @Test
   public void test_renameIndexFilesForSegments_emptySegments() throws IOException
   {
+    HadoopIngestionSpec ingestionSpec = mockIngestionSpec();
     List<DataSegmentAndIndexZipFilePath> segmentAndIndexZipFilePaths = ImmutableList.of();
-    JobHelper.renameIndexFilesForSegments(DUMMY_SPEC, segmentAndIndexZipFilePaths);
+
+    PowerMock.replayAll();
+
+    JobHelper.renameIndexFilesForSegments(ingestionSpec, segmentAndIndexZipFilePaths);
+
+    PowerMock.verifyAll();
   }
 
   @Test
   public void test_renameIndexFilesForSegments_segmentIndexFileRenamedSuccessfully()
       throws IOException
   {
-    String tmpPath = "/tmp/index.zip.0";
-    String finalPath = "/final/index.zip.0";
+    HadoopIngestionSpec ingestionSpec = mockIngestionSpec();
+    mockFileSystem(true);
+    DataSegment segment = PowerMock.createMock(DataSegment.class);
+
     List<DataSegmentAndIndexZipFilePath> segmentAndIndexZipFilePaths = ImmutableList.of(
         new DataSegmentAndIndexZipFilePath(
-            DATA_SEGMENT,
-            tmpPath,
-            finalPath
+            segment,
+            TMP_PATH,
+            FINAL_PATH
         )
     );
-    PowerMock.mockStaticNice(FileSystemHelper.class);
-    FileSystem fileSystem = PowerMock.createMock(FileSystem.class);
-    EasyMock.expect(FileSystemHelper.get(
-        EasyMock.anyObject(URI.class),
-        EasyMock.anyObject(Configuration.class)
-    )).andReturn(fileSystem);
-    EasyMock.expect(fileSystem.exists(EasyMock.anyObject(Path.class))).andReturn(false);
-    EasyMock.expect(fileSystem.rename(EasyMock.anyObject(Path.class), EasyMock.anyObject(Path.class))).andReturn(true);
-
     PowerMock.replayAll();
 
-    JobHelper.renameIndexFilesForSegments(DUMMY_SPEC, segmentAndIndexZipFilePaths);
+    JobHelper.renameIndexFilesForSegments(ingestionSpec, segmentAndIndexZipFilePaths);
 
     PowerMock.verifyAll();
   }
@@ -154,15 +88,38 @@ public class JobHelperPowerMockTest
   public void test_renameIndexFilesForSegments_segmentIndexFileRenamedFailed_throwsException()
       throws IOException
   {
-    String tmpPath = "/tmp/index.zip.0";
-    String finalPath = "/final/index.zip.0";
+    HadoopIngestionSpec ingestionSpec = mockIngestionSpec();
+    mockFileSystem(false);
+    DataSegment segment = PowerMock.createMock(DataSegment.class);
     List<DataSegmentAndIndexZipFilePath> segmentAndIndexZipFilePaths = ImmutableList.of(
         new DataSegmentAndIndexZipFilePath(
-            DATA_SEGMENT,
-            tmpPath,
-            finalPath
+            segment,
+            TMP_PATH,
+            FINAL_PATH
         )
     );
+
+    PowerMock.replayAll();
+
+    JobHelper.renameIndexFilesForSegments(ingestionSpec, segmentAndIndexZipFilePaths);
+
+    PowerMock.verifyAll();
+  }
+
+  private HadoopIngestionSpec mockIngestionSpec()
+  {
+    HadoopDruidIndexerConfig indexerConfig = PowerMock.createMock(HadoopDruidIndexerConfig.class);
+    HadoopIngestionSpec ingestionSpec = PowerMock.createMock(HadoopIngestionSpec.class);
+    PowerMock.mockStaticNice(HadoopDruidIndexerConfig.class);
+    EasyMock.expect(indexerConfig.getAllowedProperties()).andReturn(ImmutableMap.of()).anyTimes();
+    indexerConfig.addJobProperties(EasyMock.anyObject(Configuration.class));
+    EasyMock.expectLastCall();
+    EasyMock.expect(HadoopDruidIndexerConfig.fromSpec(ingestionSpec)).andReturn(indexerConfig);
+    return ingestionSpec;
+  }
+
+  private void mockFileSystem(boolean renameSuccess) throws IOException
+  {
     PowerMock.mockStaticNice(FileSystemHelper.class);
     FileSystem fileSystem = PowerMock.createMock(FileSystem.class);
     EasyMock.expect(FileSystemHelper.get(
@@ -170,12 +127,7 @@ public class JobHelperPowerMockTest
         EasyMock.anyObject(Configuration.class)
     )).andReturn(fileSystem);
     EasyMock.expect(fileSystem.exists(EasyMock.anyObject(Path.class))).andReturn(false);
-    EasyMock.expect(fileSystem.rename(EasyMock.anyObject(Path.class), EasyMock.anyObject(Path.class))).andReturn(false);
-
-    PowerMock.replayAll();
-
-    JobHelper.renameIndexFilesForSegments(DUMMY_SPEC, segmentAndIndexZipFilePaths);
-
-    PowerMock.verifyAll();
+    EasyMock.expect(fileSystem.rename(EasyMock.anyObject(Path.class), EasyMock.anyObject(Path.class)))
+            .andReturn(renameSuccess);
   }
 }
