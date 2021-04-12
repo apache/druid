apache/druid

commit c0e6d1c7f81c8e19fdcf6f1bd2c11fd3eb926bb0

vectorize 'auto' long decoding (#11004)
* Vectorize LongDeserializers.

Also, add many more tests.

* more faster

* more more faster

* more cleanup

* fixes

* forbidden

* benchmark style

* idk why

* adjust

* add preconditions for value >= 0 for writers

* add 64 bit exception

Co-authored-by: Gian Merlino <gian@imply.io>

processing/src/main/java/org/apache/druid/segment/data/CompressionFactory.java
@@ -283,26 +283,9 @@ public class CompressionFactory
 
     long read(int index);
 
-    default void read(long[] out, int outPosition, int startIndex, int length)
-    {
-      for (int i = 0; i < length; i++) {
-        out[outPosition + i] = read(startIndex + i);
-      }
-    }
+    void read(long[] out, int outPosition, int startIndex, int length);
 
-    default int read(long[] out, int outPosition, int[] indexes, int length, int indexOffset, int limit)
-    {
-      for (int i = 0; i < length; i++) {
-        int index = indexes[outPosition + i] - indexOffset;
-        if (index >= limit) {
-          return i;
-        }
-
-        out[outPosition + i] = read(index);
-      }
-
-      return length;
-    }
+    int read(long[] out, int outPosition, int[] indexes, int length, int indexOffset, int limit);
 
     LongEncodingReader duplicate();
   }

processing/src/main/java/org/apache/druid/segment/data/LongsLongEncodingReader.java
@@ -19,52 +19,56 @@
 
 package org.apache.druid.segment.data;
 
+import org.apache.datasketches.memory.Memory;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.nio.LongBuffer;
 
 public class LongsLongEncodingReader implements CompressionFactory.LongEncodingReader
 {
-  private LongBuffer buffer;
+  private Memory buffer;
 
   public LongsLongEncodingReader(ByteBuffer fromBuffer, ByteOrder order)
   {
-    this.buffer = fromBuffer.asReadOnlyBuffer().order(order).asLongBuffer();
-  }
-
-  private LongsLongEncodingReader(LongBuffer buffer)
-  {
-    this.buffer = buffer;
+    this.buffer = Memory.wrap(fromBuffer.slice(), order);
   }
 
   @Override
   public void setBuffer(ByteBuffer buffer)
   {
-    this.buffer = buffer.asLongBuffer();
+    this.buffer = Memory.wrap(buffer.slice(), buffer.order());
   }
 
   @Override
   public long read(int index)
   {
-    return buffer.get(buffer.position() + index);
+    return buffer.getLong((long) index << 3);
   }
 
   @Override
   public void read(final long[] out, final int outPosition, final int startIndex, final int length)
   {
-    final int oldPosition = buffer.position();
-    try {
-      buffer.position(oldPosition + startIndex);
-      buffer.get(out, outPosition, length);
+    buffer.getLongArray((long) startIndex << 3, out, outPosition, length);
+  }
+
+  @Override
+  public int read(long[] out, int outPosition, int[] indexes, int length, int indexOffset, int limit)
+  {
+    for (int i = 0; i < length; i++) {
+      int index = indexes[outPosition + i] - indexOffset;
+      if (index >= limit) {
+        return i;
+      }
+
+      out[outPosition + i] = buffer.getLong((long) index << 3);
     }
-    finally {
-      buffer.position(oldPosition);
-    }
+
+    return length;
   }
 
   @Override
   public CompressionFactory.LongEncodingReader duplicate()
   {
-    return new LongsLongEncodingReader(buffer.duplicate());
+    return this;
   }
 }

benchmarks/src/test/java/org/apache/druid/benchmark/compression/BaseColumnarLongsBenchmark.java
@@ -1,0 +1,211 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.benchmark.compression;
+
+import org.apache.druid.collections.bitmap.WrappedImmutableRoaringBitmap;
+import org.apache.druid.java.util.common.RE;
+import org.apache.druid.segment.data.ColumnarLongs;
+import org.apache.druid.segment.data.ColumnarLongsSerializer;
+import org.apache.druid.segment.data.CompressedColumnarLongsSupplier;
+import org.apache.druid.segment.data.CompressionFactory;
+import org.apache.druid.segment.data.CompressionStrategy;
+import org.apache.druid.segment.vector.BitmapVectorOffset;
+import org.apache.druid.segment.vector.NoFilterVectorOffset;
+import org.apache.druid.segment.vector.VectorOffset;
+import org.apache.druid.segment.writeout.OnHeapMemorySegmentWriteOutMedium;
+import org.apache.druid.segment.writeout.SegmentWriteOutMedium;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.roaringbitmap.buffer.MutableRoaringBitmap;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
+import java.util.BitSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+@State(Scope.Benchmark)
+public class BaseColumnarLongsBenchmark
+{
+  static final int VECTOR_SIZE = 512;
+
+  /**
+   * Name of the long encoding strategy. For longs, this is a composite of both byte level block compression and
+   * encoding of values within the block.
+   */
+  @Param({
+      "lz4-longs",
+      "lz4-auto"
+  })
+  String encoding;
+
+  Random rand = new Random(0);
+
+  long[] vals;
+
+  long minValue;
+  long maxValue;
+
+  @Nullable
+  BitSet filter;
+
+  VectorOffset vectorOffset;
+
+  void setupFilters(int rows, double filteredRowCountPercentage)
+  {
+    // todo: filter set distributions to simulate different select patterns?
+    //  (because benchmarks don't take long enough already..)
+    filter = null;
+    final int filteredRowCount = (int) Math.floor(rows * filteredRowCountPercentage);
+
+    if (filteredRowCount < rows) {
+      // setup bitset filter
+      filter = new BitSet();
+      MutableRoaringBitmap bitmap = new MutableRoaringBitmap();
+      for (int i = 0; i < filteredRowCount; i++) {
+        int rowToAccess = rand.nextInt(rows);
+        // Skip already selected rows if any
+        while (filter.get(rowToAccess)) {
+          rowToAccess = rand.nextInt(rows);
+        }
+        filter.set(rowToAccess);
+        bitmap.add(rowToAccess);
+      }
+      vectorOffset = new BitmapVectorOffset(
+          VECTOR_SIZE,
+          new WrappedImmutableRoaringBitmap(bitmap.toImmutableRoaringBitmap()),
+          0,
+          rows
+      );
+    } else {
+      vectorOffset = new NoFilterVectorOffset(VECTOR_SIZE, 0, rows);
+    }
+  }
+
+  static int encodeToFile(long[] vals, String encoding, FileChannel output)throws IOException
+  {
+    SegmentWriteOutMedium writeOutMedium = new OnHeapMemorySegmentWriteOutMedium();
+
+    ColumnarLongsSerializer serializer;
+    switch (encoding) {
+      case "lz4-longs":
+        serializer = CompressionFactory.getLongSerializer(
+            encoding,
+            writeOutMedium,
+            "lz4-longs",
+            ByteOrder.LITTLE_ENDIAN,
+            CompressionFactory.LongEncodingStrategy.LONGS,
+            CompressionStrategy.LZ4
+        );
+        break;
+      case "lz4-auto":
+        serializer = CompressionFactory.getLongSerializer(
+            encoding,
+            writeOutMedium,
+            "lz4-auto",
+            ByteOrder.LITTLE_ENDIAN,
+            CompressionFactory.LongEncodingStrategy.AUTO,
+            CompressionStrategy.LZ4
+        );
+        break;
+      case "none-longs":
+        serializer = CompressionFactory.getLongSerializer(
+            encoding,
+            writeOutMedium,
+            "none-longs",
+            ByteOrder.LITTLE_ENDIAN,
+            CompressionFactory.LongEncodingStrategy.LONGS,
+            CompressionStrategy.NONE
+        );
+        break;
+      case "none-auto":
+        serializer = CompressionFactory.getLongSerializer(
+            encoding,
+            writeOutMedium,
+            "none-auto",
+            ByteOrder.LITTLE_ENDIAN,
+            CompressionFactory.LongEncodingStrategy.AUTO,
+            CompressionStrategy.NONE
+        );
+        break;
+      default:
+        throw new RuntimeException("unknown encoding");
+    }
+
+    serializer.open();
+    for (long val : vals) {
+      serializer.add(val);
+    }
+    serializer.writeTo(output, null);
+    return (int) serializer.getSerializedSize();
+  }
+
+  static ColumnarLongs createColumnarLongs(String encoding, ByteBuffer buffer)
+  {
+    switch (encoding) {
+      case "lz4-longs":
+      case "lz4-auto":
+      case "none-auto":
+      case "none-longs":
+        return CompressedColumnarLongsSupplier.fromByteBuffer(buffer, ByteOrder.LITTLE_ENDIAN).get();
+    }
+
+    throw new IllegalArgumentException("unknown encoding");
+  }
+
+
+  // for testing encodings: validate that all encoders read the same values
+  // noinspection unused
+  static void checkSanity(Map<String, ColumnarLongs> encoders, List<String> encodings, int rows)
+  {
+    for (int i = 0; i < rows; i++) {
+      checkRowSanity(encoders, encodings, i);
+    }
+  }
+
+  static void checkRowSanity(Map<String, ColumnarLongs> encoders, List<String> encodings, int row)
+  {
+    if (encodings.size() > 1) {
+      for (int i = 0; i < encodings.size() - 1; i++) {
+        String currentKey = encodings.get(i);
+        String nextKey = encodings.get(i + 1);
+        ColumnarLongs current = encoders.get(currentKey);
+        ColumnarLongs next = encoders.get(nextKey);
+        long vCurrent = current.get(row);
+        long vNext = next.get(row);
+        if (vCurrent != vNext) {
+          throw new RE(
+              "values do not match at row %s - %s:%s %s:%s",
+              row,
+              currentKey,
+              vCurrent,
+              nextKey,
+              vNext
+          );
+        }
+      }
+    }
+  }
+}

benchmarks/src/test/java/org/apache/druid/benchmark/compression/BaseColumnarLongsFromGeneratorBenchmark.java
@@ -1,0 +1,395 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.benchmark.compression;
+
+import com.google.common.collect.ImmutableList;
+import org.apache.druid.java.util.common.StringUtils;
+import org.apache.druid.segment.column.ValueType;
+import org.apache.druid.segment.generator.ColumnValueGenerator;
+import org.apache.druid.segment.generator.GeneratorColumnSchema;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import java.io.File;
+import java.util.List;
+
+@State(Scope.Benchmark)
+public class BaseColumnarLongsFromGeneratorBenchmark extends BaseColumnarLongsBenchmark
+{
+  static int SEED = 1;
+
+  /**
+   * Controls the probability that any generated value will be a zero, to simulate sparely populated columns
+   */
+  @Param({
+      "0.0",
+      "0.25",
+      "0.5",
+      "0.75",
+      "0.95"
+  })
+  double zeroProbability;
+
+  /**
+   * Number of rows generated for the value distribution
+   */
+  @Param({"5000000"})
+  int rows;
+
+  /**
+   * Value distributions to simulate various patterns of long column
+   */
+  @Param({
+      "enumerated-0-1",
+      "enumerated-full",
+      "normal-1-32",
+      "normal-40-1000",
+      "sequential-1000",
+      "sequential-unique",
+      "uniform-1",
+      "uniform-2",
+      "uniform-3",
+      "uniform-4",
+      "uniform-8",
+      "uniform-12",
+      "uniform-16",
+      "uniform-20",
+      "uniform-24",
+      "uinform-32",
+      "uniform-40",
+      "uniform-48",
+      "uniform-56",
+      "uniform-64",
+      "zipf-low-100",
+      "zipf-low-100000",
+      "zipf-low-32-bit",
+      "zipf-high-100",
+      "zipf-high-100000",
+      "zipf-high-32-bit"
+  })
+  String distribution;
+
+
+  static ColumnValueGenerator makeGenerator(
+      String distribution,
+      int rows,
+      double zeroProbability
+  )
+  {
+    List<Object> enumerated;
+    List<Double> probability;
+    switch (distribution) {
+      case "enumerated-0-1":
+        enumerated = ImmutableList.of(0, 1);
+        probability = ImmutableList.of(0.6, 0.4);
+        return GeneratorColumnSchema.makeEnumerated(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            enumerated,
+            probability
+        ).makeGenerator(SEED);
+      case "enumerated-full":
+        enumerated = ImmutableList.of(
+            0,
+            1,
+            Long.MAX_VALUE - 1,
+            Long.MIN_VALUE + 1,
+            Long.MIN_VALUE / 2,
+            Long.MAX_VALUE / 2
+        );
+        probability = ImmutableList.of(0.4, 0.2, 0.1, 0.1, 0.1, 0.1);
+        return GeneratorColumnSchema.makeEnumerated(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            enumerated,
+            probability
+        ).makeGenerator(SEED);
+      case "normal-1-32":
+        return GeneratorColumnSchema.makeNormal(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            1.0,
+            (double) (1L << 32),
+            true
+        ).makeGenerator(SEED);
+      case "normal-40-1000":
+        return GeneratorColumnSchema.makeNormal(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            (double) (1L << 40),
+            1000.0,
+            true
+        ).makeGenerator(SEED);
+      case "sequential-1000":
+        return GeneratorColumnSchema.makeSequential(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            Integer.MAX_VALUE - 1001,
+            Integer.MAX_VALUE - 1
+        ).makeGenerator(SEED);
+      case "sequential-unique":
+        return GeneratorColumnSchema.makeSequential(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0,
+            rows
+        ).makeGenerator(SEED);
+      case "uniform-1":
+        return GeneratorColumnSchema.makeDiscreteUniform(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0,
+            1
+        ).makeGenerator(SEED);
+      case "uniform-2":
+        return GeneratorColumnSchema.makeDiscreteUniform(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0,
+            4
+        ).makeGenerator(SEED);
+      case "uniform-3":
+        return GeneratorColumnSchema.makeDiscreteUniform(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            1000000,
+            1000008
+        ).makeGenerator(SEED);
+      case "uniform-4":
+        return GeneratorColumnSchema.makeDiscreteUniform(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0,
+            1 << 4
+        ).makeGenerator(SEED);
+      case "uniform-8":
+        return GeneratorColumnSchema.makeDiscreteUniform(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0,
+            1 << 8
+        ).makeGenerator(SEED);
+      case "uniform-12":
+        return GeneratorColumnSchema.makeDiscreteUniform(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0,
+            1 << 12
+        ).makeGenerator(SEED);
+      case "uniform-16":
+        return GeneratorColumnSchema.makeDiscreteUniform(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0,
+            1 << 16
+        ).makeGenerator(SEED);
+      case "uniform-20":
+        return GeneratorColumnSchema.makeContinuousUniform(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0,
+            1 << 20
+        ).makeGenerator(SEED);
+      case "uniform-24":
+        return GeneratorColumnSchema.makeContinuousUniform(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0,
+            (1 << 24) - 1
+        ).makeGenerator(SEED);
+      case "uinform-32":
+        return GeneratorColumnSchema.makeContinuousUniform(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0,
+            Integer.MAX_VALUE - 1
+        ).makeGenerator(SEED);
+      case "uniform-40":
+        return GeneratorColumnSchema.makeContinuousUniform(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0L,
+            (1L << 40) - 1
+        ).makeGenerator(SEED);
+      case "uniform-48":
+        return GeneratorColumnSchema.makeContinuousUniform(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0,
+            (1L << 48) - 1
+        ).makeGenerator(SEED);
+      case "uniform-56":
+        return GeneratorColumnSchema.makeContinuousUniform(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0,
+            (1L << 56 - 1)
+        ).makeGenerator(SEED);
+      case "uniform-64":
+        return GeneratorColumnSchema.makeContinuousUniform(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0,
+            Long.MAX_VALUE - 1
+        ).makeGenerator(SEED);
+      case "zipf-low-100":
+        return GeneratorColumnSchema.makeLazyZipf(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0,
+            100,
+            1d
+        ).makeGenerator(SEED);
+      case "zipf-low-100000":
+        return GeneratorColumnSchema.makeLazyZipf(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            -50000,
+            50000,
+            1d
+        ).makeGenerator(SEED);
+      case "zipf-low-32-bit":
+        return GeneratorColumnSchema.makeLazyZipf(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            0d,
+            0,
+            Integer.MAX_VALUE,
+            1d
+        ).makeGenerator(SEED);
+      case "zipf-high-100":
+        return GeneratorColumnSchema.makeLazyZipf(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            0,
+            100,
+            3d
+        ).makeGenerator(SEED);
+      case "zipf-high-100000":
+        return GeneratorColumnSchema.makeLazyZipf(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            zeroProbability,
+            -50000,
+            50000,
+            3d
+        ).makeGenerator(SEED);
+      case "zipf-high-32-bit":
+        return GeneratorColumnSchema.makeLazyZipf(
+            distribution,
+            ValueType.LONG,
+            true,
+            1,
+            0d,
+            0,
+            Integer.MAX_VALUE,
+            3d
+        ).makeGenerator(SEED);
+    }
+    throw new IllegalArgumentException("unknown distribution");
+  }
+
+  static String getGeneratorEncodedFilename(String encoding, String distribution, int rows, double nullProbability)
+  {
+    return StringUtils.format("%s-%s-%s-%s.bin", encoding, distribution, rows, nullProbability);
+  }
+
+  static File getTmpDir()
+  {
+    final String dirPath = "tmp/encoding/longs/";
+    File dir = new File(dirPath);
+    dir.mkdirs();
+    return dir;
+  }
+}

benchmarks/src/test/java/org/apache/druid/benchmark/compression/BaseColumnarLongsFromSegmentsBenchmark.java
@@ -1,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.benchmark.compression;
+
+import org.apache.druid.java.util.common.StringUtils;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import java.io.File;
+
+@State(Scope.Benchmark)
+public class BaseColumnarLongsFromSegmentsBenchmark extends BaseColumnarLongsBenchmark
+{
+  /**
+   * Long columns to read from the segment file specified by {@link #segmentPath}
+   */
+  @Param({
+      "__time",
+      "followers",
+      "friends",
+      "max_followers",
+      "max_retweets",
+      "max_statuses",
+      "retweets",
+      "statuses",
+      "tweets"
+  })
+  String columnName;
+
+  /**
+   * Number of rows in the segment. This should actually match the number of rows specified in {@link #segmentPath}. If
+   * it is smaller than only this many rows will be read, if larger then the benchmark will explode trying to read more
+   * data than exists rows.
+   *
+   * This is a hassle, but ensures that the row count ends up in the output measurements.
+   */
+  @Param({"3259585"})
+  int rows;
+
+
+  /**
+   * Path to a segment file to read long columns from. This shouldn't really be used as a parameter, but is nice to
+   * be included in the output measurements.
+   *
+   * This is BYO segment, as this file doesn't probably exist for you, replace it and other parameters with the segment
+   * to test.
+   */
+  @Param({"tmp/segments/twitter-ticker-1/"})
+  String segmentPath;
+
+  /**
+   * Friendly name of the segment. Like {@link #segmentPath}, this shouldn't really be used as a parameter, but is also
+   * nice to be included in the output measurements.
+   */
+  @Param({"twitter-ticker"})
+  String segmentName;
+
+  String getColumnEncodedFileName(String encoding, String segmentName, String columnName)
+  {
+    return StringUtils.format("%s-%s-longs-%s.bin", encoding, segmentName, columnName);
+  }
+
+  File getTmpDir()
+  {
+    final String dirPath = StringUtils.format("tmp/encoding/%s", segmentName);
+    File dir = new File(dirPath);
+    dir.mkdirs();
+    return dir;
+  }
+}

commit 0902d0e6e7918c91991f2d3bb3a9c72be00f4add

LongsLongEncodingReader: Implement "duplicate", fixing concurrency bug.
Regression introduced in #11004 due to overzealous optimization. Even though
we replaced stateful usage of ByteBuffer with stateless usage of Memory, we
still need to create a new object on "duplicate" due to semantics of setBuffer.

processing/src/main/java/org/apache/druid/segment/data/CompressionFactory.java
@@ -287,6 +287,11 @@ public class CompressionFactory
 
     int read(long[] out, int outPosition, int[] indexes, int length, int indexOffset, int limit);
 
+    /**
+     * Duplicates this reader, creating a new reader that does not share any state. Important to achieve thread-safety,
+     * because a common pattern is to duplicate a reader multiple times and then call {@link #setBuffer} on the
+     * various duplicates.
+     */
     LongEncodingReader duplicate();
   }
 

processing/src/main/java/org/apache/druid/segment/data/LongsLongEncodingReader.java
@@ -69,6 +69,6 @@ public class LongsLongEncodingReader implements CompressionFactory.LongEncodingR
   @Override
   public CompressionFactory.LongEncodingReader duplicate()
   {
-    return this;
+    return new LongsLongEncodingReader(buffer.getByteBuffer(), buffer.getTypeByteOrder());
   }
 }