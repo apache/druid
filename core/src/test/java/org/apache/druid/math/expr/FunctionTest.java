1061aff3ef36c6bbcfb771840950d732ac4f7ca3
chenyuzhi459 <553673833@qq.com>
2021-04-12 09:31:39 +0800

    code style

--- core/src/test/java/org/apache/druid/math/expr/FunctionTest.java
+++ core/src/test/java/org/apache/druid/math/expr/FunctionTest.java
@@ -22,7 +22,6 @@ package org.apache.druid.math.expr;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import org.apache.druid.common.config.NullHandling;
-import org.apache.druid.java.util.common.IAE;
 import org.apache.druid.java.util.common.Pair;
 import org.apache.druid.testing.InitializedNullHandlingTest;
 import org.junit.Assert;
