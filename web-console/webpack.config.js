722b02dc7f2cdb06e3764faab035fc60a0bb5c44
John Gozde <john@gozde.ca>
2021-04-16 11:32:22 -0600

    No sourcemaps in prod

--- web-console/webpack.config.js
+++ web-console/webpack.config.js
@@ -57,7 +57,7 @@ module.exports = env => {
 
   return {
     mode: mode,
-    devtool: mode === 'production' ? 'hidden-source-map' : 'eval-cheap-module-source-map',
+    devtool: mode === 'production' ? undefined : 'eval-cheap-module-source-map',
     entry: {
       'web-console': './src/entry.ts',
     },
