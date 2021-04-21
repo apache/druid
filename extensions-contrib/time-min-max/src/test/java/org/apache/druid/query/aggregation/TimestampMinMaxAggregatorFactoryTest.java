883e1d4d72de814f340973405f78c592aa1a8c66
Clint Wylie <cwylie@apache.org>
2021-04-21 16:27:48 -0700

    fix serde issues with time-min-max extension

--- extensions-contrib/time-min-max/src/test/java/org/apache/druid/query/aggregation/TimestampMinMaxAggregatorFactoryTest.java
+++ extensions-contrib/time-min-max/src/test/java/org/apache/druid/query/aggregation/TimestampMinMaxAggregatorFactoryTest.java
@@ -19,12 +19,16 @@
 
 package org.apache.druid.query.aggregation;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nl.jqno.equalsverifier.EqualsVerifier;
 import org.apache.druid.java.util.common.granularity.Granularities;
 import org.apache.druid.query.Druids;
 import org.apache.druid.query.aggregation.post.FieldAccessPostAggregator;
 import org.apache.druid.query.aggregation.post.FinalizingFieldAccessPostAggregator;
 import org.apache.druid.query.timeseries.TimeseriesQuery;
 import org.apache.druid.query.timeseries.TimeseriesQueryQueryToolChest;
+import org.apache.druid.segment.TestHelper;
 import org.apache.druid.segment.column.RowSignature;
 import org.apache.druid.segment.column.ValueType;
 import org.junit.Assert;
@@ -32,6 +36,54 @@ import org.junit.Test;
 
 public class TimestampMinMaxAggregatorFactoryTest
 {
+  private static final ObjectMapper JSON_MAPPER = TestHelper.makeJsonMapper();
+
+  @Test
+  public void testSerde() throws JsonProcessingException
+  {
+    TimestampMaxAggregatorFactory maxAgg = new TimestampMaxAggregatorFactory("timeMax", "__time", null);
+    TimestampMinAggregatorFactory minAgg = new TimestampMinAggregatorFactory("timeMin", "__time", null);
+
+    Assert.assertEquals(
+        maxAgg,
+        JSON_MAPPER.readValue(JSON_MAPPER.writeValueAsString(maxAgg), TimestampMaxAggregatorFactory.class)
+    );
+    Assert.assertEquals(
+        maxAgg.getCombiningFactory(),
+        JSON_MAPPER.readValue(
+            JSON_MAPPER.writeValueAsString(maxAgg.getCombiningFactory()),
+            TimestampMaxAggregatorFactory.class
+        )
+    );
+
+    Assert.assertEquals(
+        minAgg,
+        JSON_MAPPER.readValue(JSON_MAPPER.writeValueAsString(minAgg), TimestampMinAggregatorFactory.class)
+    );
+    Assert.assertEquals(
+        minAgg.getCombiningFactory(),
+        JSON_MAPPER.readValue(
+            JSON_MAPPER.writeValueAsString(minAgg.getCombiningFactory()),
+            TimestampMinAggregatorFactory.class
+        )
+    );
+  }
+
+  @Test
+  public void testEqualsAndHashcode()
+  {
+    EqualsVerifier.forClass(TimestampMinAggregatorFactory.class)
+                  .withNonnullFields("name", "comparator", "initValue")
+                  .withIgnoredFields("timestampSpec")
+                  .usingGetClass()
+                  .verify();
+    EqualsVerifier.forClass(TimestampMaxAggregatorFactory.class)
+                  .withNonnullFields("name", "comparator", "initValue")
+                  .withIgnoredFields("timestampSpec")
+                  .usingGetClass()
+                  .verify();
+  }
+
   @Test
   public void testResultArraySignature()
   {
