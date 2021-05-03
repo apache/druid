84aac4832d66d2bf2c9e2903b9772bb5882c5ccf
Maytas Monsereenusorn <maytasm@apache.org>
2021-05-03 11:50:45 -0700

    Add feature to automatically remove rules based on retention period (#11164)
    * Add feature to automatically remove rules based on retention period
    
    * Add feature to automatically remove rules based on retention period
    
    * address comments

--- server/src/test/java/org/apache/druid/server/coordinator/DruidCoordinatorTest.java
+++ server/src/test/java/org/apache/druid/server/coordinator/DruidCoordinatorTest.java
@@ -146,6 +146,8 @@ public class DruidCoordinatorTest extends CuratorTestBase
         null,
         null,
         null,
+        null,
+        null,
         10,
         new Duration("PT0s")
     );
