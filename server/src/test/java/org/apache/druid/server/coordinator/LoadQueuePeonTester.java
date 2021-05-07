d73f72e50892fe0b7450d8d4bf5678c3f8270cfe
Maytas Monsereenusorn <maytasm@apache.org>
2021-05-06 22:25:23 -0700

    Add feature to automatically remove supervisor based on retention period (#11200)
    * add auto clean up
    
    * add test
    
    * add test
    
    * fix test
    
    * Address comments
    
    * Address comments

--- server/src/test/java/org/apache/druid/server/coordinator/LoadQueuePeonTester.java
+++ server/src/test/java/org/apache/druid/server/coordinator/LoadQueuePeonTester.java
@@ -49,6 +49,8 @@ public class LoadQueuePeonTester extends CuratorLoadQueuePeon
             null,
             null,
             null,
+            null,
+            null,
             10,
             new Duration("PT1s")
         )
