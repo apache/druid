28e801200e1a1bfe3ba2cd3b79690c11a006bbb8
Clint Wylie <cwylie@apache.org>
2021-05-21 01:27:51 -0700

    only skip stuff for PR builds, add some tests

--- check_test_suite.py
+++ check_test_suite.py
@@ -27,7 +27,7 @@ always_run_jobs = ['license checks', '(openjdk8) packaging check', '(openjdk11) 
 # of CI can be skipped
 ignore_prefixes = ['.github', '.idea', '.asf.yaml', '.backportrc.json', '.codecov.yml', '.dockerignore', '.gitignore',
                    '.lgtm.yml', 'CONTRIBUTING.md', 'setup-hooks.sh', 'upload.sh', 'dev', 'distribution/docker',
-                   'distribution/asf-release-process-guide.md', '.travis.yml', 'check-test-suite.py']
+                   'distribution/asf-release-process-guide.md', '.travis.yml', 'check_test_suite.py']
 # these files are docs changes
 # if changes are limited to this set then we can skip web-console and java
 # if no changes in this set we can skip docs
@@ -60,11 +60,15 @@ def check_console(file):
         print("found web-console file change: {}".format(file))
     return is_console
 
+
+# given a test suite name (travis job name) and set of files in a git diff
+# try to determine if the test suite should run or not
 def check_should_run_suite(suite, diff_files):
     if suite in always_run_jobs:
         # you gotta do what you gotta do
         return True
 
+    # is everything in the ignorable list
     all_ignore = True
     any_docs = False
     all_docs = True
@@ -94,32 +98,38 @@ def check_should_run_suite(suite, diff_files):
     return True
 
 def failWithUsage():
-    sys.stderr.write("usage: check-test-suite.py <test-suite-name>\n")
-    sys.stderr.write("  e.g., check-test-suite.py docs")
+    sys.stderr.write("usage: check_test_suite.py <test-suite-name>\n")
+    sys.stderr.write("  e.g., check_test_suite.py docs")
     sys.exit(1)
 
-suite_name = ""
+if __name__ == '__main__':
+    suite_name = ""
 
-if len(sys.argv) == 1:
-    if 'TRAVIS_JOB_NAME' in os.environ:
-        suite_name = os.environ['TRAVIS_JOB_NAME']
+    if len(sys.argv) == 1:
+        if 'TRAVIS_JOB_NAME' in os.environ:
+            suite_name = os.environ['TRAVIS_JOB_NAME']
+        else:
+            failWithUsage()
+    elif len(sys.argv) == 2:
+        suite_name = sys.argv[1]
     else:
         failWithUsage()
-elif len(sys.argv) == 2:
-    suite_name = sys.argv[1]
-else:
-    failWithUsage()
 
 
-all_changed_files_string = subprocess.check_output("git diff --name-only HEAD~1", shell=True).decode('UTF-8')
-all_changed_files = all_changed_files_string.splitlines()
-print("Checking if suite '{}' needs to run test on diff:\n{}".format(suite_name, all_changed_files_string))
-needs_run = check_should_run_suite(suite_name, all_changed_files)
+    all_changed_files_string = subprocess.check_output("git diff --name-only HEAD~1", shell=True).decode('UTF-8')
+    all_changed_files = all_changed_files_string.splitlines()
+    print("Checking if suite '{}' needs to run test on diff:\n{}".format(suite_name, all_changed_files_string))
 
-if needs_run:
-    print("Changes detected, need to run test suite '{}'".format(suite_name))
-    sys.exit(1)
+    # we should always run all test suites for builds that are not for a pull request
+    is_pr = False
+    if 'TRAVIS_PULL_REQUEST' in os.environ and os.environ['TRAVIS_PULL_REQUEST'] != 'false':
+        is_pr = True
+    needs_run = not is_pr or check_should_run_suite(suite_name, all_changed_files)
 
-print("No applicable changes detected, can skip test suite '{}'".format(suite_name))
-sys.exit(0)
+    if needs_run:
+        print("Changes detected, need to run test suite '{}'".format(suite_name))
+        sys.exit(1)
+
+    print("No applicable changes detected, can skip test suite '{}'".format(suite_name))
+    sys.exit(0)
 
