b022f4c641d51af5fbc51eb8212ed0bb0714231b
Vadim Ogievetsky <vadim@ogievetsky.com>
2021-06-11 16:00:42 -0700

    explain tests

--- web-console/src/views/query-view/explain-dialog/explain-dialog.spec.tsx
+++ web-console/src/views/query-view/explain-dialog/explain-dialog.spec.tsx
@@ -16,21 +16,61 @@
  * limitations under the License.
  */
 
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import React from 'react';
+
+import {
+  BasicQueryExplanation,
+  parseQueryPlan,
+  QueryState,
+  SemiJoinQueryExplanation,
+} from '../../../utils';
 
 import { ExplainDialog } from './explain-dialog';
 
+let explainState: QueryState<BasicQueryExplanation | SemiJoinQueryExplanation | string> =
+  QueryState.INIT;
+
+jest.mock('../../../hooks', () => {
+  return {
+    useQueryManager: () => [explainState],
+  };
+});
+
 describe('ExplainDialog', () => {
-  it('matches snapshot', () => {
-    const queryPlanDialog = (
+  function makeExplainDialog() {
+    return (
       <ExplainDialog
         setQueryString={() => {}}
         queryWithContext={{ queryString: 'test', queryContext: {}, wrapQueryLimit: undefined }}
         onClose={() => {}}
       />
     );
-    render(queryPlanDialog);
-    expect(document.body.lastChild).toMatchSnapshot();
+  }
+
+  it('matches snapshot on init', () => {
+    expect(shallow(makeExplainDialog())).toMatchSnapshot();
+  });
+
+  it('matches snapshot on loading', () => {
+    explainState = QueryState.LOADING;
+
+    expect(shallow(makeExplainDialog())).toMatchSnapshot();
+  });
+
+  it('matches snapshot on error', () => {
+    explainState = new QueryState({ error: new Error('test error') });
+
+    expect(shallow(makeExplainDialog())).toMatchSnapshot();
+  });
+
+  it('matches snapshot on some data', () => {
+    explainState = new QueryState({
+      data: parseQueryPlan(
+        `DruidQueryRel(query=[{"queryType":"topN","dataSource":{"type":"table","name":"kttm-multi-day"},"virtualColumns":[],"dimension":{"type":"default","dimension":"browser","outputName":"d0","outputType":"STRING"},"metric":{"type":"numeric","metric":"a0"},"threshold":101,"intervals":{"type":"intervals","intervals":["-146136543-09-08T08:23:32.096Z/146140482-04-24T15:36:27.903Z"]},"filter":null,"granularity":{"type":"all"},"aggregations":[{"type":"count","name":"a0"}],"postAggregations":[],"context":{"sqlOuterLimit":101,"sqlQueryId":"5905fe8d-9a91-41e0-8f3a-d7a8ac21dce6"},"descending":false}], signature=[{d0:STRING, a0:LONG}])`,
+      ),
+    });
+
+    expect(shallow(makeExplainDialog())).toMatchSnapshot();
   });
 });
