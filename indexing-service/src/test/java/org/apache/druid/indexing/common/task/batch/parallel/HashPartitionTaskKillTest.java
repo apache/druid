6ca2cb5e2c7437bb2bb87152aebde6c651dc991d
Agustin Gonzalez <agustin.gonzalez@imply.io>
2021-07-23 17:04:21 -0700

    Make it fail

--- indexing-service/src/test/java/org/apache/druid/indexing/common/task/batch/parallel/HashPartitionTaskKillTest.java
+++ indexing-service/src/test/java/org/apache/druid/indexing/common/task/batch/parallel/HashPartitionTaskKillTest.java
@@ -1,10 +1,20 @@
 /*
- * Copyright (c) Imply Data, Inc. All rights reserved.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * This software is the confidential and proprietary information
- * of Imply Data, Inc. You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms
- * of the license agreement you entered into with Imply.
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.apache.druid.indexing.common.task.batch.parallel;
@@ -113,7 +123,7 @@ public class HashPartitionTaskKillTest extends AbstractMultiPhaseParallelIndexin
                 new HashedPartitionsSpec(null, 3,
                                          ImmutableList.of("dim1", "dim2")
                 ),
-                1, false
+                2, false
         );
 
     final TaskActionClient actionClient = createActionClient(task);
@@ -124,7 +134,7 @@ public class HashPartitionTaskKillTest extends AbstractMultiPhaseParallelIndexin
 
     final TaskStatus taskStatus = task.run(toolbox);
 
-    Assert.assertTrue(taskStatus.isSuccess());
+    Assert.assertTrue(taskStatus.isFailure());
 
 
   }
