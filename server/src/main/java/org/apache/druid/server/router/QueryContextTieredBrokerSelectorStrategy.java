c0f1f3ec9442441b1a01dbc4eca07f029434fe69
Kashif Faraz <kashif.faraz@gmail.com>
2021-07-26 17:40:22 +0530

    Add QueryContextTieredBrokerSelectorStrategy

---
+++ server/src/main/java/org/apache/druid/server/router/QueryContextTieredBrokerSelectorStrategy.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.server.router;
+
+import com.google.common.base.Optional;
+import org.apache.commons.lang.StringUtils;
+import org.apache.druid.java.util.common.logger.Logger;
+import org.apache.druid.query.Query;
+import org.apache.druid.query.QueryContexts;
+
+/**
+ * Implementation of {@link TieredBrokerSelectorStrategy} which uses the flag
+ * {@link QueryContexts#BROKER_SERVICE_NAME} in the Query context to select the
+ * Broker tier.
+ */
+public class QueryContextTieredBrokerSelectorStrategy implements TieredBrokerSelectorStrategy
+{
+
+  private static final Logger log = new Logger(QueryContextTieredBrokerSelectorStrategy.class);
+
+  @Override
+  public Optional<String> getBrokerServiceName(TieredBrokerConfig tierConfig, Query query)
+  {
+    try {
+      String brokerServiceName = QueryContexts.getBrokerServiceName(query);
+
+      // Verify that the brokerServiceName is valid
+      if (StringUtils.isEmpty(brokerServiceName)
+          || !tierConfig.getTierToBrokerMap().containsValue(brokerServiceName)) {
+        log.debug("Could not find Broker Service [%s] in TieredBrokerConfig", brokerServiceName);
+        return Optional.absent();
+      } else {
+        return Optional.of(brokerServiceName);
+      }
+    }
+    catch (Exception e) {
+      log.error(e, "Error getting Broker Service name from Query Context");
+      return Optional.absent();
+    }
+  }
+}
