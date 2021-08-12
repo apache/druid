d58b85aade59c8956e48c6a2e28ab5bc966b430e
Jihoon Son <jihoonson@apache.org>
2021-08-12 13:36:57 -0700

    resultFormat name in camel case

---
+++ sql/src/test/java/org/apache/druid/sql/http/ResultFormatTest.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.sql.http;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.druid.jackson.DefaultObjectMapper;
+import org.apache.druid.java.util.common.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.EnumSet;
+import java.util.stream.Collectors;
+
+@RunWith(Parameterized.class)
+public class ResultFormatTest
+{
+  @Parameters(name = "{0}")
+  public static Iterable<Object[]> constructorFeeder()
+  {
+    return EnumSet.allOf(ResultFormat.class)
+                  .stream()
+                  .map(format -> new Object[]{format})
+                  .collect(Collectors.toList());
+  }
+
+  private final ObjectMapper jsonMapper = new DefaultObjectMapper();
+  private final ResultFormat target;
+
+  public ResultFormatTest(ResultFormat target)
+  {
+    this.target = target;
+  }
+
+  @Test
+  public void testSerde() throws JsonProcessingException
+  {
+    final String json = jsonMapper.writeValueAsString(target);
+    Assert.assertEquals(StringUtils.format("\"%s\"", target.toString()), json);
+    Assert.assertEquals(target, jsonMapper.readValue(json, ResultFormat.class));
+  }
+}
