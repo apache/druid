310fa66701c3620c7f2aced8a37b40d327793f38
Kashif Faraz <kashif.faraz@gmail.com>
2021-09-16 18:58:51 +0530

    Do not make alert for InterruptedException in CommonCacheNotifier

--- extensions-core/druid-basic-security/src/main/java/org/apache/druid/security/basic/CommonCacheNotifier.java
+++ extensions-core/druid-basic-security/src/main/java/org/apache/druid/security/basic/CommonCacheNotifier.java
@@ -141,9 +141,7 @@ public class CommonCacheNotifier
               LOG.debug(callerName + ":Received responses for cache update notifications.");
             }
             catch (InterruptedException e) {
-              LOG.noStackTrace()
-                 .makeAlert(e, callerName + ":Interrupted while handling updates for cachedUserMaps.")
-                 .emit();
+              LOG.info(e, "%s: Interrupted while handling updates for cachedUserMaps.", callerName);
             }
             catch (Throwable t) {
               LOG.makeAlert(t, callerName + ":Error occured while handling updates for cachedUserMaps.").emit();
