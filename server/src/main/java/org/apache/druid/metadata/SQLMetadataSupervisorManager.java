abcb32cebad18ebbdfdc9347c2bcd79abc466915
David Bar <david.bar@gmail.com>
2021-10-18 21:53:01 +0300

    Adjust tests

--- server/src/main/java/org/apache/druid/metadata/SQLMetadataSupervisorManager.java
+++ server/src/main/java/org/apache/druid/metadata/SQLMetadataSupervisorManager.java
@@ -51,11 +51,9 @@ import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.IntStream;
 
 @ManageLifecycle
