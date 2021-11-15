5fd5cd480dd29706dd6e4b3c736611fe8dc74c85
Soumyava Das <soumyava.das@imply.io>
2021-11-15 12:29:32 -0800

    Fixing expected results for infinity

--- core/src/test/java/org/apache/druid/math/expr/FunctionTest.java
+++ core/src/test/java/org/apache/druid/math/expr/FunctionTest.java
@@ -37,6 +37,7 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import javax.annotation.Nullable;
+import javax.validation.constraints.Null;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Locale;
@@ -788,8 +789,8 @@ public class FunctionTest extends InitializedNullHandlingTest
     // NaN and Infinity cases
     assertExpr("safe_divide(NaN, 0.0)", NullHandling.defaultDoubleValue());
     assertExpr("safe_divide(0, NaN)", 0.0);
-    assertExpr("safe_divide(0, POSITIVE_INFINITY)", 0L);
-    assertExpr("safe_divide(POSITIVE_INFINITY,0)", 0L);
+    assertExpr("safe_divide(0, POSITIVE_INFINITY)", NullHandling.defaultLongValue());
+    assertExpr("safe_divide(POSITIVE_INFINITY,0)", NullHandling.defaultLongValue());
   }
 
   @Test
