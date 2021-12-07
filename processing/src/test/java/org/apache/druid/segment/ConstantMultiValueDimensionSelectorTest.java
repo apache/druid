a6ab19e759831a262519e3fb9951339fc96e836c
Clint Wylie <cwylie@apache.org>
2021-12-06 17:57:31 -0800

    dang

--- processing/src/test/java/org/apache/druid/segment/ConstantMultiValueDimensionSelectorTest.java
+++ processing/src/test/java/org/apache/druid/segment/ConstantMultiValueDimensionSelectorTest.java
@@ -29,7 +29,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Collections;
-import java.util.List;
 
 public class ConstantMultiValueDimensionSelectorTest extends InitializedNullHandlingTest
 {
