5d074a741a29c92a793d978cc0878cff38db8c7f
tejaswini.bandlamudi <tejaswini.bandlamudi@imply.io>
2022-02-02 10:48:08 +0530

    Making single request with Range header and checking Content-Range header to check if it is honoured.

--- core/src/main/java/org/apache/druid/data/input/impl/HttpEntity.java
+++ core/src/main/java/org/apache/druid/data/input/impl/HttpEntity.java
@@ -80,32 +80,25 @@ public class HttpEntity extends RetryingInputEntity
     return t -> t instanceof IOException;
   }
 
-  private static void addAuthHeader(URLConnection urlConnection, String userName, PasswordProvider passwordProvider){
+  public static InputStream openInputStream(URI object, String userName, PasswordProvider passwordProvider, long offset)
+      throws IOException
+  {
+    final URLConnection urlConnection = object.toURL().openConnection();
     if (!Strings.isNullOrEmpty(userName) && passwordProvider != null) {
       String userPass = userName + ":" + passwordProvider.getPassword();
       String basicAuthString = "Basic " + Base64.getEncoder().encodeToString(StringUtils.toUtf8(userPass));
       urlConnection.setRequestProperty("Authorization", basicAuthString);
     }
-  }
-
-  public static InputStream openInputStream(URI object, String userName, PasswordProvider passwordProvider, long offset)
-      throws IOException
-  {
-    final URLConnection urlConnection = object.toURL().openConnection();
-    addAuthHeader(urlConnection, userName, passwordProvider);
-    final String acceptRanges = urlConnection.getHeaderField(HttpHeaders.ACCEPT_RANGES);
-    final boolean withRanges = "bytes".equalsIgnoreCase(acceptRanges);
-    if (withRanges && offset > 0) {
-      // Set header for range request.
-      // Since we need to set only the start offset, the header is "bytes=<range-start>-".
-      // See https://tools.ietf.org/html/rfc7233#section-2.1
-      urlConnection.getInputStream().close();
-      final URLConnection newUrlConnection = object.toURL().openConnection();
-      addAuthHeader(newUrlConnection, userName, passwordProvider);
-      newUrlConnection.addRequestProperty(HttpHeaders.RANGE, StringUtils.format("bytes=%d-", offset));
-      return newUrlConnection.getInputStream();
+    // Set header for range request.
+    // Since we need to set only the start offset, the header is "bytes=<range-start>-".
+    // See https://tools.ietf.org/html/rfc7233#section-2.1
+    urlConnection.addRequestProperty(HttpHeaders.RANGE, StringUtils.format("bytes=%d-", offset));
+    final String contentRange = urlConnection.getHeaderField(HttpHeaders.CONTENT_RANGE);
+    final boolean withContentRange = contentRange.startsWith("bytes ");
+    if (withContentRange && offset > 0) {
+      return urlConnection.getInputStream();
     } else {
-      if (!withRanges && offset > 0) {
+      if (!withContentRange && offset > 0) {
         LOG.warn(
             "Since the input source doesn't support range requests, the object input stream is opened from the start and "
             + "then skipped. This may make the ingestion speed slower. Consider enabling prefetch if you see this message"
