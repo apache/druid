758c9106bfd92f641a952a644c9cd4464d697ff0
Clint Wylie <cwylie@apache.org>
2022-03-09 01:23:43 -0800

    move indexOf into GenericIndexed

--- processing/src/main/java/org/apache/druid/segment/data/GenericIndexed.java
+++ processing/src/main/java/org/apache/druid/segment/data/GenericIndexed.java
@@ -347,15 +347,28 @@ public class GenericIndexed<T> implements CloseableIndexed<T>, Serializer
   @Override
   public int indexOf(@Nullable T value)
   {
-    return indexOf(this, value);
-  }
-
-  private int indexOf(Indexed<T> indexed, @Nullable T value)
-  {
     if (!allowReverseLookup) {
       throw new UnsupportedOperationException("Reverse lookup not allowed.");
     }
-    return Indexed.indexOf(indexed, size, strategy, value);
+    int minIndex = 0;
+    int maxIndex = size - 1;
+    while (minIndex <= maxIndex) {
+      int currIndex = (minIndex + maxIndex) >>> 1;
+
+      T currValue = get(currIndex);
+      int comparison = strategy.compare(currValue, value);
+      if (comparison == 0) {
+        return currIndex;
+      }
+
+      if (comparison < 0) {
+        minIndex = currIndex + 1;
+      } else {
+        maxIndex = currIndex - 1;
+      }
+    }
+
+    return -(minIndex + 1);
   }
 
   @Override
