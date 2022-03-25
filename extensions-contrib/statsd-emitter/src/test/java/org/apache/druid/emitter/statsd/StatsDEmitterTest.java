1d79e68f13c2db2476ec70d45e3944b13a4736fa
Xavier Léauté <xvrl@apache.org>
2022-03-25 10:28:48 -0700

    fix checkstyle

--- extensions-contrib/statsd-emitter/src/test/java/org/apache/druid/emitter/statsd/StatsDEmitterTest.java
+++ extensions-contrib/statsd-emitter/src/test/java/org/apache/druid/emitter/statsd/StatsDEmitterTest.java
@@ -244,7 +244,9 @@ public class StatsDEmitterTest
         true,
         true
     );
-    try (StatsDEmitter emitter = StatsDEmitter.of(config, new ObjectMapper())) {}
+    try (StatsDEmitter emitter = StatsDEmitter.of(config, new ObjectMapper())) {
+
+    }
   }
 
   @Test
