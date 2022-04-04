85e4b5f06ee4b617c97309c6d19a8ac4fcf46c16
tejaswini.bandlamudi <tejaswini.bandlamudi@imply.io>
2022-04-04 09:42:53 +0530

    Dropping inputSegmentSizeBytes from console auto-compaction config form and changing DEFAULT_INPUT_SEGMENT_SIZE_BYTES to Long.MAX_VALUE

--- web-console/src/components/auto-form/auto-form.spec.tsx
+++ web-console/src/components/auto-form/auto-form.spec.tsx
@@ -75,7 +75,6 @@ describe('AutoForm', () => {
           {
             dataSource: 'ds',
             taskPriority: 25,
-            inputSegmentSizeBytes: 109951162777600,
             maxRowsPerSegment: null,
             skipOffsetFromLatest: 'P4D',
             tuningConfig: {
@@ -121,7 +120,6 @@ describe('AutoForm', () => {
         {
           dataSource: 'ds',
           taskPriority: 25,
-          inputSegmentSizeBytes: 109951162777600,
           skipOffsetFromLatest: 'P4D',
           tuningConfig: {
             partitionsSpec: {
