f9bdb3b236c01a32eecd98f13e42fce0e1e5c3d4
superivaj <pareja.javier@gmail.com>
2022-05-20 18:53:08 +0200

    Fix usage of maxColumnsToMerge in auto-compaction tuning config (#12551)
    Issue: 
    Even though `CompactionTuningConfig` allows a `maxColumnsToMerge` config
    (to optimize memory usage, particulary for datasources with many dimensions),
    the corresponding client object `ClientCompactionTaskQueryTuningConfig`
    (used by the coordinator duty `CompactSegments` to trigger auto-compaction)
    does not contain this field. Thus, the value of `maxColumnsToMerge` specified
    in any datasource compaction config is ignored.
    
    Changes:
    - Add field `maxColumnsToMerge` in `ClientCompactionTaskQueryTuningConfig`
      and `UserCompactionTaskQueryTuningConfig`
    - Fix tests

--- server/src/test/java/org/apache/druid/server/coordinator/DataSourceCompactionConfigTest.java
+++ server/src/test/java/org/apache/druid/server/coordinator/DataSourceCompactionConfigTest.java
@@ -141,6 +141,7 @@ public class DataSourceCompactionConfigTest extends InitializedNullHandlingTest
             null,
             null,
             null,
+            null,
             null
         ),
         null,
@@ -176,6 +177,7 @@ public class DataSourceCompactionConfigTest extends InitializedNullHandlingTest
             null,
             null,
             10000L,
+            null,
             null,
             null,
             null,
@@ -242,7 +244,8 @@ public class DataSourceCompactionConfigTest extends InitializedNullHandlingTest
         new Duration(3000L),
         7,
         1000,
-        100
+        100,
+        2
     );
 
     final String json = OBJECT_MAPPER.writeValueAsString(tuningConfig);
@@ -282,7 +285,8 @@ public class DataSourceCompactionConfigTest extends InitializedNullHandlingTest
         new Duration(3000L),
         7,
         1000,
-        100
+        100,
+        2
     );
 
     final String json = OBJECT_MAPPER.writeValueAsString(tuningConfig);
