f6cc0ce9fecfa25cdefcd79025ef6ac7c39b684c
superivaj <pareja.javier@gmail.com>
2022-05-20 15:09:28 +0200

    Fix test error with maxColumnsToMerge

--- indexing-service/src/test/java/org/apache/druid/indexing/common/task/ClientCompactionTaskQuerySerdeTest.java
+++ indexing-service/src/test/java/org/apache/druid/indexing/common/task/ClientCompactionTaskQuerySerdeTest.java
@@ -288,7 +288,7 @@ public class ClientCompactionTaskQuerySerdeTest
                 null,
                 null,
                 null,
-                null,
+                2,
                 null,
                 null
             )
