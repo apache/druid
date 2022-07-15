d4403c15aa8a43c957d6a18ad27812c8fe7dca90
Jianhuan Liu <liujianhuan@qianxin.com>
2022-07-15 17:13:12 +0800

    Upgrade prometheus version, add more labels to PrometheusEmitter (#12769)
    Changes:
    - Upgrade prometheus to version 0.16.0
    - Add optional labels `druid_service` and `host_name` to `PrometheusEmitter`

--- extensions-contrib/prometheus-emitter/src/main/java/org/apache/druid/emitter/prometheus/PrometheusEmitterConfig.java
+++ extensions-contrib/prometheus-emitter/src/main/java/org/apache/druid/emitter/prometheus/PrometheusEmitterConfig.java
@@ -53,13 +53,21 @@ public class PrometheusEmitterConfig
   @Nullable
   private final String pushGatewayAddress;
 
+  @JsonProperty
+  private final boolean addHostAsLabel;
+
+  @JsonProperty
+  private final boolean addServiceAsLabel;
+
   @JsonCreator
   public PrometheusEmitterConfig(
       @JsonProperty("strategy") @Nullable Strategy strategy,
       @JsonProperty("namespace") @Nullable String namespace,
       @JsonProperty("dimensionMapPath") @Nullable String dimensionMapPath,
       @JsonProperty("port") @Nullable Integer port,
-      @JsonProperty("pushGatewayAddress") @Nullable String pushGatewayAddress
+      @JsonProperty("pushGatewayAddress") @Nullable String pushGatewayAddress,
+      @JsonProperty("addHostAsLabel") boolean addHostAsLabel,
+      @JsonProperty("addServiceAsLabel") boolean addServiceAsLabel
   )
   {
 
@@ -72,6 +80,8 @@ public class PrometheusEmitterConfig
       Preconditions.checkNotNull(pushGatewayAddress, "Invalid pushGateway address");
     }
     this.pushGatewayAddress = pushGatewayAddress;
+    this.addHostAsLabel = addHostAsLabel;
+    this.addServiceAsLabel = addServiceAsLabel;
   }
 
   public String getNamespace()
@@ -97,6 +107,16 @@ public class PrometheusEmitterConfig
   public Strategy getStrategy()
   {
     return strategy;
+  }
+
+  public boolean isAddHostAsLabel()
+  {
+    return addHostAsLabel;
+  }
+
+  public boolean isAddServiceAsLabel()
+  {
+    return addServiceAsLabel;
   }
 
   public enum Strategy
