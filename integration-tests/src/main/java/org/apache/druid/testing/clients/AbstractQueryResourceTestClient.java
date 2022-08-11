813ff853687b3552c2897fbf9bc8e2dd93d03af2
Abhishek Agarwal <1477457+abhishekagarwal87@users.noreply.github.com>
2022-08-11 12:06:21 +0530

    retry queries if there is some flakiness

--- integration-tests/src/main/java/org/apache/druid/testing/clients/AbstractQueryResourceTestClient.java
+++ integration-tests/src/main/java/org/apache/druid/testing/clients/AbstractQueryResourceTestClient.java
@@ -26,6 +26,7 @@ import com.google.inject.Inject;
 import org.apache.druid.guice.annotations.Smile;
 import org.apache.druid.java.util.common.IAE;
 import org.apache.druid.java.util.common.ISE;
+import org.apache.druid.java.util.common.logger.Logger;
 import org.apache.druid.java.util.http.client.HttpClient;
 import org.apache.druid.java.util.http.client.Request;
 import org.apache.druid.java.util.http.client.response.BytesFullResponseHandler;
@@ -33,12 +34,16 @@ import org.apache.druid.java.util.http.client.response.BytesFullResponseHolder;
 import org.apache.druid.java.util.http.client.response.StatusResponseHandler;
 import org.apache.druid.java.util.http.client.response.StatusResponseHolder;
 import org.apache.druid.testing.guice.TestClient;
+import org.apache.druid.testing.utils.ITRetryUtil;
+import org.apache.druid.utils.Throwables;
+import org.jboss.netty.channel.ChannelException;
 import org.jboss.netty.handler.codec.http.HttpHeaders;
 import org.jboss.netty.handler.codec.http.HttpMethod;
 import org.jboss.netty.handler.codec.http.HttpResponseStatus;
 
 import javax.annotation.Nullable;
 import javax.ws.rs.core.MediaType;
+
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -47,9 +52,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 public abstract class AbstractQueryResourceTestClient<QueryType>
 {
+  private static final Logger LOG = new Logger(AbstractQueryResourceTestClient.class);
+
   final String contentTypeHeader;
 
   /**
@@ -145,10 +153,30 @@ public abstract class AbstractQueryResourceTestClient<QueryType>
         request.addHeader(HttpHeaders.Names.ACCEPT, this.acceptHeader);
       }
 
-      BytesFullResponseHolder response = httpClient.go(
-          request,
-          new BytesFullResponseHandler()
-      ).get();
+      final AtomicReference<BytesFullResponseHolder> responseRef = new AtomicReference<>();
+
+      ITRetryUtil.retryUntil(() -> {
+        try {
+          responseRef.set(httpClient.go(
+              request,
+              new BytesFullResponseHandler()
+          ).get());
+        }
+        catch (Throwable t) {
+          ChannelException ce = Throwables.getCauseOfType(t, ChannelException.class);
+          if (ce != null) {
+            LOG.info(ce, "Encountered a channel exception. Retrying the query request");
+            return false;
+          }
+        }
+        return true;
+      },
+          true,
+          1000,
+          3,
+          "waiting for queries to complete");
+
+      BytesFullResponseHolder response = responseRef.get();
 
       if (!response.getStatus().equals(HttpResponseStatus.OK)) {
         throw new ISE(
