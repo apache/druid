apache/druid

commit de82c611de82f91f2f5e36fefc67b981d0408a4d

Harmonize implementations of "visit" for Exprs from ExprMacros. (#12230)
* Harmonize implementations of "visit" for Exprs from ExprMacros.

Many of them had bugs where they would not visit all of the original
arguments. I don't think this has user-visible consequences right now,
but it's possible it would in a future world where "visit" is used
for more stuff than it is today.

So, this patch all updates all implementations to a more consistent
style that emphasizes reapplying the macro to the shuttled args.

* Test fixes, test coverage, PR review comments.

core/src/main/java/org/apache/druid/math/expr/Expr.java
@@ -126,6 +126,9 @@ public interface Expr extends Cacheable
    * Programatically rewrite the {@link Expr} tree with a {@link Shuttle}. Each {@link Expr} is responsible for
    * ensuring the {@link Shuttle} can visit all of its {@link Expr} children, as well as updating its children
    * {@link Expr} with the results from the {@link Shuttle}, before finally visiting an updated form of itself.
+   *
+   * When this Expr is the result of {@link ExprMacroTable.ExprMacro#apply}, all of the original arguments to the
+   * macro must be visited, including arguments that may have been "baked in" to this Expr.
    */
   Expr visit(Shuttle shuttle);
 
@@ -158,6 +161,7 @@ public interface Expr extends Cacheable
    * Check if an expression can be 'vectorized', for a given set of inputs. If this method returns true,
    * {@link #buildVectorized} is expected to produce a {@link ExprVectorProcessor} which can evaluate values in batches
    * to use with vectorized query engines.
+   *
    * @param inspector
    */
   default boolean canVectorize(InputBindingInspector inspector)
@@ -168,6 +172,7 @@ public interface Expr extends Cacheable
   /**
    * Builds a 'vectorized' expression processor, that can operate on batches of input values for use in vectorized
    * query engines.
+   *
    * @param inspector
    */
   default <T> ExprVectorProcessor<T> buildVectorized(VectorInputBindingInspector inspector)
@@ -390,6 +395,17 @@ public interface Expr extends Cacheable
      * Provide the {@link Shuttle} with an {@link Expr} to inspect and potentially rewrite.
      */
     Expr visit(Expr expr);
+
+    default List<Expr> visitAll(List<Expr> exprs)
+    {
+      final List<Expr> newExprs = new ArrayList<>();
+
+      for (final Expr arg : exprs) {
+        newExprs.add(visit(arg));
+      }
+
+      return newExprs;
+    }
   }
 
   /**

core/src/main/java/org/apache/druid/math/expr/FunctionalExpr.java
@@ -204,8 +204,7 @@ class FunctionExpr implements Expr
   @Override
   public Expr visit(Shuttle shuttle)
   {
-    List<Expr> newArgs = args.stream().map(shuttle::visit).collect(Collectors.toList());
-    return shuttle.visit(new FunctionExpr(function, name, newArgs));
+    return shuttle.visit(new FunctionExpr(function, name, shuttle.visitAll(args)));
   }
 
   @Override
@@ -334,8 +333,7 @@ class ApplyFunctionExpr implements Expr
   public Expr visit(Shuttle shuttle)
   {
     LambdaExpr newLambda = (LambdaExpr) lambdaExpr.visit(shuttle);
-    List<Expr> newArgs = argsExpr.stream().map(shuttle::visit).collect(Collectors.toList());
-    return shuttle.visit(new ApplyFunctionExpr(function, name, newLambda, newArgs));
+    return shuttle.visit(new ApplyFunctionExpr(function, name, newLambda, shuttle.visitAll(argsExpr)));
   }
 
   @Override

core/src/test/java/org/apache/druid/math/expr/ExprTest.java
@@ -19,8 +19,14 @@
 
 package org.apache.druid.math.expr;
 
+import com.google.common.collect.ImmutableList;
 import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ExprTest
 {
@@ -196,6 +202,7 @@ public class ExprTest
   {
     EqualsVerifier.forClass(LambdaExpr.class).usingGetClass().verify();
   }
+
   @Test
   public void testEqualsContractForNullLongExpr()
   {
@@ -212,5 +219,32 @@ public class ExprTest
                   .withIgnoredFields("outputType", "value")
                   .withPrefabValues(ExpressionType.class, ExpressionType.DOUBLE, ExpressionType.STRING)
                   .verify();
+  }
+
+  @Test
+  public void testShuttleVisitAll()
+  {
+    final List<Expr> visitedExprs = new ArrayList<>();
+
+    final Expr.Shuttle shuttle = expr -> {
+      visitedExprs.add(expr);
+      return expr;
+    };
+
+    shuttle.visitAll(Collections.emptyList());
+    Assert.assertEquals("Visiting an empty list", Collections.emptyList(), visitedExprs);
+
+    final List<Expr> oneIdentifier = Collections.singletonList(new IdentifierExpr("ident"));
+    visitedExprs.clear();
+    shuttle.visitAll(oneIdentifier);
+    Assert.assertEquals("One identifier", oneIdentifier, visitedExprs);
+
+    final List<Expr> twoIdentifiers = ImmutableList.of(
+        new IdentifierExpr("ident1"),
+        new IdentifierExpr("ident2")
+    );
+    visitedExprs.clear();
+    shuttle.visitAll(twoIdentifiers);
+    Assert.assertEquals("Two identifiers", twoIdentifiers, visitedExprs);
   }
 }

extensions-core/druid-bloom-filter/src/main/java/org/apache/druid/query/expressions/BloomFilterExpressions.java
@@ -35,7 +35,6 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class BloomFilterExpressions
 {
@@ -88,7 +87,7 @@ public class BloomFilterExpressions
         @Override
         public Expr visit(Shuttle shuttle)
         {
-          return shuttle.visit(this);
+          return shuttle.visit(apply(shuttle.visitAll(args)));
         }
 
         @Nullable
@@ -171,8 +170,7 @@ public class BloomFilterExpressions
         @Override
         public Expr visit(Shuttle shuttle)
         {
-          List<Expr> newArgs = args.stream().map(x -> x.visit(shuttle)).collect(Collectors.toList());
-          return shuttle.visit(new BloomExpr(newArgs));
+          return shuttle.visit(apply(shuttle.visitAll(args)));
         }
 
         @Nullable
@@ -259,8 +257,7 @@ public class BloomFilterExpressions
         @Override
         public Expr visit(Shuttle shuttle)
         {
-          Expr newArg = arg.visit(shuttle);
-          return shuttle.visit(new BloomExpr(filter, newArg));
+          return shuttle.visit(apply(shuttle.visitAll(args)));
         }
 
         @Nullable
@@ -331,8 +328,7 @@ public class BloomFilterExpressions
         @Override
         public Expr visit(Shuttle shuttle)
         {
-          List<Expr> newArgs = args.stream().map(x -> x.visit(shuttle)).collect(Collectors.toList());
-          return shuttle.visit(new DynamicBloomExpr(newArgs));
+          return shuttle.visit(apply(shuttle.visitAll(args)));
         }
 
         @Nullable

extensions-core/testing-tools/src/main/java/org/apache/druid/query/expressions/SleepExprMacro.java
@@ -85,8 +85,7 @@ public class SleepExprMacro implements ExprMacro
       @Override
       public Expr visit(Shuttle shuttle)
       {
-        Expr newArg = arg.visit(shuttle);
-        return shuttle.visit(new SleepExpr(newArg));
+        return shuttle.visit(apply(shuttle.visitAll(args)));
       }
 
       /**

processing/src/main/java/org/apache/druid/query/expression/CaseInsensitiveContainsExprMacro.java
@@ -58,6 +58,6 @@ public class CaseInsensitiveContainsExprMacro implements ExprMacroTable.ExprMacr
 
     final Expr arg = args.get(0);
     final Expr searchStr = args.get(1);
-    return new ContainsExpr(FN_NAME, arg, searchStr, false);
+    return new ContainsExpr(FN_NAME, arg, searchStr, false, shuttle -> apply(shuttle.visitAll(args)));
   }
 }

processing/src/main/java/org/apache/druid/query/expression/ContainsExpr.java
@@ -39,20 +39,31 @@ class ContainsExpr extends ExprMacroTable.BaseScalarUnivariateMacroFunctionExpr
 {
   private final Function<String, Boolean> searchFunction;
   private final Expr searchStrExpr;
+  private final Function<Shuttle, Expr> visitFunction;
 
-  ContainsExpr(String functioName, Expr arg, Expr searchStrExpr, boolean caseSensitive)
+  ContainsExpr(
+      final String functionName,
+      final Expr arg,
+      final Expr searchStrExpr,
+      final boolean caseSensitive,
+      final Function<Shuttle, Expr> visitFunction
+  )
   {
-    super(functioName, arg);
-    this.searchStrExpr = validateSearchExpr(searchStrExpr, functioName);
-    // Creates the function eagerly to avoid branching in eval.
-    this.searchFunction = createFunction(searchStrExpr, caseSensitive);
+    this(functionName, arg, searchStrExpr, createFunction(searchStrExpr, caseSensitive), visitFunction);
   }
 
-  private ContainsExpr(String functioName, Expr arg, Expr searchStrExpr, Function<String, Boolean> searchFunction)
+  private ContainsExpr(
+      final String functionName,
+      final Expr arg,
+      final Expr searchStrExpr,
+      final Function<String, Boolean> searchFunction,
+      final Function<Shuttle, Expr> visitFunction
+  )
   {
-    super(functioName, arg);
+    super(functionName, arg);
     this.searchFunction = searchFunction;
-    this.searchStrExpr = validateSearchExpr(searchStrExpr, functioName);
+    this.searchStrExpr = validateSearchExpr(searchStrExpr, functionName);
+    this.visitFunction = visitFunction;
   }
 
   @Nonnull
@@ -80,8 +91,7 @@ class ContainsExpr extends ExprMacroTable.BaseScalarUnivariateMacroFunctionExpr
   @Override
   public Expr visit(Expr.Shuttle shuttle)
   {
-    Expr newArg = arg.visit(shuttle);
-    return shuttle.visit(new ContainsExpr(name, newArg, searchStrExpr, searchFunction));
+    return visitFunction.apply(shuttle);
   }
 
   @Override
@@ -90,20 +100,20 @@ class ContainsExpr extends ExprMacroTable.BaseScalarUnivariateMacroFunctionExpr
     return StringUtils.format("%s(%s, %s)", name, arg.stringify(), searchStrExpr.stringify());
   }
 
-  private Function<String, Boolean> createFunction(Expr searchStrExpr, boolean caseSensitive)
-  {
-    String searchStr = StringUtils.nullToEmptyNonDruidDataString((String) searchStrExpr.getLiteralValue());
-    if (caseSensitive) {
-      return s -> s.contains(searchStr);
-    }
-    return s -> org.apache.commons.lang.StringUtils.containsIgnoreCase(s, searchStr);
-  }
-
   private Expr validateSearchExpr(Expr searchExpr, String functioName)
   {
     if (!ExprUtils.isStringLiteral(searchExpr)) {
       throw new IAE("Function[%s] substring must be a string literal", functioName);
     }
     return searchExpr;
+  }
+
+  private static Function<String, Boolean> createFunction(Expr searchStrExpr, boolean caseSensitive)
+  {
+    String searchStr = StringUtils.nullToEmptyNonDruidDataString((String) searchStrExpr.getLiteralValue());
+    if (caseSensitive) {
+      return s -> s.contains(searchStr);
+    }
+    return s -> org.apache.commons.lang.StringUtils.containsIgnoreCase(s, searchStr);
   }
 }

processing/src/main/java/org/apache/druid/query/expression/ContainsExprMacro.java
@@ -57,6 +57,6 @@ public class ContainsExprMacro implements ExprMacroTable.ExprMacro
 
     final Expr arg = args.get(0);
     final Expr searchStr = args.get(1);
-    return new ContainsExpr(FN_NAME, arg, searchStr, true);
+    return new ContainsExpr(FN_NAME, arg, searchStr, true, shuttle -> shuttle.visit(apply(shuttle.visitAll(args))));
   }
 }

commit 21c8912e26d7441e6169d8613265db2e5cc043b8

Updates from code review, and some additional stuff inspired by the investigation.
- Remove unnecessary formatting code from DruidExpression.doubleLiteral:
  it handles things just fine with its default behavior.

- Fix a problem where expression literals could not represent Long.MIN_VALUE.
  Now, integer literals start life off as BigIntegerExpr instead of LongExpr,
  and are converted to LongExpr during flattening. This is necessary because,
  in order to avoid ambiguity between unary minus and negative literals, our
  grammar does not actually have true negative literals. Negative numbers must
  be represented as unary minus next to a positive literal.

- Fix a bug  introduced in #12230 where shuttle.visitAll(args) delegated
  to shuttle.visit(arg) instead of arg.visit(shuttle). The latter does
  a recursive visitation, which is the intended behavior.

core/src/main/java/org/apache/druid/math/expr/ConstantExpr.java
@@ -29,6 +29,7 @@ import org.apache.druid.math.expr.vector.VectorProcessors;
 import org.apache.druid.segment.column.TypeStrategy;
 
 import javax.annotation.Nullable;
+import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Objects;
@@ -98,6 +99,59 @@ abstract class ConstantExpr<T> implements Expr
   public String stringify()
   {
     return toString();
+  }
+}
+
+/**
+ * Even though expressions don't generally support BigInteger, we need this object so we can represent
+ * {@link Long#MIN_VALUE} as a {@link UnaryMinusExpr} applied to {@link ConstantExpr}. Antlr cannot parse negative
+ * longs directly, due to ambiguity between negative numbers and unary minus.
+ */
+class BigIntegerExpr extends ConstantExpr<BigInteger>
+{
+  public BigIntegerExpr(BigInteger value)
+  {
+    super(ExpressionType.LONG, Preconditions.checkNotNull(value, "value"));
+  }
+
+  @Override
+  public String toString()
+  {
+    return value.toString();
+  }
+
+  @Override
+  public ExprEval eval(ObjectBinding bindings)
+  {
+    // Eval succeeds if the BigInteger is in long range.
+    // Callers that need to process out-of-long-range values, like UnaryMinusExpr, must use getLiteralValue().
+    return ExprEval.ofLong(value.longValueExact());
+  }
+
+  @Override
+  public boolean canVectorize(InputBindingInspector inspector)
+  {
+    // No vectorization needed: Parser.flatten converts BigIntegerExpr to LongExpr at parse time.
+    return false;
+  }
+
+  @Override
+  public boolean equals(Object o)
+  {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    BigIntegerExpr otherExpr = (BigIntegerExpr) o;
+    return value.equals(otherExpr.value);
+  }
+
+  @Override
+  public int hashCode()
+  {
+    return value.hashCode();
   }
 }
 

core/src/main/java/org/apache/druid/math/expr/Expr.java
@@ -406,7 +406,7 @@ public interface Expr extends Cacheable
       final List<Expr> newExprs = new ArrayList<>();
 
       for (final Expr arg : exprs) {
-        newExprs.add(visit(arg));
+        newExprs.add(arg.visit(this));
       }
 
       return newExprs;

core/src/main/java/org/apache/druid/math/expr/ExprListenerImpl.java
@@ -30,6 +30,7 @@ import org.apache.druid.math.expr.antlr.ExprBaseListener;
 import org.apache.druid.math.expr.antlr.ExprParser;
 
 import javax.annotation.Nullable;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -145,7 +146,7 @@ public class ExprListenerImpl extends ExprBaseListener
   {
     nodes.put(
         ctx,
-        new LongExpr(Long.parseLong(ctx.getText()))
+        new BigIntegerExpr(new BigInteger(ctx.getText()))
     );
   }
 

core/src/main/java/org/apache/druid/math/expr/Parser.java
@@ -182,6 +182,14 @@ public class Parser
         }
       }
       return childExpr;
+    }).visit(childExpr -> {
+      // Now that unary minus has been applied to any BigIntegerExpr where it is needed, transform all remaining
+      // BigIntegerExpr into LongExpr.
+      if (childExpr instanceof BigIntegerExpr) {
+        return childExpr.eval(InputBindings.nilBindings()).toExpr();
+      } else {
+        return childExpr;
+      }
     });
   }
 

core/src/main/java/org/apache/druid/math/expr/UnaryOperatorExpr.java
@@ -29,6 +29,7 @@ import org.apache.druid.math.expr.vector.VectorProcessors;
 import org.apache.druid.segment.column.Types;
 
 import javax.annotation.Nullable;
+import java.math.BigInteger;
 import java.util.Objects;
 
 /**
@@ -122,6 +123,10 @@ class UnaryMinusExpr extends UnaryExpr
   @Override
   public ExprEval eval(ObjectBinding bindings)
   {
+    if (expr instanceof BigIntegerExpr) {
+      // Special case to handle unary minus for Long.MIN_VALUE: converting the literal to long directly is impossible
+      return ExprEval.of(((BigInteger) expr.getLiteralValue()).multiply(BigInteger.valueOf(-1)).longValueExact());
+    }
     ExprEval ret = expr.eval(bindings);
     if (NullHandling.sqlCompatible() && (ret.value() == null)) {
       return ExprEval.of(null);

sql/src/main/java/org/apache/druid/sql/calcite/expression/DruidExpression.java
@@ -111,17 +111,7 @@ public class DruidExpression
 
   public static String doubleLiteral(final double n)
   {
-    final String s = String.valueOf(n);
-
-    // Ensure number is parsed as a double: add ".0" if necessary.
-    for (int i = 0; i < s.length(); i++) {
-      final char c = s.charAt(i);
-      if (!Character.isDigit(c)) {
-        return s;
-      }
-    }
-
-    return s + ".0";
+    return String.valueOf(n);
   }
 
   public static String stringLiteral(final String s)

core/src/test/java/org/apache/druid/math/expr/ParserTest.java
@@ -29,12 +29,15 @@ import org.apache.druid.segment.column.TypeStrategies;
 import org.apache.druid.segment.column.TypeStrategiesTest;
 import org.apache.druid.segment.column.TypeStrategy;
 import org.apache.druid.testing.InitializedNullHandlingTest;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.util.Collections;
 import java.util.List;
@@ -67,14 +70,14 @@ public class ParserTest extends InitializedNullHandlingTest
     Assert.assertEquals(expected, actual);
   }
 
-
   @Test
   public void testParseConstants()
   {
     validateLiteral("null", null, null);
     validateLiteral("'hello'", ExpressionType.STRING, "hello");
     validateLiteral("'hello \\uD83E\\uDD18'", ExpressionType.STRING, "hello \uD83E\uDD18");
-    validateLiteral("1", ExpressionType.LONG, 1L);
+    validateLiteral("1", ExpressionType.LONG, BigInteger.valueOf(1L));
+    validateLiteral(String.valueOf(Long.MAX_VALUE), ExpressionType.LONG, BigInteger.valueOf(Long.MAX_VALUE));
     validateLiteral("1.", ExpressionType.DOUBLE, 1.0, false);
     validateLiteral("1.234", ExpressionType.DOUBLE, 1.234);
     validateLiteral("1e10", ExpressionType.DOUBLE, 1.0E10, false);
@@ -94,6 +97,48 @@ public class ParserTest extends InitializedNullHandlingTest
   }
 
   @Test
+  public void testParseOutOfRangeLong()
+  {
+    // Two greater than Long.MAX_VALUE
+    final String s = "9223372036854775809";
+
+    // When not flattening, the "out of long range" error happens during eval.
+    final Expr expr = Parser.parse(s, ExprMacroTable.nil(), false);
+    final ArithmeticException e = Assert.assertThrows(
+        ArithmeticException.class,
+        () -> expr.eval(InputBindings.nilBindings())
+    );
+    MatcherAssert.assertThat(e.getMessage(), CoreMatchers.containsString("BigInteger out of long range"));
+
+    // When flattening, the "out of long range" error happens during parse, not eval.
+    final ArithmeticException e2 = Assert.assertThrows(
+        ArithmeticException.class,
+        () -> Parser.parse(s, ExprMacroTable.nil(), true)
+    );
+    MatcherAssert.assertThat(e2.getMessage(), CoreMatchers.containsString("BigInteger out of long range"));
+  }
+
+  @Test
+  public void testFlattenBinaryOpConstantConstant()
+  {
+    final Expr expr = Parser.parse("(2 + -3)", ExprMacroTable.nil(), true);
+    Assert.assertTrue(expr.isLiteral());
+    Assert.assertEquals(-1L, expr.getLiteralValue());
+  }
+
+  @Test
+  public void testFlattenBinaryOpIdentifierConstant()
+  {
+    final Expr expr = Parser.parse("(s + -3)", ExprMacroTable.nil(), true);
+    Assert.assertFalse(expr.isLiteral());
+    MatcherAssert.assertThat(expr, CoreMatchers.instanceOf(BinPlusExpr.class));
+
+    final Expr right = ((BinPlusExpr) expr).right;
+    Assert.assertTrue(right.isLiteral());
+    Assert.assertEquals(-3L, right.getLiteralValue());
+  }
+
+  @Test
   public void testSimpleUnaryOps1()
   {
     String actual = Parser.parse("-x", ExprMacroTable.nil()).toString();
@@ -108,6 +153,7 @@ public class ParserTest extends InitializedNullHandlingTest
   @Test
   public void testSimpleUnaryOps2()
   {
+    validateFlatten(String.valueOf(Long.MIN_VALUE), String.valueOf(Long.MIN_VALUE), String.valueOf(Long.MIN_VALUE));
     validateFlatten("-1", "-1", "-1");
     validateFlatten("--1", "--1", "1");
     validateFlatten("-1+2", "(+ -1 2)", "1");
@@ -735,7 +781,11 @@ public class ParserTest extends InitializedNullHandlingTest
     Assert.assertEquals(type, parsed.getOutputType(emptyBinding));
     Assert.assertEquals(type, parsedFlat.getOutputType(emptyBinding));
     Assert.assertEquals(expected, parsed.getLiteralValue());
-    Assert.assertEquals(expected, parsedFlat.getLiteralValue());
+    Assert.assertEquals(
+        // Special case comparison: literal integers start life as BigIntegerExpr; converted to LongExpr later.
+        expected instanceof BigInteger ? ((BigInteger) expected).longValueExact() : expected,
+        parsedFlat.getLiteralValue()
+    );
     if (roundTrip) {
       Assert.assertEquals(expr, parsed.stringify());
       Assert.assertEquals(expr, parsedFlat.stringify());

sql/src/test/java/org/apache/druid/sql/calcite/expression/DruidExpressionTest.java
@@ -1,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.sql.calcite.expression;
+
+import org.apache.druid.math.expr.Expr;
+import org.apache.druid.math.expr.ExprMacroTable;
+import org.apache.druid.math.expr.Parser;
+import org.apache.druid.testing.InitializedNullHandlingTest;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DruidExpressionTest extends InitializedNullHandlingTest
+{
+  @Test
+  public void test_doubleLiteral_asString()
+  {
+    Assert.assertEquals("0.0", DruidExpression.doubleLiteral(0));
+    Assert.assertEquals("-2.0", DruidExpression.doubleLiteral(-2));
+    Assert.assertEquals("2.0", DruidExpression.doubleLiteral(2));
+    Assert.assertEquals("2.1", DruidExpression.doubleLiteral(2.1));
+    Assert.assertEquals("2.12345678", DruidExpression.doubleLiteral(2.12345678));
+    Assert.assertEquals("2.2E122", DruidExpression.doubleLiteral(2.2e122));
+    Assert.assertEquals("NaN", DruidExpression.doubleLiteral(Double.NaN));
+    Assert.assertEquals("Infinity", DruidExpression.doubleLiteral(Double.POSITIVE_INFINITY));
+    Assert.assertEquals("-Infinity", DruidExpression.doubleLiteral(Double.NEGATIVE_INFINITY));
+    Assert.assertEquals("4.9E-324", DruidExpression.doubleLiteral(Double.MIN_VALUE));
+    Assert.assertEquals("1.7976931348623157E308", DruidExpression.doubleLiteral(Double.MAX_VALUE));
+    Assert.assertEquals("2.2250738585072014E-308", DruidExpression.doubleLiteral(Double.MIN_NORMAL));
+  }
+
+  @Test
+  public void test_doubleLiteral_roundTrip()
+  {
+    final double[] doubles = {
+        0,
+        -2,
+        2,
+        2.1,
+        2.12345678,
+        2.2e122,
+        Double.NaN,
+        Double.POSITIVE_INFINITY,
+        Double.NEGATIVE_INFINITY,
+        Double.MIN_VALUE,
+        Double.MAX_VALUE,
+        Double.MIN_NORMAL
+    };
+
+    for (double n : doubles) {
+      final Expr expr = Parser.parse(DruidExpression.doubleLiteral(n), ExprMacroTable.nil());
+      Assert.assertTrue(expr.isLiteral());
+      MatcherAssert.assertThat(expr.getLiteralValue(), CoreMatchers.instanceOf(Double.class));
+      Assert.assertEquals(n, (double) expr.getLiteralValue(), 0d);
+    }
+  }
+
+  @Test
+  public void test_longLiteral_asString()
+  {
+    Assert.assertEquals("0", DruidExpression.longLiteral(0));
+    Assert.assertEquals("-2", DruidExpression.longLiteral(-2));
+    Assert.assertEquals("2", DruidExpression.longLiteral(2));
+    Assert.assertEquals("9223372036854775807", DruidExpression.longLiteral(Long.MAX_VALUE));
+    Assert.assertEquals("-9223372036854775808", DruidExpression.longLiteral(Long.MIN_VALUE));
+  }
+
+  @Test
+  public void longLiteral_roundTrip()
+  {
+    final long[] longs = {
+        0,
+        -2,
+        2,
+        Long.MAX_VALUE,
+        Long.MIN_VALUE
+    };
+
+    for (long n : longs) {
+      final Expr expr = Parser.parse(DruidExpression.longLiteral(n), ExprMacroTable.nil());
+      Assert.assertTrue(expr.isLiteral());
+      MatcherAssert.assertThat(expr.getLiteralValue(), CoreMatchers.instanceOf(Number.class));
+      Assert.assertEquals(n, ((Number) expr.getLiteralValue()).longValue());
+    }
+  }
+}

sql/src/test/java/org/apache/druid/sql/calcite/expression/ExpressionsTest.java
@@ -23,14 +23,11 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.apache.calcite.avatica.util.TimeUnit;
 import org.apache.calcite.avatica.util.TimeUnitRange;
-import org.apache.calcite.jdbc.JavaTypeFactoryImpl;
-import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.sql.SqlFunction;
 import org.apache.calcite.sql.SqlIntervalQualifier;
 import org.apache.calcite.sql.fun.SqlStdOperatorTable;
 import org.apache.calcite.sql.fun.SqlTrimFunction;
 import org.apache.calcite.sql.parser.SqlParserPos;
-import org.apache.calcite.sql.type.SqlTypeFactoryImpl;
 import org.apache.calcite.sql.type.SqlTypeName;
 import org.apache.druid.common.config.NullHandling;
 import org.apache.druid.java.util.common.DateTimes;
@@ -65,7 +62,6 @@ import org.apache.druid.sql.calcite.expression.builtin.TimeFormatOperatorConvers
 import org.apache.druid.sql.calcite.expression.builtin.TimeParseOperatorConversion;
 import org.apache.druid.sql.calcite.expression.builtin.TimeShiftOperatorConversion;
 import org.apache.druid.sql.calcite.expression.builtin.TruncateOperatorConversion;
-import org.apache.druid.sql.calcite.planner.DruidTypeSystem;
 import org.joda.time.Period;
 import org.junit.Before;
 import org.junit.Test;
@@ -2480,11 +2476,14 @@ public class ExpressionsTest extends ExpressionTestBase
         makeExpression("human_readable_binary_byte_format(9223372036854775807)"),
         "8.00 EiB"
     );
-
-    /*
-     * NOTE: Test for Long.MIN_VALUE is skipped since ExprListnerImpl#exitLongExpr fails to parse Long.MIN_VALUE
-     * This cases has also been verified in the tests of underlying implementation
-     */
+    testHelper.testExpressionString(
+        HumanReadableFormatOperatorConversion.BINARY_BYTE_FORMAT.calciteOperator(),
+        ImmutableList.of(
+            testHelper.makeLiteral(Long.MIN_VALUE)
+        ),
+        makeExpression("human_readable_binary_byte_format(-9223372036854775808)"),
+        "-8.00 EiB"
+    );
 
     /*
      * test input with variable reference
@@ -2574,10 +2573,14 @@ public class ExpressionsTest extends ExpressionTestBase
         makeExpression("human_readable_decimal_byte_format(9223372036854775807)"),
         "9.22 EB"
     );
-
-    /*
-     * NOTE: Test for Long.MIN_VALUE is skipped since ExprListnerImpl#exitLongExpr fails to parse Long.MIN_VALUE
-     */
+    testHelper.testExpressionString(
+        HumanReadableFormatOperatorConversion.DECIMAL_BYTE_FORMAT.calciteOperator(),
+        ImmutableList.of(
+            testHelper.makeLiteral(Long.MIN_VALUE)
+        ),
+        makeExpression("human_readable_decimal_byte_format(-9223372036854775808)"),
+        "-9.22 EB"
+    );
 
     /*
      * test input with variable reference
@@ -2635,13 +2638,5 @@ public class ExpressionsTest extends ExpressionTestBase
         makeExpression("human_readable_decimal_byte_format(45678,3)"),
         "45.678 KB"
     );
-  }
-
-  @Test
-  public void testLiteralToDruidExpressionForLong()
-  {
-    final RelDataTypeFactory typeFactory = new SqlTypeFactoryImpl(DruidTypeSystem.INSTANCE);
-
-    Expressions.literalToDruidExpression(null, )
   }
 }