4cad445d9f56ab0b19f801d667742118af73a49c
Kashif Faraz <kashif.faraz@gmail.com>
2022-09-26 09:41:16 +0530

    Remove unused import

--- server/src/test/java/org/apache/druid/server/coordinator/DruidCoordinatorTest.java
+++ server/src/test/java/org/apache/druid/server/coordinator/DruidCoordinatorTest.java
@@ -85,7 +85,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
