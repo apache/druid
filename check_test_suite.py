26fcd8f082ce9d89fdfc38e195dbb87d37072fd0
Kashif Faraz <kashif.faraz@gmail.com>
2022-10-24 14:54:08 +0530

    Force scripts to use python3.6

--- check_test_suite.py
+++ check_test_suite.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.6
 
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
