a28b8c26745cc9656ce4aa3e0301f6ebd8ed76f4
Adarsh Sanjeev <adarshsanjeev@gmail.com>
2022-11-08 10:12:07 +0530

    Improve rowkey object size estimate (#13319)
    * Improve rowkey object size estimate
    
    * Address review comments
    
    * Update comment
    
    * Fix test

--- processing/src/main/java/org/apache/druid/frame/key/RowKey.java
+++ processing/src/main/java/org/apache/druid/frame/key/RowKey.java
@@ -32,6 +32,10 @@ public class RowKey
 {
   private static final RowKey EMPTY_KEY = new RowKey(new byte[0]);
 
+  // Constant to account for hashcode and object overhead
+  // 24 bytes (header) + 8 bytes (reference) + 8 bytes (hashCode long) + 4 bytes (safe estimate of hashCodeComputed)
+  static final int OBJECT_OVERHEAD_SIZE_BYTES = 44;
+
   private final byte[] key;
 
   // Cached hashcode. Computed on demand, not in the constructor, to avoid unnecessary computation.
@@ -109,8 +113,12 @@ public class RowKey
     return Arrays.toString(key);
   }
 
-  public int getNumberOfBytes()
+  /**
+   * Estimate number of bytes taken by an object of {@link RowKey}. Only returns an estimate and does not account for
+   * platform or JVM specific implementation.
+   */
+  public int estimatedObjectSizeBytes()
   {
-    return array().length;
+    return OBJECT_OVERHEAD_SIZE_BYTES + array().length;
   }
 }
