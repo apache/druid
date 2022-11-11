357e8e37789cb699e8f7cac2afac14fa19007274
Adarsh Sanjeev <adarshsanjeev@gmail.com>
2022-11-11 09:24:36 +0530

    Modify quantile sketchs to add byte[] directly

--- processing/src/main/java/org/apache/druid/frame/key/RowKeyComparator.java
+++ processing/src/main/java/org/apache/druid/frame/key/RowKeyComparator.java
@@ -19,129 +19,32 @@
 
 package org.apache.druid.frame.key;
 
-import com.google.common.primitives.Ints;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
-import it.unimi.dsi.fastutil.ints.IntList;
-import org.apache.druid.frame.read.FrameReaderUtils;
-
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Comparator for {@link RowKey} instances.
  *
- * Comparison logic in this class is very similar to {@link FrameComparisonWidget}, but is different because it works
- * on byte[] instead of Frames.
+ * Delegates the comparing to a {@link ByteRowKeyComparator}.
  */
 public class RowKeyComparator implements Comparator<RowKey>
 {
-  private final int firstFieldPosition;
-  private final int[] ascDescRunLengths;
+  private final ByteRowKeyComparator byteRowKeyComparatorDelegate;
 
-  private RowKeyComparator(
-      final int firstFieldPosition,
-      final int[] ascDescRunLengths
-  )
+  private RowKeyComparator(final ByteRowKeyComparator byteRowKeyComparatorDelegate)
   {
-    this.firstFieldPosition = firstFieldPosition;
-    this.ascDescRunLengths = ascDescRunLengths;
+    this.byteRowKeyComparatorDelegate = byteRowKeyComparatorDelegate;
   }
 
   public static RowKeyComparator create(final List<SortColumn> keyColumns)
   {
-    return new RowKeyComparator(
-        computeFirstFieldPosition(keyColumns.size()),
-        computeAscDescRunLengths(keyColumns)
-    );
-  }
-
-  /**
-   * Compute the offset into each key where the first field starts.
-   *
-   * Public so {@link FrameComparisonWidgetImpl} can use it.
-   */
-  public static int computeFirstFieldPosition(final int fieldCount)
-  {
-    return Ints.checkedCast((long) fieldCount * Integer.BYTES);
-  }
-
-  /**
-   * Given a list of sort columns, compute an array of the number of ascending fields in a run, followed by number of
-   * descending fields in a run, followed by ascending, etc. For example: ASC, ASC, DESC, ASC would return [2, 1, 1]
-   * and DESC, DESC, ASC would return [0, 2, 1].
-   *
-   * Public so {@link FrameComparisonWidgetImpl} can use it.
-   */
-  public static int[] computeAscDescRunLengths(final List<SortColumn> sortColumns)
-  {
-    final IntList ascDescRunLengths = new IntArrayList(4);
-
-    boolean descending = false;
-    int runLength = 0;
-
-    for (final SortColumn column : sortColumns) {
-      if (column.descending() != descending) {
-        ascDescRunLengths.add(runLength);
-        runLength = 0;
-        descending = !descending;
-      }
-
-      runLength++;
-    }
-
-    if (runLength > 0) {
-      ascDescRunLengths.add(runLength);
-    }
-
-    return ascDescRunLengths.toIntArray();
+    return new RowKeyComparator(ByteRowKeyComparator.create(keyColumns));
   }
 
   @Override
-  @SuppressWarnings("SubtractionInCompareTo")
   public int compare(final RowKey key1, final RowKey key2)
   {
-    // Similar logic to FrameComparaisonWidgetImpl, but implementation is different enough that we need our own.
-    // Major difference is Frame v. Frame instead of byte[] v. byte[].
-
-    final byte[] keyArray1 = key1.array();
-    final byte[] keyArray2 = key2.array();
-
-    int comparableBytesStartPosition1 = firstFieldPosition;
-    int comparableBytesStartPosition2 = firstFieldPosition;
-
-    boolean ascending = true;
-    int field = 0;
-
-    for (int numFields : ascDescRunLengths) {
-      if (numFields > 0) {
-        final int nextField = field + numFields;
-        final int comparableBytesEndPosition1 = RowKeyReader.fieldEndPosition(keyArray1, nextField - 1);
-        final int comparableBytesEndPosition2 = RowKeyReader.fieldEndPosition(keyArray2, nextField - 1);
-
-        int cmp = FrameReaderUtils.compareByteArraysUnsigned(
-            keyArray1,
-            comparableBytesStartPosition1,
-            comparableBytesEndPosition1 - comparableBytesStartPosition1,
-            keyArray2,
-            comparableBytesStartPosition2,
-            comparableBytesEndPosition2 - comparableBytesStartPosition2
-        );
-
-        if (cmp != 0) {
-          return ascending ? cmp : -cmp;
-        }
-
-        field = nextField;
-        comparableBytesStartPosition1 = comparableBytesEndPosition1;
-        comparableBytesStartPosition2 = comparableBytesEndPosition2;
-      }
-
-      ascending = !ascending;
-    }
-
-    return 0;
+    return byteRowKeyComparatorDelegate.compare(key1.array(), key2.array());
   }
 
   @Override
@@ -154,24 +57,20 @@ public class RowKeyComparator implements Comparator<RowKey>
       return false;
     }
     RowKeyComparator that = (RowKeyComparator) o;
-    return firstFieldPosition == that.firstFieldPosition
-           && Arrays.equals(ascDescRunLengths, that.ascDescRunLengths);
+    return byteRowKeyComparatorDelegate.equals(that.byteRowKeyComparatorDelegate);
   }
 
   @Override
   public int hashCode()
   {
-    int result = Objects.hash(firstFieldPosition);
-    result = 31 * result + Arrays.hashCode(ascDescRunLengths);
-    return result;
+    return byteRowKeyComparatorDelegate.hashCode();
   }
 
   @Override
   public String toString()
   {
     return "RowKeyComparator{" +
-           "firstFieldPosition=" + firstFieldPosition +
-           ", ascDescRunLengths=" + Arrays.toString(ascDescRunLengths) +
+           "byteRowKeyComparatorDelegate=" + byteRowKeyComparatorDelegate +
            '}';
   }
 }
