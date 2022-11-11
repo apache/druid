56d5c9780d6c09d6844136a6c678e3b417388bfe
Didip Kerabat <didipk@gmail.com>
2022-11-10 23:46:40 -0800

    Use standard library to correctly glob and stop at the correct folder structure when filtering cloud objects (#13027)
    * Use standard library to correctly glob and stop at the correct folder structure when filtering cloud objects.
    
    Removed:
    
    import org.apache.commons.io.FilenameUtils;
    
    Add:
    
    import java.nio.file.FileSystems;
    import java.nio.file.PathMatcher;
    import java.nio.file.Paths;
    
    * Forgot to update CloudObjectInputSource as well.
    
    * Fix tests.
    
    * Removed unused exceptions.
    
    * Able to reduced user mistakes, by removing the protocol and the bucket on filter.
    
    * add 1 more test.
    
    * add comment on filterWithoutProtocolAndBucket
    
    * Fix lint issue.
    
    * Fix another lint issue.
    
    * Replace all mention of filter -> objectGlob per convo here:
    
    https://github.com/apache/druid/pull/13027#issuecomment-1266410707
    
    * fix 1 bad constructor.
    
    * Fix the documentation.
    
    * Don’t do anything clever with the object path.
    
    * Remove unused imports.
    
    * Fix spelling error.
    
    * Fix incorrect search and replace.
    
    * Addressing Gian’s comment.
    
    * add filename on .spelling
    
    * Fix documentation.
    
    * fix documentation again
    
    Co-authored-by: Didip Kerabat <didip@apple.com>

--- extensions-core/s3-extensions/src/main/java/org/apache/druid/data/input/s3/S3InputSource.java
+++ extensions-core/s3-extensions/src/main/java/org/apache/druid/data/input/s3/S3InputSource.java
@@ -37,7 +37,6 @@ import com.google.common.base.Preconditions;
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
 import com.google.common.collect.Iterators;
-import org.apache.commons.io.FilenameUtils;
 import org.apache.druid.common.aws.AWSClientConfig;
 import org.apache.druid.common.aws.AWSEndpointConfig;
 import org.apache.druid.common.aws.AWSProxyConfig;
@@ -59,6 +58,9 @@ import org.apache.druid.utils.Streams;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.net.URI;
+import java.nio.file.FileSystems;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
@@ -94,6 +96,7 @@ public class S3InputSource extends CloudObjectInputSource
    * @param uris                User provided uris to read input data
    * @param prefixes            User provided prefixes to read input data
    * @param objects             User provided cloud objects values to read input data
+   * @param objectGlob          User provided globbing rule to filter input data path
    * @param s3InputSourceConfig User provided properties for overriding the default S3 credentials
    * @param awsProxyConfig      User provided proxy information for the overridden s3 client
    * @param awsEndpointConfig   User provided s3 endpoint and region for overriding the default S3 endpoint
@@ -108,14 +111,14 @@ public class S3InputSource extends CloudObjectInputSource
       @JsonProperty("uris") @Nullable List<URI> uris,
       @JsonProperty("prefixes") @Nullable List<URI> prefixes,
       @JsonProperty("objects") @Nullable List<CloudObjectLocation> objects,
-      @JsonProperty("filter") @Nullable String filter,
+      @JsonProperty("objectGlob") @Nullable String objectGlob,
       @JsonProperty("properties") @Nullable S3InputSourceConfig s3InputSourceConfig,
       @JsonProperty("proxyConfig") @Nullable AWSProxyConfig awsProxyConfig,
       @JsonProperty("endpointConfig") @Nullable AWSEndpointConfig awsEndpointConfig,
       @JsonProperty("clientConfig") @Nullable AWSClientConfig awsClientConfig
   )
   {
-    super(S3StorageDruidModule.SCHEME, uris, prefixes, objects, filter);
+    super(S3StorageDruidModule.SCHEME, uris, prefixes, objects, objectGlob);
     this.inputDataConfig = Preconditions.checkNotNull(inputDataConfig, "S3DataSegmentPusherConfig");
     Preconditions.checkNotNull(s3Client, "s3Client");
     this.s3InputSourceConfig = s3InputSourceConfig;
@@ -184,7 +187,7 @@ public class S3InputSource extends CloudObjectInputSource
       List<URI> uris,
       List<URI> prefixes,
       List<CloudObjectLocation> objects,
-      String filter,
+      String objectGlob,
       S3InputSourceConfig s3InputSourceConfig,
       AWSProxyConfig awsProxyConfig,
       AWSEndpointConfig awsEndpointConfig,
@@ -199,7 +202,7 @@ public class S3InputSource extends CloudObjectInputSource
         uris,
         prefixes,
         objects,
-        filter,
+        objectGlob,
         s3InputSourceConfig,
         awsProxyConfig,
         awsEndpointConfig,
@@ -215,7 +218,7 @@ public class S3InputSource extends CloudObjectInputSource
       List<URI> uris,
       List<URI> prefixes,
       List<CloudObjectLocation> objects,
-      String filter,
+      String objectGlob,
       S3InputSourceConfig s3InputSourceConfig,
       AWSProxyConfig awsProxyConfig,
       AWSEndpointConfig awsEndpointConfig,
@@ -231,7 +234,7 @@ public class S3InputSource extends CloudObjectInputSource
         uris,
         prefixes,
         objects,
-        filter,
+        objectGlob,
         s3InputSourceConfig,
         awsProxyConfig,
         awsEndpointConfig,
@@ -340,7 +343,7 @@ public class S3InputSource extends CloudObjectInputSource
         null,
         null,
         split.get(),
-        getFilter(),
+        getObjectGlob(),
         getS3InputSourceConfig(),
         getAwsProxyConfig(),
         getAwsEndpointConfig(),
@@ -380,7 +383,7 @@ public class S3InputSource extends CloudObjectInputSource
            "uris=" + getUris() +
            ", prefixes=" + getPrefixes() +
            ", objects=" + getObjects() +
-           ", filter=" + getFilter() +
+           ", objectGlob=" + getObjectGlob() +
            ", s3InputSourceConfig=" + getS3InputSourceConfig() +
            ", awsProxyConfig=" + getAwsProxyConfig() +
            ", awsEndpointConfig=" + getAwsEndpointConfig() +
@@ -399,10 +402,12 @@ public class S3InputSource extends CloudObjectInputSource
       );
 
       // Skip files that didn't match filter.
-      if (org.apache.commons.lang.StringUtils.isNotBlank(getFilter())) {
+      if (org.apache.commons.lang.StringUtils.isNotBlank(getObjectGlob())) {
+        PathMatcher m = FileSystems.getDefault().getPathMatcher("glob:" + getObjectGlob());
+
         iterator = Iterators.filter(
             iterator,
-            object -> FilenameUtils.wildcardMatch(object.getKey(), getFilter())
+            object -> m.matches(Paths.get(object.getKey()))
         );
       }
 
