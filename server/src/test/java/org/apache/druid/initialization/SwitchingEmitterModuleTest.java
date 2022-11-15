9903b04e0fe50562015d79a7adfb1b402a271c9d
Tom Fenwick <tom.fenwick@imply.io>
2022-11-15 11:55:50 -0800

    add apache license to top of test

--- server/src/test/java/org/apache/druid/initialization/SwitchingEmitterModuleTest.java
+++ server/src/test/java/org/apache/druid/initialization/SwitchingEmitterModuleTest.java
@@ -1,3 +1,23 @@
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.druid.initialization;
 
 import com.google.common.collect.ImmutableList;
@@ -10,7 +30,6 @@ import com.google.inject.Module;
 import com.google.inject.name.Names;
 import org.apache.druid.guice.DruidGuiceExtensions;
 import org.apache.druid.guice.LifecycleModule;
-import org.apache.druid.guice.ServerModule;
 import org.apache.druid.jackson.JacksonModule;
 import org.apache.druid.java.util.emitter.core.Emitter;
 import org.apache.druid.server.emitter.SwitchingEmitterConfig;
