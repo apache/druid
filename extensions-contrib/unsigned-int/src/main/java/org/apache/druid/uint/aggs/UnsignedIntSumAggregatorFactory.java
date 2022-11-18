22661f41fe9b48e6289f33f99819f8f0233cf623
Rahul Gidwani <churro@apache.org>
2022-11-18 10:05:47 -0800

    Checkstyle issues

--- extensions-contrib/unsigned-int/src/main/java/org/apache/druid/uint/aggs/UnsignedIntSumAggregatorFactory.java
+++ extensions-contrib/unsigned-int/src/main/java/org/apache/druid/uint/aggs/UnsignedIntSumAggregatorFactory.java
@@ -23,11 +23,8 @@ import com.fasterxml.jackson.annotation.JacksonInject;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.apache.druid.math.expr.ExprMacroTable;
-import org.apache.druid.query.aggregation.AggregatorFactory;
 import org.apache.druid.query.aggregation.LongSumAggregatorFactory;
-import org.apache.druid.query.aggregation.any.DoubleAnyAggregatorFactory;
 import org.apache.druid.segment.column.ColumnType;
-import org.apache.druid.segment.column.ValueType;
 import org.apache.druid.uint.UnsignedIntComplexSerde;
 
 import javax.annotation.Nullable;
