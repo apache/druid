da7b93a87a7431f50230cdcc9f57df71b5c4e79a
tejaswini.bandlamudi <tejaswini.bandlamudi@imply.io>
2022-11-30 17:04:55 +0530

    test license checks failure

--- .github/scripts/license_checks_script.sh
+++ .github/scripts/license_checks_script.sh
@@ -18,7 +18,6 @@ ${MVN} apache-rat:check -Prat --fail-at-end \
 -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
 -Drat.consoleOutput=true ${HADOOP_PROFILE}
 # Generate dependency reports and checks they are valid.
-exit 1
 mkdir -p target
 distribution/bin/generate-license-dependency-reports.py . target --clean-maven-artifact-transfer --parallel 2
 distribution/bin/check-licenses.py licenses.yaml target/license-reports
