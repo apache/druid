30498c1f98346050a9121bae113ace4fd4059302
Tejaswini Bandlamudi <96047043+tejaswini-imply@users.noreply.github.com>
2022-12-02 15:06:31 +0530

    Update gha & travis checks (#13412)
    * update static-checks GHA to run sequentially
    remove static-checks from travis.yml
    move docs, web-console, packaging checks from travis to GHA
    
    * nit
    
    * nit
    
    * groups all checks, runs on 8, 11, 17 jdks
    
    * nit
    
    * adds license info
    
    * update permissions on scripts folder
    
    * nit
    
    * nit
    
    * fix packaging check
    
    * changes naming, cleans repo before license checks
    
    * simulate failure
    
    * bump up license checks
    
    * test license checks failure
    
    * test license checks failure
    
    * test license checks failure
    
    * verify gha script run exit code
    
    * fail fast in case of shell script
    
    * verified fail fast in case of shell script

--- .github/scripts/license_checks_script.sh
+++ .github/scripts/license_checks_script.sh
@@ -17,7 +17,11 @@
 
 set -e
 
-sudo apt-get update && sudo apt-get install python3 -y
-curl https://bootstrap.pypa.io/pip/3.5/get-pip.py | sudo -H python3
-pip3 install wheel  # install wheel first explicitly
-pip3 install --upgrade pyyaml
+./.github/scripts/setup_generate_license.sh
+${MVN} apache-rat:check -Prat --fail-at-end \
+-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
+-Drat.consoleOutput=true ${HADOOP_PROFILE}
+# Generate dependency reports and checks they are valid.
+mkdir -p target
+distribution/bin/generate-license-dependency-reports.py . target --clean-maven-artifact-transfer --parallel 2
+distribution/bin/check-licenses.py licenses.yaml target/license-reports
