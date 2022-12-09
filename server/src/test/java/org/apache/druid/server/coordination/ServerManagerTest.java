82d90c01f5f52254e864273c5d7db65f2f2392c3
Soumyava Das <soumyava.das@imply.io>
2022-12-08 18:28:21 -0800

    Adding test to increase branch coverage

--- server/src/test/java/org/apache/druid/server/coordination/ServerManagerTest.java
+++ server/src/test/java/org/apache/druid/server/coordination/ServerManagerTest.java
@@ -32,6 +32,7 @@ import org.apache.druid.collections.bitmap.BitmapFactory;
 import org.apache.druid.common.config.NullHandling;
 import org.apache.druid.jackson.DefaultObjectMapper;
 import org.apache.druid.java.util.common.IAE;
+import org.apache.druid.java.util.common.ISE;
 import org.apache.druid.java.util.common.Intervals;
 import org.apache.druid.java.util.common.MapUtils;
 import org.apache.druid.java.util.common.Pair;
@@ -51,6 +52,7 @@ import org.apache.druid.query.Druids;
 import org.apache.druid.query.ForwardingQueryProcessingPool;
 import org.apache.druid.query.NoopQueryRunner;
 import org.apache.druid.query.Query;
+import org.apache.druid.query.QueryDataSource;
 import org.apache.druid.query.QueryMetrics;
 import org.apache.druid.query.QueryPlus;
 import org.apache.druid.query.QueryProcessingPool;
@@ -469,6 +471,25 @@ public class ServerManagerTest
     Assert.assertSame(NoopQueryRunner.class, queryRunner.getClass());
   }
 
+  @Test(expected = ISE.class)
+  public void testGetQueryRunnerForSegmentsWhenTimelineIsMissingReportingMissingSegmentsOnQueryDataSource()
+  {
+    final Interval interval = Intervals.of("0000-01-01/P1D");
+    final SearchQuery query = searchQueryWithQueryDataSource("unknown_datasource", interval, Granularities.ALL);
+    final List<SegmentDescriptor> unknownSegments = Collections.singletonList(
+        new SegmentDescriptor(interval, "unknown_version", 0)
+    );
+    final QueryRunner<Result<SearchResultValue>> queryRunner = serverManager.getQueryRunnerForSegments(
+        query,
+        unknownSegments
+    );
+    final ResponseContext responseContext = DefaultResponseContext.createEmpty();
+    final List<Result<SearchResultValue>> results = queryRunner.run(QueryPlus.wrap(query), responseContext).toList();
+    Assert.assertTrue(results.isEmpty());
+    Assert.assertNotNull(responseContext.getMissingSegments());
+    Assert.assertEquals(unknownSegments, responseContext.getMissingSegments());
+  }
+
   @Test
   public void testGetQueryRunnerForSegmentsWhenTimelineIsMissingReportingMissingSegments()
   {
@@ -631,6 +652,30 @@ public class ServerManagerTest
   {
     return Druids.newSearchQueryBuilder()
                  .dataSource(datasource)
+                 .intervals(Collections.singletonList(interval))
+                 .granularity(granularity)
+                 .limit(10000)
+                 .query("wow")
+                 .build();
+  }
+
+
+  private SearchQuery searchQueryWithQueryDataSource(String datasource, Interval interval, Granularity granularity)
+  {
+    final ImmutableList<SegmentDescriptor> descriptors = ImmutableList.of(
+        new SegmentDescriptor(Intervals.of("2000/3000"), "0", 0),
+        new SegmentDescriptor(Intervals.of("2000/3000"), "0", 1)
+    );
+    return Druids.newSearchQueryBuilder()
+                 .dataSource(
+                     new QueryDataSource(
+                         Druids.newTimeseriesQueryBuilder()
+                               .dataSource(datasource)
+                               .intervals(new MultipleSpecificSegmentSpec(descriptors))
+                               .granularity(Granularities.ALL)
+                               .build()
+                     )
+                 )
                  .intervals(Collections.singletonList(interval))
                  .granularity(granularity)
                  .limit(10000)
