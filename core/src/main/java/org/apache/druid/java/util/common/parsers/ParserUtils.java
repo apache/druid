988499a0bb9b34cef8bd75067773784eff1c1687
Maytas Monsereenusorn <maytasm@apache.org>
2023-01-02 01:50:01 -0800

    fix checkstyle

--- core/src/main/java/org/apache/druid/java/util/common/parsers/ParserUtils.java
+++ core/src/main/java/org/apache/druid/java/util/common/parsers/ParserUtils.java
@@ -29,7 +29,6 @@ import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TimeZone;
