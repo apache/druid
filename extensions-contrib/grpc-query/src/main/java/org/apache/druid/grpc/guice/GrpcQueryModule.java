b0a4ced61ad0e1aec196455e6edb8f7f6b041aee
Paul Rogers <progers@apache.org>
2023-01-14 14:36:09 -0800

    Snapshot

---
+++ extensions-contrib/grpc-query/src/main/java/org/apache/druid/grpc/guice/GrpcQueryModule.java
@@ -0,0 +1,16 @@
+package org.apache.druid.grpc.guice;
+
+import com.google.inject.Binder;
+import org.apache.druid.initialization.DruidModule;
+
+public class GrpcQueryModule implements DruidModule
+{
+
+  @Override
+  public void configure(Binder binder)
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ extensions-contrib/grpc-query/src/main/java/org/apache/druid/grpc/server/QueryServer.java
@@ -0,0 +1,76 @@
+package org.apache.druid.grpc.server;
+
+import io.grpc.Grpc;
+import io.grpc.InsecureServerCredentials;
+import io.grpc.Server;
+import io.grpc.stub.StreamObserver;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+import org.druid.grpc.proto.QueryGrpc;
+import org.druid.grpc.proto.QueryOuterClass.QueryRequest;
+import org.druid.grpc.proto.QueryOuterClass.QueryResponse;
+
+
+public class QueryServer
+{
+  private static final Logger logger = Logger.getLogger(QueryServer.class.getName());
+
+  private Server server;
+
+  private void start() throws IOException {
+    /* The port on which the server should run */
+    int port = 50051;
+    server = Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
+        .addService(new QueryImpl())
+        .build()
+        .start();
+    logger.info("Server started, listening on " + port);
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+        System.err.println("*** shutting down gRPC server since JVM is shutting down");
+        try {
+          QueryServer.this.stop();
+        } catch (InterruptedException e) {
+          e.printStackTrace(System.err);
+        }
+        System.err.println("*** server shut down");
+      }
+    });
+  }
+
+  private void stop() throws InterruptedException {
+    if (server != null) {
+      server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+    }
+  }
+
+  /**
+   * Await termination on the main thread since the grpc library uses daemon threads.
+   */
+  private void blockUntilShutdown() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
+    }
+  }
+
+  /**
+   * Main launches the server from the command line.
+   */
+  public static void main(String[] args) throws IOException, InterruptedException {
+    final QueryServer server = new QueryServer();
+    server.start();
+    server.blockUntilShutdown();
+  }
+
+  static class QueryImpl extends QueryGrpc.QueryImplBase {
+
+    @Override
+    public void submitQuery(QueryRequest request, StreamObserver<QueryResponse> responseObserver)
+    {
+    }
+  }
+
+}
