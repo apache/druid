01047cae682ec39906617ff2589f3a01fb8568de
imply-cheddar <eric.tschetter@imply.io>
2023-01-19 10:06:59 +0900

    Adjust Operators to be Pausable
    This enables "merge" style operations that
    combine multiple streams.
    
    This change includes a naive implementation
    of one such merge operator just to provide
    concrete evidence that the refactoring is
    effective.

--- processing/src/main/java/org/apache/druid/query/rowsandcols/column/DoubleArrayColumn.java
+++ processing/src/main/java/org/apache/druid/query/rowsandcols/column/DoubleArrayColumn.java
@@ -20,10 +20,13 @@
 package org.apache.druid.query.rowsandcols.column;
 
 import org.apache.druid.java.util.common.ISE;
+import org.apache.druid.java.util.common.Numbers;
+import org.apache.druid.query.rowsandcols.util.FindResult;
 import org.apache.druid.segment.column.ColumnType;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Arrays;
 
 public class DoubleArrayColumn implements Column
 {
@@ -40,62 +43,7 @@ public class DoubleArrayColumn implements Column
   @Override
   public ColumnAccessor toAccessor()
   {
-    return new ColumnAccessor()
-    {
-      @Override
-      public ColumnType getType()
-      {
-        return ColumnType.DOUBLE;
-      }
-
-      @Override
-      public int numRows()
-      {
-        return vals.length;
-      }
-
-      @Override
-      public boolean isNull(int rowNum)
-      {
-        return false;
-      }
-
-      @Override
-      public Object getObject(int rowNum)
-      {
-        return vals[rowNum];
-      }
-
-      @Override
-      public double getDouble(int rowNum)
-      {
-        return vals[rowNum];
-      }
-
-      @Override
-      public float getFloat(int rowNum)
-      {
-        return (float) vals[rowNum];
-      }
-
-      @Override
-      public long getLong(int rowNum)
-      {
-        return (long) vals[rowNum];
-      }
-
-      @Override
-      public int getInt(int rowNum)
-      {
-        return (int) vals[rowNum];
-      }
-
-      @Override
-      public int compareRows(int lhsRowNum, int rhsRowNum)
-      {
-        return Double.compare(lhsRowNum, rhsRowNum);
-      }
-    };
+    return new MyColumnAccessor();
   }
 
   @Nullable
@@ -125,5 +73,123 @@ public class DoubleArrayColumn implements Column
       };
     }
     return null;
+  }
+
+  private class MyColumnAccessor implements BinarySearchableAccessor
+  {
+    @Override
+    public ColumnType getType()
+    {
+      return ColumnType.DOUBLE;
+    }
+
+    @Override
+    public int numRows()
+    {
+      return vals.length;
+    }
+
+    @Override
+    public boolean isNull(int rowNum)
+    {
+      return false;
+    }
+
+    @Override
+    public Object getObject(int rowNum)
+    {
+      return vals[rowNum];
+    }
+
+    @Override
+    public double getDouble(int rowNum)
+    {
+      return vals[rowNum];
+    }
+
+    @Override
+    public float getFloat(int rowNum)
+    {
+      return (float) vals[rowNum];
+    }
+
+    @Override
+    public long getLong(int rowNum)
+    {
+      return (long) vals[rowNum];
+    }
+
+    @Override
+    public int getInt(int rowNum)
+    {
+      return (int) vals[rowNum];
+    }
+
+    @Override
+    public int compareRows(int lhsRowNum, int rhsRowNum)
+    {
+      return Double.compare(lhsRowNum, rhsRowNum);
+    }
+
+    @Override
+    public FindResult findNull(int startIndex, int endIndex)
+    {
+      return FindResult.notFound(endIndex);
+    }
+
+    @Override
+    public FindResult findDouble(int startIndex, int endIndex, double val)
+    {
+      if (vals[startIndex] == val) {
+        int end = startIndex + 1;
+
+        while (end < endIndex && vals[end] == val) {
+          ++end;
+        }
+        return FindResult.found(startIndex, end);
+      }
+
+      int i = Arrays.binarySearch(vals, startIndex, endIndex, val);
+      if (i > 0) {
+        int foundStart = i;
+        int foundEnd = i + 1;
+
+        while (foundStart - 1 >= startIndex && vals[foundStart - 1] == val) {
+          --foundStart;
+        }
+
+        while (foundEnd < endIndex && vals[foundEnd] == val) {
+          ++foundEnd;
+        }
+
+        return FindResult.found(foundStart, foundEnd);
+      } else {
+        return FindResult.notFound(-(i + 1));
+      }
+    }
+
+    @Override
+    public FindResult findFloat(int startIndex, int endIndex, float val)
+    {
+      return findDouble(startIndex, endIndex, val);
+    }
+
+    @Override
+    public FindResult findLong(int startIndex, int endIndex, long val)
+    {
+      return findDouble(startIndex, endIndex, val);
+    }
+
+    @Override
+    public FindResult findString(int startIndex, int endIndex, String val)
+    {
+      return findDouble(startIndex, endIndex, Numbers.tryParseDouble(val, 0));
+    }
+
+    @Override
+    public FindResult findComplex(int startIndex, int endIndex, Object val)
+    {
+      return findDouble(startIndex, endIndex, Numbers.tryParseDouble(val, 0));
+    }
   }
 }
