606396da139ee5c74407a5af1e99f7d44a0ed4a7
Soumyava Das <soumyava.das@imply.io>
2023-03-15 14:23:29 -0700

    A change in the comments to be in sync with code

--- processing/src/main/java/org/apache/druid/segment/UnnestStorageAdapter.java
+++ processing/src/main/java/org/apache/druid/segment/UnnestStorageAdapter.java
@@ -274,7 +274,7 @@ public class UnnestStorageAdapter implements StorageAdapter
     /*
     The goal of this function is to take a filter from the top of Correlate (queryFilter)
     and a filter from the top of Uncollect (here unnest filter) and then do a rewrite
-    to generate filters to be passed to base cursor (pre-filters) and unnest cursor (post-filters)
+    to generate filters to be passed to base cursor (filtersOnLeftDataSource) and unnest cursor (filtersOnUncollect)
     based on the following scenarios:
 
     1. If there is an AND filter between unnested column and left e.g. select * from foo, UNNEST(dim3) as u(d3) where d3 IN (a,b) and m1 < 10
@@ -282,32 +282,32 @@ public class UnnestStorageAdapter implements StorageAdapter
        unnest filter -> d3 IN (a,b)
 
        Output should be:
-       pre-filter -> dim3 IN (a,b) AND m1 < 10
-       post-filter -> d3 IN (a,b)
+       filtersOnLeftDataSource -> dim3 IN (a,b) AND m1 < 10
+       filtersOnUncollect -> d3 IN (a,b)
 
     2. There is an AND filter between unnested column and left e.g. select * from foo, UNNEST(ARRAY[dim1,dim2]) as u(d12) where d12 IN (a,b) and m1 < 10
        query filter -> m1 < 10
        unnest filter -> d12 IN (a,b)
 
        Output should be:
-       pre-filter -> m1 < 10 (as unnest is on a virtual column it cannot be added to the pre-filter)
-       post-filter -> d12 IN (a,b)
+       filtersOnLeftDataSource -> m1 < 10 (as unnest is on a virtual column it cannot be added to the pre-filter)
+       filtersOnUncollect -> d12 IN (a,b)
 
     3. There is an OR filter involving unnested and left column e.g.  select * from foo, UNNEST(dim3) as u(d3) where d3 IN (a,b) or m1 < 10
        query filter -> d3 IN (a,b) or m1 < 10
        unnest filter -> null
 
        Output should be:
-       query filter -> dim3 IN (a,b) or m1 < 10
-       post filter -> d3 IN (a,b) or m1 < 10
+       filtersOnLeftDataSource -> dim3 IN (a,b) or m1 < 10
+       filtersOnUncollect -> d3 IN (a,b) or m1 < 10
 
      4. There is an OR filter involving unnested and left column e.g. select * from foo, UNNEST(ARRAY[dim1,dim2]) as u(d12) where d12 IN (a,b) or m1 < 10
        query filter -> d12 IN (a,b) or m1 < 10
        unnest filter -> null
 
        Output should be:
-       query filter -> null (as the filter cannot be re-written due to presence of virtual columns)
-       post filter -> d12 IN (a,b) or m1 < 10
+       filtersOnLeftDataSource -> null (as the filter cannot be re-written due to presence of virtual columns)
+       filtersOnUncollect -> d12 IN (a,b) or m1 < 10
      */
     class FilterSplitter
     {
