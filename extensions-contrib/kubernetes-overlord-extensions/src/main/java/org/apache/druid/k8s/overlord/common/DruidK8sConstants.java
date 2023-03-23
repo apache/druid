36df2495e135a12efea3d3b4765e960753d497fa
Nicholas Lippis <nick.lippis@imply.io>
2023-03-23 18:45:01 -0400

    Set TASK_ID as environment variable in PodTemplateTaskAdapter (#13969)

--- extensions-contrib/kubernetes-overlord-extensions/src/main/java/org/apache/druid/k8s/overlord/common/DruidK8sConstants.java
+++ extensions-contrib/kubernetes-overlord-extensions/src/main/java/org/apache/druid/k8s/overlord/common/DruidK8sConstants.java
@@ -33,6 +33,7 @@ public class DruidK8sConstants
   public static final String TLS_ENABLED = "tls.enabled";
   public static final String TASK_JSON_ENV = "TASK_JSON";
   public static final String TASK_DIR_ENV = "TASK_DIR";
+  public static final String TASK_ID_ENV = "TASK_ID";
   public static final String JAVA_OPTS = "JAVA_OPTS";
   public static final String DRUID_HOST_ENV = "druid_host";
   public static final String DRUID_HOSTNAME_ENV = "HOSTNAME";
