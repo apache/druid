abb7133153be7f7f638e2f83e3f2d9339737efdd
Vadim Ogievetsky <vadim@ogievetsky.com>
2023-03-29 16:19:49 -0700

    Web console: use EXTEND syntax (#13985)
    * use EXTEND syntax
    
    * update licenses
    
    * update demo queries
    
    * updated snapshots
    
    * add join algorithm selector
    
    * dismiss

--- web-console/script/create-sql-docs.js
+++ web-console/script/create-sql-docs.js
@@ -23,8 +23,15 @@ const snarkdown = require('snarkdown');
 
 const writefile = 'lib/sql-docs.js';
 
-const MINIMUM_EXPECTED_NUMBER_OF_FUNCTIONS = 162;
+const MINIMUM_EXPECTED_NUMBER_OF_FUNCTIONS = 164;
 const MINIMUM_EXPECTED_NUMBER_OF_DATA_TYPES = 14;
+
+const initialFunctionDocs = {
+  TABLE: [['external', convertMarkdownToHtml('Defines a logical table from an external.')]],
+  EXTERN: [
+    ['inputSource, inputFormat, rowSignature?', convertMarkdownToHtml('Reads external data')],
+  ],
+};
 
 function hasHtmlTags(str) {
   return /<(a|br|span|div|p|code)\/?>/.test(str);
@@ -67,7 +74,7 @@ const readDoc = async () => {
 
   const lines = data.split('\n');
 
-  const functionDocs = {};
+  const functionDocs = initialFunctionDocs;
   const dataTypeDocs = {};
   for (let line of lines) {
     const functionMatch = line.match(/^\|\s*`(\w+)\(([^|]*)\)`\s*\|([^|]+)\|(?:([^|]+)\|)?$/);
