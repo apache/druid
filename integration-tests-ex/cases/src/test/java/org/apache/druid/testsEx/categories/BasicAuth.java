884ef2f8f7117b73455af40eb82526c1fb2e884d
Paul Rogers <progers@apache.org>
2023-03-30 16:10:20 -0700

    Snapshot

--- integration-tests-ex/cases/src/test/java/org/apache/druid/testsEx/categories/BasicAuth.java
+++
@@ -1,6 +0,0 @@
-package org.apache.druid.testsEx.categories;
-
-public class BasicAuth
-{
-
-}
--- integration-tests-ex/cases/src/test/java/org/apache/druid/testsEx/security/BasicAuthHelper.java
+++ integration-tests-ex/cases/src/test/java/org/apache/druid/testsEx/security/BasicAuthHelper.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.druid.testsEx.security;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -14,12 +33,15 @@ import org.jboss.netty.handler.codec.http.HttpMethod;
 
 import java.util.List;
 
+/**
+ * Coordinator client which performs various basic auth operations.
+ */
 public class BasicAuthHelper
 {
   private final HttpClient adminClient;
   private final String coordinatorUrl;
   private final ObjectMapper jsonMapper;
-  
+
   public BasicAuthHelper(HttpClient adminClient, String coordinatorUrl, ObjectMapper jsonMapper)
   {
     this.adminClient = adminClient;
@@ -40,7 +62,7 @@ public class BasicAuthHelper
         null
     );
   }
-  
+
   public void createAuthorizationUser(String user)
   {
     HttpUtil.makeRequest(
@@ -54,7 +76,7 @@ public class BasicAuthHelper
         null
     );
   }
-  
+
   public void setPassword(String user, String password)
   {
     try {
@@ -72,7 +94,7 @@ public class BasicAuthHelper
       throw new RE(e, "Failed to format credentials");
     }
   }
-  
+
   public void createRole(String role)
   {
     HttpUtil.makeRequest(
@@ -86,7 +108,7 @@ public class BasicAuthHelper
         null
     );
   }
-  
+
   public void assignRoleToUser(String role, String user)
   {
     HttpUtil.makeRequest(
@@ -101,7 +123,7 @@ public class BasicAuthHelper
         null
     );
   }
-  
+
   public void updateRolePermissions(String role, List<ResourceAction> permissions)
   {
     byte[] permissionsBytes;
@@ -121,7 +143,7 @@ public class BasicAuthHelper
         permissionsBytes
     );
   }
-  
+
   public void createUserAndRoleWithPermissions(
       String user,
       String password,
