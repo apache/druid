3d3607816a8b9c5fe3c61f8ffee72f9bfb207e5a
Soumyava Das <soumyava.das@imply.io>
2023-03-30 08:41:52 -0700

    Fixing the issue on cast on project with same datatype on both left and right

--- sql/src/main/java/org/apache/druid/sql/calcite/rule/DruidFilterUnnestRule.java
+++ sql/src/main/java/org/apache/druid/sql/calcite/rule/DruidFilterUnnestRule.java
@@ -97,11 +97,11 @@ public class DruidFilterUnnestRule extends RelOptRule
       final Project rightP = call.rel(0);
       if (rightP.getChildExps().size() > 0) {
         final SqlKind rightProjectKind = rightP.getChildExps().get(0).getKind();
-        final SqlTypeName rightType = rightP.getChildExps().get(0).getType().getSqlTypeName();
-        // allow rule to trigger only if there's a non-decimal CAST or numeric literal cast
-        return rightP.getProjects().size() == 1 && ((rightProjectKind == SqlKind.CAST
-                                                     && rightType != SqlTypeName.DECIMAL)
-                                                    || rightProjectKind == SqlKind.LITERAL);
+        final SqlTypeName projectType = rightP.getChildExps().get(0).getType().getSqlTypeName();
+        final SqlTypeName unnestDataType = call.rel(1).getRowType().getFieldList().get(0).getType().getSqlTypeName();
+        // allow rule to trigger only if project involves a cast on the same row type
+        return rightP.getProjects().size() == 1 && ((rightProjectKind == SqlKind.CAST || rightProjectKind == SqlKind.LITERAL)
+                                                     && projectType == unnestDataType);
       }
       return false;
     }
