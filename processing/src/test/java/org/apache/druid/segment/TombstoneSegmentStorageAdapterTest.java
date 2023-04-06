1c242687ecdaadfc9d1c1066e9da43c1bb002b3a
Abhishek Agarwal <1477457+abhishekagarwal87@users.noreply.github.com>
2023-04-06 12:04:35 +0530

    rename test

--- processing/src/test/java/org/apache/druid/segment/TombstoneSegmentStorageAdapterTest.java
+++ processing/src/test/java/org/apache/druid/segment/TombstoneSegmentStorageAdapterTest.java
@@ -33,7 +33,7 @@ import org.junit.Test;
 import javax.annotation.Nullable;
 
 
-public class SimpleStorageAdapterTest
+public class TombstoneSegmentStorageAdapterTest
 {
   @Test
   public void testTombstoneDefaultInterface()
