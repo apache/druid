0df2f1dcedbe04e0329c18cd70ae52b54cbbc6a2
Gian Merlino <gianmerlino@gmail.com>
2023-04-12 09:14:20 -0700

    Style fix.

--- sql/src/test/java/org/apache/druid/sql/calcite/util/CalciteTests.java
+++ sql/src/test/java/org/apache/druid/sql/calcite/util/CalciteTests.java
@@ -42,7 +42,6 @@ import org.apache.druid.java.util.http.client.HttpClient;
 import org.apache.druid.java.util.http.client.Request;
 import org.apache.druid.java.util.http.client.response.HttpResponseHandler;
 import org.apache.druid.math.expr.ExprMacroTable;
-import org.apache.druid.query.GlobalTableDataSource;
 import org.apache.druid.query.QueryRunnerFactoryConglomerate;
 import org.apache.druid.query.QuerySegmentWalker;
 import org.apache.druid.segment.join.JoinableFactory;
@@ -77,7 +76,6 @@ import org.apache.druid.timeline.DataSegment;
 import org.joda.time.Duration;
 
 import javax.annotation.Nullable;
-
 import java.io.File;
 import java.util.Collection;
 import java.util.HashMap;
