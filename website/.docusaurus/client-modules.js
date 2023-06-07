8769096366cc89d573098477d9b5615613e4c0d5
brian.le <brian.le@imply.io>
2023-06-06 17:01:32 -0700

    delete .docusaurus folder

--- website/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/Users/brian/my-druid-fork/website/node_modules/@docusaurus/plugin-google-gtag/lib/gtag'),
-  require('/Users/brian/my-druid-fork/website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/brian/my-druid-fork/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/brian/my-druid-fork/website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/brian/my-druid-fork/src/css/customTheme.css'),
-];
