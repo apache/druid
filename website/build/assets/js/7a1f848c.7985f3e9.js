56bd0f70424e8c1e01800f0cd5ed13b0f47c587c
brian.le <brian.le@imply.io>
2023-06-06 17:03:12 -0700

    delete build folder and add to gitignore

--- website/build/assets/js/7a1f848c.7985f3e9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[3114],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- website/build/assets/js/8894.143a3960.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[8894],{8894:(e,s,k)=>{k.r(s)}}]);
