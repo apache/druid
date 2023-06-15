47f38d1ed8b9fb747d85f9adadcb6f39d62f62f1
George Wu <george.wu@imply.io>
2023-06-15 15:12:13 -0400

    remove lock

--- extensions-contrib/kubernetes-overlord-extensions/src/main/java/org/apache/druid/k8s/overlord/KubernetesTaskRunner.java
+++ extensions-contrib/kubernetes-overlord-extensions/src/main/java/org/apache/druid/k8s/overlord/KubernetesTaskRunner.java
@@ -161,19 +161,17 @@ public class KubernetesTaskRunner implements TaskLogStreamer, TaskRunner
   {
     KubernetesPeonLifecycle peonLifecycle = peonLifecycleFactory.build(task);
 
-    synchronized (tasks) {
-      KubernetesWorkItem workItem = tasks.get(task.getId());
+    KubernetesWorkItem workItem = tasks.get(task.getId());
 
-      if (workItem == null) {
-        throw new ISE("Task [%s] disappeared", task.getId());
-      }
-
-      if (workItem.isShutdownRequested()) {
-        throw new ISE("Task [%s] has been shut down", task.getId());
-      }
-
-      workItem.setKubernetesPeonLifecycle(peonLifecycle);
+    if (workItem == null) {
+      throw new ISE("Task [%s] disappeared", task.getId());
     }
+
+    if (workItem.isShutdownRequested()) {
+      throw new ISE("Task [%s] has been shut down", task.getId());
+    }
+
+    workItem.setKubernetesPeonLifecycle(peonLifecycle);
 
     try {
       TaskStatus taskStatus;
