1d785ca9e273dce41896ab6122e0f92e54c4a6e1
Kashif Faraz <kashif.faraz@gmail.com>
2023-06-19 10:13:12 +0530

    Fix dependency

--- server/src/test/java/org/apache/druid/server/http/MetadataResourceTest.java
+++ server/src/test/java/org/apache/druid/server/http/MetadataResourceTest.java
@@ -23,7 +23,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import net.thisptr.jackson.jq.internal.misc.Lists;
+import com.google.common.collect.Lists;
 import org.apache.druid.client.DataSourcesSnapshot;
 import org.apache.druid.client.ImmutableDruidDataSource;
 import org.apache.druid.indexing.overlord.IndexerMetadataStorageCoordinator;
