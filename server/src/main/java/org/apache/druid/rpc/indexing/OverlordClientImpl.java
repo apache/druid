apache/druid

commit 2f9619a96fbde224a7de3233d278087fd4782a0f

Use OverlordClient for all Overlord RPCs. (#14581)
* Use OverlordClient for all Overlord RPCs.

Continuing the work from #12696, this patch removes HttpIndexingServiceClient
and the IndexingService flavor of DruidLeaderClient completely. All remaining
usages are migrated to OverlordClient.

Supporting changes include:

1) Add a variety of methods to OverlordClient.

2) Update MetadataTaskStorage to skip the complete-task lookup when
   the caller requests zero completed tasks. This helps performance of
   the "get active tasks" APIs, which don't want to see complete ones.

* Use less forbidden APIs.

* Fixes from CI.

* Add test coverage.

* Two more tests.

* Fix test.

* Updates from CR.

* Remove unthrown exceptions.

* Refactor to improve testability and test coverage.

* Add isNil tests.

* Remove unnecessary "deserialize" methods.

server/src/main/java/org/apache/druid/rpc/indexing/OverlordClientImpl.java
@@ -22,22 +22,37 @@ package org.apache.druid.rpc.indexing;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import org.apache.druid.client.JsonParserIterator;
+import org.apache.druid.client.indexing.IndexingTotalWorkerCapacityInfo;
+import org.apache.druid.client.indexing.IndexingWorkerInfo;
 import org.apache.druid.client.indexing.TaskPayloadResponse;
 import org.apache.druid.client.indexing.TaskStatusResponse;
 import org.apache.druid.common.guava.FutureUtils;
 import org.apache.druid.indexer.TaskStatus;
+import org.apache.druid.indexer.TaskStatusPlus;
+import org.apache.druid.indexing.overlord.supervisor.SupervisorStatus;
 import org.apache.druid.java.util.common.StringUtils;
 import org.apache.druid.java.util.common.jackson.JacksonUtils;
+import org.apache.druid.java.util.common.parsers.CloseableIterator;
 import org.apache.druid.java.util.http.client.response.BytesFullResponseHandler;
-import org.apache.druid.java.util.http.client.response.BytesFullResponseHolder;
+import org.apache.druid.java.util.http.client.response.InputStreamResponseHandler;
+import org.apache.druid.java.util.http.client.response.StringFullResponseHandler;
 import org.apache.druid.rpc.IgnoreHttpResponseHandler;
 import org.apache.druid.rpc.RequestBuilder;
 import org.apache.druid.rpc.ServiceClient;
 import org.apache.druid.rpc.ServiceRetryPolicy;
 import org.jboss.netty.handler.codec.http.HttpMethod;
+import org.joda.time.Interval;
 
-import java.io.IOException;
+import javax.annotation.Nullable;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -56,6 +71,25 @@ public class OverlordClientImpl implements OverlordClient
   }
 
   @Override
+  public ListenableFuture<URI> findCurrentLeader()
+  {
+    return FutureUtils.transform(
+        client.asyncRequest(
+            new RequestBuilder(HttpMethod.GET, "/druid/indexer/v1/leader"),
+            new StringFullResponseHandler(StandardCharsets.UTF_8)
+        ),
+        holder -> {
+          try {
+            return new URI(holder.getContent());
+          }
+          catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+          }
+        }
+    );
+  }
+
+  @Override
   public ListenableFuture<Void> runTask(final String taskId, final Object taskObject)
   {
     return FutureUtils.transform(
@@ -65,7 +99,8 @@ public class OverlordClientImpl implements OverlordClient
             new BytesFullResponseHandler()
         ),
         holder -> {
-          final Map<String, Object> map = deserialize(holder, JacksonUtils.TYPE_REFERENCE_MAP_STRING_OBJECT);
+          final Map<String, Object> map =
+              JacksonUtils.readValue(jsonMapper, holder.getContent(), JacksonUtils.TYPE_REFERENCE_MAP_STRING_OBJECT);
           final String returnedTaskId = (String) map.get("task");
 
           Preconditions.checkState(
@@ -92,6 +127,39 @@ public class OverlordClientImpl implements OverlordClient
   }
 
   @Override
+  public ListenableFuture<CloseableIterator<TaskStatusPlus>> taskStatuses(
+      @Nullable String state,
+      @Nullable String dataSource,
+      @Nullable Integer maxCompletedTasks
+  )
+  {
+    final StringBuilder pathBuilder = new StringBuilder("/druid/indexer/v1/tasks");
+    int params = 0;
+
+    if (state != null) {
+      pathBuilder.append('?').append("state=").append(StringUtils.urlEncode(state));
+      params++;
+    }
+
+    if (dataSource != null) {
+      pathBuilder.append(params == 0 ? '?' : '&').append("datasource=").append(StringUtils.urlEncode(dataSource));
+      params++;
+    }
+
+    if (maxCompletedTasks != null) {
+      pathBuilder.append(params == 0 ? '?' : '&').append("maxCompletedTasks=").append(maxCompletedTasks);
+    }
+
+    return FutureUtils.transform(
+        client.asyncRequest(
+            new RequestBuilder(HttpMethod.GET, pathBuilder.toString()),
+            new InputStreamResponseHandler()
+        ),
+        in -> asJsonParserIterator(in, TaskStatusPlus.class)
+    );
+  }
+
+  @Override
   public ListenableFuture<Map<String, TaskStatus>> taskStatuses(final Set<String> taskIds)
   {
     return FutureUtils.transform(
@@ -100,7 +168,8 @@ public class OverlordClientImpl implements OverlordClient
                 .jsonContent(jsonMapper, taskIds),
             new BytesFullResponseHandler()
         ),
-        holder -> deserialize(holder, new TypeReference<Map<String, TaskStatus>>() {})
+        holder ->
+            JacksonUtils.readValue(jsonMapper, holder.getContent(), new TypeReference<Map<String, TaskStatus>>() {})
     );
   }
 
@@ -114,7 +183,30 @@ public class OverlordClientImpl implements OverlordClient
             new RequestBuilder(HttpMethod.GET, path),
             new BytesFullResponseHandler()
         ),
-        holder -> deserialize(holder, TaskStatusResponse.class)
+        holder -> JacksonUtils.readValue(jsonMapper, holder.getContent(), TaskStatusResponse.class)
+    );
+  }
+
+  @Override
+  public ListenableFuture<Map<String, List<Interval>>> findLockedIntervals(Map<String, Integer> minTaskPriority)
+  {
+    final String path = "/druid/indexer/v1/lockedIntervals";
+
+    return FutureUtils.transform(
+        client.asyncRequest(
+            new RequestBuilder(HttpMethod.POST, path)
+                .jsonContent(jsonMapper, minTaskPriority),
+            new BytesFullResponseHandler()
+        ),
+        holder -> {
+          final Map<String, List<Interval>> response = JacksonUtils.readValue(
+              jsonMapper,
+              holder.getContent(),
+              new TypeReference<Map<String, List<Interval>>>() {}
+          );
+
+          return response == null ? Collections.emptyMap() : response;
+        }
     );
   }
 
@@ -128,12 +220,82 @@ public class OverlordClientImpl implements OverlordClient
             new RequestBuilder(HttpMethod.GET, path),
             new BytesFullResponseHandler()
         ),
-        holder -> deserialize(holder, JacksonUtils.TYPE_REFERENCE_MAP_STRING_OBJECT)
+        holder -> JacksonUtils.readValue(jsonMapper, holder.getContent(), JacksonUtils.TYPE_REFERENCE_MAP_STRING_OBJECT)
     );
   }
 
   @Override
-  public ListenableFuture<TaskPayloadResponse> taskPayload(String taskId)
+  public ListenableFuture<CloseableIterator<SupervisorStatus>> supervisorStatuses()
+  {
+    final String path = "/druid/indexer/v1/supervisor?system";
+
+    return FutureUtils.transform(
+        client.asyncRequest(
+            new RequestBuilder(HttpMethod.GET, path),
+            new InputStreamResponseHandler()
+        ),
+        in -> asJsonParserIterator(in, SupervisorStatus.class)
+    );
+  }
+
+  @Override
+  public ListenableFuture<List<IndexingWorkerInfo>> getWorkers()
+  {
+    final String path = "/druid/indexer/v1/workers";
+
+    return FutureUtils.transform(
+        client.asyncRequest(
+            new RequestBuilder(HttpMethod.GET, path),
+            new BytesFullResponseHandler()
+        ),
+        holder ->
+            JacksonUtils.readValue(jsonMapper, holder.getContent(), new TypeReference<List<IndexingWorkerInfo>>() {})
+    );
+  }
+
+  @Override
+  public ListenableFuture<IndexingTotalWorkerCapacityInfo> getTotalWorkerCapacity()
+  {
+    final String path = "/druid/indexer/v1/totalWorkerCapacity";
+
+    return FutureUtils.transform(
+        client.asyncRequest(
+            new RequestBuilder(HttpMethod.GET, path),
+            new BytesFullResponseHandler()
+        ),
+        holder -> JacksonUtils.readValue(jsonMapper, holder.getContent(), IndexingTotalWorkerCapacityInfo.class)
+    );
+  }
+
+  @Override
+  public ListenableFuture<Integer> killPendingSegments(String dataSource, Interval interval)
+  {
+    final String path = StringUtils.format(
+        "/druid/indexer/v1/pendingSegments/%s?interval=%s",
+        StringUtils.urlEncode(dataSource),
+        StringUtils.urlEncode(interval.toString())
+    );
+
+    return FutureUtils.transform(
+        client.asyncRequest(
+            new RequestBuilder(HttpMethod.DELETE, path),
+            new BytesFullResponseHandler()
+        ),
+        holder -> {
+          final Map<String, Object> resultMap = JacksonUtils.readValue(
+              jsonMapper,
+              holder.getContent(),
+              JacksonUtils.TYPE_REFERENCE_MAP_STRING_OBJECT
+          );
+
+          final Object numDeletedObject = resultMap.get("numDeleted");
+          return (Integer) Preconditions.checkNotNull(numDeletedObject, "numDeletedObject");
+        }
+    );
+  }
+
+  @Override
+  public ListenableFuture<TaskPayloadResponse> taskPayload(final String taskId)
   {
     final String path = StringUtils.format("/druid/indexer/v1/task/%s", StringUtils.urlEncode(taskId));
 
@@ -142,9 +304,7 @@ public class OverlordClientImpl implements OverlordClient
             new RequestBuilder(HttpMethod.GET, path),
             new BytesFullResponseHandler()
         ),
-        holder -> deserialize(holder, new TypeReference<TaskPayloadResponse>()
-        {
-        })
+        holder -> JacksonUtils.readValue(jsonMapper, holder.getContent(), TaskPayloadResponse.class)
     );
   }
 
@@ -154,23 +314,15 @@ public class OverlordClientImpl implements OverlordClient
     return new OverlordClientImpl(client.withRetryPolicy(retryPolicy), jsonMapper);
   }
 
-  private <T> T deserialize(final BytesFullResponseHolder bytesHolder, final Class<T> clazz)
+  private <T> JsonParserIterator<T> asJsonParserIterator(final InputStream in, final Class<T> clazz)
   {
-    try {
-      return jsonMapper.readValue(bytesHolder.getContent(), clazz);
-    }
-    catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private <T> T deserialize(final BytesFullResponseHolder bytesHolder, final TypeReference<T> typeReference)
-  {
-    try {
-      return jsonMapper.readValue(bytesHolder.getContent(), typeReference);
-    }
-    catch (IOException e) {
-      throw new RuntimeException(e);
-    }
+    return new JsonParserIterator<>(
+        jsonMapper.getTypeFactory().constructType(clazz),
+        Futures.immediateFuture(in),
+        "", // We don't know URL at this point, but it's OK to use empty; it's used for logs/errors
+        null,
+        "", // We don't know host at this point, but it's OK to use empty; it's used for logs/errors
+        jsonMapper
+    );
   }
 }

server/src/test/java/org/apache/druid/rpc/indexing/OverlordClientImplTest.java
@@ -20,39 +20,420 @@
 package org.apache.druid.rpc.indexing;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.util.concurrent.ListenableFuture;
 import org.apache.druid.client.indexing.ClientKillUnusedSegmentsTaskQuery;
 import org.apache.druid.client.indexing.ClientTaskQuery;
+import org.apache.druid.client.indexing.IndexingTotalWorkerCapacityInfo;
+import org.apache.druid.client.indexing.IndexingWorker;
+import org.apache.druid.client.indexing.IndexingWorkerInfo;
 import org.apache.druid.client.indexing.TaskPayloadResponse;
+import org.apache.druid.common.guava.FutureUtils;
+import org.apache.druid.indexer.RunnerTaskState;
+import org.apache.druid.indexer.TaskLocation;
+import org.apache.druid.indexer.TaskState;
+import org.apache.druid.indexer.TaskStatusPlus;
+import org.apache.druid.indexing.overlord.supervisor.SupervisorStatus;
 import org.apache.druid.jackson.DefaultObjectMapper;
+import org.apache.druid.java.util.common.DateTimes;
+import org.apache.druid.java.util.common.Intervals;
+import org.apache.druid.java.util.common.StringUtils;
+import org.apache.druid.java.util.http.client.response.StringFullResponseHolder;
+import org.apache.druid.rpc.HttpResponseException;
 import org.apache.druid.rpc.MockServiceClient;
 import org.apache.druid.rpc.RequestBuilder;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
 import org.jboss.netty.handler.codec.http.HttpMethod;
 import org.jboss.netty.handler.codec.http.HttpResponseStatus;
+import org.jboss.netty.handler.codec.http.HttpVersion;
+import org.joda.time.Interval;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class OverlordClientImplTest
 {
+  private static final List<TaskStatusPlus> STATUSES = Collections.singletonList(
+      new TaskStatusPlus(
+          "taskId",
+          null,
+          null,
+          DateTimes.of("2000"),
+          DateTimes.of("2000"),
+          TaskState.RUNNING,
+          RunnerTaskState.RUNNING,
+          null,
+          TaskLocation.unknown(),
+          null,
+          null
+      )
+  );
 
+  private ObjectMapper jsonMapper;
+  private MockServiceClient serviceClient;
+  private OverlordClient overlordClient;
+
+  @Before
+  public void setup()
+  {
+    jsonMapper = new DefaultObjectMapper();
+    serviceClient = new MockServiceClient();
+    overlordClient = new OverlordClientImpl(serviceClient, jsonMapper);
+  }
+
+  @After
+  public void tearDown()
+  {
+    serviceClient.verify();
+  }
 
   @Test
-  public void testTaskPayload() throws ExecutionException, InterruptedException, JsonProcessingException
+  public void test_findCurrentLeader() throws Exception
   {
-    final String taskID = "taskId_1";
-    MockServiceClient client = new MockServiceClient();
-    final OverlordClientImpl overlordClient = new OverlordClientImpl(client, DefaultObjectMapper.INSTANCE);
-
-    ClientTaskQuery clientTaskQuery = new ClientKillUnusedSegmentsTaskQuery(taskID, "test", null, null);
-
-    client.expect(new RequestBuilder(HttpMethod.GET, "/druid/indexer/v1/task/" + taskID),
-                  HttpResponseStatus.OK,
-                  Collections.emptyMap(),
-                  DefaultObjectMapper.INSTANCE.writeValueAsBytes(new TaskPayloadResponse(taskID, clientTaskQuery))
+    serviceClient.expectAndRespond(
+        new RequestBuilder(HttpMethod.GET, "/druid/indexer/v1/leader"),
+        HttpResponseStatus.OK,
+        ImmutableMap.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON),
+        StringUtils.toUtf8("http://followTheLeader")
     );
 
-    Assert.assertEquals(clientTaskQuery, overlordClient.taskPayload(taskID).get().getPayload());
+    Assert.assertEquals(URI.create("http://followTheLeader"), overlordClient.findCurrentLeader().get());
+  }
+
+  @Test
+  public void test_taskStatuses_null_null_null() throws Exception
+  {
+    serviceClient.expectAndRespond(
+        new RequestBuilder(HttpMethod.GET, "/druid/indexer/v1/tasks"),
+        HttpResponseStatus.OK,
+        ImmutableMap.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON),
+        jsonMapper.writeValueAsBytes(STATUSES)
+    );
+
+    Assert.assertEquals(
+        STATUSES,
+        ImmutableList.copyOf(overlordClient.taskStatuses(null, null, null).get())
+    );
+  }
+
+  @Test
+  public void test_taskStatuses_RUNNING_null_null() throws Exception
+  {
+    serviceClient.expectAndRespond(
+        new RequestBuilder(HttpMethod.GET, "/druid/indexer/v1/tasks?state=RUNNING"),
+        HttpResponseStatus.OK,
+        ImmutableMap.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON),
+        jsonMapper.writeValueAsBytes(STATUSES)
+    );
+
+    Assert.assertEquals(
+        STATUSES,
+        ImmutableList.copyOf(overlordClient.taskStatuses("RUNNING", null, null).get())
+    );
+  }
+
+  @Test
+  public void test_taskStatuses_RUNNING_foo_null() throws Exception
+  {
+    serviceClient.expectAndRespond(
+        new RequestBuilder(HttpMethod.GET, "/druid/indexer/v1/tasks?state=RUNNING&datasource=foo"),
+        HttpResponseStatus.OK,
+        ImmutableMap.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON),
+        jsonMapper.writeValueAsBytes(STATUSES)
+    );
+
+    Assert.assertEquals(
+        STATUSES,
+        ImmutableList.copyOf(overlordClient.taskStatuses("RUNNING", "foo", null).get())
+    );
+  }
+
+  @Test
+  public void test_taskStatuses_null_foo_null() throws Exception
+  {
+    serviceClient.expectAndRespond(
+        new RequestBuilder(HttpMethod.GET, "/druid/indexer/v1/tasks?datasource=foo"),
+        HttpResponseStatus.OK,
+        ImmutableMap.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON),
+        jsonMapper.writeValueAsBytes(STATUSES)
+    );
+
+    Assert.assertEquals(
+        STATUSES,
+        ImmutableList.copyOf(overlordClient.taskStatuses(null, "foo", null).get())
+    );
+  }
+
+  @Test
+  public void test_taskStatuses_RUNNING_foo_zero() throws Exception
+  {
+    serviceClient.expectAndRespond(
+        new RequestBuilder(
+            HttpMethod.GET,
+            "/druid/indexer/v1/tasks?state=RUNNING&datasource=foo%3F&maxCompletedTasks=0"
+        ),
+        HttpResponseStatus.OK,
+        ImmutableMap.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON),
+        jsonMapper.writeValueAsBytes(STATUSES)
+    );
+
+    Assert.assertEquals(
+        STATUSES,
+        ImmutableList.copyOf(overlordClient.taskStatuses("RUNNING", "foo?", 0).get())
+    );
+  }
+
+  @Test
+  public void test_taskStatuses_null_null_zero() throws Exception
+  {
+    serviceClient.expectAndRespond(
+        new RequestBuilder(
+            HttpMethod.GET,
+            "/druid/indexer/v1/tasks?maxCompletedTasks=0"
+        ),
+        HttpResponseStatus.OK,
+        ImmutableMap.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON),
+        jsonMapper.writeValueAsBytes(STATUSES)
+    );
+
+    Assert.assertEquals(
+        STATUSES,
+        ImmutableList.copyOf(overlordClient.taskStatuses(null, null, 0).get())
+    );
+  }
+
+  @Test
+  public void test_findLockedIntervals() throws Exception
+  {
+    final Map<String, Integer> priorityMap = ImmutableMap.of("foo", 3);
+    final Map<String, List<Interval>> lockMap =
+        ImmutableMap.of("foo", Collections.singletonList(Intervals.of("2000/2001")));
+
+    serviceClient.expectAndRespond(
+        new RequestBuilder(HttpMethod.POST, "/druid/indexer/v1/lockedIntervals")
+            .jsonContent(jsonMapper, priorityMap),
+        HttpResponseStatus.OK,
+        ImmutableMap.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON),
+        jsonMapper.writeValueAsBytes(lockMap)
+    );
+
+    Assert.assertEquals(
+        lockMap,
+        overlordClient.findLockedIntervals(priorityMap).get()
+    );
+  }
+
+  @Test
+  public void test_findLockedIntervals_nullReturn() throws Exception
+  {
+    final Map<String, Integer> priorityMap = ImmutableMap.of("foo", 3);
+
+    serviceClient.expectAndRespond(
+        new RequestBuilder(HttpMethod.POST, "/druid/indexer/v1/lockedIntervals")
+            .jsonContent(jsonMapper, priorityMap),
+        HttpResponseStatus.OK,
+        ImmutableMap.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON),
+        jsonMapper.writeValueAsBytes(null)
+    );
+
+    Assert.assertEquals(
+        Collections.emptyMap(),
+        overlordClient.findLockedIntervals(priorityMap).get()
+    );
+  }
+
+  @Test
+  public void test_supervisorStatuses() throws Exception
+  {
+    final List<SupervisorStatus> statuses = ImmutableList.of(
+        new SupervisorStatus.Builder()
+            .withId("foo")
+            .withSource("kafka")
+            .withState("chill")
+            .build()
+    );
+
+    serviceClient.expectAndRespond(
+        new RequestBuilder(HttpMethod.GET, "/druid/indexer/v1/supervisor?system"),
+        HttpResponseStatus.OK,
+        ImmutableMap.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON),
+        jsonMapper.writeValueAsBytes(statuses)
+    );
+
+    Assert.assertEquals(
+        statuses,
+        ImmutableList.copyOf(overlordClient.supervisorStatuses().get())
+    );
+  }
+
+  @Test
+  public void test_taskReportAsMap() throws Exception
+  {
+    final String taskId = "testTaskId";
+    final Map<String, Object> response = ImmutableMap.of("test", "value");
+
+    serviceClient.expectAndRespond(
+        new RequestBuilder(HttpMethod.GET, "/druid/indexer/v1/task/testTaskId/reports"),
+        HttpResponseStatus.OK,
+        ImmutableMap.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON),
+        jsonMapper.writeValueAsBytes(response)
+    );
+
+    final ListenableFuture<Map<String, Object>> future = overlordClient.taskReportAsMap(taskId);
+    Assert.assertEquals(response, future.get());
+  }
+
+  @Test
+  public void test_taskReportAsMap_notFound()
+  {
+    String taskId = "testTaskId";
+    String errorMsg = "No task reports were found for this task. "
+                      + "The task may not exist, or it may not have completed yet.";
+
+    serviceClient.expectAndThrow(
+        new RequestBuilder(HttpMethod.GET, "/druid/indexer/v1/task/testTaskId/reports"),
+        new HttpResponseException(
+            new StringFullResponseHolder(
+                new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND),
+                StandardCharsets.UTF_8
+            ).addChunk(errorMsg)
+        )
+    );
+
+    final ListenableFuture<Map<String, Object>> future = overlordClient.taskReportAsMap(taskId);
+
+    final ExecutionException e = Assert.assertThrows(
+        ExecutionException.class,
+        future::get
+    );
+
+    MatcherAssert.assertThat(e.getCause(), CoreMatchers.instanceOf(HttpResponseException.class));
+    Assert.assertEquals(
+        HttpResponseStatus.NOT_FOUND.getCode(),
+        ((HttpResponseException) e.getCause()).getResponse().getStatus().getCode()
+    );
+  }
+
+  @Test
+  public void test_getTaskReport_empty()
+  {
+    final String taskID = "testTaskId";
+
+    serviceClient.expectAndRespond(
+        new RequestBuilder(HttpMethod.GET, "/druid/indexer/v1/task/" + taskID + "/reports"),
+        HttpResponseStatus.OK,
+        Collections.emptyMap(),
+        StringUtils.toUtf8("{}")
+    );
+
+    final Map<String, Object> actualResponse =
+        FutureUtils.getUnchecked(overlordClient.taskReportAsMap(taskID), true);
+    Assert.assertEquals(Collections.emptyMap(), actualResponse);
+  }
+
+  @Test
+  public void test_getTotalWorkerCapacity() throws Exception
+  {
+    int currentClusterCapacity = 5;
+    int maximumCapacityWithAutoScale = 10;
+
+    // Mock response for /druid/indexer/v1/totalWorkerCapacity
+    final IndexingTotalWorkerCapacityInfo indexingTotalWorkerCapacityInfo = new IndexingTotalWorkerCapacityInfo(
+        currentClusterCapacity,
+        maximumCapacityWithAutoScale
+    );
+
+    serviceClient.expectAndRespond(
+        new RequestBuilder(HttpMethod.GET, "/druid/indexer/v1/totalWorkerCapacity"),
+        HttpResponseStatus.OK,
+        Collections.emptyMap(),
+        jsonMapper.writeValueAsBytes(indexingTotalWorkerCapacityInfo)
+    );
+
+    Assert.assertEquals(
+        indexingTotalWorkerCapacityInfo,
+        FutureUtils.getUnchecked(overlordClient.getTotalWorkerCapacity(), true)
+    );
+  }
+
+  @Test
+  public void test_getWorkers() throws Exception
+  {
+    final List<IndexingWorkerInfo> workers = ImmutableList.of(
+        new IndexingWorkerInfo(
+            new IndexingWorker("http", "localhost", "1.2.3.4", 3, "2"),
+            0,
+            Collections.emptySet(),
+            Collections.emptyList(),
+            DateTimes.of("2000"),
+            null
+        )
+    );
+
+    serviceClient.expectAndRespond(
+        new RequestBuilder(HttpMethod.GET, "/druid/indexer/v1/workers"),
+        HttpResponseStatus.OK,
+        Collections.emptyMap(),
+        jsonMapper.writeValueAsBytes(workers)
+    );
+
+    Assert.assertEquals(
+        workers,
+        FutureUtils.getUnchecked(overlordClient.getWorkers(), true)
+    );
+  }
+
+  @Test
+  public void test_killPendingSegments() throws Exception
+  {
+    serviceClient.expectAndRespond(
+        new RequestBuilder(
+            HttpMethod.DELETE,
+            "/druid/indexer/v1/pendingSegments/foo?interval=2000-01-01T00%3A00%3A00.000Z%2F2001-01-01T00%3A00%3A00.000Z"
+        ),
+        HttpResponseStatus.OK,
+        Collections.emptyMap(),
+        jsonMapper.writeValueAsBytes(
+            ImmutableMap.of("numDeleted", 2L)
+        )
+    );
+
+    Assert.assertEquals(
+        Integer.valueOf(2),
+        FutureUtils.getUnchecked(overlordClient.killPendingSegments("foo", Intervals.of("2000/2001")), true)
+    );
+  }
+
+  @Test
+  public void test_taskPayload() throws ExecutionException, InterruptedException, JsonProcessingException
+  {
+    final String taskID = "taskId_1";
+    final ClientTaskQuery clientTaskQuery = new ClientKillUnusedSegmentsTaskQuery(taskID, "test", null, null);
+
+    serviceClient.expectAndRespond(
+        new RequestBuilder(HttpMethod.GET, "/druid/indexer/v1/task/" + taskID),
+        HttpResponseStatus.OK,
+        Collections.emptyMap(),
+        DefaultObjectMapper.INSTANCE.writeValueAsBytes(new TaskPayloadResponse(taskID, clientTaskQuery))
+    );
+
+    Assert.assertEquals(
+        clientTaskQuery,
+        overlordClient.taskPayload(taskID).get().getPayload()
+    );
   }
 }

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/guice/SqlTaskModule.java
@@ -21,11 +21,8 @@ package org.apache.druid.msq.guice;
 
 import com.fasterxml.jackson.databind.Module;
 import com.google.inject.Binder;
-import org.apache.druid.client.indexing.HttpIndexingServiceClient;
-import org.apache.druid.client.indexing.IndexingServiceClient;
 import org.apache.druid.discovery.NodeRole;
 import org.apache.druid.guice.Jerseys;
-import org.apache.druid.guice.LazySingleton;
 import org.apache.druid.guice.LifecycleModule;
 import org.apache.druid.guice.annotations.LoadScope;
 import org.apache.druid.initialization.DruidModule;
@@ -48,7 +45,6 @@ public class SqlTaskModule implements DruidModule
     LifecycleModule.register(binder, SqlTaskResource.class);
     Jerseys.addResource(binder, SqlTaskResource.class);
     LifecycleModule.register(binder, SqlStatementResource.class);
-    binder.bind(IndexingServiceClient.class).to(HttpIndexingServiceClient.class).in(LazySingleton.class);
     Jerseys.addResource(binder, SqlStatementResource.class);
   }
 

commit 2242a4199fcb945488e68bd3e7195a5a936bed25

Fix maxCompletedTasks parameter in OverlordClientImpl.
It was sent to the server as "maxCompletedTasks", but the server expects
"max". This caused it to be ignored. This bug was introduced in #14581.

server/src/main/java/org/apache/druid/rpc/indexing/OverlordClientImpl.java
@@ -147,7 +147,7 @@ public class OverlordClientImpl implements OverlordClient
     }
 
     if (maxCompletedTasks != null) {
-      pathBuilder.append(params == 0 ? '?' : '&').append("maxCompletedTasks=").append(maxCompletedTasks);
+      pathBuilder.append(params == 0 ? '?' : '&').append("max=").append(maxCompletedTasks);
     }
 
     return FutureUtils.transform(

server/src/test/java/org/apache/druid/rpc/indexing/OverlordClientImplTest.java
@@ -184,7 +184,7 @@ public class OverlordClientImplTest
     serviceClient.expectAndRespond(
         new RequestBuilder(
             HttpMethod.GET,
-            "/druid/indexer/v1/tasks?state=RUNNING&datasource=foo%3F&maxCompletedTasks=0"
+            "/druid/indexer/v1/tasks?state=RUNNING&datasource=foo%3F&max=0"
         ),
         HttpResponseStatus.OK,
         ImmutableMap.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON),
@@ -203,7 +203,7 @@ public class OverlordClientImplTest
     serviceClient.expectAndRespond(
         new RequestBuilder(
             HttpMethod.GET,
-            "/druid/indexer/v1/tasks?maxCompletedTasks=0"
+            "/druid/indexer/v1/tasks?max=0"
         ),
         HttpResponseStatus.OK,
         ImmutableMap.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON),