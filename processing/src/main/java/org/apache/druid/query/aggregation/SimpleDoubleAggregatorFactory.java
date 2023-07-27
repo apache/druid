f8fed6bdd18c89e5c04cbfa31f3021f9bdbf422e
Clint Wylie <cwylie@apache.org>
2023-07-26 17:43:24 -0700

    fix checker

--- processing/src/main/java/org/apache/druid/query/aggregation/SimpleDoubleAggregatorFactory.java
+++ processing/src/main/java/org/apache/druid/query/aggregation/SimpleDoubleAggregatorFactory.java
@@ -34,8 +34,6 @@ import org.apache.druid.segment.ColumnValueSelector;
 import org.apache.druid.segment.column.ColumnCapabilities;
 import org.apache.druid.segment.column.ColumnHolder;
 import org.apache.druid.segment.column.ColumnType;
-import org.apache.druid.segment.column.Types;
-import org.apache.druid.segment.column.ValueType;
 import org.apache.druid.segment.vector.VectorColumnSelectorFactory;
 import org.apache.druid.segment.vector.VectorValueSelector;
 
@@ -135,7 +133,7 @@ public abstract class SimpleDoubleAggregatorFactory extends NullableNumericAggre
   {
     if (fieldName != null) {
       ColumnCapabilities capabilities = columnSelectorFactory.getColumnCapabilities(fieldName);
-      return Types.is(capabilities, ValueType.STRING);
+      return capabilities == null || !capabilities.isNumeric();
     }
     return false;
   }
