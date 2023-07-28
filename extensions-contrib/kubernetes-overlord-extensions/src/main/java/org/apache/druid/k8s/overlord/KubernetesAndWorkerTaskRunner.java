36ef49ed816d4bf8f3cf76682bceb117e5c14304
George Wu <george.wu@imply.io>
2023-07-28 11:22:56 -0400

    Working

--- extensions-contrib/kubernetes-overlord-extensions/src/main/java/org/apache/druid/k8s/overlord/KubernetesAndWorkerTaskRunner.java
+++ extensions-contrib/kubernetes-overlord-extensions/src/main/java/org/apache/druid/k8s/overlord/KubernetesAndWorkerTaskRunner.java
@@ -1,8 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.druid.k8s.overlord;
 
 import com.google.common.base.Optional;
 import com.google.common.base.Predicate;
-import com.google.common.collect.Maps;
 import com.google.common.util.concurrent.ListenableFuture;
 import io.vavr.collection.Iterator;
 import org.apache.druid.indexer.TaskStatus;
@@ -15,18 +33,19 @@ import org.apache.druid.indexing.overlord.autoscaling.ScalingStats;
 import org.apache.druid.indexing.overlord.config.WorkerTaskRunnerConfig;
 import org.apache.druid.indexing.worker.Worker;
 import org.apache.druid.java.util.common.Pair;
-import org.apache.druid.k8s.overlord.KubernetesTaskRunner;
 import org.apache.druid.tasklogs.TaskLogStreamer;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.stream.Collectors;
 
-public class KubernetesAndWorkerTaskRunner implements TaskLogStreamer, WorkerTaskRunner {
+public class KubernetesAndWorkerTaskRunner implements TaskLogStreamer, WorkerTaskRunner
+{
   private final KubernetesTaskRunner kubernetesTaskRunner;
   private final WorkerTaskRunner workerTaskRunner;
   private final Boolean sendTasksToWorkerTaskRunner;
@@ -43,30 +62,35 @@ public class KubernetesAndWorkerTaskRunner implements TaskLogStreamer, WorkerTas
   }
 
   @Override
-  public List<Pair<Task, ListenableFuture<TaskStatus>>> restore() {
+  public List<Pair<Task, ListenableFuture<TaskStatus>>> restore()
+  {
     return Iterator.concat(kubernetesTaskRunner.restore(), kubernetesTaskRunner.restore()).collect(Collectors.toList());
   }
 
   @Override
-  public void start() {
+  public void start()
+  {
     kubernetesTaskRunner.start();
     workerTaskRunner.start();
   }
 
   @Override
-  public void registerListener(TaskRunnerListener listener, Executor executor) {
+  public void registerListener(TaskRunnerListener listener, Executor executor)
+  {
     kubernetesTaskRunner.registerListener(listener, executor);
     workerTaskRunner.registerListener(listener, executor);
   }
 
   @Override
-  public void unregisterListener(String listenerId) {
+  public void unregisterListener(String listenerId)
+  {
     kubernetesTaskRunner.unregisterListener(listenerId);
     workerTaskRunner.unregisterListener(listenerId);
   }
 
   @Override
-  public ListenableFuture<TaskStatus> run(Task task) {
+  public ListenableFuture<TaskStatus> run(Task task)
+  {
     if (sendTasksToWorkerTaskRunner) {
       return workerTaskRunner.run(task);
     } else {
@@ -75,107 +99,124 @@ public class KubernetesAndWorkerTaskRunner implements TaskLogStreamer, WorkerTas
   }
 
   @Override
-  public void shutdown(String taskid, String reason) {
+  public void shutdown(String taskid, String reason)
+  {
     // Technically this is a no-op for tasks a runner does't know about.
     workerTaskRunner.shutdown(taskid, reason);
     kubernetesTaskRunner.shutdown(taskid, reason);
   }
 
   @Override
-  public void stop() {
+  public void stop()
+  {
     kubernetesTaskRunner.stop();
     workerTaskRunner.stop();
   }
 
   @Override
-  public Collection<? extends TaskRunnerWorkItem> getRunningTasks() {
+  public Collection<? extends TaskRunnerWorkItem> getRunningTasks()
+  {
     return Iterator.concat(kubernetesTaskRunner.getRunningTasks(), workerTaskRunner.getRunningTasks()).collect(Collectors.toList());
   }
 
   @Override
-  public Collection<? extends TaskRunnerWorkItem> getPendingTasks() {
+  public Collection<? extends TaskRunnerWorkItem> getPendingTasks()
+  {
     return Iterator.concat(kubernetesTaskRunner.getPendingTasks(), workerTaskRunner.getPendingTasks()).collect(Collectors.toList());
 
   }
 
   @Override
-  public Collection<? extends TaskRunnerWorkItem> getKnownTasks() {
+  public Collection<? extends TaskRunnerWorkItem> getKnownTasks()
+  {
     return Iterator.concat(kubernetesTaskRunner.getKnownTasks(), workerTaskRunner.getKnownTasks()).collect(Collectors.toList());
 
   }
 
   @Override
-  public Optional<ScalingStats> getScalingStats() {
+  public Optional<ScalingStats> getScalingStats()
+  {
     return workerTaskRunner.getScalingStats();
   }
 
   @Override
-  public Map<String, Long> getTotalTaskSlotCount() {
-    Map<String, Long> taskSlotCounts = Maps.newHashMap();
+  public Map<String, Long> getTotalTaskSlotCount()
+  {
+    Map<String, Long> taskSlotCounts = new HashMap<>();
     taskSlotCounts.putAll(kubernetesTaskRunner.getTotalTaskSlotCount());
     taskSlotCounts.putAll(workerTaskRunner.getTotalTaskSlotCount());
     return taskSlotCounts;
   }
 
   @Override
-  public Map<String, Long> getIdleTaskSlotCount() {
-    Map<String, Long> taskSlotCounts = Maps.newHashMap();
+  public Map<String, Long> getIdleTaskSlotCount()
+  {
+    Map<String, Long> taskSlotCounts = new HashMap<>();
     taskSlotCounts.putAll(kubernetesTaskRunner.getIdleTaskSlotCount());
     taskSlotCounts.putAll(workerTaskRunner.getIdleTaskSlotCount());
     return taskSlotCounts;
   }
 
   @Override
-  public Map<String, Long> getUsedTaskSlotCount() {
-    Map<String, Long> taskSlotCounts = Maps.newHashMap();
+  public Map<String, Long> getUsedTaskSlotCount()
+  {
+    Map<String, Long> taskSlotCounts = new HashMap<>();
     taskSlotCounts.putAll(kubernetesTaskRunner.getUsedTaskSlotCount());
     taskSlotCounts.putAll(workerTaskRunner.getUsedTaskSlotCount());
     return taskSlotCounts;
   }
 
   @Override
-  public Map<String, Long> getLazyTaskSlotCount() {
-    Map<String, Long> taskSlotCounts = Maps.newHashMap();
+  public Map<String, Long> getLazyTaskSlotCount()
+  {
+    Map<String, Long> taskSlotCounts = new HashMap<>();
     taskSlotCounts.putAll(kubernetesTaskRunner.getLazyTaskSlotCount());
     taskSlotCounts.putAll(workerTaskRunner.getLazyTaskSlotCount());
     return taskSlotCounts;
   }
 
   @Override
-  public Map<String, Long> getBlacklistedTaskSlotCount() {
-    Map<String, Long> taskSlotCounts = Maps.newHashMap();
+  public Map<String, Long> getBlacklistedTaskSlotCount()
+  {
+    Map<String, Long> taskSlotCounts = new HashMap<>();
     taskSlotCounts.putAll(kubernetesTaskRunner.getBlacklistedTaskSlotCount());
     taskSlotCounts.putAll(workerTaskRunner.getBlacklistedTaskSlotCount());
     return taskSlotCounts;
   }
 
   @Override
-  public Collection<ImmutableWorkerInfo> getWorkers() {
+  public Collection<ImmutableWorkerInfo> getWorkers()
+  {
     return workerTaskRunner.getWorkers();
   }
 
   @Override
-  public Collection<Worker> getLazyWorkers() {
+  public Collection<Worker> getLazyWorkers()
+  {
     return workerTaskRunner.getLazyWorkers();
   }
 
   @Override
-  public Collection<Worker> markWorkersLazy(Predicate<ImmutableWorkerInfo> isLazyWorker, int maxWorkers) {
+  public Collection<Worker> markWorkersLazy(Predicate<ImmutableWorkerInfo> isLazyWorker, int maxWorkers)
+  {
     return workerTaskRunner.markWorkersLazy(isLazyWorker, maxWorkers);
   }
 
   @Override
-  public WorkerTaskRunnerConfig getConfig() {
+  public WorkerTaskRunnerConfig getConfig()
+  {
     return workerTaskRunner.getConfig();
   }
 
   @Override
-  public Collection<Task> getPendingTaskPayloads() {
+  public Collection<Task> getPendingTaskPayloads()
+  {
     return workerTaskRunner.getPendingTaskPayloads();
   }
 
   @Override
-  public Optional<InputStream> streamTaskLog(String taskid, long offset) throws IOException {
+  public Optional<InputStream> streamTaskLog(String taskid, long offset) throws IOException
+  {
     Optional<InputStream> kubernetesTaskLog = kubernetesTaskRunner.streamTaskLog(taskid, offset);
     if (kubernetesTaskLog.isPresent()) {
       return kubernetesTaskLog;
--- extensions-contrib/kubernetes-overlord-extensions/src/main/java/org/apache/druid/k8s/overlord/KubernetesAndWorkerTaskRunnerFactory.java
+++ extensions-contrib/kubernetes-overlord-extensions/src/main/java/org/apache/druid/k8s/overlord/KubernetesAndWorkerTaskRunnerFactory.java
@@ -1,29 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.druid.k8s.overlord;
 
 import com.google.inject.Inject;
 import org.apache.druid.indexing.overlord.TaskRunnerFactory;
-import org.apache.druid.indexing.overlord.WorkerTaskRunner;
-import org.apache.druid.k8s.overlord.KubernetesAndWorkerTaskRunner;
+import org.apache.druid.indexing.overlord.hrtr.HttpRemoteTaskRunner;
 
-public class KubernetesAndWorkerTaskRunnerFactory implements TaskRunnerFactory<KubernetesAndWorkerTaskRunner> {
+
+public class KubernetesAndWorkerTaskRunnerFactory implements TaskRunnerFactory<KubernetesAndWorkerTaskRunner>
+{
   public static final String TYPE_NAME = "k8sAndWorker";
 
   private final KubernetesTaskRunnerFactory kubernetesTaskRunnerFactory;
-  private final TaskRunnerFactory<WorkerTaskRunner> workerTaskRunnerFactory;
+  private final TaskRunnerFactory<HttpRemoteTaskRunner> workerTaskRunnerFactory;
 
   private KubernetesAndWorkerTaskRunner runner;
 
   @Inject
   public KubernetesAndWorkerTaskRunnerFactory(
       KubernetesTaskRunnerFactory kubernetesTaskRunnerFactory,
-      TaskRunnerFactory<WorkerTaskRunner> workerTaskRunnerFactory
+      TaskRunnerFactory<HttpRemoteTaskRunner> workerTaskRunnerFactory
   )
   {
-   this.kubernetesTaskRunnerFactory = kubernetesTaskRunnerFactory;
-   this.workerTaskRunnerFactory = workerTaskRunnerFactory;
+    this.kubernetesTaskRunnerFactory = kubernetesTaskRunnerFactory;
+    this.workerTaskRunnerFactory = workerTaskRunnerFactory;
   }
+
   @Override
-  public KubernetesAndWorkerTaskRunner build() {
+  public KubernetesAndWorkerTaskRunner build()
+  {
     runner = new KubernetesAndWorkerTaskRunner(
         kubernetesTaskRunnerFactory.build(),
         workerTaskRunnerFactory.build(),
@@ -33,7 +55,8 @@ public class KubernetesAndWorkerTaskRunnerFactory implements TaskRunnerFactory<K
   }
 
   @Override
-  public KubernetesAndWorkerTaskRunner get() {
+  public KubernetesAndWorkerTaskRunner get()
+  {
     return runner;
   }
 }
