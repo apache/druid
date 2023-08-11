b30d37f7748699960e8d0a94d3b1da7fdd9ea110
Zoltan Haindrich <zoltan.haindrich@imply.io>
2023-08-11 12:14:58 +0000

    fix style

--- processing/src/main/java/org/apache/druid/query/aggregation/CountSumAggregatorFactory.java
+++ processing/src/main/java/org/apache/druid/query/aggregation/CountSumAggregatorFactory.java
@@ -7,27 +7,28 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.apache.druid.query.aggregation;
-
-import javax.annotation.Nullable;
-
-import org.apache.druid.math.expr.ExprMacroTable;
 
 import com.fasterxml.jackson.annotation.JacksonInject;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Supplier;
+import org.apache.druid.math.expr.ExprMacroTable;
 
-public class CountSumAggregatorFactory extends LongSumAggregatorFactory {
+import javax.annotation.Nullable;
+
+public class CountSumAggregatorFactory extends LongSumAggregatorFactory
+{
 
   private final Supplier<byte[]> cacheKey;
 
@@ -56,5 +57,4 @@ public class CountSumAggregatorFactory extends LongSumAggregatorFactory {
   {
     return true;
   }
-
 }
