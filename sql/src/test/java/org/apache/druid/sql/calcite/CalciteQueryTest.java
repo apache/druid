d0f5487bb43c8ea2df9addbcbc516ce4127f0544
Zoltan Haindrich <zoltan.haindrich@imply.io>
2023-08-17 10:53:34 +0000

    extend test; disable vectorization - no support for earliest

--- sql/src/test/java/org/apache/druid/sql/calcite/CalciteQueryTest.java
+++ sql/src/test/java/org/apache/druid/sql/calcite/CalciteQueryTest.java
@@ -14273,8 +14273,9 @@ public class CalciteQueryTest extends BaseCalciteQueryTest
   @Test
   public void testLatestByOnStringColumnWithoutMaxBytesSpecified()
   {
+    cannotVectorize();
     testQuery(
-        "SELECT dim2,LATEST(dim3),LATEST_BY(dim1, __time) FROM druid.foo where dim2='abc' group by 1",
+        "SELECT dim2,LATEST(dim3),LATEST_BY(dim1, __time),EARLIEST(dim3),EARLIEST_BY(dim1, __time) FROM druid.foo where dim2='abc' group by 1",
         ImmutableList.of(
             GroupByQuery.builder()
                 .setDataSource(CalciteTests.DATASOURCE1)
@@ -14288,12 +14289,14 @@ public class CalciteQueryTest extends BaseCalciteQueryTest
                 .setAggregatorSpecs(
                     aggregators(
                         new StringLastAggregatorFactory("a0", "dim3", "__time", 1024),
-                        new StringLastAggregatorFactory("a1", "dim1", "__time", 1024)))
+                        new StringLastAggregatorFactory("a1", "dim1", "__time", 1024),
+                        new StringFirstAggregatorFactory("a2", "dim3", "__time", 1024),
+                        new StringFirstAggregatorFactory("a3", "dim1", "__time", 1024)))
                 .build()
 
         ),
         ImmutableList.of(
-            new Object[] {"abc", useDefault ? "" : null, "def"}
+            new Object[] { "abc", useDefault ? "" : null, "def", useDefault ? "" : null, "def" }
         ));
   }
 }
