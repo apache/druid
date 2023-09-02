b28453f4a4b6db84ac1db19deb93781380128d2f
krishnanand5 <krishna1729atom@gmail.com>
2023-09-01 20:29:58 -0500

    + Replacing TreeMap with LinkedHashMap

--- extensions-core/s3-extensions/src/test/java/org/apache/druid/storage/s3/S3DataSegmentPusherConfigTest.java
+++ extensions-core/s3-extensions/src/test/java/org/apache/druid/storage/s3/S3DataSegmentPusherConfigTest.java
@@ -29,8 +29,8 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.Set;
-import java.util.TreeMap;
 
 public class S3DataSegmentPusherConfigTest
 {
@@ -44,8 +44,8 @@ public class S3DataSegmentPusherConfigTest
 
     S3DataSegmentPusherConfig config = JSON_MAPPER.readValue(jsonConfig, S3DataSegmentPusherConfig.class);
 
-    TreeMap<String, String> expected = JSON_MAPPER.readValue(jsonConfig, TreeMap.class);
-    TreeMap<String, String> actual = JSON_MAPPER.readValue(JSON_MAPPER.writeValueAsString(config), TreeMap.class);
+    LinkedHashMap<String, String> expected = JSON_MAPPER.readValue(jsonConfig, LinkedHashMap.class);
+    LinkedHashMap<String, String> actual = JSON_MAPPER.readValue(JSON_MAPPER.writeValueAsString(config), LinkedHashMap.class);
 
     Assert.assertEquals(expected, actual);
   }
