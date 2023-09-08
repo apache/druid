904f99b559cd19e5317c4721d2e9189c274ec288
Clint Wylie <cwylie@apache.org>
2023-09-08 15:57:10 -0700

    longer compatibility window for nested column format v4 changes: * add back nested column v4 serializers * 'json' schema by default still uses the newer 'nested common format' used by 'auto', but now has an optional 'formatVersion' property which can be specified to override format versions on native ingest jobs * add system config to specify default column format stuff, 'druid.indexing.formats', and property 'druid.indexing.formats.nestedColumnFormatVersion' to specify system level preferred nested column format for friendly rolling upgrades from versions which do not support the newer 'nested common format' used by 'auto'

---
+++ processing/src/main/java/org/apache/druid/segment/DefaultColumnFormatConfig.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.segment;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.annotation.Nullable;
+import java.util.Objects;
+
+public class DefaultColumnFormatConfig
+{
+  @JsonProperty("nestedColumnFormatVersion")
+  private final Integer nestedColumnFormatVersion;
+
+  @JsonCreator
+  public DefaultColumnFormatConfig(
+      @JsonProperty("nestedColumnFormatVersion") @Nullable Integer nestedColumnFormatVersion
+  )
+  {
+    this.nestedColumnFormatVersion = nestedColumnFormatVersion;
+  }
+
+  @Nullable
+  @JsonProperty("nestedColumnFormatVersion")
+  public Integer getNestedColumnFormatVersion()
+  {
+    return nestedColumnFormatVersion;
+  }
+
+  @Override
+  public boolean equals(Object o)
+  {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    DefaultColumnFormatConfig that = (DefaultColumnFormatConfig) o;
+    return Objects.equals(nestedColumnFormatVersion, that.nestedColumnFormatVersion);
+  }
+
+  @Override
+  public int hashCode()
+  {
+    return Objects.hash(nestedColumnFormatVersion);
+  }
+
+  @Override
+  public String toString()
+  {
+    return "DefaultColumnFormatsConfig{" +
+           "nestedColumnFormatVersion=" + nestedColumnFormatVersion +
+           '}';
+  }
+}
