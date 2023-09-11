d014e8dc230102f2c2bf1e88168545b25182fd96
Suneet Saldanha <suneet@apache.org>
2023-09-11 10:35:46 -0700

    fix

--- indexing-service/src/main/java/org/apache/druid/indexing/overlord/TaskRunner.java
+++ indexing-service/src/main/java/org/apache/druid/indexing/overlord/TaskRunner.java
@@ -31,7 +31,6 @@ import org.apache.druid.java.util.common.Pair;
 import org.apache.druid.java.util.common.StringUtils;
 
 import javax.annotation.Nullable;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
