e85d18645f12b4a829ee220cc9f93bf8ba8aa718
Hiroshi Fukada <hiroshi.fukada@konghq.com>
2023-09-21 17:48:11 -0400

    New: Add DDSketch-Druid extension
    - Based off of http://www.vldb.org/pvldb/vol12/p2195-masson.pdf and uses
     the corresponding https://github.com/DataDog/sketches-java library
    - contains tests for post building and using aggregation/post
      aggregation.
    - New aggregator: `ddSketch`
    - New post aggregators: `quantileFromDDSketch` and
      `quantilesFromDDSketch`

---
+++ docs/development/extensions-contrib/ddsketch-quantiles.md
@@ -0,0 +1,111 @@
+---
+id: ddsketch-quantiles
+title: "DDSketches for Approximate Quantiles module"
+---
+
+<!--
+  ~ Licensed to the Apache Software Foundation (ASF) under one
+  ~ or more contributor license agreements.  See the NOTICE file
+  ~ distributed with this work for additional information
+  ~ regarding copyright ownership.  The ASF licenses this file
+  ~ to you under the Apache License, Version 2.0 (the
+  ~ "License"); you may not use this file except in compliance
+  ~ with the License.  You may obtain a copy of the License at
+  ~
+  ~   http://www.apache.org/licenses/LICENSE-2.0
+  ~
+  ~ Unless required by applicable law or agreed to in writing,
+  ~ software distributed under the License is distributed on an
+  ~ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  ~ KIND, either express or implied.  See the License for the
+  ~ specific language governing permissions and limitations
+  ~ under the License.
+  -->
+
+
+This module provides aggregators for approximate quantile queries using the [DDSketch](https://github.com/datadog/sketches-java) library. The DDSketch library provides a fast, and fully-mergeable quantile sketch with relative error. If the true quantile is 100, a sketch with relative error of 1% guarantees a quantile value between 101 and 99. This is important and highly valuable behavior for long tail distributions.
+
+To use this Apache Druid extension, [include](../../configuration/extensions.md#loading-extensions) in the extensions load list.
+
+### Aggregator
+
+The result of the aggregation is a DDSketch that is the union of all sketches either built from raw data or read from the segments. The single number that is returned represents the total number of included datapoints. The default aggregator type of `ddSketch` uses the collapsingLowestDense strategy for storing and merging sketch. This means that in favor of keeping the highest values represented at the highest accuracy, the sketch will collapse and merge lower, smaller values in the sketch. Collapsed bins will lose accuracy guarantees. The default number of bins is 1000. Sketches can only be merged when using the same relativeError values.
+
+The `ddSketch` aggregator operates over raw data and precomputed sketches.
+
+```json
+{
+  "type" : "ddSketch",
+  "name" : <output_name>,
+  "fieldName" : <input_name>,
+  "relativeError" : <double(0, 1)>,
+  "numBins": <int>
+ }
+```
+
+|property|description|required?|
+|--------|-----------|---------|
+|type|Must be "ddSketch" |yes|
+|name|A String for the output (result) name of the calculation.|yes|
+|fieldName|A String for the name of the input field (can contain sketches or raw numeric values).|yes|
+|relativeError||Describes the precision in which to store the sketch. Must be a number between 0 and 1.|no, defaults to 0.01 (1% error)|
+|numBins|Total number of bins the sketch is allowed to use to describe the distribution. This has a direct impact on max memory used|no, defaults to 1000|
+
+### Post Aggregators
+
+Users can query for a set of quantiles using the `quantilesFromDDSketch` post-aggregator on the sketches created by the `ddSketch` aggregators.
+
+```json
+{
+  "type"  : "quantilesFromDDSketch",
+  "name" : <output_name>,
+  "field" : <reference to moment sketch>,
+  "fractions" : <array of doubles in [0,1]>
+}
+```
+
+Single quantiles may be fetched as well.
+
+```json
+{
+  "type"  : "quantileFromDDSketch",
+  "name" : <output_name>,
+  "field" : <reference to moment sketch>,
+  "fraction" : <double [0,1]>
+}
+```
+
+### Example
+As an example of a query with sketches pre-aggregated at ingestion time, one could set up the following aggregator at ingest:
+
+```json
+{
+  "type": "ddSketch",
+  "name": "sketch",
+  "fieldName": "value",
+  "relativeError": 0.01,
+  "numBins": 1000,
+}
+```
+
+and make queries using the following aggregator + post-aggregator:
+
+```json
+{
+  "aggregations": [{
+    "type": "ddSketch",
+    "name": "sketch",
+    "fieldName": "sketch",
+  }],
+  "postAggregations": [
+  {
+    "type": "quantilesFromDDSketch",
+    "name": "quantiles",
+    "fractions": [0.5, 0.75, 0.9, 0.99],
+    "field": {
+      "type": "fieldAccess",
+      "fieldName": "sketch"
+    }
+  }]
+}
+```
---
+++ extensions-contrib/ddsketch/src/main/java/org/apache/druid/query/aggregation/ddsketch/DDSketchAggregatorFactory.java
@@ -0,0 +1,327 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.query.aggregation.ddsketch;
+
+import com.datadoghq.sketch.ddsketch.DDSketch;
+import com.datadoghq.sketch.ddsketch.DDSketches;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import org.apache.druid.query.aggregation.AggregateCombiner;
+import org.apache.druid.query.aggregation.Aggregator;
+import org.apache.druid.query.aggregation.AggregatorFactory;
+import org.apache.druid.query.aggregation.AggregatorFactoryNotMergeableException;
+import org.apache.druid.query.aggregation.BufferAggregator;
+import org.apache.druid.query.aggregation.ObjectAggregateCombiner;
+import org.apache.druid.query.cache.CacheKeyBuilder;
+import org.apache.druid.segment.ColumnSelectorFactory;
+import org.apache.druid.segment.ColumnValueSelector;
+import org.apache.druid.segment.column.ColumnType;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Aggregation operations over the ddsketch quantile sketch
+ * available on <a href="https://github.com/DataDog/sketches-java">github</a> and described
+ * in the paper
+ * <a href="https://blog.acolyer.org/2019/09/06/ddsketch/">
+ * Computing relative error quantiles using ddsketch</a>.
+ * <p>
+ */
+@JsonTypeName(DDSketchAggregatorFactory.TYPE_NAME)
+public class DDSketchAggregatorFactory extends AggregatorFactory
+{
+  private static final byte CACHE_TYPE_ID = 0x50;
+  // Default relative error
+  public static final double DEFAULT_RELATIVE_ERROR = 0.01;
+
+  // Default num bins
+  public static final int DEFAULT_NUM_BINS = 1000;
+
+  @Nonnull
+  private final String name;
+  @Nonnull
+  private final String fieldName;
+
+  private final double relativeError;
+
+  private final int numBins;
+
+  private final byte cacheTypeId;
+
+  public static final String TYPE_NAME = "ddSketch";
+  public static final ColumnType TYPE = ColumnType.ofComplex(TYPE_NAME);
+
+  @JsonCreator
+  public DDSketchAggregatorFactory(
+      @JsonProperty("name") final String name,
+      @JsonProperty("fieldName") final String fieldName,
+      @JsonProperty("relativeError") final Double relativeError,
+      @JsonProperty("numBins") final Integer numBins
+  )
+  {
+    this(name, fieldName, relativeError, numBins, CACHE_TYPE_ID);
+  }
+
+  DDSketchAggregatorFactory(
+      final String name,
+      final String fieldName,
+      @Nullable final Double relativeError,
+      @Nullable final Integer numBins,
+      final byte cacheTypeId
+  )
+  {
+    this.name = Objects.requireNonNull(name, "Must have a valid, non-null aggregator name");
+    this.fieldName = Objects.requireNonNull(fieldName, "Parameter fieldName must be specified");
+    this.relativeError = relativeError == null ? DEFAULT_RELATIVE_ERROR : relativeError;
+    this.numBins = numBins == null ? DEFAULT_NUM_BINS : numBins;
+    this.cacheTypeId = cacheTypeId;
+  }
+
+
+  @Override
+  public byte[] getCacheKey()
+  {
+    return new CacheKeyBuilder(
+        cacheTypeId
+    ).appendString(fieldName).appendDouble(relativeError).appendInt(numBins).build();
+  }
+
+
+  @Override
+  public Aggregator factorize(ColumnSelectorFactory metricFactory)
+  {
+    return new DDSketchAggregator(metricFactory.makeColumnValueSelector(fieldName), relativeError, numBins);
+  }
+
+  @Override
+  public BufferAggregator factorizeBuffered(ColumnSelectorFactory metricFactory)
+  {
+    return new DDSketchBufferAggregator(metricFactory.makeColumnValueSelector(fieldName), relativeError, numBins);
+  }
+
+  public static final Comparator<DDSketch> COMPARATOR = Comparator.nullsFirst(
+      Comparator.comparingLong(a -> a.serializedSize())
+  );
+
+  @Override
+  public Comparator getComparator()
+  {
+    return COMPARATOR;
+  }
+
+  @Override
+  public Object combine(@Nullable Object lhs, @Nullable Object rhs)
+  {
+    if (lhs == null) {
+      return rhs;
+    }
+    if (rhs == null) {
+      return lhs;
+    }
+    DDSketch union = (DDSketch) lhs;
+    union.mergeWith((DDSketch) rhs);
+    return union;
+  }
+
+  @Override
+  public AggregatorFactory getCombiningFactory()
+  {
+    return new DDSketchAggregatorFactory(name, name, relativeError, numBins);
+  }
+
+  @Override
+  public AggregatorFactory getMergingFactory(AggregatorFactory other) throws AggregatorFactoryNotMergeableException
+  {
+    if (other.getName().equals(this.getName()) && this.getClass() == other.getClass()) {
+      return getCombiningFactory();
+    } else {
+      throw new AggregatorFactoryNotMergeableException(this, other);
+    }
+  }
+
+  @Override
+  public List<AggregatorFactory> getRequiredColumns()
+  {
+    return Collections.singletonList(
+        new DDSketchAggregatorFactory(
+            name,
+            fieldName,
+            relativeError,
+            numBins
+        )
+    );
+  }
+
+  @Override
+  public Object deserialize(Object serializedSketch)
+  {
+    return DDSketchUtils.deserialize(serializedSketch);
+  }
+
+  @Nullable
+  @Override
+  public Object finalizeComputation(@Nullable Object object)
+  {
+    return object == null ? null : ((DDSketch) object).getCount();
+  }
+
+  @Override
+  @JsonProperty
+  public String getName()
+  {
+    return name;
+  }
+
+  @JsonProperty
+  public String getFieldName()
+  {
+    return fieldName;
+  }
+
+  @JsonProperty
+  public double getRelativeError()
+  {
+    return relativeError;
+  }
+
+  @JsonProperty
+  public int getNumBins()
+  {
+    return numBins;
+  }
+
+  @Override
+  public List<String> requiredFields()
+  {
+    return Collections.singletonList(fieldName);
+  }
+
+  /**
+   * actual type is {@link DDSketch}
+   */
+  @Override
+  public ColumnType getIntermediateType()
+  {
+    return TYPE;
+  }
+
+  @Override
+  public ColumnType getResultType()
+  {
+    return TYPE;
+  }
+
+  // The bounded lower collapsing yields a size of roughly numBins * 8 in terms of size.
+  // Since negative and positive value stores exist per sketch, we multiply this by 2.
+  // then we add 1k for measure.
+  @Override
+  public int getMaxIntermediateSize()
+  {
+    return numBins * 8 * 2 + 1000;
+  }
+
+  @Override
+  public AggregatorFactory withName(String newName)
+  {
+    return new DDSketchAggregatorFactory(newName, getFieldName(), getRelativeError(), getNumBins(), cacheTypeId);
+  }
+
+  @Override
+  public AggregateCombiner<DDSketch> makeAggregateCombiner()
+  {
+    return new ObjectAggregateCombiner<DDSketch>()
+    {
+      private DDSketch combined = DDSketches.collapsingLowestDense(relativeError, numBins);
+
+      @Override
+      public void reset(final ColumnValueSelector selector)
+      {
+        combined.clear();
+        fold(selector);
+      }
+
+      @Override
+      public void fold(final ColumnValueSelector selector)
+      {
+        DDSketch other = (DDSketch) selector.getObject();
+        if (other == null) {
+          return;
+        }
+        if (combined == null) {
+          combined = DDSketches.collapsingLowestDense(relativeError, numBins);
+        }
+        combined.mergeWith(other);
+      }
+
+      @Nullable
+      @Override
+      public DDSketch getObject()
+      {
+        return combined;
+      }
+
+      @Override
+      public Class<DDSketch> classOfObject()
+      {
+        return DDSketch.class;
+      }
+    };
+  }
+
+  @Override
+  public boolean equals(final Object o)
+  {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || !getClass().equals(o.getClass())) {
+      return false;
+    }
+    final DDSketchAggregatorFactory that = (DDSketchAggregatorFactory) o;
+
+    return Objects.equals(name, that.name) &&
+           Objects.equals(fieldName, that.fieldName) &&
+           relativeError == that.relativeError &&
+           numBins == that.numBins;
+  }
+
+  @Override
+  public int hashCode()
+  {
+    return Objects.hash(name, fieldName, relativeError, numBins);
+  }
+
+  @Override
+  public String toString()
+  {
+    return getClass().getSimpleName() + "{"
+           + "name=" + name
+           + ", fieldName=" + fieldName
+           + ", relativeError=" + relativeError
+           + ", numBins=" + numBins
+           + "}";
+  }
+}
