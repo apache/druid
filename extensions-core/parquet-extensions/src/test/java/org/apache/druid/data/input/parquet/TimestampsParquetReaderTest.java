e0d06071297b805111bd35a676dc99bd8588799e
ycp2 <ycp2@fa23-cs527-045.cs.illinois.edu>
2023-09-28 17:09:27 -0500

    Matching java format

--- extensions-core/parquet-extensions/src/test/java/org/apache/druid/data/input/parquet/TimestampsParquetReaderTest.java
+++ extensions-core/parquet-extensions/src/test/java/org/apache/druid/data/input/parquet/TimestampsParquetReaderTest.java
@@ -94,11 +94,11 @@ public class TimestampsParquetReaderTest extends BaseParquetReaderTest
                                       + "  \"idx\" : 1,\n"
                                       + "  \"date_as_date\" : 1497744000000\n"
                                       + "}";
-    try{
+    try {
         JSONAssert.assertEquals(expectedJson, DEFAULT_JSON_WRITER.writeValueAsString(sampledAsString.get(0).getRawValues()), false);
         JSONAssert.assertEquals(expectedJson, DEFAULT_JSON_WRITER.writeValueAsString(sampledAsDate.get(0).getRawValues()), false);
-    } catch (JSONException jse){
-        Assert.fail("Not comparing JSON strings");
+    } catch (JSONException jse) {
+        Assert.fail("Not comparing JSON strings. Error: " + jse.getMessage());
     }
   }
 
