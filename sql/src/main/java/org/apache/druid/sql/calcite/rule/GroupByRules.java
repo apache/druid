284767a567b00c685b7722dfb7122c89f507055f
Zoltan Haindrich <zoltan.haindrich@imply.io>
2023-10-03 15:02:49 +0000

    x

--- sql/src/main/java/org/apache/druid/sql/calcite/rule/GroupByRules.java
+++ sql/src/main/java/org/apache/druid/sql/calcite/rule/GroupByRules.java
@@ -20,7 +20,6 @@
 package org.apache.druid.sql.calcite.rule;
 
 import org.apache.calcite.rel.core.AggregateCall;
-import org.apache.calcite.rel.core.Project;
 import org.apache.calcite.rex.RexBuilder;
 import org.apache.calcite.rex.RexNode;
 import org.apache.druid.query.aggregation.AggregatorFactory;
