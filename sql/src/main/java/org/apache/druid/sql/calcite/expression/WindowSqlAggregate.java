3360c8cc7518048ebbaba29f8b0c9b21b474b5ed
Zoltan Haindrich <zoltan.haindrich@imply.io>
2023-10-03 15:55:27 +0000

    finish core aggs

--- sql/src/main/java/org/apache/druid/sql/calcite/expression/WindowSqlAggregate.java
+++ sql/src/main/java/org/apache/druid/sql/calcite/expression/WindowSqlAggregate.java
@@ -20,7 +20,6 @@
 package org.apache.druid.sql.calcite.expression;
 
 import org.apache.calcite.rel.core.AggregateCall;
-import org.apache.calcite.rel.core.Project;
 import org.apache.calcite.rex.RexBuilder;
 import org.apache.calcite.sql.SqlAggFunction;
 import org.apache.druid.java.util.common.UOE;
@@ -28,6 +27,7 @@ import org.apache.druid.segment.column.RowSignature;
 import org.apache.druid.sql.calcite.aggregation.Aggregation;
 import org.apache.druid.sql.calcite.aggregation.SqlAggregator;
 import org.apache.druid.sql.calcite.planner.PlannerContext;
+import org.apache.druid.sql.calcite.rel.InputAccessor;
 import org.apache.druid.sql.calcite.rel.VirtualColumnRegistry;
 
 import javax.annotation.Nullable;
@@ -60,7 +60,7 @@ public class WindowSqlAggregate implements SqlAggregator
       RexBuilder rexBuilder,
       String name,
       AggregateCall aggregateCall,
-      Project project,
+      InputAccessor inputAccessor,
       List<Aggregation> existingAggregations,
       boolean finalizeAggregations
   )
