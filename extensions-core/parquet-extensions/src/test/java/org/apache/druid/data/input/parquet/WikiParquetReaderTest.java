98f6b098c6f41b7e67acbccf0cc013c718212b1c
Rette66 <wenxiuw2@illinois.edu>
2023-10-03 21:50:38 -0500

    fix testWIKI test

--- extensions-core/parquet-extensions/src/test/java/org/apache/druid/data/input/parquet/WikiParquetReaderTest.java
+++ extensions-core/parquet-extensions/src/test/java/org/apache/druid/data/input/parquet/WikiParquetReaderTest.java
@@ -62,7 +62,7 @@ public class WikiParquetReaderTest extends BaseParquetReaderTest
     reader = createReader("example/wiki/wiki.parquet", schema, JSONPathSpec.DEFAULT);
     List<InputRowListPlusRawValues> sampled = sampleAllRows(reader);
 
-    final String expectedJson = "{\n"
+    final String expectedJsonString = "{\n"
                                 + "  \"continent\" : \"North America\",\n"
                                 + "  \"country\" : \"United States\",\n"
                                 + "  \"added\" : 57,\n"
