cdfc3f2bfea8ef6e9036adca8d7648e8f91d1cae
Zoltan Haindrich <zoltan.haindrich@imply.io>
2023-10-09 16:24:07 +0000

    undo windowing changes

--- sql/src/main/java/org/apache/druid/sql/calcite/rel/Windowing.java
+++ sql/src/main/java/org/apache/druid/sql/calcite/rel/Windowing.java
@@ -172,16 +172,10 @@ public class Windowing
 
         ProcessorMaker maker = KNOWN_WINDOW_FNS.get(aggregateCall.getAggregation().getName());
         if (maker == null) {
-            VirtualColumnRegistry virtualColumnRegistry = VirtualColumnRegistry.create(
-                sourceRowSignature,
-                plannerContext.getExpressionParser(),
-                plannerContext.getPlannerConfig().isForceExpressionVirtualColumns()
-            );
-
           final Aggregation aggregation = GroupByRules.translateAggregateCall(
               plannerContext,
               sourceRowSignature,
-              virtualColumnRegistry,
+              null,
               rexBuilder,
               InputAccessor.buildFor(
                   rexBuilder,
