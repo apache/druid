b09e30067e1c32e63cd01e5adcefc8f7d253b144
Laksh Singla <lakshsingla@gmail.com>
2023-10-09 11:43:26 +0530

    readd decoupled ignore

--- sql/src/test/java/org/apache/druid/sql/calcite/CalciteQueryTest.java
+++ sql/src/test/java/org/apache/druid/sql/calcite/CalciteQueryTest.java
@@ -3246,6 +3246,7 @@ public class CalciteQueryTest extends BaseCalciteQueryTest
     );
   }
 
+  @DecoupledIgnore(mode = Modes.NOT_ENOUGH_RULES)
   @Test
   public void testExactCountDistinctUsingSubqueryOnUnionAllTables()
   {
