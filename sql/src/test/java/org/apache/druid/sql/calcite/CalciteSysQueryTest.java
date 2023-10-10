23605c1edddd697e584e41965d44ffc2d8866396
Zoltan Haindrich <zoltan.haindrich@imply.io>
2023-10-10 11:10:50 +0200

    Enable resultset validation of Drill tests (#15096)
    - introduces a test_X method for every testcase (995 testcases)
    - added a resultset parser which reads the expected resultset based on the result schema
    - loaded a few more datasets
    - added a testcase to ensure that all files have a corresponding testcase
    - renamed DecoupledIgnore to NegativeTest
    - categorized the failing 268 tests

--- sql/src/test/java/org/apache/druid/sql/calcite/CalciteSysQueryTest.java
+++ sql/src/test/java/org/apache/druid/sql/calcite/CalciteSysQueryTest.java
@@ -20,15 +20,15 @@
 package org.apache.druid.sql.calcite;
 
 import com.google.common.collect.ImmutableList;
-import org.apache.druid.sql.calcite.DecoupledIgnore.DecoupledIgnoreProcessor;
-import org.apache.druid.sql.calcite.DecoupledIgnore.Modes;
+import org.apache.druid.sql.calcite.NotYetSupported.Modes;
+import org.apache.druid.sql.calcite.NotYetSupported.NotYetSupportedProcessor;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class CalciteSysQueryTest extends BaseCalciteQueryTest
 {
   @Rule(order = 0)
-  public DecoupledIgnoreProcessor decoupledIgnoreProcessor = new DecoupledIgnoreProcessor();
+  public NotYetSupportedProcessor NegativeTestProcessor = new NotYetSupportedProcessor();
 
   @Test
   public void testTasksSum()
@@ -46,7 +46,7 @@ public class CalciteSysQueryTest extends BaseCalciteQueryTest
         .run();
   }
 
-  @DecoupledIgnore(mode = Modes.EXPRESSION_NOT_GROUPED)
+  @NotYetSupported(Modes.EXPRESSION_NOT_GROUPED)
   @Test
   public void testTasksSumOver()
   {
