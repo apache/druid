ce919f43565378a471383c9940f0fc8aa67cf99e
Zoltan Haindrich <zoltan.haindrich@imply.io>
2023-10-20 04:45:18 +0000

    checkstyle

--- processing/src/main/java/org/apache/druid/query/rowsandcols/LazilyDecoratedRowsAndColumns.java
+++ processing/src/main/java/org/apache/druid/query/rowsandcols/LazilyDecoratedRowsAndColumns.java
@@ -218,7 +218,7 @@ public class LazilyDecoratedRowsAndColumns implements RowsAndColumns
       if (virtualColumns == null) {
         cols = base.getColumnNames();
       } else {
-        cols = ImmutableList.<String> builder()
+        cols = ImmutableList.<String>builder()
             .addAll(base.getColumnNames())
             .addAll(virtualColumns.getColumnNames())
             .build();
