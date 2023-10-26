b10e0002258fcefe52a20bdd3d01272b8e5657b3
same8891 <55152301+same8891@users.noreply.github.com>
2023-10-26 16:33:40 -0500

    Update ParametrizedUriEmitterTest.java

--- processing/src/test/java/org/apache/druid/java/util/emitter/core/ParametrizedUriEmitterTest.java
+++ processing/src/test/java/org/apache/druid/java/util/emitter/core/ParametrizedUriEmitterTest.java
@@ -18,6 +18,7 @@
  */
 
 package org.apache.druid.java.util.emitter.core;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
