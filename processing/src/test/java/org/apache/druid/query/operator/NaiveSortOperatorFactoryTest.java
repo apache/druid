53811437bcf387c7d813277ce5e7dce37ae83bd7
Zoltan Haindrich <zoltan.haindrich@imply.io>
2023-10-27 10:06:31 +0000

    implement test

--- processing/src/test/java/org/apache/druid/query/operator/NaiveSortOperatorFactoryTest.java
+++ processing/src/test/java/org/apache/druid/query/operator/NaiveSortOperatorFactoryTest.java
@@ -19,7 +19,43 @@
 
 package org.apache.druid.query.operator;
 
+import com.google.common.collect.Lists;
+import com.google.common.testing.EqualsTester;
+import org.apache.druid.query.operator.ColumnWithDirection.Direction;
+import org.junit.Test;
+
+import java.util.Collections;
+
 public class NaiveSortOperatorFactoryTest
 {
-}
+  @Test
+  public void testEquals()
+  {
+    ColumnWithDirection colA = new ColumnWithDirection("a", Direction.ASC);
+    ColumnWithDirection colAdesc = new ColumnWithDirection("a", Direction.DESC);
+    ColumnWithDirection colB = new ColumnWithDirection("b", Direction.ASC);
 
+    new EqualsTester()
+        .addEqualityGroup(
+            new NaiveSortOperatorFactory(Collections.emptyList()),
+            new NaiveSortOperatorFactory(null))
+        .addEqualityGroup(
+            new NaiveSortOperatorFactory(Lists.newArrayList(colA)),
+            new NaiveSortOperatorFactory(Lists.newArrayList(colA)))
+        .addEqualityGroup(
+            new NaiveSortOperatorFactory(Lists.newArrayList(colAdesc)))
+        .addEqualityGroup(
+            new NaiveSortOperatorFactory(Lists.newArrayList(colB)))
+        .addEqualityGroup(
+            new NaiveSortOperatorFactory(Lists.newArrayList(colA, colB)))
+        .addEqualityGroup(
+            new NaiveSortOperatorFactory(Lists.newArrayList(colB, colA)))
+        // invalid cases; but currently allowed ones
+        .addEqualityGroup(
+            new NaiveSortOperatorFactory(Lists.newArrayList((ColumnWithDirection) null)))
+        .addEqualityGroup(
+            new NaiveSortOperatorFactory(Lists.newArrayList(colA, colB, colA)))
+        .testEquals();
+  }
+
+}
