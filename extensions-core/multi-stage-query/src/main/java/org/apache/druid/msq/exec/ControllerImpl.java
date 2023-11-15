2e79fd56a7ff0ad91d4945b49924be67d9f80e83
Abhishek Radhakrishnan <abhishek.rb19@gmail.com>
2023-11-14 23:35:36 -0800

    MSQ generates tombstones honoring granularity specified in a `REPLACE` query. (#15243)
    * MSQ generates tombstones honoring the query's granularity.
    
    This change tweaks to only account for the infinite-interval tombstones.
    For finite-interval tombstones, the MSQ query granualrity will be used
    which is consistent with how MSQ works.
    
    * more tests and some cleanup.
    
    * checkstyle
    
    * comment edits
    
    * Throw TooManyBuckets fault based on review; add more tests.
    
    * Add javadocs for both methods on reconciling the methods.
    
    * review: Move testReplaceTombstonesWithTooManyBucketsThrowsException to MsqFaultsTest
    
    * remove unused imports.
    
    * Move TooManyBucketsException to indexing package for shared exception handling.
    
    * lower max bucket for tests and fixup count
    
    * Advance and count the iterator.
    
    * checkstyle

--- extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/exec/ControllerImpl.java
+++ extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/exec/ControllerImpl.java
@@ -75,6 +75,7 @@ import org.apache.druid.indexing.common.actions.SegmentTransactionalInsertAction
 import org.apache.druid.indexing.common.actions.SegmentTransactionalReplaceAction;
 import org.apache.druid.indexing.common.actions.TaskAction;
 import org.apache.druid.indexing.common.actions.TaskActionClient;
+import org.apache.druid.indexing.common.task.batch.TooManyBucketsException;
 import org.apache.druid.indexing.common.task.batch.parallel.TombstoneHelper;
 import org.apache.druid.indexing.overlord.SegmentPublishResult;
 import org.apache.druid.java.util.common.DateTimes;
@@ -121,6 +122,7 @@ import org.apache.druid.msq.indexing.error.MSQFaultUtils;
 import org.apache.druid.msq.indexing.error.MSQWarningReportLimiterPublisher;
 import org.apache.druid.msq.indexing.error.MSQWarnings;
 import org.apache.druid.msq.indexing.error.QueryNotSupportedFault;
+import org.apache.druid.msq.indexing.error.TooManyBucketsFault;
 import org.apache.druid.msq.indexing.error.TooManyWarningsFault;
 import org.apache.druid.msq.indexing.error.UnknownFault;
 import org.apache.druid.msq.indexing.error.WorkerRpcFailedFault;
@@ -1423,13 +1425,17 @@ public class ControllerImpl implements Controller
               intervalsToDrop,
               destination.getReplaceTimeChunks(),
               task.getDataSource(),
-              destination.getSegmentGranularity()
+              destination.getSegmentGranularity(),
+              Limits.MAX_PARTITION_BUCKETS
           );
           segmentsWithTombstones.addAll(tombstones);
           numTombstones = tombstones.size();
         }
         catch (IllegalStateException e) {
           throw new MSQException(e, InsertLockPreemptedFault.instance());
+        }
+        catch (TooManyBucketsException e) {
+          throw new MSQException(e, new TooManyBucketsFault(Limits.MAX_PARTITION_BUCKETS));
         }
       }
 
