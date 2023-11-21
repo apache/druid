a5c42162aab708b56aaae877997d141da71e0196
Zoltan Haindrich <kirk@rxd.hu>
2023-11-21 16:28:00 +0000

    planB

--- processing/src/main/java/org/apache/druid/query/CacheStrategy.java
+++ processing/src/main/java/org/apache/druid/query/CacheStrategy.java
@@ -24,8 +24,6 @@ import com.google.common.base.Function;
 import org.apache.druid.guice.annotations.ExtensionPoint;
 import org.apache.druid.java.util.common.ISE;
 import org.apache.druid.query.aggregation.AggregatorFactory;
-import org.apache.druid.segment.column.ColumnType;
-
 import java.util.Iterator;
 import java.util.List;
 
@@ -158,16 +156,16 @@ public interface CacheStrategy<T, CacheType, QueryType extends Query<T>>
 
 
       if (isResultLevelCache) {
-        ColumnType resultType = aggregator.getResultType();
-        ColumnType intermediateType = aggregator.getIntermediateType();
-        if(!resultType.isPrimitive() && resultType.equals(intermediateType)) {
-          addToResultFunction.apply(aggregator.getName(), i, aggregator.deserialize(resultIter.next()));
-
-        }else {
-        // aggregator - type complex -> aggregator.deserialize
-                       // else leave alone
+//        ColumnType resultType = aggregator.getResultType();
+//        ColumnType intermediateType = aggregator.getIntermediateType();
+//        if(!resultType.isPrimitive() && resultType.equals(intermediateType)) {
+//          addToResultFunction.apply(aggregator.getName(), i, aggregator.deserialize(resultIter.next()));
+//
+//        }else {
+//        // aggregator - type complex -> aggregator.deserialize
+//                       // else leave alone
         addToResultFunction.apply(aggregator.getName(), i, resultIter.next());
-        }
+//        }
       } else {
         addToResultFunction.apply(aggregator.getName(), i, aggregator.deserialize(resultIter.next()));
       }
