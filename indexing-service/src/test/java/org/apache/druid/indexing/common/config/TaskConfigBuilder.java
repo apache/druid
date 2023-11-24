ca12664621e2ecc5b1da7c17e9f5a331b2bf144a
cryptoe <karankumar1100@gmail.com>
2023-11-24 21:52:56 +0530

    Fixing build

--- indexing-service/src/test/java/org/apache/druid/indexing/common/config/TaskConfigBuilder.java
+++ indexing-service/src/test/java/org/apache/druid/indexing/common/config/TaskConfigBuilder.java
@@ -42,6 +42,8 @@ public class TaskConfigBuilder
   private boolean enableTaskLevelLogPush;
   private Long tmpStorageBytesPerTask;
 
+  private Boolean enableConcurrentAppendAndReplace;
+
   public TaskConfigBuilder setBaseDir(String baseDir)
   {
     this.baseDir = baseDir;
@@ -132,6 +134,18 @@ public class TaskConfigBuilder
     return this;
   }
 
+  public TaskConfigBuilder enableConcurrentAppendAndReplace()
+  {
+    this.enableConcurrentAppendAndReplace = true;
+    return this;
+  }
+
+  public TaskConfigBuilder disableConcurrentAppendAndReplace()
+  {
+    this.enableConcurrentAppendAndReplace = false;
+    return this;
+  }
+
   public TaskConfig build()
   {
     return new TaskConfig(
@@ -149,7 +163,8 @@ public class TaskConfigBuilder
         batchProcessingMode,
         storeEmptyColumns,
         enableTaskLevelLogPush,
-        tmpStorageBytesPerTask
+        tmpStorageBytesPerTask,
+        enableConcurrentAppendAndReplace
     );
   }
 }
