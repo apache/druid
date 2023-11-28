eb056e23b5e7d9ff2b1ffc3d8731c818eae4fade
Zoltan Haindrich <kirk@rxd.hu>
2023-11-28 14:19:09 +0100

    Fix dictionarySize overrides in tests (#15354)
    I think this is a problem as it discards the false return value when the putToKeyBuffer can't store the value because of the limit
    
    Not forwarding the return value at that point may lead to the normal continuation here regardless something was not added to the dictionary like here

--- sql/src/test/java/org/apache/druid/sql/calcite/CalciteQueryTest.java
+++ sql/src/test/java/org/apache/druid/sql/calcite/CalciteQueryTest.java
@@ -14396,7 +14396,7 @@ public class CalciteQueryTest extends BaseCalciteQueryTest
         .sql("SELECT dim1,ROW_NUMBER() OVER () from druid.foo")
         .run());
 
-    assertThat(e, invalidSqlIs("The query contains window functions; To run these window functions, enable [WINDOW_FUNCTIONS] in query context. (line [1], column [13])"));
+    assertThat(e, invalidSqlIs("The query contains window functions; To run these window functions, specify [enableWindowing] in query context. (line [1], column [13])"));
   }
 
   @Test
