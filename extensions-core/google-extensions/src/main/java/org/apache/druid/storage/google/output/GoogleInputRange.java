3bd31256cdbd9ad78dc18101642ed53242ae4f99
Vishesh Garg <gargvishesh@gmail.com>
2023-12-13 10:21:33 +0530

    Add toString to GoogleInputRange

--- extensions-core/google-extensions/src/main/java/org/apache/druid/storage/google/output/GoogleInputRange.java
+++ extensions-core/google-extensions/src/main/java/org/apache/druid/storage/google/output/GoogleInputRange.java
@@ -77,4 +77,15 @@ public class GoogleInputRange
   {
     return Objects.hash(start, size, bucket, path);
   }
+
+  @Override
+  public String toString()
+  {
+    return "GoogleInputRange{" +
+           "start=" + start +
+           ", size=" + size +
+           ", bucket='" + bucket + '\'' +
+           ", path='" + path + '\'' +
+           '}';
+  }
 }
