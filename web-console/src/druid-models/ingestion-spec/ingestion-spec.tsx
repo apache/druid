935582c60188017fbda1c5153486dd67a0e2cec1
Abhishek Balaji Radhakrishnan <abhishek.rb19@gmail.com>
2023-12-20 04:12:29 -0500

    Web console changes to show Delta Lake

--- web-console/src/druid-models/ingestion-spec/ingestion-spec.tsx
+++ web-console/src/druid-models/ingestion-spec/ingestion-spec.tsx
@@ -98,6 +98,7 @@ export type IngestionComboType =
   | 'index_parallel:inline'
   | 'index_parallel:s3'
   | 'index_parallel:azure'
+  | 'index_parallel:delta'
   | 'index_parallel:google'
   | 'index_parallel:hdfs';
 
@@ -136,6 +137,7 @@ export function getIngestionComboType(
       switch (inputSource.type) {
         case 'local':
         case 'http':
+        case 'deltaLake':
         case 'druid':
         case 'inline':
         case 's3':
@@ -169,6 +171,9 @@ export function getIngestionTitle(ingestionType: IngestionComboTypeWithExtra): s
 
     case 'index_parallel:azure':
       return 'Azure Data Lake';
+
+    case 'index_parallel:delta':
+      return 'Delta Lake';
 
     case 'index_parallel:google':
       return 'Google Cloud Storage';
@@ -459,7 +464,7 @@ export function getIoConfigFormFields(ingestionComboType: IngestionComboType): F
     name: 'inputSource.type',
     label: 'Source type',
     type: 'string',
-    suggestions: ['local', 'http', 'inline', 's3', 'azure', 'google', 'hdfs'],
+    suggestions: ['local', 'http', 'inline', 'delta', 's3', 'azure', 'google', 'hdfs'],
     info: (
       <p>
         Druid connects to raw data through{' '}
@@ -892,6 +897,18 @@ export function getIoConfigFormFields(ingestionComboType: IngestionComboType): F
         inputSourceFilter,
       ];
 
+    case 'index_parallel:delta':
+      return [
+        inputSourceType,
+        {
+          name: 'inputSource.tablePath',
+          label: 'Delta table path',
+          type: 'string',
+          placeholder: '/path/to/deltaTable',
+          required: true,
+        },
+      ];
+
     case 'index_parallel:hdfs':
       return [
         inputSourceType,
@@ -1082,6 +1099,7 @@ export function getIoConfigTuningFormFields(
     case 'index_parallel:s3':
     case 'index_parallel:azure':
     case 'index_parallel:google':
+    case 'index_parallel:delta':
     case 'index_parallel:hdfs':
       return [
         {
