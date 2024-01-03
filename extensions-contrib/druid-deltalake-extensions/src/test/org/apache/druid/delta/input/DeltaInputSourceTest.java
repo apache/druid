68e8dcb9deead70a925cb9c77b8744eccf2b790e
Abhishek Balaji Radhakrishnan <abhishek.rb19@gmail.com>
2024-01-03 12:49:38 -0800

    Test init

---
+++ extensions-contrib/druid-deltalake-extensions/src/test/org/apache/druid/delta/input/DeltaInputSourceTest.java
@@ -0,0 +1,16 @@
+package org.apache.druid.delta.input;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DeltaInputSourceTest
+{
+
+  @Test
+  public void testInputSourceInit()
+  {
+
+  }
+
+}
