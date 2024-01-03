b0e52c99bbc248d54b43e5f06880a4c13be00383
Gian Merlino <gianmerlino@gmail.com>
2024-01-02 21:09:18 -0800

    Fix ColumnSelectorColumnIndexSelector#getColumnCapabilities. (#15614)
    * Fix ColumnSelectorColumnIndexSelector#getColumnCapabilities.
    
    It was using virtualColumns.getColumnCapabilities, which only returns
    capabilities for virtual columns, not regular columns. The effect of this
    is that expression filters (and in some cases, arrayContainsElement filters)
    would build value matchers rather than use indexes.
    
    I think this has been like this since #12315, which added the
    getColumnCapabilities method to BitmapIndexSelector, and included the same
    implementation as exists in the code today.
    
    This error is easy to make due to the design of virtualColumns.getColumnCapabilities,
    so to help avoid it in the future, this patch renames the method to
    getColumnCapabilitiesWithoutFallback to emphasize that it does not return
    capabilities for regular columns.
    
    * Make getColumnCapabilitiesWithoutFallback package-private.
    
    * Fix expression filter bitmap usage.

--- processing/src/main/java/org/apache/druid/segment/VirtualColumns.java
+++ processing/src/main/java/org/apache/druid/segment/VirtualColumns.java
@@ -394,8 +394,12 @@ public class VirtualColumns implements Cacheable
     return virtualColumn.makeVectorObjectSelector(columnName, columnSelector, offset);
   }
 
+  /**
+   * Get capabilities for the virtual column "columnName". If columnName is not a virtual column, returns null.
+   * Package-private since production callers want {@link #getColumnCapabilitiesWithFallback(ColumnInspector, String)}.
+   */
   @Nullable
-  public ColumnCapabilities getColumnCapabilities(ColumnInspector inspector, String columnName)
+  ColumnCapabilities getColumnCapabilitiesWithoutFallback(ColumnInspector inspector, String columnName)
   {
     final VirtualColumn virtualColumn = getVirtualColumn(columnName);
     if (virtualColumn != null) {
@@ -405,10 +409,14 @@ public class VirtualColumns implements Cacheable
     }
   }
 
+  /**
+   * Get capabilities for the column "columnName". If columnName is not a virtual column, delegates to the
+   * provided {@link ColumnInspector}.
+   */
   @Nullable
   public ColumnCapabilities getColumnCapabilitiesWithFallback(ColumnInspector inspector, String columnName)
   {
-    final ColumnCapabilities virtualColumnCapabilities = getColumnCapabilities(inspector, columnName);
+    final ColumnCapabilities virtualColumnCapabilities = getColumnCapabilitiesWithoutFallback(inspector, columnName);
     if (virtualColumnCapabilities != null) {
       return virtualColumnCapabilities;
     } else {
