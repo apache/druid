f2804809eb6bb201b0cc12854298ecb376966ebf
Abhishek Balaji Radhakrishnan <abhishek.rb19@gmail.com>
2024-01-03 12:25:09 -0800

    Add InputSource, module init and helper code to process delta files.

---
+++ extensions-contrib/druid-deltalake-extensions/src/main/java/org/apache/druid/delta/input/DeltaInputRow.java
@@ -0,0 +1,197 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.delta.input;
+
+import io.delta.kernel.types.ArrayType;
+import io.delta.kernel.types.BinaryType;
+import io.delta.kernel.types.BooleanType;
+import io.delta.kernel.types.ByteType;
+import io.delta.kernel.types.DataType;
+import io.delta.kernel.types.DateType;
+import io.delta.kernel.types.DecimalType;
+import io.delta.kernel.types.DoubleType;
+import io.delta.kernel.types.FloatType;
+import io.delta.kernel.types.IntegerType;
+import io.delta.kernel.types.LongType;
+import io.delta.kernel.types.MapType;
+import io.delta.kernel.types.ShortType;
+import io.delta.kernel.types.StringType;
+import io.delta.kernel.types.StructField;
+import io.delta.kernel.types.StructType;
+import io.delta.kernel.types.TimestampType;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+import org.apache.druid.data.input.InputRow;
+import org.apache.druid.data.input.InputRowSchema;
+import org.apache.druid.data.input.Row;
+import org.apache.druid.data.input.impl.MapInputRowParser;
+import org.joda.time.DateTime;
+
+import javax.annotation.Nullable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DeltaInputRow implements InputRow
+{
+  private final io.delta.kernel.data.Row row;
+  private final StructType schema;
+  private final Object2IntMap<String> fieldNameToOrdinal = new Object2IntOpenHashMap<>();
+  private static final ZoneId ZONE_ID = ZoneId.systemDefault(); // TIMEZONE HANDLING?????
+  private final InputRow delegateRow;
+
+  public DeltaInputRow(io.delta.kernel.data.Row row, InputRowSchema inputRowSchema)
+  {
+    this.row = row;
+    this.schema = row.getSchema();
+    List<String> fieldNames = this.schema.fieldNames();
+    for (int i = 0; i < fieldNames.size(); ++i) {
+      fieldNameToOrdinal.put(fieldNames.get(i), i);
+    }
+    fieldNameToOrdinal.defaultReturnValue(-1);
+
+    Map<String, Object> theMap = new HashMap<>();
+    for (String fieldName : fieldNames) {
+      theMap.put(fieldName, _getRaw(fieldName));
+    }
+    delegateRow = MapInputRowParser.parse(inputRowSchema, theMap);
+
+  }
+
+  @Override
+  public List<String> getDimensions()
+  {
+    return delegateRow.getDimensions();
+  }
+
+  @Override
+  public long getTimestampFromEpoch()
+  {
+    return delegateRow.getTimestampFromEpoch();
+  }
+
+  @Override
+  public DateTime getTimestamp()
+  {
+    return delegateRow.getTimestamp();
+  }
+
+  @Override
+  public List<String> getDimension(String dimension)
+  {
+    return delegateRow.getDimension(dimension);
+  }
+
+  @Nullable
+  @Override
+  public Object getRaw(String dimension)
+  {
+    return delegateRow.getRaw(dimension);
+  }
+
+  @Nullable
+  public Object _getRaw(String dimension)
+  {
+    StructField field = schema.get(dimension);
+    if (field == null) {
+      return null;
+    } else if (field.isMetadataColumn()) {
+      return null;
+    }
+
+
+    int ordinal = fieldNameToOrdinal.getInt(dimension);
+    if (ordinal < 0) {
+      return null;
+    }
+    return getValue(field.getDataType(), row, ordinal);
+
+  }
+
+  @Nullable
+  @Override
+  public Number getMetric(String metric)
+  {
+    return delegateRow.getMetric(metric);
+  }
+
+  @Override
+  public int compareTo(Row o)
+  {
+    return this.getTimestamp().compareTo(o.getTimestamp());
+  }
+
+  @Nullable
+  private static Object getValue(DataType dataType, io.delta.kernel.data.Row dataRow, int columnOrdinal)
+  {
+    if (dataRow.isNullAt(columnOrdinal)) {
+      return null;
+    } else if (dataType instanceof BooleanType) {
+      return dataRow.getBoolean(columnOrdinal);
+    } else if (dataType instanceof ByteType) {
+      return dataRow.getByte(columnOrdinal);
+    } else if (dataType instanceof ShortType) {
+      return dataRow.getShort(columnOrdinal);
+    } else if (dataType instanceof IntegerType) {
+      return dataRow.getInt(columnOrdinal);
+    } else if (dataType instanceof DateType) {
+      // DateType data is stored internally as the number of days since 1970-01-01
+      int daysSinceEpochUTC = dataRow.getInt(columnOrdinal);
+      return LocalDate.ofEpochDay(daysSinceEpochUTC).atStartOfDay(ZONE_ID).toEpochSecond();
+    } else if (dataType instanceof LongType) {
+      return dataRow.getLong(columnOrdinal);
+    } else if (dataType instanceof TimestampType) {
+      // TimestampType data is stored internally as the number of microseconds since epoch
+      long microSecsSinceEpochUTC = dataRow.getLong(columnOrdinal);
+      LocalDateTime dateTime = LocalDateTime.ofEpochSecond(
+          microSecsSinceEpochUTC / 1_000_000 /* epochSecond */,
+          (int) (1000 * microSecsSinceEpochUTC % 1_000_000) /* nanoOfSecond */,
+          ZoneOffset.UTC);
+      return dateTime.atZone(ZONE_ID).toInstant().toEpochMilli();
+    } else if (dataType instanceof FloatType) {
+      return dataRow.getFloat(columnOrdinal);
+    } else if (dataType instanceof DoubleType) {
+      return dataRow.getDouble(columnOrdinal);
+    } else if (dataType instanceof StringType) {
+      return dataRow.getString(columnOrdinal);
+    } else if (dataType instanceof BinaryType) {
+      return new String(dataRow.getBinary(columnOrdinal));
+    } else if (dataType instanceof DecimalType) {
+      return dataRow.getDecimal(columnOrdinal).longValue();
+    } else if (dataType instanceof StructType) {
+      return "TODO: struct value";
+    } else if (dataType instanceof ArrayType) {
+      return "TODO: list value";
+    } else if (dataType instanceof MapType) {
+      return "TODO: map value";
+    } else {
+      throw new UnsupportedOperationException("unsupported data type: " + dataType);
+    }
+  }
+
+  public Map<String, Object> getRawRowAsMap()
+  {
+    return RowSerde.convertRowToJsonObject(row);
+  }
+}
---
+++ extensions-contrib/druid-deltalake-extensions/src/main/java/org/apache/druid/delta/input/DeltaInputSourceReader.java
@@ -0,0 +1,207 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.delta.input;
+
+import io.delta.kernel.data.FilteredColumnarBatch;
+import io.delta.kernel.data.Row;
+import io.delta.kernel.types.ArrayType;
+import io.delta.kernel.types.BinaryType;
+import io.delta.kernel.types.BooleanType;
+import io.delta.kernel.types.ByteType;
+import io.delta.kernel.types.DataType;
+import io.delta.kernel.types.DateType;
+import io.delta.kernel.types.DecimalType;
+import io.delta.kernel.types.DoubleType;
+import io.delta.kernel.types.FloatType;
+import io.delta.kernel.types.IntegerType;
+import io.delta.kernel.types.LongType;
+import io.delta.kernel.types.MapType;
+import io.delta.kernel.types.ShortType;
+import io.delta.kernel.types.StringType;
+import io.delta.kernel.types.StructType;
+import io.delta.kernel.types.TimestampType;
+import org.apache.druid.data.input.InputRow;
+import org.apache.druid.data.input.InputRowListPlusRawValues;
+import org.apache.druid.data.input.InputRowSchema;
+import org.apache.druid.data.input.InputSourceReader;
+import org.apache.druid.data.input.InputStats;
+import org.apache.druid.java.util.common.logger.Logger;
+import org.apache.druid.java.util.common.parsers.CloseableIterator;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.NoSuchElementException;
+
+public class DeltaInputSourceReader implements InputSourceReader
+{
+  private static final Logger log = new Logger(DeltaInputSourceReader.class);
+
+  private final io.delta.kernel.utils.CloseableIterator<FilteredColumnarBatch> filteredColumnarBatchCloseableIterator;
+  private final InputRowSchema inputRowSchema;
+
+  public DeltaInputSourceReader(
+      io.delta.kernel.utils.CloseableIterator<FilteredColumnarBatch> filteredColumnarBatchCloseableIterator,
+      InputRowSchema inputRowSchema
+  )
+  {
+    this.filteredColumnarBatchCloseableIterator = filteredColumnarBatchCloseableIterator;
+    this.inputRowSchema = inputRowSchema;
+  }
+
+  @Override
+  public CloseableIterator<InputRow> read()
+  {
+    return new DeltaInputSourceIterator(filteredColumnarBatchCloseableIterator, inputRowSchema);
+  }
+
+  @Override
+  public CloseableIterator<InputRow> read(InputStats inputStats) throws IOException
+  {
+    return new DeltaInputSourceIterator(filteredColumnarBatchCloseableIterator, inputRowSchema);
+  }
+
+  @Override
+  public CloseableIterator<InputRowListPlusRawValues> sample() throws IOException
+  {
+    CloseableIterator<InputRow> inner = read();
+    return new CloseableIterator<InputRowListPlusRawValues>()
+    {
+      @Override
+      public void close() throws IOException
+      {
+        inner.close();
+      }
+
+      @Override
+      public boolean hasNext()
+      {
+        return inner.hasNext();
+      }
+
+      @Override
+      public InputRowListPlusRawValues next()
+      {
+        DeltaInputRow deltaInputRow = (DeltaInputRow) inner.next();
+        return InputRowListPlusRawValues.of(deltaInputRow, deltaInputRow.getRawRowAsMap());
+      }
+    };
+  }
+
+  private static class DeltaInputSourceIterator implements CloseableIterator<InputRow>
+  {
+    private final io.delta.kernel.utils.CloseableIterator<FilteredColumnarBatch> filteredColumnarBatchCloseableIterator;
+
+    private io.delta.kernel.utils.CloseableIterator<Row> currentBatch = null;
+    private final InputRowSchema inputRowSchema;
+
+    public DeltaInputSourceIterator(
+        io.delta.kernel.utils.CloseableIterator<FilteredColumnarBatch> filteredColumnarBatchCloseableIterator,
+        InputRowSchema inputRowSchema
+    )
+    {
+      this.filteredColumnarBatchCloseableIterator = filteredColumnarBatchCloseableIterator;
+      this.inputRowSchema = inputRowSchema;
+    }
+
+    @Override
+    public boolean hasNext()
+    {
+      while (currentBatch == null || !currentBatch.hasNext()) {
+        if (!filteredColumnarBatchCloseableIterator.hasNext()) {
+          return false; // No more batches or records to read!
+        }
+        currentBatch = filteredColumnarBatchCloseableIterator.next().getRows();
+      }
+      return true;
+    }
+
+    @Override
+    public InputRow next()
+    {
+      if (!hasNext()) {
+        throw new NoSuchElementException();
+      }
+
+      Row dataRow = currentBatch.next();
+
+      // TODO: construct schema? remove this after debugging
+      return new DeltaInputRow(dataRow, inputRowSchema);
+    }
+
+    @Override
+    public void close() throws IOException
+    {
+      filteredColumnarBatchCloseableIterator.close();
+    }
+  }
+
+  /**
+   * Derive value using the type mappings from row's schema pertaining to an ordinal
+   */
+  private static String getValue(Row dataRow, int columnOrdinal)
+  {
+    DataType dataType = dataRow.getSchema().at(columnOrdinal).getDataType();
+    if (dataRow.isNullAt(columnOrdinal)) {
+      return null;
+    } else if (dataType instanceof BooleanType) {
+      return Boolean.toString(dataRow.getBoolean(columnOrdinal));
+    } else if (dataType instanceof ByteType) {
+      return Byte.toString(dataRow.getByte(columnOrdinal));
+    } else if (dataType instanceof ShortType) {
+      return Short.toString(dataRow.getShort(columnOrdinal));
+    } else if (dataType instanceof IntegerType) {
+      return Integer.toString(dataRow.getInt(columnOrdinal));
+    } else if (dataType instanceof DateType) {
+      // DateType data is stored internally as the number of days since 1970-01-01
+      int daysSinceEpochUTC = dataRow.getInt(columnOrdinal);
+      return LocalDate.ofEpochDay(daysSinceEpochUTC).toString();
+    } else if (dataType instanceof LongType) {
+      return Long.toString(dataRow.getLong(columnOrdinal));
+    } else if (dataType instanceof TimestampType) {
+      // TimestampType data is stored internally as the number of microseconds since epoch
+      long microSecsSinceEpochUTC = dataRow.getLong(columnOrdinal);
+      LocalDateTime dateTime = LocalDateTime.ofEpochSecond(
+          microSecsSinceEpochUTC / 1_000_000 /* epochSecond */,
+          (int) (1000 * microSecsSinceEpochUTC % 1_000_000) /* nanoOfSecond */,
+          ZoneOffset.UTC);
+      return dateTime.toString();
+    } else if (dataType instanceof FloatType) {
+      return Float.toString(dataRow.getFloat(columnOrdinal));
+    } else if (dataType instanceof DoubleType) {
+      return Double.toString(dataRow.getDouble(columnOrdinal));
+    } else if (dataType instanceof StringType) {
+      return dataRow.getString(columnOrdinal);
+    } else if (dataType instanceof BinaryType) {
+      return new String(dataRow.getBinary(columnOrdinal));
+    } else if (dataType instanceof DecimalType) {
+      return dataRow.getDecimal(columnOrdinal).toString();
+    } else if (dataType instanceof StructType) {
+      return "TODO: struct value";
+    } else if (dataType instanceof ArrayType) {
+      return "TODO: list value";
+    } else if (dataType instanceof MapType) {
+      return "TODO: map value";
+    } else {
+      throw new UnsupportedOperationException("unsupported data type: " + dataType);
+    }
+  }
+}
