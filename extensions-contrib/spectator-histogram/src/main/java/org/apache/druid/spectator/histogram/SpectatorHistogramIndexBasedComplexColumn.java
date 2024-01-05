0f4aaba45d43e7bf453591260db8a93e7a41b555
Ben Sykes <bsykes@netflix.com>
2024-01-05 15:54:21 -0800

    Use ColumnValueSelector directly so that we support being queried as a Number using longSum or doubleSum aggregators as well as a histogram. When queried as a Number, we're returning the count of entries in the histogram.

--- extensions-contrib/spectator-histogram/src/main/java/org/apache/druid/spectator/histogram/SpectatorHistogramIndexBasedComplexColumn.java
+++ extensions-contrib/spectator-histogram/src/main/java/org/apache/druid/spectator/histogram/SpectatorHistogramIndexBasedComplexColumn.java
@@ -19,12 +19,18 @@
 
 package org.apache.druid.spectator.histogram;
 
+import org.apache.druid.query.monomorphicprocessing.RuntimeShapeInspector;
+import org.apache.druid.segment.ColumnValueSelector;
 import org.apache.druid.segment.column.ComplexColumn;
+import org.apache.druid.segment.data.ReadableOffset;
+
+import javax.annotation.Nullable;
 
 public class SpectatorHistogramIndexBasedComplexColumn implements ComplexColumn
 {
   private final SpectatorHistogramIndexed index;
   private final String typeName;
+  private static final Number ZERO = 0;
 
   public SpectatorHistogramIndexBasedComplexColumn(String typeName, SpectatorHistogramIndexed index)
   {
@@ -59,5 +65,66 @@ public class SpectatorHistogramIndexBasedComplexColumn implements ComplexColumn
   @Override
   public void close()
   {
+  }
+
+  @Override
+  public ColumnValueSelector<SpectatorHistogram> makeColumnValueSelector(ReadableOffset offset)
+  {
+    // Use ColumnValueSelector directly so that we support being queried as a Number using
+    // longSum or doubleSum aggregators, the NullableNumericBufferAggregator will call isNull.
+    // This allows us to behave as a Number or SpectatorHistogram object.
+    // When queried as a Number, we're returning the count of entries in the histogram.
+    // As such, we can safely return 0 where the histogram is null.
+    return new ColumnValueSelector<SpectatorHistogram>()
+    {
+      @Override
+      public boolean isNull()
+      {
+        return getObject() == null;
+      }
+
+      private Number getOrZero()
+      {
+        SpectatorHistogram histogram = getObject();
+        return histogram != null ? histogram : ZERO;
+      }
+
+      @Override
+      public long getLong()
+      {
+        return getOrZero().longValue();
+      }
+
+      @Override
+      public float getFloat()
+      {
+        return getOrZero().floatValue();
+      }
+
+      @Override
+      public double getDouble()
+      {
+        return getOrZero().doubleValue();
+      }
+
+      @Nullable
+      @Override
+      public SpectatorHistogram getObject()
+      {
+        return (SpectatorHistogram) getRowValue(offset.getOffset());
+      }
+
+      @Override
+      public Class classOfObject()
+      {
+        return getClazz();
+      }
+
+      @Override
+      public void inspectRuntimeShape(RuntimeShapeInspector inspector)
+      {
+        inspector.visit("column", SpectatorHistogramIndexBasedComplexColumn.this);
+      }
+    };
   }
 }
