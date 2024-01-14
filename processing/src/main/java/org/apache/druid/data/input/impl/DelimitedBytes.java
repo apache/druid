86b912d2cc5794b2d8cae883f1535c6010997fce
Gian Merlino <gianmerlino@gmail.com>
2024-01-14 00:55:48 -0800

    Faster parsing: reduce String usage, list-based input rows.
    Three changes:
    
    1) Reworked FastLineIterator to optionally avoid generating Strings
       entirely, and reduce copying somewhat. Benefits the line-oriented
       JSON, CSV, delimited (TSV), and regex formats.
    
    2) In the delimited (TSV) format, when the delimiter is a single byte,
       split on UTF-8 bytes directly.
    
    3) In CSV and delimited (TSV) formats, use list-based input rows when
       the column list is provided upfront by the user.

---
+++ processing/src/main/java/org/apache/druid/data/input/impl/DelimitedBytes.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.data.input.impl;
+
+import org.apache.druid.java.util.common.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Utility function for {@link DelimitedInputFormat}.
+ */
+public class DelimitedBytes
+{
+  /**
+   * Parameter for {@link #split(byte[], byte, int)} signifying that we do not know the expected number of fields.
+   */
+  public static final int UNKNOWN_FIELD_COUNT = -1;
+
+  /**
+   * Split UTF-8 bytes by a particular delimiter.
+   *
+   * @param bytes     utf-8 bytes
+   * @param delimiter the delimiter
+   * @param numFields expected number of fields, or {@link #UNKNOWN_FIELD_COUNT}
+   */
+  public static List<String> split(final byte[] bytes, final byte delimiter, final int numFields)
+  {
+    final List<String> out = numFields == UNKNOWN_FIELD_COUNT ? new ArrayList<>() : new ArrayList<>(numFields);
+
+    int start = 0;
+    int position = 0;
+
+    while (position < bytes.length) {
+      if (bytes[position] == delimiter) {
+        out.add(StringUtils.fromUtf8(bytes, start, position - start));
+        start = position + 1;
+      }
+
+      position++;
+    }
+
+    out.add(StringUtils.fromUtf8(bytes, start, position - start));
+    return out;
+  }
+}
