20e15efbbc2b2c4707a998925d814b33406cb863
Clint Wylie <cwylie@apache.org>
2024-01-16 12:40:27 -0800

    default value mode is dead to me

--- processing/src/main/java/org/apache/druid/segment/serde/NestedCommonFormatColumnPartSerde.java
+++ processing/src/main/java/org/apache/druid/segment/serde/NestedCommonFormatColumnPartSerde.java
@@ -22,6 +22,7 @@ package org.apache.druid.segment.serde;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.druid.common.config.NullHandling;
 import org.apache.druid.java.util.common.io.smoosh.SmooshedFileMapper;
 import org.apache.druid.segment.column.ColumnBuilder;
 import org.apache.druid.segment.column.ColumnCapabilitiesImpl;
@@ -308,8 +309,12 @@ public class NestedCommonFormatColumnPartSerde implements ColumnPartSerde
       ColumnType logicalType = simpleType == null ? ColumnType.NESTED_DATA : simpleType;
       builder.setType(logicalType);
       builder.setNestedCommonFormatColumnSupplier(supplier);
-      // nested columns only have a null value index
-      if (hasNulls) {
+      // in default value mode, SQL planning by default uses selector filters for things like 'is null', which does
+      // not work correctly for complex types (or arrays). so, only hook up this index in sql compatible mode so that
+      // query results are consistent when using an index or the value matcher
+      // additionally, nested columns only have a null value index, so we only bother with the index supplier if there
+      // are actually any null rows, otherwise we use the default 'no indexes' supplier
+      if (NullHandling.sqlCompatible() && hasNulls) {
         builder.setIndexSupplier(supplier, false, false);
       }
       builder.setColumnFormat(new NestedCommonFormatColumn.Format(logicalType, hasNulls, enforceLogicalType));
