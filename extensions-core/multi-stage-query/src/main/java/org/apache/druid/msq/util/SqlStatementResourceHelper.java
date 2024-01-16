b259c9b2847e9663b37b3bd208f635aa19cc7efb
Abhishek Balaji Radhakrishnan <abhishek.rb19@gmail.com>
2024-01-16 09:59:37 -0800

    fix checkstyle

--- extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/util/SqlStatementResourceHelper.java
+++ extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/util/SqlStatementResourceHelper.java
@@ -63,12 +63,10 @@ import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class SqlStatementResourceHelper
