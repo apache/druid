e1e9e72908893863afa6542aadfcc7ef2210667b
Amatya <amatya.avadhanula@imply.io>
2024-01-17 15:31:07 +0530

    clean up

--- indexing-service/src/main/java/org/apache/druid/indexing/common/actions/TaskAction.java
+++ indexing-service/src/main/java/org/apache/druid/indexing/common/actions/TaskAction.java
@@ -51,7 +51,7 @@ import java.util.concurrent.Future;
     @JsonSubTypes.Type(name = "checkPointDataSourceMetadata", value = CheckPointDataSourceMetadataAction.class),
     @JsonSubTypes.Type(name = "surrogateAction", value = SurrogateAction.class),
     @JsonSubTypes.Type(name = "updateStatus", value = UpdateStatusAction.class),
-    @JsonSubTypes.Type(name = "updateLocation", value = UpdateLocationAction.class),
+    @JsonSubTypes.Type(name = "updateLocation", value = UpdateLocationAction.class)
 })
 public interface TaskAction<RetType>
 {
