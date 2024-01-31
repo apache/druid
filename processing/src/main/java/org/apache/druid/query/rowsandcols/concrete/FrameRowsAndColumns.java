d4b534d43645c2246ff78ad762d45f2ce85e2d89
Soumyava Das <soumyava.das@imply.io>
2024-01-30 20:52:30 -0800

    Addressing shapeshifting issues with window functions

--- processing/src/main/java/org/apache/druid/query/rowsandcols/concrete/FrameRowsAndColumns.java
+++ processing/src/main/java/org/apache/druid/query/rowsandcols/concrete/FrameRowsAndColumns.java
@@ -28,15 +28,17 @@ import org.apache.druid.java.util.common.ISE;
 import org.apache.druid.java.util.common.Intervals;
 import org.apache.druid.query.rowsandcols.RowsAndColumns;
 import org.apache.druid.query.rowsandcols.column.Column;
+import org.apache.druid.segment.CloseableShapeshifter;
 import org.apache.druid.segment.StorageAdapter;
 import org.apache.druid.segment.column.ColumnType;
 import org.apache.druid.segment.column.RowSignature;
 
 import javax.annotation.Nullable;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.LinkedHashMap;
 
-public class FrameRowsAndColumns implements RowsAndColumns
+public class FrameRowsAndColumns implements RowsAndColumns, AutoCloseable, CloseableShapeshifter
 {
   private final Frame frame;
   private final RowSignature signature;
@@ -90,5 +92,11 @@ public class FrameRowsAndColumns implements RowsAndColumns
       return (T) new FrameStorageAdapter(frame, FrameReader.create(signature), Intervals.ETERNITY);
     }
     return null;
+  }
+
+  @Override
+  public void close() throws IOException
+  {
+    // nothing to close
   }
 }
