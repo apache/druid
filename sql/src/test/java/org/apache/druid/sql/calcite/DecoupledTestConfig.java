8112d1d1b7e74b8036d71b42b0d50b1a1c83cd4b
Zoltan Haindrich <kirk@rxd.hu>
2024-01-31 14:46:46 +0000

    describe stuff

--- sql/src/test/java/org/apache/druid/sql/calcite/DecoupledTestConfig.java
+++ sql/src/test/java/org/apache/druid/sql/calcite/DecoupledTestConfig.java
@@ -19,31 +19,54 @@
 
 package org.apache.druid.sql.calcite;
 
+import org.apache.calcite.rel.rules.CoreRules;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Specifies test case level matching customizations for decoupled mode.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 public @interface DecoupledTestConfig
 {
+  /**
+   * Enables the framework to ignore native query differences.
+   *
+   * The value of this field should describe the root cause of the difference.
+   */
   NativeQueryIgnore nativeQueryIgnore() default NativeQueryIgnore.NONE;
 
   enum NativeQueryIgnore
   {
     NONE,
-    // decoupled has moved virtualcolumn to postagg (improved plan)
-    // CoreRules.AGGREGATE_ANY_PULL_UP_CONSTANTS
+    /**
+     * Decoupled has moved virtualcolumn to postagg (improved plan)
+     * caused by: {@link CoreRules#AGGREGATE_ANY_PULL_UP_CONSTANTS}
+     */
     EXPR_POSTAGG,
-    // dim1/dim2 exchange
+    /**
+     * Aggregate column order changes.
+     *
+     * dim1/dim2 exchange
+     */
     AGG_COL_EXCHANGE,
-    // this happens when AGGREGATE_REMOVE gets supressed by
-    // AGGREGATE_CASE_REWRITE
+    /**
+     * This happens when {@link CoreRules#AGGREGATE_REMOVE} gets supressed by {@link CoreRules#AGGREGATE_CASE_TO_FILTER}
+     */
     AGGREGATE_REMOVE_NOT_FIRED,
-    // improved plan - AGGREGATE_ANY_PULL_UP_CONSTANTS ; enable for default?
+    /**
+     * Improved plan
+     *
+     * Seen that it was induced by {@link CoreRules#AGGREGATE_ANY_PULL_UP_CONSTANTS}
+     */
     IMPROVED_PLAN,
-    // worse plan; may loose vectorization; but no extra queries
+    /**
+     * Worse plan; may loose vectorization; but no extra queries
+     */
     SLIGHTLY_WORSE_PLAN;
 
     public boolean isPresent()
