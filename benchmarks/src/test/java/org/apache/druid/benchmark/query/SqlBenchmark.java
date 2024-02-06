87a01950d4c096394c1f3d0a77e735ce5c89733a
Clint Wylie <cwylie@apache.org>
2024-02-05 23:59:36 -0800

    uncomment

--- benchmarks/src/test/java/org/apache/druid/benchmark/query/SqlBenchmark.java
+++ benchmarks/src/test/java/org/apache/druid/benchmark/query/SqlBenchmark.java
@@ -755,19 +755,19 @@ public class SqlBenchmark
     }
   }
 
-//  @Benchmark
-//  @BenchmarkMode(Mode.AverageTime)
-//  @OutputTimeUnit(TimeUnit.MILLISECONDS)
-//  public void planSql(Blackhole blackhole)
-//  {
-//    final Map<String, Object> context = ImmutableMap.of(
-//        QueryContexts.VECTORIZE_KEY, vectorize,
-//        QueryContexts.VECTORIZE_VIRTUAL_COLUMNS_KEY, vectorize
-//    );
-//    final String sql = QUERIES.get(Integer.parseInt(query));
-//    try (final DruidPlanner planner = plannerFactory.createPlannerForTesting(engine, sql, context)) {
-//      final PlannerResult plannerResult = planner.plan();
-//      blackhole.consume(plannerResult);
-//    }
-//  }
+  @Benchmark
+  @BenchmarkMode(Mode.AverageTime)
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  public void planSql(Blackhole blackhole)
+  {
+    final Map<String, Object> context = ImmutableMap.of(
+        QueryContexts.VECTORIZE_KEY, vectorize,
+        QueryContexts.VECTORIZE_VIRTUAL_COLUMNS_KEY, vectorize
+    );
+    final String sql = QUERIES.get(Integer.parseInt(query));
+    try (final DruidPlanner planner = plannerFactory.createPlannerForTesting(engine, sql, context)) {
+      final PlannerResult plannerResult = planner.plan();
+      blackhole.consume(plannerResult);
+    }
+  }
 }
