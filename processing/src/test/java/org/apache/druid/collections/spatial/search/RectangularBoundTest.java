cf9f5f16b74e13740cc633b9e6219a529a6f23d9
Pranav Bhole <pranavbhole@gmail.com>
2024-02-10 16:11:19 -0800

    Rectangular bound test

--- processing/src/test/java/org/apache/druid/collections/spatial/search/RectangularBoundTest.java
+++ processing/src/test/java/org/apache/druid/collections/spatial/search/RectangularBoundTest.java
@@ -49,6 +49,34 @@ public class RectangularBoundTest
   }
 
   @Test
+  public void testRectangularBound()
+  {
+    double[][] insidePoints = new double[][]{
+        {37.795717853074635, -122.40906979480418},
+        {37.79625791859653, -122.39638788940042},
+        {37.79685798676811, -122.39335030726777},
+        {37.7966179600844, -122.39798262002006}
+    };
+    double[][] outsidePoints = new double[][]{
+        {37.79805810848854, -122.39236309307468},
+        {37.78197485768925, -122.41886599718191},
+        {37.798298130492945, -122.39608413118715},
+        {37.783595343766216, -122.41932163450181}
+    };
+    RectangularBound rectangularBound = new RectangularBound(
+        new double[]{37.78185482027019, -122.41795472254213},
+        new double[]{37.797638168104185, -122.39228715352137},
+        10
+    );
+    for (double[] insidePoint : insidePoints) {
+      Assert.assertTrue(rectangularBound.contains(insidePoint));
+    }
+    for (double[] outsidePoint : outsidePoints) {
+      Assert.assertFalse(rectangularBound.contains(outsidePoint));
+    }
+  }
+
+  @Test
   public void testEqualsContract()
   {
     EqualsVerifier.forClass(RectangularBound.class)
