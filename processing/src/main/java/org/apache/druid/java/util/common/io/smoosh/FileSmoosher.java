2f64b0dd8110b2b69e14c6ab2df988cb8abea9a5
Sensor <fectrain@outlook.com>
2024-02-29 11:09:53 +0800

    Apply suggestions from code review
    Co-authored-by: Kashif Faraz <kashif.faraz@gmail.com>

--- processing/src/main/java/org/apache/druid/java/util/common/io/smoosh/FileSmoosher.java
+++ processing/src/main/java/org/apache/druid/java/util/common/io/smoosh/FileSmoosher.java
@@ -491,7 +491,7 @@ public class FileSmoosher implements Closeable
     {
       closer.close();
       if (LOG.isDebugEnabled()) {
-        FileSmoosher.LOG.debug("Created smoosh file [%s] of size [%s] bytes.", outFile.getAbsolutePath(), outFile.length());
+        LOG.debug("Created smoosh file [%s] of size [%s] bytes.", outFile.getAbsolutePath(), outFile.length());
       }
     }
   }
