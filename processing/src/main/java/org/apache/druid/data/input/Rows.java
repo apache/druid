16e1d42b4c870a4d92d8dac1c11abd7f29aef9af
Gian Merlino <gianmerlino@gmail.com>
2024-03-04 08:30:27 -0800

    Rows.objectToNumber: Accept decimals with output type LONG. (#15999)
    * Rows.objectToNumber: Accept decimals with output type LONG.
    
    PR #15615 added an optimization to avoid parsing numbers twice in cases
    where we know that they should definitely be longs or
    definitely be doubles. Rather than try parsing as long first, and then
    try parsing as double, it would use only the parsing routine specific to
    the requested outputType.
    
    This caused a bug: previously, we would accept decimals like "1.0" or
    "1.23" as longs, by truncating them to "1". After that patch, we would
    treat such decimals as nulls when the outputType is set to LONG.
    
    This patch retains the short-circuit for doubles: if outputType is
    DOUBLE, we only parse the string as a double. But for outputType LONG,
    this patch restores the old behavior: try to parse as long first,
    then double.

--- processing/src/main/java/org/apache/druid/data/input/Rows.java
+++ processing/src/main/java/org/apache/druid/data/input/Rows.java
@@ -24,6 +24,7 @@ import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.primitives.Longs;
 import org.apache.druid.common.config.NullHandling;
 import org.apache.druid.java.util.common.IAE;
+import org.apache.druid.java.util.common.ISE;
 import org.apache.druid.java.util.common.StringUtils;
 import org.apache.druid.java.util.common.parsers.ParseException;
 import org.apache.druid.segment.column.ValueType;
@@ -112,10 +113,12 @@ public final class Rows
       throw new IAE("Output type[%s] must be numeric", outputType);
     }
 
+    final Number asNumber;
+
     if (inputValue == null) {
-      return (Number) NullHandling.defaultValueForType(outputType != null ? outputType : ValueType.LONG);
+      asNumber = (Number) NullHandling.defaultValueForType(outputType != null ? outputType : ValueType.LONG);
     } else if (inputValue instanceof Number) {
-      return (Number) inputValue;
+      asNumber = (Number) inputValue;
     } else if (inputValue instanceof String) {
       try {
         String metricValueString = StringUtils.removeChar(((String) inputValue).trim(), ',');
@@ -130,25 +133,11 @@ public final class Rows
           v = Longs.tryParse(metricValueString);
         }
 
-        if (v == null && outputType != ValueType.LONG) {
+        if (v == null) {
           v = Double.valueOf(metricValueString);
         }
 
-        if (v == null) {
-          if (throwParseExceptions) {
-            throw new ParseException(
-                String.valueOf(inputValue),
-                "Unable to parse value[%s] for field[%s] as type[%s]",
-                inputValue.getClass(),
-                name,
-                outputType
-            );
-          } else {
-            return (Number) NullHandling.defaultValueForType(outputType);
-          }
-        } else {
-          return outputType == ValueType.FLOAT ? Float.valueOf(v.floatValue()) : v;
-        }
+        asNumber = v;
       }
       catch (Exception e) {
         if (throwParseExceptions) {
@@ -174,6 +163,18 @@ public final class Rows
       } else {
         return (Number) NullHandling.defaultValueForType(outputType != null ? outputType : ValueType.LONG);
       }
+    }
+
+    if (outputType == null || asNumber == null) {
+      return asNumber;
+    } else if (outputType == ValueType.LONG) {
+      return asNumber.longValue();
+    } else if (outputType == ValueType.FLOAT) {
+      return asNumber.floatValue();
+    } else if (outputType == ValueType.DOUBLE) {
+      return asNumber.doubleValue();
+    } else {
+      throw new ISE("Cannot read number as type[%s]", outputType);
     }
   }
 
