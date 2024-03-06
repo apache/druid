731a4201d087b18de562c57375e64da7eaa3f537
Vishesh Garg <gargvishesh@gmail.com>
2024-03-06 20:15:39 +0530

    Remove unused exceptions

--- extensions-core/google-extensions/src/test/java/org/apache/druid/storage/google/GoogleDataSegmentKillerTest.java
+++ extensions-core/google-extensions/src/test/java/org/apache/druid/storage/google/GoogleDataSegmentKillerTest.java
@@ -96,7 +96,7 @@ public class GoogleDataSegmentKillerTest extends EasyMockSupport
   }
 
   @Test
-  public void killWithErrorTest() throws SegmentLoadingException
+  public void killWithErrorTest()
   {
     Assert.assertThrows(SegmentLoadingException.class, () -> {
       storage.delete(EasyMock.eq(BUCKET), EasyMock.eq(INDEX_PATH));
@@ -113,7 +113,7 @@ public class GoogleDataSegmentKillerTest extends EasyMockSupport
   }
 
   @Test
-  public void killRetryWithErrorTest() throws SegmentLoadingException, IOException
+  public void killRetryWithErrorTest() throws SegmentLoadingException
   {
     storage.delete(EasyMock.eq(BUCKET), EasyMock.eq(INDEX_PATH));
     EasyMock.expectLastCall().andThrow(RECOVERABLE_EXCEPTION).once().andVoid().once();
