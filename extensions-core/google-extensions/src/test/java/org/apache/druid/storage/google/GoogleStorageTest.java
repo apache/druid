5298f2289706a71fc18dbf6a5424658620b3d293
Vishesh Garg <gargvishesh@gmail.com>
2024-03-08 16:44:29 +0530

    Remove unused import

--- extensions-core/google-extensions/src/test/java/org/apache/druid/storage/google/GoogleStorageTest.java
+++ extensions-core/google-extensions/src/test/java/org/apache/druid/storage/google/GoogleStorageTest.java
@@ -39,7 +39,6 @@ import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 public class GoogleStorageTest
