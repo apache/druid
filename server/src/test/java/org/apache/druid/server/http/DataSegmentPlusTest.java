634ced2d488b1d980965629fbc26efce12585774
rishabh singh <rishabh.singh@imply.io>
2024-03-14 22:55:58 +0530

    Fix DataSegmentPlusTest failure

--- server/src/test/java/org/apache/druid/server/http/DataSegmentPlusTest.java
+++ server/src/test/java/org/apache/druid/server/http/DataSegmentPlusTest.java
@@ -108,7 +108,7 @@ public class DataSegmentPlusTest
         JacksonUtils.TYPE_REFERENCE_MAP_STRING_OBJECT
     );
 
-    Assert.assertEquals(4, objectMap.size());
+    Assert.assertEquals(6, objectMap.size());
     final Map<String, Object> segmentObjectMap = MAPPER.readValue(
         MAPPER.writeValueAsString(segmentPlus.getDataSegment()),
         JacksonUtils.TYPE_REFERENCE_MAP_STRING_OBJECT
