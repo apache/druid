cea778c543a8a84676366877ff35eeacacc717f4
YongGang <mail.che@gmail.com>
2024-03-15 10:19:34 -0700

    update Unit Test

--- extensions-core/multi-stage-query/src/test/java/org/apache/druid/msq/indexing/report/MSQTaskReportTest.java
+++ extensions-core/multi-stage-query/src/test/java/org/apache/druid/msq/indexing/report/MSQTaskReportTest.java
@@ -62,6 +62,7 @@ public class MSQTaskReportTest
 {
   private static final String TASK_ID = "mytask";
   private static final String HOST = "example.com:1234";
+  private static final Map<String, Object> TAGS = ImmutableMap.of("tag1", "value1", "tag2", "value2");
   public static final QueryDefinition QUERY_DEFINITION =
       QueryDefinition
           .builder()
@@ -135,6 +136,7 @@ public class MSQTaskReportTest
     );
 
     Assert.assertEquals(TASK_ID, report2.getTaskId());
+    Assert.assertNull(report2.getTags());
     Assert.assertEquals(report.getPayload().getStatus().getStatus(), report2.getPayload().getStatus().getStatus());
     Assert.assertNull(report2.getPayload().getStatus().getErrorReport());
     Assert.assertEquals(report.getPayload().getStatus().getRunningTasks(), report2.getPayload().getStatus().getRunningTasks());
@@ -221,7 +223,7 @@ public class MSQTaskReportTest
 
     final MSQTaskReport report = new MSQTaskReport(
         TASK_ID,
-        null,
+        TAGS,
         new MSQTaskReportPayload(
             new MSQStatusReport(TaskState.SUCCESS, null, new ArrayDeque<>(), null, 0, new HashMap<>(), 1, 2, status),
             MSQStagesReport.create(
@@ -254,6 +256,7 @@ public class MSQTaskReportTest
     final MSQTaskReport report2 = (MSQTaskReport) reportMap.get(MSQTaskReport.REPORT_KEY);
 
     Assert.assertEquals(TASK_ID, report2.getTaskId());
+    Assert.assertEquals(TAGS, report2.getTags());
     Assert.assertEquals(report.getPayload().getStatus().getStatus(), report2.getPayload().getStatus().getStatus());
     Assert.assertEquals(report.getPayload().getStages(), report2.getPayload().getStages());
   }
