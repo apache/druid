3ef33a31ba8d8c58bb79fd2f7f43fb9a57a1649a
Adarsh Sanjeev <adarshsanjeev@gmail.com>
2024-03-19 15:11:04 +0530

    Fix incorrect header names for certain export queries (#16096)
    * Fix incorrect header names for certain queries
    
    * Fix incorrect header names for certain queries
    
    * Maintain upgrade compatibility
    
    * Fix tests
    
    * Change null handling

--- extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/exec/ControllerImpl.java
+++ extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/exec/ControllerImpl.java
@@ -1913,7 +1913,8 @@ public class ControllerImpl implements Controller
                                  .processorFactory(new ExportResultsFrameProcessorFactory(
                                      queryId,
                                      exportStorageProvider,
-                                     resultFormat
+                                     resultFormat,
+                                     columnMappings
                                  ))
       );
       return builder.build();
