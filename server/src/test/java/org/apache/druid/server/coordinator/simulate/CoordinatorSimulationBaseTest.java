352902156abaac20a21bbb79a37d166ebe50db1d
Kashif Faraz <kashif.faraz@gmail.com>
2024-03-21 12:56:59 +0530

    Fix mark segment unused when overshadowed by zero replica segment (#16181)
    Bug:
    In the `MarkOvershadowedSegmentsAsUnused` duty, the coordinator marks a segment
    as unused if it is overshadowed by a segment currently being served by a historical or broker.
    But it is possible to have segments that are eligible for a load rule but require zero replicas
    to be loaded. (Such segments can be queried only using the MSQ engine).
    If such a zero-replica segment overshadows any other segment, the overshadowed segment will
    never be marked as unused and will continue to exist in the metadata store as a dangling segment.
    
    Fix:
    - In a coordinator run, keep track of segments that are eligible for a load rule but require zero replicas
    - Allow the zero-replicas segments to overshadow old segments and hence mark the latter as unused
    
    Other changes:
    - Add simulation test to verify new behaviour. This test fails with the current code.
    - Clean up javadocs

--- server/src/test/java/org/apache/druid/server/coordinator/simulate/CoordinatorSimulationBaseTest.java
+++ server/src/test/java/org/apache/druid/server/coordinator/simulate/CoordinatorSimulationBaseTest.java
@@ -223,6 +223,8 @@ public abstract class CoordinatorSimulationBaseTest implements
     static final String LOAD_QUEUE_COUNT = "segment/loadQueue/count";
     static final String DROP_QUEUE_COUNT = "segment/dropQueue/count";
     static final String CANCELLED_ACTIONS = "segment/loadQueue/cancelled";
+
+    static final String OVERSHADOWED_COUNT = "segment/overshadowed/count";
   }
 
   static class Segments
