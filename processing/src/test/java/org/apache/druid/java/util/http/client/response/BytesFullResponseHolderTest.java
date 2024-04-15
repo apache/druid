b019af35eff64aded68120689649c8b0910497a8
Adarsh Sanjeev <adarshsanjeev@gmail.com>
2024-04-15 09:14:15 +0530

    Fix checkstyle

--- processing/src/test/java/org/apache/druid/java/util/http/client/response/BytesFullResponseHolderTest.java
+++ processing/src/test/java/org/apache/druid/java/util/http/client/response/BytesFullResponseHolderTest.java
@@ -34,50 +34,59 @@ import org.mockito.Mockito;
 
 import java.util.Objects;
 
-public class BytesFullResponseHolderTest {
+public class BytesFullResponseHolderTest
+{
+  ObjectMapper objectMapper = new DefaultObjectMapper();
 
-    ObjectMapper objectMapper = new DefaultObjectMapper();
+  @Test
+  public void testDeserialize() throws Exception
+  {
+    final ResponseObject payload = new ResponseObject("payload123");
 
-    @Test
-    public void testDeserialize() throws Exception {
-        final ResponseObject payload = new ResponseObject("payload123");
+    final DefaultHttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
 
-        final DefaultHttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+    final BytesFullResponseHolder target = Mockito.spy(new BytesFullResponseHolder(response));
+    target.addChunk(objectMapper.writeValueAsBytes(payload));
 
-        final BytesFullResponseHolder target = Mockito.spy(new BytesFullResponseHolder(response));
-        target.addChunk(objectMapper.writeValueAsBytes(payload));
+    final ResponseObject deserialize = target.deserialize(objectMapper, new TypeReference<ResponseObject>() {
+    });
 
-        final ResponseObject deserialize = target.deserialize(objectMapper, new TypeReference<ResponseObject>() {
-        });
+    Assert.assertEquals(payload, deserialize);
+    Mockito.verify(target, Mockito.times(1)).deserialize(ArgumentMatchers.any(), ArgumentMatchers.any());
+  }
 
-        Assert.assertEquals(payload, deserialize);
-        Mockito.verify(target, Mockito.times(1)).deserialize(ArgumentMatchers.any(), ArgumentMatchers.any());
+  static class ResponseObject
+  {
+    String payload;
+
+    @JsonCreator
+    public ResponseObject(@JsonProperty("payload") String payload)
+    {
+      this.payload = payload;
     }
 
-    static class ResponseObject {
-        String payload;
-
-        @JsonCreator
-        public ResponseObject(@JsonProperty("payload") String payload) {
-            this.payload = payload;
-        }
-
-        @JsonProperty("payload")
-        public String getPayload() {
-            return payload;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            ResponseObject that = (ResponseObject) o;
-            return Objects.equals(payload, that.payload);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(payload);
-        }
+    @JsonProperty("payload") public String getPayload()
+    {
+      return payload;
     }
+
+    @Override
+    public boolean equals(Object o)
+    {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      ResponseObject that = (ResponseObject) o;
+      return Objects.equals(payload, that.payload);
+    }
+
+    @Override
+    public int hashCode()
+    {
+      return Objects.hashCode(payload);
+    }
+  }
 }
