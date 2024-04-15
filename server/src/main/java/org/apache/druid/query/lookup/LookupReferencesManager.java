3f8fcf9fed5e42241f5fd9e48167c85c7115530d
Andrew Ho <a.ho@salesforce.com>
2024-04-15 13:16:46 -0700

    Improve logging for lookups

--- server/src/main/java/org/apache/druid/query/lookup/LookupReferencesManager.java
+++ server/src/main/java/org/apache/druid/query/lookup/LookupReferencesManager.java
@@ -394,7 +394,7 @@ public class LookupReferencesManager implements LookupExtractorFactoryContainerP
     if (lookupConfig.getEnableLookupSyncOnStartup()) {
       lookupBeanList = getLookupListFromCoordinator(lookupListeningAnnouncerConfig.getLookupTier());
       if (lookupBeanList == null) {
-        LOG.info("Coordinator is unavailable. Loading saved snapshot instead");
+        LOG.info("No lookup list found from coordinator. Loading saved snapshot instead");
         lookupBeanList = getLookupListFromSnapshot();
       }
     } else {
