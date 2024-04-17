f273a32910a06eac4e03d448b68ff31a941ecb81
Adithya Chakilam <adithya.chakilam@imply.io>
2024-04-17 15:00:58 -0500

    Expose records throughput by partition in live reports

--- indexing-service/src/main/java/org/apache/druid/indexing/seekablestream/SeekableStreamIndexTaskRunner.java
+++ indexing-service/src/main/java/org/apache/druid/indexing/seekablestream/SeekableStreamIndexTaskRunner.java
@@ -1635,6 +1635,7 @@ public abstract class SeekableStreamIndexTaskRunner<PartitionIdType, SequenceOff
     payload.put("ingestionState", ingestionState);
     payload.put("unparseableEvents", events);
     payload.put("rowStats", doGetRowStats());
+    payload.put("recordsProcessed", partitionsThroughput);
 
     ingestionStatsAndErrors.put("taskId", task.getId());
     ingestionStatsAndErrors.put("payload", payload);
