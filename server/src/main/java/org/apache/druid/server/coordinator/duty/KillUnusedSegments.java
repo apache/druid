8a924eb7d9a68ae30470822d22c44c9a26ba5439
Andreas Maechler <amaechler@gmail.com>
2024-04-26 10:52:34 -0600

    Fix log count

--- server/src/main/java/org/apache/druid/server/coordinator/duty/KillUnusedSegments.java
+++ server/src/main/java/org/apache/druid/server/coordinator/duty/KillUnusedSegments.java
@@ -239,7 +239,7 @@ public class KillUnusedSegments implements CoordinatorDuty
 
     log.info(
         "Submitted [%d] kill tasks for [%d] datasources. Remaining datasources to kill: %s",
-        submittedTasks, dataSourcesToKill.size(), remainingDatasourcesToKill
+        submittedTasks, dataSourcesToKill.size() - remainingDatasourcesToKill.size(), remainingDatasourcesToKill
     );
 
     stats.add(Stats.Kill.SUBMITTED_TASKS, submittedTasks);
