3206f7a3471c4cccdc4eb85c42a556056c71cdd9
Gian Merlino <gianmerlino@gmail.com>
2024-04-29 15:23:18 -0700

    Use valueOrDefault.

--- processing/src/main/java/org/apache/druid/segment/virtual/ExpressionDeferredGroupByVectorColumnSelector.java
+++ processing/src/main/java/org/apache/druid/segment/virtual/ExpressionDeferredGroupByVectorColumnSelector.java
@@ -108,7 +108,7 @@ public class ExpressionDeferredGroupByVectorColumnSelector implements GroupByVec
       keyOffset += subSelector.getGroupingKeySize();
     }
 
-    resultRow.getArray()[resultRowPosition] = expr.eval(tmpResultRowBindings).value();
+    resultRow.getArray()[resultRowPosition] = expr.eval(tmpResultRowBindings).valueOrDefault();
   }
 
   @Override
