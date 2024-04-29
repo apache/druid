1b7532991a5a729506f2a9deba02708b1598e74d
Kashif Faraz <kashif.faraz@gmail.com>
2024-04-30 00:07:13 +0530

    Remove usage of skife from DruidCoordinatorConfig (#15705)
    * Remove usage of skife from DruidCoordinatorConfig
    
    * Remove old config class
    
    * Address static checks
    
    * Fix tests
    
    * Remove unnecessary mocks
    
    * Fix config typos
    
    * Fix config condition
    
    * Fix test, spotbug check
    
    * Move validation to DruidCoordinatorConfig
    
    * Move DruidCoordinatorConfig to different package
    
    * Fix validation of killunusedconfig
    
    * Simplify and fix KillSupervisorsCustomDuty
    
    * Address review comments
    
    * Fix new tests
    
    * Add KillUnusedSchemasConfig
    
    * Remove KillUnusedSchemasConfig
    
    * Minor renames

--- server/src/main/java/org/apache/druid/server/coordinator/duty/MetadataCleanupDuty.java
+++ server/src/main/java/org/apache/druid/server/coordinator/duty/MetadataCleanupDuty.java
@@ -19,21 +19,19 @@
 
 package org.apache.druid.server.coordinator.duty;
 
-import com.google.common.base.Preconditions;
 import org.apache.druid.java.util.common.DateTimes;
 import org.apache.druid.java.util.common.logger.Logger;
-import org.apache.druid.server.coordinator.DruidCoordinatorConfig;
 import org.apache.druid.server.coordinator.DruidCoordinatorRuntimeParams;
+import org.apache.druid.server.coordinator.config.MetadataCleanupConfig;
 import org.apache.druid.server.coordinator.stats.CoordinatorStat;
 import org.joda.time.DateTime;
-import org.joda.time.Duration;
 
 import javax.annotation.Nullable;
 
 /**
  * Performs cleanup of stale metadata entries created before a configured retain duration.
  * <p>
- * In every invocation of {@link #run}, the duty checks if the {@link #cleanupPeriod}
+ * In every invocation of {@link #run}, the duty checks if the {@code cleanupPeriod}
  * has elapsed since the {@link #lastCleanupTime}. If it has, then the method
  * {@link #cleanupEntriesCreatedBefore(DateTime)} is invoked. Otherwise, the duty
  * completes immediately without making any changes.
@@ -42,40 +40,27 @@ public abstract class MetadataCleanupDuty implements CoordinatorDuty
 {
   private static final Logger log = new Logger(MetadataCleanupDuty.class);
 
-  private final String propertyPrefix;
   private final String entryType;
   private final CoordinatorStat cleanupCountStat;
 
-  private final boolean cleanupEnabled;
-  private final Duration cleanupPeriod;
-  private final Duration retainDuration;
+  private final MetadataCleanupConfig cleanupConfig;
 
   private DateTime lastCleanupTime = DateTimes.utc(0);
 
   protected MetadataCleanupDuty(
       String entryType,
-      String propertyPrefix,
-      boolean cleanupEnabled,
-      Duration cleanupPeriod,
-      Duration retainDuration,
-      CoordinatorStat cleanupCountStat,
-      DruidCoordinatorConfig coordinatorConfig
+      MetadataCleanupConfig cleanupConfig,
+      CoordinatorStat cleanupCountStat
   )
   {
-    this.propertyPrefix = propertyPrefix;
     this.entryType = entryType;
-    this.cleanupEnabled = cleanupEnabled;
-    this.cleanupPeriod = cleanupPeriod;
-    this.retainDuration = retainDuration;
+    this.cleanupConfig = cleanupConfig;
     this.cleanupCountStat = cleanupCountStat;
 
-    if (this.cleanupEnabled) {
-      validatePeriod(cleanupPeriod, coordinatorConfig.getCoordinatorMetadataStoreManagementPeriod());
-      validateRetainDuration(retainDuration);
-
+    if (cleanupConfig.isCleanupEnabled()) {
       log.debug(
           "Enabled cleanup of [%s] with period [%s] and durationToRetain [%s].",
-          entryType, cleanupPeriod, retainDuration
+          entryType, cleanupConfig.getCleanupPeriod(), cleanupConfig.getDurationToRetain()
       );
     }
   }
@@ -84,18 +69,18 @@ public abstract class MetadataCleanupDuty implements CoordinatorDuty
   @Override
   public DruidCoordinatorRuntimeParams run(DruidCoordinatorRuntimeParams params)
   {
-    if (!cleanupEnabled) {
+    if (!cleanupConfig.isCleanupEnabled()) {
       return params;
     }
 
     final DateTime now = getCurrentTime();
 
     // Perform cleanup only if cleanup period has elapsed
-    if (lastCleanupTime.plus(cleanupPeriod).isBefore(now)) {
+    if (lastCleanupTime.plus(cleanupConfig.getCleanupPeriod()).isBefore(now)) {
       lastCleanupTime = now;
 
       try {
-        DateTime minCreatedTime = now.minus(retainDuration);
+        DateTime minCreatedTime = now.minus(cleanupConfig.getDurationToRetain());
         int deletedEntries = cleanupEntriesCreatedBefore(minCreatedTime);
         log.info("Removed [%,d] [%s] created before [%s].", deletedEntries, entryType, minCreatedTime);
 
@@ -112,38 +97,16 @@ public abstract class MetadataCleanupDuty implements CoordinatorDuty
   /**
    * Cleans up metadata entries created before the {@code minCreatedTime}.
    * <p>
-   * This method is not invoked if the {@link #cleanupPeriod} has not elapsed
+   * This method is not invoked if the {@code cleanupPeriod} has not elapsed
    * since the {@link #lastCleanupTime}.
    *
    * @return Number of deleted metadata entries
    */
   protected abstract int cleanupEntriesCreatedBefore(DateTime minCreatedTime);
 
-  private void validatePeriod(Duration period, Duration metadataManagementPeriod)
-  {
-    Preconditions.checkArgument(
-        period != null && period.getMillis() >= metadataManagementPeriod.getMillis(),
-        "[%s.period] must be greater than [druid.coordinator.period.metadataStoreManagementPeriod]",
-        propertyPrefix
-    );
-  }
-
-  private void validateRetainDuration(Duration retainDuration)
-  {
-    Preconditions.checkArgument(
-        retainDuration != null && retainDuration.getMillis() >= 0,
-        "[%s.durationToRetain] must be 0 milliseconds or higher",
-        propertyPrefix
-    );
-    Preconditions.checkArgument(
-        retainDuration.getMillis() < System.currentTimeMillis(),
-        "[%s.durationToRetain] cannot be greater than current time in milliseconds",
-        propertyPrefix
-    );
-  }
-
   protected DateTime getCurrentTime()
   {
     return DateTimes.nowUtc();
   }
+
 }
