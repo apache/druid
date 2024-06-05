db1eb49401f2c91c5e660bb0c3d40397ef437db1
Gian Merlino <gianmerlino@gmail.com>
2024-06-05 00:09:26 -0700

    Fix capabilities reported by UnnestStorageAdapter.
    UnnestStorageAdapter and its cursors did not return capabilities correctly
    for the output column. This patch fixes two problems:
    
    1) UnnestStorageAdapter returned the capabilities of the unnest virtual
       column prior to unnesting. It should return the post-unnest capabilities.
    
    2) UnnestColumnValueSelectorCursor passed through isDictionaryEncoded from
       the unnest virtual column. This is incorrect, because the dimension selector
       created by this class never has a dictionary. This is the cause of #16543.

--- processing/src/test/java/org/apache/druid/segment/UnnestStorageAdapterTest.java
+++ processing/src/test/java/org/apache/druid/segment/UnnestStorageAdapterTest.java
@@ -194,7 +194,7 @@ public class UnnestStorageAdapterTest extends InitializedNullHandlingTest
   }
 
   @Test
-  public void test_group_of_unnest_adapters_column_capabilities()
+  public void test_unnest_adapter_column_capabilities()
   {
     String colName = "multi-string1";
     List<String> columnsInTable = Arrays.asList(
@@ -220,7 +220,14 @@ public class UnnestStorageAdapterTest extends InitializedNullHandlingTest
       Assert.assertEquals(capabilities.getType(), valueTypes.get(i));
     }
     assertColumnReadsIdentifier(adapter.getUnnestColumn(), colName);
-
+    Assert.assertEquals(
+        adapter.getColumnCapabilities(OUTPUT_COLUMN_NAME).isDictionaryEncoded(),
+        ColumnCapabilities.Capable.TRUE // passed through from dict-encoded input
+    );
+    Assert.assertEquals(
+        adapter.getColumnCapabilities(OUTPUT_COLUMN_NAME).hasMultipleValues(),
+        ColumnCapabilities.Capable.FALSE
+    );
   }
 
   @Test
