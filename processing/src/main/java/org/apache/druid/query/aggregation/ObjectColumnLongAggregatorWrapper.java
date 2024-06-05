21a06c6549d7afcb1ec0eac0b007a9161236bd85
Gian Merlino <gianmerlino@gmail.com>
2024-06-05 15:49:29 -0700

    Coerce COMPLEX to number in numeric aggregators.
    PR #15371 eliminated ObjectColumnSelector's built-in implementations of
    numeric methods, which had been marked deprecated.
    
    However, some complex types, like SpectatorHistogram, can be successfully coerced
    to number. The documentation for spectator histograms encourages taking advantage of
    this by aggregating complex columns with doubleSum and longSum. Currently, this
    doesn't work properly for IncrementalIndex, where the behavior relied on those
    deprecated ObjectColumnSelector methods.
    
    This patch fixes the behavior by making two changes:
    
    1) SimpleXYZAggregatorFactory (XYZ = type; base class for simple numeric aggregators;
       all of these extend NullableNumericAggregatorFactory) use getObject for STRING
       and COMPLEX. Previously, getObject was only used for STRING.
    
    2) NullableNumericAggregatorFactory (base class for simple numeric aggregators)
       has a new protected method "useGetObject". This allows the base class to
       correctly check for null (using getObject or isNull).
    
    The patch also adds a test for SpectatorHistogram + doubleSum + IncrementalIndex.

--- processing/src/main/java/org/apache/druid/query/aggregation/ObjectColumnLongAggregatorWrapper.java
+++ processing/src/main/java/org/apache/druid/query/aggregation/ObjectColumnLongAggregatorWrapper.java
@@ -20,51 +20,50 @@
 package org.apache.druid.query.aggregation;
 
 import org.apache.druid.java.util.common.Numbers;
-import org.apache.druid.segment.BaseFloatColumnValueSelector;
+import org.apache.druid.segment.BaseLongColumnValueSelector;
 import org.apache.druid.segment.BaseObjectColumnValueSelector;
-import org.apache.druid.segment.selector.settable.SettableValueFloatColumnValueSelector;
+import org.apache.druid.segment.selector.settable.SettableValueLongColumnValueSelector;
 
-import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.function.Function;
 
 /**
- * This class can be used to wrap Float BufferAggregator that consume float type columns to handle String type.
+ * This class can be used to wrap Long Aggregator that consume long type columns to handle Object type.
  */
-public class StringColumnFloatBufferAggregatorWrapper extends DelegatingBufferAggregator
+public class ObjectColumnLongAggregatorWrapper extends DelegatingAggregator
 {
   private final BaseObjectColumnValueSelector selector;
-  private final float nullValue;
-  private final SettableValueFloatColumnValueSelector floatSelector;
+  private final long nullValue;
+  private final SettableValueLongColumnValueSelector longSelector;
 
-  public StringColumnFloatBufferAggregatorWrapper(
+  public ObjectColumnLongAggregatorWrapper(
       BaseObjectColumnValueSelector selector,
-      Function<BaseFloatColumnValueSelector, BufferAggregator> delegateBuilder,
-      float nullValue
+      Function<BaseLongColumnValueSelector, Aggregator> delegateBuilder,
+      long nullValue
   )
   {
-    this.floatSelector = new SettableValueFloatColumnValueSelector();
+    this.longSelector = new SettableValueLongColumnValueSelector();
     this.selector = selector;
     this.nullValue = nullValue;
-    this.delegate = delegateBuilder.apply(floatSelector);
+    this.delegate = delegateBuilder.apply(longSelector);
   }
 
   @Override
-  public void aggregate(ByteBuffer buf, int position)
+  public void aggregate()
   {
     Object update = selector.getObject();
 
     if (update == null) {
-      floatSelector.setValue(nullValue);
-      delegate.aggregate(buf, position);
+      longSelector.setValue(nullValue);
+      delegate.aggregate();
     } else if (update instanceof List) {
       for (Object o : (List) update) {
-        floatSelector.setValue(Numbers.tryParseFloat(o, nullValue));
-        delegate.aggregate(buf, position);
+        longSelector.setValue(Numbers.tryParseLong(o, nullValue));
+        delegate.aggregate();
       }
     } else {
-      floatSelector.setValue(Numbers.tryParseFloat(update, nullValue));
-      delegate.aggregate(buf, position);
+      longSelector.setValue(Numbers.tryParseLong(update, nullValue));
+      delegate.aggregate();
     }
   }
 }
