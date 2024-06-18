475f5b5830a101ec184015e640ec60f72bf6550c
Zoltan Haindrich <kirk@rxd.hu>
2024-06-18 15:43:46 +0000

    cleanup

--- quidem-it/src/test/java/org/apache/druid/quidem/QTest.java
+++ quidem-it/src/test/java/org/apache/druid/quidem/QTest.java
@@ -19,21 +19,9 @@
 
 package org.apache.druid.quidem;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import org.apache.druid.sql.calcite.util.SqlTestFramework.QueryComponentSupplier;
 import org.junit.jupiter.api.Test;
-import org.reflections.Reflections;
-import org.reflections.scanners.SubTypesScanner;
-import org.reflections.util.ConfigurationBuilder;
-import org.reflections.util.FilterBuilder;
-
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.Set;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -60,42 +48,5 @@ public class QTest extends DruidQuidemTestBase
         fail("Record file found: " + name);
       }
     }
-  }
-
-  @Test
-  public void testQTest() throws Exception
-  {
-    LoadingCache<String, Set<Class<? extends QueryComponentSupplier>>> componentSupplierClassCache = CacheBuilder
-        .newBuilder()
-        .build(new CacheLoader<String, Set<Class<? extends QueryComponentSupplier>>>()
-        {
-          @Override
-          public Set<Class<? extends QueryComponentSupplier>> load(String pkg) throws MalformedURLException
-          {
-            return new Reflections(
-                new ConfigurationBuilder()
-                    // .addUrls(ClasspathHelper.forPackage(pkg,
-                    // getClass().getClassLoader()))
-
-                    // .addClassLoaders(getClass().getClassLoader().getParent())
-                    .setScanners(
-                        new SubTypesScanner(true)
-
-                    )
-                    .filterInputsBy(
-                        new FilterBuilder().includePackage(pkg).and(
-                            s -> s.contains("ComponentSupplier")
-                        )
-
-                    )
-                    .setUrls(org.reflections.util.ClasspathHelper.forJavaClassPath())
-
-            )
-                .getSubTypesOf(QueryComponentSupplier.class);
-          }
-        });
-
-    Set<Class<? extends QueryComponentSupplier>> a = componentSupplierClassCache.get("");
-    throw new RuntimeException("X" + a);
   }
 }
