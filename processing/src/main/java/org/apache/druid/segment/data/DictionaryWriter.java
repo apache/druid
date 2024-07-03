93ca7e7d0b238259427b58f7b5dcea06298401ed
Adarsh Sanjeev <adarshsanjeev@gmail.com>
2024-07-03 10:33:55 +0530

    - Modify DictionaryWriter#write to return the index of the value written. - Add a readBufferCache to FrontCodedIntArrayIndexedWriter. - Track cardinality with FixedIndexedWriter.

--- processing/src/main/java/org/apache/druid/segment/data/DictionaryWriter.java
+++ processing/src/main/java/org/apache/druid/segment/data/DictionaryWriter.java
@@ -28,12 +28,41 @@ public interface DictionaryWriter<T> extends Serializer
 {
   boolean isSorted();
 
+  /**
+   * Prepares the writer for writing
+   *
+   * @throws IOException if there is a problem with IO
+   */
   void open() throws IOException;
 
-  void write(@Nullable T objectToWrite) throws IOException;
+  /**
+   * Writes an object to the dictionary.
+   * <p>
+   * Returns the index of the value that was just written.  This is defined as the `int` value that can be passed
+   * into {@link #get} such that it will return the same value back.
+   *
+   * @param objectToWrite object to be written to the dictionary
+   * @return index of the value that was just written
+   * @throws IOException if there is a problem with IO
+   */
+  int write(@Nullable T objectToWrite) throws IOException;
 
+  /**
+   * Returns an object that has already been written via the {@link #write} method.
+   *
+   * @param dictId index of the object to return
+   * @return the object identified by the given index
+   * @throws IOException if there is a problem with IO
+   */
   @Nullable
   T get(int dictId) throws IOException;
 
+  /**
+   * Returns the number of items that have been written so far in this dictionary.  Any number lower than this
+   * cardinality can be passed into {@link #get} and a value will be returned.  If a value greater than or equal to
+   * the cardinality is passed into {@link #get} all sorts of things could happen, but likely none of them are good.
+   *
+   * @return the number of items that have been written so far
+   */
   int getCardinality();
 }
