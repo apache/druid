7a34b6e0929b7b90588d6922d396acb6dedec81c
Zoltan Haindrich <kirk@rxd.hu>
2024-07-19 04:28:02 +0000

    cleanup

--- extensions-core/multi-stage-query/src/test/java/org/apache/druid/msq/exec/MSQDrillWindowQueryTest.java
+++ extensions-core/multi-stage-query/src/test/java/org/apache/druid/msq/exec/MSQDrillWindowQueryTest.java
@@ -20,20 +20,13 @@
 package org.apache.druid.msq.exec;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Supplier;
-import com.google.common.base.Suppliers;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-import com.google.inject.Binder;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.Provides;
-import com.google.inject.TypeLiteral;
 import org.apache.druid.guice.DruidInjectorBuilder;
 import org.apache.druid.guice.LazySingleton;
 import org.apache.druid.initialization.ServerInjectorBuilderTest.TestDruidModule;
-import org.apache.druid.java.util.emitter.service.ServiceEmitter;
 import org.apache.druid.msq.exec.MSQDrillWindowQueryTest.DrillWindowQueryMSQComponentSupplier;
 import org.apache.druid.msq.guice.MultiStageQuery;
 import org.apache.druid.msq.sql.MSQTaskSqlEngine;
@@ -42,17 +35,8 @@ import org.apache.druid.msq.test.ExtractResultsFactory;
 import org.apache.druid.msq.test.MSQTestOverlordServiceClient;
 import org.apache.druid.msq.test.MSQTestTaskActionClient;
 import org.apache.druid.msq.test.VerifyMSQSupportedNativeQueriesPredicate;
-import org.apache.druid.query.DefaultQueryConfig;
 import org.apache.druid.query.groupby.TestGroupByBuffers;
 import org.apache.druid.server.QueryLifecycleFactory;
-import org.apache.druid.server.QueryScheduler;
-import org.apache.druid.server.QuerySchedulerProvider;
-import org.apache.druid.server.log.RequestLogger;
-import org.apache.druid.server.log.TestRequestLogger;
-import org.apache.druid.server.metrics.NoopServiceEmitter;
-import org.apache.druid.server.security.AuthenticatorMapper;
-import org.apache.druid.server.security.AuthorizerMapper;
-import org.apache.druid.server.security.Escalator;
 import org.apache.druid.sql.SqlStatementFactory;
 import org.apache.druid.sql.SqlToolbox;
 import org.apache.druid.sql.avatica.DruidMeta;
@@ -61,12 +45,7 @@ import org.apache.druid.sql.calcite.DrillWindowQueryTest;
 import org.apache.druid.sql.calcite.QueryTestBuilder;
 import org.apache.druid.sql.calcite.SqlTestFrameworkConfig;
 import org.apache.druid.sql.calcite.TempDirProducer;
-import org.apache.druid.sql.calcite.planner.CalciteRulesManager;
-import org.apache.druid.sql.calcite.planner.CatalogResolver;
 import org.apache.druid.sql.calcite.run.SqlEngine;
-import org.apache.druid.sql.calcite.schema.DruidSchemaName;
-import org.apache.druid.sql.calcite.util.CalciteTests;
-import org.apache.druid.sql.guice.SqlModule;
 
 @SqlTestFrameworkConfig.ComponentSupplier(DrillWindowQueryMSQComponentSupplier.class)
 public class MSQDrillWindowQueryTest extends DrillWindowQueryTest
@@ -84,32 +63,6 @@ public class MSQDrillWindowQueryTest extends DrillWindowQueryTest
       super.configureGuice(builder);
       builder.addModules(CalciteMSQTestsHelper.fetchModules(tempDirProducer::newTempFolder, TestGroupByBuffers.createDefault()).toArray(new Module[0]));
       builder.addModule(new TestMSQSqlModule());
-    }
-
-    private static class TestSqlModule0 extends TestDruidModule
-    {
-      @Override
-      public void configure(Binder binder)
-      {
-        binder.install(new SqlModule.SqlStatementFactoryModule());
-        binder.bind(String.class)
-            .annotatedWith(DruidSchemaName.class)
-            .toInstance(CalciteTests.DRUID_SCHEMA_NAME);
-        binder.bind(new TypeLiteral<Supplier<DefaultQueryConfig>>()
-        {
-        }).toInstance(Suppliers.ofInstance(new DefaultQueryConfig(ImmutableMap.of())));
-        binder.bind(CalciteRulesManager.class).toInstance(new CalciteRulesManager(ImmutableSet.of()));
-        TestRequestLogger testRequestLogger = new TestRequestLogger();
-        binder.bind(RequestLogger.class).toInstance(testRequestLogger);
-        binder.bind(CatalogResolver.class).toInstance(CatalogResolver.NULL_RESOLVER);
-        binder.bind(ServiceEmitter.class).to(NoopServiceEmitter.class);
-        binder.bind(QueryScheduler.class)
-            .toProvider(QuerySchedulerProvider.class)
-            .in(LazySingleton.class);
-        binder.bind(AuthenticatorMapper.class).toInstance(CalciteTests.TEST_AUTHENTICATOR_MAPPER);
-        binder.bind(AuthorizerMapper.class).toInstance(CalciteTests.TEST_AUTHORIZER_MAPPER);
-        binder.bind(Escalator.class).toInstance(CalciteTests.TEST_AUTHENTICATOR_ESCALATOR);
-      }
     }
 
     static class TestMSQSqlModule extends TestDruidModule {
