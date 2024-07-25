16f44b21b2fb005bc9e658258148b8ee66893ef3
sreemanamala <sree.manamala@imply.io>
2024-07-25 08:18:40 +0530

    refactor

--- processing/src/test/java/org/apache/druid/query/rowsandcols/concrete/ColumnBasedFrameRowsAndColumnsTest.java
+++ processing/src/test/java/org/apache/druid/query/rowsandcols/concrete/ColumnBasedFrameRowsAndColumnsTest.java
@@ -34,17 +34,23 @@ public class ColumnBasedFrameRowsAndColumnsTest extends RowsAndColumnsTestBase
   }
 
   public static Function<MapOfColumnsRowsAndColumns, ColumnBasedFrameRowsAndColumns> MAKER = input -> {
-
     return buildFrame(input);
   };
 
   public static ColumnBasedFrameRowsAndColumns buildFrame(MapOfColumnsRowsAndColumns input)
   {
-    LazilyDecoratedRowsAndColumns rac = new LazilyDecoratedRowsAndColumns(input, null, null, null, OffsetLimit.limit(Integer.MAX_VALUE), null, null);
+    LazilyDecoratedRowsAndColumns rac = new LazilyDecoratedRowsAndColumns(
+        input,
+        null,
+        null,
+        null,
+        OffsetLimit.limit(Integer.MAX_VALUE),
+        null,
+        null
+    );
 
     rac.numRows(); // materialize
 
     return (ColumnBasedFrameRowsAndColumns) rac.getBase();
   }
-
 }
