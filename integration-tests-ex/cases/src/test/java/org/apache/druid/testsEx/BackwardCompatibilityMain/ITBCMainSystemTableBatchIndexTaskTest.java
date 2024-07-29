6c896ecad4bb0edc6cbdedd5fa5d7d15c3a7c6cf
rishabh singh <rishabh.singh@imply.io>
2024-07-30 00:11:11 +0530

    Fix tests

--- integration-tests-ex/cases/src/test/java/org/apache/druid/testsEx/BackwardCompatibilityMain/ITBCMainSystemTableBatchIndexTaskTest.java
+++ integration-tests-ex/cases/src/test/java/org/apache/druid/testsEx/BackwardCompatibilityMain/ITBCMainSystemTableBatchIndexTaskTest.java
@@ -21,12 +21,12 @@ package org.apache.druid.testsEx.BackwardCompatibilityMain;
 
 import org.apache.druid.testsEx.categories.BackwardCompatibilityMain;
 import org.apache.druid.testsEx.config.DruidTestRunner;
-import org.apache.druid.testsEx.msq.MultiStageQuery;
+import org.apache.druid.testsEx.indexer.SystemTableBatchIndexTaskTest;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 @RunWith(DruidTestRunner.class)
 @Category(BackwardCompatibilityMain.class)
-public class ITBCMainMultiStageQuery extends MultiStageQuery
+public class ITBCMainSystemTableBatchIndexTaskTest extends SystemTableBatchIndexTaskTest
 {
 }
