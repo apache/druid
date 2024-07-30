7a06f735724f742d93b51f8e430b256dc9b41629
Amatya <amatya.avadhanula@imply.io>
2024-07-30 08:52:31 +0530

    Unused import

--- server/src/main/java/org/apache/druid/metadata/LockFilterPolicy.java
+++ server/src/main/java/org/apache/druid/metadata/LockFilterPolicy.java
@@ -27,7 +27,6 @@ import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * Specifies a policy to filter active locks held by a datasource
