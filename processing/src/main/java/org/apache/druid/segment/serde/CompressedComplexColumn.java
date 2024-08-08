1b10b27d10cf6100efe048fa816a01d15f6148e1
Clint Wylie <cwylie@apache.org>
2024-08-08 02:06:50 -0700

    simplify

--- processing/src/main/java/org/apache/druid/segment/serde/CompressedComplexColumn.java
+++ processing/src/main/java/org/apache/druid/segment/serde/CompressedComplexColumn.java
@@ -20,22 +20,15 @@
 package org.apache.druid.segment.serde;
 
 import org.apache.druid.collections.bitmap.ImmutableBitmap;
-import org.apache.druid.query.monomorphicprocessing.RuntimeShapeInspector;
-import org.apache.druid.segment.ColumnValueSelector;
-import org.apache.druid.segment.ObjectColumnSelector;
 import org.apache.druid.segment.column.ComplexColumn;
 import org.apache.druid.segment.data.CompressedVariableSizedBlobColumn;
 import org.apache.druid.segment.data.ObjectStrategy;
-import org.apache.druid.segment.data.ReadableOffset;
-import org.apache.druid.segment.vector.ReadableVectorInspector;
-import org.apache.druid.segment.vector.ReadableVectorOffset;
-import org.apache.druid.segment.vector.VectorObjectSelector;
 import org.apache.druid.utils.CloseableUtils;
 
 import javax.annotation.Nullable;
 import java.nio.ByteBuffer;
 
-public class CompressedComplexColumn implements ComplexColumn
+public final class CompressedComplexColumn implements ComplexColumn
 {
   private final String typeName;
   private final CompressedVariableSizedBlobColumn compressedColumn;
@@ -68,6 +61,7 @@ public class CompressedComplexColumn implements ComplexColumn
   }
 
   @Override
+  @Nullable
   public Object getRowValue(int rowNum)
   {
     if (nullValues.get(rowNum)) {
@@ -91,103 +85,5 @@ public class CompressedComplexColumn implements ComplexColumn
   public void close()
   {
     CloseableUtils.closeAndWrapExceptions(compressedColumn);
-  }
-
-  @Override
-  public ColumnValueSelector<?> makeColumnValueSelector(ReadableOffset offset)
-  {
-    return new ObjectColumnSelector()
-    {
-      @Nullable
-      @Override
-      public Object getObject()
-      {
-        if (nullValues.get(offset.getOffset())) {
-          return null;
-        }
-        final ByteBuffer valueBuffer = compressedColumn.get(offset.getOffset());
-        final ByteBuffer dataCopyBuffer = ByteBuffer.allocate(valueBuffer.remaining());
-        dataCopyBuffer.put(valueBuffer);
-        dataCopyBuffer.rewind();
-        return objectStrategy.fromByteBuffer(dataCopyBuffer, dataCopyBuffer.remaining());
-      }
-
-      @Override
-      public Class classOfObject()
-      {
-        return getClazz();
-      }
-
-      @Override
-      public void inspectRuntimeShape(RuntimeShapeInspector inspector)
-      {
-        inspector.visit("column", compressedColumn);
-        inspector.visit("nullValues", nullValues);
-      }
-    };
-  }
-
-  @Override
-  public VectorObjectSelector makeVectorObjectSelector(ReadableVectorOffset offset)
-  {
-    return new VectorObjectSelector()
-    {
-      final Object[] vector = new Object[offset.getMaxVectorSize()];
-
-      private int id = ReadableVectorInspector.NULL_ID;
-
-      @Override
-      public Object[] getObjectVector()
-      {
-        if (id == offset.getId()) {
-          return vector;
-        }
-
-        if (offset.isContiguous()) {
-          final int startOffset = offset.getStartOffset();
-          final int vectorSize = offset.getCurrentVectorSize();
-
-          for (int i = 0; i < vectorSize; i++) {
-            vector[i] = getForOffset(startOffset + i);
-          }
-        } else {
-          final int[] offsets = offset.getOffsets();
-          final int vectorSize = offset.getCurrentVectorSize();
-
-          for (int i = 0; i < vectorSize; i++) {
-            vector[i] = getForOffset(offsets[i]);
-
-          }
-        }
-
-        id = offset.getId();
-        return vector;
-      }
-
-      @Nullable
-      private Object getForOffset(int offset)
-      {
-        if (nullValues.get(offset)) {
-          return null;
-        }
-        final ByteBuffer valueBuffer = compressedColumn.get(offset);
-        final ByteBuffer dataCopyBuffer = ByteBuffer.allocate(valueBuffer.remaining());
-        dataCopyBuffer.put(valueBuffer);
-        dataCopyBuffer.rewind();
-        return objectStrategy.fromByteBuffer(dataCopyBuffer, dataCopyBuffer.remaining());
-      }
-
-      @Override
-      public int getCurrentVectorSize()
-      {
-        return offset.getCurrentVectorSize();
-      }
-
-      @Override
-      public int getMaxVectorSize()
-      {
-        return offset.getMaxVectorSize();
-      }
-    };
   }
 }
