7b3271d4a85e22b662120db0d6497f5d3d12940b
Gian Merlino <gianmerlino@gmail.com>
2024-08-21 14:55:02 -0700

    Add timeBoundaryInspector to test.

--- processing/src/test/java/org/apache/druid/query/CursorGranularizerTest.java
+++ processing/src/test/java/org/apache/druid/query/CursorGranularizerTest.java
@@ -33,8 +33,11 @@ import org.apache.druid.segment.CursorBuildSpec;
 import org.apache.druid.segment.CursorHolder;
 import org.apache.druid.segment.Cursors;
 import org.apache.druid.segment.IndexBuilder;
+import org.apache.druid.segment.QueryableIndex;
 import org.apache.druid.segment.QueryableIndexStorageAdapter;
+import org.apache.druid.segment.QueryableIndexTimeBoundaryInspector;
 import org.apache.druid.segment.StorageAdapter;
+import org.apache.druid.segment.TimeBoundaryInspector;
 import org.apache.druid.segment.column.ColumnType;
 import org.apache.druid.segment.column.RowSignature;
 import org.apache.druid.segment.incremental.IncrementalIndexSchema;
@@ -55,6 +58,7 @@ public class CursorGranularizerTest extends InitializedNullHandlingTest
   public final TemporaryFolder temporaryFolder = new TemporaryFolder();
 
   private StorageAdapter adapter;
+  private TimeBoundaryInspector timeBoundaryInspector;
 
   @Before
   public void setup() throws IOException
@@ -147,7 +151,9 @@ public class CursorGranularizerTest extends InitializedNullHandlingTest
                     )
                     .tmpDir(temporaryFolder.newFolder());
 
-    adapter = new QueryableIndexStorageAdapter(bob.buildMMappedIndex());
+    final QueryableIndex index = bob.buildMMappedIndex();
+    adapter = new QueryableIndexStorageAdapter(index);
+    timeBoundaryInspector = QueryableIndexTimeBoundaryInspector.create(index);
   }
 
   @Test
@@ -157,7 +163,7 @@ public class CursorGranularizerTest extends InitializedNullHandlingTest
       final Cursor cursor = cursorHolder.asCursor();
       CursorGranularizer granularizer = CursorGranularizer.create(
           cursor,
-          null,
+          timeBoundaryInspector,
           Order.ASCENDING,
           Granularities.HOUR,
           adapter.getInterval()
@@ -210,7 +216,7 @@ public class CursorGranularizerTest extends InitializedNullHandlingTest
       final Cursor cursor = cursorHolder.asCursor();
       CursorGranularizer granularizer = CursorGranularizer.create(
           cursor,
-          null,
+          timeBoundaryInspector,
           Order.DESCENDING,
           Granularities.HOUR,
           adapter.getInterval()
