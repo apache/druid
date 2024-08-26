apache/druid

commit 0603d5153d8177856687c957ae0061492c81e1d4

Segments sorted by non-time columns. (#16849)
* Segments primarily sorted by non-time columns.

Currently, segments are always sorted by __time, followed by the sort
order provided by the user via dimensionsSpec or CLUSTERED BY. Sorting
by __time enables efficient execution of queries involving time-ordering
or granularity. Time-ordering is a simple matter of reading the rows in
stored order, and granular cursors can be generated in streaming fashion.

However, for various workloads, it's better for storage footprint and
query performance to sort by arbitrary orders that do not start with __time.
With this patch, users can sort segments by such orders.

For spec-based ingestion, users add "useExplicitSegmentSortOrder: true" to
dimensionsSpec. The "dimensions" list determines the sort order. To
define a sort order that includes "__time", users explicitly
include a dimension named "__time".

For SQL-based ingestion, users set the context parameter
"useExplicitSegmentSortOrder: true". The CLUSTERED BY clause is then
used as the explicit segment sort order.

In both cases, when the new "useExplicitSegmentSortOrder" parameter is
false (the default), __time is implicitly prepended to the sort order,
as it always was prior to this patch.

The new parameter is experimental for two main reasons. First, such
segments can cause errors when loaded by older servers, due to violating
their expectations that timestamps are always monotonically increasing.
Second, even on newer servers, not all queries can run on non-time-sorted
segments. Scan queries involving time-ordering and any query involving
granularity will not run. (To partially mitigate this, a currently-undocumented
SQL feature "sqlUseGranularity" is provided. When set to false the SQL planner
avoids using "granularity".)

Changes on the write path:

1) DimensionsSpec can now optionally contain a __time dimension, which

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/sql/MSQTaskSqlEngine.java
@@ -23,6 +23,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.Inject;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+import org.apache.calcite.rel.RelCollation;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.RelRoot;
 import org.apache.calcite.rel.core.Project;
@@ -33,6 +35,7 @@ import org.apache.calcite.rel.type.RelDataTypeField;
 import org.apache.calcite.schema.Table;
 import org.apache.calcite.sql.dialect.CalciteSqlDialect;
 import org.apache.calcite.sql.type.SqlTypeName;
+import org.apache.druid.data.input.impl.DimensionsSpec;
 import org.apache.druid.error.DruidException;
 import org.apache.druid.error.InvalidInput;
 import org.apache.druid.error.InvalidSqlInput;
@@ -43,6 +46,7 @@ import org.apache.druid.msq.querykit.QueryKitUtils;
 import org.apache.druid.msq.util.ArrayIngestMode;
 import org.apache.druid.msq.util.DimensionSchemaUtils;
 import org.apache.druid.msq.util.MultiStageQueryContext;
+import org.apache.druid.query.QueryContext;
 import org.apache.druid.rpc.indexing.OverlordClient;
 import org.apache.druid.segment.column.ColumnHolder;
 import org.apache.druid.segment.column.ColumnType;
@@ -61,7 +65,6 @@ import org.apache.druid.sql.destination.IngestDestination;
 import org.apache.druid.sql.destination.TableDestination;
 
 import javax.annotation.Nullable;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -176,8 +179,7 @@ public class MSQTaskSqlEngine implements SqlEngine
   )
   {
     validateInsert(
-        relRoot.rel,
-        relRoot.fields,
+        relRoot,
         destination instanceof TableDestination
         ? plannerContext.getPlannerToolbox()
                         .rootSchema()
@@ -221,35 +223,38 @@ public class MSQTaskSqlEngine implements SqlEngine
    * Engine-specific validation that happens after the query is planned.
    */
   private static void validateInsert(
-      final RelNode rootRel,
-      final List<Entry<Integer, String>> fieldMappings,
+      final RelRoot relRoot,
       @Nullable Table targetTable,
       final PlannerContext plannerContext
   )
   {
-    final int timeColumnIndex = getTimeColumnIndex(fieldMappings);
+    final Map<String, Integer> outputFieldMap = validateNoDuplicateAliases(relRoot.fields);
+    final int timeColumnIndex = getTimeColumnIndex(outputFieldMap);
     final Granularity segmentGranularity = getSegmentGranularity(plannerContext);
-
-    validateNoDuplicateAliases(fieldMappings);
-    validateTimeColumnType(rootRel, timeColumnIndex);
+    validateTimeColumnType(relRoot.rel, timeColumnIndex);
     validateTimeColumnExistsIfNeeded(timeColumnIndex, segmentGranularity);
-    validateLimitAndOffset(rootRel, Granularities.ALL.equals(segmentGranularity));
-    validateTypeChanges(rootRel, fieldMappings, targetTable, plannerContext);
+    validateLimitAndOffset(relRoot.rel, Granularities.ALL.equals(segmentGranularity));
+    validateTypeChanges(relRoot.rel, relRoot.fields, targetTable, plannerContext);
+    validateSortOrderBeginsWithTimeIfRequired(relRoot.fields, relRoot.collation, plannerContext);
   }
 
   /**
    * SQL allows multiple output columns with the same name. However, we don't allow this for INSERT or REPLACE
    * queries, because we use these output names to generate columns in segments. They must be unique.
+   *
+   * @return map of output alias (rhs of the Pair) to position in the {@link RelRoot#rel} (lhs of the Pair)
    */
-  private static void validateNoDuplicateAliases(final List<Entry<Integer, String>> fieldMappings)
+  private static Map<String, Integer> validateNoDuplicateAliases(final List<Entry<Integer, String>> fieldMappings)
   {
-    final Set<String> aliasesSeen = new HashSet<>();
+    final Map<String, Integer> retVal = new Object2IntOpenHashMap<>();
 
     for (final Entry<Integer, String> field : fieldMappings) {
-      if (!aliasesSeen.add(field.getValue())) {
+      if (retVal.put(field.getValue(), field.getKey()) != null) {
         throw InvalidSqlInput.exception("Duplicate field in SELECT: [%s]", field.getValue());
       }
     }
+
+    return retVal;
   }
 
   /**
@@ -347,7 +352,7 @@ public class MSQTaskSqlEngine implements SqlEngine
    * MVDs and arrays are mixed. So, we detect them as invalid.
    *
    * @param rootRel        root rel
-   * @param fieldMappings  field mappings from {@link #validateInsert(RelNode, List, Table, PlannerContext)}
+   * @param fieldMappings  field mappings from {@link #validateInsert(RelRoot, Table, PlannerContext)}
    * @param targetTable    table we are inserting (or replacing) into, if any
    * @param plannerContext planner context
    */
@@ -422,20 +427,80 @@ public class MSQTaskSqlEngine implements SqlEngine
   }
 
   /**
-   * Returns the index of {@link ColumnHolder#TIME_COLUMN_NAME} within a list of field mappings from
-   * {@link #validateInsert(RelNode, List, Table, PlannerContext)}.
+   * Validate that the sort order given by CLUSTERED BY or {@link MultiStageQueryContext#getSortOrder(QueryContext)}
+   * begins with {@link ColumnHolder#TIME_COLUMN_NAME}, unless {@link MultiStageQueryContext#CTX_FORCE_TIME_SORT}
+   * is set.
    *
-   * Returns -1 if the list does not contain a time column.
+   * @param fieldMappings  field mappings from {@link #validateInsert(RelRoot, Table, PlannerContext)}
+   * @param rootCollation  collation of the root rel. Corresponds to the CLUSTERED BY
+   * @param plannerContext planner context
    */
-  private static int getTimeColumnIndex(final List<Entry<Integer, String>> fieldMappings)
+  private static void validateSortOrderBeginsWithTimeIfRequired(
+      final List<Entry<Integer, String>> fieldMappings,
+      final RelCollation rootCollation,
+      final PlannerContext plannerContext
+  )
   {
-    for (final Entry<Integer, String> field : fieldMappings) {
-      if (field.getValue().equals(ColumnHolder.TIME_COLUMN_NAME)) {
-        return field.getKey();
-      }
+    // Segment sort order is determined by the segmentSortOrder parameter if set. Otherwise it's determined by
+    // the rootCollation.
+
+    final QueryContext context = plannerContext.queryContext();
+
+    if (!MultiStageQueryContext.isForceSegmentSortByTime(context)) {
+      // Any sort order is allowed. Skip check.
+      return;
     }
 
-    return -1;
+    final List<String> contextSortOrder = MultiStageQueryContext.getSortOrder(context);
+
+    if (!contextSortOrder.isEmpty()) {
+      final boolean timeIsFirst = ColumnHolder.TIME_COLUMN_NAME.equals(contextSortOrder.get(0));
+
+      if (!timeIsFirst) {
+        throw InvalidSqlInput.exception(
+            "Context parameter[%s] must start with[%s] unless context parameter[%s] is set to[false]. %s",
+            MultiStageQueryContext.CTX_SORT_ORDER,
+            ColumnHolder.TIME_COLUMN_NAME,
+            MultiStageQueryContext.CTX_FORCE_TIME_SORT,
+            DimensionsSpec.WARNING_NON_TIME_SORT_ORDER
+        );
+      }
+    } else if (!rootCollation.getFieldCollations().isEmpty()) {
+      int timePosition = -1;
+      for (int i = 0; i < rootCollation.getFieldCollations().size(); i++) {
+        final String fieldCollationName =
+            fieldMappings.get(rootCollation.getFieldCollations().get(i).getFieldIndex()).getValue();
+        if (ColumnHolder.TIME_COLUMN_NAME.equals(fieldCollationName)) {
+          timePosition = i;
+          break;
+        }
+      }
+
+      if (timePosition > 0) {
+        throw InvalidSqlInput.exception(
+            "Sort order (CLUSTERED BY) cannot include[%s] in position[%d] unless context parameter[%s] "
+            + "is set to[false]. %s",
+            ColumnHolder.TIME_COLUMN_NAME,
+            timePosition,
+            MultiStageQueryContext.CTX_FORCE_TIME_SORT,
+            DimensionsSpec.WARNING_NON_TIME_SORT_ORDER
+        );
+      }
+    }
+  }
+
+  /**
+   * Returns the index of {@link ColumnHolder#TIME_COLUMN_NAME} within a list of field mappings from
+   * {@link #validateInsert(RelRoot, Table, PlannerContext)}.
+   *
+   * @param outputFieldMapping mapping from {@link #validateNoDuplicateAliases(List)}
+   *
+   * @return field position, or -1 if the list does not contain a time column.
+   */
+  private static int getTimeColumnIndex(final Map<String, Integer> outputFieldMapping)
+  {
+    final Integer position = outputFieldMapping.get(ColumnHolder.TIME_COLUMN_NAME);
+    return position != null ? position : -1;
   }
 
   /**

commit fbe1f4be397c9eeb5b9b47c7c6dbf75687175452

MSQ: Fix validation of time position in collations.
It is possible for the collation to refer to a field that isn't mapped,
such as when the DML includes "CLUSTERED BY some_function(some_field)".
In this case, the collation refers to a projected column that is not
part of the field mappings. Prior to this patch, that would lead to an
out of bounds list access on fieldMappings.

This patch fixes the problem by identifying the position of __time in
the fieldMappings first, rather than retrieving each collation field
from fieldMappings.

Fixes a bug introduced in #16849.

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/sql/MSQTaskSqlEngine.java
@@ -466,22 +466,29 @@ public class MSQTaskSqlEngine implements SqlEngine
         );
       }
     } else if (!rootCollation.getFieldCollations().isEmpty()) {
-      int timePosition = -1;
-      for (int i = 0; i < rootCollation.getFieldCollations().size(); i++) {
-        final String fieldCollationName =
-            fieldMappings.get(rootCollation.getFieldCollations().get(i).getFieldIndex()).getValue();
-        if (ColumnHolder.TIME_COLUMN_NAME.equals(fieldCollationName)) {
-          timePosition = i;
+      int timePositionInRow = -1;
+      for (int i = 0; i < fieldMappings.size(); i++) {
+        Entry<Integer, String> entry = fieldMappings.get(i);
+        if (ColumnHolder.TIME_COLUMN_NAME.equals(entry.getValue())) {
+          timePositionInRow = i;
           break;
         }
       }
 
-      if (timePosition > 0) {
+      int timePositionInCollation = -1;
+      for (int i = 0; i < rootCollation.getFieldCollations().size(); i++) {
+        if (rootCollation.getFieldCollations().get(i).getFieldIndex() == timePositionInRow) {
+          timePositionInCollation = i;
+          break;
+        }
+      }
+
+      if (timePositionInCollation > 0) {
         throw InvalidSqlInput.exception(
             "Sort order (CLUSTERED BY) cannot include[%s] in position[%d] unless context parameter[%s] "
             + "is set to[false]. %s",
             ColumnHolder.TIME_COLUMN_NAME,
-            timePosition,
+            timePositionInCollation,
             MultiStageQueryContext.CTX_FORCE_TIME_SORT,
             DimensionsSpec.WARNING_NON_TIME_SORT_ORDER
         );

extensions-core/multi-stage-query/src/test/java/org/apache/druid/msq/exec/MSQReplaceTest.java
@@ -278,6 +278,86 @@ public class MSQReplaceTest extends MSQTestBase
   @ParameterizedTest(name = "{index}:with context {0}")
   public void testReplaceOnFooWithAllClusteredByDimExplicitSort(String contextName, Map<String, Object> context)
   {
+    // Tests [CLUSTERED BY LOWER(dim1)], i.e. an expression that is not actually stored.
+    RowSignature rowSignature = RowSignature.builder()
+                                            .add("__time", ColumnType.LONG)
+                                            .add("dim1", ColumnType.STRING)
+                                            .add("m1", ColumnType.FLOAT)
+                                            .build();
+
+    DataSegment existingDataSegment0 = DataSegment.builder()
+                                                  .interval(Intervals.of("2000-01-01T/2000-01-04T"))
+                                                  .size(50)
+                                                  .version(MSQTestTaskActionClient.VERSION)
+                                                  .dataSource("foo")
+                                                  .build();
+
+    DataSegment existingDataSegment1 = DataSegment.builder()
+                                                  .interval(Intervals.of("2001-01-01T/2001-01-04T"))
+                                                  .size(50)
+                                                  .version(MSQTestTaskActionClient.VERSION)
+                                                  .dataSource("foo")
+                                                  .build();
+
+    Mockito.doCallRealMethod()
+           .doReturn(ImmutableSet.of(existingDataSegment0, existingDataSegment1))
+           .when(testTaskActionClient)
+           .submit(new RetrieveUsedSegmentsAction("foo", ImmutableList.of(Intervals.ETERNITY)));
+
+    testIngestQuery().setSql(" REPLACE INTO foo OVERWRITE ALL "
+                             + "SELECT __time, dim1, m1 "
+                             + "FROM foo "
+                             + "PARTITIONED BY ALL "
+                             + "CLUSTERED BY LOWER(dim1)")
+                     .setExpectedDataSource("foo")
+                     .setExpectedRowSignature(rowSignature)
+                     .setQueryContext(context)
+                     .setExpectedDestinationIntervals(Intervals.ONLY_ETERNITY)
+                     .setExpectedSegments(
+                         ImmutableSet.of(
+                             SegmentId.of("foo", Intervals.ETERNITY, "test", 0)
+                         )
+                     )
+                     .setExpectedShardSpec(NumberedShardSpec.class)
+                     .setExpectedResultRows(
+                         ImmutableList.of(
+                             new Object[]{946684800000L, NullHandling.sqlCompatible() ? "" : null, 1.0f},
+                             new Object[]{946771200000L, "10.1", 2.0f},
+                             new Object[]{946857600000L, "2", 3.0f},
+                             new Object[]{978307200000L, "1", 4.0f},
+                             new Object[]{978393600000L, "def", 5.0f},
+                             new Object[]{978480000000L, "abc", 6.0f}
+                         )
+                     )
+                     .setExpectedSegmentGenerationProgressCountersForStageWorker(
+                         CounterSnapshotMatcher
+                             .with().segmentRowsProcessed(6),
+                         1, 0
+                     )
+                     .setExpectedLastCompactionState(
+                         expectedCompactionState(
+                             context,
+                             Collections.singletonList("dim1"),
+                             DimensionsSpec.builder()
+                                           .setDimensions(
+                                               ImmutableList.of(
+                                                   new StringDimensionSchema("dim1"),
+                                                   new FloatDimensionSchema("m1")
+                                               )
+                                           )
+                                           .setDimensionExclusions(Collections.singletonList("__time"))
+                                           .build(),
+                             GranularityType.ALL,
+                             Intervals.ETERNITY
+                         )
+                     )
+                     .verifyResults();
+  }
+
+  @MethodSource("data")
+  @ParameterizedTest(name = "{index}:with context {0}")
+  public void testReplaceOnFooWithAllClusteredByExpression(String contextName, Map<String, Object> context)
+  {
     RowSignature rowSignature = RowSignature.builder()
                                             .add("__time", ColumnType.LONG)
                                             .add("dim1", ColumnType.STRING)