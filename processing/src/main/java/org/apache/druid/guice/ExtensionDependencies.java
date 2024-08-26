7adb61c038f1961652137bc1d7b58e5824230747
George Wu <georgew5656@berkeley.edu>
2024-08-26 13:12:19 -0400

    cleanup extra lines

--- processing/src/main/java/org/apache/druid/guice/ExtensionDependencies.java
+++ processing/src/main/java/org/apache/druid/guice/ExtensionDependencies.java
@@ -1,53 +1,29 @@
 package org.apache.druid.guice;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import java.util.ArrayList;
 
 public class ExtensionDependencies {
   @JsonProperty("name")
-  private String name;
+  final private String name;
 
   @JsonProperty("dependencies")
-  private List<String> dependencies;
+  final private List<String> dependencies;
 
-  // Default constructor
-  public ExtensionDependencies() {
-    this.dependencies = new ArrayList<>();
-  }
-
-  // Constructor with parameters
-  public ExtensionDependencies(String name, List<String> dependencies) {
+  @JsonCreator
+  public ExtensionDependencies(final String name, final List<String> dependencies) {
     this.name = name;
     this.dependencies = dependencies != null ? dependencies : new ArrayList<>();
   }
 
-  // Getter for name
   public String getName() {
     return name;
   }
 
-  // Setter for name
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  // Getter for dependencies
   public List<String> getDependencies() {
     return dependencies;
   }
-
-  // Setter for dependencies
-  public void setDependencies(List<String> dependencies) {
-    this.dependencies = dependencies != null ? dependencies : new ArrayList<>();
-  }
-
-  // toString method for easy printing
-  @Override
-  public String toString() {
-    return "ExtensionDependencies{" +
-        "name='" + name + '\'' +
-        ", dependencies=" + dependencies +
-        '}';
   }
 }
