5beae639150054a0875dcd6d5e1478fb384458df
rishabh singh <rishabh.singh@imply.io>
2024-09-06 12:54:15 +0530

    Add produces annotation to ParallelIndexSupervisorTask#report

--- indexing-service/src/main/java/org/apache/druid/indexing/common/task/batch/parallel/ParallelIndexSupervisorTask.java
+++ indexing-service/src/main/java/org/apache/druid/indexing/common/task/batch/parallel/ParallelIndexSupervisorTask.java
@@ -1400,6 +1400,7 @@ public class ParallelIndexSupervisorTask extends AbstractBatchIndexTask
   @POST
   @Path("/report")
   @Consumes(SmileMediaTypes.APPLICATION_JACKSON_SMILE)
+  @Produces(SmileMediaTypes.APPLICATION_JACKSON_SMILE)
   public Response report(
       SubTaskReport report,
       @Context final HttpServletRequest req
