7076e3032db3433aa4d3286f569d38b71f0b60a4
rishabh singh <rishabh.singh@imply.io>
2024-09-17 22:22:42 +0530

    checkstyle

--- server/src/test/java/org/apache/druid/segment/metadata/FingerprintGeneratorTest.java
+++ server/src/test/java/org/apache/druid/segment/metadata/FingerprintGeneratorTest.java
@@ -32,7 +32,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
