d13f14257129f48b50b7e86b34a337ed29f1ec2b
Pankaj Kumar <pkumar@confluent.io>
2024-09-23 10:00:20 +0530

    Removing extra changes and minor fix

--- server/src/main/java/org/apache/druid/guice/http/JettyHttpClientModule.java
+++ server/src/main/java/org/apache/druid/guice/http/JettyHttpClientModule.java
@@ -23,27 +23,16 @@ import com.google.common.base.Preconditions;
 import com.google.inject.Binder;
 import com.google.inject.Binding;
 import com.google.inject.Module;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
 import org.apache.druid.guice.JsonConfigProvider;
 import org.apache.druid.guice.LazySingleton;
 import org.apache.druid.guice.annotations.Global;
 import org.apache.druid.java.util.common.lifecycle.Lifecycle;
-import org.apache.druid.java.util.emitter.service.ServiceEmitter;
-import org.apache.druid.java.util.emitter.service.ServiceMetricEvent;
-import org.apache.druid.java.util.metrics.AbstractMonitor;
-import org.apache.druid.java.util.metrics.MonitorUtils;
-import org.apache.druid.server.metrics.DataSourceTaskIdHolder;
-import org.apache.druid.server.metrics.MetricsModule;
-import org.apache.druid.server.metrics.MonitorsConfig;
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
-import javax.inject.Provider;
 import javax.net.ssl.SSLContext;
 import java.lang.annotation.Annotation;
-import java.util.Map;
 
 /**
  *
@@ -73,12 +62,10 @@ public class JettyHttpClientModule implements Module
           .toProvider(new HttpClientProvider(annotationClazz))
           .in(LazySingleton.class);
 
-    MetricsModule.register(binder, JettyHttpClientModule.JettyMonitor.class);
   }
 
   public static class HttpClientProvider extends AbstractHttpClientProvider<HttpClient>
   {
-    private QueuedThreadPool threadPool;
 
     public HttpClientProvider(Class<? extends Annotation> annotation)
     {
@@ -105,9 +92,9 @@ public class JettyHttpClientModule implements Module
       httpClient.setMaxRequestsQueuedPerDestination(config.getNumRequestsQueued());
       httpClient.setConnectTimeout(config.getClientConnectTimeout());
       httpClient.setRequestBufferSize(config.getRequestBuffersize());
-      threadPool = new QueuedThreadPool(config.getNumMaxThreads());
-      threadPool.setName(JettyHttpClientModule.class.getSimpleName() + "-threadPool-" + threadPool.hashCode());
-      httpClient.setExecutor(threadPool);
+      final QueuedThreadPool pool = new QueuedThreadPool(config.getNumMaxThreads());
+      pool.setName(JettyHttpClientModule.class.getSimpleName() + "-threadPool-" + pool.hashCode());
+      httpClient.setExecutor(pool);
 
       final Lifecycle lifecycle = getLifecycleProvider().get();
 
@@ -138,36 +125,6 @@ public class JettyHttpClientModule implements Module
       }
 
       return httpClient;
-    }
-  }
-
-  @Provides
-  @Singleton
-  public JettyHttpClientModule.JettyMonitor getJettyMonitor(DataSourceTaskIdHolder dataSourceTaskIdHolder, Provider<HttpClientProvider> httpClientProvider)
-  {
-    return new JettyHttpClientModule.JettyMonitor(dataSourceTaskIdHolder.getDataSource(), dataSourceTaskIdHolder.getTaskId(), httpClientProvider.get().threadPool);
-  }
-
-  public static class JettyMonitor extends AbstractMonitor
-  {
-    private final Map<String, String[]> dimensions;
-    private final QueuedThreadPool threadPool;
-
-    public JettyMonitor(String dataSource, String taskId, QueuedThreadPool threadPool)
-    {
-      this.dimensions = MonitorsConfig.mapOfDatasourceAndTaskID(dataSource, taskId);
-      this.threadPool = threadPool;
-    }
-
-    @Override
-    public boolean doMonitor(ServiceEmitter emitter)
-    {
-      final ServiceMetricEvent.Builder builder = new ServiceMetricEvent.Builder();
-      MonitorUtils.addDimensionsToBuilder(builder, dimensions);
-      if (threadPool != null) {
-        emitter.emit(builder.setMetric("jetty/httpClient/threadpool/queueSize", threadPool.getQueueSize()));
-      }
-      return true;
     }
   }
 }
