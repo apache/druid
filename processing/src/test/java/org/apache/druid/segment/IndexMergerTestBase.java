8b1504f964a24b9a715416222a1c30421faf4137
Clint Wylie <cwylie@apache.org>
2024-10-05 02:27:00 -0700

    fix test

--- processing/src/test/java/org/apache/druid/segment/IndexMergerTestBase.java
+++ processing/src/test/java/org/apache/druid/segment/IndexMergerTestBase.java
@@ -411,7 +411,7 @@ public class IndexMergerTestBase extends InitializedNullHandlingTest
             Granularities.NONE,
             Boolean.TRUE,
             Cursors.ascendingTimeOrder(),
-            Collections.emptyList()
+            null
         ),
         index.getMetadata()
     );
