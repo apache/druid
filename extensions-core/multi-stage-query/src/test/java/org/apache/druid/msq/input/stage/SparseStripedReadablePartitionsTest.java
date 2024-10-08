da86521f92073883886580d5788ffa6152fb7058
Gian Merlino <gianmerlino@gmail.com>
2024-10-07 23:34:50 -0700

    MSQ: Allow for worker gaps.
    In a Dart query, all Historicals are given worker IDs, but not all of them
    are going to actually be started or receive work orders. This can create gaps
    in the set of workers. For example, workers 1 and 3 could have work assigned
    while workers 0 and 2 do not.
    
    This patch updates ControllerStageTracker and WorkerInputs to handle such
    gaps, by using the set of actual worker numbers, rather than 0..workerCount,
    in various places.

--- extensions-core/multi-stage-query/src/test/java/org/apache/druid/msq/input/stage/SparseStripedReadablePartitionsTest.java
+++ extensions-core/multi-stage-query/src/test/java/org/apache/druid/msq/input/stage/SparseStripedReadablePartitionsTest.java
@@ -23,68 +23,50 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import it.unimi.dsi.fastutil.ints.IntAVLTreeSet;
+import it.unimi.dsi.fastutil.ints.IntSet;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.apache.druid.msq.guice.MSQIndexingModule;
 import org.apache.druid.segment.TestHelper;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class StripedReadablePartitionsTest
+public class SparseStripedReadablePartitionsTest
 {
-  @Test
-  public void testFromDenseSet()
-  {
-    // Tests that when ReadablePartitions.striped is called with a dense set, we get StripedReadablePartitions.
-
-    final IntAVLTreeSet workers = new IntAVLTreeSet();
-    workers.add(0);
-    workers.add(1);
-
-    final ReadablePartitions readablePartitionsFromSet = ReadablePartitions.striped(1, workers, 3);
-
-    MatcherAssert.assertThat(
-        readablePartitionsFromSet,
-        CoreMatchers.instanceOf(StripedReadablePartitions.class)
-    );
-
-    Assert.assertEquals(
-        ReadablePartitions.striped(1, 2, 3),
-        readablePartitionsFromSet
-    );
-  }
-
   @Test
   public void testPartitionNumbers()
   {
-    final StripedReadablePartitions partitions = (StripedReadablePartitions) ReadablePartitions.striped(1, 2, 3);
+    final SparseStripedReadablePartitions partitions =
+        (SparseStripedReadablePartitions) ReadablePartitions.striped(1, new IntAVLTreeSet(new int[]{1, 3}), 3);
     Assert.assertEquals(ImmutableSet.of(0, 1, 2), partitions.getPartitionNumbers());
   }
 
   @Test
-  public void testNumWorkers()
+  public void testWorkers()
   {
-    final StripedReadablePartitions partitions = (StripedReadablePartitions) ReadablePartitions.striped(1, 2, 3);
-    Assert.assertEquals(2, partitions.getNumWorkers());
+    final SparseStripedReadablePartitions partitions =
+        (SparseStripedReadablePartitions) ReadablePartitions.striped(1, new IntAVLTreeSet(new int[]{1, 3}), 3);
+    Assert.assertEquals(IntSet.of(1, 3), partitions.getWorkers());
   }
 
   @Test
   public void testStageNumber()
   {
-    final StripedReadablePartitions partitions = (StripedReadablePartitions) ReadablePartitions.striped(1, 2, 3);
+    final SparseStripedReadablePartitions partitions =
+        (SparseStripedReadablePartitions) ReadablePartitions.striped(1, new IntAVLTreeSet(new int[]{1, 3}), 3);
     Assert.assertEquals(1, partitions.getStageNumber());
   }
 
   @Test
   public void testSplit()
   {
-    final ReadablePartitions partitions = ReadablePartitions.striped(1, 2, 3);
+    final IntAVLTreeSet workers = new IntAVLTreeSet(new int[]{1, 3});
+    final SparseStripedReadablePartitions partitions =
+        (SparseStripedReadablePartitions) ReadablePartitions.striped(1, workers, 3);
 
     Assert.assertEquals(
         ImmutableList.of(
-            new StripedReadablePartitions(1, 2, new IntAVLTreeSet(new int[]{0, 2})),
-            new StripedReadablePartitions(1, 2, new IntAVLTreeSet(new int[]{1}))
+            new SparseStripedReadablePartitions(1, workers, new IntAVLTreeSet(new int[]{0, 2})),
+            new SparseStripedReadablePartitions(1, workers, new IntAVLTreeSet(new int[]{1}))
         ),
         partitions.split(2)
     );
@@ -96,7 +78,8 @@ public class StripedReadablePartitionsTest
     final ObjectMapper mapper = TestHelper.makeJsonMapper()
                                           .registerModules(new MSQIndexingModule().getJacksonModules());
 
-    final ReadablePartitions partitions = ReadablePartitions.striped(1, 2, 3);
+    final IntAVLTreeSet workers = new IntAVLTreeSet(new int[]{1, 3});
+    final ReadablePartitions partitions = ReadablePartitions.striped(1, workers, 3);
 
     Assert.assertEquals(
         partitions,
@@ -110,6 +93,6 @@ public class StripedReadablePartitionsTest
   @Test
   public void testEquals()
   {
-    EqualsVerifier.forClass(StripedReadablePartitions.class).usingGetClass().verify();
+    EqualsVerifier.forClass(SparseStripedReadablePartitions.class).usingGetClass().verify();
   }
 }
