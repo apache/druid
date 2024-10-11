apache/druid

commit 878adff9aaa8d28ddbba0119c44dae4809dd5744

MSQ profile for Brokers and Historicals. (#17140)
This patch adds a profile of MSQ named "Dart" that runs on Brokers and
Historicals, and which is compatible with the standard SQL query API.
For more high-level description, and notes on future work, refer to #17139.

This patch contains the following changes, grouped into packages.

Controller (org.apache.druid.msq.dart.controller):

The controller runs on Brokers. Main classes are,

- DartSqlResource, which serves /druid/v2/sql/dart/.
- DartSqlEngine and DartQueryMaker, the entry points from SQL that actually
  run the MSQ controller code.
- DartControllerContext, which configures the MSQ controller.
- DartMessageRelays, which sets up relays (see "message relays" below) to read
  messages from workers' DartControllerClients.
- DartTableInputSpecSlicer, which assigns work based on a TimelineServerView.

Worker (org.apache.druid.msq.dart.worker)

The worker runs on Historicals. Main classes are,

- DartWorkerResource, which supplies the regular MSQ WorkerResource, plus
  Dart-specific APIs.
- DartWorkerRunner, which runs MSQ worker code.
- DartWorkerContext, which configures the MSQ worker.
- DartProcessingBuffersProvider, which provides processing buffers from
  sliced-up merge buffers.
- DartDataSegmentProvider, which provides segments from the Historical's
  local cache.

Message relays (org.apache.druid.messages):

To avoid the need for Historicals to contact Brokers during a query, which
would create opportunities for queries to get stuck, all connections are
opened from Broker to Historical. This is made possible by a message relay
system, where the relay server (worker) has an outbox of messages.

The relay client (controller) connects to the outbox and retrieves messages.
Code for this system lives in the "server" package to keep it separate from
the MSQ extension and make it easier to maintain. The worker-to-controller

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/rpc/WorkerResource.java
@@ -56,6 +56,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class WorkerResource
 {
@@ -104,6 +105,8 @@ public class WorkerResource
         worker.readStageOutput(new StageId(queryId, stageNumber), partitionNumber, offset);
 
     final AsyncContext asyncContext = req.startAsync();
+    final AtomicBoolean responseResolved = new AtomicBoolean();
+
     asyncContext.setTimeout(GET_CHANNEL_DATA_TIMEOUT);
     asyncContext.addListener(
         new AsyncListener()
@@ -116,6 +119,10 @@ public class WorkerResource
           @Override
           public void onTimeout(AsyncEvent event)
           {
+            if (responseResolved.compareAndSet(false, true)) {
+              return;
+            }
+
             HttpServletResponse response = (HttpServletResponse) asyncContext.getResponse();
             response.setStatus(HttpServletResponse.SC_OK);
             event.getAsyncContext().complete();
@@ -144,7 +151,11 @@ public class WorkerResource
           @Override
           public void onSuccess(final InputStream inputStream)
           {
-            HttpServletResponse response = (HttpServletResponse) asyncContext.getResponse();
+            if (!responseResolved.compareAndSet(false, true)) {
+              return;
+            }
+
+            final HttpServletResponse response = (HttpServletResponse) asyncContext.getResponse();
 
             try (final OutputStream outputStream = response.getOutputStream()) {
               if (inputStream == null) {
@@ -188,7 +199,7 @@ public class WorkerResource
           @Override
           public void onFailure(Throwable e)
           {
-            if (!dataFuture.isCancelled()) {
+            if (responseResolved.compareAndSet(false, true)) {
               try {
                 HttpServletResponse response = (HttpServletResponse) asyncContext.getResponse();
                 response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);

commit 1fdd30b8545acd6db477c401072cb2179d7570f4

MSQ WorkerResource: Fix timeout handler for httpGetChannelData.
The timeout handler should fire if the response has not been handled yet
(i.e. if responseResolved was previously false). However, it erroneously
fires only if the response *was* handled. This causes HTTP 500 errors if
the timeout actually does fire. The timeout is 30 seconds, which can be
hit during pipelined queries, if an earlier stage of the query hasn't
produced its first frame within 30 seconds.

This fixes a regression introduced in #17140.

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/rpc/WorkerResource.java
@@ -120,12 +120,10 @@ public class WorkerResource
           public void onTimeout(AsyncEvent event)
           {
             if (responseResolved.compareAndSet(false, true)) {
-              return;
+              HttpServletResponse response = (HttpServletResponse) asyncContext.getResponse();
+              response.setStatus(HttpServletResponse.SC_OK);
+              event.getAsyncContext().complete();
             }
-
-            HttpServletResponse response = (HttpServletResponse) asyncContext.getResponse();
-            response.setStatus(HttpServletResponse.SC_OK);
-            event.getAsyncContext().complete();
           }
 
           @Override