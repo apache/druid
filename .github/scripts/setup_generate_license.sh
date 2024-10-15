494c26def3d35d453dd3ab7bf2909bad4c91cd5c
abhishekagarwal87 <1477457+abhishekagarwal87@users.noreply.github.com>
2024-10-15 21:12:41 +0530

    no pip

--- .github/scripts/setup_generate_license.sh
+++ .github/scripts/setup_generate_license.sh
@@ -18,6 +18,5 @@
 set -e
 
 sudo apt-get update && sudo apt-get install python3 -y
-curl https://bootstrap.pypa.io/pip/3.7/get-pip.py | sudo -H python3
 pip3 install wheel  # install wheel first explicitly
 pip3 install --upgrade pyyaml
