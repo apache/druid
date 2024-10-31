5c69e1b6be37fd535ff55786516d221da1d631b5
Zoltan Haindrich <kirk@rxd.hu>
2024-10-31 17:14:48 +0000

    cleanup; move to innerclass/etc

--- processing/src/main/java/org/apache/druid/query/union/UnionQueryLogic.java
+++ processing/src/main/java/org/apache/druid/query/union/UnionQueryLogic.java
@@ -19,16 +19,84 @@
 
 package org.apache.druid.query.union;
 
-import org.apache.druid.query.AbstractQueryLogic;
+import com.google.inject.Inject;
+import org.apache.druid.java.util.common.guava.Sequence;
+import org.apache.druid.java.util.common.guava.Sequences;
 import org.apache.druid.query.Query;
+import org.apache.druid.query.QueryLogic;
+import org.apache.druid.query.QueryPlus;
 import org.apache.druid.query.QueryRunner;
+import org.apache.druid.query.QueryRunnerFactoryConglomerate;
 import org.apache.druid.query.QuerySegmentWalker;
+import org.apache.druid.query.ToolChestBasedResultSerializedRunner;
+import org.apache.druid.query.context.ResponseContext;
 
-public class UnionQueryLogic extends AbstractQueryLogic
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnionQueryLogic implements QueryLogic
 {
+  protected QueryRunnerFactoryConglomerate conglomerate;
+
+  @Inject
+  public void initialize(QueryRunnerFactoryConglomerate conglomerate)
+  {
+    this.conglomerate = conglomerate;
+  }
+
   @Override
   public <T> QueryRunner<Object> entryPoint(Query<T> query, QuerySegmentWalker walker)
   {
     return new UnionQueryRunner((UnionQuery) query, conglomerate, walker);
+  }
+
+  static class UnionQueryRunner implements QueryRunner<Object>
+  {
+    private final QueryRunnerFactoryConglomerate conglomerate;
+    private final QuerySegmentWalker walker;
+    private final List<QueryRunner> runners;
+
+    public UnionQueryRunner(
+        UnionQuery query,
+        QueryRunnerFactoryConglomerate conglomerate,
+        QuerySegmentWalker walker)
+    {
+      this.conglomerate = conglomerate;
+      this.walker = walker;
+      this.runners = makeSubQueryRunners(query);
+    }
+
+    private List<QueryRunner> makeSubQueryRunners(UnionQuery unionQuery)
+    {
+      List<QueryRunner> runners = new ArrayList<>();
+      for (Query<?> query : unionQuery.queries) {
+        runners.add(buildRunnerFor(query));
+      }
+      return runners;
+    }
+
+    private QueryRunner<?> buildRunnerFor(Query<?> query)
+    {
+      QueryLogic queryLogic = conglomerate.getQueryLogic(query);
+      if (queryLogic != null) {
+        return queryLogic.entryPoint(query, walker);
+      }
+      return new ToolChestBasedResultSerializedRunner(query, walker, conglomerate.getToolChest(query));
+    }
+
+    @Override
+    public Sequence<Object> run(QueryPlus<Object> queryPlus, ResponseContext responseContext)
+    {
+      UnionQuery unionQuery = queryPlus.unwrapQuery(UnionQuery.class);
+
+      List<Sequence<Object>> seqs = new ArrayList<>();
+      for (int i = 0; i < runners.size(); i++) {
+        Query<?> q = unionQuery.queries.get(i);
+        QueryRunner runner = runners.get(i);
+        Sequence run = runner.run(queryPlus.withQuery(q), responseContext);
+        seqs.add(run);
+      }
+      return Sequences.concat(seqs);
+    }
   }
 }
