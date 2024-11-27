f1f19eead7900dc7f39e1cf2e82bf7540de55814
cryptoe <karankumar1100@gmail.com>
2024-11-27 18:22:52 +0530

    Adding leader and not being leader logging on the overlord.

--- indexing-service/src/main/java/org/apache/druid/indexing/overlord/DruidOverlord.java
+++ indexing-service/src/main/java/org/apache/druid/indexing/overlord/DruidOverlord.java
@@ -106,7 +106,7 @@ public class DruidOverlord
         giant.lock();
 
         // I AM THE MASTER OF THE UNIVERSE.
-        log.info("By the power of Grayskull, I have the power!");
+        log.info("By the power of Grayskull, I have the power. I am the leader");
 
         try {
           final TaskRunner taskRunner = runnerFactory.build();
@@ -193,6 +193,7 @@ public class DruidOverlord
         giant.lock();
         try {
           initialized = false;
+          log.info("I am no longer the leader...");
           final Lifecycle leaderLifecycle = leaderLifecycleRef.getAndSet(null);
 
           if (leaderLifecycle != null) {
