8b90f20dc8fdcd685fdc3c27e159a2641b1ed246
Zoltan Haindrich <kirk@rxd.hu>
2024-12-04 11:06:44 +0000

    apply rule later; retain benefits of AggregateRemove

--- sql/src/main/java/org/apache/druid/sql/calcite/planner/CalciteRulesManager.java
+++ sql/src/main/java/org/apache/druid/sql/calcite/planner/CalciteRulesManager.java
@@ -493,9 +493,9 @@ public class CalciteRulesManager
 
     // Calcite rules.
     rules.addAll(BASE_RULES);
-    rules.add(new DruidAggregateCaseToFilterRule(plannerContext.queryContext().isExtendedFilteredSumRewrite()));
     rules.addAll(ABSTRACT_RULES);
     rules.addAll(ABSTRACT_RELATIONAL_RULES);
+    rules.add(new DruidAggregateCaseToFilterRule(plannerContext.queryContext().isExtendedFilteredSumRewrite()));
     rules.addAll(configurableRuleSet(plannerContext));
 
     if (plannerContext.getJoinAlgorithm().requiresSubquery()) {
