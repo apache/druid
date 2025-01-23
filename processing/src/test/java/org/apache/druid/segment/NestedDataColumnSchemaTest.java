0a1bab46b709b221f6a5e5bc7681003c1c263410
Clint Wylie <cwylie@apache.org>
2025-01-23 04:01:47 -0800

    log.warn on invalid nested column format version instead of failure

--- processing/src/test/java/org/apache/druid/segment/NestedDataColumnSchemaTest.java
+++ processing/src/test/java/org/apache/druid/segment/NestedDataColumnSchemaTest.java
@@ -22,7 +22,6 @@ package org.apache.druid.segment;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.InjectableValues;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.druid.error.DruidException;
 import org.apache.druid.jackson.DefaultObjectMapper;
 import org.junit.Assert;
 import org.junit.Test;
@@ -55,26 +54,5 @@ public class NestedDataColumnSchemaTest
     final String there = "{\"type\":\"json\", \"name\":\"test\"}";
     NestedDataColumnSchema andBack = MAPPER.readValue(there, NestedDataColumnSchema.class);
     Assert.assertEquals(new NestedDataColumnSchema("test", 5), andBack);
-  }
-
-  @Test
-  public void testSerdeOverride() throws JsonProcessingException
-  {
-    Throwable t = Assert.assertThrows(DruidException.class, () -> new NestedDataColumnSchema("test", 4));
-    Assert.assertEquals("Unsupported nested column format version[4]", t.getMessage());
-  }
-
-  @Test
-  public void testVersionTooSmall()
-  {
-    Throwable t = Assert.assertThrows(DruidException.class, () -> new NestedDataColumnSchema("test", 3));
-    Assert.assertEquals("Unsupported nested column format version[3]", t.getMessage());
-  }
-
-  @Test
-  public void testVersionTooBig()
-  {
-    Throwable t = Assert.assertThrows(DruidException.class, () -> new NestedDataColumnSchema("test", 6));
-    Assert.assertEquals("Unsupported nested column format version[6]", t.getMessage());
   }
 }
