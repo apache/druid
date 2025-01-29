e0a7c9d6b78569486bbdd2d5e0d78963a516261f
Zoltan Haindrich <kirk@rxd.hu>
2025-01-29 17:02:12 +0000

    use q.getDSA

--- server/src/main/java/org/apache/druid/client/CachingClusteredClient.java
+++ server/src/main/java/org/apache/druid/client/CachingClusteredClient.java
@@ -275,7 +275,7 @@ public class CachingClusteredClient implements QuerySegmentWalker
       this.query = queryPlus.getQuery();
       this.toolChest = conglomerate.getToolChest(query);
       this.strategy = toolChest.getCacheStrategy(query, objectMapper);
-      this.dataSourceAnalysis = query.getDataSource().getAnalysis();
+      this.dataSourceAnalysis = query.getDataSourceAnalysis();
 
       this.useCache = CacheUtil.isUseSegmentCache(query, strategy, cacheConfig, CacheUtil.ServerType.BROKER);
       this.populateCache = CacheUtil.isPopulateSegmentCache(query, strategy, cacheConfig, CacheUtil.ServerType.BROKER);
