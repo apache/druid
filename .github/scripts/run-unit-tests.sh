0a3bd19e96bb148bcdc369848b5741370a6b4427
Akshat Jain <akjn11@gmail.com>
2025-02-07 10:41:46 +0530

    Address review comments

--- .github/scripts/run-unit-tests.sh
+++ .github/scripts/run-unit-tests.sh
@@ -18,9 +18,8 @@
 set -e
 set -x
 
-JACOCO_REPORT_HASH="$(echo "$*" | md5sum | cut -d ' ' -f1)"
 OPTS+="-pl !integration-tests,!:druid-it-tools,!:druid-it-image,!:druid-it-cases"
 OPTS+=" -Dsurefire.failIfNoSpecifiedTests=false -P skip-static-checks -Dweb.console.skip=true -Dmaven.javadoc.skip=true"
-OPTS+=" -Djacoco.destFile='target/jacoco-${JACOCO_REPORT_HASH}.exec'"
+OPTS+=" -Djacoco.destFile=\"target/jacoco-${HASH}.exec\""
 
 mvn -B $OPTS test "$@"
