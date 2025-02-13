c47e6c6d3efb38d037a0cbd4c39565d531cb372e
Zoltan Haindrich <kirk@rxd.hu>
2025-02-13 08:42:07 +0000

    change sig SegmentManager#getTimeline

--- server/src/test/java/org/apache/druid/server/coordination/ServerManagerTest.java
+++ server/src/test/java/org/apache/druid/server/coordination/ServerManagerTest.java
@@ -553,7 +553,7 @@ public class ServerManagerTest
     final Interval interval = Intervals.of("P1d/2011-04-01");
     final SearchQuery query = searchQuery("test", interval, Granularities.ALL);
     final Optional<VersionedIntervalTimeline<String, ReferenceCountingSegment>> maybeTimeline = segmentManager
-        .getTimeline(query.getDataSource().getAnalysis());
+        .getTimeline(query.getDataSourceAnalysis().getBaseTableDataSource().get());
     Assert.assertTrue(maybeTimeline.isPresent());
     final List<TimelineObjectHolder<String, ReferenceCountingSegment>> holders = maybeTimeline.get().lookup(interval);
     final List<SegmentDescriptor> closedSegments = new ArrayList<>();
