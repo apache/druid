8c93b4b0b9cbd27fe1737ea593ac8e29a91233fe
Adarsh Sanjeev <adarshsanjeev@gmail.com>
2025-03-12 16:20:42 +0530

    bump web-console version to 31.0.2, prepare docker-compose file for 31.0.2

--- web-console/src/links.ts
+++ web-console/src/links.ts
@@ -17,7 +17,7 @@
  */
 
 // This is set to the latest available version and should be updated to the next version before release
-const DRUID_DOCS_VERSION = '31.0.1';
+const DRUID_DOCS_VERSION = '31.0.2';
 
 function fillVersion(str: string): string {
   return str.replace(/\{\{VERSION}}/g, DRUID_DOCS_VERSION);
