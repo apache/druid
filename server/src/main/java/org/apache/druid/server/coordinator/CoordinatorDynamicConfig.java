f69a527a465aca2c3574b75b933f4c0e22dac31b
Adarsh Sanjeev <adarshsanjeev@gmail.com>
2025-04-07 15:38:37 +0530

    Update javadoc

--- server/src/main/java/org/apache/druid/server/coordinator/CoordinatorDynamicConfig.java
+++ server/src/main/java/org/apache/druid/server/coordinator/CoordinatorDynamicConfig.java
@@ -328,7 +328,7 @@ public class CoordinatorDynamicConfig
   /**
    * Map of source to target Historicals to place into cloning mode. Target historicals are not involved in normal
    * segment assignment or segment balancing. Instead, segment assignments to the source Historical are mirrored to
-   * the target Historical, so that the source Historical becomes an exact copy. Segments on the target Historical do
+   * the target Historical, so that the target Historical becomes an exact copy. Segments on the target Historical do
    * not count towards the desired segment replication counts.
    */
   @JsonProperty
