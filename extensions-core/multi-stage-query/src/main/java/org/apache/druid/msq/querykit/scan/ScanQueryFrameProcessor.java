e6a45ffcf0e859d484e4678e6e6fe96b007dc324
Gian Merlino <gianmerlino@gmail.com>
2025-04-15 00:46:35 -0700

    Fix two issues where CursorHolders were not closed on failure of asCursor().
    One in TopNQueryEngine (native) and one in ScanQueryFrameProcessor (MSQ).
    
    This is especially important because asCursor() itself can fail in ways that
    leave the CursorHolder with resources that must be closed.

--- extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/querykit/scan/ScanQueryFrameProcessor.java
+++ extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/querykit/scan/ScanQueryFrameProcessor.java
@@ -79,6 +79,7 @@ import org.apache.druid.segment.VirtualColumn;
 import org.apache.druid.segment.VirtualColumns;
 import org.apache.druid.segment.column.RowSignature;
 import org.apache.druid.timeline.SegmentId;
+import org.apache.druid.utils.CloseableUtils;
 
 import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
@@ -268,7 +269,15 @@ public class ScanQueryFrameProcessor extends BaseLeafFrameProcessor
                   null
               )
           );
-      final Cursor nextCursor = nextCursorHolder.asCursor();
+
+      final Cursor nextCursor;
+
+      // If asCursor() fails, we need to close nextCursorHolder immediately.
+      try {
+        nextCursor = nextCursorHolder.asCursor();
+      } catch (Throwable t) {
+        throw CloseableUtils.closeAndWrapInCatch(t, nextCursorHolder);
+      }
 
       if (nextCursor == null) {
         // No cursors!
