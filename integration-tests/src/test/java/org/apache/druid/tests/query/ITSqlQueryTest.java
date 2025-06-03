fdd476826e8a648063330c787c92e7491ea430e8
Kashif Faraz <kashif.faraz@gmail.com>
2025-06-03 16:51:13 +0530

    Update integration-tests/src/test/java/org/apache/druid/tests/query/ITSqlQueryTest.java

--- integration-tests/src/test/java/org/apache/druid/tests/query/ITSqlQueryTest.java
+++ integration-tests/src/test/java/org/apache/druid/tests/query/ITSqlQueryTest.java
@@ -46,7 +46,7 @@ import java.util.function.Function;
 /**
  * Test the SQL endpoint with different Content-Type
  */
-@Test(groups = {TestNGGroup.QUERY, TestNGGroup.CENTRALIZED_DATASOURCE_SCHEMA}, enabled = false)
+@Test(groups = {TestNGGroup.QUERY, TestNGGroup.CENTRALIZED_DATASOURCE_SCHEMA})
 @Guice(moduleFactory = DruidTestModuleFactory.class)
 public class ITSqlQueryTest
 {
