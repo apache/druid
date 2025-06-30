419c612cc099452e4f97cf3f6e364df7b2315c13
capistrant <lucas.capistrant@imply.io>
2025-06-30 08:56:22 -0500

    checkstyle fixes

--- extensions-core/kafka-extraction-namespace/src/test/java/org/apache/druid/query/lookup/TestKafkaExtractionCluster.java
+++ extensions-core/kafka-extraction-namespace/src/test/java/org/apache/druid/query/lookup/TestKafkaExtractionCluster.java
@@ -19,7 +19,6 @@
 
 package org.apache.druid.query.lookup;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
