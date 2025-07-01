a3ede38c09a72b405fb6527a1d69298a1c90e462
Gian Merlino <gianmerlino@gmail.com>
2025-07-01 02:08:10 -0700

    Remove unused imports.

--- extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/exec/FrameContext.java
+++ extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/exec/FrameContext.java
@@ -20,10 +20,6 @@
 package org.apache.druid.msq.exec;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.druid.frame.FrameType;
-import org.apache.druid.frame.allocation.MemoryAllocatorFactory;
-import org.apache.druid.frame.key.KeyColumn;
-import org.apache.druid.frame.write.FrameWriterFactory;
 import org.apache.druid.msq.kernel.WorkOrder;
 import org.apache.druid.msq.querykit.DataSegmentProvider;
 import org.apache.druid.query.groupby.GroupingEngine;
@@ -31,13 +27,11 @@ import org.apache.druid.query.policy.PolicyEnforcer;
 import org.apache.druid.segment.IndexIO;
 import org.apache.druid.segment.IndexMergerV9;
 import org.apache.druid.segment.SegmentWrangler;
-import org.apache.druid.segment.column.RowSignature;
 import org.apache.druid.segment.incremental.RowIngestionMeters;
 import org.apache.druid.segment.loading.DataSegmentPusher;
 
 import java.io.Closeable;
 import java.io.File;
-import java.util.List;
 
 /**
  * Provides services and objects for the functioning of the frame processors. Scoped to a specific stage of a
