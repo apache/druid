04a154aa29989dd3f5f12eed3e3636a11e4afc79
cryptoe <karankumar1100@gmail.com>
2025-07-21 10:55:40 +0530

    More adjustments.

--- integration-tests/script/setup_k8s_cluster.sh
+++ integration-tests/script/setup_k8s_cluster.sh
@@ -22,8 +22,8 @@ export KUBECONFIG=$HOME/.kube/config
 
 # Launch K8S cluster
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.18.1/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-curl -sfL https://get.k3s.io | bash /dev/stdin  --docker  &> abc.log
-cat abc.log
+curl -sfL https://get.k3s.io | bash /dev/stdin  --docker || true > k3s_install.log 2>&1
+cat k3s_install.log
 systemctl status k3s.service
 mkdir -p $HOME/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml $KUBECONFIG
