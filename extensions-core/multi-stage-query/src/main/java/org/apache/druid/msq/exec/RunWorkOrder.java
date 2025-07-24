apache/druid

commit 2b1f7df6dceecc663a916413e880057093c6078a

MSQ: Enable stages to control their own shuffle logic. (#18144)
* MSQ: Enable stages to control their own shuffle logic.

This patch pushes shuffle logic from RunWorkOrder into the stage definition.
The motivation is to enable optimizations that take advantage of data already
being sorted or partitioned in the desired way.

Main changes:

- FrameProcessorFactory is renamed StageProcessor, and its "makeProcessors"
  method is replaced with a more powerful "execute" method. The new "execute"
  is expected to fully contain partitioning and shuffling logic.

- RunWorkOrder, the worker class that manages execution, no longer has any
  shuffle-related processing code. It delegates this to the StageProcessor.

- StandardStageOperations, StandardStageRunner, & StandardStageProcessor are
  introduced for StageProcessor that want to perform shuffling in the "standard"
  way that was formerly mandatory.

Other changes:

- Moved FrameContext and related classes from the "kernel" package to the "exec" package.

- Remove "readableChannelUsableWhenWriting" from OutputChannel, to reduce complexity.
  It was only used by the standard hashPartition implementation, and wasn't strictly
  needed there.

* Fix import orders.

* Fixes for tests.

* Rename StandardStageOperations to StandardShuffleOperations.

* Updates from review.

* Clarifications.

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/exec/std/ProcessorsAndChannels.java
@@ -17,17 +17,17 @@
  * under the License.
  */
 
-package org.apache.druid.msq.kernel;
+package org.apache.druid.msq.exec.std;
 
+import org.apache.druid.frame.processor.FrameProcessor;
 import org.apache.druid.frame.processor.OutputChannels;
 import org.apache.druid.frame.processor.manager.ProcessorManager;
 
 /**
- * Returned from {@link FrameProcessorFactory#makeProcessors}.
+ * Accepted by {@link StandardStageRunner#run(ProcessorsAndChannels)}. Includes a processor manager (which generates
+ * processors) and a list of output channels.
  *
- * Includes a processor sequence and a list of output channels.
- *
- * @param <T> return type of {@link org.apache.druid.frame.processor.FrameProcessor} from {@link #getProcessorManager()}
+ * @param <T> return type of {@link FrameProcessor} from {@link #getProcessorManager()}
  * @param <R> result type of {@link ProcessorManager#result()}
  */
 public class ProcessorsAndChannels<T, R>

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/exec/std/ResultAndChannels.java
@@ -1,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.msq.exec.std;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import org.apache.druid.frame.channel.BlockingQueueFrameChannel;
+import org.apache.druid.frame.processor.OutputChannelFactory;
+import org.apache.druid.frame.processor.OutputChannels;
+import org.apache.druid.msq.exec.StageProcessor;
+
+/**
+ * Represents the completion of some processing ({@link #resultFuture()}) and the output channels associated
+ * with that processing ({@link #outputChannels()}).
+ *
+ * @see StageProcessor class-level javadoc for future discussion of the meaning of "result" and "output channels"
+ */
+public class ResultAndChannels<T>
+{
+  private final ListenableFuture<T> result;
+  private final OutputChannels outputChannels;
+
+  public ResultAndChannels(
+      final ListenableFuture<T> result,
+      final OutputChannels outputChannels
+  )
+  {
+    this.result = result;
+    this.outputChannels = outputChannels;
+  }
+
+  /**
+   * Future that represents the completion of processing. When this future resolves, the output has been fully
+   * generated and all processing has stopped.
+   */
+  public ListenableFuture<T> resultFuture()
+  {
+    return result;
+  }
+
+  /**
+   * Processed outputs. If unbuffered (e.g. {@link BlockingQueueFrameChannel} these are readable prior to
+   * {@link #resultFuture()} resolving. If buffered, these are readable after {@link #resultFuture()} resolves.
+   *
+   * This class does not itself tell you if the output channels are buffered. This can be known by checking
+   * {@link OutputChannelFactory#isBuffered()} on the factory that created them.
+   */
+  public OutputChannels outputChannels()
+  {
+    return outputChannels;
+  }
+}

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/querykit/BaseLeafStageProcessor.java
@@ -37,6 +37,9 @@ import org.apache.druid.frame.processor.manager.ProcessorManager;
 import org.apache.druid.frame.processor.manager.ProcessorManagers;
 import org.apache.druid.frame.write.FrameWriterFactory;
 import org.apache.druid.msq.counters.CounterTracker;
+import org.apache.druid.msq.exec.FrameContext;
+import org.apache.druid.msq.exec.std.BasicStandardStageProcessor;
+import org.apache.druid.msq.exec.std.ProcessorsAndChannels;
 import org.apache.druid.msq.input.InputSlice;
 import org.apache.druid.msq.input.InputSliceReader;
 import org.apache.druid.msq.input.InputSlices;
@@ -45,8 +48,6 @@ import org.apache.druid.msq.input.ReadableInputs;
 import org.apache.druid.msq.input.external.ExternalInputSlice;
 import org.apache.druid.msq.input.stage.StageInputSlice;
 import org.apache.druid.msq.input.table.SegmentsInputSlice;
-import org.apache.druid.msq.kernel.FrameContext;
-import org.apache.druid.msq.kernel.ProcessorsAndChannels;
 import org.apache.druid.msq.kernel.StageDefinition;
 import org.apache.druid.query.Query;
 import org.apache.druid.query.planning.ExecutionVertex;
@@ -67,11 +68,11 @@ import java.util.function.Function;
  * other stages. The term "leaf" represents the fact that they are capable of being leaves in the query tree. However,
  * they do not *need* to be leaves. They can read from prior stages as well.
  */
-public abstract class BaseLeafFrameProcessorFactory extends BaseFrameProcessorFactory
+public abstract class BaseLeafStageProcessor extends BasicStandardStageProcessor
 {
   private final Query<?> query;
 
-  protected BaseLeafFrameProcessorFactory(Query<?> query)
+  protected BaseLeafStageProcessor(Query<?> query)
   {
     this.query = query;
   }
@@ -91,7 +92,7 @@ public abstract class BaseLeafFrameProcessorFactory extends BaseFrameProcessorFa
       final boolean removeNullBytes
   ) throws IOException
   {
-    // BaseLeafFrameProcessorFactory is used for native Druid queries, where the following input cases can happen:
+    // BaseLeafStageProcessor is used for native Druid queries, where the following input cases can happen:
     //   1) Union datasources: N nonbroadcast inputs, which are treated as one big input
     //   2) Join datasources: one nonbroadcast input, N broadcast inputs
     //   3) All other datasources: single input
@@ -184,7 +185,7 @@ public abstract class BaseLeafFrameProcessorFactory extends BaseFrameProcessorFa
       final FrameContext frameContext
   )
   {
-    final BaseLeafFrameProcessorFactory factory = this;
+    final BaseLeafStageProcessor factory = this;
     // Read all base inputs in separate processors, one per processor.
     final Iterable<ReadableInput> processorBaseInputs = readBaseInputs(
         stageDefinition,

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/querykit/WindowOperatorQueryStageProcessor.java
@@ -36,13 +36,14 @@ import org.apache.druid.frame.processor.manager.ProcessorManagers;
 import org.apache.druid.java.util.common.guava.Sequence;
 import org.apache.druid.java.util.common.guava.Sequences;
 import org.apache.druid.msq.counters.CounterTracker;
+import org.apache.druid.msq.exec.FrameContext;
+import org.apache.druid.msq.exec.std.BasicStandardStageProcessor;
+import org.apache.druid.msq.exec.std.ProcessorsAndChannels;
 import org.apache.druid.msq.input.InputSlice;
 import org.apache.druid.msq.input.InputSliceReader;
 import org.apache.druid.msq.input.ReadableInput;
 import org.apache.druid.msq.input.stage.ReadablePartition;
 import org.apache.druid.msq.input.stage.StageInputSlice;
-import org.apache.druid.msq.kernel.FrameContext;
-import org.apache.druid.msq.kernel.ProcessorsAndChannels;
 import org.apache.druid.msq.kernel.StageDefinition;
 import org.apache.druid.query.operator.OperatorFactory;
 import org.apache.druid.query.operator.WindowOperatorQuery;
@@ -55,7 +56,7 @@ import java.util.Objects;
 import java.util.function.Consumer;
 
 @JsonTypeName("window")
-public class WindowOperatorQueryFrameProcessorFactory extends BaseFrameProcessorFactory
+public class WindowOperatorQueryStageProcessor extends BasicStandardStageProcessor
 {
   private final WindowOperatorQuery query;
   private final List<OperatorFactory> operatorList;
@@ -64,7 +65,7 @@ public class WindowOperatorQueryFrameProcessorFactory extends BaseFrameProcessor
   private final List<String> partitionColumnNames;
 
   @JsonCreator
-  public WindowOperatorQueryFrameProcessorFactory(
+  public WindowOperatorQueryStageProcessor(
       @JsonProperty("query") WindowOperatorQuery query,
       @JsonProperty("operatorList") List<OperatorFactory> operatorFactoryList,
       @JsonProperty("stageRowSignature") RowSignature stageRowSignature,
@@ -188,7 +189,7 @@ public class WindowOperatorQueryFrameProcessorFactory extends BaseFrameProcessor
     if (o == null || getClass() != o.getClass()) {
       return false;
     }
-    WindowOperatorQueryFrameProcessorFactory that = (WindowOperatorQueryFrameProcessorFactory) o;
+    WindowOperatorQueryStageProcessor that = (WindowOperatorQueryStageProcessor) o;
     return maxRowsMaterializedInWindow == that.maxRowsMaterializedInWindow
            && Objects.equals(query, that.query)
            && Objects.equals(operatorList, that.operatorList)

commit 2b9b3b006b064fd75fb5276abbf37413ac71fefa

MSQ: Fix excessive retention of writable stage output channels.
Fixes a regression from #18144. The refactoring in that patch lost some
logic that ensured stage output channels were stored in read-only form.
This is important, because the writable form includes a 1MB frame memory
allocation buffer. It can add up to a lot of memory if retained across
lots of channels.

This patch simplifies things by unconditionally converting all
stage outputs to read-only before they are retained, by replacing the
channel in "stageOutputChannels.add(channel)" with "channel.readOnly()".
It also simplifies various other bits of code that deal with intermediate
output channels, by converting them to read-only in the constructors of
ProcessorsAndChannels and ResultAndChannels, rather than at only some call
sites.

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/exec/RunWorkOrder.java
@@ -327,7 +327,6 @@ public class RunWorkOrder
                 stageOutputChannels.sort(Comparator.comparing(OutputChannel::getPartitionNumber));
                 outputChannels = OutputChannels.wrap(
                     stageOutputChannels.stream()
-                                       .map(OutputChannel::readOnly)
                                        .sorted(Comparator.comparing(OutputChannel::getPartitionNumber))
                                        .collect(Collectors.toList()));
                 stageOutputChannels.clear();
@@ -493,7 +492,7 @@ public class RunWorkOrder
         outputChannelFactory,
         channel -> {
           synchronized (stageOutputChannels) {
-            stageOutputChannels.add(channel);
+            stageOutputChannels.add(channel.readOnly());
           }
         }
     );

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/exec/std/ProcessorsAndChannels.java
@@ -41,7 +41,7 @@ public class ProcessorsAndChannels<T, R>
   )
   {
     this.processorManager = processorManager;
-    this.outputChannels = outputChannels;
+    this.outputChannels = outputChannels.readOnly();
   }
 
   public ProcessorManager<T, R> getProcessorManager()

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/exec/std/ResultAndChannels.java
@@ -42,7 +42,7 @@ public class ResultAndChannels<T>
   )
   {
     this.result = result;
-    this.outputChannels = outputChannels;
+    this.outputChannels = outputChannels.readOnly();
   }
 
   /**

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/exec/std/StandardShuffleOperations.java
@@ -523,7 +523,7 @@ public class StandardShuffleOperations
         ),
         newResultAndChannels -> new ResultAndChannels<>(
             newResultAndChannels.resultFuture(),
-            newResultAndChannels.outputChannels().readOnly()
+            newResultAndChannels.outputChannels()
         )
     );
 

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/querykit/BaseLeafStageProcessor.java
@@ -176,7 +176,7 @@ public abstract class BaseLeafStageProcessor extends BasicStandardStageProcessor
     }
 
     //noinspection unchecked,rawtypes
-    return new ProcessorsAndChannels<>(processorManager, OutputChannels.wrapReadOnly(outputChannels));
+    return new ProcessorsAndChannels<>(processorManager, OutputChannels.wrap(outputChannels));
   }
 
   private ProcessorManager<Object, Long> createBaseLeafProcessorManagerWithHandoff(

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/querykit/WindowOperatorQueryStageProcessor.java
@@ -172,7 +172,7 @@ public class WindowOperatorQueryStageProcessor extends BasicStandardStageProcess
 
     return new ProcessorsAndChannels<>(
         ProcessorManagers.of(processors),
-        OutputChannels.wrapReadOnly(ImmutableList.copyOf(outputChannels.values()))
+        OutputChannels.wrap(ImmutableList.copyOf(outputChannels.values()))
     );
   }
 

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/querykit/common/OffsetLimitStageProcessor.java
@@ -135,7 +135,7 @@ public class OffsetLimitStageProcessor extends BasicStandardStageProcessor
 
     return new ProcessorsAndChannels<>(
         ProcessorManagers.of(workerSupplier),
-        OutputChannels.wrapReadOnly(Collections.singletonList(outputChannel))
+        OutputChannels.wrap(Collections.singletonList(outputChannel))
     );
   }
 

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/querykit/groupby/GroupByPostShuffleStageProcessor.java
@@ -128,7 +128,7 @@ public class GroupByPostShuffleStageProcessor extends BasicStandardStageProcesso
 
     return new ProcessorsAndChannels<>(
         ProcessorManagers.of(processors),
-        OutputChannels.wrapReadOnly(ImmutableList.copyOf(outputChannels.values()))
+        OutputChannels.wrap(ImmutableList.copyOf(outputChannels.values()))
     );
   }
 

extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/querykit/results/QueryResultStageProcessor.java
@@ -111,7 +111,7 @@ public class QueryResultStageProcessor extends BasicStandardStageProcessor
 
     return new ProcessorsAndChannels<>(
         ProcessorManagers.of(processors),
-        OutputChannels.wrapReadOnly(ImmutableList.copyOf(outputChannels.values()))
+        OutputChannels.wrap(ImmutableList.copyOf(outputChannels.values()))
     );
   }
 

processing/src/main/java/org/apache/druid/frame/processor/OutputChannel.java
@@ -21,14 +21,13 @@ package org.apache.druid.frame.processor;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Suppliers;
-import com.google.errorprone.annotations.concurrent.GuardedBy;
+import org.apache.druid.error.DruidException;
 import org.apache.druid.frame.allocation.MemoryAllocator;
 import org.apache.druid.frame.channel.FrameWithPartition;
 import org.apache.druid.frame.channel.ReadableFrameChannel;
 import org.apache.druid.frame.channel.ReadableNilFrameChannel;
 import org.apache.druid.frame.channel.WritableFrameChannel;
 import org.apache.druid.java.util.common.IAE;
-import org.apache.druid.java.util.common.ISE;
 
 import javax.annotation.Nullable;
 import java.util.function.Function;
@@ -43,13 +42,11 @@ import java.util.function.Supplier;
  */
 public class OutputChannel
 {
-  @GuardedBy("this")
   @Nullable
-  private WritableFrameChannel writableChannel;
+  private volatile WritableFrameChannel writableChannel;
 
-  @GuardedBy("this")
   @Nullable
-  private MemoryAllocator frameMemoryAllocator;
+  private volatile MemoryAllocator frameMemoryAllocator;
 
   private final Supplier<ReadableFrameChannel> readableChannelSupplier;
   private final int partitionNumber;
@@ -160,13 +157,18 @@ public class OutputChannel
    * Returns the writable channel of this pair. The producer writes to this channel. Throws ISE if the output channel is
    * read only.
    */
-  public synchronized WritableFrameChannel getWritableChannel()
+  public WritableFrameChannel getWritableChannel()
   {
-    if (writableChannel == null) {
-      throw new ISE("Writable channel is not available. The output channel might be marked as read-only,"
-                    + " hence no writes are allowed.");
+    // Store into a local since the field is volatile.
+    final WritableFrameChannel theWritableChannel = writableChannel;
+
+    if (theWritableChannel == null) {
+      throw DruidException.defensive(
+          "Writable channel is not available. "
+          + "The output channel might be marked as read-only, hence no writes are allowed."
+      );
     } else {
-      return writableChannel;
+      return theWritableChannel;
     }
   }
 
@@ -174,13 +176,18 @@ public class OutputChannel
    * Returns the memory allocator for the writable channel. The producer uses this to generate frames for the channel.
    * Throws ISE if the output channel is read only.
    */
-  public synchronized MemoryAllocator getFrameMemoryAllocator()
+  public MemoryAllocator getFrameMemoryAllocator()
   {
-    if (frameMemoryAllocator == null) {
-      throw new ISE("Frame allocator is not available. The output channel might be marked as read-only,"
-                    + " hence memory allocator is not required.");
+    // Store into a local since the field is volatile.
+    final MemoryAllocator theFrameMemoryAllocator = frameMemoryAllocator;
+
+    if (theFrameMemoryAllocator == null) {
+      throw DruidException.defensive(
+          "Frame memory allocator is not available. "
+          + "The output channel might be marked as read-only, hence no writes are allowed."
+      );
     } else {
-      return frameMemoryAllocator;
+      return theFrameMemoryAllocator;
     }
   }
 
@@ -203,14 +210,18 @@ public class OutputChannel
     return partitionNumber;
   }
 
-  public synchronized OutputChannel mapWritableChannel(final Function<WritableFrameChannel, WritableFrameChannel> mapFn)
+  public OutputChannel mapWritableChannel(final Function<WritableFrameChannel, WritableFrameChannel> mapFn)
   {
-    if (writableChannel == null) {
+    // Store into locals since the fields are volatile.
+    final WritableFrameChannel theWritableChannel = writableChannel;
+    final MemoryAllocator theFrameMemoryAllocator = frameMemoryAllocator;
+
+    if (theWritableChannel == null || theFrameMemoryAllocator == null) {
       return this;
     } else {
       return new OutputChannel(
-          mapFn.apply(writableChannel),
-          frameMemoryAllocator,
+          mapFn.apply(theWritableChannel),
+          theFrameMemoryAllocator,
           readableChannelSupplier,
           partitionNumber
       );
@@ -220,17 +231,27 @@ public class OutputChannel
   /**
    * Returns a read-only version of this instance. Read-only versions have neither {@link #getWritableChannel()} nor
    * {@link #getFrameMemoryAllocator()}, and therefore require substantially less memory.
+   *
+   * Returns the same instance if it is already read-only.
    */
   public OutputChannel readOnly()
   {
-    return OutputChannel.readOnly(readableChannelSupplier, partitionNumber);
+    return isReadOnly() ? this : OutputChannel.readOnly(readableChannelSupplier, partitionNumber);
+  }
+
+  /**
+   * Returns whether this instance is read-only (has no writable channel).
+   */
+  public boolean isReadOnly()
+  {
+    return writableChannel == null;
   }
 
   /**
    * Removes the reference to the {@link #writableChannel} and {@link #frameMemoryAllocator} from the object, making
    * it more efficient
    */
-  public synchronized void convertToReadOnly()
+  public void convertToReadOnly()
   {
     this.writableChannel = null;
     this.frameMemoryAllocator = null;

processing/src/main/java/org/apache/druid/frame/processor/OutputChannels.java
@@ -67,15 +67,6 @@ public class OutputChannels
   }
 
   /**
-   * Creates an instance wrapping read-only versions (see {@link OutputChannel#readOnly()}) of all the
-   * provided channels.
-   */
-  public static OutputChannels wrapReadOnly(final List<OutputChannel> outputChannels)
-  {
-    return new OutputChannels(outputChannels.stream().map(OutputChannel::readOnly).collect(Collectors.toList()));
-  }
-
-  /**
    * Verifies there is exactly one channel per partition.
    */
   public OutputChannels verifySingleChannel()
@@ -139,6 +130,24 @@ public class OutputChannels
    */
   public OutputChannels readOnly()
   {
-    return wrapReadOnly(outputChannels);
+    if (isReadOnly()) {
+      return this;
+    } else {
+      return new OutputChannels(outputChannels.stream().map(OutputChannel::readOnly).collect(Collectors.toList()));
+    }
+  }
+
+  /**
+   * Returns whether this instance is read-only (all channels have {@link OutputChannel#isReadOnly()}).
+   */
+  public boolean isReadOnly()
+  {
+    for (final OutputChannel channel : outputChannels) {
+      if (!channel.isReadOnly()) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }

processing/src/test/java/org/apache/druid/frame/channel/ComposingWritableFrameChannelTest.java
@@ -22,10 +22,10 @@ package org.apache.druid.frame.channel;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.ListenableFuture;
+import org.apache.druid.error.DruidException;
 import org.apache.druid.frame.Frame;
 import org.apache.druid.frame.allocation.ArenaMemoryAllocator;
 import org.apache.druid.frame.processor.OutputChannel;
-import org.apache.druid.java.util.common.ISE;
 import org.apache.druid.query.ResourceLimitExceededException;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
@@ -95,22 +95,20 @@ public class ComposingWritableFrameChannelTest
 
 
     // Test if the older channel has been converted to read only
-    Assert.assertThrows(ISE.class, outputChannel1::getWritableChannel);
+    Assert.assertThrows(DruidException.class, outputChannel1::getWritableChannel);
     composingWritableFrameChannel.close();
 
-    Exception ise1 = Assert.assertThrows(IllegalStateException.class, () -> outputChannel1.getFrameMemoryAllocator());
+    Exception ise1 = Assert.assertThrows(DruidException.class, outputChannel1::getFrameMemoryAllocator);
     MatcherAssert.assertThat(
         ise1,
-        ThrowableMessageMatcher.hasMessage(CoreMatchers.equalTo(
-            "Frame allocator is not available. The output channel might be marked as read-only, hence memory allocator is not required."))
+        ThrowableMessageMatcher.hasMessage(CoreMatchers.startsWith("Frame memory allocator is not available."))
     );
 
 
-    Exception ise2 = Assert.assertThrows(IllegalStateException.class, () -> outputChannel2.getFrameMemoryAllocator());
+    Exception ise2 = Assert.assertThrows(DruidException.class, outputChannel2::getFrameMemoryAllocator);
     MatcherAssert.assertThat(
         ise2,
-        ThrowableMessageMatcher.hasMessage(CoreMatchers.equalTo(
-            "Frame allocator is not available. The output channel might be marked as read-only, hence memory allocator is not required."))
+        ThrowableMessageMatcher.hasMessage(CoreMatchers.startsWith("Frame memory allocator is not available."))
     );
 
   }

processing/src/test/java/org/apache/druid/frame/processor/OutputChannelFactoryTest.java
@@ -20,6 +20,7 @@
 package org.apache.druid.frame.processor;
 
 import com.google.common.collect.Iterables;
+import org.apache.druid.error.DruidException;
 import org.apache.druid.frame.Frame;
 import org.apache.druid.frame.FrameType;
 import org.apache.druid.frame.channel.FrameWithPartition;
@@ -177,6 +178,6 @@ public abstract class OutputChannelFactoryTest extends InitializedNullHandlingTe
 
     Assert.assertEquals(1, channel.getPartitionNumber());
     Assert.assertTrue(channel.getReadableChannel().isFinished());
-    Assert.assertThrows(IllegalStateException.class, channel::getWritableChannel);
+    Assert.assertThrows(DruidException.class, channel::getWritableChannel);
   }
 }

processing/src/test/java/org/apache/druid/frame/processor/OutputChannelTest.java
@@ -19,6 +19,7 @@
 
 package org.apache.druid.frame.processor;
 
+import org.apache.druid.error.DruidException;
 import org.apache.druid.frame.allocation.HeapMemoryAllocator;
 import org.apache.druid.frame.channel.BlockingQueueFrameChannel;
 import org.apache.druid.frame.channel.WritableFrameFileChannel;
@@ -39,19 +40,17 @@ public class OutputChannelTest
     Assert.assertTrue(channel.getReadableChannel().isFinished());
 
     // No writable channel: cannot call getWritableChannel.
-    final IllegalStateException e1 = Assert.assertThrows(IllegalStateException.class, channel::getWritableChannel);
+    final DruidException e1 = Assert.assertThrows(DruidException.class, channel::getWritableChannel);
     MatcherAssert.assertThat(
         e1,
-        ThrowableMessageMatcher.hasMessage(CoreMatchers.equalTo(
-            "Writable channel is not available. The output channel might be marked as read-only, hence no writes are allowed."))
+        ThrowableMessageMatcher.hasMessage(CoreMatchers.startsWith("Writable channel is not available."))
     );
 
     // No writable channel: cannot call getFrameMemoryAllocator.
-    final IllegalStateException e2 = Assert.assertThrows(IllegalStateException.class, channel::getFrameMemoryAllocator);
+    final DruidException e2 = Assert.assertThrows(DruidException.class, channel::getFrameMemoryAllocator);
     MatcherAssert.assertThat(
         e2,
-        ThrowableMessageMatcher.hasMessage(CoreMatchers.equalTo(
-            "Frame allocator is not available. The output channel might be marked as read-only, hence memory allocator is not required."))
+        ThrowableMessageMatcher.hasMessage(CoreMatchers.startsWith("Frame memory allocator is not available."))
     );
 
     // Mapping the writable channel of a nil channel has no effect, because there is no writable channel.

processing/src/test/java/org/apache/druid/frame/processor/OutputChannelsTest.java
@@ -23,6 +23,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import it.unimi.dsi.fastutil.ints.IntSet;
 import it.unimi.dsi.fastutil.ints.IntSets;
+import org.apache.druid.error.DruidException;
 import org.apache.druid.frame.allocation.HeapMemoryAllocator;
 import org.apache.druid.frame.channel.BlockingQueueFrameChannel;
 import org.hamcrest.CoreMatchers;
@@ -76,26 +77,24 @@ public class OutputChannelsTest
     Assert.assertEquals(1, readOnlyChannels.getAllChannels().size());
     Assert.assertEquals(1, channels.getChannelsForPartition(1).size());
 
-    final IllegalStateException e = Assert.assertThrows(
-        IllegalStateException.class,
+    final DruidException e = Assert.assertThrows(
+        DruidException.class,
         () -> Iterables.getOnlyElement(readOnlyChannels.getAllChannels()).getWritableChannel()
     );
 
     MatcherAssert.assertThat(
         e,
-        ThrowableMessageMatcher.hasMessage(CoreMatchers.equalTo(
-            "Writable channel is not available. The output channel might be marked as read-only, hence no writes are allowed."))
+        ThrowableMessageMatcher.hasMessage(CoreMatchers.startsWith("Writable channel is not available."))
     );
 
-    final IllegalStateException e2 = Assert.assertThrows(
-        IllegalStateException.class,
+    final DruidException e2 = Assert.assertThrows(
+        DruidException.class,
         () -> Iterables.getOnlyElement(readOnlyChannels.getAllChannels()).getFrameMemoryAllocator()
     );
 
     MatcherAssert.assertThat(
         e2,
-        ThrowableMessageMatcher.hasMessage(CoreMatchers.equalTo(
-            "Frame allocator is not available. The output channel might be marked as read-only, hence memory allocator is not required."))
+        ThrowableMessageMatcher.hasMessage(CoreMatchers.startsWith("Frame memory allocator is not available."))
     );
   }
 