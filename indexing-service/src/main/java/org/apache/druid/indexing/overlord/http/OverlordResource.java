493cf96a13dc25e4d1dfae25ccd104bbafd97fe7
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2025-08-01 14:02:12 +0530

    Add configuration error message for noop use case.

--- indexing-service/src/main/java/org/apache/druid/indexing/overlord/http/OverlordResource.java
+++ indexing-service/src/main/java/org/apache/druid/indexing/overlord/http/OverlordResource.java
@@ -792,7 +792,7 @@ public class OverlordResource
         return Response.status(Response.Status.NOT_FOUND)
                        .entity(
                            "No log was found for this task. "
-                           + "The task may not exist, or it may not have begun running yet."
+                           + "The task may not exist, it may not have begun running yet or the taskStream configuration may be faulty, kindly verify the same."
                        )
                        .build();
       }
