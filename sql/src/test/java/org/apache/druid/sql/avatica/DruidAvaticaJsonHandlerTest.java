8ddb9103c41c70d44389d18f1523e826e70b99f1
capistrant <lucas.capistrant@imply.io>
2025-08-22 23:41:05 -0500

    add some UTs

--- sql/src/test/java/org/apache/druid/sql/avatica/DruidAvaticaJsonHandlerTest.java
+++ sql/src/test/java/org/apache/druid/sql/avatica/DruidAvaticaJsonHandlerTest.java
@@ -19,9 +19,9 @@
 
 package org.apache.druid.sql.avatica;
 
-import org.apache.druid.java.util.common.StringUtils;
 import org.apache.druid.server.DruidNode;
 import org.easymock.EasyMock;
+import org.eclipse.jetty.http.HttpFields;
 import org.eclipse.jetty.http.HttpURI;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Request;
@@ -30,23 +30,18 @@ import org.eclipse.jetty.util.Callback;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.nio.ByteBuffer;
-
-public class DruidAvaticaProtobufHandlerTest extends DruidAvaticaHandlerTest
+public class DruidAvaticaJsonHandlerTest extends DruidAvaticaHandlerTest
 {
   @Override
   protected String getJdbcUrlTail()
   {
-    return StringUtils.format(
-            "%s;serialization=protobuf",
-            DruidAvaticaProtobufHandler.AVATICA_PATH
-    );
+    return DruidAvaticaJsonHandler.AVATICA_PATH;
   }
 
   @Override
   protected Handler.Abstract getAvaticaHandler(final DruidMeta druidMeta)
   {
-    return new DruidAvaticaProtobufHandler(
+    return new DruidAvaticaJsonHandler(
             druidMeta,
             new DruidNode("dummy", "dummy", false, 1, null, true, false),
             new AvaticaMonitor()
@@ -54,10 +49,10 @@ public class DruidAvaticaProtobufHandlerTest extends DruidAvaticaHandlerTest
   }
 
   @Test
-  public void testNonPostRequestReturns400() throws Exception
+  public void testNonPostRequestSucceeds() throws Exception
   {
     DruidMeta druidMeta = EasyMock.mock(DruidMeta.class);
-    DruidAvaticaProtobufHandler handler = new DruidAvaticaProtobufHandler(
+    DruidAvaticaJsonHandler handler = new DruidAvaticaJsonHandler(
         druidMeta,
         new DruidNode("dummy", "dummy", false, 1, null, true, false),
         new AvaticaMonitor()
@@ -67,19 +62,18 @@ public class DruidAvaticaProtobufHandlerTest extends DruidAvaticaHandlerTest
     Response response = EasyMock.mock(Response.class);
     Callback callback = EasyMock.mock(Callback.class);
     HttpURI httpURI = EasyMock.mock(HttpURI.class);
+    HttpFields.Mutable headers = EasyMock.mock(HttpFields.Mutable.class);
 
     EasyMock.expect(request.getHttpURI()).andReturn(httpURI);
-    EasyMock.expect(httpURI.getPath()).andReturn(DruidAvaticaProtobufHandler.AVATICA_PATH_NO_TRAILING_SLASH);
+    EasyMock.expect(httpURI.getPath()).andReturn(DruidAvaticaJsonHandler.AVATICA_PATH_NO_TRAILING_SLASH);
     EasyMock.expect(request.getMethod()).andReturn("GET");
+
+    EasyMock.expect(response.getHeaders()).andReturn(headers);
     
-    response.setStatus(400);
-    EasyMock.expectLastCall();
+    headers.put("Content-Type", "application/json;charset=utf-8");
+    EasyMock.expectLastCall().andReturn(null);
     
-    response.write(
-        EasyMock.eq(true), 
-        EasyMock.anyObject(ByteBuffer.class), 
-        EasyMock.eq(callback)
-    );
+    callback.succeeded();
     EasyMock.expectLastCall();
 
     EasyMock.replay(request, response, callback, httpURI);
