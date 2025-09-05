a85c85e22bc40e8b970a1733f15a95da60bb584d
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2025-09-06 00:56:49 +0530

    Get embedded tests working

--- extensions-core/kafka-indexing-service/src/main/java/org/apache/druid/indexing/kafka/supervisor/KafkaSupervisor.java
+++ extensions-core/kafka-indexing-service/src/main/java/org/apache/druid/indexing/kafka/supervisor/KafkaSupervisor.java
@@ -76,6 +76,8 @@ import java.util.TreeMap;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import static java.lang.Math.max;
+
 /**
  * Supervisor responsible for managing the KafkaIndexTasks for a single dataSource. At a high level, the class accepts a
  * {@link KafkaSupervisorSpec} which includes the Kafka topic and configuration as well as an ingestion spec which will
@@ -168,7 +170,7 @@ public class KafkaSupervisor extends SeekableStreamSupervisor<KafkaTopicPartitio
     if (partitionId.isMultiTopicPartition()) {
       return Math.abs(31 * partitionId.topic().hashCode() + partitionId.partition()) % taskCount;
     } else {
-      return partitionId.partition() / minPartitionsPerTaskGroup;
+      return max(taskCount - 1, partitionId.partition() / minPartitionsPerTaskGroup);
     }
   }
 
