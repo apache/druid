e37d91174d3b5cdeb6d26888007ce56debf79be8
Gian Merlino <gianmerlino@gmail.com>
2025-09-22 11:52:23 -0700

    SQL: Stricter validation for GREATEST/LEAST.
    At the execution layer, the greatest and least functions only accept
    numbers and strings. They throw errors when encountering other types.
    To line up with this, the SQL bindings should verify that the SQL
    type is a type that is represented by a string or number.

--- sql/src/main/java/org/apache/druid/sql/calcite/expression/builtin/ReductionOperatorConversionHelper.java
+++ sql/src/main/java/org/apache/druid/sql/calcite/expression/builtin/ReductionOperatorConversionHelper.java
@@ -21,12 +21,22 @@ package org.apache.druid.sql.calcite.expression.builtin;
 
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
+import org.apache.calcite.sql.SqlCallBinding;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.SqlOperandCountRange;
+import org.apache.calcite.sql.SqlOperator;
+import org.apache.calcite.sql.type.SqlOperandCountRanges;
+import org.apache.calcite.sql.type.SqlOperandTypeChecker;
 import org.apache.calcite.sql.type.SqlReturnTypeInference;
+import org.apache.calcite.sql.type.SqlTypeFamily;
 import org.apache.calcite.sql.type.SqlTypeName;
+import org.apache.druid.error.DruidException;
 import org.apache.druid.java.util.common.IAE;
 import org.apache.druid.math.expr.ExpressionTypeConversion;
+import org.apache.druid.math.expr.Function;
 import org.apache.druid.segment.column.ColumnType;
 import org.apache.druid.segment.column.ValueType;
+import org.apache.druid.sql.calcite.expression.OperatorConversions;
 import org.apache.druid.sql.calcite.planner.Calcites;
 
 class ReductionOperatorConversionHelper
@@ -53,7 +63,6 @@ class ReductionOperatorConversionHelper
 
         SqlTypeName returnSqlTypeName = SqlTypeName.NULL;
         boolean hasDouble = false;
-        boolean isString = false;
         for (int i = 0; i < n; i++) {
           final RelDataType type = opBinding.getOperandType(i);
           final SqlTypeName sqlTypeName = type.getSqlTypeName();
@@ -70,13 +79,16 @@ class ReductionOperatorConversionHelper
           if (valueType != null) {
             if (valueType.is(ValueType.STRING)) {
               returnSqlTypeName = sqlTypeName;
-              isString = true;
               break;
             } else if (valueType.anyOf(ValueType.DOUBLE, ValueType.FLOAT)) {
               returnSqlTypeName = SqlTypeName.DOUBLE;
               hasDouble = true;
             } else if (valueType.is(ValueType.LONG) && !hasDouble) {
               returnSqlTypeName = SqlTypeName.BIGINT;
+            } else {
+              // The operand checker of the function should prevent other types from reaching us.
+              // Throw a defensive exception if we encounter one.
+              throw DruidException.defensive("Got type[%s], which should have been a validation error.", type);
             }
           } else if (sqlTypeName != SqlTypeName.NULL) {
             throw new IAE("Argument %d has invalid type: %s", i, sqlTypeName);
@@ -85,5 +97,52 @@ class ReductionOperatorConversionHelper
 
         return typeFactory.createTypeWithNullability(typeFactory.createSqlType(returnSqlTypeName), true);
       };
+
+  /**
+   * Type checker that matches the implementation of {@link Function.ReduceFunction}: only accept SQL types that
+   * are represented by string or number at the execution layer.
+   */
+  static final SqlOperandTypeChecker OPERAND_TYPE_CHECKER = new SqlOperandTypeChecker()
+  {
+    @Override
+    public boolean checkOperandTypes(
+        final SqlCallBinding callBinding,
+        final boolean throwOnFailure
+    )
+    {
+      for (SqlNode operand : callBinding.operands()) {
+        final RelDataType type = callBinding.getValidator().deriveType(callBinding.getScope(), operand);
+        final boolean validType =
+            SqlTypeFamily.STRING.contains(type)
+            || SqlTypeFamily.NUMERIC.contains(type)
+            || SqlTypeFamily.DATETIME.contains(type)
+            || SqlTypeFamily.DATETIME_INTERVAL.contains(type)
+            || SqlTypeFamily.BOOLEAN.contains(type)
+            || SqlTypeFamily.NULL.contains(type);
+
+        if (!validType) {
+          return OperatorConversions.throwOrReturn(
+              throwOnFailure,
+              callBinding,
+              SqlCallBinding::newValidationSignatureError
+          );
+        }
+      }
+
+      return true;
+    }
+
+    @Override
+    public SqlOperandCountRange getOperandCountRange()
+    {
+      return SqlOperandCountRanges.any();
+    }
+
+    @Override
+    public String getAllowedSignatures(SqlOperator op, String opName)
+    {
+      return opName + "(<STRING | NUMERIC | DATETIME | BOOLEAN | NULL>, ...)";
+    }
+  };
 }
 
