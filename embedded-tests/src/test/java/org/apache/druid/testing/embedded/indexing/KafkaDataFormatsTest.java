10380fe87acb7f1bfeb7c30b87c0c2f54fc0459d
Zoltan Haindrich <kirk@rxd.hu>
2025-09-24 13:41:37 +0000

    fix-test

--- embedded-tests/src/test/java/org/apache/druid/testing/embedded/indexing/KafkaDataFormatsTest.java
+++ embedded-tests/src/test/java/org/apache/druid/testing/embedded/indexing/KafkaDataFormatsTest.java
@@ -140,7 +140,7 @@ public class KafkaDataFormatsTest extends EmbeddedClusterTestBase
     int recordCount = generateStreamAndPublishToKafka(dataSource, serializer, false);
 
     Map<String, Object> avroSchema = createWikipediaAvroSchemaMap();
-
+    
     InlineSchemaAvroBytesDecoder avroBytesDecoder = new InlineSchemaAvroBytesDecoder(
         overlord.bindings().jsonMapper(),
         avroSchema
@@ -175,23 +175,23 @@ public class KafkaDataFormatsTest extends EmbeddedClusterTestBase
     kafkaServer.createTopicWithPartitions(dataSource, 3);
     EventSerializer serializer = new AvroEventSerializer();
     int recordCount = generateStreamAndPublishToKafka(dataSource, serializer, false);
-
+    
     Map<String, Object> avroSchema = createWikipediaAvroSchemaMap();
-
+    
     InlineSchemaAvroBytesDecoder avroBytesDecoder = new InlineSchemaAvroBytesDecoder(
         overlord.bindings().jsonMapper(),
         avroSchema
     );
-
+    
     JSONPathSpec flattenSpec = new JSONPathSpec(true, null);
-
+    
     AvroStreamInputFormat inputFormat = new AvroStreamInputFormat(
         flattenSpec,
         avroBytesDecoder,
         false,
         null
     );
-
+    
     KafkaSupervisorSpec supervisorSpec = createKafkaSupervisor(dataSource, dataSource, inputFormat);
     final String supervisorId = cluster.callApi().postSupervisor(supervisorSpec);
     Assertions.assertEquals(dataSource, supervisorId);
@@ -618,7 +618,7 @@ public class KafkaDataFormatsTest extends EmbeddedClusterTestBase
     schema.put("namespace", "org.apache.druid");
     schema.put("name", "wikipedia");
     schema.put("type", "record");
-
+    
     List<Map<String, Object>> fields = new ArrayList<>();
     fields.add(Map.of("name", "timestamp", "type", "string"));
     fields.add(Map.of("name", "page", "type", "string"));
@@ -636,7 +636,7 @@ public class KafkaDataFormatsTest extends EmbeddedClusterTestBase
     fields.add(Map.of("name", "added", "type", "long"));
     fields.add(Map.of("name", "deleted", "type", "long"));
     fields.add(Map.of("name", "delta", "type", "long"));
-
+    
     schema.put("fields", fields);
     return schema;
   }
