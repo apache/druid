9b6913a6cdb3fd6178c47d5bd27e2d244892d218
Karan Kumar <karankumar1100@gmail.com>
2025-11-11 08:08:37 +0530

    Auto switch cgroup monitors from v1 to v2 (#18705)

--- processing/src/main/java/org/apache/druid/java/util/metrics/CgroupV2CpuMonitor.java
+++ processing/src/main/java/org/apache/druid/java/util/metrics/CgroupV2CpuMonitor.java
@@ -21,8 +21,6 @@ package org.apache.druid.java.util.metrics;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.primitives.Longs;
-import org.apache.druid.java.util.common.logger.Logger;
 import org.apache.druid.java.util.emitter.service.ServiceEmitter;
 import org.apache.druid.java.util.emitter.service.ServiceMetricEvent;
 import org.apache.druid.java.util.metrics.cgroups.CgroupDiscoverer;
@@ -30,23 +28,15 @@ import org.apache.druid.java.util.metrics.cgroups.Cpu;
 import org.apache.druid.java.util.metrics.cgroups.ProcCgroupV2Discoverer;
 import org.apache.druid.java.util.metrics.cgroups.ProcSelfCgroupDiscoverer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 /**
  * Monitor that reports cpu usage stats by reading `cpu.stat` reported by cgroupv2
  */
 public class CgroupV2CpuMonitor extends FeedDefiningMonitor
 {
-  private static final Logger LOG = new Logger(CgroupV2CpuMonitor.class);
-  private static final String CPU_STAT_FILE = "cpu.stat";
   private static final String SNAPSHOT = "snapshot";
   final CgroupDiscoverer cgroupDiscoverer;
   final Map<String, String[]> dimensions;
@@ -75,13 +65,21 @@ public class CgroupV2CpuMonitor extends FeedDefiningMonitor
   {
     final ServiceMetricEvent.Builder builder = builder();
     MonitorUtils.addDimensionsToBuilder(builder, dimensions);
-    Snapshot snapshot = snapshot();
+    builder.setDimension("cgroupversion", cgroupDiscoverer.getCgroupVersion().name());
+    final Cpu.CpuMetrics cpuSnapshot = cgroupDiscoverer.getCpuMetrics();
+    emitter.emit(builder.setMetric("cgroup/cpu/shares", cpuSnapshot.getShares()));
+    emitter.emit(builder.setMetric(
+        "cgroup/cpu/cores_quota",
+        CgroupUtil.computeProcessorQuota(cpuSnapshot.getQuotaUs(), cpuSnapshot.getPeriodUs())
+    ));
+
+
     final Map<String, Long> elapsed = diff.to(
         "usage",
         ImmutableMap.<String, Long>builder()
-                    .put(CgroupUtil.USER, snapshot.getUserUsec())
-                    .put(CgroupUtil.SYSTEM, snapshot.getSystemUsec())
-                    .put(CgroupUtil.TOTAL, snapshot.getUsageUsec())
+                    .put(CgroupUtil.USER, cpuSnapshot.getUserUs())
+                    .put(CgroupUtil.SYSTEM, cpuSnapshot.getSystemUs())
+                    .put(CgroupUtil.TOTAL, cpuSnapshot.getTotalUs())
                     .put(SNAPSHOT, ChronoUnit.MICROS.between(Instant.EPOCH, Instant.now()))
                     .build()
     );
@@ -96,68 +94,5 @@ public class CgroupV2CpuMonitor extends FeedDefiningMonitor
       emitter.emit(builder.setMetric(CgroupUtil.CPU_USER_USAGE_METRIC, userUsagePct));
     }
     return true;
-  }
-
-  /*
-  file: cpu.stat
-
-  sample content:
-  usage_usec 2379951538
-  user_usec 1802023024
-  system_usec 577928513
-  nr_periods 1581231
-  nr_throttled 59
-  throttled_usec 3095133
-  */
-  public Snapshot snapshot()
-  {
-    Map<String, Long> entries = new HashMap<>();
-    try (final BufferedReader reader = Files.newBufferedReader(
-        Paths.get(cgroupDiscoverer.discover(Cpu.CGROUP).toString(), CPU_STAT_FILE)
-    )) {
-      for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-        final String[] parts = line.split(Pattern.quote(" "));
-        if (parts.length != 2) {
-          // ignore
-          continue;
-        }
-        entries.put(parts[0], Longs.tryParse(parts[1]));
-      }
-    }
-    catch (IOException | RuntimeException ex) {
-      LOG.error(ex, "Unable to fetch cpu snapshot");
-    }
-
-    return new Snapshot(entries.get("usage_usec"), entries.get("user_usec"), entries.get("system_usec"));
-  }
-
-
-  public static class Snapshot
-  {
-    private final long usageUsec;
-    private final long userUsec;
-    private final long systemUsec;
-
-    public Snapshot(long usageUsec, long userUsec, long systemUsec)
-    {
-      this.usageUsec = usageUsec;
-      this.userUsec = userUsec;
-      this.systemUsec = systemUsec;
-    }
-
-    public long getUsageUsec()
-    {
-      return usageUsec;
-    }
-
-    public long getUserUsec()
-    {
-      return userUsec;
-    }
-
-    public long getSystemUsec()
-    {
-      return systemUsec;
-    }
   }
 }
