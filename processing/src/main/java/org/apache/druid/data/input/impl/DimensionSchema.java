d788bb405bd1ba4497c7d0d13cab1f641cbb05ea
Clint Wylie <cwylie@apache.org>
2025-11-19 11:41:39 -0800

    add javadoc

--- processing/src/main/java/org/apache/druid/data/input/impl/DimensionSchema.java
+++ processing/src/main/java/org/apache/druid/data/input/impl/DimensionSchema.java
@@ -176,6 +176,11 @@ public abstract class DimensionSchema
   @JsonIgnore
   public abstract ColumnType getColumnType();
 
+  /**
+   * Returns true if the {@link DimensionHandler#makeIndexer()} of this schema can produce multi-valued
+   * {@link ColumnType#STRING} columns. This method is used by MSQ compaction to determine if it needs to download the
+   * segments to check if any are actually multi-valued.
+   */
   @JsonIgnore
   public boolean canBeMultiValued()
   {
