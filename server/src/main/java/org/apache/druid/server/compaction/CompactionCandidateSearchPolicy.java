63ae29dc29ca04eef1980d838a0163974171d1d8
Gian Merlino <gianmerlino@gmail.com>
2025-12-05 21:42:35 -0800

    Remove unused import.

--- server/src/main/java/org/apache/druid/server/compaction/CompactionCandidateSearchPolicy.java
+++ server/src/main/java/org/apache/druid/server/compaction/CompactionCandidateSearchPolicy.java
@@ -21,7 +21,6 @@ package org.apache.druid.server.compaction;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import org.apache.druid.annotations.SubclassesMustOverrideEqualsAndHashCode;
 import org.apache.druid.java.util.common.StringUtils;
 import org.apache.druid.server.coordinator.duty.CompactSegments;
 
