69eb43a51ac7993f78054abcbae0e205e97c8caf
Sasha Syrotenko <alexander.syrotenko@imply.io>
2026-01-13 08:29:35 +0200

    Create cost autoscaler documentation, cleanup stream supervisor tests (#18904)
    * Create CostBasedAutoScaler documentation, cleanup stream supervisor tests
    
    * Fix lint issue
    
    * Spell check and review

--- indexing-service/src/test/java/org/apache/druid/indexing/seekablestream/supervisor/SeekableStreamSupervisorSpecTest.java
+++ indexing-service/src/test/java/org/apache/druid/indexing/seekablestream/supervisor/SeekableStreamSupervisorSpecTest.java
@@ -17,103 +17,56 @@
  * under the License.
  */
 
-package org.apache.druid.indexing.seekablestream;
+package org.apache.druid.indexing.seekablestream.supervisor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import org.apache.druid.data.input.impl.ByteEntity;
-import org.apache.druid.data.input.impl.DimensionSchema;
 import org.apache.druid.data.input.impl.JsonInputFormat;
-import org.apache.druid.data.input.impl.StringDimensionSchema;
-import org.apache.druid.data.input.impl.TimestampSpec;
 import org.apache.druid.error.DruidException;
 import org.apache.druid.error.DruidExceptionMatcher;
-import org.apache.druid.indexer.granularity.UniformGranularitySpec;
-import org.apache.druid.indexing.common.task.Task;
-import org.apache.druid.indexing.overlord.DataSourceMetadata;
-import org.apache.druid.indexing.overlord.IndexerMetadataStorageCoordinator;
-import org.apache.druid.indexing.overlord.TaskMaster;
-import org.apache.druid.indexing.overlord.TaskStorage;
 import org.apache.druid.indexing.overlord.supervisor.Supervisor;
 import org.apache.druid.indexing.overlord.supervisor.SupervisorManager;
 import org.apache.druid.indexing.overlord.supervisor.SupervisorStateManager;
 import org.apache.druid.indexing.overlord.supervisor.SupervisorStateManagerConfig;
-import org.apache.druid.indexing.overlord.supervisor.autoscaler.LagStats;
 import org.apache.druid.indexing.overlord.supervisor.autoscaler.SupervisorTaskAutoScaler;
-import org.apache.druid.indexing.seekablestream.common.OrderedSequenceNumber;
-import org.apache.druid.indexing.seekablestream.common.RecordSupplier;
-import org.apache.druid.indexing.seekablestream.supervisor.IdleConfig;
-import org.apache.druid.indexing.seekablestream.supervisor.LagAggregator;
-import org.apache.druid.indexing.seekablestream.supervisor.SeekableStreamSupervisor;
-import org.apache.druid.indexing.seekablestream.supervisor.SeekableStreamSupervisorIOConfig;
-import org.apache.druid.indexing.seekablestream.supervisor.SeekableStreamSupervisorIngestionSpec;
-import org.apache.druid.indexing.seekablestream.supervisor.SeekableStreamSupervisorReportPayload;
-import org.apache.druid.indexing.seekablestream.supervisor.SeekableStreamSupervisorSpec;
-import org.apache.druid.indexing.seekablestream.supervisor.SeekableStreamSupervisorTuningConfig;
+import org.apache.druid.indexing.seekablestream.SeekableStreamIndexTaskClientFactory;
 import org.apache.druid.indexing.seekablestream.supervisor.autoscaler.AutoScalerConfig;
 import org.apache.druid.indexing.seekablestream.supervisor.autoscaler.LagBasedAutoScaler;
 import org.apache.druid.indexing.seekablestream.supervisor.autoscaler.LagBasedAutoScalerConfig;
 import org.apache.druid.indexing.seekablestream.supervisor.autoscaler.NoopTaskAutoScaler;
 import org.apache.druid.jackson.DefaultObjectMapper;
 import org.apache.druid.java.util.common.StringUtils;
-import org.apache.druid.java.util.common.granularity.Granularities;
 import org.apache.druid.java.util.common.parsers.JSONPathSpec;
-import org.apache.druid.java.util.emitter.service.ServiceEmitter;
 import org.apache.druid.java.util.metrics.DruidMonitorSchedulerConfig;
 import org.apache.druid.java.util.metrics.StubServiceEmitter;
 import org.apache.druid.metadata.MetadataSupervisorManager;
 import org.apache.druid.metadata.TestSupervisorSpec;
 import org.apache.druid.query.DruidMetrics;
-import org.apache.druid.query.aggregation.CountAggregatorFactory;
-import org.apache.druid.segment.TestHelper;
-import org.apache.druid.segment.incremental.RowIngestionMetersFactory;
 import org.apache.druid.segment.indexing.DataSchema;
 import org.easymock.EasyMock;
-import org.easymock.EasyMockSupport;
 import org.hamcrest.MatcherAssert;
-import org.joda.time.DateTime;
-import org.joda.time.Duration;
 import org.joda.time.Period;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.annotation.Nullable;
-import java.io.File;
-import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.TreeMap;
-import java.util.concurrent.ScheduledExecutorService;
 
 import static org.junit.Assert.assertThrows;
 
-public class SeekableStreamSupervisorSpecTest extends EasyMockSupport
+public class SeekableStreamSupervisorSpecTest extends SeekableStreamSupervisorTestBase
 {
   private SeekableStreamSupervisorIngestionSpec ingestionSchema;
-  private TaskStorage taskStorage;
-  private TaskMaster taskMaster;
-  private IndexerMetadataStorageCoordinator indexerMetadataStorageCoordinator;
-  private StubServiceEmitter emitter;
-  private RowIngestionMetersFactory rowIngestionMetersFactory;
   private DataSchema dataSchema;
   private SeekableStreamSupervisorTuningConfig seekableStreamSupervisorTuningConfig;
   private SeekableStreamSupervisorIOConfig seekableStreamSupervisorIOConfig;
-  private static final ObjectMapper OBJECT_MAPPER = TestHelper.makeJsonMapper();
-  private SeekableStreamIndexTaskClientFactory taskClientFactory;
-  private static final String STREAM = "stream";
-  private static final String DATASOURCE = "testDS";
-  private static final String SUPERVISOR = "supervisor";
-  private SeekableStreamSupervisorSpec spec;
   private SupervisorStateManagerConfig supervisorConfig;
-
   private SeekableStreamSupervisor supervisor4;
 
   private SeekableStreamIndexTaskClientFactory indexTaskClientFactory;
@@ -125,16 +78,9 @@ public class SeekableStreamSupervisorSpecTest extends EasyMockSupport
   public void setUp()
   {
     ingestionSchema = EasyMock.mock(SeekableStreamSupervisorIngestionSpec.class);
-    taskStorage = EasyMock.mock(TaskStorage.class);
-    taskMaster = EasyMock.mock(TaskMaster.class);
-    indexerMetadataStorageCoordinator = EasyMock.mock(IndexerMetadataStorageCoordinator.class);
-    emitter = new StubServiceEmitter();
-    rowIngestionMetersFactory = EasyMock.mock(RowIngestionMetersFactory.class);
     dataSchema = EasyMock.mock(DataSchema.class);
     seekableStreamSupervisorTuningConfig = EasyMock.mock(SeekableStreamSupervisorTuningConfig.class);
     seekableStreamSupervisorIOConfig = EasyMock.mock(SeekableStreamSupervisorIOConfig.class);
-    taskClientFactory = EasyMock.mock(SeekableStreamIndexTaskClientFactory.class);
-    spec = EasyMock.mock(SeekableStreamSupervisorSpec.class);
     supervisorConfig = new SupervisorStateManagerConfig();
     indexTaskClientFactory = EasyMock.mock(SeekableStreamIndexTaskClientFactory.class);
     mapper = new DefaultObjectMapper();
@@ -143,403 +89,6 @@ public class SeekableStreamSupervisorSpecTest extends EasyMockSupport
     supervisor4 = EasyMock.mock(SeekableStreamSupervisor.class);
 
     EasyMock.expect(spec.getContextValue(DruidMetrics.TAGS)).andReturn(null).anyTimes();
-  }
-
-  private abstract class BaseTestSeekableStreamSupervisor extends SeekableStreamSupervisor<String, String, ByteEntity>
-  {
-    private BaseTestSeekableStreamSupervisor()
-    {
-      super(
-          "testSupervisorId",
-          taskStorage,
-          taskMaster,
-          indexerMetadataStorageCoordinator,
-          taskClientFactory,
-          OBJECT_MAPPER,
-          spec,
-          rowIngestionMetersFactory,
-          false
-      );
-    }
-
-    @Override
-    protected String baseTaskName()
-    {
-      return "test";
-    }
-
-    @Override
-    protected void updatePartitionLagFromStream()
-    {
-      // do nothing
-    }
-
-    @Nullable
-    @Override
-    protected Map<String, Long> getPartitionRecordLag()
-    {
-      return null;
-    }
-
-    @Nullable
-    @Override
-    protected Map<String, Long> getPartitionTimeLag()
-    {
-      return null;
-    }
-
-    @Override
-    protected SeekableStreamIndexTaskIOConfig createTaskIoConfig(
-        int groupId,
-        Map<String, String> startPartitions,
-        Map<String, String> endPartitions,
-        String baseSequenceName,
-        DateTime minimumMessageTime,
-        DateTime maximumMessageTime,
-        Set<String> exclusiveStartSequenceNumberPartitions,
-        SeekableStreamSupervisorIOConfig ioConfig
-    )
-    {
-      return new SeekableStreamIndexTaskIOConfig<>(
-          groupId,
-          baseSequenceName,
-          new SeekableStreamStartSequenceNumbers<>(STREAM, startPartitions, exclusiveStartSequenceNumberPartitions),
-          new SeekableStreamEndSequenceNumbers<>(STREAM, endPartitions),
-          true,
-          minimumMessageTime,
-          maximumMessageTime,
-          ioConfig.getInputFormat(),
-          ioConfig.getTaskDuration().getStandardMinutes()
-      )
-      {
-      };
-    }
-
-    @Override
-    protected List<SeekableStreamIndexTask<String, String, ByteEntity>> createIndexTasks(
-        int replicas,
-        String baseSequenceName,
-        ObjectMapper sortingMapper,
-        TreeMap<Integer, Map<String, String>> sequenceOffsets,
-        SeekableStreamIndexTaskIOConfig taskIoConfig,
-        SeekableStreamIndexTaskTuningConfig taskTuningConfig,
-        RowIngestionMetersFactory rowIngestionMetersFactory
-    )
-    {
-      return null;
-    }
-
-    @Override
-    protected int getTaskGroupIdForPartition(String partition)
-    {
-      return 0;
-    }
-
-    @Override
-    protected boolean checkSourceMetadataMatch(DataSourceMetadata metadata)
-    {
-      return true;
-    }
-
-    @Override
-    protected boolean doesTaskMatchSupervisor(Task task)
-    {
-      return true;
-    }
-
-    @Override
-    protected SeekableStreamDataSourceMetadata<String, String> createDataSourceMetaDataForReset(
-        String stream,
-        Map<String, String> map
-    )
-    {
-      return null;
-    }
-
-    @Override
-    protected OrderedSequenceNumber<String> makeSequenceNumber(String seq, boolean isExclusive)
-    {
-      return new OrderedSequenceNumber<>(seq, isExclusive)
-      {
-        @Override
-        public int compareTo(OrderedSequenceNumber<String> o)
-        {
-          return new BigInteger(this.get()).compareTo(new BigInteger(o.get()));
-        }
-      };
-    }
-
-    @Override
-    protected Map<String, Long> getRecordLagPerPartition(Map<String, String> currentOffsets)
-    {
-      return null;
-    }
-
-    @Override
-    protected Map<String, Long> getTimeLagPerPartition(Map<String, String> currentOffsets)
-    {
-      return null;
-    }
-
-    @Override
-    protected RecordSupplier<String, String, ByteEntity> setupRecordSupplier()
-    {
-      return recordSupplier;
-    }
-
-    @Override
-    protected SeekableStreamSupervisorReportPayload<String, String> createReportPayload(
-        int numPartitions,
-        boolean includeOffsets
-    )
-    {
-      return new SeekableStreamSupervisorReportPayload<>(
-          SUPERVISOR,
-          DATASOURCE,
-          STREAM,
-          1,
-          1,
-          1L,
-          null,
-          null,
-          null,
-          null,
-          null,
-          null,
-          false,
-          true,
-          null,
-          null,
-          null
-      )
-      {
-      };
-    }
-
-    @Override
-    protected String getNotSetMarker()
-    {
-      return "NOT_SET";
-    }
-
-    @Override
-    protected String getEndOfPartitionMarker()
-    {
-      return "EOF";
-    }
-
-    @Override
-    protected boolean isEndOfShard(String seqNum)
-    {
-      return false;
-    }
-
-    @Override
-    protected boolean isShardExpirationMarker(String seqNum)
-    {
-      return false;
-    }
-
-    @Override
-    protected boolean useExclusiveStartSequenceNumberForNonFirstSequence()
-    {
-      return false;
-    }
-  }
-
-  private class TestSeekableStreamSupervisor extends BaseTestSeekableStreamSupervisor
-  {
-    private int partitionNumbers;
-
-    public TestSeekableStreamSupervisor(int partitionNumbers)
-    {
-      this.partitionNumbers = partitionNumbers;
-    }
-
-    @Override
-    protected void scheduleReporting(ScheduledExecutorService reportingExec)
-    {
-      // do nothing
-    }
-
-    @Override
-    public LagStats computeLagStats()
-    {
-      return new LagStats(0, 0, 0);
-    }
-
-    @Override
-    public int getPartitionCount()
-    {
-      return partitionNumbers;
-    }
-  }
-
-  private class StateOverrideTestSeekableStreamSupervisor extends TestSeekableStreamSupervisor
-  {
-    private SupervisorStateManager.State state;
-
-    public StateOverrideTestSeekableStreamSupervisor(SupervisorStateManager.State state, int partitionNumbers)
-    {
-      super(partitionNumbers);
-      this.state = state;
-    }
-
-    @Override
-    public SupervisorStateManager.State getState()
-    {
-      return state;
-    }
-  }
-
-  private static class TestSeekableStreamSupervisorSpec extends SeekableStreamSupervisorSpec
-  {
-    private SeekableStreamSupervisor supervisor;
-
-    public TestSeekableStreamSupervisorSpec(
-        SeekableStreamSupervisorIngestionSpec ingestionSchema,
-        @Nullable Map<String, Object> context,
-        Boolean suspended,
-        TaskStorage taskStorage,
-        TaskMaster taskMaster,
-        IndexerMetadataStorageCoordinator indexerMetadataStorageCoordinator,
-        SeekableStreamIndexTaskClientFactory indexTaskClientFactory,
-        ObjectMapper mapper,
-        ServiceEmitter emitter,
-        DruidMonitorSchedulerConfig monitorSchedulerConfig,
-        RowIngestionMetersFactory rowIngestionMetersFactory,
-        SupervisorStateManagerConfig supervisorStateManagerConfig,
-        SeekableStreamSupervisor supervisor,
-        String id
-    )
-    {
-      super(
-          id,
-          ingestionSchema,
-          context,
-          suspended,
-          taskStorage,
-          taskMaster,
-          indexerMetadataStorageCoordinator,
-          indexTaskClientFactory,
-          mapper,
-          emitter,
-          monitorSchedulerConfig,
-          rowIngestionMetersFactory,
-          supervisorStateManagerConfig
-      );
-
-      this.supervisor = supervisor;
-    }
-
-    @Override
-    public Supervisor createSupervisor()
-    {
-      return supervisor;
-    }
-
-    @Override
-    public String getType()
-    {
-      return null;
-    }
-
-    @Override
-    public String getSource()
-    {
-      return null;
-    }
-
-    @Override
-    protected SeekableStreamSupervisorSpec toggleSuspend(boolean suspend)
-    {
-      return null;
-    }
-  }
-
-  private static SeekableStreamSupervisorTuningConfig getTuningConfig()
-  {
-    return new SeekableStreamSupervisorTuningConfig()
-    {
-      @Override
-      public Integer getWorkerThreads()
-      {
-        return 1;
-      }
-
-      @Override
-      public Long getChatRetries()
-      {
-        return 1L;
-      }
-
-      @Override
-      public Duration getHttpTimeout()
-      {
-        return new Period("PT1M").toStandardDuration();
-      }
-
-      @Override
-      public Duration getShutdownTimeout()
-      {
-        return new Period("PT1S").toStandardDuration();
-      }
-
-      @Override
-      public Duration getRepartitionTransitionDuration()
-      {
-        return new Period("PT2M").toStandardDuration();
-      }
-
-      @Override
-      public Duration getOffsetFetchPeriod()
-      {
-        return new Period("PT5M").toStandardDuration();
-      }
-
-      @Override
-      public SeekableStreamIndexTaskTuningConfig convertToTaskTuningConfig()
-      {
-        return new SeekableStreamIndexTaskTuningConfig(
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null
-        )
-        {
-          @Override
-          public SeekableStreamIndexTaskTuningConfig withBasePersistDirectory(File dir)
-          {
-            return null;
-          }
-
-          @Override
-          public String toString()
-          {
-            return null;
-          }
-        };
-      }
-    };
   }
 
   @Test
@@ -1528,7 +1077,11 @@ public class SeekableStreamSupervisorSpecTest extends EasyMockSupport
             DruidException.Category.INVALID_INPUT,
             "invalidInput"
         ).expectMessageIs(
-            StringUtils.format("Cannot update supervisor spec from type[%s] to type[%s]", proposedSpec.getClass().getSimpleName(), otherSpec.getClass().getSimpleName())
+            StringUtils.format(
+                "Cannot update supervisor spec from type[%s] to type[%s]",
+                proposedSpec.getClass().getSimpleName(),
+                otherSpec.getClass().getSimpleName()
+            )
         )
     );
   }
@@ -1897,27 +1450,6 @@ public class SeekableStreamSupervisorSpecTest extends EasyMockSupport
     EasyMock.replay(dataSchema);
   }
 
-  private static DataSchema getDataSchema()
-  {
-    List<DimensionSchema> dimensions = new ArrayList<>();
-    dimensions.add(StringDimensionSchema.create("dim1"));
-    dimensions.add(StringDimensionSchema.create("dim2"));
-
-    return DataSchema.builder()
-                     .withDataSource(DATASOURCE)
-                     .withTimestamp(new TimestampSpec("timestamp", "iso", null))
-                     .withDimensions(dimensions)
-                     .withAggregators(new CountAggregatorFactory("rows"))
-                     .withGranularity(
-                         new UniformGranularitySpec(
-                             Granularities.HOUR,
-                             Granularities.NONE,
-                             ImmutableList.of()
-                         )
-                     )
-                     .build();
-  }
-
   private SeekableStreamSupervisorIOConfig getIOConfig(int taskCount, boolean scaleOut)
   {
     if (scaleOut) {
@@ -1963,6 +1495,11 @@ public class SeekableStreamSupervisorSpecTest extends EasyMockSupport
       {
       };
     }
+  }
+
+  private static DataSchema getDataSchema()
+  {
+    return getDataSchema(DATASOURCE);
   }
 
   private static Map<String, Object> getScaleOutProperties(int maxTaskCount)
