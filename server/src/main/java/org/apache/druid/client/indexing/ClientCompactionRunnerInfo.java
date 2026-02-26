d5d63c753cf5c3216081ebd0dc9797deb8c72876
Clint Wylie <cwylie@apache.org>
2026-02-26 15:41:10 -0800

    fixes

--- server/src/main/java/org/apache/druid/client/indexing/ClientCompactionRunnerInfo.java
+++ server/src/main/java/org/apache/druid/client/indexing/ClientCompactionRunnerInfo.java
@@ -43,8 +43,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 
 /**
@@ -171,9 +169,6 @@ public class ClientCompactionRunnerInfo
       );
     }
     if (partitionsSpec instanceof DimensionRangePartitionsSpec && dimensionSchemas != null) {
-      Map<String, DimensionSchema> dimensionSchemaMap = dimensionSchemas.stream().collect(
-          Collectors.toMap(DimensionSchema::getName, Function.identity())
-      );
       RowSignature.Builder baseSignatureBuilder = RowSignature.builder().addTimeColumn();
       for (DimensionSchema dimensionSchema : dimensionSchemas) {
         baseSignatureBuilder.add(dimensionSchema.getName(), dimensionSchema.getColumnType());
