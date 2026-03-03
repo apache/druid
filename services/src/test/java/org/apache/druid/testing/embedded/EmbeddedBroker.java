f4e11b938c817a024117fdf7885794ab9c22acd3
Kashif Faraz <kashif.faraz@gmail.com>
2026-03-03 11:43:40 +0530

    Reduce metadata poll period in embedded tests

--- services/src/test/java/org/apache/druid/testing/embedded/EmbeddedBroker.java
+++ services/src/test/java/org/apache/druid/testing/embedded/EmbeddedBroker.java
@@ -50,6 +50,7 @@ public class EmbeddedBroker extends EmbeddedDruidServer<EmbeddedBroker>
     {
       this.handler = handler;
       addProperty("druid.sql.planner.metadataRefreshPeriod", "PT0.1s");
+      addProperty("druid.sql.planner.metadataSegmentPollPeriod", "PT0.1s");
     }
 
     @Override
