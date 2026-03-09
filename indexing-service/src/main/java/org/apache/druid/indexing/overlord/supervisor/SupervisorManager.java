e5f6f6eacc4eb0c87f0192d1089a10376ebc3b23
Aru Raghuwanshi <aruraghuwanshi@gmail.com>
2026-03-09 14:54:03 -0700

    Fix import order in SupervisorManager to satisfy Checkstyle

--- indexing-service/src/main/java/org/apache/druid/indexing/overlord/supervisor/SupervisorManager.java
+++ indexing-service/src/main/java/org/apache/druid/indexing/overlord/supervisor/SupervisorManager.java
@@ -46,8 +46,8 @@ import org.apache.druid.server.metrics.SupervisorStatsProvider;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
