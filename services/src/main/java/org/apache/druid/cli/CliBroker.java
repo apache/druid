74342b2a582576cf7aae32dd7a9ab0d1709b91aa
mshahid6 <maryam.shahid1299@gmail.com>
2026-03-09 14:21:17 -0700

    Broker Config and Query Blocklist (#19011)
    Added a new BrokerDynamicConfig to enable dynamic, runtime configuration of Druid brokers without restarts and a query blocklist for dynamically blocking queries in the situation there is a rogue app/user spamming the cluster without relying on static configs/restarts.

--- services/src/main/java/org/apache/druid/cli/CliBroker.java
+++ services/src/main/java/org/apache/druid/cli/CliBroker.java
@@ -28,6 +28,7 @@ import com.google.inject.Module;
 import com.google.inject.name.Names;
 import org.apache.druid.client.BrokerSegmentWatcherConfig;
 import org.apache.druid.client.BrokerServerView;
+import org.apache.druid.client.BrokerViewOfBrokerConfig;
 import org.apache.druid.client.BrokerViewOfCoordinatorConfig;
 import org.apache.druid.client.CachingClusteredClient;
 import org.apache.druid.client.DirectDruidClientFactory;
@@ -180,6 +181,7 @@ public class CliBroker extends ServerRunnable
           LifecycleModule.register(binder, Server.class);
           binder.bind(SegmentManager.class).in(LazySingleton.class);
           binder.bind(BrokerViewOfCoordinatorConfig.class).in(ManageLifecycle.class);
+          binder.bind(BrokerViewOfBrokerConfig.class).in(ManageLifecycle.class);
           binder.bind(ZkCoordinator.class).in(ManageLifecycle.class);
           binder.bind(ServerTypeConfig.class).toInstance(new ServerTypeConfig(ServerType.BROKER));
           Jerseys.addResource(binder, HistoricalResource.class);
