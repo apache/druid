b81ebcee78eab89df68e1f8b08a6bcdb4ad88ddb
Lucas Capistrant <capistrant@users.noreply.github.com>
2026-03-10 11:34:49 -0500

    Upgrade Kubernetes clients and related dependencies in `kubernetes-overlord-extensions` and `druid-kubernetes-extensions` (#19071)
    * fabric8 bump checkpoint
    
    * expose webclientoptions in configuration and override additionalconfig for vertx
    
    * document new power
    
    * Remove unnecessary complication of the druid vertx factory wrapper
    
    * use junit5
    
    * bump okhttp to try and fix a dependency enforcer
    
    * update licenses.yaml
    
    * nit spacing
    
    * min dep enforcer requires bump of kotlin-stdlib
    
    * tinkering with licenses yaml
    
    * Bump kubernetes-extensions k8s dep so we can align on okhttp
    
    also get a bunch of other licenses in line
    
    * cleanup object mapper for k8s-ol-ext
    
    * minor dep updates + exclude protobuf from druid-kubernetes-extensions
    
    * Keep working on getting these dependencies to play nicely together
    
    * Update aws sdk in licenses
    
    * fix kubernetes-extensions pom
    
    * Working on static checks still
    
    * k8s overlord ext pom

--- extensions-core/kubernetes-overlord-extensions/src/test/java/org/apache/druid/k8s/overlord/taskadapter/DruidPeonClientIntegrationTest.java
+++ extensions-core/kubernetes-overlord-extensions/src/test/java/org/apache/druid/k8s/overlord/taskadapter/DruidPeonClientIntegrationTest.java
@@ -90,7 +90,7 @@ public class DruidPeonClientIntegrationTest
         new NamedType(ParallelIndexTuningConfig.class, "index_parallel"),
         new NamedType(IndexTask.IndexTuningConfig.class, "index")
     );
-    k8sClient = new DruidKubernetesClient(new DruidKubernetesVertxHttpClientFactory(new DruidKubernetesVertxHttpClientConfig()), new ConfigBuilder().build());
+    k8sClient = new DruidKubernetesClient(new DruidKubernetesVertxHttpClientFactory(new DruidKubernetesVertxHttpClientConfig(), new ObjectMapper()), new ConfigBuilder().build());
     peonClient = new KubernetesPeonClient(k8sClient, "default", null, false, new NoopServiceEmitter());
     druidNode = new DruidNode(
         "test",
