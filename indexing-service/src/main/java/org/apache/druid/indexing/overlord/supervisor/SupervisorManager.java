ef17672541349418eb757de346219bb395dc73b4
Andrew Ho <andrewho135@gmail.com>
2026-03-20 14:55:57 -0700

    javadoc update

--- indexing-service/src/main/java/org/apache/druid/indexing/overlord/supervisor/SupervisorManager.java
+++ indexing-service/src/main/java/org/apache/druid/indexing/overlord/supervisor/SupervisorManager.java
@@ -374,6 +374,7 @@ public class SupervisorManager
    * - Supervisor must be RUNNING - needs active stream connection
    * - Supervisor must be a SeekableStreamSupervisor
    * - useEarliestOffset must be false (otherwise supervisor always starts from earliest)
+   * - useConcurrentLocks must be true
    * @param id supervisor ID
    * @return Map containing supervisorId and skipped offset ranges
    * @throws IllegalArgumentException if supervisor doesn't exist or if useEarliestOffset is true
