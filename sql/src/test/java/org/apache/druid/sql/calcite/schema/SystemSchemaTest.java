0bc9e25ea3ad8839ba27e3d8bfc298393afc6841
razinbouzar <razinbouzar@gmail.com>
2026-03-21 13:34:09 -0400

    spellcheck doesn't like jar.
    spellcheck doesn't like jar. replacing it with binary to be consistent.
    
    also complained about build_revision

--- sql/src/test/java/org/apache/druid/sql/calcite/schema/SystemSchemaTest.java
+++ sql/src/test/java/org/apache/druid/sql/calcite/schema/SystemSchemaTest.java
@@ -588,7 +588,7 @@ public class SystemSchemaTest extends CalciteTestBase
     final SystemSchema.ServersTable serversTable = (SystemSchema.ServersTable) schema.getTableMap().get("servers");
     final RelDataType serverRowType = serversTable.getRowType(new JavaTypeFactoryImpl());
     final List<RelDataTypeField> serverFields = serverRowType.getFieldList();
-    Assert.assertEquals(15, serverFields.size());
+    Assert.assertEquals(16, serverFields.size());
     Assert.assertEquals("server", serverFields.get(0).getName());
     Assert.assertEquals(SqlTypeName.VARCHAR, serverFields.get(0).getType().getSqlTypeName());
 
