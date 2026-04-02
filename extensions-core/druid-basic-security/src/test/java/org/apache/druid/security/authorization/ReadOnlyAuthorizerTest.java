c8a1267d5aa5553ecec10a1ce6d46dc078541f63
aho135 <andrewho135@gmail.com>
2026-04-02 15:39:36 -0700

    feat: Add an authorizer for read only operations (#19243)
    * Add an authorizer for read only operations
    
    * Checkstyle fixes
    
    * Remove use of deprecated methods
    
    * Register ReadOnlyAuthorizer
    
    * Migrate ReadOnlyAuthorizer from druid-basic-security to druid-server
    
    * Document ReadOnly authorizer
    
    * Spell check
    
    * Hook up ReadOnlyAuthorizer with Policy
    
    * Move ReadOnlyAuthorizer back to druid-basic-security
    
    * Example configuration for basic security with readonly
    
    * Change to readOnly for consistency
    
    * Fix configuration example
    
    * Update druid-basic-security.md

---
+++ extensions-core/druid-basic-security/src/test/java/org/apache/druid/security/authorization/ReadOnlyAuthorizerTest.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.security.authorization;
+
+import org.apache.druid.query.filter.NullFilter;
+import org.apache.druid.query.policy.RowFilterPolicy;
+import org.apache.druid.security.basic.authorization.ReadOnlyAuthorizer;
+import org.apache.druid.server.security.Access;
+import org.apache.druid.server.security.Action;
+import org.apache.druid.server.security.AuthenticationResult;
+import org.apache.druid.server.security.Resource;
+import org.apache.druid.server.security.ResourceType;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReadOnlyAuthorizerTest
+{
+
+  @Test
+  public void testAuth()
+  {
+    ReadOnlyAuthorizer authorizer = new ReadOnlyAuthorizer(null);
+    AuthenticationResult authenticationResult = new AuthenticationResult("anonymous", "anonymous", null, null);
+    Access access = authorizer.authorize(
+            authenticationResult,
+            new Resource("testResource", ResourceType.DATASOURCE),
+            Action.WRITE
+    );
+    Assert.assertFalse(access.isAllowed());
+    access = authorizer.authorize(
+            authenticationResult,
+            new Resource("testResource", ResourceType.DATASOURCE),
+            Action.READ
+    );
+    Assert.assertTrue(access.isAllowed());
+  }
+
+  @Test
+  public void testAuthWithPolicy()
+  {
+    RowFilterPolicy policy = RowFilterPolicy.from(new NullFilter("column", null));
+    ReadOnlyAuthorizer authorizerWithPolicy = new ReadOnlyAuthorizer(policy);
+    AuthenticationResult authenticationResult = new AuthenticationResult("anonymous", "anonymous", null, null);
+
+    // READ on DATASOURCE should return policy restriction
+    Access access = authorizerWithPolicy.authorize(
+            authenticationResult,
+            new Resource("testResource", ResourceType.DATASOURCE),
+            Action.READ
+    );
+    Assert.assertTrue(access.isAllowed());
+    Assert.assertTrue(access.getPolicy().isPresent());
+    Assert.assertEquals(policy, access.getPolicy().get());
+
+    // WRITE should still be denied
+    access = authorizerWithPolicy.authorize(
+            authenticationResult,
+            new Resource("testResource", ResourceType.DATASOURCE),
+            Action.WRITE
+    );
+    Assert.assertFalse(access.isAllowed());
+
+    // READ on non-DATASOURCE should not have policy
+    access = authorizerWithPolicy.authorize(
+            authenticationResult,
+            new Resource("testResource", ResourceType.STATE),
+            Action.READ
+    );
+    Assert.assertTrue(access.isAllowed());
+    Assert.assertFalse(access.getPolicy().isPresent());
+  }
+}
