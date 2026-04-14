f673b00a949d254afe73e21b98805f28ce9bdbb7
cecemei <yingqian.mei@gmail.com>
2026-04-14 10:49:21 -0700

    export

--- extensions-core/s3-extensions/src/main/java/org/apache/druid/data/input/s3/S3InputSource.java
+++ extensions-core/s3-extensions/src/main/java/org/apache/druid/data/input/s3/S3InputSource.java
@@ -41,37 +41,21 @@ import org.apache.druid.data.input.impl.SplittableInputSource;
 import org.apache.druid.data.input.impl.systemfield.SystemField;
 import org.apache.druid.data.input.impl.systemfield.SystemFields;
 import org.apache.druid.java.util.common.RetryUtils;
-import org.apache.druid.java.util.common.StringUtils;
 import org.apache.druid.storage.s3.S3InputDataConfig;
 import org.apache.druid.storage.s3.S3StorageDruidModule;
 import org.apache.druid.storage.s3.S3Utils;
 import org.apache.druid.storage.s3.ServerSideEncryptingAmazonS3;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.http.apache.ApacheHttpClient;
-import software.amazon.awssdk.http.apache.ProxyConfiguration;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.S3ClientBuilder;
-import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
-import software.amazon.awssdk.services.sts.StsClient;
-import software.amazon.awssdk.services.sts.StsClientBuilder;
-import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider;
-import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.net.URI;
-import java.time.Duration;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.UUID;
 
 public class S3InputSource extends CloudObjectInputSource
 {
@@ -134,72 +118,20 @@ public class S3InputSource extends CloudObjectInputSource
     this.awsClientConfig = awsClientConfig;
     this.awsEndpointConfig = awsEndpointConfig;
 
-    this.s3ClientSupplier = Suppliers.memoize(
-        () -> {
-          if (s3ClientBuilder != null && s3InputSourceConfig != null) {
-            // Build a custom S3Client with the provided configuration
-            S3ClientBuilder customBuilder = S3Client.builder();
-
-            // Configure endpoint and region
-            if (awsEndpointConfig != null && awsEndpointConfig.getUrl() != null) {
-              String endpointUrl = awsEndpointConfig.getUrl();
-              // Ensure endpoint URL has a scheme
-              if (!endpointUrl.startsWith("http://") && !endpointUrl.startsWith("https://")) {
-                boolean useHttps = S3Utils.useHttps(awsClientConfig, awsEndpointConfig);
-                endpointUrl = S3Utils.ensureEndpointHasScheme(endpointUrl, useHttps);
-              }
-              customBuilder.endpointOverride(URI.create(endpointUrl));
-              if (awsEndpointConfig.getSigningRegion() != null) {
-                customBuilder.region(Region.of(awsEndpointConfig.getSigningRegion()));
-              }
-            }
-
-            // Configure S3-specific settings
-            if (awsClientConfig != null) {
-              S3Configuration.Builder s3ConfigBuilder = S3Configuration.builder()
-                  .pathStyleAccessEnabled(awsClientConfig.isEnablePathStyleAccess())
-                  .chunkedEncodingEnabled(!awsClientConfig.isDisableChunkedEncoding());
-              customBuilder.serviceConfiguration(s3ConfigBuilder.build());
-              customBuilder.crossRegionAccessEnabled(awsClientConfig.isCrossRegionAccessEnabled());
-            }
-
-            // Configure HTTP client with proxy if needed
-            ApacheHttpClient.Builder httpClientBuilder = ApacheHttpClient.builder();
-            if (awsProxyConfig != null && awsProxyConfig.getHost() != null) {
-              ProxyConfiguration proxyConfig = S3Utils.buildProxyConfiguration(awsProxyConfig);
-              if (proxyConfig != null) {
-                httpClientBuilder.proxyConfiguration(proxyConfig);
-              }
-            }
-            customBuilder.httpClientBuilder(httpClientBuilder);
-
-            // Configure credentials
-            AwsCredentialsProvider credentialsProvider;
-            if (s3InputSourceConfig.isCredentialsConfigured()) {
-              credentialsProvider = createStaticCredentialsProvider(s3InputSourceConfig);
-            } else {
-              credentialsProvider = awsCredentialsProvider;
-            }
-
-            // Apply assume role if configured
-            if (s3InputSourceConfig.getAssumeRoleArn() != null) {
-              credentialsProvider = createAssumeRoleCredentialsProvider(
-                  s3InputSourceConfig,
-                  credentialsProvider
-              );
-            }
-
-            customBuilder.credentialsProvider(credentialsProvider);
-
-            // Build and wrap in ServerSideEncryptingAmazonS3
-            return s3ClientBuilder
-                .setS3ClientSupplier(customBuilder::build)
-                .build();
-          } else {
-            return s3Client;
-          }
-        }
-    );
+    this.s3ClientSupplier = Suppliers.memoize(() -> {
+      if (s3ClientBuilder == null || s3InputSourceConfig == null) {
+        return s3Client;
+      }
+      return ServerSideEncryptingAmazonS3.builder(
+          awsCredentialsProvider,
+          null,
+          awsProxyConfig,
+          awsEndpointConfig,
+          awsClientConfig,
+          s3InputSourceConfig,
+          null
+      ).build();
+    });
     this.maxRetries = RetryUtils.DEFAULT_MAX_TRIES;
   }
 
@@ -276,61 +208,6 @@ public class S3InputSource extends CloudObjectInputSource
   public Set<String> getTypes()
   {
     return Collections.singleton(TYPE_KEY);
-  }
-
-  private AwsCredentialsProvider createAssumeRoleCredentialsProvider(
-      S3InputSourceConfig s3InputSourceConfig,
-      AwsCredentialsProvider baseCredentialsProvider
-  )
-  {
-    String assumeRoleArn = s3InputSourceConfig.getAssumeRoleArn();
-    String roleSessionName = StringUtils.format("druid-s3-input-source-%s", UUID.randomUUID().toString());
-
-    StsClientBuilder stsBuilder = StsClient.builder()
-        .credentialsProvider(baseCredentialsProvider);
-
-    // If we have endpoint config, use its region for STS too
-    if (awsEndpointConfig != null && awsEndpointConfig.getSigningRegion() != null) {
-      stsBuilder.region(Region.of(awsEndpointConfig.getSigningRegion()));
-    }
-
-    StsClient stsClient = stsBuilder.build();
-
-    AssumeRoleRequest.Builder assumeRoleRequestBuilder = AssumeRoleRequest.builder()
-        .roleArn(assumeRoleArn)
-        .roleSessionName(roleSessionName)
-        .durationSeconds(3600);
-
-    if (s3InputSourceConfig.getAssumeRoleExternalId() != null) {
-      assumeRoleRequestBuilder.externalId(s3InputSourceConfig.getAssumeRoleExternalId());
-    }
-
-    return StsAssumeRoleCredentialsProvider.builder()
-        .stsClient(stsClient)
-        .refreshRequest(assumeRoleRequestBuilder.build())
-        .asyncCredentialUpdateEnabled(true)
-        .staleTime(Duration.ofMinutes(3))
-        .build();
-  }
-
-  @Nonnull
-  private StaticCredentialsProvider createStaticCredentialsProvider(S3InputSourceConfig s3InputSourceConfig)
-  {
-    if (s3InputSourceConfig.getSessionToken() != null) {
-      AwsSessionCredentials sessionCredentials = AwsSessionCredentials.create(
-          s3InputSourceConfig.getAccessKeyId().getPassword(),
-          s3InputSourceConfig.getSecretAccessKey().getPassword(),
-          s3InputSourceConfig.getSessionToken().getPassword()
-      );
-      return StaticCredentialsProvider.create(sessionCredentials);
-    } else {
-      return StaticCredentialsProvider.create(
-          AwsBasicCredentials.create(
-              s3InputSourceConfig.getAccessKeyId().getPassword(),
-              s3InputSourceConfig.getSecretAccessKey().getPassword()
-          )
-      );
-    }
   }
 
   @Nullable
