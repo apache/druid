a9b924aa2218f6d88bbcd10d04c4308bdc502a4d
Clint Wylie <cwylie@apache.org>
2026-04-27 01:20:38 -0700

    got a bit carried away

---
+++ server/src/test/java/org/apache/druid/client/CoordinatorServerViewTest.java
@@ -0,0 +1,472 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.client;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import org.apache.druid.java.util.common.Intervals;
+import org.apache.druid.java.util.common.Pair;
+import org.apache.druid.java.util.common.concurrent.Execs;
+import org.apache.druid.query.TableDataSource;
+import org.apache.druid.segment.realtime.appenderator.SegmentSchemas;
+import org.apache.druid.server.coordination.DruidServerMetadata;
+import org.apache.druid.server.coordination.ServerType;
+import org.apache.druid.server.metrics.NoopServiceEmitter;
+import org.apache.druid.timeline.DataSegment;
+import org.apache.druid.timeline.TimelineLookup;
+import org.apache.druid.timeline.TimelineObjectHolder;
+import org.apache.druid.timeline.partition.NoneShardSpec;
+import org.apache.druid.timeline.partition.PartitionHolder;
+import org.easymock.EasyMock;
+import org.joda.time.Interval;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@RunWith(Parameterized.class)
+public class CoordinatorServerViewTest
+{
+  private static final long AWAIT_SECONDS = 10;
+
+  private CountDownLatch segmentViewInitLatch;
+  private CountDownLatch segmentAddedLatch;
+  private CountDownLatch segmentRemovedLatch;
+
+  private CountDownLatch callbackSegmentViewInitLatch;
+  private CountDownLatch callbackSegmentAddedLatch;
+  private CountDownLatch callbackSegmentRemovedLatch;
+  private CountDownLatch callbackServerSegmentRemovedLatch;
+
+  private TestServerInventoryView baseView;
+  private CoordinatorServerView coordinatorServerView;
+  private ExecutorService callbackExec;
+
+  private boolean setDruidClientFactory;
+
+  @Parameterized.Parameters
+  public static Object[] data()
+  {
+    return new Object[]{true, false};
+  }
+
+  public CoordinatorServerViewTest(boolean setDruidClientFactory)
+  {
+    this.setDruidClientFactory = setDruidClientFactory;
+  }
+
+  @Before
+  public void setUp()
+  {
+    callbackExec = Execs.singleThreaded("CoordinatorServerViewTest-%s");
+  }
+
+  @Test
+  public void testSingleServerAddedRemovedSegment() throws Exception
+  {
+    segmentViewInitLatch = new CountDownLatch(1);
+    segmentAddedLatch = new CountDownLatch(1);
+    segmentRemovedLatch = new CountDownLatch(1);
+    callbackSegmentViewInitLatch = new CountDownLatch(1);
+    callbackSegmentAddedLatch = new CountDownLatch(1);
+    callbackServerSegmentRemovedLatch = new CountDownLatch(1);
+    callbackSegmentRemovedLatch = new CountDownLatch(1);
+
+    setupViews(setDruidClientFactory);
+
+    final DruidServer druidServer = new DruidServer(
+        "localhost:1234",
+        "localhost:1234",
+        null,
+        10000000L,
+        null,
+        ServerType.HISTORICAL,
+        "default_tier",
+        0
+    );
+
+    baseView.addServer(druidServer);
+    baseView.markInventoryInitialized();
+
+    final DataSegment segment = dataSegmentWithIntervalAndVersion("2014-10-20T00:00:00Z/P1D", "v1");
+    final int partition = segment.getShardSpec().getPartitionNum();
+    final Interval intervals = Intervals.of("2014-10-20T00:00:00Z/P1D");
+    baseView.addSegment(druidServer, segment);
+    Assert.assertTrue(awaitLatch(segmentViewInitLatch));
+    Assert.assertTrue(awaitLatch(segmentAddedLatch));
+
+    Assert.assertTrue(awaitLatch(callbackSegmentViewInitLatch));
+    Assert.assertTrue(awaitLatch(callbackSegmentAddedLatch));
+
+    if (setDruidClientFactory) {
+      Assert.assertNotNull(coordinatorServerView.getQueryRunner(druidServer.getName()));
+    } else {
+      Assert.assertNull(coordinatorServerView.getQueryRunner(druidServer.getName()));
+    }
+
+    TimelineLookup timeline = coordinatorServerView.getTimeline(new TableDataSource("test_overlord_server_view"));
+    List<TimelineObjectHolder> serverLookupRes = (List<TimelineObjectHolder>) timeline.lookup(
+        intervals
+    );
+    Assert.assertEquals(1, serverLookupRes.size());
+
+    TimelineObjectHolder<String, SegmentLoadInfo> actualTimelineObjectHolder = serverLookupRes.get(0);
+    Assert.assertEquals(intervals, actualTimelineObjectHolder.getInterval());
+    Assert.assertEquals("v1", actualTimelineObjectHolder.getVersion());
+
+    PartitionHolder<SegmentLoadInfo> actualPartitionHolder = actualTimelineObjectHolder.getObject();
+    Assert.assertTrue(actualPartitionHolder.isComplete());
+    Assert.assertEquals(1, Iterables.size(actualPartitionHolder));
+
+    SegmentLoadInfo segmentLoadInfo = actualPartitionHolder.iterator().next().getObject();
+    Assert.assertFalse(segmentLoadInfo.isEmpty());
+    Assert.assertEquals(
+        druidServer.getMetadata(),
+        Iterables.getOnlyElement(segmentLoadInfo.toImmutableSegmentLoadInfo().getServers())
+    );
+    Assert.assertNotNull(timeline.findChunk(intervals, "v1", partition));
+
+    baseView.removeSegment(druidServer, segment);
+    Assert.assertTrue(awaitLatch(segmentRemovedLatch));
+    Assert.assertTrue(awaitLatch(callbackServerSegmentRemovedLatch));
+    Assert.assertTrue(awaitLatch(callbackSegmentRemovedLatch));
+
+    Assert.assertEquals(
+        0,
+        ((List<TimelineObjectHolder>) timeline.lookup(Intervals.of("2014-10-20T00:00:00Z/P1D"))).size()
+    );
+    Assert.assertNull(timeline.findChunk(intervals, "v1", partition));
+  }
+
+  @Test
+  public void testMultipleServerAddedRemovedSegment() throws Exception
+  {
+    segmentViewInitLatch = new CountDownLatch(1);
+    segmentAddedLatch = new CountDownLatch(5);
+
+    // temporarily set latch count to 1
+    segmentRemovedLatch = new CountDownLatch(1);
+
+    callbackSegmentViewInitLatch = new CountDownLatch(1);
+    callbackSegmentAddedLatch = new CountDownLatch(5);
+    callbackServerSegmentRemovedLatch = new CountDownLatch(1);
+    callbackSegmentRemovedLatch = new CountDownLatch(1);
+
+    setupViews(setDruidClientFactory);
+
+    final List<DruidServer> druidServers = Lists.transform(
+        ImmutableList.of("localhost:0", "localhost:1", "localhost:2", "localhost:3", "localhost:4"),
+        input -> new DruidServer(
+            input,
+            input,
+            null,
+            10000000L,
+            null,
+            ServerType.HISTORICAL,
+            "default_tier",
+            0
+        )
+    );
+
+    for (DruidServer druidServer : druidServers) {
+      baseView.addServer(druidServer);
+    }
+    baseView.markInventoryInitialized();
+
+    final List<DataSegment> segments = Lists.transform(
+        ImmutableList.of(
+            Pair.of("2011-04-01/2011-04-03", "v1"),
+            Pair.of("2011-04-03/2011-04-06", "v1"),
+            Pair.of("2011-04-01/2011-04-09", "v2"),
+            Pair.of("2011-04-06/2011-04-09", "v3"),
+            Pair.of("2011-04-01/2011-04-02", "v3")
+        ),
+        input -> dataSegmentWithIntervalAndVersion(input.lhs, input.rhs)
+    );
+
+    for (int i = 0; i < 5; ++i) {
+      baseView.addSegment(druidServers.get(i), segments.get(i));
+    }
+    Assert.assertTrue(awaitLatch(segmentViewInitLatch));
+    Assert.assertTrue(awaitLatch(segmentAddedLatch));
+    Assert.assertTrue(awaitLatch(callbackSegmentViewInitLatch));
+    Assert.assertTrue(awaitLatch(callbackSegmentAddedLatch));
+
+    for (int i = 0; i < 5; ++i) {
+      if (setDruidClientFactory) {
+        Assert.assertNotNull(coordinatorServerView.getQueryRunner(druidServers.get(i).getName()));
+      } else {
+        Assert.assertNull(coordinatorServerView.getQueryRunner(druidServers.get(i).getName()));
+      }
+    }
+
+    TimelineLookup timeline = coordinatorServerView.getTimeline(new TableDataSource("test_overlord_server_view"));
+    assertValues(
+        Arrays.asList(
+            createExpected("2011-04-01/2011-04-02", "v3", druidServers.get(4), segments.get(4)),
+            createExpected("2011-04-02/2011-04-06", "v2", druidServers.get(2), segments.get(2)),
+            createExpected("2011-04-06/2011-04-09", "v3", druidServers.get(3), segments.get(3))
+        ),
+        (List<TimelineObjectHolder>) timeline.lookup(
+            Intervals.of(
+                "2011-04-01/2011-04-09"
+            )
+        )
+    );
+
+    // unannounce the segment created by dataSegmentWithIntervalAndVersion("2011-04-01/2011-04-09", "v2")
+    baseView.removeSegment(druidServers.get(2), segments.get(2));
+    Assert.assertTrue(awaitLatch(segmentRemovedLatch));
+    Assert.assertTrue(awaitLatch(callbackSegmentRemovedLatch));
+    Assert.assertTrue(awaitLatch(callbackServerSegmentRemovedLatch));
+
+    // renew segmentRemovedLatch since we still have 4 segments to unannounce
+    segmentRemovedLatch = new CountDownLatch(4);
+    callbackServerSegmentRemovedLatch = new CountDownLatch(4);
+    callbackSegmentRemovedLatch = new CountDownLatch(4);
+
+    timeline = coordinatorServerView.getTimeline(new TableDataSource("test_overlord_server_view"));
+    assertValues(
+        Arrays.asList(
+            createExpected("2011-04-01/2011-04-02", "v3", druidServers.get(4), segments.get(4)),
+            createExpected("2011-04-02/2011-04-03", "v1", druidServers.get(0), segments.get(0)),
+            createExpected("2011-04-03/2011-04-06", "v1", druidServers.get(1), segments.get(1)),
+            createExpected("2011-04-06/2011-04-09", "v3", druidServers.get(3), segments.get(3))
+        ),
+        (List<TimelineObjectHolder>) timeline.lookup(Intervals.of("2011-04-01/2011-04-09"))
+    );
+
+    // unannounce all the segments
+    for (int i = 0; i < 5; ++i) {
+      // skip the one that was previously unannounced
+      if (i != 2) {
+        baseView.removeSegment(druidServers.get(i), segments.get(i));
+      }
+    }
+    Assert.assertTrue(awaitLatch(segmentRemovedLatch));
+    Assert.assertTrue(awaitLatch(callbackSegmentRemovedLatch));
+    Assert.assertTrue(awaitLatch(callbackServerSegmentRemovedLatch));
+
+    Assert.assertEquals(
+        0,
+        ((List<TimelineObjectHolder>) timeline.lookup(Intervals.of("2011-04-01/2011-04-09"))).size()
+    );
+  }
+
+  private boolean awaitLatch(CountDownLatch latch) throws InterruptedException
+  {
+    return latch.await(AWAIT_SECONDS, TimeUnit.SECONDS);
+  }
+
+  private Pair<Interval, Pair<String, Pair<DruidServer, DataSegment>>> createExpected(
+      String intervalStr,
+      String version,
+      DruidServer druidServer,
+      DataSegment segment
+  )
+  {
+    return Pair.of(Intervals.of(intervalStr), Pair.of(version, Pair.of(druidServer, segment)));
+  }
+
+  private void assertValues(
+      List<Pair<Interval, Pair<String, Pair<DruidServer, DataSegment>>>> expected, List<TimelineObjectHolder> actual
+  )
+  {
+    Assert.assertEquals(expected.size(), actual.size());
+
+    for (int i = 0; i < expected.size(); ++i) {
+      Pair<Interval, Pair<String, Pair<DruidServer, DataSegment>>> expectedPair = expected.get(i);
+      TimelineObjectHolder<String, SegmentLoadInfo> actualTimelineObjectHolder = actual.get(i);
+
+      Assert.assertEquals(expectedPair.lhs, actualTimelineObjectHolder.getInterval());
+      Assert.assertEquals(expectedPair.rhs.lhs, actualTimelineObjectHolder.getVersion());
+
+      PartitionHolder<SegmentLoadInfo> actualPartitionHolder = actualTimelineObjectHolder.getObject();
+      Assert.assertTrue(actualPartitionHolder.isComplete());
+      Assert.assertEquals(1, Iterables.size(actualPartitionHolder));
+
+      SegmentLoadInfo segmentLoadInfo = actualPartitionHolder.iterator().next().getObject();
+      Assert.assertFalse(segmentLoadInfo.isEmpty());
+      Assert.assertEquals(expectedPair.rhs.rhs.lhs.getMetadata(),
+                          Iterables.getOnlyElement(segmentLoadInfo.toImmutableSegmentLoadInfo().getServers()));
+      Assert.assertEquals(expectedPair.rhs.rhs.lhs.getMetadata(), segmentLoadInfo.pickOne());
+    }
+  }
+
+  private void setupViews(boolean setDruidClientFactory) throws InterruptedException
+  {
+    baseView = new TestServerInventoryView()
+    {
+      @Override
+      public void registerSegmentCallback(Executor exec, final SegmentCallback callback)
+      {
+        super.registerSegmentCallback(
+            exec,
+            new SegmentCallback()
+            {
+              @Override
+              public CallbackAction segmentAdded(DruidServerMetadata server, DataSegment segment)
+              {
+                CallbackAction res = callback.segmentAdded(server, segment);
+                segmentAddedLatch.countDown();
+                return res;
+              }
+
+              @Override
+              public CallbackAction segmentRemoved(DruidServerMetadata server, DataSegment segment)
+              {
+                CallbackAction res = callback.segmentRemoved(server, segment);
+                segmentRemovedLatch.countDown();
+                return res;
+              }
+
+              @Override
+              public CallbackAction segmentViewInitialized()
+              {
+                CallbackAction res = callback.segmentViewInitialized();
+                segmentViewInitLatch.countDown();
+                return res;
+              }
+
+              @Override
+              public CallbackAction segmentSchemasAnnounced(SegmentSchemas segmentSchemas)
+              {
+                return CallbackAction.CONTINUE;
+              }
+            }
+        );
+      }
+    };
+
+    DirectDruidClientFactory druidClientFactory = null;
+
+    if (setDruidClientFactory) {
+      druidClientFactory = EasyMock.createMock(DirectDruidClientFactory.class);
+      DirectDruidClient directDruidClient = EasyMock.mock(DirectDruidClient.class);
+      EasyMock.expect(druidClientFactory.makeDirectClient(EasyMock.anyObject(DruidServer.class)))
+              .andReturn(directDruidClient)
+              .anyTimes();
+
+      EasyMock.replay(druidClientFactory);
+    }
+
+    coordinatorServerView = new CoordinatorServerView(
+        baseView,
+        new CoordinatorSegmentWatcherConfig(),
+        new NoopServiceEmitter(),
+        druidClientFactory
+    );
+
+    baseView.start();
+    baseView.markInventoryInitialized();
+    initServerViewTimelineCallback(coordinatorServerView);
+    coordinatorServerView.start();
+  }
+
+  private void initServerViewTimelineCallback(final CoordinatorServerView serverView)
+  {
+    serverView.registerTimelineCallback(
+        callbackExec,
+        new TimelineServerView.TimelineCallback()
+        {
+          @Override
+          public ServerView.CallbackAction timelineInitialized()
+          {
+            callbackSegmentViewInitLatch.countDown();
+            return ServerView.CallbackAction.CONTINUE;
+          }
+
+          @Override
+          public ServerView.CallbackAction segmentAdded(final DruidServerMetadata server, final DataSegment segment)
+          {
+            callbackSegmentAddedLatch.countDown();
+            return ServerView.CallbackAction.CONTINUE;
+          }
+
+          @Override
+          public ServerView.CallbackAction segmentRemoved(final DataSegment segment)
+          {
+            callbackSegmentRemovedLatch.countDown();
+            return ServerView.CallbackAction.CONTINUE;
+          }
+
+          @Override
+          public ServerView.CallbackAction serverSegmentRemoved(
+              final DruidServerMetadata server,
+              final DataSegment segment
+          )
+          {
+            callbackServerSegmentRemovedLatch.countDown();
+            return ServerView.CallbackAction.CONTINUE;
+          }
+
+          @Override
+          public ServerView.CallbackAction segmentSchemasAnnounced(SegmentSchemas segmentSchemas)
+          {
+            return ServerView.CallbackAction.CONTINUE;
+          }
+        }
+    );
+  }
+
+  private DataSegment dataSegmentWithIntervalAndVersion(String intervalStr, String version)
+  {
+    return DataSegment.builder()
+                      .dataSource("test_overlord_server_view")
+                      .interval(Intervals.of(intervalStr))
+                      .loadSpec(
+                          ImmutableMap.of(
+                              "type",
+                              "local",
+                              "path",
+                              "somewhere"
+                          )
+                      )
+                      .version(version)
+                      .dimensions(ImmutableList.of())
+                      .metrics(ImmutableList.of())
+                      .shardSpec(NoneShardSpec.instance())
+                      .binaryVersion(9)
+                      .size(0)
+                      .build();
+  }
+
+  @After
+  public void tearDown()
+  {
+    if (baseView != null) {
+      baseView.stop();
+    }
+    if (callbackExec != null) {
+      callbackExec.shutdownNow();
+    }
+  }
+}
--- server/src/test/java/org/apache/druid/server/coordination/coordination/BatchDataSegmentAnnouncerTest.java
+++ server/src/test/java/org/apache/druid/server/coordination/coordination/BatchDataSegmentAnnouncerTest.java
@@ -20,30 +20,60 @@
 package org.apache.druid.server.coordination.coordination;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import org.apache.druid.java.util.common.DateTimes;
+import org.apache.druid.java.util.common.concurrent.Execs;
+import org.apache.druid.segment.column.ColumnType;
+import org.apache.druid.segment.realtime.appenderator.SegmentSchemas;
 import org.apache.druid.server.coordination.BatchDataSegmentAnnouncer;
 import org.apache.druid.server.coordination.ChangeRequestHistory;
 import org.apache.druid.server.coordination.ChangeRequestsSnapshot;
 import org.apache.druid.server.coordination.DataSegmentChangeRequest;
 import org.apache.druid.server.coordination.SegmentChangeRequestDrop;
 import org.apache.druid.server.coordination.SegmentChangeRequestLoad;
+import org.apache.druid.server.coordination.SegmentSchemasChangeRequest;
 import org.apache.druid.timeline.DataSegment;
+import org.apache.druid.timeline.SegmentId;
 import org.apache.druid.timeline.partition.NoneShardSpec;
 import org.joda.time.Interval;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 public class BatchDataSegmentAnnouncerTest
 {
+  private static final int NUM_THREADS = 4;
+
   private BatchDataSegmentAnnouncer announcer;
+  private Set<DataSegment> testSegments;
+  private ExecutorService exec;
 
   @Before
   public void setUp()
   {
     announcer = new BatchDataSegmentAnnouncer();
+    testSegments = new HashSet<>();
+    for (int i = 0; i < 100; i++) {
+      testSegments.add(makeSegment(i));
+    }
+    exec = Execs.multiThreaded(NUM_THREADS, "BatchDataSegmentAnnouncerTest-%d");
+  }
+
+  @After
+  public void tearDown()
+  {
+    announcer.stop();
+    exec.shutdownNow();
   }
 
   @Test
@@ -56,10 +86,10 @@ public class BatchDataSegmentAnnouncerTest
     announcer.announceSegment(segmentB);
     announcer.unannounceSegment(segmentA);
 
+    // When the counter is negative, getSegmentChangesSince returns the current snapshot of
+    // announced segments, which at this point is just segmentB.
     ChangeRequestsSnapshot<DataSegmentChangeRequest> snapshot =
         announcer.getSegmentChangesSince(new ChangeRequestHistory.Counter(-1, -1)).get();
-    // When counter is negative, getSegmentChangesSince returns the current snapshot of announced segments, which
-    // at this point is just segmentB.
     Assert.assertEquals(1, snapshot.getRequests().size());
     Assert.assertTrue(snapshot.getRequests().get(0) instanceof SegmentChangeRequestLoad);
   }
@@ -107,18 +137,160 @@ public class BatchDataSegmentAnnouncerTest
     Assert.assertEquals(1, snapshot.getRequests().size());
   }
 
-  private DataSegment makeSegment(int offset)
+  @Test
+  public void testSchemaAnnounce() throws Exception
   {
-    return new DataSegment(
-        "test",
-        new Interval(DateTimes.utc(0), DateTimes.utc(offset + 1)),
-        "v1",
-        null,
-        ImmutableList.of("dim"),
-        ImmutableList.of("met"),
-        NoneShardSpec.instance(),
-        0,
-        1
+    String dataSource = "foo";
+    String segmentId = "id";
+    String taskId = "t1";
+    SegmentSchemas.SegmentSchema absoluteSchema1 =
+        new SegmentSchemas.SegmentSchema(
+            dataSource,
+            segmentId,
+            false,
+            20,
+            ImmutableList.of("dim1", "dim2"),
+            Collections.emptyList(),
+            ImmutableMap.of("dim1", ColumnType.STRING, "dim2", ColumnType.STRING)
+        );
+
+
+    SegmentSchemas.SegmentSchema absoluteSchema2 =
+        new SegmentSchemas.SegmentSchema(
+            dataSource,
+            segmentId,
+            false,
+            40,
+            ImmutableList.of("dim1", "dim2", "dim3"),
+            ImmutableList.of(),
+            ImmutableMap.of("dim1", ColumnType.UNKNOWN_COMPLEX, "dim2", ColumnType.STRING, "dim3", ColumnType.STRING)
+        );
+
+    SegmentSchemas.SegmentSchema deltaSchema =
+        new SegmentSchemas.SegmentSchema(
+            dataSource,
+            segmentId,
+            true,
+            40,
+            ImmutableList.of("dim3"),
+            ImmutableList.of("dim1"),
+            ImmutableMap.of("dim1", ColumnType.UNKNOWN_COMPLEX, "dim3", ColumnType.STRING)
+        );
+
+    announcer.announceSegmentSchemas(
+        taskId,
+        new SegmentSchemas(Collections.singletonList(absoluteSchema1)),
+        new SegmentSchemas(Collections.singletonList(absoluteSchema1))
     );
+
+    ChangeRequestsSnapshot<DataSegmentChangeRequest> snapshot;
+
+    snapshot = announcer.getSegmentChangesSince(
+        new ChangeRequestHistory.Counter(-1, -1)
+    ).get();
+    Assert.assertEquals(1, snapshot.getRequests().size());
+    Assert.assertEquals(1, snapshot.getCounter().getCounter());
+
+    Assert.assertEquals(
+        absoluteSchema1,
+        ((SegmentSchemasChangeRequest) snapshot.getRequests().get(0))
+            .getSegmentSchemas()
+            .getSegmentSchemaList()
+            .get(0)
+    );
+    announcer.announceSegmentSchemas(
+        taskId,
+        new SegmentSchemas(Collections.singletonList(absoluteSchema2)),
+        new SegmentSchemas(Collections.singletonList(deltaSchema))
+    );
+
+    snapshot = announcer.getSegmentChangesSince(snapshot.getCounter()).get();
+
+    Assert.assertEquals(
+        deltaSchema,
+        ((SegmentSchemasChangeRequest) snapshot.getRequests().get(0))
+            .getSegmentSchemas()
+            .getSegmentSchemaList()
+            .get(0)
+    );
+    Assert.assertEquals(1, snapshot.getRequests().size());
+    Assert.assertEquals(2, snapshot.getCounter().getCounter());
+
+    snapshot = announcer.getSegmentChangesSince(
+        new ChangeRequestHistory.Counter(-1, -1)
+    ).get();
+    Assert.assertEquals(
+        absoluteSchema2,
+        ((SegmentSchemasChangeRequest) snapshot.getRequests().get(0))
+            .getSegmentSchemas()
+            .getSegmentSchemaList()
+            .get(0)
+    );
+    Assert.assertEquals(1, snapshot.getRequests().size());
+    Assert.assertEquals(2, snapshot.getCounter().getCounter());
+  }
+
+  @Test(timeout = 5000L)
+  public void testAnnounceSegmentsWithSameSegmentConcurrently() throws ExecutionException, InterruptedException
+  {
+    final List<Future<?>> futures = new ArrayList<>(NUM_THREADS);
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+      futures.add(exec.submit(() -> announcer.announceSegments(testSegments)));
+    }
+
+    for (Future<?> future : futures) {
+      future.get();
+    }
+
+    ChangeRequestsSnapshot<DataSegmentChangeRequest> snapshot =
+        announcer.getSegmentChangesSince(ChangeRequestHistory.Counter.ZERO).get();
+    Assert.assertEquals(testSegments.size(), snapshot.getRequests().size());
+  }
+
+  @Test(timeout = 5000L)
+  public void testAnnounceSegmentWithSameSegmentConcurrently() throws ExecutionException, InterruptedException
+  {
+    final List<Future<?>> futures = new ArrayList<>(NUM_THREADS);
+
+    final DataSegment segment1 = makeSegment(0);
+    final DataSegment segment2 = makeSegment(1);
+    final DataSegment segment3 = makeSegment(2);
+    final DataSegment segment4 = makeSegment(3);
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+      futures.add(
+          exec.submit(() -> {
+            announcer.announceSegment(segment1);
+            announcer.announceSegment(segment2);
+            announcer.announceSegment(segment3);
+            announcer.announceSegment(segment4);
+          })
+      );
+    }
+
+    for (Future<?> future : futures) {
+      future.get();
+    }
+
+    ChangeRequestsSnapshot<DataSegmentChangeRequest> snapshot =
+        announcer.getSegmentChangesSince(ChangeRequestHistory.Counter.ZERO).get();
+    Assert.assertEquals(4, snapshot.getRequests().size());
+  }
+
+  private static DataSegment makeSegment(int offset)
+  {
+    Interval interval = new Interval(
+        DateTimes.of("2013-01-01").plusDays(offset),
+        DateTimes.of("2013-01-02").plusDays(offset)
+    );
+    SegmentId segmentId = SegmentId.of("foo", interval, DateTimes.nowUtc().toString(), null);
+    return DataSegment.builder(segmentId)
+                      .loadSpec(ImmutableMap.of("type", "local"))
+                      .dimensions(ImmutableList.of("dim1", "dim2"))
+                      .metrics(ImmutableList.of("met1", "met2"))
+                      .shardSpec(NoneShardSpec.instance())
+                      .size(0)
+                      .build();
   }
 }
