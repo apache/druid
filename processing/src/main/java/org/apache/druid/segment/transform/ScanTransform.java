88dda0a7288637b0371c2c26316856885c818d69
Abhishek Balaji Radhakrishnan <abhishek.rb19@gmail.com>
2026-04-26 22:25:29 -0700

    Checkstyle

--- processing/src/main/java/org/apache/druid/segment/transform/ScanTransform.java
+++ processing/src/main/java/org/apache/druid/segment/transform/ScanTransform.java
@@ -42,8 +42,8 @@ import org.apache.druid.segment.column.RowSignature;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
