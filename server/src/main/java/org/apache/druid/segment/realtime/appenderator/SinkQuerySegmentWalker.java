apache/druid

commit d5eb94d0e0d9fcfba9321c42aab75f99d6a258e7

Restore Sink Metric Emission Behaviour: Emit them per-Sink instead of per-FireHydrant (#17170)
* Emit aggregate segment processing metrics per sink instead of firehydrant

* add docs

* minor change

* checkstyle

* Fix DefaultQueryMetricsTest

* Minor changes in SinkMetricsEmittingQueryRunner

* spotbugs

* Address review comments

* Use ImmutableSet and ImmutableMap

* Create a helper class for saving state of StubServiceEmitter

* Add SinkQuerySegmentWalkerBenchmark

* Create SegmentMetrics class for tracking segment metrics

---------

Co-authored-by: Akshat Jain <akjn11@gmail.com>

server/src/main/java/org/apache/druid/segment/realtime/appenderator/SinkQuerySegmentWalker.java
@@ -22,6 +22,8 @@ package org.apache.druid.segment.realtime.appenderator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import org.apache.druid.client.CachingQueryRunner;
 import org.apache.druid.client.cache.Cache;
 import org.apache.druid.client.cache.CacheConfig;
@@ -30,18 +32,23 @@ import org.apache.druid.client.cache.ForegroundCachePopulator;
 import org.apache.druid.java.util.common.ISE;
 import org.apache.druid.java.util.common.Pair;
 import org.apache.druid.java.util.common.guava.FunctionalIterable;
+import org.apache.druid.java.util.common.guava.LazySequence;
+import org.apache.druid.java.util.common.guava.Sequence;
+import org.apache.druid.java.util.common.guava.SequenceWrapper;
+import org.apache.druid.java.util.common.guava.Sequences;
 import org.apache.druid.java.util.emitter.EmittingLogger;
 import org.apache.druid.java.util.emitter.service.ServiceEmitter;
 import org.apache.druid.query.BySegmentQueryRunner;
 import org.apache.druid.query.CPUTimeMetricQueryRunner;
 import org.apache.druid.query.DataSource;
+import org.apache.druid.query.DefaultQueryMetrics;
 import org.apache.druid.query.DirectQueryProcessingPool;
 import org.apache.druid.query.FinalizeResultsQueryRunner;
-import org.apache.druid.query.MetricsEmittingQueryRunner;
 import org.apache.druid.query.NoopQueryRunner;
 import org.apache.druid.query.Query;
 import org.apache.druid.query.QueryDataSource;
 import org.apache.druid.query.QueryMetrics;
+import org.apache.druid.query.QueryPlus;
 import org.apache.druid.query.QueryProcessingPool;
 import org.apache.druid.query.QueryRunner;
 import org.apache.druid.query.QueryRunnerFactory;
@@ -52,6 +59,7 @@ import org.apache.druid.query.QueryToolChest;
 import org.apache.druid.query.ReportTimelineMissingSegmentQueryRunner;
 import org.apache.druid.query.SegmentDescriptor;
 import org.apache.druid.query.SinkQueryRunners;
+import org.apache.druid.query.context.ResponseContext;
 import org.apache.druid.query.planning.DataSourceAnalysis;
 import org.apache.druid.query.spec.SpecificSegmentQueryRunner;
 import org.apache.druid.query.spec.SpecificSegmentSpec;
@@ -69,6 +77,7 @@ import org.apache.druid.timeline.partition.PartitionChunk;
 import org.apache.druid.utils.CloseableUtils;
 import org.joda.time.Interval;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -76,8 +85,11 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
+import java.util.function.ObjLongConsumer;
 import java.util.stream.Collectors;
 
 /**
@@ -87,6 +99,19 @@ public class SinkQuerySegmentWalker implements QuerySegmentWalker
 {
   private static final EmittingLogger log = new EmittingLogger(SinkQuerySegmentWalker.class);
   private static final String CONTEXT_SKIP_INCREMENTAL_SEGMENT = "skipIncrementalSegment";
+
+  private static final Set<String> SEGMENT_QUERY_METRIC = ImmutableSet.of(DefaultQueryMetrics.QUERY_SEGMENT_TIME);
+  private static final Set<String> SEGMENT_CACHE_AND_WAIT_METRICS = ImmutableSet.of(
+      DefaultQueryMetrics.QUERY_WAIT_TIME,
+      DefaultQueryMetrics.QUERY_SEGMENT_AND_CACHE_TIME
+  );
+
+  private static final Map<String, ObjLongConsumer<? super QueryMetrics<?>>> METRICS_TO_REPORT =
+      ImmutableMap.of(
+          DefaultQueryMetrics.QUERY_SEGMENT_TIME, QueryMetrics::reportSegmentTime,
+          DefaultQueryMetrics.QUERY_SEGMENT_AND_CACHE_TIME, QueryMetrics::reportSegmentAndCacheTime,
+          DefaultQueryMetrics.QUERY_WAIT_TIME, QueryMetrics::reportWaitTime
+      );
 
   private final String dataSource;
 
@@ -193,6 +218,7 @@ public class SinkQuerySegmentWalker implements QuerySegmentWalker
     final List<SinkSegmentReference> allSegmentReferences = new ArrayList<>();
     final Map<SegmentDescriptor, SegmentId> segmentIdMap = new HashMap<>();
     final LinkedHashMap<SegmentDescriptor, List<QueryRunner<T>>> allRunners = new LinkedHashMap<>();
+    final ConcurrentHashMap<String, SinkMetricsEmittingQueryRunner.SegmentMetrics> segmentMetricsAccumulator = new ConcurrentHashMap<>();
 
     try {
       for (final SegmentDescriptor descriptor : specs) {
@@ -231,12 +257,13 @@ public class SinkQuerySegmentWalker implements QuerySegmentWalker
               descriptor,
               sinkSegmentReferences.stream().map(
                   segmentReference -> {
-                    QueryRunner<T> runner = new MetricsEmittingQueryRunner<>(
+                    QueryRunner<T> runner = new SinkMetricsEmittingQueryRunner<>(
                         emitter,
                         factory.getToolchest(),
                         factory.createRunner(segmentReference.getSegment()),
-                        QueryMetrics::reportSegmentTime,
-                        queryMetrics -> queryMetrics.segment(sinkSegmentId.toString())
+                        segmentMetricsAccumulator,
+                        SEGMENT_QUERY_METRIC,
+                        sinkSegmentId.toString()
                     );
 
                     // 1) Only use caching if data is immutable
@@ -273,13 +300,14 @@ public class SinkQuerySegmentWalker implements QuerySegmentWalker
 
                     // Regardless of whether caching is enabled, do reportSegmentAndCacheTime outside the
                     // *possible* caching.
-                    runner = new MetricsEmittingQueryRunner<>(
+                    runner = new SinkMetricsEmittingQueryRunner<>(
                         emitter,
                         factory.getToolchest(),
                         runner,
-                        QueryMetrics::reportSegmentAndCacheTime,
-                        queryMetrics -> queryMetrics.segment(sinkSegmentId.toString())
-                    ).withWaitMeasuredFromNow();
+                        segmentMetricsAccumulator,
+                        SEGMENT_CACHE_AND_WAIT_METRICS,
+                        sinkSegmentId.toString()
+                    );
 
                     // Emit CPU time metrics.
                     runner = CPUTimeMetricQueryRunner.safeBuild(
@@ -344,7 +372,17 @@ public class SinkQuerySegmentWalker implements QuerySegmentWalker
       return new ResourceIdPopulatingQueryRunner<>(
           QueryRunnerHelper.makeClosingQueryRunner(
               CPUTimeMetricQueryRunner.safeBuild(
-                  new FinalizeResultsQueryRunner<>(toolChest.mergeResults(mergedRunner, true), toolChest),
+                  new SinkMetricsEmittingQueryRunner<>(
+                      emitter,
+                      toolChest,
+                      new FinalizeResultsQueryRunner<>(
+                          toolChest.mergeResults(mergedRunner, true),
+                          toolChest
+                      ),
+                      segmentMetricsAccumulator,
+                      Collections.emptySet(),
+                      null
+                  ),
                   toolChest,
                   emitter,
                   cpuTimeAccumulator,
@@ -415,7 +453,153 @@ public class SinkQuerySegmentWalker implements QuerySegmentWalker
     // with subsegments (hydrants).
     return segmentId + "_H" + hydrantNumber;
   }
+  
+  /**
+   * This class is responsible for emitting query/segment/time, query/wait/time and query/segmentAndCache/Time metrics for a Sink.
+   * It accumulates query/segment/time and query/segmentAndCache/time metric for each FireHydrant at the level of Sink.
+   * query/wait/time metric is the time taken to process the first FireHydrant for the Sink.
+   * <p>
+   * This class operates in two distinct modes based on whether {@link SinkMetricsEmittingQueryRunner#segmentId} is null or non-null.
+   * When segmentId is non-null, it accumulates the metrics. When segmentId is null, it emits the accumulated metrics.
+   * <p>
+   * This class is derived from {@link org.apache.druid.query.MetricsEmittingQueryRunner}.
+   */
+  private static class SinkMetricsEmittingQueryRunner<T> implements QueryRunner<T>
+  {
+    private final ServiceEmitter emitter;
+    private final QueryToolChest<T, ? extends Query<T>> queryToolChest;
+    private final QueryRunner<T> queryRunner;
+    private final ConcurrentHashMap<String, SegmentMetrics> segmentMetricsAccumulator;
+    private final Set<String> metricsToCompute;
+    @Nullable
+    private final String segmentId;
+    private final long creationTimeNs;
 
+    private SinkMetricsEmittingQueryRunner(
+        ServiceEmitter emitter,
+        QueryToolChest<T, ? extends Query<T>> queryToolChest,
+        QueryRunner<T> queryRunner,
+        ConcurrentHashMap<String, SegmentMetrics> segmentMetricsAccumulator,
+        Set<String> metricsToCompute,
+        @Nullable String segmentId
+    )
+    {
+      this.emitter = emitter;
+      this.queryToolChest = queryToolChest;
+      this.queryRunner = queryRunner;
+      this.segmentMetricsAccumulator = segmentMetricsAccumulator;
+      this.metricsToCompute = metricsToCompute;
+      this.segmentId = segmentId;
+      this.creationTimeNs = System.nanoTime();
+    }
+
+    @Override
+    public Sequence<T> run(final QueryPlus<T> queryPlus, final ResponseContext responseContext)
+    {
+      QueryPlus<T> queryWithMetrics = queryPlus.withQueryMetrics(queryToolChest);
+      return Sequences.wrap(
+          // Use LazySequence because we want to account execution time of queryRunner.run() (it prepares the underlying
+          // Sequence) as part of the reported query time, i.e. we want to execute queryRunner.run() after
+          // `startTimeNs = System.nanoTime();`
+          new LazySequence<>(() -> queryRunner.run(queryWithMetrics, responseContext)),
+          new SequenceWrapper()
+          {
+            private long startTimeNs;
+
+            @Override
+            public void before()
+            {
+              startTimeNs = System.nanoTime();
+            }
+
+            @Override
+            public void after(boolean isDone, Throwable thrown)
+            {
+              if (segmentId != null) {
+                // accumulate metrics
+                final SegmentMetrics metrics = segmentMetricsAccumulator.computeIfAbsent(segmentId, id -> new SegmentMetrics());
+                if (metricsToCompute.contains(DefaultQueryMetrics.QUERY_WAIT_TIME)) {
+                  metrics.setWaitTime(startTimeNs - creationTimeNs);
+                }
+                if (metricsToCompute.contains(DefaultQueryMetrics.QUERY_SEGMENT_TIME)) {
+                  metrics.addSegmentTime(System.nanoTime() - startTimeNs);
+                }
+                if (metricsToCompute.contains(DefaultQueryMetrics.QUERY_SEGMENT_AND_CACHE_TIME)) {
+                  metrics.addSegmentAndCacheTime(System.nanoTime() - startTimeNs);
+                }
+              } else {
+                final QueryMetrics<?> queryMetrics = queryWithMetrics.getQueryMetrics();
+                // report accumulated metrics
+                for (Map.Entry<String, SegmentMetrics> segmentAndMetrics : segmentMetricsAccumulator.entrySet()) {
+                  queryMetrics.segment(segmentAndMetrics.getKey());
+
+                  for (Map.Entry<String, ObjLongConsumer<? super QueryMetrics<?>>> reportMetric : METRICS_TO_REPORT.entrySet()) {
+                    final String metricName = reportMetric.getKey();
+                    switch (metricName) {
+                      case DefaultQueryMetrics.QUERY_SEGMENT_TIME:
+                        reportMetric.getValue().accept(queryMetrics, segmentAndMetrics.getValue().getSegmentTime());
+                      case DefaultQueryMetrics.QUERY_WAIT_TIME:
+                        reportMetric.getValue().accept(queryMetrics, segmentAndMetrics.getValue().getWaitTime());
+                      case DefaultQueryMetrics.QUERY_SEGMENT_AND_CACHE_TIME:
+                        reportMetric.getValue().accept(queryMetrics, segmentAndMetrics.getValue().getSegmentAndCacheTime());
+                    }
+                  }
+
+                  try {
+                    queryMetrics.emit(emitter);
+                  }
+                  catch (Exception e) {
+                    // Query should not fail, because of emitter failure. Swallowing the exception.
+                    log.error(e, "Failed to emit metrics for segment[%s]", segmentAndMetrics.getKey());
+                  }
+                }
+              }
+            }
+          }
+      );
+    }
+
+    /**
+     * Class to track segment related metrics during query execution.
+     */
+    private static class SegmentMetrics
+    {
+      private final AtomicLong querySegmentTime = new AtomicLong(0);
+      private final AtomicLong queryWaitTime = new AtomicLong(0);
+      private final AtomicLong querySegmentAndCacheTime = new AtomicLong(0);
+
+      private void addSegmentTime(long time)
+      {
+        querySegmentTime.addAndGet(time);
+      }
+
+      private void setWaitTime(long time)
+      {
+        queryWaitTime.set(time);
+      }
+
+      private void addSegmentAndCacheTime(long time)
+      {
+        querySegmentAndCacheTime.addAndGet(time);
+      }
+
+      private long getSegmentTime()
+      {
+        return querySegmentTime.get();
+      }
+
+      private long getWaitTime()
+      {
+        return queryWaitTime.get();
+      }
+
+      private long getSegmentAndCacheTime()
+      {
+        return querySegmentAndCacheTime.get();
+      }
+    }
+  }
+  
   private static class SinkHolder implements Overshadowable<SinkHolder>
   {
     private final Sink sink;

server/src/test/java/org/apache/druid/segment/realtime/appenderator/StreamAppenderatorTest.java
@@ -34,6 +34,7 @@ import org.apache.druid.java.util.common.DateTimes;
 import org.apache.druid.java.util.common.Intervals;
 import org.apache.druid.java.util.common.Pair;
 import org.apache.druid.java.util.common.granularity.Granularities;
+import org.apache.druid.java.util.metrics.StubServiceEmitter;
 import org.apache.druid.metadata.PendingSegmentRecord;
 import org.apache.druid.query.Druids;
 import org.apache.druid.query.Order;
@@ -70,8 +71,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutionException;
@@ -1861,9 +1864,11 @@ public class StreamAppenderatorTest extends InitializedNullHandlingTest
   public void testQueryByIntervals() throws Exception
   {
     try (
+        final StubServiceEmitter serviceEmitter = new StubServiceEmitter();
         final StreamAppenderatorTester tester =
             new StreamAppenderatorTester.Builder().maxRowsInMemory(2)
                                                   .basePersistDirectory(temporaryFolder.newFolder())
+                                                  .withServiceEmitter(serviceEmitter)
                                                   .build()) {
       final Appenderator appenderator = tester.getAppenderator();
 
@@ -1902,35 +1907,17 @@ public class StreamAppenderatorTest extends InitializedNullHandlingTest
           results1
       );
 
-      // Query2: 2000/2002
-      final TimeseriesQuery query2 = Druids.newTimeseriesQueryBuilder()
-                                           .dataSource(StreamAppenderatorTester.DATASOURCE)
-                                           .intervals(ImmutableList.of(Intervals.of("2000/2002")))
-                                           .aggregators(
-                                               Arrays.asList(
-                                                   new LongSumAggregatorFactory("count", "count"),
-                                                   new LongSumAggregatorFactory("met", "met")
-                                               )
-                                           )
-                                           .granularity(Granularities.DAY)
-                                           .build();
-
-      final List<Result<TimeseriesResultValue>> results2 =
-          QueryPlus.wrap(query2).run(appenderator, ResponseContext.createEmpty()).toList();
-      Assert.assertEquals(
-          "query2",
-          ImmutableList.of(
-              new Result<>(
-                  DateTimes.of("2000"),
-                  new TimeseriesResultValue(ImmutableMap.of("count", 3L, "met", 7L))
-              ),
-              new Result<>(
-                  DateTimes.of("2001"),
-                  new TimeseriesResultValue(ImmutableMap.of("count", 4L, "met", 120L))
+      verifySinkMetrics(
+          serviceEmitter,
+          new HashSet<>(
+              Arrays.asList(
+                  IDENTIFIERS.get(0).asSegmentId().toString(),
+                  IDENTIFIERS.get(1).asSegmentId().toString()
               )
-          ),
-          results2
+          )
       );
+
+      serviceEmitter.flush();
 
       // Query3: 2000/2001T01
       final TimeseriesQuery query3 = Druids.newTimeseriesQueryBuilder()
@@ -1960,6 +1947,19 @@ public class StreamAppenderatorTest extends InitializedNullHandlingTest
           ),
           results3
       );
+
+      verifySinkMetrics(
+          serviceEmitter,
+          new HashSet<>(
+              Arrays.asList(
+                  IDENTIFIERS.get(0).asSegmentId().toString(),
+                  IDENTIFIERS.get(1).asSegmentId().toString(),
+                  IDENTIFIERS.get(2).asSegmentId().toString()
+              )
+          )
+      );
+
+      serviceEmitter.flush();
 
       // Query4: 2000/2001T01, 2001T03/2001T04
       final TimeseriesQuery query4 = Druids.newTimeseriesQueryBuilder()
@@ -1994,6 +1994,16 @@ public class StreamAppenderatorTest extends InitializedNullHandlingTest
           ),
           results4
       );
+      verifySinkMetrics(
+          serviceEmitter,
+          new HashSet<>(
+              Arrays.asList(
+                  IDENTIFIERS.get(0).asSegmentId().toString(),
+                  IDENTIFIERS.get(1).asSegmentId().toString(),
+                  IDENTIFIERS.get(2).asSegmentId().toString()
+              )
+          )
+      );
     }
   }
 
@@ -2001,9 +2011,11 @@ public class StreamAppenderatorTest extends InitializedNullHandlingTest
   public void testQueryBySegments() throws Exception
   {
     try (
+        StubServiceEmitter serviceEmitter = new StubServiceEmitter();
         final StreamAppenderatorTester tester =
             new StreamAppenderatorTester.Builder().maxRowsInMemory(2)
                                                   .basePersistDirectory(temporaryFolder.newFolder())
+                                                  .withServiceEmitter(serviceEmitter)
                                                   .build()) {
       final Appenderator appenderator = tester.getAppenderator();
 
@@ -2052,6 +2064,17 @@ public class StreamAppenderatorTest extends InitializedNullHandlingTest
           results1
       );
 
+      verifySinkMetrics(
+          serviceEmitter,
+          new HashSet<>(
+              Collections.singletonList(
+                  IDENTIFIERS.get(2).asSegmentId().toString()
+              )
+          )
+      );
+
+      serviceEmitter.flush();
+
       // Query2: segment #2, partial
       final TimeseriesQuery query2 = Druids.newTimeseriesQueryBuilder()
                                            .dataSource(StreamAppenderatorTester.DATASOURCE)
@@ -2087,6 +2110,17 @@ public class StreamAppenderatorTest extends InitializedNullHandlingTest
           ),
           results2
       );
+
+      verifySinkMetrics(
+          serviceEmitter,
+          new HashSet<>(
+              Collections.singletonList(
+                  IDENTIFIERS.get(2).asSegmentId().toString()
+              )
+          )
+      );
+
+      serviceEmitter.flush();
 
       // Query3: segment #2, two disjoint intervals
       final TimeseriesQuery query3 = Druids.newTimeseriesQueryBuilder()
@@ -2129,6 +2163,17 @@ public class StreamAppenderatorTest extends InitializedNullHandlingTest
           results3
       );
 
+      verifySinkMetrics(
+          serviceEmitter,
+          new HashSet<>(
+              Collections.singletonList(
+                  IDENTIFIERS.get(2).asSegmentId().toString()
+              )
+          )
+      );
+
+      serviceEmitter.flush();
+
       final ScanQuery query4 = Druids.newScanQueryBuilder()
                                      .dataSource(StreamAppenderatorTester.DATASOURCE)
                                      .intervals(
@@ -2164,6 +2209,33 @@ public class StreamAppenderatorTest extends InitializedNullHandlingTest
           new Object[]{DateTimes.of("2001T03").getMillis(), "foo", 1L, 64L},
           ((List<Object>) ((List<Object>) results4.get(1).getEvents()).get(0)).toArray()
       );
+
+      verifySinkMetrics(
+          serviceEmitter,
+          new HashSet<>(
+              Collections.singletonList(
+                  IDENTIFIERS.get(2).asSegmentId().toString()
+              )
+          )
+      );
+
+      serviceEmitter.flush();
+    }
+  }
+
+  private void verifySinkMetrics(StubServiceEmitter emitter, Set<String> segmentIds)
+  {
+    Map<String, List<StubServiceEmitter.ServiceMetricEventSnapshot>> events = emitter.getMetricEvents();
+    int segments = segmentIds.size();
+    Assert.assertEquals(4, events.size());
+    Assert.assertTrue(events.containsKey("query/cpu/time"));
+    Assert.assertEquals(segments, events.get("query/segment/time").size());
+    Assert.assertEquals(segments, events.get("query/segmentAndCache/time").size());
+    Assert.assertEquals(segments, events.get("query/wait/time").size());
+    for (String id : segmentIds) {
+      Assert.assertTrue(events.get("query/segment/time").stream().anyMatch(value -> value.getUserDims().containsValue(id)));
+      Assert.assertTrue(events.get("query/segmentAndCache/time").stream().anyMatch(value -> value.getUserDims().containsValue(id)));
+      Assert.assertTrue(events.get("query/wait/time").stream().anyMatch(value -> value.getUserDims().containsValue(id)));
     }
   }
 

commit f357a4b86af4cbb2632be49120a80da3a6cfa7c4

fix: Peon metrics emit same value (#19484)
This PR fixes incorrect Peon sink-level query metric emission in SinkQuerySegmentWalker. This bug exists since v32, introduced by https://github.com/apache/druid/pull/17170.

The existing code iterated over METRICS_TO_REPORT and used a switch without break statements. Because reportMetric.getValue() is bound to the current metric reporter, fallthrough caused later accumulator values to be reported through the wrong reporter. For example, the query/segment/time reporter could be called with segment time, then wait time, then segment-and-cache time before emit(). Since DefaultQueryMetrics stores metric values by metric name before emission, the last value overwrote earlier ones.

server/src/main/java/org/apache/druid/segment/realtime/appenderator/SinkQuerySegmentWalker.java
@@ -22,7 +22,6 @@ package org.apache.druid.segment.realtime.appenderator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import org.apache.druid.client.CachingQueryRunner;
 import org.apache.druid.client.cache.Cache;
@@ -90,7 +89,6 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.ObjLongConsumer;
 import java.util.stream.Collectors;
 
 /**
@@ -106,13 +104,6 @@ public class SinkQuerySegmentWalker implements QuerySegmentWalker
       DefaultQueryMetrics.QUERY_WAIT_TIME,
       DefaultQueryMetrics.QUERY_SEGMENT_AND_CACHE_TIME
   );
-
-  private static final Map<String, ObjLongConsumer<? super QueryMetrics<?>>> METRICS_TO_REPORT =
-      ImmutableMap.of(
-          DefaultQueryMetrics.QUERY_SEGMENT_TIME, QueryMetrics::reportSegmentTime,
-          DefaultQueryMetrics.QUERY_SEGMENT_AND_CACHE_TIME, QueryMetrics::reportSegmentAndCacheTime,
-          DefaultQueryMetrics.QUERY_WAIT_TIME, QueryMetrics::reportWaitTime
-      );
 
   private final String dataSource;
 
@@ -539,17 +530,11 @@ public class SinkQuerySegmentWalker implements QuerySegmentWalker
                 for (Map.Entry<String, SegmentMetrics> segmentAndMetrics : segmentMetricsAccumulator.entrySet()) {
                   queryMetrics.segment(segmentAndMetrics.getKey());
 
-                  for (Map.Entry<String, ObjLongConsumer<? super QueryMetrics<?>>> reportMetric : METRICS_TO_REPORT.entrySet()) {
-                    final String metricName = reportMetric.getKey();
-                    switch (metricName) {
-                      case DefaultQueryMetrics.QUERY_SEGMENT_TIME:
-                        reportMetric.getValue().accept(queryMetrics, segmentAndMetrics.getValue().getSegmentTime());
-                      case DefaultQueryMetrics.QUERY_WAIT_TIME:
-                        reportMetric.getValue().accept(queryMetrics, segmentAndMetrics.getValue().getWaitTime());
-                      case DefaultQueryMetrics.QUERY_SEGMENT_AND_CACHE_TIME:
-                        reportMetric.getValue().accept(queryMetrics, segmentAndMetrics.getValue().getSegmentAndCacheTime());
-                    }
-                  }
+                  final SegmentMetrics segmentMetrics = segmentAndMetrics.getValue();
+
+                  queryMetrics.reportSegmentTime(segmentMetrics.getSegmentTime());
+                  queryMetrics.reportWaitTime(segmentMetrics.getWaitTime());
+                  queryMetrics.reportSegmentAndCacheTime(segmentMetrics.getSegmentAndCacheTime());
 
                   try {
                     queryMetrics.emit(emitter);

server/src/test/java/org/apache/druid/segment/realtime/appenderator/StreamAppenderatorTest.java
@@ -35,6 +35,7 @@ import org.apache.druid.java.util.common.Pair;
 import org.apache.druid.java.util.common.granularity.Granularities;
 import org.apache.druid.java.util.metrics.StubServiceEmitter;
 import org.apache.druid.metadata.PendingSegmentRecord;
+import org.apache.druid.query.DefaultQueryMetrics;
 import org.apache.druid.query.Druids;
 import org.apache.druid.query.Order;
 import org.apache.druid.query.QueryPlus;
@@ -2296,14 +2297,14 @@ public class StreamAppenderatorTest extends InitializedNullHandlingTest
   private void verifySinkMetrics(StubServiceEmitter emitter, Set<String> segmentIds)
   {
     int segments = segmentIds.size();
-    emitter.verifyEmitted("query/cpu/time", 1);
-    Assert.assertEquals(segments, emitter.getMetricEvents("query/segment/time").size());
-    Assert.assertEquals(segments, emitter.getMetricEvents("query/segmentAndCache/time").size());
-    Assert.assertEquals(segments, emitter.getMetricEvents("query/wait/time").size());
+    emitter.verifyEmitted(DefaultQueryMetrics.QUERY_CPU_TIME, 1);
+    Assert.assertEquals(segments, emitter.getMetricEvents(DefaultQueryMetrics.QUERY_SEGMENT_TIME).size());
+    Assert.assertEquals(segments, emitter.getMetricEvents(DefaultQueryMetrics.QUERY_SEGMENT_AND_CACHE_TIME).size());
+    Assert.assertEquals(segments, emitter.getMetricEvents(DefaultQueryMetrics.QUERY_WAIT_TIME).size());
     for (String id : segmentIds) {
-      Assert.assertTrue(emitter.getMetricEvents("query/segment/time").stream().anyMatch(value -> value.getUserDims().containsValue(id)));
-      Assert.assertTrue(emitter.getMetricEvents("query/segmentAndCache/time").stream().anyMatch(value -> value.getUserDims().containsValue(id)));
-      Assert.assertTrue(emitter.getMetricEvents("query/wait/time").stream().anyMatch(value -> value.getUserDims().containsValue(id)));
+      Assert.assertTrue(emitter.getMetricEvents(DefaultQueryMetrics.QUERY_SEGMENT_TIME).stream().anyMatch(value -> value.getUserDims().containsValue(id)));
+      Assert.assertTrue(emitter.getMetricEvents(DefaultQueryMetrics.QUERY_SEGMENT_AND_CACHE_TIME).stream().anyMatch(value -> value.getUserDims().containsValue(id)));
+      Assert.assertTrue(emitter.getMetricEvents(DefaultQueryMetrics.QUERY_WAIT_TIME).stream().anyMatch(value -> value.getUserDims().containsValue(id)));
     }
   }
 