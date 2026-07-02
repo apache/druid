e3e0ae354ded1204221975f2a1165e248c232081
Sasha Syrotenko <alexander.syrotenko@imply.io>
2026-07-02 11:29:47 +0300

    Decrease over-provisioning penalty value

--- indexing-service/src/main/java/org/apache/druid/indexing/seekablestream/supervisor/autoscaler/WeightedCostFunction.java
+++ indexing-service/src/main/java/org/apache/druid/indexing/seekablestream/supervisor/autoscaler/WeightedCostFunction.java
@@ -74,7 +74,7 @@ public class WeightedCostFunction
    * Penalty magnitude applied when idle ratio is 1 (fully wasted capacity).
    * Controls the steepness of the U-shape on the over-provisioning side.
    */
-  static final double OVER_PROVISIONING_PENALTY = 2.5;
+  static final double OVER_PROVISIONING_PENALTY = 2.0;
 
   /**
    * Computes cost for a given task count using compute time metrics.
