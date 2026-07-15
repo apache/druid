63002ecd75e10dd7ad0d5a74c5f42f4d95590726
Kashif Faraz <kashif.faraz@gmail.com>
2026-07-15 19:03:07 +0530

    Limit number of unused segment rows scanned

--- server/src/main/java/org/apache/druid/metadata/SqlSegmentsMetadataQuery.java
+++ server/src/main/java/org/apache/druid/metadata/SqlSegmentsMetadataQuery.java
@@ -1065,20 +1065,36 @@ public class SqlSegmentsMetadataQuery
   }
 
   /**
-   * Gets unused segment intervals for the specified datasource. There is no
-   * guarantee on the order of intervals in the list or on whether the limited
-   * list contains the earliest or latest intervals present in the datasource.
+   * Gets unused segment intervals for the specified datasource.
+   * <p>
+   * Note: This method does NOT guarantee that:
+   * <ul>
+   * <li>the intervals in the result would be ordered</li>
+   * <li>the result would contain the earliest or latest intervals for this datasource</li>
+   * <li>it would scan all unused segments for this datasource. So, the result
+   * may contain less than {@code limit} entries even when there are more distinct
+   * unused segment intervals in the metadata store for this datasource.</li>
+   * </ul>
    *
    * @return List of unused segment intervals containing upto {@code limit} interval entries.
    */
   public List<Interval> retrieveUnusedSegmentIntervals(String dataSource, int limit)
   {
     final String sql = StringUtils.format(
-        "SELECT start, %2$send%2$s FROM %1$s"
-        + " WHERE dataSource = :dataSource AND used = false"
-        + " GROUP BY %2$send%2$s, start"
-        + "  %3$s",
-        dbTables.getSegmentsTable(), connector.getQuoteString(), connector.limitClause(limit)
+        """
+            SELECT start, %2$send%2$s FROM
+            (
+              SELECT start, %2$send%2$s FROM %1$s
+              WHERE dataSource = :dataSource AND used = false
+              %3$s
+            ) AS unused
+            GROUP BY %2$send%2$s, start
+            %4$s
+            """,
+        dbTables.getSegmentsTable(),
+        connector.getQuoteString(),
+        connector.limitClause(1_000_000), // limit unused segment rows scanned to 1M
+        connector.limitClause(limit)
     );
 
     final List<Interval> intervals = connector.inReadOnlyTransaction(
