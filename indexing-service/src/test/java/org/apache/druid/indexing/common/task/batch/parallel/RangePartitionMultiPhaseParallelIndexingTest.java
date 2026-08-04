6548e2719be9d1ea5a67dfd2ed549cd3fc7f6bb5
Frank Chen <frank.chen021@outlook.com>
2026-08-04 10:22:20 +0800

    Replace deprecated JUnit assertThat calls (#19819)

--- indexing-service/src/test/java/org/apache/druid/indexing/common/task/batch/parallel/RangePartitionMultiPhaseParallelIndexingTest.java
+++ indexing-service/src/test/java/org/apache/druid/indexing/common/task/batch/parallel/RangePartitionMultiPhaseParallelIndexingTest.java
@@ -43,6 +43,7 @@ import org.apache.druid.timeline.DataSegment;
 import org.apache.druid.timeline.partition.DimensionRangeShardSpec;
 import org.apache.druid.timeline.partition.NumberedShardSpec;
 import org.apache.druid.timeline.partition.SingleDimensionShardSpec;
+import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.joda.time.Interval;
 import org.junit.Assert;
@@ -455,14 +456,14 @@ public class RangePartitionMultiPhaseParallelIndexingTest extends AbstractMultiP
 
     for (StringTuple value : values) {
       if (start != null) {
-        Assert.assertThat(value.compareTo(start), Matchers.greaterThanOrEqualTo(0));
+        MatcherAssert.assertThat(value.compareTo(start), Matchers.greaterThanOrEqualTo(0));
       }
 
       if (end != null) {
         if (value == null) {
           Assert.assertNull("null values should be in first partition", start);
         } else {
-          Assert.assertThat(value.compareTo(end), Matchers.lessThan(0));
+          MatcherAssert.assertThat(value.compareTo(end), Matchers.lessThan(0));
         }
       }
     }
