971a0d83834e673b7cdba0d050a5a9305a5591b8
Frank Chen <frank.chen021@outlook.com>
2026-08-05 07:01:47 +0800

    test: preserve unique temp directories in junit5 migrations

--- extensions-core/orc-extensions/src/test/java/org/apache/druid/data/input/orc/OrcReaderTest.java
+++ extensions-core/orc-extensions/src/test/java/org/apache/druid/data/input/orc/OrcReaderTest.java
@@ -714,11 +714,8 @@ public class OrcReaderTest extends InitializedNullHandlingTest
     return inputFormat.createReader(schema, entity, newFolder(temporaryFolder, "junit"));
   }
 
-  private static File newFolder(File root, String... subDirs) throws IOException
+  private static File newFolder(File root, String... subDirs)
   {
-    String subFolder = String.join("/", subDirs);
-    File result = new File(root, subFolder);
-    FileUtils.mkdirp(result);
-    return result;
+    return FileUtils.createTempDirInLocation(root.toPath(), String.join("-", subDirs));
   }
 }
