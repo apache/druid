eb275d31a6b6ad6c100ba65baf692f9455b024fe
Kashif Faraz <kashif.faraz@gmail.com>
2026-08-10 11:11:28 +0530

    Fix checkstyle

--- server/src/main/java/org/apache/druid/metadata/IndexerSQLMetadataStorageCoordinator.java
+++ server/src/main/java/org/apache/druid/metadata/IndexerSQLMetadataStorageCoordinator.java
@@ -553,7 +553,7 @@ public class IndexerSQLMetadataStorageCoordinator implements IndexerMetadataStor
 
       return result;
     }
-    catch (CallbackFailedException|DruidException e) {
+    catch (CallbackFailedException | DruidException e) {
       return SegmentPublishResult.fail(e.getMessage());
     }
   }
