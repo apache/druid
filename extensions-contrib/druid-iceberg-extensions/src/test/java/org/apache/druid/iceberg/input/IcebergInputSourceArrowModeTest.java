3de50f0f5b5a2323b60f6aba509a1eac49190136
shekharrajak <shekharrajak@live.com>
2026-09-02 19:41:56 +0530

    test: cover Arrow reader mode on IcebergInputSource, drop iceberg_arrow type

--- extensions-contrib/druid-iceberg-extensions/src/test/java/org/apache/druid/iceberg/input/IcebergInputSourceArrowModeTest.java
+++ extensions-contrib/druid-iceberg-extensions/src/test/java/org/apache/druid/iceberg/input/IcebergInputSourceArrowModeTest.java
@@ -19,15 +19,24 @@
 
 package org.apache.druid.iceberg.input;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.NamedType;
 import com.google.common.collect.ImmutableMap;
 import org.apache.druid.data.input.ColumnsFilter;
+import org.apache.druid.data.input.InputRow;
 import org.apache.druid.data.input.InputRowSchema;
+import org.apache.druid.data.input.InputSource;
 import org.apache.druid.data.input.InputSourceReader;
 import org.apache.druid.data.input.impl.DimensionsSpec;
+import org.apache.druid.data.input.impl.LocalInputSourceFactory;
 import org.apache.druid.data.input.impl.TimestampSpec;
 import org.apache.druid.error.DruidException;
 import org.apache.druid.iceberg.filter.IcebergEqualsFilter;
+import org.apache.druid.iceberg.filter.IcebergFilter;
+import org.apache.druid.jackson.DefaultObjectMapper;
+import org.apache.druid.java.util.common.DateTimes;
 import org.apache.druid.java.util.common.FileUtils;
+import org.apache.druid.java.util.common.parsers.CloseableIterator;
 import org.apache.iceberg.DataFile;
 import org.apache.iceberg.Files;
 import org.apache.iceberg.PartitionKey;
@@ -43,6 +52,7 @@ import org.apache.iceberg.io.DataWriter;
 import org.apache.iceberg.io.OutputFile;
 import org.apache.iceberg.parquet.Parquet;
 import org.apache.iceberg.types.Types;
+import org.joda.time.DateTime;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -54,7 +64,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-public class IcebergArrowInputSourceTest
+/**
+ * Covers {@link IcebergInputSource} with {@code useArrowReader} enabled, where the Arrow delegate reads the
+ * table scan directly instead of going through a warehouse input source.
+ */
+public class IcebergInputSourceArrowModeTest
 {
   private IcebergCatalog testCatalog;
   private TableIdentifier tableIdentifier;
@@ -87,22 +101,14 @@ public class IcebergArrowInputSourceTest
   @Test
   public void testReadWithNullInputStatsDoesNotNpe() throws IOException
   {
-    final IcebergArrowInputSource src = new IcebergArrowInputSource(
-        TABLENAME,
-        NAMESPACE,
-        null,
-        testCatalog,
-        null,
-        null,
-        1024
-    );
+    final IcebergInputSource src = arrowSource(null, null, null);
     final InputRowSchema schemaWithMissingTs = new InputRowSchema(
-        new TimestampSpec(null, null, org.apache.druid.java.util.common.DateTimes.utc(0L)),
+        new TimestampSpec(null, null, DateTimes.utc(0L)),
         DimensionsSpec.builder().build(),
         ColumnsFilter.all()
     );
     final InputSourceReader reader = src.reader(schemaWithMissingTs, null, FileUtils.createTempDir());
-    try (org.apache.druid.java.util.common.parsers.CloseableIterator<org.apache.druid.data.input.InputRow> it = reader.read()) {
+    try (CloseableIterator<InputRow> it = reader.read()) {
       while (it.hasNext()) {
         Assertions.assertNotNull(it.next());
       }
@@ -112,30 +118,30 @@ public class IcebergArrowInputSourceTest
   @Test
   public void testIsNotSplittable()
   {
-    final IcebergArrowInputSource src = new IcebergArrowInputSource(
-        TABLENAME,
-        NAMESPACE,
-        null,
-        testCatalog,
-        null,
-        null,
-        1024
-    );
+    final IcebergInputSource src = arrowSource(null, null, null);
     Assertions.assertFalse(src.isSplittable());
     Assertions.assertFalse(src.needsFormat());
   }
 
   @Test
-  public void testResidualFilterModeFail() throws IOException
+  public void testSingleSplitReturnsOwningSourceSoRowsAreNotDropped() throws IOException
   {
-    final IcebergArrowInputSource src = new IcebergArrowInputSource(
-        TABLENAME,
-        NAMESPACE,
+    final IcebergInputSource src = arrowSource(null, null, null);
+    Assertions.assertEquals(1, src.estimateNumSplits(null, null));
+    final InputSource split = src.createSplits(null, null)
+                                 .map(src::withSplit)
+                                 .findFirst()
+                                 .orElseThrow(() -> new IllegalStateException("expected one split"));
+    Assertions.assertSame(src, split);
+  }
+
+  @Test
+  public void testResidualFilterModeFail()
+  {
+    final IcebergInputSource src = arrowSource(
         new IcebergEqualsFilter("id", "123988"),
-        testCatalog,
         null,
-        ResidualFilterMode.FAIL,
-        1024
+        ResidualFilterMode.FAIL
     );
     final InputRowSchema inputRowSchema = new InputRowSchema(
         new TimestampSpec("timestamp", "millis", null),
@@ -172,23 +178,101 @@ public class IcebergArrowInputSourceTest
     table.updateSpec().removeField("id").commit();
     appendRow(table, table.spec(), ImmutableMap.of("id", filterId, "name", "Bar"));
 
-    final IcebergArrowInputSource src = new IcebergArrowInputSource(
-        TABLENAME,
-        NAMESPACE,
+    final IcebergInputSource src = arrowSource(
         new IcebergEqualsFilter("id", filterId),
-        testCatalog,
-        org.apache.druid.java.util.common.DateTimes.utc(afterPartitionedSnapshot),
-        ResidualFilterMode.FAIL,
-        1024
+        DateTimes.utc(afterPartitionedSnapshot),
+        ResidualFilterMode.FAIL
     );
     final InputRowSchema inputRowSchema = new InputRowSchema(
-        new TimestampSpec(null, null, org.apache.druid.java.util.common.DateTimes.utc(0L)),
+        new TimestampSpec(null, null, DateTimes.utc(0L)),
         DimensionsSpec.builder().build(),
         ColumnsFilter.all()
     );
 
     final InputSourceReader reader = src.reader(inputRowSchema, null, FileUtils.createTempDir());
     reader.read().close();
+  }
+
+  @Test
+  public void testWarehouseSourceNotRequiredInArrowMode()
+  {
+    Assertions.assertFalse(arrowSource(null, null, null).isSplittable());
+  }
+
+  @Test
+  public void testWarehouseSourceRequiredWhenArrowDisabled()
+  {
+    Assertions.assertThrows(
+        NullPointerException.class,
+        () -> new IcebergInputSource(
+            TABLENAME,
+            NAMESPACE,
+            null,
+            testCatalog,
+            null,
+            null,
+            null,
+            false,
+            null
+        )
+    );
+  }
+
+  @Test
+  public void testArrowPropertiesSurviveSerde() throws IOException
+  {
+    final ObjectMapper mapper = new DefaultObjectMapper();
+    mapper.registerSubtypes(
+        new NamedType(LocalCatalog.class, LocalCatalog.TYPE_KEY),
+        new NamedType(IcebergInputSource.class, IcebergInputSource.TYPE_KEY)
+    );
+    final IcebergInputSource src = arrowSource(null, null, null);
+    final IcebergInputSource roundTripped = (IcebergInputSource) mapper.readValue(
+        mapper.writeValueAsBytes(src),
+        InputSource.class
+    );
+    Assertions.assertTrue(roundTripped.isUseArrowReader());
+    Assertions.assertEquals(512, roundTripped.getArrowBatchSize());
+    Assertions.assertFalse(roundTripped.needsFormat());
+    Assertions.assertFalse(roundTripped.isSplittable());
+  }
+
+  @Test
+  public void testDefaultsToStandardModeWhenArrowUnset()
+  {
+    final IcebergInputSource src = new IcebergInputSource(
+        TABLENAME,
+        NAMESPACE,
+        null,
+        testCatalog,
+        new LocalInputSourceFactory(),
+        null,
+        null,
+        null,
+        null
+    );
+    Assertions.assertFalse(src.isUseArrowReader());
+    Assertions.assertTrue(src.needsFormat());
+    Assertions.assertTrue(src.isSplittable());
+  }
+
+  private IcebergInputSource arrowSource(
+      final IcebergFilter icebergFilter,
+      final DateTime snapshotTime,
+      final ResidualFilterMode residualFilterMode
+  )
+  {
+    return new IcebergInputSource(
+        TABLENAME,
+        NAMESPACE,
+        icebergFilter,
+        testCatalog,
+        null,
+        snapshotTime,
+        residualFilterMode,
+        true,
+        512
+    );
   }
 
   private void createAndLoadTable(TableIdentifier id) throws IOException
