b006fe634d0b81eadb4729046c521393550a507f
Frank Chen <frank.chen021@outlook.com>
2026-09-07 18:43:05 +0800

    fix: use portable printf in Docker startup scripts

--- distribution/docker/peon.sh
+++ distribution/docker/peon.sh
@@ -80,8 +80,8 @@ setKey() {
     # Delete from all
     sed -ri "/$key=/d" $COMMON_CONF_DIR/common.runtime.properties
     [ -f $service_conf ] && sed -ri "/$key=/d" $service_conf
-    [ -f $service_conf ] && echo -e "\n$key=$value" >>$service_conf
-    [ -f $service_conf ] || echo -e "\n$key=$value" >>$COMMON_CONF_DIR/common.runtime.properties
+    [ -f $service_conf ] && printf '\n%s=%s\n' "$key" "$value" >>$service_conf
+    [ -f $service_conf ] || printf '\n%s=%s\n' "$key" "$value" >>$COMMON_CONF_DIR/common.runtime.properties
 
     echo "Setting $key=$value in $service_conf"
 }
