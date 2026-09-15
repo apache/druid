1f8f0252713570c3ebd1ebded699ca8abbdc3d7c
shekharrajak <shekharrajak@live.com>
2026-09-15 11:46:22 +0530

    fix: stabilize autoscaler scale-up test startup

--- embedded-tests/src/test/java/org/apache/druid/testing/embedded/indexing/autoscaler/CostBasedAutoScalerIntegrationTest.java
+++ embedded-tests/src/test/java/org/apache/druid/testing/embedded/indexing/autoscaler/CostBasedAutoScalerIntegrationTest.java
@@ -48,13 +48,6 @@ import org.junit.jupiter.api.Timeout;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.apache.druid.indexing.seekablestream.supervisor.autoscaler.CostBasedAutoScaler.OPTIMAL_TASK_COUNT_METRIC;
 
@@ -67,8 +60,6 @@ import static org.apache.druid.indexing.seekablestream.supervisor.autoscaler.Cos
 public class CostBasedAutoScalerIntegrationTest extends StreamIndexTestBase
 {
   private static final int PARTITION_COUNT = 50;
-  private static final int MAX_SCALE_UP_RECORD_BATCHES = 30;
-  private static final long SCALE_UP_PUBLISH_INTERVAL_MILLIS = 100;
   private static final long TASK_COUNT_RETRY_INTERVAL_MILLIS = 1_000;
   private static final int TASK_COUNT_RETRY_COUNT = 60;
 
@@ -249,9 +240,7 @@ public class CostBasedAutoScalerIntegrationTest extends StreamIndexTestBase
   }
 
   @Test
-  @Timeout(value = 20, unit = TimeUnit.MINUTES)
   public void test_autoScaler_scalesUpAndDown_withSlowPublish()
-      throws Exception
   {
     final String topic = EmbeddedClusterApis.createTestDatasourceName();
     kafkaServer.createTopicWithPartitions(topic, 4);
@@ -288,69 +277,39 @@ public class CostBasedAutoScalerIntegrationTest extends StreamIndexTestBase
             .waitForEvent(event -> event.hasMetricName("task/run/time")
                                         .hasDimension(DruidMetrics.DATASOURCE, dataSource));
 
-    final AtomicBoolean keepPublishing = new AtomicBoolean(true);
-    final AtomicInteger totalRecords = new AtomicInteger();
-    final ExecutorService publisher = Executors.newSingleThreadExecutor();
-    final Future<?> publisherFuture = publisher.submit(() -> {
-      for (int i = 0; i < MAX_SCALE_UP_RECORD_BATCHES && keepPublishing.get(); ++i) {
-        totalRecords.addAndGet(publish1kRecords(topic, false));
-        try {
-          TimeUnit.MILLISECONDS.sleep(SCALE_UP_PUBLISH_INTERVAL_MILLIS);
-        }
-        catch (InterruptedException e) {
-          Thread.currentThread().interrupt();
-          return;
-        }
-      }
-    });
-
-    try {
-      overlord.latchableEmitter().waitForEvent(
-          event -> event.hasMetricName(OPTIMAL_TASK_COUNT_METRIC)
-                        .hasDimension(DruidMetrics.SUPERVISOR_ID, supervisor.getId())
-                        .hasValueMatching(Matchers.greaterThan(1L))
-      );
-
-      overlord.latchableEmitter().waitForEvent(
-          event -> event.hasMetricName("task/autoScaler/updatedCount")
-                        .hasDimension(DruidMetrics.SUPERVISOR_ID, supervisor.getId())
-                        .hasValueMatching(Matchers.greaterThan(1L))
-      );
-      keepPublishing.set(false);
-      publisherFuture.get(30, TimeUnit.SECONDS);
-      ITRetryUtil.retryUntil(
-          () -> getCurrentTaskCount(supervisor.getId()) > 1,
-          true,
-          TASK_COUNT_RETRY_INTERVAL_MILLIS,
-          TASK_COUNT_RETRY_COUNT,
-          "supervisor task count to scale up"
-      );
-      waitUntilPublishedRecordsAreIngested(totalRecords.get());
-
-      // Let the tasks work through the lag.
-      // Do not publish any more records so that the idleness causes scale-down
-      overlord.latchableEmitter().waitForEvent(
-          event -> event.hasMetricName("task/autoScaler/updatedCount")
-                        .hasDimension(DruidMetrics.SUPERVISOR_ID, supervisor.getId())
-                        .hasValueMatching(Matchers.equalTo(1L))
-      );
-      ITRetryUtil.retryUntilEquals(
-          () -> getCurrentTaskCount(supervisor.getId()),
-          1,
-          TASK_COUNT_RETRY_INTERVAL_MILLIS,
-          TASK_COUNT_RETRY_COUNT,
-          "supervisor task count to scale down"
-      );
+    int totalRecords = 0;
+    for (int i = 0; i < 10; ++i) {
+      totalRecords += publish1kRecords(topic, false);
     }
-    catch (Exception e) {
-      throwPublisherFailureIfDone(publisherFuture);
-      throw e;
-    }
-    finally {
-      keepPublishing.set(false);
-      publisher.shutdownNow();
-      cluster.callApi().postSupervisor(supervisor.createSuspendedSpec());
-    }
+
+    overlord.latchableEmitter().waitForEvent(
+        event -> event.hasMetricName("task/autoScaler/updatedCount")
+                      .hasDimension(DruidMetrics.SUPERVISOR_ID, supervisor.getId())
+                      .hasValueMatching(Matchers.greaterThan(1L))
+    );
+    ITRetryUtil.retryUntil(
+        () -> getCurrentTaskCount(supervisor.getId()) > 1,
+        true,
+        TASK_COUNT_RETRY_INTERVAL_MILLIS,
+        TASK_COUNT_RETRY_COUNT,
+        "supervisor task count to scale up"
+    );
+    waitUntilPublishedRecordsAreIngested(totalRecords);
+
+    overlord.latchableEmitter().waitForEvent(
+        event -> event.hasMetricName("task/autoScaler/updatedCount")
+                      .hasDimension(DruidMetrics.SUPERVISOR_ID, supervisor.getId())
+                      .hasValueMatching(Matchers.equalTo(1L))
+    );
+    ITRetryUtil.retryUntilEquals(
+        () -> getCurrentTaskCount(supervisor.getId()),
+        1,
+        TASK_COUNT_RETRY_INTERVAL_MILLIS,
+        TASK_COUNT_RETRY_COUNT,
+        "supervisor task count to scale down"
+    );
+
+    cluster.callApi().postSupervisor(supervisor.createSuspendedSpec());
 
     cluster.callApi().waitForAllSegmentsToBeAvailable(dataSource, coordinator, broker);
     Assertions.assertEquals(String.valueOf(totalRecords.get()), cluster.runSql("SELECT COUNT(*) FROM %s", dataSource));
@@ -453,25 +412,4 @@ public class CostBasedAutoScalerIntegrationTest extends StreamIndexTestBase
     return supervisorSpec.getSpec().getIOConfig().getTaskCount();
   }
 
-  private static void throwPublisherFailureIfDone(Future<?> publisherFuture)
-      throws Exception
-  {
-    if (!publisherFuture.isDone()) {
-      return;
-    }
-
-    try {
-      publisherFuture.get();
-    }
-    catch (ExecutionException e) {
-      final Throwable cause = e.getCause();
-      if (cause instanceof Error) {
-        throw (Error) cause;
-      } else if (cause instanceof Exception) {
-        throw (Exception) cause;
-      } else {
-        throw e;
-      }
-    }
-  }
 }
